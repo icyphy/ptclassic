12ec6268271d2e36133f7be4c54cdf968dec0865
messer <messer@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-01-18 13:18:25 +0000

    made work with main.cc

--- trunk/ptclassic/src/kernel/Universe.h
+++ trunk/ptclassic/src/kernel/Universe.h
@@ -4,6 +4,7 @@
 #include "type.h"
 #include "Star.h"
 #include "Scheduler.h"
+#include "Galaxy.h"
 
 
 // SCCS version identification
@@ -13,8 +14,8 @@
 
 	Universes are defined in this file.
 	A Universe is a Star with a pointer to a component Galaxy,
-	and a scheduler to fire the blocks in the Galaxy.
-	From the outside, it looks like a star with no PortHoles.
+	and a Scheduler to fire the blocks in the Galaxy.
+	From the outside, it looks like a Star with no PortHoles.
 
 	Currently, only an SDFUniverse is defined here.
 
