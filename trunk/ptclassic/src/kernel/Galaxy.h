fb967e580458043756ac1f0380ece18447d265b8
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-03-19 17:59:31 +0000

    Combine the four connect methods into one (since MultiPortHole is now a derived type of PortHole we can do this). Add Galaxy::blockWithName and BlockList::blockWithName. Make KnownBlock (see KnownBlock.h) a friend of BlockList.

--- trunk/ptclassic/src/kernel/Galaxy.h
+++ trunk/ptclassic/src/kernel/Galaxy.h
@@ -6,9 +6,25 @@
 #include "Block.h"
 #include "Star.h"
 
+/**************************************************************************
+Version identification:
+$Id$
 
-// SCCS version identification
-// @(#)Galaxy.h	1.7	1/14/90
+ Copyright (c) 1990 The Regents of the University of California.
+                       All Rights Reserved.
+
+ Programmer:  E. A. Lee and D. G. Messerschmitt
+ Date of creation: 1/17/89
+ Revisions:
+    3/19/90 - J. Buck
+	Combine the four connect methods into one (since MultiPortHole
+	is now a derived type of PortHole we can do this).
+	Add Galaxy::blockWithName and BlockList::blockWithName.
+ 	Make KnownBlock (see KnownBlock.h) a friend of BlockList.
+
+Definition of the Galaxy class, together with the BlockList class.
+
+**************************************************************************/
 
 	////////////////////////////////////
 	// class BlockList
@@ -19,6 +35,7 @@
 class BlockList : SequentialList
 {
 	friend class Galaxy;
+	friend class KnownBlock;
 
 	// Add Block to list
 	void put(Block* b) {SequentialList::put(b);}
@@ -28,6 +45,10 @@ class BlockList : SequentialList
 
 	// Return next Block on list
 	Block& operator ++ () {return *(Block*) next();}
+
+	// Find a block with the given name and return pointer; NULL if not found
+protected:
+	Block* blockWithName (char* name);
 };
 
 	////////////////////////////////////
@@ -55,31 +76,6 @@ protected:
 	// Geodesic& connect(PortHole& source, PortHole& destination,
 			// int numberDelays, Particle& delayValue);
 
-	// connect is overloaded so that it can accept MultiPortHoles on
-	// either input.  Each such call to connect allocates a new PortHole,
-	// so in an interactive execution we have to be careful about
-	// disconnecting these connections.  We cannot simply remake a
-	// connection with another call to connect.
-	Geodesic& connect(MultiPortHole& source, PortHole& destination,
-			  int numberDelays = 0) {
-		return connect(source.realPort().newPort(),
-			       destination,
-			       numberDelays);
-	}
-	Geodesic& connect(PortHole& source, MultiPortHole& destination,
-			  int numberDelays = 0) {
-		return connect(source,
-			       destination.realPort().newPort(),
-			       numberDelays);
-	}
-	Geodesic& connect(MultiPortHole& source, MultiPortHole& destination,
-			  int numberDelays = 0) {
-		return connect(source.realPort().newPort(),
-			       destination.realPort().newPort(),
-			       numberDelays);
-	}
-
-
 	// Connect a Galaxy PortHole to a PortHole of a sub-block
 	alias(PortHole& galPort, PortHole& blockPort) {
 		galPort.alias = &blockPort;
@@ -90,6 +86,8 @@ protected:
 		galPort.alias = &blockPort;
 	}
 
+	// support blockWithName message to access internal block list
+	Block* blockWithName (char* name) {return blocks.blockWithName(name);}
 public:
 	// Return the number of blocks in the galaxy.
 	int numberBlocks() {return blocks.size();}
