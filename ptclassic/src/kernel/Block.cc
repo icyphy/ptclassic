44fd399980848994fedfd0f16c707532dc52aae5
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-03-19 17:49:14 +0000

    add the method Block::portWithName (for the interpreter)

--- ptclassic/src/kernel/Block.cc
+++ ptclassic/src/kernel/Block.cc
@@ -9,8 +9,11 @@ $Id$
                        All Rights Reserved.
 
  Programmer:  E. A. Lee and D. G. Messerschmitt
- Date of creation: 1/17/89
+ Date of creation: 1/17/90
  Revisions:
+	3/19/90 - J. Buck
+		  add method Block::portWithName
+		  returns a pointer to the PortHole with the given name
 
  Routines implementing class Block methods
 
@@ -52,4 +55,24 @@ void Block :: initialize()
 	// Call initialize() for each PortHole
 	for(int i = numberPorts(); i>0; i--)
 		nextPort().initialize();
+}
+
+// This method returns a PortHole corresponding to the given name.
+// If the name refers to a MultiPortHole, a new PortHole is created.
+// The real port is always returned (no need to check for aliases).
+PortHole *
+Block::portWithName (char *name) {
+	// If the MultiPortHole name matches, return that
+	// (this will create a new connection)
+	if (saveMPH && strcmp (name,saveMPH->readName()) == 0)
+		return &saveMPH->newConnection();
+	// Not found, search the port list
+	else for (int i=numberPorts(); i>0; i--) {
+		PortHole *pt;
+		pt = &nextPort();
+		if (strcmp (name, pt->readName()) == 0)
+			return &(pt->newConnection());
+	}
+	// Still not found, return NULL
+	return NULL;
 }
