0307ffd78d03e9ab8e501ca2ac03d7f6971e7870
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-04-12 15:31:20 +0000

    A major rewrite -- fix bugs, increase efficiency, implement += -= *= /=

--- trunk/ptclassic/src/kernel/Fraction.h
+++ trunk/ptclassic/src/kernel/Fraction.h
@@ -12,66 +12,91 @@
  This class represents fractions using integers for the numerator
  and denominator.
 
+ Thoroughly rewritten by Joe Buck to be more efficient, implement
+ assignment operators, and to work around some g++ bugs.
+
 */
 
-// The following structure is defined so that Euclid's algorithm
+// The following class is defined so that Euclid's algorithm
 // can return both the least common multiple and greatest common
 // divisor.
 
-struct GcdLcm {
+// The only way to build a GcdLcm is to start with a fraction: the
+// constructor builds one.
+
+class GcdLcm {
+	friend class Fraction;
 	int gcd;
 	int lcm;
+	GcdLcm(const Fraction& f);
 };
 
 class Fraction
 {
-public:
+	friend class GcdLcm;
 	int numerator;
 	int denominator;
-
+public:
 	// Constructors
-	Fraction () {
-		numerator = 0;
-		denominator = 1;
-	}
-	Fraction (int i) {
-		numerator = i;
-		denominator = 1;
-	}
-	Fraction (int i, int j) {
-		numerator = i;
-		denominator = j;
-	}
+	Fraction () : numerator(0), denominator(1) { }
+
+	Fraction (int i) : numerator(i), denominator(1) { }
+
+	Fraction (int i, int j) : numerator(i), denominator(j) { }
+
+	// Copy constructor
+	Fraction (const Fraction& a) : numerator(a.numerator),
+				       denominator(a.denominator) {}
 
 	// Cast to type double:
 	operator double() { return (double)numerator/(double)denominator; }
 
-	// Addition operator
-	friend Fraction operator + (Fraction, Fraction);
-
-	// Subtraction operator
-	friend Fraction operator - (Fraction, Fraction);
-
-	// Multiplication operator
-	friend Fraction operator * (Fraction, Fraction);
-
-	// Division operator
-	friend Fraction operator / (Fraction, Fraction);
+	// Assignment operators
+	Fraction& operator += (const Fraction& f);
+	Fraction& operator -= (const Fraction& f);
+	Fraction& operator *= (const Fraction& f);
+	Fraction& operator /= (const Fraction& f);
 
 	// Equality test operator
 	// This operator assumes the fractions have been simplified!!
-	friend int operator == (Fraction, Fraction);
+	friend int operator == (const Fraction&,const Fraction&);
 
 	// Print operator
-	operator char* ();
+	operator char* () const;
 
 	// Simplify the fraction (compute an equivalent fraction with
 	// numerator and denominator relatively prime).
 	// The simplified fraction replaces the original.
 	void simplify();
 
-	// Greatest common divisor and least common multiple function
-	GcdLcm computeGcdLcm();
 };
+
+// Notice: these functions don't have to be friends!
+// Design based on Andrew Koenig's method -- the temporaries
+// (res in the functions below) will almost always be optimized away
+
+inline Fraction operator+ (const Fraction& a,const Fraction& b) {
+	Fraction res(a);
+	res += b;
+	return res;
+}
+
+inline Fraction operator- (const Fraction& a,const Fraction& b) {
+	Fraction res(a);
+	res -= b;
+	return res;
+}
+
+inline Fraction operator* (const Fraction& a,const Fraction& b) {
+	Fraction res(a);
+	res *= b;
+	return res;
+}
+
+inline Fraction operator/ (const Fraction& a,const Fraction& b) {
+	Fraction res(a);
+	res /= b;
+	return res;
+}
 
 #endif
