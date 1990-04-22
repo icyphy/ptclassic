bf0e133cb23382e237caa151f3ad7e7c9ddaf857
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-04-22 13:02:44 +0000

    The class was completely rewritten, though the new interface is a superset of the old.  The class no longer leaks memory.  All substrings are allocated dynamically.  Casting to (char*) causes the dynamic memory to be rearranged and all substrings to be combined into one string.

--- ptclassic/src/kernel/StringList.cc
+++ ptclassic/src/kernel/StringList.cc
@@ -1,59 +1,80 @@
 #include <std.h>
 #include "StringList.h"
 
-#define SMALL_STRING 10
-
+#define SMALL_STRING 20
 
 // SCCS version identification
 // $Id$
 
-StringList :: operator = (char* s)
-	 {put(s);totalSize+=strlen(s);}
- 
-StringList :: operator += (char* s)
-	 {put(s);totalSize+=strlen(s);}
+// Note: all components of a StringList are in dynamic memory,
+// and are deleted by the StringList destructor
 
-StringList :: operator = (int i)
-{
-	char* s = new char[SMALL_STRING];
-	sprintf(s,"%d",i);
-	put(s);
-	totalSize+=strlen(s);
+// Add another StringList to the StringList
+StringList&
+StringList :: operator += (StringList& l) {
+	l.reset();
+
+	for(int i = l.size(); i > 0; i--) {
+		char* s = l.next();
+		char* n = new char[strlen(s)+1];
+		strcpy(n,s);
+		put(n);
+	}
+	totalSize += l.totalSize;
+	return *this;
 }
-  
+
+// Add in a char*
+StringList&
+StringList :: operator += (const char* s) {
+	int nadd = strlen(s);
+	char* n = new char[nadd+1];
+	strcpy(n,s);
+	put(n);
+	totalSize+=nadd;
+	return *this;
+}
+
+// Add in an int: uses the above method
+StringList&  
 StringList :: operator += (int i)
 {
-        char* s = new char[SMALL_STRING]; 
-        sprintf(s,"%d",i); 
-        put(s);
-        totalSize+=strlen(s);
+	char buf[SMALL_STRING];
+        sprintf(buf,"%d",i); 
+	return *this += buf;
 }
 
-StringList :: operator = (float f)
+// Add in a double: uses the above method
+StringList&
+StringList :: operator += (double f)
 {
-        char* s = new char[SMALL_STRING]; 
-        sprintf(s,"%f",f); 
-        put(s);
-        totalSize+=strlen(s);
-}
-  
-StringList :: operator += (float f)
-{
-        char* s = new char[SMALL_STRING]; 
-        sprintf(s,"%f",f); 
-        put(s);
-        totalSize+=strlen(s);
+	char buf[SMALL_STRING];
+        sprintf(buf,"%g",f);
+	return *this += buf;
 }
 
-StringList :: operator char* ()
-{
-	char* s = new char[totalSize];
+// This method consolidates all strings into one string and returns the
+// memory.  It is used in the cast to char*.
+char*
+StringList :: consolidate () {
+	// Handle empty StringList
+	if (totalSize == 0)
+		return 0;
+	// If already one segment, handle it w/o work
+	if (size() <= 1) return next();
+	// Allocate new memory
+	char* s = new char[totalSize+1];
 	int totalSoFar = 0;
 	reset();
 	for(int i = size(); i > 0; i--) {
 		char* ss = next();
 		strcpy(s+totalSoFar,ss);
+		delete ss;
 		totalSoFar += strlen(ss);
-		}
+	}
+	initialize();
+	put(s);
+	totalSize = totalSoFar;
 	return s;
 }
+
