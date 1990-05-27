71f5535c92b59ee6fdd2b3a8f7542eff28610181
kuroda <jkuroda@users.noreply.github.com>
1990-05-27 11:04:19 +0000

    Initial revision

---
+++ ptclassic/src/kernel/StringState.cc
@@ -0,0 +1,58 @@
+
+#include <std.h>
+#include "StringState.h"
+
+/**************************************************************************
+Version identification:
+$Id$
+
+ Copyright (c) 1990 The Regents of the University of California.
+                       All Rights Reserved.
+
+ Programmer:  I. Kuroda
+ Date of creation: 5/26/90
+ Revisions:
+
+ Functions for class StringState
+
+**************************************************************************/
+
+
+/*************************************************************************
+
+	class StringState methods
+
+**************************************************************************/
+
+void StringState  :: initialize() {
+	val = evalExpression(initValue, parent()->parent());
+}
+
+const char* StringState :: evalExpression(const char* string, Block*  blockIAmIn) {
+
+//	This part may  be used later for file read etc.
+//
+//	lexptr = string;
+//	ParseToken t = getParseToken(string, blockIAmIn);
+//	if(!strcmp(t.tok,"ID")) return ((StringState*)t.s)->val;
+//	else return 0;
+	return string;
+}
+
+StringState :: operator StringList() {
+                StringList  out;
+                out = "String State: ";
+                out += "initValue= ";
+                out += initValue;
+                out += "\t";
+                out += readName();
+                out += "\t";
+                out += "val= ";
+                out += val;
+                out += "\n";
+        return  out;
+};
+
+static StringState proto;
+static KnownState entry(proto,"string");
+	 
