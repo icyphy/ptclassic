6dafea765335c4fe6d4bdd0c6e1f88aa73d8cb80
edg <edg@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-05-27 14:08:48 +0000

    Change default palette definition

--- ptclassic/src/pigilib/edit.c
+++ ptclassic/src/pigilib/edit.c
@@ -222,7 +222,7 @@ long userOptionWord;
 /* Structures to store palette list and dialog box */
 static char **palettes;
 static int palettes_n;
-static char *defaultPalettes = "~ptolemy/lib/sdf/sdf.pal:./user:./init";
+static char *defaultPalettes = "~ptolemy/lib/sdf/icon/sdf.pal:./user.pal:./init.pal";
 static dmWhichItem *items;
 
 /* ListLength  4/28/88
