002cd833eb284b8c428abdcfcbfc7a1db306bfc7
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-05-29 19:04:22 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/kernel/SDFStar.cc
@@ -0,0 +1,52 @@
+/******************************************************************
+Version identification:
+$Id$
+
+ Copyright (c) 1990 The Regents of the University of California.
+                       All Rights Reserved.
+
+ Programmer:  E. A. Lee and D. G. Messerschmitt
+ Date of creation: 5/29/90
+ Split off from Star.h by J. Buck
+
+*******************************************************************/
+
+#include "SDFStar.h"
+#include "SDFConnect.h"
+
+/*******************************************************************
+
+	class SDFStar methods
+
+********************************************************************/
+
+
+void SDFStar :: beforeGo()
+{
+        for(int i = numberPorts(); i>0; i--) {
+                SDFPortHole& port = (SDFPortHole&)nextPort();
+                port.beforeGo();
+                }
+}
+ 
+void SDFStar :: afterGo()
+{
+        for(int i = numberPorts(); i>0; i--) {
+                SDFPortHole& port = (SDFPortHole&)nextPort();
+                port.afterGo();
+                }
+}
+
+// Redefine method setting internal data in the Block
+// so that various SDF-specific initilizations can be performed.
+// If the parent pointer is not provied, it defaults to NULL
+Block& SDFStar :: setBlock(char* s, Block* parent = NULL) {
+	// First invoke the generic setBlock
+	Block::setBlock(s,parent);
+
+	// Then perform SDF-specific initializations
+	repetitions = 0;
+	noTimes = 0;
+
+	return *this;
+}
