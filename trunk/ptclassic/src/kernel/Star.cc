e2f9ac672d0f35692b3323c8ed4c5301081f7a78
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-05-29 18:48:48 +0000

    move SDFStar stuff to SDFStar.cc; change StringList cast to printVerbose function

--- trunk/ptclassic/src/kernel/Star.cc
+++ trunk/ptclassic/src/kernel/Star.cc
@@ -8,6 +8,8 @@ $Id$
  Programmer:  E. A. Lee and D. G. Messerschmitt
  Date of creation: 12/15/89
 
+ SDF-specific stuff moved to SDFStar.cc, 5/29/90.
+
 *******************************************************************/
 #include "Star.h"
 #include "StringList.h"
@@ -18,7 +20,8 @@ $Id$
 
 ********************************************************************/
 
-Star :: operator StringList () {
+StringList
+Star :: printVerbose () {
 	StringList out;
 	out = "Star: ";
 	out += readFullName();
@@ -29,41 +32,4 @@ Star :: operator StringList () {
 	out += printPorts ("star");
         out += printStates("star");
 	return out;
-}
-
-/*******************************************************************
-
-	class SDFStar methods
-
-********************************************************************/
-
-
-void SDFStar :: beforeGo()
-{
-        for(int i = numberPorts(); i>0; i--) {
-                SDFPortHole& port = (SDFPortHole&)nextPort();
-                port.beforeGo();
-                }
-}
- 
-void SDFStar :: afterGo()
-{
-        for(int i = numberPorts(); i>0; i--) {
-                SDFPortHole& port = (SDFPortHole&)nextPort();
-                port.afterGo();
-                }
-}
-
-// Redefine method setting internal data in the Block
-// so that various SDF-specific initilizations can be performed.
-// If the parent pointer is not provied, it defaults to NULL
-Block& SDFStar :: setBlock(char* s, Block* parent = NULL) {
-	// First invoke the generic setBlock
-	Block::setBlock(s,parent);
-
-	// Then perform SDF-specific initializations
-	repetitions = 0;
-	noTimes = 0;
-
-	return *this;
 }
