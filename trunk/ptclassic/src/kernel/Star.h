456b4c749445ca7cf1986e733bd5563c1e89ab96
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-05-29 18:49:52 +0000

    move SDF-specific stuff to SDFStar.h; StringList cast now named printVerbose

--- trunk/ptclassic/src/kernel/Star.h
+++ trunk/ptclassic/src/kernel/Star.h
@@ -16,9 +16,7 @@ $Id$
 #define _Star_h 1
 
 #include "type.h"
-#include "Connect.h"
 #include "Block.h"
-#include "Output.h"
 
 
 	////////////////////////////////////
@@ -47,49 +45,8 @@ public:
         virtual void wrapup() {};
 
 	// Method to print out description
-	operator StringList ();
+	StringList printVerbose ();
 
-        // initialize states
-        void initState() { Block::initState();}
-};
-
-#include "Fraction.h"
-
-	////////////////////////////////////
-	// class SDFStar
-	////////////////////////////////////
-
-class SDFStar : public Star  {
-
-public:
-	// The number of repetitions of the star in a periodic
-	// schedule.  Initialized to 1 by setBlock.  Set to correct
-	// value by an SDF scheduler.  It is represented as a fraction
-	// for the convenience of the scheduler.
-	Fraction repetitions;
-
-	// During scheduling, the scheduler must keep track of how
-	// many times it has scheduled a star.  This is a convenient
-	// place to do that.
-	unsigned noTimes;
-
-// The following methods are provided by the system and accessed
-// by the Scheduler
-
-	// Methods called by the Scheduler before go() and
-	//  after go(); they call methods of the same name for each
-	//  input or output PortHole
-	// They can be used to do things like get Particles from
-	//  input Geodesics, put Particles onto output Geodesics, etc.
-	// In general behavior can be different for different types
-	//  of PortHoles
-	void beforeGo();
-	void afterGo();
-
-	// Redefine method setting internal data in the Block
-	// so that various SDF-specific initilizations can be performed.
-	// If the parent pointer is not provied, it defaults to NULL
-	Block& setBlock(char* starName, Block* parent = NULL);
 };
 
 #endif
