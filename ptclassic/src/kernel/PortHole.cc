cf1b8195b812437346522b9f1cbde28cba9bce07
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-05-29 18:59:39 +0000

    setPlasma now allows dissimilar types to be connected; the plasma for the input porthole is used.  Hopefully the user is doing the right thing. SDF types are now in SDFConnect.cc.  cast to StringList now printVerbose().

--- ptclassic/src/kernel/PortHole.cc
+++ ptclassic/src/kernel/PortHole.cc
@@ -11,15 +11,17 @@ $Id$
                        All Rights Reserved.
 
  Programmer:  E. A. Lee and D. G. Messerschmitt
- Date of creation: 1/17/89
+ Date of creation: 1/17/90
  Revisions:
 
-Code for functions declared in Connection.h
+Code for functions declared in Connect.h
 
 **************************************************************************/
 
 extern Error errorHandler;
 extern PlasmaList plasmaList;
+
+// Methods for CircularBuffer
 
 CircularBuffer :: CircularBuffer(int i)
 {
@@ -71,6 +73,36 @@ Pointer* CircularBuffer :: previous(int i)
         return buffer + ((current - i) % dimen);
 }
 
+// The connect method
+// This method is virtual and may be overridden
+
+void GenericPort :: connect(GenericPort& destination,int numberDelays)
+{
+	// Resolve any aliases and MultiPortHole stuff:
+	// newConnection is a virtual function that does the right
+	// thing for all types of PortHoles.
+	PortHole& realSource = newConnection();
+	PortHole& realDest = destination.newConnection();
+
+	Geodesic* geo = realSource.allocateGeodesic();
+	geo->originatingPort = &realSource;
+	geo->destinationPort = &realDest;
+	realSource.myGeodesic = geo;
+	realDest.myGeodesic = geo;
+
+	// Set the farSidePort pointers in both blocks
+	// This information is redundant, since it also appears in the
+	// Geodesic, but to get it from the Geodesic, you have to know
+	// which PortHole is an input, and which is an output.
+	realSource.farSidePort = &realDest;
+	realDest.farSidePort = &realSource;
+
+	// Set the number of delays
+	geo->numInitialParticles = numberDelays;
+
+	return;
+}
+	
 void PortHole :: allocateBuffer()
 {
 	// If there is a buffer, release its memory
@@ -100,37 +132,17 @@ PortHole& PortHole :: setPort(const char* s,
 	GenericPort::setPort (s, parent, t);
         myGeodesic = NULL;
 	myBuffer = NULL;
-	bufferSize = numberTokens;
 	numberTokens = 1;
+	bufferSize = numberTokens;
         farSidePort = NULL;
 	if (t != ANYTYPE)
 		myPlasma = plasmaList.getPlasma(t);
         return *this;
 }
 
-PortHole& SDFPortHole :: setPort (
-			     const char* s,
-                             Block* parent,
-                             dataType t = FLOAT,
-                             unsigned numTokens = 1,
-			     unsigned delay = 0)
-{
-	// Initialize PortHole
-        PortHole::setPort(s,parent,t);
-
-	// Based on the delay, we allocate a Buffer assuming no
-	// past Particles are allocated
-        numberTokens = numTokens;
-	// The number of Particles the buffer has to hold is:
-	//		numberTokens current and future Particles
-	//		delay past Particles
-	bufferSize = numberTokens + delay;
-
-        return *this;
-}
-
 // Print a Generic Port
-GenericPort :: operator StringList () {
+StringList
+GenericPort :: printVerbose () {
         StringList out;
  
         if(isItInput())
@@ -151,9 +163,10 @@ GenericPort :: operator StringList () {
 	return out;
 }
 
-PortHole :: operator StringList () {
+StringList
+PortHole :: printVerbose () {
 	StringList out;
-	out = GenericPort:: operator StringList ();
+	out = GenericPort::printVerbose();
 	if (alias == NULL) {
            if (farSidePort != NULL) {
               out += "    Connected to port: ";
@@ -166,21 +179,23 @@ PortHole :: operator StringList () {
         return out;
 }
 
-MultiPortHole :: operator StringList () {
+StringList
+MultiPortHole :: printVerbose () {
 	StringList out;
 	out = "Multi ";
-	out += GenericPort:: operator StringList ();
+	out += GenericPort::printVerbose();
 	out += "This MultiPortHole contains ";
 	out += numberPorts();
 	out += " PortHoles.\n";
-// Should add stuff on the PortList
+// We don't add stuff on the PortList, since that will be printed
+// out by the enclosing Block::printPorts.
 	return out;
 }
 
 Plasma*
 PortHole :: setPlasma () {
-	// first case: my type is known
-	if (myPlasma) return myPlasma;
+	// first case: my type is known, and I'm an input.  Leave alone.
+	if (myPlasma && isItInput()) return myPlasma;
 	// second case: I've been told where to look for the type
 	if (typePort)
 		myPlasma = typePort->setPlasma();
@@ -244,85 +259,6 @@ Particle& PortHole ::  operator % (int delay)
                 "Attempt to access nonexistent input Particle"
 			);
         return *(Particle*)*p;
-}
-
-void InSDFPort :: beforeGo()
-{
-// It is assumed this routine is called before a Star executes...
-// It gets numberTokens Particles from the Geodesic and stores them
-// in the buffer, then setting current time to the last Particle input
-
-	for(int i = numberTokens; i>0; i--)
-		{
-		// Move the current time pointer in the buffer
-		Pointer* p = myBuffer->next();
-
-                // Put current Particle back into Plasma  to be
-		// recycled back to some OutSDFPort
-		myPlasma->put((Particle*)*p);
- 
-		// Get another Particle from the Geodesic
-        	*p = myGeodesic->get();
-		}
-}
-
-void OutSDFPort :: beforeGo()
-{
-	Pointer* p;
-
-// It is assumed this routine is called before a Star executes....
-// It moves current by numberTokens in the buffer, initializing
-//  each Particle; after the Star executes these Particles will
-//  contain data destined for another Star, and will be launched
-//  into the Geodesic
-
-// For this case, the buffer always stores the same set of
-// Particles -- copies of these Particles are launched into the
-// Geodesic so that there is no possibility of modifying a Particle
-// that resides in the Geodesic or InSDFPort on the other side
-
-// After this routine executes, the buffer current will point to
-// the last Particle, so the operator% references Particles relative
-// to this time
-
-	for(int i = numberTokens; i>0; i--) {
-	
-		// Get and initialize next Particle
-        	p = myBuffer->next();
-		((Particle*)*p)->initialize();
-		}
-}
-
-void OutSDFPort :: afterGo()
-{
-	Pointer* p;
-
-// This method is called after go(); the buffer now contains numberTokens
-// Particles that are to be send to the output Geodesic
-// We send copies to prevent the Star from modifying Particles in the
-// Geodesic or InPortHole on the other side
-
-	// Back up in the buffer by numberTokens
-	for(int i = numberTokens; i>0; i--)
-        	p = myBuffer->last();
-
-	// Now move forward numberTokens times, sending copy of
-	// Particle to Geodesic
-	for(i = numberTokens; i>0; i--) {
-		p = myBuffer->next();
-		
-		// Get Particle from Plasma
-		Particle* pp = myPlasma->get();
-
-		// Copy from the buffer to this Particle
-		*pp = *(Particle*)*p;
-
-		// Launch this Particle into the Geodesic
-		myGeodesic->put(pp);
-		}
-
-	// Note that the Particle is left in the buffer to be
-	// accessed as a past Particle
 }
 
 Particle& InOtherPort :: operator ++ ()
@@ -396,36 +332,6 @@ PortHole& MultiPortHole :: newPort() {
         return *newport;
 }
 
-
-MultiPortHole& MultiSDFPort :: setPort (const char* s,
-                             Block* parent,
-                             dataType t = FLOAT,
-                             unsigned numTokens = 1) {
-        MultiPortHole::setPort(s,parent,t);
-        numberTokens = numTokens;
-        return *this;
-}
-
-PortHole& MultiInSDFPort :: newPort () {
-        InSDFPort* newport = new InSDFPort;
-        ports.put(*newport);
-        parent()->
-            addPort(newport->
-                        setPort(newName(), parent(), type, numberTokens));
-	newport->typePort = typePort;
-        return *newport;
-}
- 
- 
-PortHole& MultiOutSDFPort :: newPort () {
-        OutSDFPort* newport = new OutSDFPort;
-        ports.put(*newport);
-        parent()->
-            addPort(newport->
-                        setPort(newName(), parent(), type, numberTokens));
-	newport->typePort = typePort;
-        return *newport;
-}
 
 Particle* Geodesic::get()
 {
