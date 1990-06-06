0c7d53d0bf7dd81672e5e430188c3cdd82bd2c70
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-06-06 19:26:28 +0000

    No longer anything left for these files(moved to Universe.cc)

--- ptclassic/src/domains/sdf/kernel/SDFUniverse.cc
+++ ptclassic/src/domains/sdf/kernel/SDFUniverse.cc
@@ -1,22 +1,4 @@
-#include "SDFUniverse.h"
-#include "StringList.h"
-
-
 // SCCS version identification
 // $Id$
 
-StringList
-SDFUniverse :: printVerbose () {
-	StringList out;
-	out = "SYNCHRONOUS DATAFLOW UNIVERSE: ";
-	out += readFullName ();
-	out += "\n";
-	out += "Descriptor: ";
-	out += readDescriptor();
-	out += "\n";
-	out += "CONTENTS:\n";
-
-	out += myTopology->printVerbose();
-
-	return out;
-}
+// There are currently no methods here.
