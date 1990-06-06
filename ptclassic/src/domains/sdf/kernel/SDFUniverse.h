def18ece3173c3aa4feb1d9d72a97d81d636387f
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-06-06 19:25:53 +0000

    This is now derived from Universe, not SDFStar

--- ptclassic/src/domains/sdf/kernel/SDFUniverse.h
+++ ptclassic/src/domains/sdf/kernel/SDFUniverse.h
@@ -1,53 +1,31 @@
 #ifndef _SDFUniverse_h
 #define _SDFUniverse_h 1
 
-#include "type.h"
-#include "SDFStar.h"
+#include "Universe.h"
 #include "SDFScheduler.h"
-#include "Galaxy.h"
-#include "StringList.h"
-
 
 // SCCS version identification
 // $Id$
 
 /*******************************************************************
-	This file used to be named Universe.h.
 
-	An SDF Universe is an SDF Star with a pointer to a component Galaxy,
+	An SDF Universe is a Star with a pointer to a component Galaxy,
 	and an SDF Scheduler to fire the blocks in the Galaxy.
 	From the outside, it looks like a Star with no PortHoles.
 
+	Revised to be a type of Universe: J. Buck, 6/6/90
 ********************************************************************/
 
 	//////////////////////////////
 	// SDFUniverse
 	//////////////////////////////
+
+const char SDFstring[] = "SYNCHRONOUS DATAFLOW";
 	
-class SDFUniverse : public SDFStar {
-	SDFScheduler scheduler;
+class SDFUniverse : public Universe {
 public:
-	// Temporarily make this public:  Maybe can be made private later
-	// with scheduler as a friend.
-	Block* myTopology;
-
-	void initialize() {scheduler.setup(*myTopology);}
-	void go() {scheduler.run(*myTopology);}
-	void wrapup () {scheduler.wrapup(*myTopology);}
-
-	// The "full dump"
-	StringList printVerbose ();
-
-	// Display the schedule
-	StringList displaySchedule() {return scheduler.displaySchedule();}
-
-	// Define clone() for convenience so Universe writers don't need to
-	// Since it returns NULL, clone will fail unless redefined.
-	Block* clone() { return NULL;}
-protected:
-	// The addBlock method should get invoked only once, with a
-	// reference to the component galaxy specified.
-	void addBlock(Block& b) {myTopology = &b;}
+	SDFUniverse(Galaxy& g) : Universe(new SDFScheduler,SDFstring,&g) {}
+	SDFUniverse() : Universe(new SDFScheduler,SDFstring,0) {}
 };
 
 #endif
