24711b1972375dd1f434716f1c7c9579258f095c
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-06-06 19:25:19 +0000

    This is now derived from Universe, not DEStar

--- trunk/ptclassic/src/domains/de/kernel/DEUniverse.h
+++ trunk/ptclassic/src/domains/de/kernel/DEUniverse.h
@@ -1,12 +1,8 @@
 #ifndef _DEUniverse_h
 #define _DEUniverse_h 1
 
-#include "type.h"
-#include "DEStar.h"
+#include "Universe.h"
 #include "DEScheduler.h"
-#include "Galaxy.h"
-#include "StringList.h"
-
 
 // SCCS version identification
 // $Id$
@@ -14,46 +10,22 @@
 /*******************************************************************
 
 	Programmer : Soonhoi Ha
-	Description : DEUniverse is also defined here.
+	Description : DEUniverse is defined here.
 	Modification date : 5/30/90 - 
 
+	Revised to be a type of Universe: J. Buck, 6/6/90
 ********************************************************************/
 
 	//////////////////////////////
 	// DEUniverse
 	//////////////////////////////
+
+const char DEstring[] = "DISCRETE EVENT";
 	
-class DEUniverse : public DEStar {
-	DEScheduler scheduler;
+class DEUniverse : public Universe {
 public:
-	// Temporarily make this public:  Maybe can be made private later
-	// with scheduler as a friend.
-	Block* myTopology;
-
-	// setup the stopping condition
-	void setStopTime(float limit) 
-		{scheduler.setStopTime(limit) ;}
-
-	void initialize() {scheduler.setup(*myTopology);}
-	void go()
-		{scheduler.run(*myTopology);}
-	void wrapup () {scheduler.wrapup(*myTopology);}
-
-	// Display the schedule
-	StringList displaySchedule() 
-		{return scheduler.displaySchedule(*myTopology);}
-
-	// Print it out
-	StringList printVerbose ();
-
-	// Define clone() for convenience so Universe writers don't need to
-	// Since it returns NULL, clone will fail unless redefined.
-	Block* clone() { return NULL;}
-
-protected:
-	// The addBlock method should get invoked only once, with a
-	// reference to the component galaxy specified.
-	void addBlock(Block& b) {myTopology = &b;}
+	DEUniverse(Galaxy& g) : Universe(new DEScheduler,DEstring,&g) {}
+	DEUniverse() : Universe(new DEScheduler,DEstring,0) {}
 };
 
 #endif
