a19c9291b243b4bf51cbd7d762d4586ff53d08e0
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-06-21 15:18:07 +0000

    The virtual methods start() and wrapup() are now rooted in class Block (Block.h) rather than Star (Star.h).

--- ptclassic/src/kernel/Star.h
+++ ptclassic/src/kernel/Star.h
@@ -15,9 +15,7 @@ $Id$
 #ifndef _Star_h
 #define _Star_h 1
 
-#include "type.h"
 #include "Block.h"
-
 
 	////////////////////////////////////
 	// class Star
@@ -25,24 +23,15 @@ $Id$
 
 class Star : public Block  {
 
-// Have to make the following public or make every individual
-// scheduler a friend.  It is not sufficient to make the base class
-// Scheduler a friend (is this yet another language defect?).
-
 public:
 // The writer of a Star code has the option to
-// provide the following methods
+// provide the methods start, go, and wrapup.
+// Any Block, however, may have start and wrapup.
 
-	// Start the simulation; i.e. do everything necessary
-	// to start over, like initializing state variables
-	virtual void start() {};
-
+	// User supplied method.
 	// Run the simulation, accessing Particles on the
 	// input PortHoles, generating Particles on the output PortHoles
         virtual void go() {}; 
-
-	// Perform functions at wrapup, such as collecting stats
-        virtual void wrapup() {};
 
 	// Method to print out description
 	StringList printVerbose ();
