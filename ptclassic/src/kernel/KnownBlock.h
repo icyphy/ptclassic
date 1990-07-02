42d53ea3464fac6ef6a612c5e20acfda55df8dfc
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-07-02 19:40:30 +0000

    add support for wormholes -- move some domain bookkeeping to the Domain class

--- ptclassic/src/kernel/KnownBlock.h
+++ ptclassic/src/kernel/KnownBlock.h
@@ -14,8 +14,6 @@ This class provides a list of all known blocks.  There is a separate
 list for each domain (corresponding to blocks that may be run by
 a specific scheduler).
 
-This class also provides a list of all known schedulers.
-
 The idea is that each star or galaxy that is "known to the system"
 should add an instance of itself to the known list by code something
 like
@@ -24,11 +22,8 @@ static MyType proto;
 static KnownBlock entry(proto,"MyType");
 
 Then the static method KnownBlock::clone(name) can produce a new
-instance of class name.  Similarly, schedulers do the exact same
+instance of class name.
 thing:
-
-static MySched proto;
-static KnownBlock entry(proto,"MyDomain");
 
 *******************************************************************/
 #ifndef _KnownBlock_h
@@ -37,6 +32,7 @@ static KnownBlock entry(proto,"MyDomain");
 #include "Block.h"
 #include "Galaxy.h"
 #include "StringList.h"
+#include "Domain.h"
 
 class Scheduler;
 
@@ -46,16 +42,12 @@ class KnownListEntry {
 	KnownListEntry *next;
 };
 
-const int NUMDOMAINS = 10;
-
 class KnownBlock {
 private:
 // The known block lists, one per domain
 	static KnownListEntry* allBlocks[NUMDOMAINS];
 // The set of domain names
 	static const char* domainNames[NUMDOMAINS];
-// A scheduler for each domain
-	static Scheduler* allSchedulers[NUMDOMAINS];
 
 	static int currentDomain;
 	static int numDomains;
@@ -68,9 +60,6 @@ public:
 // The constructor takes a block and a name, and adds a corresponding
 // entry to the known list.
 	KnownBlock (Block &block, const char* name);
-
-// Alternate constructor, creates a scheduler entry.
-	KnownBlock (Scheduler& sched, const char* domain);
 
 // The find method returns a pointer the appropriate block in
 // the current domain.
