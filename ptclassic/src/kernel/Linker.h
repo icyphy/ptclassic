6a6c1052ffda28d96083457b43094a39ce6d3f45
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-07-30 14:20:19 +0000

    Initial revision

---
+++ ptclassic/src/kernel/Linker.h
@@ -0,0 +1,20 @@
+/**************************************************************************
+Version identification:
+$Id$
+
+ Copyright (c) 1990 The Regents of the University of California.
+                       All Rights Reserved.
+
+ Programmer:  J. Buck
+ Date of creation: 7/30/90
+
+ This class implements support for incremental linking.
+**************************************************************************/
+
+class Linker {
+public:
+	static void init(const char* myName);
+	static int linkObj(const char* objName);
+private:
+	static const char* ptolemyName;
+};
