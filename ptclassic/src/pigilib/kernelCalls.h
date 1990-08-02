f006716e6b8c9cc15f2749b3ecc2399a6b9f7a88
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-08-02 16:58:26 +0000

    move declarations out of kernelCalls.cc to here

--- ptclassic/src/pigilib/kernelCalls.h
+++ ptclassic/src/pigilib/kernelCalls.h
@@ -2,3 +2,54 @@
 Version identification:
 $Id$
 */
+
+#include "InterpGalaxy.h"
+#include "Universe.h"
+
+/* boolean data type for pigi */
+typedef int boolean;
+
+/* error handling function for pigi */
+extern "C" void ErrAdd(char*);
+
+// Terminal data structs pigi
+#define TERM_ARR_MAX 14  /* max # I/O terms = max # positions */
+struct Term {
+    char *name;
+    boolean multiple;
+};
+
+struct TermList {
+    Term in[TERM_ARR_MAX];
+    int in_n;
+    Term out[TERM_ARR_MAX];
+    int out_n;
+};
+
+// Parameter structs for pigi
+struct ParamStruct {
+    const char *name;
+    const char *value;
+};
+typedef struct ParamStruct ParamType;
+
+struct ParamListStruct {
+    int length; /* length of array */
+    ParamType *array; /* points to first element */
+};
+typedef struct ParamListStruct ParamListType;
+
+// An InterpUniverse is an Universe with an interpreted galaxy
+// in it.  This is accomplished by making it Runnable and an InterpGalaxy.
+
+class InterpUniverse : public InterpGalaxy, public Runnable {
+public:
+        InterpUniverse () : Runnable(KnownBlock::newSched(),
+                                     KnownBlock::domain(),this)
+        { setBlock("mainGalaxy",NULL);}
+        void newSched() {
+                delete scheduler;
+                scheduler = KnownBlock::newSched();
+                type = KnownBlock::domain();
+        }
+};
