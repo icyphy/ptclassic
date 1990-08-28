840b853ea51b1a7fe096c97768efe411b44b33d6
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-08-28 12:14:11 +0000

    reduce two states into one. currentTime is now invisible.

--- trunk/ptclassic/src/domains/sdf/kernel/SDFWormhole.cc
+++ trunk/ptclassic/src/domains/sdf/kernel/SDFWormhole.cc
@@ -25,34 +25,22 @@ extern Error errorHandler;
 
 
 void SDFWormhole :: go() {
-	// The following three lines had better be moved between
-	// start() and go(), since the state is adjusted after the
-	// compile time scheduling. Currently, no method is calling
-	// after scheduling before running. How about add adjustStates()
-	// to Star class and call it after scheduling?
 
-	// set delay states.
-	messageProcessingTime = double(iterationPeriod) / double(repetitions);
-
-	// inform sampler stars about samplingPeriod.
-	gal.setState("samplingPeriod", 
-		form("%f",float(messageProcessingTime)));
+	// inform sampler stars about sampling time.
+	gal.setState("samplingTime", 
+		form("%f",mySched()->currentTime));
 	gal.initState();
 
 	run();
 
-	// Adjust currentTime for next invocation.
-	currentTime += messageProcessingTime;
 }
 
 // Constructor
 SDFWormhole :: SDFWormhole(Galaxy& g) : Wormhole(*this,g)
 {
 	buildEventHorizons ();
-	addState(iterationPeriod.setState("iterationPeriod", this,
-		  "10.0", "simulated iteration Period of SDF system"));
-	gal.addState(samplingPeriod.setState("samplingPeriod",&gal,"10.0",
-		     "time increment for sampling stars"));
+	gal.addState(samplingTime.setState("samplingTime",&gal,"10.0",
+		     "sampling time for sampler stars"));
 }
 
 
