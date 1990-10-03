11f0d901698234b83cfd66abfb05ab9d82498f6f
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-10-03 12:08:28 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/stars/SDFSub.pl
@@ -0,0 +1,40 @@
+ident {
+// Output = pos input minus all neg inputs
+// Version identification:
+// $Id$
+
+// Copyright (c) 1990 The Regents of the University of California.
+//                       All Rights Reserved.
+
+// Programmer: E. A. Lee
+// Date of creation: 9/19/90
+// Converted to use preprocessor: 10/3/90 by JTB
+}
+defstar {
+	name { FloatDiff }
+	domain { SDF }
+	desc { "Output the pos input minus all neg inputs" }
+	input {
+		name { pos }
+		type { float }
+	}
+	inmulti {
+		name { neg }
+		type { float }
+	}
+	output {
+		name { output }
+		type { float }
+	}
+	start {
+		neg.reset();
+	}
+	go {
+		float diff;
+		diff = float(pos%0);
+		for(int i=neg.numberPorts(); i>0; i--)
+			diff -= float(neg()%0);
+		output%0 << diff;
+	}
+}
+
