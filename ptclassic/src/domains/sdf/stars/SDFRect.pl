503c504351b491fe92e57ebd6a3bb6666a6c7872
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-10-03 12:42:57 +0000

    Initial revision

---
+++ ptclassic/src/domains/sdf/stars/SDFRect.pl
@@ -0,0 +1,48 @@
+ident {
+// $Id$
+// This star produces an impulse or impulse train.
+// From the gabriel "impulse" star.
+// Programmer: J. Buck
+// Date of creation: 5/31/90
+// Converted to use preprocessor: 10/3/90
+
+// Copyright (c) 1990 The Regents of the University of California.
+//			All Rights Reserved.
+}
+defstar {
+	name { FloatRect }
+	domain { SDF }
+	desc {
+	   "Generates an rectangular pulse of height 'height' (default 1.0).\n"
+	   "with width 'width' (default 8)."
+	}
+	output {
+		name { output }
+		type { float }
+	}
+	defstate {
+		name { height }
+		type { float }
+		default { 1.0 }
+		desc { "height of rectangular pulse" }
+	}
+	defstate {
+		name { width }
+		type { int }
+		default { 8 }
+		desc { "width of rectangular pulse" }
+	}
+	defstate {
+		name { count }
+		type { int }
+		default { 0 }
+		desc { "internal counting state" }
+	}
+	go {
+		double t = 0.0;
+		if (int(count) < int(width)) t = height;
+		count = int(count) + 1;
+		output%0 << t;
+	}
+}
+
