cc7db9a3c4e65dd95fb0f69634abf02cde6ac6e2
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-10-30 11:25:52 +0000

    initialize a member in ToEventHorizon class

--- ptclassic/src/kernel/EventHorizon.cc
+++ ptclassic/src/kernel/EventHorizon.cc
@@ -147,6 +147,7 @@ void ToEventHorizon :: initialize()
 	// Initialize members
 	timeStamp = 0.0;
 	dataNew = FALSE;
+	outputBeforeDeadlocked = FALSE;
 
 	// if on the boundary, call ghostPort :: initialize()
 	if (isItInput())
