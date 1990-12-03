b748d8e296761be3cf2370cae28284e00b665ba9
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-12-03 18:01:08 +0000

    maintain a list of known predefined global states together with the list of state types.  lookup returns global states.  find now returns a const State* pointer, as does lookup.

--- ptclassic/src/kernel/KnownState.h
+++ ptclassic/src/kernel/KnownState.h
@@ -18,7 +18,7 @@ The idea is that each star or galaxy that is "known to the system"
 should add an instance of itself to the known list by code something
 like
 
-static MyType proto;
+static const MyType proto;
 static KnownState entry(proto,"MyType");
 
 Then the static method KnownBlock::clone(name) can produce a new
@@ -37,22 +37,32 @@ class KnownState {
 // the constructor is called.
 
         static StateList *allStates;
+	static StateList *allGlobals;
         static int numStates;
+	static int numGlobals;
 public:
 
 // The constructor takes a state and a name, and adds a corresponding
 // entry to the known list.
         KnownState (State &state, const char* name);
 
+// This constructor takes a state, a name, and a value, and adds a
+// corresponding entry to the global symbol list.
+	KnownState (State& state, const char* name, const char* value);
+
 // The find method returns a pointer the appropriate state in
 // allstates.
-        static State* find (const char* name);
+        static const State* find (const char* type);
+
+// The lookup method returns a pointer to the appropriate state in
+// allGlobals.
+	static const State* lookup (const char* name);
 
 // The clone method takes a string, finds the appropriate state in
 // allstates, and returns a clone of that block.
-        static State* clone (const char* name);
+        static State* clone (const char* type);
 
-// Return the names of all the known states.
+// Return the names of all the known state types.
         static StringList nameList();
 
 // Return the number of known states.
