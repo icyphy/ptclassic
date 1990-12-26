576a1ad7b2d449a9b3294c2b9d99b94c60c65542
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-12-26 13:09:11 +0000

    Initial revision

---
+++ ptclassic/src/domains/ddf/stars/ddfstars.mk
@@ -0,0 +1,16 @@
+# $Id$
+#
+# This file contains a list of the official DDF stars.  It is intended
+# to be included in other makefiles.  Before it is included, the variable
+# DDF must be defined to refer to the directory containing the object files
+# for the stars.
+
+DDFSTARS= \
+ $(DDF)/DDFSelf.o \
+ $(DDF)/DDFDownCounter.o \
+ $(DDF)/DDFLastOfN.o \
+ $(DDF)/DDFRepeater.o \
+ $(DDF)/DDFSelect.o \
+ $(DDF)/DDFSwitch.o
+
+
