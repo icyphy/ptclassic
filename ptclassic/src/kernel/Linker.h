fbaa8b22fc660016add8dcaade4f8e91e494c0c8
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1990-12-31 13:32:46 +0000

    add a mechanism for letting other modules know when linker is running (so objects may be marked as dynamically linked)

--- ptclassic/src/kernel/Linker.h
+++ ptclassic/src/kernel/Linker.h
@@ -13,10 +13,19 @@ $Id$
 
 class Linker {
 public:
+// initialization function.  Must be called first.
 	static void init(const char* myName);
+
+// link in a new star.
 	static int linkObj(const char* objName);
+
+// check whether linker is active (so objects can be marked as dynamically
+// linked).  Actually it's set while constructors or other functions that
+// have just been linked are being run.
+	static int isActive() { return activeFlag;}
 private:
 	static const char* genHeader(const char*);
 	static const char* ptolemyName;
 	static int pid;
+	static int activeFlag;
 };
