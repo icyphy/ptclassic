9ab9b07fdb09ce46e879c7a980f1dd96e31f3a99
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-01-10 17:52:13 +0000

    changed to use auto documentation

--- trunk/ptclassic/src/domains/ddf/stars/DDFDownCounter.pl
+++ trunk/ptclassic/src/domains/ddf/stars/DDFDownCounter.pl
@@ -1,19 +1,14 @@
-ident {
-// Copyright (c) 1990 The Regents of the University of California.
-//			All Rights Reserved.
-
-// This star generates (int-1), (int-2), ... 1,0 samples for (int) input.
-// Written by: Soonhoi Ha
-// Date of creation: 8/28/90
-// Converted to use preprocessor, 12/1/90
-// 
-// $Id$
-}
-
 defstar {
 	name { DownCounter }
 	domain { DDF }
-	desc { "A DownCounter from input value to zero" }
+	desc { A down counter from the input value to zero. }
+	version { $Id$ }
+	author { Soonhoi Ha }
+	copyright { 1991 The Regents of the University of California }
+	location { DDF library }
+	explanation {
+This star generates (int-1), (int-2), ... 1,0 samples for (int) input.
+	}
 	input {
 		name { input }
 		type { int }
