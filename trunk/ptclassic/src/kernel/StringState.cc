6c9052340df2f8fe71c5fae0095c8fb8e44e0dfa
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-02-27 14:06:31 +0000

    add ability to substitute state names in {} characters

--- trunk/ptclassic/src/kernel/StringState.cc
+++ trunk/ptclassic/src/kernel/StringState.cc
@@ -1,8 +1,6 @@
-
 #include <std.h>
 #include "StringState.h"
-
-const int TOKSIZE = 80;
+#include "miscFuncs.h"
 
 /**************************************************************************
 Version identification:
@@ -11,9 +9,13 @@ $Id$
  Copyright (c) 1990 The Regents of the University of California.
                        All Rights Reserved.
 
- Programmer:  I. Kuroda
+ Programmer:  I. Kuroda and J. Buck
  Date of creation: 5/26/90
  Revisions:
+	Support value substitution in StringStates, with the syntax
+	state foo = {foo}
+
+	State names within braces are replaced by their values.
 
  Functions for class StringState
 
@@ -28,7 +30,55 @@ $Id$
 
 
 void StringState  :: initialize() {
-	val = initValue;
+	char buf[2048];
+	delete val;
+	const char* p = initValue;
+	char *q = buf;
+	while (*p) {
+		if (*p == '{') {
+			p++;
+			// a statename: {name}
+			// blanks inside are allowed: { name }, say.
+			char statename[128], *s = statename;
+			while (*p && *p != '}') {
+				if (*p == ' ') p++;
+				else *s++ = *p++;
+			}
+			if (*p == 0) {
+				parseError ("missing '}' character");
+				return;
+			}
+			*s = 0;
+			p++;	// skip the '}' character
+			const State* st = lookup(statename, parent()->parent());
+			if (!st) {
+				parseError ("undefined symbol", statename);
+				return;
+			}
+			StringList sl = st->currentValue();
+			const char* sltext = sl;
+			int l = strlen (sltext);
+			strcpy (q, sltext);
+			q += l;
+		}
+		else if (*p == '\\') {
+			*q++ = *p++;
+			if (*p) *q++ = *p++;
+		}
+		else *q++ = *p++;
+	}
+	*q = 0;
+	val = savestring (buf);
+}
+
+StringList StringState :: currentValue() const {
+	StringList res;
+	if (val) res = val;
+	return res;
+}
+
+State* StringState :: clone() const {
+	return new StringState;
 }
 
 // make known state entry
