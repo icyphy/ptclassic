3c0175c34419228f87be2a59ef6a951bfc108799
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-03-02 15:24:21 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/bdf/stars/BDFSwitch.pl
@@ -0,0 +1,50 @@
+defstar {
+	name { Switch }
+	domain { BDF }
+	version { $Id$ }
+	author { J. T. Buck }
+	location { BDF main library }
+	desc {
+Switches input events to one of two outputs, depending on
+the value of the control input.  If control is true, the
+value is written to trueOutput; otherwise it is written to
+falseOutput.
+	}
+	input {
+		name { input }
+		type { ANYTYPE }
+	}
+	input {
+		name { control }
+		type { int }
+	}
+	output {
+		name { trueOutput }
+		type { =input }
+	}
+	output {
+		name { falseOutput }
+		type { =input }
+	}
+	constructor {
+		trueOutput.setBDFParams(1, control, TRUE, 0);
+		falseOutput.setBDFParams(1, control, FALSE, 0);
+	}
+	go {
+		// get Particles from Geodesic
+		control.grabData();
+		input.grabData();
+
+		// read control value, and route input
+		// to output depending on it.
+		if ((int) (control%0)) {
+			trueOutput%0 = input%0;
+			trueOutput.sendData();
+		} else {
+			falseOutput%0 = input%0;
+			falseOutput.sendData();
+		}
+	}
+}
+
+
