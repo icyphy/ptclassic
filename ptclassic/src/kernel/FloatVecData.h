144e1782e26d3932f219d28c47b5734f8e189e72
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-04-03 12:52:52 +0000

    Initial revision

---
+++ ptclassic/src/kernel/FloatVecData.h
@@ -0,0 +1,53 @@
+#ifndef _FloatVecData_h
+#define _FloatVecData_h 1
+/**************************************************************************
+Version identification:
+$Id$
+
+Copyright (c) 1991 The Regents of the University of California.
+                       All Rights Reserved.
+
+Programmer:  J. T. Buck
+Date of creation: 4/3/91
+
+This is a simple vector packet body.  It stores an array of float values
+of arbitrary length.  The length is specified by the constructor.
+**************************************************************************/
+
+#include "Packet.h"
+
+class FloatVecData : public PacketData {
+private:
+	int len;
+public:
+	// the pointer is public for simplicity
+	float *data;
+
+	int length() const { return len;}
+	const char* dataType() const { return "FloatVecData";}
+
+	// constructor: makes an uninitialized array
+	FloatVecData(int l) : len(l) {
+		data = new float[l];
+	}
+
+	// constructor: makes an initialized array from a float array
+	FloatVecData(int l,float *srcData) : len(l) {
+		data = new float[l];
+		for (int i = 0; i < l; i++)
+			data[i] = *srcData++;
+	}
+
+	// constructor: makes an initialized array from a double array
+	FloatVecData(int l,double *srcData) : len(l) {
+		data = new float[l];
+		for (int i = 0; i < l; i++)
+			data[i] = *srcData++;
+	}
+
+	// destructor
+	~FloatVecData() {
+		delete data;
+	}
+};
+#endif
