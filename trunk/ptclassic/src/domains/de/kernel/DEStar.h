0222bccbed37cba87aa528cc2d4dce39f8420f8b
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-04-16 10:22:57 +0000

    remove a member called "depth" from the star class to porthole class.

--- trunk/ptclassic/src/domains/de/kernel/DEStar.h
+++ trunk/ptclassic/src/domains/de/kernel/DEStar.h
@@ -9,7 +9,6 @@ $Id$
  Date of creation: 5/30/90
 
  Revised 10/2/90 -- DERepeatStar now have its own .h files.
- Revised 11/6/90 -- add two properties : depth, delayType.
 
 *******************************************************************/
 #ifndef _DEStar_h
@@ -22,22 +21,11 @@ $Id$
 #include "Star.h"
 #include "IntState.h"
 
-class PriorityQueue;
-
 	////////////////////////////////////
 	// class DEStar
 	////////////////////////////////////
 
 class DEStar : public Star {
-friend class DEScheduler;
-protected:
-	// Flag to indicate whether it is a delay-type star or not.
-	// If a star simulates the latency, it is called delay-type.
-	// Examples : Delay, UniDelay, Server, etc...
-	// The proper place to set this flag will be "constructor".
-	// For wormhole case, it is set in the start() method depending on
-	// whether the inside domain is timed or untimed.
-	int delayType;
 
 public:
 	// initialize domain-specific members
@@ -48,6 +36,9 @@ public:
 
 	// my domain
 	const char* domain() const;
+
+	// constructor
+	DEStar() : delayType(FALSE) {}
 
 	// Redefine method setting internal data in the Block
 	// so that various DE-specific initilizations can be performed.
@@ -67,16 +58,15 @@ public:
 	// Pointer to the event queue of the universe or warmhole
 	// in which the DEstar is. It is set by the Scheduler :
 	// initialize().
-	PriorityQueue *eventQ;
+	EventQueue *eventQ;
 
-	// The depth is the longest path from a star to any terminating star.
-	// Terminating stars are those that have no output PortHoles, or
-	// delay-type Stars, or those that are connected to the output
-	// of the Wormhole.  To use depth as a fineLevel for PriorityQueue,
-	// we do sign-reversal : depth = - longest path.  Therefore,
-	// stars with smallest depth has the highest priority for scheduling
-	// at the same global time.
- 	int depth;
+        // Flag to indicate whether it is a delay-type star or not.
+        // If a star simulates the latency, it is called delay-type.
+        // Examples : Delay, UniDelay, Server, etc...
+        // The proper place to set this flag will be "constructor".
+        // For wormhole case, it is set in the start() method depending on
+        // whether the inside domain is timed or untimed.
+        int delayType;
 };
 
 #endif
