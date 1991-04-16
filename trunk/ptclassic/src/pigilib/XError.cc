7355fb1f4d02b295a8c40c9aceda50b47883a8c6
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-04-16 12:47:54 +0000

    change name to XError.cc; get rid of UserOutput stuff; add code to call marking functions in pigi

--- trunk/ptclassic/src/pigilib/XError.cc
+++ trunk/ptclassic/src/pigilib/XError.cc
@@ -7,146 +7,83 @@ $Id$
 
  Programmer: J. Buck
  Date of creation: 10/4/90
+ Renamed to XError.cc, 4/15/91
 
  This is a replacement version of the methods for the Error class 
- in Ptolemy (the UserOutput class is the same) -- link this in instead
- of Output.cc to go through the PrintErr error message handler in pigi.
+ in Ptolemy -- link this in instead of Error.cc to go through the
+ PrintErr error message handler in pigi.
 
 *******************************************************************/
 extern "C" {
-void PrintErr(const char*);
+	void PrintErr(const char*);
+	void PigiErrorMark(const char*);
+	void FindClear();
 }
 
 #include <stream.h>
-#include "Output.h"
+#include "Error.h"
 #include "Scheduler.h"
 #include "miscFuncs.h"
-#include "ComplexSubset.h"
 #include "NamedObj.h"
 
-// A global output error handler
-Error errorHandler;
+typedef const char cc;
 
-// Constructor: set the ostream to cerr
-Error :: Error () : UserOutput (cerr) {}
-
-Error :: Error (ostream& foo) : UserOutput (foo) {}
-
-void
-Error :: error() {
-	PrintErr("(no message specified)");
-}
-
-void
-Error :: error(const char *s) {
-	PrintErr(s);
-}
-
-void
-Error :: error(const char *s, const char *c) {
-	char* buf = new char[strlen(s)+strlen(c)+1];
-	strcpy (buf, s);
-	strcat (buf, c);
+static void outMsg(cc* obj, int warn, cc* m1, cc* m2, cc* m3) {
+	const char* status = warn ? "warning: " : "";
+	if (!m2) m2 = "";
+	if (!m3) m3 = "";
+	int l = strlen(status)+strlen(m1)+strlen(m2)+strlen(m3) + 1;
+	if (obj) l += strlen(obj) + 2;
+	char* buf = new char[l];
+	if (obj)
+		sprintf (buf, "%s%s: %s%s%s", status, obj, m1, m2, m3);
+	else
+		sprintf (buf, "%s%s%s%s", status, m1, m2, m3);
 	PrintErr (buf);
 	delete buf;
 }
 
 void
-Error :: error (const char *s, int errorCode) {
-	char buf[32];
-	sprintf (buf, "Error code: %d", errorCode);
-	error (s, buf);
+Error :: error(cc* m1, cc* m2, cc* m3) {
+	outMsg(0, 0, m1, m2, m3);
 }
 
-// print error and halt schedule
 void
-Error :: abortRun (const char *s) {
-	errorHandler.error (s);
+Error :: warn(cc* m1, cc* m2, cc* m3) {
+	outMsg(0, 1, m1, m2, m3);
+}
+
+void
+Error :: error (NamedObj& o, cc* m1, cc* m2, cc* m3) {
+	StringList nam = o.readFullName();
+	PigiErrorMark(nam);
+	outMsg(nam, 0, m1, m2, m3);
+}
+
+void
+Error :: warn (NamedObj& o, cc* m1, cc* m2, cc* m3) {
+	StringList nam = o.readFullName();
+	PigiErrorMark(nam);
+	outMsg(nam, 1, m1, m2, m3);
+	FindClear();
+}
+
+void
+Error :: abortRun (cc *m1, cc* m2, cc* m3) {
+	outMsg (0, 0, m1, m2, m3);
 	Scheduler::requestHalt();
 }
 
-// this one gives the name of the object
 void
-Error :: abortRun (NamedObj& n, const char *str) {
-	StringList s = n.readFullName();
-	errorHandler.error (s, str);
+Error :: abortRun (NamedObj& o, cc* m1, cc* m2, cc* m3) {
+	error (o, m1, m2, m3);
 	Scheduler::requestHalt();
 }
 
-// constructor: output to cout
-UserOutput :: UserOutput() : outputStream(&cout), myStream(0) {}
+// marking is supported in this implementation
+int Error :: canMark() { return 1;}
 
-// general constructor
-UserOutput :: UserOutput(ostream& foo) : outputStream(&foo), myStream(0) {}
-
-// destructor
-UserOutput :: ~UserOutput() {
-	// delete the stream only if I created it
-	if (myStream)
-		delete outputStream;
-}
-
-// change the file written to.
-void 
-UserOutput :: fileName(const char *fileName) {
-	if (myStream)
-		delete outputStream;
-	if ( strcmp(fileName,"cout") == 0 ) {
-		outputStream = &cout;
-		myStream = FALSE;
-	}
-	else {
-		outputStream = new ostream(expandPathName(fileName),
-					   io_writeonly,a_create);
-		myStream = TRUE;
-	}
-}
-
-void
-UserOutput :: outputString(const char *s) {
-	*outputStream << s;
-}
-
-UserOutput&
-UserOutput :: operator << (int i) {
-	*outputStream << i;
-	return *this;
-}
-
-UserOutput&
-UserOutput :: operator << (const char *s) {
-	*outputStream << s;
-	return *this;
-}
-
-UserOutput&
-UserOutput :: operator << (float f) {
-	*outputStream << f;
-	return *this;
-}
-
-UserOutput&
-UserOutput :: operator << (const Complex& C) {
-        *outputStream << "(" << C.real() << "," << C.imag() << ")";
-        return *this;
-}
-
-void
-UserOutput :: flush () {
-	outputStream->flush();
-}
-
-// While this function does not belong to the UserOutput or Error
-// classes, it's used for stars that do I/O so it is defined here
-// (and its prototype is in Output.h)
-
-// This function returns a new, unique temporary file name.
-// It lives on the heap and may be deleted when no longer needed.
-const char* tempFileName() {
-	int pid = getpid();
-	static int count = 1;
-	char* buf = new char[17];
-	sprintf (buf, "/tmp/pt%04x.%04d", pid, count);
-	count++;
-	return buf;
+void Error :: mark(NamedObj& o) {
+	StringList n = o.readFullName();
+	PigiErrorMark(n);
 }
