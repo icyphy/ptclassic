f3087486b5d0e4b4c51aecb80f45a706a38d0859
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-07-22 14:59:00 +0000

    add string function prototypes

--- ptclassic/src/kernel/miscFuncs.h
+++ ptclassic/src/kernel/miscFuncs.h
@@ -9,13 +9,36 @@ $Id$
  Date of creation: 6/10/90
 
  This header file provides prototypes for miscellaneous
- library functions that are not members.
+ library functions that are not members, together with some
+ commonly used library functions.
 
 *************************************************************************/
 
 #ifndef _miscFuncs_h
 #define _miscFuncs_h 1
 #include <std.h>
+
+extern "C" {
+char*     strcat(char*, const char*);
+char*     strchr(const char*, int);
+int       strcmp(const char*, const char*);
+char*     strcpy(char*, const char*);
+int       strlen(const char*);
+char*     strncat(char*, const char*, int);
+int       strncmp(const char*, const char*, int);
+char*     strncpy(char*, const char*, int);
+char*     strrchr(const char*, int);
+}
+
+#ifdef USG
+inline char* index(const char* s, int c) { return strchr(s, c); }
+inline char* rindex(const char* s, int c) { return strrchr(s, c); }
+#else
+extern "C" {
+char*     index(const char*, int);
+char*     rindex(const char*, int);
+}
+#endif
 
 const char* expandPathName(const char*);
 
