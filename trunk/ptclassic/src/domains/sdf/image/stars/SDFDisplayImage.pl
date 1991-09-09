497c78afc363fec1997012d51c789486855bab1f
haskell <haskell@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-09-09 12:08:23 +0000

    Now give different filenames to each image in a sequence.

--- trunk/ptclassic/src/domains/sdf/image/stars/SDFDisplayImage.pl
+++ trunk/ptclassic/src/domains/sdf/image/stars/SDFDisplayImage.pl
@@ -1,84 +1,92 @@
 defstar {
-    name      { DisplayImage }
-    domain    { SDF }
-    version   { $Id$ }
-    author    { J. Buck, Paul Haskell }
-    copyright { 1991 The Regents of the University of California }
-    location  { SDF image library }
-    desc {
+	name		{ DisplayImage }
+	domain		{ SDF }
+	version		{ $Id$ }
+	author		{ J. Buck, Paul Haskell }
+	copyright	{ 1991 The Regents of the University of California }
+	location	{ SDF image library }
+	desc {
 Accept a black-and-white image from an input packet, and generate
-output in PGM format.  Send the output to a user-specified command
-(by default, "xv" is used).  Of course, the specified program must
+output in PGM format. Send the output to a user-specified command
+(by default, "xv" is used). Of course, the specified program must
 be in your PATH.
 
-The user can set the filename of the displayed image (which will
+The user can set the root filename of the displayed image (which will
 probably be printed in the image display window titlebar) and
 can choose whether or not the image file is saved or deleted.
-    }
+The image frame number is appended to the root filename to form the
+complete filename of the displayed image.
+	}
 
-    ccinclude { "GrayImage.h" , <std.h> , <stdio.h>, "UserOutput.h" }
+	ccinclude { "GrayImage.h" , <std.h> , <stdio.h>, "UserOutput.h" }
 
 // INPUT AND STATES.
 
-    input {
-        name { inData }
-        type { packet }
-    }
-    defstate {
-        name { command }
-        type { string }
-        default { "xv" }
-        desc { Program to run on PGM data }
-    }
-    defstate {
-        name { imageName }
-        type { string }
-        default { "" }
-        desc { If non-null, name for PGM file }
-    }
-    defstate {
-        name { saveImage }
-        type { string }
-        default { "n" }
-        desc { If 'y' or 'Y', then save the file }
-    }
+	input {
+		name { inData }
+		type { packet }
+	}
+	defstate {
+		name { command }
+		type { string }
+		default { "xv" }
+		desc { Program to run on PGM data }
+	}
+	defstate {
+		name { imageName }
+		type { string }
+		default { "" }
+		desc { If non-null, name for PGM file }
+	}
+	defstate {
+		name { saveImage }
+		type { string }
+		default { "n" }
+		desc { If 'y' or 'Y', then save the file }
+	}
 
 // CODE.
-    go {
+	go {
 // Read data from input.
-        Packet pkt;
-        (inData%0).getPacket(pkt);
-	TYPE_CHECK(pkt,"GrayImage");
+		Packet pkt;
+		(inData%0).getPacket(pkt);
+		TYPE_CHECK(pkt,"GrayImage");
+		GrayImage* imD = (GrayImage*) pkt.myData();
 
 // Set filename and save values.
-        const char* saveMe = saveImage;
-        int del = !((saveMe[0] == 'y') || (saveMe[0] == 'Y'));
-        const char* fileName = imageName;
-        if (*fileName == 0) {
-            fileName = tempFileName();
-        }
-        FILE* fd = fopen(fileName, "w");
-        if (fd == 0) {
-            Error::abortRun(*this," can't create file ", fileName);
-            return;
-        }
-        GrayImage* imD = (GrayImage*) pkt.myData();
+		const char* saveMe = saveImage;
+		int del = !((saveMe[0] == 'y') || (saveMe[0] == 'Y'));
 
-// Write the PGM header and then the data.
-        fprintf (fd, "P5\n %d %d 255\n", imD->retWidth(),
-             imD->retHeight());
-        fwrite((char*)imD->retData(), sizeof(unsigned char), (unsigned)
-                        imD->retWidth() * imD->retHeight(), fd);
-        fclose(fd);
+		char fileName[256];
+		if (!(const char*) imageName) {
+			strcpy(fileName, tempFileName());
+		} else {
+			strcpy(fileName, imageName);
+		}
+		char numstr[16];
+		sprintf(numstr, ".%d", imD->retId());
+		strcat(fileName, numstr);
 
-        char buf[256];
-        sprintf (buf, "(%s %s", (const char*)command, fileName);
-        if (del) {
-            strcat (buf, "; rm -f ");
-            strcat (buf, fileName);
-        }
-// Run command in the background
-        strcat (buf, ")&");
-        system (buf);
-    } // end go{}
+		FILE* fd = fopen(fileName, "w");
+		if (fd == (FILE*) NULL) {
+			Error::abortRun(*this, "can not create: ", fileName);
+			return;
+		}
+
+// Write the PGM header and the data, and then run.
+		fprintf (fd, "P5\n %d %d 255\n", imD->retWidth(),
+				imD->retHeight());
+		fwrite((char*)imD->retData(), sizeof(unsigned char), (unsigned)
+				imD->retWidth() * imD->retHeight(), fd);
+		fclose(fd);
+
+		char cmdbuf[256];
+		sprintf (cmdbuf, "(%s %s", (const char*) command, fileName);
+		if (del) {
+			strcat (cmdbuf, "; rm -f ");
+			strcat (cmdbuf, fileName);
+		}
+		strcat (cmdbuf, ")&");		// Run command in the background
+		system (cmdbuf);
+	} // end go{}
 } // end defstar { DisplayImage }
--- trunk/ptclassic/src/domains/sdf/image/stars/SDFDisplayRGB.pl
+++ trunk/ptclassic/src/domains/sdf/image/stars/SDFDisplayRGB.pl
@@ -1,116 +1,126 @@
 defstar {
-    name      { DisplayRgb }
-    domain    { SDF }
-    version   { $Id$ }
-    author    { Sun-Inn Shih }
-    copyright { 1991 The Regents of the University of California }
-    location  { SDF image library }
-    desc {
+	name		{ DisplayRgb }
+	domain		{ SDF }
+	version		{ $Id$ }
+	author		{ Sun-Inn Shih }
+	copyright	{ 1991 The Regents of the University of California }
+	location	{ SDF image library }
+	desc {
 Accept three ColorImages (Red, Green, and Blue) from three input
 packets, and generate a PPM format color image file.  Send the filename
 to a user-specified command (by default, "xv" is used).
 
-The user can set the filename of the displayed image (which will
+The user can set the root filename of the displayed image (which will
 probably be printed in the image display window titlebar) and
 can choose whether or not the image file is saved or deleted.
-    }
+The frameId of the received image is appended to the root filename
+to produce the full filename of the displayed image.
+	}
 
-    ccinclude { "GrayImage.h" , <std.h> , <stdio.h>, "UserOutput.h" }
+	ccinclude { "GrayImage.h" , <std.h> , <stdio.h>, "UserOutput.h" }
 
 // INPUT AND STATES.
-    input {
-        name { rinput }
-        type { packet }
-    }
-    input {
-        name { ginput }
-        type { packet }
-    }
-    input {
-        name { binput }
-        type { packet }
-    }
+	input {
+		name { rinput }
+		type { packet }
+	}
+	input {
+		name { ginput }
+		type { packet }
+	}
+	input {
+		name { binput }
+		type { packet }
+	}
 
-    defstate {
-        name { command }
-        type { string }
-        default { "xv" }
-        desc { Program to run on PGM data }
-    }
-    defstate {
-        name { imageName }
-        type { string }
-        default { "" }
-        desc { If non-null, name for PGM file }
-    }
-    defstate {
-        name { saveColor }
-        type { string }
-        default { "n" }
-        desc { If 'y' or 'Y', then save the file }
-    }
+	defstate {
+		name { command }
+		type { string }
+		default { "xv" }
+		desc { Program to run on PGM data }
+	}
+	defstate {
+		name { imageName }
+		type { string }
+		default { "" }
+		desc { If non-null, name for PGM file }
+	}
+	defstate {
+		name { saveColor }
+		type { string }
+		default { "n" }
+		desc { If 'y' or 'Y', then save the file }
+	}
 
 
-    go {
+	go {
 // Read data from input.
-        Packet rpkt, gpkt, bpkt;
-        (rinput%0).getPacket(rpkt);
+		Packet rpkt, gpkt, bpkt;
+		(rinput%0).getPacket(rpkt);
 	TYPE_CHECK(rpkt,"GrayImage");
-        (ginput%0).getPacket(gpkt);
-	TYPE_CHECK(gpkt,"GrayImage");
-        (binput%0).getPacket(bpkt);
-	TYPE_CHECK(bpkt,"GrayImage");
+		(ginput%0).getPacket(gpkt);
+		TYPE_CHECK(gpkt,"GrayImage");
+		(binput%0).getPacket(bpkt);
+		TYPE_CHECK(bpkt,"GrayImage");
+		GrayImage* tempyD = (GrayImage*) rpkt.myData();
+		GrayImage* tempuD = (GrayImage*) gpkt.myData();
+		GrayImage* tempvD = (GrayImage*) bpkt.myData();
+
 // Set filename and save values.
-        const char* saveMe = saveColor;
-        int del = !((saveMe[0] == 'y') || (saveMe[0] == 'Y'));
-        const char* fileName = imageName;
-        if (*fileName == 0) {
-            fileName = tempFileName();
-        }
-        FILE* fp = fopen(fileName, "w");
-        if (fp == (FILE*) NULL) {
-            Error::abortRun(*this," can't create file ", fileName);
-            return;
-        }
-        GrayImage* tempyD = (GrayImage*) rpkt.myData();
-        GrayImage* tempuD = (GrayImage*) gpkt.myData();
-        GrayImage* tempvD = (GrayImage*) bpkt.myData();
+		const char* saveMe = saveColor;
+		int del = !((saveMe[0] == 'y') || (saveMe[0] == 'Y'));
+
+		char fileName[256];
+		if (!(const char*) imageName) {
+			strcpy(fileName, tempFileName());
+		} else {
+			strcpy(fileName, (const char*) imageName);
+		}
+		char numtmp[16];
+		sprintf(numtmp, ".%d", tempyD->retId());
+		strcat(fileName, numtmp);
+
+		FILE* fp = fopen(fileName, "w");
+		if (fp == (FILE*) NULL) {
+			Error::abortRun(*this, fileName, ": can't create");
+			return;
+		}
 
 // change into RGB format
-        int Width  = tempyD->retWidth();
-        int Height = tempyD->retHeight();
+		int Width  = tempyD->retWidth();
+		int Height = tempyD->retHeight();
 
-        unsigned char* rfp = tempyD->retData();
-        unsigned char* gfp = tempuD->retData();
-        unsigned char* bfp = tempvD->retData();
+		unsigned char* rfp = tempyD->retData();
+		unsigned char* gfp = tempuD->retData();
+		unsigned char* bfp = tempvD->retData();
 
-        unsigned char* rgbfp= new unsigned char[3*Width*Height];
+		unsigned char* rgbfp= new unsigned char[3*Width*Height];
 
-        int i, j, temp1, temp2, temp3;
-        for (i = 0; i < Height; i++) {
-            temp1 = i*Width;
-            for (j = 0; j < Width; j++){
-                temp2 = j+temp1;
-                temp3 = 3*temp2;
-                rgbfp[temp3]   = rfp[temp2];
-                rgbfp[temp3+1] = gfp[temp2];
-                rgbfp[temp3+2] = bfp[temp2];
-        }   }
+		int i, j, temp1, temp2, temp3;
+		for (i = 0; i < Height; i++) {
+			temp1 = i*Width;
+			for (j = 0; j < Width; j++){
+				temp2 = j+temp1;
+				temp3 = 3*temp2;
+				rgbfp[temp3] = rfp[temp2];
+				rgbfp[temp3+1] = gfp[temp2];
+				rgbfp[temp3+2] = bfp[temp2];
+		}	}
 
 // Write the PGM header and then the data.
-        fprintf (fp, "P6\n %d %d 255\n", Width, Height);
-        fwrite((char*)rgbfp, sizeof(unsigned char), unsigned(3*Width*Height),
-                fp);
-        fclose(fp);
+		fprintf (fp, "P6\n %d %d 255\n", Width, Height);
+		fwrite((char*)rgbfp, sizeof(unsigned char), unsigned(3*Width*Height),
+				fp);
+		fclose(fp);
 
-        char buf[256];
-        sprintf (buf, "(%s %s", (const char*) command, fileName);
-        if (del) {
-            strcat(buf, "; rm -f ");
-            strcat(buf, fileName);
-        }
+		char buf[256];
+		sprintf (buf, "(%s %s", (const char*) command, fileName);
+		if (del) {
+			strcat(buf, "; rm -f ");
+			strcat(buf, fileName);
+		}
 // Run command in the background
-        strcat (buf, ")&");
-        system (buf);
-    } // end go{}
+		strcat (buf, ")&");
+		system (buf);
+	} // end go{}
 } // end defstar { DisplayRgb }
