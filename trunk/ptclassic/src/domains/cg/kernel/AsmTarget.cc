98bd523cec3f1ea99449b5355a246fea6bbbb265
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-10-02 14:30:24 +0000

    rename AsmArch to AsmTarget, derive from Target, many changes for the new regime

--- trunk/ptclassic/src/domains/cg/kernel/AsmTarget.cc
+++ trunk/ptclassic/src/domains/cg/kernel/AsmTarget.cc
@@ -7,48 +7,37 @@ $Id$
 
  Programmer: J. Buck
 
- Basic assembly language code architecture for a single processor.
+ Basic assembly language code Target for a single processor.
  Either an SDFScheduler or a LoopScheduler object can be used as
- the scheduler.  More specialized architectures (for a specific
+ the scheduler.  More specialized Targets (for a specific
  processor, etc) can be derived from this.
 
 *******************************************************************/
-#include "AsmArch.h"
+#include "AsmTarget.h"
 #include "GalIter.h"
 #include "AsmStar.h"
 #include "ProcMemory.h"
 
 int
-AsmArchSched::setup(Block& b) {
-	Galaxy& g = b.asGalaxy();
+AsmTarget::setup(Galaxy& g) {
+// initialize proc ptrs and create schedule
+	if (!Target::setup(g)) return FALSE;
 	GalStarIter nextStar(g);
-	Star* s;
-	const char* typ = asmArch()->starType();
-	while ((s = nextStar++) != 0) {
-		if (!s->isA(typ)) {
-			Error::abortRun(*s,"is not of type ", typ);
-			return FALSE;
-		}
-		s->setTarget(asmArch());
-	}
-// compute schedule
-	if (!sched->setup(g)) return FALSE;
-// allocate memory, using the architecture, for all stars in the galaxy.
-// the cast is safe because of the test above.
+// allocate memory, using the Target, for all stars in the galaxy.
 // note that allocMem just posts requests; performAllocation processes
-// all the requests.
-	nextStar.reset();
-	while ((s = nextStar++) != 0) {
-		AsmStar& star = *(AsmStar*)s;
-		asmArch()->allocMem(star);
+// all the requests.  Note we've already checked that all stars are AsmStar.
+	AsmStar* s;
+	while ((s = (AsmStar*)nextStar++) != 0) {
+		allocMem(*s);
 	}
-	if (!asmArch()->performAllocation()) return FALSE;
-	asmArch()->headerCode();
+	if (!mem.performAllocation()) return FALSE;
+	headerCode();
+	return TRUE;
 }
 
 // allocate memory for all structures in a star
 int
-AsmArch::allocMem(AsmStar& star) {
+AsmTarget::allocMem(AsmStar& star) {
 // first, allocate for portholes.
 	AsmStarPortIter nextPort(star);
 	AsmPortHole* p;
@@ -61,4 +50,5 @@ AsmArch::allocMem(AsmStar& star) {
 	return mem.performAllocation();
 }
 
-int AsmArch::performAllocation() { return mem.performAllocation();}
+
+	
