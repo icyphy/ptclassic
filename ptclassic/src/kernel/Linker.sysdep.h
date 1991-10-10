6eda691242c5ec00e3e0d8e22ef7f3b9ba222e21
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-10-10 19:33:35 +0000

    fix problems on mips architecture

--- ptclassic/src/kernel/Linker.sysdep.h
+++ ptclassic/src/kernel/Linker.sysdep.h
@@ -20,6 +20,7 @@ $Id$
 // the starting address and "-A ptolemy-name" to get the symbols for the
 // running executable.
 #ifdef mips
+#define __mips 1
 #define LOADOPTS "-N -S -G 0"
 #else
 #define LOADOPTS "-N -S"
@@ -39,29 +40,44 @@ $Id$
 #endif
 
 // Prefix for constructor-calling symbols
+// This is an attempt to support both g++ and cfront.
+#ifdef __GNUG__
+#define DEST_CHAR 'D'
 #ifdef mips
-#define CONS_PREFIX "_GLOBAL"
-#else
-#define CONS_PREFIX "__GLOBAL"
+#define CONS_PREFIX "_GLOBAL_$I$"
+#define ID_POS 9
+#else /* g++, nonmips */
+#define CONS_PREFIX "__GLOBAL_$I$"
+#define ID_POS 10
+#endif
+#else /* not __GNUG__ (i.e. cfront) */
+#define CONS_PREFIX "___sti"
+#define ID_POS 5
+#define DEST_CHAR 'd'
 #endif
 
+// g++ has to see the following before it sees the declaration of
+// the flock function in std.h, or it will give a warning message.
+// (in C++ there is a namespace collision).
+struct flock;
+
 extern "C" {
+#ifdef mips
+#define __LANGUAGE_C
+#define COFF
+#include <aouthdr.h>
+#endif
 #include <a.out.h>
 }
 
-// g++ has to see the following before it sees the declaration of
-// the flock function in std.h, or it will give a bogus warning message.
-struct flock;
-
 #include <std.h>
+#ifndef mips
 #include <sys/file.h>
-#if defined(MIPSEL) || defined(USG) 
-#if !defined(hpux)
-#define COFF
-#endif
 #endif
 
 // the following code may also be needed on other platforms.
+// No, this has not been tested on a Pyramid; it comes from libg++
+// example code that suggests that this is the thing to do.
 #ifdef __pyr__
 #include <sys/mman.h>
 
