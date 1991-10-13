a8cad91b748c98367ffde0756c4e1a00ed18a7ce
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-10-13 21:49:15 +0000

    Major changes motivated by Philip's suggestions.

--- trunk/ptclassic/src/domains/de/stars/DEPrQueue.pl
+++ trunk/ptclassic/src/domains/de/stars/DEPrQueue.pl
@@ -1,25 +1,60 @@
 defstar {
 	name {PrQueue}
 	domain {DE}
-	desc { Priority queue with finite total capacity.  }
-	version { $Id$}
-	author { E. A. Lee and Soonhoi Ha }
+	desc {
+A priority queue.
+Inputs have priorities according to the number of the input, with
+"inData#1" having highest priority, "inData#2" being next, etc.
+When a "demand" is received, outputs are produced by selecting first
+based on priority, and then based on time of arrival, using a FIFO policy.
+A finite total capacity can be specified by setting the "capacity"
+parameter to a positive integer.
+The "numDemandsPending" and "consolidateDemands" states have the same
+meaning as in other Queue stars.
+The size of the queue is sent to the "size" output whenever an "inData"
+or "demand" event is processed.
+	}
+	version { $Id$ }
+	author { Soonhoi Ha and E. A. Lee }
 	copyright { 1991 The Regents of the University of California }
 	location { DE main library }
 	explanation {
 This star queues input particles and produces output particles on demand.
 The inputs are assigned priorities so that when an output is demanded,
-if any particles from \fIinput#1\fR are available, the next one in the queue
-will be sent to the output.  Only if the queue for \fIinput#1\fR is empty
-will the queue for \fIinput#2\fR be examined.
-This means that later arrivals on \fIinput#1\fR may go to the output before
-earlier arrivals on \fIinput#2\fR.
-There is currently a limit of 10 distinct inputs, and hence 10 priorities.
+if any particle from \fIinData#1\fR is available, it
+will be sent to the output.  If several particles from
+\fIinData#1\fR are available, a FIFO policy is used.
+Only if the queue for \fIinData#1\fR is empty
+will the queue for \fIinData#2\fR be examined.
+This means that later arrivals on \fIinData#1\fR may go to the output before
+earlier arrivals on \fIinData#2\fR.
+.pp
+The first particle to arrive at an input is always passed directly
+to the output, unless \fInumDemandsPending\fR is initialized to 0.
+If several particles arrive first with the same time stamp, the highest
+priority particle will go to the output.
+.pp
+If \fIconsolidateDemands\fR is set to TRUE (the default),
+then \fInumDemandsPending\fR is not permitted to rise above unity.
+This means that
+if multiple \fIdemand\fR particles with the same time stamp
+are waiting at the \fIdemand\fR input, they are
+consolidated into a single demand, and only one output is produced.
+As usual, if a \fIdemand\fR input arrives between the time that the queue
+goes empty and the next arrival of a data input, it enables
+the next \fIinData\fR particle to pass immediately to the output by
+setting the state \fInumDemandsPending\fR to unity.
+But if more than one \fIdemand\fR event arrives in this time period, the
+effect is the same as if only one such event had arrived.
+.pp
+If \fIconsolidateDemands\fR is set to FALSE, then every \fIdemand\fR input
+will eventually stimulate an output, even if successive demands arrive
+when the queue is empty, and even if successive demands arrive with the
+same time stamp.
 .pp
 A total capacity limit is specified.
 This is the limit on the total number of particles that the star
-may store in its queues at any one time, regardless of which
-input they came from.
+may store from one firing to the next, regardless of which input they came from.
 Note that storage is allocated dynamically, so a large capacity
 does not necessarily imply a large storage usage.
 When the capacity is reached, all further inputs will be ignored
@@ -61,75 +96,94 @@ after processing the input is sent to the \fIsize\fR output.
 		type {int}
 		desc { The current number of particles stored. }
 	}
-	ccinclude {
-		"ParticleQueue.h"
-	}
-	header {
-		const int NUMINPUTS = 10; // max # of inputs
-	}
+	hinclude { "PriorityQueue.h" }
 	protected {
-		class ParticleQueue;
-		ParticleQueue* queue[NUMINPUTS];
-	}
-	defstate {
-		name {curSize}
-		type {int}
-		default {"0"}
-		desc { Current number of particles stored. }
+		PriorityQueue queue;
+		int infinite;
 	}
 	defstate {
 		name {capacity}
 		type {int}
-		default {"10"}
-		desc { Maximum total number of particles stored. }
+		default { "-1" }
+		desc {
+Maximum total number of particles. If <0, capacity is infinite.
+		}
+	}
+	defstate {
+		name {numDemandsPending}
+		type {int}
+		default { "1" }
+		desc { Number of demands that have not yet stimulated outputs. }
+	}
+	defstate {
+		name { consolidateDemands }
+		type {int}
+		default { TRUE }
+		desc { If FALSE, every demand eventually stimulates an output. }
 	}
 	constructor {
+		// Hint to the scheduler that both demand and inData trigger
+		// size outputs.  And demand triggers an output.
 		inData.triggers(size);
+		demand.triggers(size);
+		demand.triggers(outData);
+
+		// Hint to the scheduler that if there is any chance of
+		// simultaneous events on inData and demand, then the inData
+		// events should be made available.
 		inData.before(demand);
 	}
 	start {
-	   if (inData.numberPorts() > NUMINPUTS) {
-		Error::abortRun(*this, "Too many input ports.");
-	   } else {
-		// Create or initialize the Queues
-		for(int i=(inData.numberPorts()-1); i>=0; i--) {
-		    if (queue[i]) queue[i]->initialize();
-		    else queue[i] = new ParticleQueue(int(capacity));
-		}
-	   }
-	   demand.dataNew = TRUE;
-
+		queue.initialize();
+		infinite = (int(capacity) < 0);
 	}
 
 	go {
-	   // If there is new data input, and the queue is not full, store it
-	   completionTime = arrivalTime;
-	   InDEMPHIter nextp(inData);
-	   for(int i=0;i<inData.numberPorts(); i++) {
+	    completionTime = arrivalTime;
+
+	    while (demand.dataNew) {
+		if (int(consolidateDemands))
+			numDemandsPending = 1;
+		else
+			numDemandsPending = int(numDemandsPending) + 1;
+		// get any pending simultaneous demand inputs.
+		demand.getSimulEvent();
+	    }
+
+	    // Iterate over the inputs, collecting new input data
+	    // All input data should be collected before any outputs are
+	    // produced so that priorites of the inputs will be observed.
+	    // Note that the queue size limitation will not be observed at
+	    // this point.  Only at the end of the firing will the queue
+	    // be truncated to capacity.
+	    InDEMPHIter nextp(inData);
+	    for(int i=0;i<inData.numberPorts(); i++) {
 		InDEPort* p = nextp++;
-		if (p->dataNew) {
-		    if (int(curSize) < int(capacity)) {
-			curSize = int(curSize) + 1;
-			queue[i]->putq(p->get());
-		    }
+		while (p->dataNew) {
+		    queue.levelput(p->get().clone(),float(i));
+		    p->getSimulEvent();
 		}
-	   }
-	   // Produce outData only if the demand input is new.
-	   if (demand.dataNew) {
-		// If a queue has data...
-		if ((int)curSize > 0) {
-		   // ...figure out which one
-		   int qNum = 0;
-		   while (qNum < inData.numberPorts() &&
-				queue[qNum]->empty()) {
-		      qNum += 1;
-		   }
-		   queue[qNum]->getq(outData.put(completionTime));
-		   demand.dataNew = FALSE;
-		   curSize = int(curSize) - 1;
+	    }
+
+	    // If the queue has data, and one or more demands are pending,
+	    // produce output data.  Higher priority data will be automatically
+	    // sent out before lower priority data, because of the order in
+	    // which it was enqueued.
+	    while (int(numDemandsPending) > 0 && queue.length() > 0 ) {
+		Particle* pp = (Particle*) queue.getFirstElem();
+		outData.put(completionTime) = *pp;
+		pp->die();
+		numDemandsPending = int(numDemandsPending) - 1;
+	    }
+
+	    // Cut queue down to size by removing lowest priority entries.
+	    if (!infinite)
+		while (queue.length() > int(capacity)) {
+		    Particle* junkp = (Particle*) queue.getLastElem();
+		    junkp->die();
 		}
-	   }
-	   // output the queue size
-	   size.put(completionTime) << int(curSize);
+
+	    // output the queue size
+	    size.put(completionTime) << queue.length();
 	}
 }
