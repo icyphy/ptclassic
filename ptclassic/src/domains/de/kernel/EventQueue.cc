8479543ef3acee594d5417e88f5008625ceab83d
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-10-13 22:16:14 +0000

    Initial revision

---
+++ ptclassic/src/domains/de/kernel/EventQueue.cc
@@ -0,0 +1,54 @@
+/**************************************************************************
+Version identification:
+$Id$
+
+ Copyright (c) 1991 The Regents of the University of California.
+                       All Rights Reserved.
+
+ Programmer:  Soonhoi Ha
+ Date of creation: 10/11/91
+ Revisions:
+
+This file contains member functions for EventQueue..
+
+******************************************************************/
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include "EventQueue.h"
+
+Event* EventQueue:: getEvent(Particle* p, PortHole* ph) {
+	Event* temp;
+	// a free event instance
+	if (freeEventHead) {
+		temp = freeEventHead;
+		freeEventHead  = temp->next;
+	} else {
+		temp = new Event;
+	}
+	// set the event members.
+	temp->dest = ph;
+	temp->p = p;
+	return temp;
+}
+
+void EventQueue:: clearFreeEvents() {
+	if (!freeEventHead) return;
+	Event* temp;
+	while (freeEventHead->next) {
+		temp = freeEventHead;
+		freeEventHead = freeEventHead->next;
+		delete temp;
+	}
+	delete freeEventHead;
+}
+
+void EventQueue:: putFreeLink(LevelLink* p) {
+	if (p->fineLevel) {
+		Event* temp = (Event*) p->e;
+		putEvent(temp);
+	}
+	PriorityQueue:: putFreeLink(p);
+}
