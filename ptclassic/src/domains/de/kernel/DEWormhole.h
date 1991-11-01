c29a41abbeb0e35b203db8dc487f63019d73d373
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-11-01 10:58:35 +0000

    correct "const" problem.

--- ptclassic/src/domains/de/kernel/DEWormhole.h
+++ ptclassic/src/domains/de/kernel/DEWormhole.h
@@ -56,7 +56,7 @@ public:
 	int isItWormhole() const { return TRUE;}
 
 	// use statelist for inner galaxy for stateWithName
-	State* stateWithName (const char* name) const {
+	State* stateWithName (const char* name) {
 		return gal.stateWithName(name);
 	}
 
