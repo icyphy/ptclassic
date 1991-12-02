f7cdcb7deb06f27f486894457d89f750ab9e5abb
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1991-12-02 19:22:54 +0000

    remove min, max functions.  Add prototype for hashstring.

--- ptclassic/src/kernel/miscFuncs.h
+++ ptclassic/src/kernel/miscFuncs.h
@@ -46,18 +46,8 @@ const char* expandPathName(const char*);
 inline char* savestring (const char* txt) {
 	return strcpy (new char[strlen(txt)+1], txt);
 }
-// with templates we will need only
-// template<class T> inline T min(T a, T b) {return (a < b ? a : b);}
-// template<class T> inline T max(T a, T b) {return (a > b ? a : b);}
 
-inline double   min(double a, double b)     {return (a < b ? a : b);}
-inline float    min(float a, float b)       {return (a < b ? a : b);}
-inline int      min(int a, int b)           {return (a < b ? a : b);}
-inline unsigned min(unsigned a, unsigned b) {return (a < b ? a : b);}
-inline double   max(double a, double b)     {return (a > b ? a : b);}
-inline float    max(float a, float b)       {return (a > b ? a : b);}
-inline int      max(int a, int b)           {return (a > b ? a : b);}
-inline unsigned max(unsigned a, unsigned b) {return (a > b ? a : b);}
+const char* hashstring(const char*);
 
 // I would prefer to make this int pow(int,int), but g++ gets confused
 // because other func is extern "C".
