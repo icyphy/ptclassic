e91d48c072038ef05fe11bc0479e1ae80b346a9c
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-02-02 15:09:03 +0000

    corrected dates; removed some non-essential functions

--- trunk/ptclassic/src/domains/xxx/kernel/XXXStar.cc
+++ trunk/ptclassic/src/domains/xxx/kernel/XXXStar.cc
@@ -1,10 +1,12 @@
+static const char file_id[] = "XXXStar.cc";
+
 /*  Version $Id$
 
-    Copyright 1991 The Regents of the University of California.
+    Copyright 1992 The Regents of the University of California.
 			All Rights Reserved.
 
     Programmer:		T.M. Parks
-    Date of creation:	5 January 1991
+    Date of creation:	5 January 1992
 
 */
 
@@ -13,10 +15,7 @@
 #endif
 
 #include "XXXStar.h"
-#include "XXXConnect.h"
 
-// The following is defined in XXXDomain.cc -- this forces that module
-// to be included if any XXX stars are linked in.
 extern const char XXXdomainName[];
 
 // Class identification.
@@ -30,22 +29,5 @@ const char* XXXStar::domain() const
 
 // Domain-specific initialization.
 void XXXStar::prepareForScheduling()
-{}
-
-// Firing definition.
-void XXXStar::fire()
 {
-    BlockPortIter port(*this);
-
-    // Get data for all PortHoles.
-    for(int i = numberPorts(); i > 0; i--)
-	(port++)->grabData();
-
-    // Fire the Star.
-    Star::fire();
-
-    // Send data for all PortHoles.
-    port.reset();
-    for(i = numberPorts(); i > 0; i--)
-	(port++)->sendData();
 }
