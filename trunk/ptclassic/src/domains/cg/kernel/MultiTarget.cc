ad78cb67047dfe9f00a9fa60e87b127089a15e12
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-02-05 17:35:54 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg/kernel/MultiTarget.cc
@@ -0,0 +1,80 @@
+static const char file_id[] = "BaseMultiTarget.cc";
+
+/******************************************************************
+Version identification:
+$Id$
+
+ Copyright (c) 1991 The Regents of the University of California.
+                       All Rights Reserved.
+
+ Programmer: Soonhoi Ha
+
+ Scheduler-independent Baseclass for all multi-processor code generation 
+ targets. Virtual methods are provided for uniform wormhole interface.
+
+*******************************************************************/
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include "BaseMultiTarget.h"
+#include "Error.h"
+#include "Profile.h"
+#include <std.h>
+
+// constructor
+BaseMultiTarget::BaseMultiTarget(const char* name,const char* starclass,
+		   const char* desc) : CGTarget(name,starclass,desc),
+	nChildrenAlloc(0), curChild(0) {
+        addState(nprocs.setState("nprocs",this,"2","number of processors"));
+        addState(inheritProcessors.setState(
+	     "inheritProcessors",this,"NO","If yes, inherit child targets"));
+        addState(sendTime.setState("sendTime",this,"1",
+                                   "time to send one datum"));
+}
+
+void BaseMultiTarget :: setProfile(Profile*) {}
+
+int BaseMultiTarget :: totalWorkLoad() { return -1; }		// undefined...
+
+int BaseMultiTarget :: computeProfile(int pNum, int, IntArray*) {
+	// do nothing in this base class.
+	return pNum;
+}
+
+void BaseMultiTarget :: insideSchedule() {}
+
+void BaseMultiTarget :: downLoadCode(int, Profile*) {}
+
+void BaseMultiTarget :: saveCommPattern() {}
+void BaseMultiTarget :: restoreCommPattern() {}
+void BaseMultiTarget :: clearCommPattern() {}
+
+// By default, assume zero communication time.
+int BaseMultiTarget :: reserveComm(int,int,int when,int) { return when; }
+int BaseMultiTarget :: scheduleComm(DLNode*,int when,int) { return when; }
+
+IntArray* BaseMultiTarget :: candidateProcs(DLNode*, ParProcessors*) 
+	{ return NULL; }
+
+void BaseMultiTarget :: setTargets(int n) { 
+	nprocs = n; 
+	char temp[6];
+	sprintf(temp, "%d", n);
+	const char* t = hashstring(temp);
+	nprocs.setValue(t); }
+
+int BaseMultiTarget :: inheritChildTargets(Target* father) {
+
+        if (int(nprocs) > father->nProcs()) {
+                Error::abortRun(readName(), " target has more children than \
+its parent target: ", father->readName());
+                return FALSE;
+        }
+
+        nChildrenAlloc = int(nprocs);
+	inheritChildren(father,0,nChildrenAlloc-1);
+	return TRUE;
+}
+
