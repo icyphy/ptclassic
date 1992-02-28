a552ef861debc291d02fd8b741cbd168fc0d6ea8
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-02-28 13:50:50 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg56/targets/Sub56Target.h
@@ -0,0 +1,44 @@
+/******************************************************************
+Version identification:
+$Id$
+
+ Copyright (c) 1992 The Regents of the University of California.
+                       All Rights Reserved.
+
+ Programmer: J. Pino
+
+ Target for Motorola 56000 assembly code generation that generates
+ two subroutines, ptolemyInit, ptolemyMain.
+ 
+*******************************************************************/
+#ifndef _Sub56Target_h
+#define _Sub56Target_h 1
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include "CG56Target.h"
+#include "StringState.h"
+
+class Sub56Target : public CG56Target {
+private:
+	int genFile(StringList&,const char*);
+	char* uname;
+	void initStates();
+protected:
+	StringState dirName;
+public:
+	Sub56Target(const char*,const char*,
+		    unsigned,unsigned,unsigned,unsigned);
+	Sub56Target(const Sub56Target&);
+	~Sub56Target();
+	void headerCode();
+	int setup(Galaxy&);
+	int run();
+	void wrapup();
+	Block* clone() const;
+	int isA(const char*) const;
+};
+
+#endif
