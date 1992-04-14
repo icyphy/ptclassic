2efbdea535b63867ae6c418f10e4dbab03f85539
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-04-14 20:52:10 +0000

    improve explanation; simplify expression to fix problem with g++2.1

--- ptclassic/src/domains/sdf/dsp/stars/SDFHilbert.pl
+++ ptclassic/src/domains/sdf/dsp/stars/SDFHilbert.pl
@@ -9,6 +9,14 @@ Output the (approximate) Hilbert transform of the input signal.
 	author { J. T. Buck }
 	copyright { 1991 The Regents of the University of California }
 	location { SDF dsp library }
+	explanation {
+This star approximates the Hilbert transform by using an FIR filter.
+The exact Hilbert transform cannot be realized; instead, we just chop
+off the response symmetrically at -N/2 and N/2.  This is good enough
+for demos, but for high accuracy we suggest the use of the Parks-McClellan
+algorithm to design a Hilbert transformer filter with the desired
+characteristics.  The "optfir" program supplied with Ptolemy can do this.
+	}
 	ccinclude { <math.h> }
 	defstate {
 		name { N }
@@ -27,8 +35,12 @@ Output the (approximate) Hilbert transform of the input signal.
 		double k = 2.0 / M_PI;
 
 		taps.resize(len);
-		for (i = 0, n = -len2; i < len; i++, n++)
-			taps[i] = (n & 1) ? k / n : 0.0;
+		for (i = 0, n = -len2; i < len; i++, n++) {
+			if ((n & 1) != 0)
+				taps[i] = k / n;
+			else
+				taps[i] = 0.0;
+		}
 		SDFFIR :: start();
 	}
 }
