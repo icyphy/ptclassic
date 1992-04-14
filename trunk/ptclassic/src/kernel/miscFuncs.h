3e5ac72b6d880ff44c611a92d6b0aa8efbdd735c
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-04-14 20:48:56 +0000

    use <string.h> for string functions; add #pragma interface

--- trunk/ptclassic/src/kernel/miscFuncs.h
+++ trunk/ptclassic/src/kernel/miscFuncs.h
@@ -16,31 +16,15 @@ $Id$
 
 #ifndef _miscFuncs_h
 #define _miscFuncs_h 1
+#ifdef __GNUG__
+#pragma interface
+#endif
+
 #include <std.h>
 #include "logNew.h"
+#include <string.h>
 
-extern "C" {
-char*     strcat(char*, const char*);
-char*     strchr(const char*, int);
-int       strcmp(const char*, const char*);
-char*     strcpy(char*, const char*);
-int       strlen(const char*);
-char*     strncat(char*, const char*, int);
-int       strncmp(const char*, const char*, int);
-char*     strncpy(char*, const char*, int);
-char*     strrchr(const char*, int);
-double	  pow(double,double);
-}
-
-#ifdef USG
-inline char* index(const char* s, int c) { return strchr(s, c); }
-inline char* rindex(const char* s, int c) { return strrchr(s, c); }
-#else
-extern "C" {
-char*     index(const char*, int);
-char*     rindex(const char*, int);
-}
-#endif
+extern "C" double pow(double,double);
 
 const char* expandPathName(const char*);
 
@@ -51,7 +35,7 @@ inline char* savestring (const char* txt) {
 const char* hashstring(const char*);
 
 // I would prefer to make this int pow(int,int), but g++ gets confused
-// because other func is extern "C".
+// because the other function is extern "C".
 
 int power(int base,int exp);
 inline double power(double base,double exp) { return pow(base,exp);}
