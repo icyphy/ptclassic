18cfdcf664dde18546d922703df5d4be94d88331
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-04-14 20:48:54 +0000

    Initial revision

---
+++ ptclassic/src/kernel/miscFuncs.cc
@@ -0,0 +1,16 @@
+// miscellaneous implementations for kernel classes.
+// inlines for Attribute and the various ConstIters classes go here.
+// $Id$
+
+// This module has no code under other compilers than g++.
+
+#ifdef __GNUG__
+static const char file_id[] = "miscImpl.cc";
+#pragma implementation "Attribute.h"
+#pragma implementation "ConstIters.h"
+#pragma implementation "miscFuncs.h"
+
+#include "Attribute.h"
+#include "ConstIters.h"
+#include "miscFuncs.h"
+#endif
