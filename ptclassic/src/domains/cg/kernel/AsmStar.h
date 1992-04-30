13dc76da08ee448dfd9d4015aefce4e607a93079
Jose Luis Pino <joseluispino@users.noreply.github.com>
1992-04-30 15:20:17 +0000

    Added $label macro that generates unique labels

--- ptclassic/src/domains/cg/kernel/AsmStar.h
+++ ptclassic/src/domains/cg/kernel/AsmStar.h
@@ -7,7 +7,7 @@ $Id$
  Copyright (c) 1991 The Regents of the University of California.
                        All Rights Reserved.
 
- Programmer: J. Buck
+ Programmer: J. Buck & J. Pino
 
  This is the baseclass for stars that generate assembly language code.  
 
@@ -17,6 +17,8 @@ $Id$
 #include "AsmConnect.h"
 // FixSample is included so AsmStars can use FIX-type ports
 #include "FixSample.h"
+#define		MAX_NUM_LABELS	10
+#define		MAX_LABEL_LEN	10
 
 // Define attribute bit masks for code generation states
 const bitWord AB_MEMORY = 0x20;	// state is in memory
@@ -113,7 +115,10 @@ protected:
 	// functions are handled: $addr(name), $mem(name),
 	// $val(name), and $ref(name).  The $ref(name) is equivalent
 	// to $mem(name):$addr(name).  The $val(name) refers to the value
-	// of a State.  The number, names, and meaning of
+	// of a State.  The $label(name) creates returns the unique
+	// label for (codeblock,name) pair.  If the label does not exist,
+	// a new, unique label is created.
+	//The number, names, and meaning of
 	// these functions can be easily redefined in derived classes.
 	virtual StringList processMacro(const char* func, const char* id,
 					const char* arg2);
@@ -133,6 +138,21 @@ private:
 	// State entry list.  This stores the addresses allocated to each
 	// State.
 	StateAddrEntry* addrList;
+	
+	// List of all local star labels
+	static char labels[MAX_NUM_LABELS][MAX_LABEL_LEN];
+
+	// Total Number of Labels generated.
+	static int numLabels;
+
+	// Total Number of Labels generated.
+	static int lastLocalLabel;
+
+	// Reset local star labels
+	void resetLabels(){ lastLocalLabel = -1; }
+
+ 	// Lookup the unique global label name for the local label
+	StringList label(const char* name);
 };
 
 class AsmStarPortIter : public BlockPortIter {
