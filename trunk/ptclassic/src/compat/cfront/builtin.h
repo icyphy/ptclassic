b2878fe41b6d7bfae49b3a4cf293d8fcd3a458eb
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-05-21 17:19:17 +0000

    move min, max stuff to minmax.h

--- trunk/ptclassic/src/compat/cfront/builtin.h
+++ trunk/ptclassic/src/compat/cfront/builtin.h
@@ -10,19 +10,4 @@
 inline double abs(double a) { return (a >= 0) ? a : -a;}
 inline int abs(int a) { return (a >= 0) ? a : -a;}
 
-// min and max functions, to match those defined in the libg++ builtin.h.
-
-// with templates we will need only
-// template<class T> inline T min(T a, T b) {return (a < b ? a : b);}
-// template<class T> inline T max(T a, T b) {return (a > b ? a : b);}
-
-inline double   min(double a, double b)     {return (a < b ? a : b);}
-inline float    min(float a, float b)       {return (a < b ? a : b);}
-inline int      min(int a, int b)           {return (a < b ? a : b);}
-inline unsigned min(unsigned a, unsigned b) {return (a < b ? a : b);}
-inline double   max(double a, double b)     {return (a > b ? a : b);}
-inline float    max(float a, float b)       {return (a > b ? a : b);}
-inline int      max(int a, int b)           {return (a > b ? a : b);}
-inline unsigned max(unsigned a, unsigned b) {return (a > b ? a : b);}
-
 #endif
