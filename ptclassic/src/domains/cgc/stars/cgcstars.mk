3b25b5e0089602a9ad436c1cd80f31df5753b661
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-06-12 13:07:10 +0000

    Initial revision

---
+++ ptclassic/src/domains/cgc/stars/cgcstars.mk
@@ -0,0 +1,12 @@
+# $Id$
+
+# This file contains a list of the official CGC  stars.  It is intended
+# to be included in other makefiles.  Before it is included, the variable
+# CGC must be defined to refer to the directory containing the object files
+# for the stars.
+
+CGCSTARS = \
+ $(CGC)/CGCFloatRamp.o \
+ $(CGC)/CGCFork.o \
+ $(CGC)/CGCPrinter.o
+
