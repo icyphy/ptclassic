d4d6b02b8471f5401495dc7eecc4229402fbf8a1
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-06-23 03:32:45 +0000

    move initOffset method from CGPortHole to AsmPortHole class

--- trunk/ptclassic/src/domains/cg/kernel/CGPortHole.cc
+++ trunk/ptclassic/src/domains/cg/kernel/CGPortHole.cc
@@ -36,41 +36,6 @@ Geodesic* CGPortHole::allocateGeodesic() {
         return g;
 }
 
-int CGPortHole::bufSize() const {
-        return cgGeo().bufSize();
-}
-
-int CGPortHole::localBufSize() const {
-	return cgGeo().localBufSize();
-}
-
-// initialize the offset member.  If there is no fork involved, input
-// portholes start reading from offset 0, and output portholes start
-// writing just after any delay tokens.
-// If there are forks, outputs are done the same way, and input pointers
-// are backed up to handle the "forkDelay".
-int CGPortHole::initOffset() {
-	int bsiz = bufSize();
-	if (isItOutput()) {
-		offset = numTokens();
-		// the following error is already reported by bufSize()
-	        if (offset > bsiz) return FALSE;
-		if (offset == bsiz) offset = 0;
-	}
-	else {
-		offset = -cgGeo().forkDelay();
-		if (offset < 0) offset += bsiz;
-		// if still < 0, forkDelay is larger than we can handle
-		if (offset < 0) {
-			Error::abortRun(*this,
-				" sorry, forkDelay is larger\n",
-				"than the current implementation can handle");
-			return FALSE;
-		}
-	}
-	return TRUE;
-}
-
 // make me a fork destination; set my source.
 void CGPortHole::setForkSource(CGPortHole * p) {
 	forkSrc = p;
@@ -87,6 +52,9 @@ void MultiCGPort :: forkProcessing (CGPortHole& p) {
 		p.setForkSource(forkSrc);
 	}
 }
+
+int CGPortHole :: bufSize() const { return cgGeo().bufSize(); }
+int CGPortHole :: localBufSize() const { return cgGeo().localBufSize(); }
 
 int InCGPort :: isItInput () const { return TRUE;}
 int OutCGPort :: isItOutput () const { return TRUE;}
