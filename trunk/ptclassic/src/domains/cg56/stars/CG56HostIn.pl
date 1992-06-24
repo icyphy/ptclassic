42b0939b90f60bff51cc67a020bf714d1e600bed
huangc <huangc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-06-24 17:07:04 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg56/stars/CG56HostIn.pl
@@ -0,0 +1,34 @@
+defstar {
+	name { HostIn  }
+	domain { CG56 }
+	desc { Data Input }
+	version { $Id$ }
+	author { Chih-Tsung, ported from Gabriel }
+	copyright { 1992 The Regents of the University of California }
+	location { CG56 demo library }
+	explanation {
+DSP56000 -- Input data from host to DSP via host port.
+	}
+	execTime {
+		return 2;
+	}
+	output {
+		name {output}
+		type {FIX}
+	}
+        state {
+                name {command}
+	        type {string}
+	        desc { }
+	        default {""}
+	}
+	codeblock (std) {
+$label(l)
+        jclr    #m_hrdf,x:m_hsr,$label(l)
+        jclr    #1,x:m_pbddr,$label(l)
+        movep   x:m_hrx,$ref(output)
+	}
+	go {
+		gencode(std);
+	}
+}
