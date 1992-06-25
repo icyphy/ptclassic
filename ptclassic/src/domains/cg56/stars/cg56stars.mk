1c28c91ffdb6d1c1899606a0d3e377f9e4798ef6
huangc <huangc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-06-25 14:18:58 +0000

    no

--- ptclassic/src/domains/cg56/stars/cg56stars.mk
+++ ptclassic/src/domains/cg56/stars/cg56stars.mk
@@ -7,21 +7,46 @@
 
 CG56STARS = \
  $(CG56)/CG56Abs.o \
+ $(CG56)/CG56ACOS.o \
  $(CG56)/CG56Add.o \
+ $(CG56)/CG56ASIN.o \
  $(CG56)/CG56BiQuad.o \
  $(CG56)/CG56BlackHole.o \
+ $(CG56)/CG56Commutator.o \
  $(CG56)/CG56Cos.o \
  $(CG56)/CG56DC.o \
+ $(CG56)/CG56Distributor.o \
  $(CG56)/CG56DownSample.o \
  $(CG56)/CG56Fork.o \
  $(CG56)/CG56Gain.o \
  $(CG56)/CG56DummySource.o \
  $(CG56)/CG56DummySink.o \
+ $(CG56)/CG56HostIn.o \
  $(CG56)/CG56HostOut.o \
  $(CG56)/CG56IIDUniform.o \
+ $(CG56)/CG56Impulse.o \
  $(CG56)/CG56Integrator.o \
+ $(CG56)/CG56Interpolator.o \
+ $(CG56)/CG56IntMult.o \
+ $(CG56)/CG56Limit.o \
+ $(CG56)/CG56Log.o \
+ $(CG56)/CG56Mult.o \
+ $(CG56)/CG56Neg.o \
+ $(CG56)/CG56Peak.o \
+ $(CG56)/CG56Pickmin.o \
+ $(CG56)/CG56PickminVal.o \
+ $(CG56)/CG56Quantizer.o \
+ $(CG56)/CG56QuantizerInt.o \
+ $(CG56)/CG56Sgn.o \
+ $(CG56)/CG56Shifter.o \
+ $(CG56)/CG56Sin.o \
+ $(CG56)/CG56Sqr.o \
+ $(CG56)/CG56Sqrt.o \
+ $(CG56)/CG56TableLookup.o \
  $(CG56)/CG56Sub.o \
  $(CG56)/CG56Tone.o \
  $(CG56)/CG56UpSample.o \
- $(CG56)/CG56WriteFile.o
+ $(CG56)/CG56WriteFile.o \
+ $(CG56)/CG56WriteFileInt.o
+ $(CG56)/CG56ZeroHld.o
 
