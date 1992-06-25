d736104ae74c81b2e83347733aca496098f87767
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-06-25 12:15:45 +0000

    added cos & hostout

--- trunk/ptclassic/src/domains/cg56/stars/cg56stars.mk
+++ trunk/ptclassic/src/domains/cg56/stars/cg56stars.mk
@@ -10,12 +10,14 @@ CG56STARS = \
  $(CG56)/CG56Add.o \
  $(CG56)/CG56BiQuad.o \
  $(CG56)/CG56BlackHole.o \
+ $(CG56)/CG56Cos.o \
  $(CG56)/CG56DC.o \
  $(CG56)/CG56DownSample.o \
  $(CG56)/CG56Fork.o \
  $(CG56)/CG56Gain.o \
  $(CG56)/CG56DummySource.o \
  $(CG56)/CG56DummySink.o \
+ $(CG56)/CG56HostOut.o \
  $(CG56)/CG56IIDUniform.o \
  $(CG56)/CG56Integrator.o \
  $(CG56)/CG56Sub.o \
