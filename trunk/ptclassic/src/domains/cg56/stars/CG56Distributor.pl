e4e00974ffc686d27c70af83de68cf76c34dbb08
huangc <huangc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-06-26 10:46:51 +0000

    no

--- trunk/ptclassic/src/domains/cg56/stars/CG56Distributor.pl
+++ trunk/ptclassic/src/domains/cg56/stars/CG56Distributor.pl
@@ -25,6 +25,22 @@ Distributes an input signal among two outputs, alternating samples.
                 default {1}
                 desc {Number of particles in a block.}
 	}
+        state  {
+                name { outputNum }
+                type { int }
+                default { 0 }
+                desc { input#() }
+                attributes { A_NONCONSTANT|A_NONSETTABLE }
+        }
+
+        state  {
+                name { runtimeVal }
+                type { int }
+                default { 0 }
+                desc { runtime value }
+                attributes { A_NONCONSTANT|A_NONSETTABLE }
+        }
+
 	start {
                 int n = output.numberPorts();
                 input.setSDFParams(n*int(blockSize),n*int(blockSize)-1);
@@ -34,19 +50,34 @@ Distributes an input signal among two outputs, alternating samples.
                ((SDFPortHole*)p)->setSDFParams(int(blockSize),int(blockSize)-1);
         }
 
+        codeblock (one) {
+        move    $ref(input),x0        ; just move data from in to out
+        move    x0,$ref(output#1)
+        }
+
  	codeblock(main) {
         move    #$addr(input),r1
         nop
+        }
+        codeblock(loop) {
         clr     a               x:(r1)+,x0
-        move    x0,$ref(output#1)
-        clr     a               x:(r1)+,x0
-        move    x0,$ref(output#2)
+        move    x0,$ref(output#outputNum)
 	}
 
-	go {
-		gencode(main);
-	}
-	exectime {
-		return 5;
-	}
+        go {
+                if (output.numberPorts() == 1) {
+                        gencode(one);
+                        return;
+                }
+                gencode(main);
+                for (int i = 1; i <= output.numberPorts(); i++) {
+                        outputNum=i;
+                        gencode(loop);
+                }
+                runtimeVal=output.numberPorts();
+        }
+       exectime {
+                return (2*(int(runtimeVal))+1) ;
+        }
 }
+
