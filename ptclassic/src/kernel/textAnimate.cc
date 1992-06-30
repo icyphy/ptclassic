53b458add18d1fb408faa5485a368976cd838f89
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-06-30 20:25:48 +0000

    Initial revision

---
+++ ptclassic/src/kernel/textAnimate.cc
@@ -0,0 +1,42 @@
+// Animation functions for text interfaces
+// $Id$
+// Copyright (c) 1992 The Regents of the University of California.
+//                      All Rights Reserved.
+//
+// Programmer: J. Buck
+
+static const char file_id[] = "textAnimate.cc";
+
+#include "SimControl.h"
+#include "Star.h"
+#include "textAnimate.h"
+#include <stream.h>
+
+static SimAction *pre = 0, *post = 0;
+
+static void textHighlight(Star* s, const char*) {
+	cout << "RUNNING: " << s->readFullName() << " ... ";
+}
+
+static void textClearHighlight(Star* s,const char*) {
+	cout << "DONE\n";
+}
+
+int textAnimationState() { return pre != 0;}
+
+void textAnimationOn() {
+	if (!pre) {
+		pre = SimControl::registerAction(textHighlight,1);
+		post = SimControl::registerAction(textClearHighlight,0);
+	}
+}
+
+void textAnimationOff() {
+	if (pre) {
+		SimControl::cancel(pre);
+		SimControl::cancel(post);
+	}
+	pre = post = 0;
+}
+
+		
