73655d8ced59d148fe581bdd4c72c9779201fbf9
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-07-09 19:44:16 +0000

    limit # of bins (default limit = 1000)

--- trunk/ptclassic/src/kernel/Histogram.h
+++ trunk/ptclassic/src/kernel/Histogram.h
@@ -30,17 +30,24 @@ private:
 	int xsize;
 public:
 	intVec() : xsize(0), data(0) {}
-	~intVec() { INC_LOG_DEL; delete data; }
+	~intVec() { INC_LOG_DEL; delete [] data; }
 	int& elem(int i) { return data[i];}
 	int& operator[](int i) { return elem(i);}
 	int capacity() const { return xsize;}
 	void resize(int newSize);
 };
 
+// default max # of bins.
+const int HISTO_MAX_BINS = 1000;
+
 class Histogram {
 public:
-	Histogram(double width = 1.0) : centerRef(0), nSamples(0), minBin(0),
-	maxBin(0), sum(0), sumsq(0), binWidth(width) {}
+	Histogram(double width = 1.0, int maxBins = HISTO_MAX_BINS)
+		: centerRef(0), nSamples(0), minBin(0),
+	maxBin(0), sum(0), sumsq(0), binWidth(width), maxNoBins(maxBins) {}
+
+	// translate value to bin number
+	int valueToBin(double x) const;
 
 	// add a new sample to the histogram.
 	void add(double x);
@@ -71,6 +78,7 @@ private:
 	int nSamples;		// number of counts
 	int minBin;		// minimum bin #
 	int maxBin;		// maximum bin #
+	int maxNoBins;		// maximum # of bins allowed.
 	double sum;		// sum of all samples
 	double sumsq;		// sum of all samples, squared
 	double binWidth;	// width of bins
@@ -89,7 +97,8 @@ public:
 			double binWidth,
 			const char* options,
 			const char* title,
-			const char* saveFile);
+			const char* saveFile,
+			int maxBins = HISTO_MAX_BINS);
 
 	void addPoint(float y);
 
