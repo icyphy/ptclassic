9ce2f39da8594ebfcb5bdb9f0247f9799174d096
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-07-23 12:28:18 +0000

    reorganize; add hooks for modifying the universe

--- ptclassic/src/domains/cg/kernel/CGTarget.h
+++ ptclassic/src/domains/cg/kernel/CGTarget.h
@@ -39,29 +39,68 @@ protected:
 	// Return a list of spaces for indenting
 	StringList indent(int depth);
 
-	// calculate the buffer request.
-	virtual int decideBufSize(Galaxy&);
+	// The following virtual functions do the stages of CGTarget::setup.
+	// generally, derived classes should overload these functions rather
+	// than overloading setup.
 
-	// do initialization: say compute offsets of portholes and
-	// generate initCode. But in this base class, do nothing
+	// perform any extra initializations of the stars in the galaxy.
+	// This version assumes the stars are CGStars; if not true, it
+	// must be overridden.
+	virtual void initStars(Galaxy&);
+
+	// perform necessary modifications to the galaxy (e.g. splice in
+	// additional stars).  This happens before initialization.
+	// returns FALSE on error.  This class has a do-nothing version.
+	// if defined, it is responsible for calling galaxy.initialize()
+	// if needed to do the start functions.
+	virtual int modifyGalaxy(Galaxy&);
+
+	// compute buffer sizes, allocate memory, etc.  return TRUE for
+	// success, false for failure.  Called after schedule generation
+	// and modifyGalaxy.
+	virtual int allocateMemory(Galaxy&);
+
+	// do initialization for code generation: for example, compute
+	// offsets of portholes and generate initCode. But in this base
+	// class, do nothing
 	virtual int codeGenInit(Galaxy&);
+
+	// writes initial code
+	virtual void headerCode();
 
 public:
 	CGTarget(const char* name, const char* starclass, const char* desc,
 		 char sep = '_');
 
 	~CGTarget();
-	void initialize();
-	// The setup method should not be invoked if the stars are not
-	// CGStars or their portHoles are not CGPortHoles.
-	int setup(Galaxy&);
+
+	// function that initializes the Target itself, before a Galaxy
+	// is attached to it.  This is called first (before setup).
 	void start();
+
+	// function that computes the schedule, allocates memory (if needed),
+	// and prepares for code generation (generating header and initial
+	// code)
+	int setup(Galaxy&);
+
+	// function that executes the schedule to generate the main code.
 	int run();
+
+	// finalization.  In derived classes, wrapup might download and
+	// execute the generated code; the baseclass simply displays the
+	// code.
 	void wrapup();
+
+	// generate a new, identical CGTarget.
 	Block* clone() const;
+
+	// fn for adding code to the target
 	void addCode(const char*);
-	virtual void headerCode();
+
+	// do we need this?
 	virtual void writeCode(UserOutput&);
+
+	// type identification
 	int isA(const char*) const;
 
 	// Total Number of Labels generated.
