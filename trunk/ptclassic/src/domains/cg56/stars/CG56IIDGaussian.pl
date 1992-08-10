0f9c63b53b590bed54b3009668706f2cad22594f
huangc <huangc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-08-10 13:46:25 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg56/stars/CG56IIDGaussian.pl
@@ -0,0 +1,113 @@
+defstar {
+	name { GaussianClt }
+	domain { CG56 }
+	desc {Gaussian Noise Source}
+	version { $Id$ }
+	author { Chih-Tsung Huang, ported from Gabriel }
+	copyright { 1992 The Regents of the University of California }
+	location { CG56 demo library }
+        explanation {
+.PP
+This star generates a sequence of random output samples with a Gaussian
+distribution (mean = 0, standard deviation = 0.1).
+.PP
+According to the central limit theorem, the sum of N random variables
+approaches a Gaussian distribution as N approaches infinity.
+This star generates an output number by summing \fIno_uniforms\fR uniform
+random variables.
+.PP
+The parameters \fIseed\fR and \fImultiplier\fR control the generation of the
+random number. 
+.SH BUGS
+This needs to be filled in.  For now, there is no seed and multiplier
+parameter; the default seed parameter from Gabriel is always used.
+We'd really need to use a 48-bit integer to get the same functionality.
+This can be done with g++ (type "long long"), but it isn't portable.
+	}
+
+        output {
+		name { output }
+		type { fix }
+	}
+        state  {
+                name { noUniforms }
+		type { int }
+		desc { uniform random variables. }
+		default { 16 }
+        }
+
+        state {
+                name { accum }
+	        type { int }
+	        default { 0 }
+	        desc { internal }
+	        attributes {
+		    A_SYMMETRIC|A_RAM|A_NONCONSTANT|A_NONSETTABLE|A_NOINIT
+	        }
+        }
+        state {
+                name { ravs }
+	        type { fixarray }
+	        default { "0" }
+	        desc { internal }
+	        attributes { A_XMEM|A_NONCONSTANT|A_NONSETTABLE|A_NOINIT }
+        }
+
+        start {
+                ravs.resize(noUniforms);
+        }		
+        initCode {
+                gencode(block);
+        }		
+        go {
+                gencode(std);
+        }		
+
+        codeblock(block) {
+; initial seed for $fullname()
+        org     x:$addr(accum)
+        dc      1246684
+        org     p:
+
+        org     y:$addr(accum)
+        dc      8123721
+        org     p:
+
+; intermediate storage for random variables -- $fullname()
+        org     $ref(ravs)
+        bsc     $val(noUniforms),0
+        org     p:
+        }
+                       
+        codeblock(std) {
+        move    #$addr(ravs),r0
+        do      #$val(noUniforms),$label(cont)
+        move    #>10916575,y1
+        move    l:<$addr(accum),x
+        mpy     x0,y1,a    #>12648789,y0
+        mac     +x1,y0,a   y1,b1
+        asr     a          y0,b0
+        mpy     x0,y0,a    a0,x1
+        addr    b,a
+        add     x1,a       #>363237,x0
+        move    a1,y0
+        mpy     x0,y0,a    a10,l:<$addr(accum)
+        move    a,x:(r0)+
+$label(cont)
+
+; generate Gaussian, mean=0, sigma=0.1
+        move    #$addr(ravs),r0
+        clr a
+        move  x:(r0)+,x0
+        rep   #$val(noUniforms)-1
+        add   x0,a        x:(r0)+,x0
+        add   x0,a
+        move  a,$ref(output)
+        }
+
+	execTime { 
+                 return 10+15*int(noUniforms);
+	}
+}
+
+
---
+++ trunk/ptclassic/src/domains/cg56/stars/CG56Sub.pl
@@ -0,0 +1,54 @@
+defstar {
+	name { FixDiff }
+	domain { CG56 }
+	desc { any input subtractor }
+	version { $Id$ }
+	author { Chih-Tsung Huang }
+	copyright { 1992 The Regents of the University of California }
+	location { CG56 demo library }
+	explanation {
+.PP
+Output the \fIpos\fR minus all \fIneg\fR inputs.
+        }    
+	input {
+		name { pos }
+		type { fix }
+ 	}
+	inmulti {
+		name { neg }
+		type { fix }
+ 	}
+        state  {
+                name { inputNum }
+                type { int }
+                default { 0 }
+                desc { input#() }
+                attributes { A_NONCONSTANT|A_NONSETTABLE }
+        }
+	output {
+		name { output }
+		type { fix }
+	}
+        codeblock(main) {
+        move    $ref(pos),a
+        }
+        codeblock(loop) {
+        move    $ref(neg#inputNum),x0
+        sub     x0,a
+        }
+        codeblock(done) {
+        move    a,$ref(output)
+        }
+
+	go {
+                gencode(main);
+                for (int i = 1; i <= neg.numberPorts(); i++) {
+                        inputNum=i;
+                        gencode(loop);
+                }
+                gencode(done);
+	}
+	execTime {
+                return 2*int(neg.numberPorts()) +2;
+       }
+}
