87d7bbefbca1d004c48376f03b796897cc32557d
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-08-25 15:59:38 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/pn/kernel/PNEventHorizon.cc
@@ -0,0 +1,16 @@
+static const char file_id[] = "$RCSfile$";
+
+/*  Version $Id$
+
+    Copyright 1992 The Regents of the University of California.
+			All Rights Reserved.
+
+    Programmer:		T.M. Parks
+    Date of creation:	20 January 1992
+*/
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include "MTDFWormConnect.h"
