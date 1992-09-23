e2e9e862be068446351e7cfcf3b2d6141e425412
kalavade <kalavade@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-09-23 18:37:59 +0000

    cleaned code

--- trunk/ptclassic/src/domains/sdf/stars/SDFBusToNum.pl
+++ trunk/ptclassic/src/domains/sdf/stars/SDFBusToNum.pl
@@ -2,9 +2,9 @@ defstar {
 	name { BusToNum }
 	domain { SDF }
 	desc {
-This will receive as its input,bus (with 'bits' no of lines ) 
-and then convert it to an integer corresponding to the
-value of this binary number and output that as a particle.
+This will receive as its input, a bus (with 'bits' no of lines ) 
+and then output a signed integer corresponding to the value of this 
+binary number.
 	}
 	author { Asawaree Kalavade }
 	version { $Id$ }
@@ -21,7 +21,7 @@ value of this binary number and output that as a particle.
 		name {bits}
 		type {int}
 		default {"7"}
-		desc {number of bits to output}
+		desc {number of bits in input}
 	}
 	defstate {
 		name {previous}
@@ -32,57 +32,70 @@ value of this binary number and output that as a particle.
 	
 	go {
 		int b;
-		int number =0 ;
-		int binary[8];
-		int change=0;
+		int binary[bits+1];
 		MPHIter nexti(input);
+		int number 	= 0;
 
-		for( int i=0; i< int (bits); i++){
-		binary[i]= (*nexti++)%0;
+	// latch bits into array
+		for( int i=0; i< bits; i++){
+			binary[i]= (*nexti++)%0;
 		}
-		for(i =0; i<7; i++)
+
+	// The following is a thor-specific condition check where we dont
+	// want the number to be latched in if the bus is tristate (value 3)
+		for(i=0; i< bits; i++)
 		{
-		if(binary[i] ==3){
-		change=1;
-		output%0 << int(previous); 
-		return; }
+			if(binary[i] ==3){
+				output%0 << previous; 
+				return; 
+			}
 		}
 
-        if(binary[6]==1){
-                for(int j=0;j<6;j++){
-                b=binary[j];
+	// check the MSB for sign bit and convert the number appropriately
+        if(binary[bits-1]==1)
+	{
+                for(int j=0;j<(bits-1);j++)
+		{
+                	b=binary[j];
                         if(b==0) b=1;
                         else b=0;
-                binary[j]= b;}
-                        for(i=0;i<6;i++) {
-                            switch( binary[5-i]) {
-                                case    0:
-                                        number <<=1;
-                                        break;
-                                case    1:
-                                        number <<=1;
-                                        number +=1;
-                                        break;
-                                                }
-                                        }
+                	binary[j]= b;
+		}
+                for(i=0;i<(bits-1);i++) 
+		{
+			switch( binary[(bits-2)-i]) 
+			{
+                        case    0:
+                       		number <<=1;
+                                break;
+                        case    1:
+                                number <<=1;
+                                number +=1;
+                                break;
+                        }
+                 }
                 number +=1;
                 number = number * (-1);
-                        }
-        else {
-                        for(i=0;i<6;i++) {
-                            switch( binary[5-i]) {
-                                case    0:
-                                        number <<=1;
-                                        break;
-                                case    1:
-                                        number <<=1;
-                                        number +=1;
-                                        break;
-                                                }
-                                        }
-                       }
+	}
+        else 
+	{
+		for(i=0;i<(bits-1);i++) 
+		{
+			switch( binary[(bits-2)-i]) 
+			{
+			case    0:
+				number <<=1;
+				break;
+			case    1:
+				number <<=1;
+				number +=1;
+				break;
+			}
+		}
+         }
 
 		previous= number;
 		output%0 << number;	
-}
+
+	} // go
 }
