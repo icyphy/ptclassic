4aff391692a0f9c34c58fa6449ecb280009c65bd
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-10-05 15:01:10 +0000

    rewrite of CGSymbol

--- trunk/ptclassic/src/domains/cg/kernel/SymbolList.cc
+++ trunk/ptclassic/src/domains/cg/kernel/SymbolList.cc
@@ -8,8 +8,6 @@ $Id$
 
  Programmer: J. Pino, J. Buck
 
- Symbol stuff.
-
 *******************************************************************/
 
 #ifdef __GNUG__
@@ -17,52 +15,90 @@ $Id$
 #endif
 
 #include "CGSymbol.h"
-#include "CGTarget.h"
+#include "miscFuncs.h"
+#include "StringList.h"
+#include "type.h"
+#include <string.h>
 
-Symbol::Symbol (CGTarget* target) {
-	initialize();
-	myTarget = target;
+Symbol::Symbol(const char* k,const char* s) {
+	key = savestring(k);
+	symbol = savestring(s);
 }
-	
+
+Symbol::~Symbol() {
+	LOG_DEL; delete(key);
+	LOG_DEL; delete(symbol);
+}
+
+int BaseSymbolList::numSymbols = 0;
+
+void BaseSymbolList::reset() { 
+	numSymbols = 0;
+}
+
 // Lookup unique symbol, if one doesn't exist, create new symbol.
-// Symbols are stored in pairs: key, symbol.
-StringList Symbol::lookup(const char* name) {
-	StringListIter next(symbols);
-	const char* p;
-    	StringList s;
-    	while ((p = next++) != 0) {
-		s = next++;
-		if(!strcmp(name,p)) return s;
-   	}
-   	// name is not on the list.  add it.
-  	symbols += name;
-  	s = name;
-	s += myTarget->separator;
-    	s += myTarget->numLabels++;
-    	// add the new symbol as well
-   	symbols += s;
-    	return s;
-}
-	
-
-NestedSymbol::NestedSymbol (CGTarget* target) {
-	initialize();
-	myTarget = target;
-}
-	
-const char* NestedSymbol::push(const char* name) {
-	StringList s;
-   	s = name;
-	s += myTarget->separator;
-    	s += myTarget->numLabels++;
-	const char* temp = s;
-   	symbols.push(temp);
-    	return savestring(temp);
+// SymbolLists are stored in pairs: key, symbol.
+const char* BaseSymbolList::lookup(const char* name) {
+	if (name==NULL) return NULL;
+	ListIter next(*this);
+	Symbol* p;
+	while ((p = (Symbol*)next++) != 0)
+		if(!strcmp(name,p->key)) return p->symbol;
+	return NULL;
 }
 
-const char* NestedSymbol::pop() {
-	if(depth()==0) return 0;
-	return symbols.pop();
+const char* BaseSymbolList::add(const char* name) {
+	if (name==NULL) return NULL;
+	StringList symbol;
+	symbol << name << separator << numSymbols++;
+	LOG_NEW; Symbol* newSymbol = new Symbol(name,symbol);
+	tup(newSymbol);
+	return newSymbol->symbol;
+}
 
+int BaseSymbolList::remove(Symbol* s) { 
+	LOG_DEL; delete(s);
+	return SequentialList::remove(s);
+}
+
+void BaseSymbolList::initialize() {
+	ListIter next(*this);
+	for (int i=size(); i > 0; i--) {
+		Symbol* p = (Symbol*)next++;
+		LOG_DEL; delete p;
+	}           
+	SequentialList::initialize();
+}               
+
+BaseSymbolList::~BaseSymbolList() {               
+    ListIter next(*this);
+    for (int i=size(); i > 0; i--) {           
+                Symbol* p = (Symbol*)next++;
+                LOG_DEL; delete p;
+    }           
+}               
+
+const char* SymbolList::lookup(const char* name) {
+	if (name==NULL) return NULL;
+	const char* pSymbol = BaseSymbolList::lookup(name);
+	if (pSymbol==NULL) pSymbol = add(name);
+	return pSymbol;
 }
 	
+const char* NestedSymbolList::push(const char* name) {
+    	return add(name);
+}
+
+const char* NestedSymbolList::pop() {
+	if (top() == NULL) return NULL;
+	Symbol* pSymbol = top();
+	StringList symbol = pSymbol->symbol;
+	remove(pSymbol);	
+	return symbol;
+}
+
+const char* NestedSymbolList::lookup(const char* name) {
+	if (top() == NULL) return NULL;
+	if (name == NULL) return top()->symbol;
+	return BaseSymbolList::lookup(name);
+}
