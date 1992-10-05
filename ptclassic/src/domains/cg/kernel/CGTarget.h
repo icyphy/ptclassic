51203feabb5cb52f4fa0297459cd0d03f34e0713
Jose Luis Pino <joseluispino@users.noreply.github.com>
1992-10-05 15:09:59 +0000

    added targetStreamLists

--- ptclassic/src/domains/cg/kernel/CGTarget.h
+++ ptclassic/src/domains/cg/kernel/CGTarget.h
@@ -23,154 +23,169 @@ $Id$
 #include "StringState.h"
 #include "CGSymbol.h"
 #include "SimControl.h"
+#include "CodeStreamList.h"
 
 class CGStar;
 class SDFSchedule;
 
 class CGTarget : public Target {
 public:
-	CGTarget(const char* name, const char* starclass, const char* desc,
-		 char sep = '_');
+    CGTarget(const char* name, const char* starclass, const char* desc,
+         char sep = '_');
 
-	~CGTarget();
+    // separator for symbols, <name><separator><unique number> == unique symbol
+    char separator;
 
-	static int haltRequested() {return SimControl::haltRequested();}
+    ~CGTarget();
 
-	// function that initializes the Target itself, before a Galaxy
-	// is attached to it.  This is called first (before setup).
-	void start();
+    static int haltRequested() {return SimControl::haltRequested();}
 
-	// function that computes the schedule, allocates memory (if needed),
-	// and prepares for code generation (generating header and initial
-	// code)
-	int setup(Galaxy&);
+    // function that initializes the Target itself, before a Galaxy
+    // is attached to it.  This is called first (before setup).
+    void start();
 
-	// function that executes the schedule to generate the main code.
-	int run();
+    // function that computes the schedule, allocates memory (if needed),
+    // and prepares for code generation (generating header and initial
+    // code)
+    int setup(Galaxy&);
 
-	// finalization.  In derived classes, wrapup might download and
-	// execute the generated code; the baseclass simply displays the
-	// code.
-	void wrapup();
+    // function that executes the schedule to generate the main code.
+    int run();
 
-	// generate a new, identical CGTarget.
-	Block* clone() const;
+    // finalization.  In derived classes, wrapup might download and
+    // execute the generated code; the baseclass simply displays the
+    // code.
+    void wrapup();
 
-	// fn for adding code to the target
-	void addCode(const char*);
+    // generate a new, identical CGTarget.
+    Block* clone() const;
 
-	// methods to compile and run the target.
-	// check access privilege later.
-	virtual int compileCode();
-	virtual int loadCode();
-	virtual int runCode();
+    // fn for adding code to the target
+    void addCode(const char*);
 
-	// do we need this?
-	virtual void writeCode(ostream&);
+    // methods to compile and run the target.
+    // check access privilege later.
+    virtual int compileCode();
+    virtual int loadCode();
+    virtual int runCode();
 
-	// generate code for a processor in a multiprocessor system
-	virtual StringList generateCode(Galaxy&);
+    // do we need this?
+    virtual void writeCode(ostream&);
 
-	// type identification
-	int isA(const char*) const;
+    // generate code for a processor in a multiprocessor system
+    virtual StringList generateCode(Galaxy&);
 
-	// Total Number of Labels generated.
-	int numLabels;
-	// Label Separator
-	char separator;
+    // type identification
+    int isA(const char*) const;
 
-	// output a comment.  Default form uses C-style comments.
-	virtual void outputComment (const char*);
+    // output a comment.  Default form uses C-style comments.
+    virtual void outputComment (const char*);
 
-	// generate code for a firing.  The definition here simply
-	// fires the star
-	void writeFiring(Star&,int depth);
+    // generate code for a firing.  The definition here simply
+    // fires the star
+    void writeFiring(Star&,int depth);
 
-	// dummy beginIteration and endIteration
-	void beginIteration(int,int);
-	void endIteration(int,int);
+    // dummy beginIteration and endIteration
+    void beginIteration(int,int);
+    void endIteration(int,int);
 
-	// system call in destination directory.  If error is specified
-	// & the system call is unsuccessful display the error message.
-	virtual int systemCall(const char*,const char* error=NULL);
+    // system call in destination directory.  If error is specified
+    // & the system call is unsuccessful display the error message.
+    virtual int systemCall(const char*,const char* error=NULL);
 
-	// Copy an SDF schedule from the multiprocessor schedule, instead
-	// of performing an SDF scheduler for a uni-processor target
-	void copySchedule(SDFSchedule&);
+    // Copy an SDF schedule from the multiprocessor schedule, instead
+    // of performing an SDF scheduler for a uni-processor target
+    void copySchedule(SDFSchedule&);
+    
+	// return the pointer of a code StringList given its name.  If it is 
+	// not found, This method allows stars to access a code StringList by 
+	// name.  If stream is not found, return NULL.
+	StringList* getStream(const char* name);
+	
 protected:
-	NestedSymbol targetNestedSymbol;
+    // Add a code StringList to the target.  This allows stars to access this
+    // stream by name.  This method should be called in the the target's
+    // constructor.  If a target tries to add a stream where another stream
+    // with the same name already exists, Error::abortRun is called.
+    void addStream(const char* name,StringList* slist);
+    
+    NestedSymbolList targetNestedSymbol;
 
-	// myCode contains the code generated for the target
-	StringList myCode;
+    // myCode contains the code generated for the target
+    StringList myCode;
 
-	StringState destDirectory;
-	IntState loopScheduler;
+    StringState destDirectory;
+    IntState loopScheduler;
 
-	char *schedFileName;
+    char *schedFileName;
 
-	// scheduling is not needed since the schedule is 
-	// copied by copySchedule method. (Multiprocessor case)
-	int noSchedule;
+    // scheduling is not needed since the schedule is 
+    // copied by copySchedule method. (Multiprocessor case)
+    int noSchedule;
 
-	// The following utilities are used by many types of code generators
-	// Return a list of spaces for indenting
-	StringList indent(int depth);
+    // The following utilities are used by many types of code generators
+    // Return a list of spaces for indenting
+    StringList indent(int depth);
 
-	// The following virtual functions do the stages of CGTarget::setup.
-	// generally, derived classes should overload these functions rather
-	// than overloading setup.
+    // The following virtual functions do the stages of CGTarget::setup.
+    // generally, derived classes should overload these functions rather
+    // than overloading setup.
 
-	// perform any extra initializations of the stars in the galaxy.
-	// This version assumes the stars are CGStars; if not true, it
-	// must be overridden.
-	virtual void initStars(Galaxy&);
+    // perform necessary modifications to the galaxy (e.g. splice in
+    // additional stars).  This happens before initialization.
+    // returns FALSE on error.  This class has a do-nothing version.
+    // if defined, it is responsible for calling galaxy.initialize()
+    // if needed to do the start functions.
+    virtual int modifyGalaxy(Galaxy&);
 
-	// perform necessary modifications to the galaxy (e.g. splice in
-	// additional stars).  This happens before initialization.
-	// returns FALSE on error.  This class has a do-nothing version.
-	// if defined, it is responsible for calling galaxy.initialize()
-	// if needed to do the start functions.
-	virtual int modifyGalaxy(Galaxy&);
+    // compute buffer sizes, allocate memory, etc.  return TRUE for
+    // success, false for failure.  Called after schedule generation
+    // and modifyGalaxy.
+    virtual int allocateMemory(Galaxy&);
 
-	// compute buffer sizes, allocate memory, etc.  return TRUE for
-	// success, false for failure.  Called after schedule generation
-	// and modifyGalaxy.
-	virtual int allocateMemory(Galaxy&);
+    // do initialization for code generation: for example, compute
+    // offsets of portholes and generate initCode. But in this base
+    // class, do nothing
+    virtual int codeGenInit(Galaxy&);
 
-	// do initialization for code generation: for example, compute
-	// offsets of portholes and generate initCode. But in this base
-	// class, do nothing
-	virtual int codeGenInit(Galaxy&);
+    // writes initial code
+    virtual void headerCode();
 
-	// writes initial code
-	virtual void headerCode();
+    // method for supervising all code generation for the case of
+    // a wormhole.  The default implementation generates an infinite
+    // loop that reads input wormholes, runs the schedule, and writes
+    // output wormholes, forever.
+    virtual int wormCodeGenerate(Galaxy&);
 
-	// method for supervising all code generation for the case of
-	// a wormhole.  The default implementation generates an infinite
-	// loop that reads input wormholes, runs the schedule, and writes
-	// output wormholes, forever.
-	virtual int wormCodeGenerate(Galaxy&);
+    // methods for generating code for reading and writing
+    // wormhole ports.  Argument is the "real port" of the interior
+    // star that is attached to an event horizon.
+    virtual void wormInputCode(PortHole&);
+    virtual void wormOutputCode(PortHole&);
 
-	// methods for generating code for reading and writing
-	// wormhole ports.  Argument is the "real port" of the interior
-	// star that is attached to an event horizon.
-	virtual void wormInputCode(PortHole&);
-	virtual void wormOutputCode(PortHole&);
+    // The following method downloads code for the inside of a wormhole
+    // and starts it executing.
+    virtual int wormLoadCode();
 
-	// The following method downloads code for the inside of a wormhole
-	// and starts it executing.
-	virtual int wormLoadCode();
+    // methods for sending and receiving data to a target when
+    // run inside of a wormhole.  Arguments are same as above.
+    virtual int sendWormData(PortHole&);
+    virtual int receiveWormData(PortHole&);
 
-	// methods for sending and receiving data to a target when
-	// run inside of a wormhole.  Arguments are same as above.
-	virtual int sendWormData(PortHole&);
-	virtual int receiveWormData(PortHole&);
-
-	// Besides the code the scheduler generated, we usually need to
-	// add more codes (such as global structure definitions). Then,
-	// this virtual method provides the hook for that.
-	// In this base class, do nothing.
-	virtual void frameCode();
+    // Besides the code the scheduler generated, we usually need to
+    // add more codes (such as global structure definitions). Then,
+    // this virtual method provides the hook for that.
+    // In this base class, do nothing.
+    virtual void frameCode();
+    
+private:
+    // codeStringLists is a list of all the code streams that a star 
+    // has access to.  These StringLists should be added from the 
+    // derived target's constructor using the protected method add.
+    // The StringLists should be accessed from the star using the 
+    // public member getStream
+    StringListList codeStringLists;
+    
 
 };
 
