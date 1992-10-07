20085b3a17d6dce5dc96b310b1db4b27480465f5
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-10-07 19:34:20 +0000

    move fork setup code into CGStar

--- trunk/ptclassic/src/domains/cg/stars/AnyAsmFork.pl
+++ trunk/ptclassic/src/domains/cg/stars/AnyAsmFork.pl
@@ -16,43 +16,10 @@ Fork star for assembly-code domains.  No code is generated at runtime.
 		name {output}
 		type {=input}
 	}
-	constructor {
-		output.setForkBuf(input);
-		isaFork();
-	}
 	exectime {
 		return 0;
 	}
-	// fork is never deferrable -- force those tokens through
-	method {
-		name { deferrable }
-		type { int }
-		code { return FALSE;}
-	}
 	start {
-		int n = input.far()->numXfer();
-		input.setSDFParams(n,n-1);
-		output.setSDFParams(n,n-1);
-		int srcDelay = input.numTokens();
-		if (srcDelay > 0) {
-			// move delays from input to outputs
-			PortHole* f = input.far();
-			input.disconnect();
-			input.connect(*f,0);
-			MPHIter nextp(output);
-			PortHole* p;
-			while ((p = nextp++) != 0) {
-				int n = p->numTokens();
-				f = p->far();
-				p->disconnect();
-				p->connect(*f,n+srcDelay);
-			}
-			// re-initialize all neighbors.
-			input.far()->parent()->initialize();
-			nextp.reset();
-			while ((p = nextp++) != 0) {
-				p->far()->parent()->initialize();
-			}
-		}
+		forkInit(input,output);
 	}
 }
