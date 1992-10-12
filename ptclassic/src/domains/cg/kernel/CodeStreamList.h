2ef2e70362333d47bd7dbf7eb9a0a045a4621b5a
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-10-12 16:50:29 +0000

    updated to reflect changes in NamedList

--- ptclassic/src/domains/cg/kernel/CodeStreamList.h
+++ ptclassic/src/domains/cg/kernel/CodeStreamList.h
@@ -13,27 +13,40 @@ Date: 10/2/92
 #ifndef _CodeStreamList_h
 #define _CodeStreamList_h 1
 
-#include "NameList.h"
+#include "NamedList.h"
 #include "StringList.h"
 
-class StringListList : private NameList {
+// For temporary backward compatibility.
+typedef StringList CodeStream;
+
+class StringListList : private NamedList
+{
 public:
+	// For temporary backward compatibility.
+	int add(const char* name, CodeStream* stream)
+	{
+	    return append(stream, name);
+	}
+
 	// adds a new StringList called name to the list.  If a StringList
 	// called name already exists in the list with a different
 	// StringList* return FALSE else return TRUE
-	int add(const char* name, StringList* slist) {
-        	return NameList::add(name,slist);
+	int append(CodeStream* stream, const char* name)
+	{
+        	return NamedList::append(stream, name);
 	}
 
-	// return the pointer for the object of the specified name.  if the
-	// StringList does not exist, return NULL
-	StringList* get(const char* name) {
-        	return (StringList*)NameList::get(name);
+	// return the pointer for the object of the specified name.
+	CodeStream* get(const char* name) const
+	{
+        	return (CodeStream*)NamedList::get(name);
 	}
 	
-	// remove a StringList from the list.  if the StringList
-	// does not exist, return FALSE.
-	int NameList::remove(const char* name);
+	// Remove a CodeStream from the list. "name" is not optional.
+	int remove(const char* name)
+	{
+	    return NamedList::remove(name);
+	}
 };
 
 #endif
