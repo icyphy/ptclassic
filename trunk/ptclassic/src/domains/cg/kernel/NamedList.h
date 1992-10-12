e8146bd8c28799545f11da15254dd64b091b25be
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-10-12 16:45:21 +0000

    changed class name from NameList to NamedList; reimplemented most functions; added NamedListIter

--- trunk/ptclassic/src/domains/cg/kernel/NamedList.h
+++ trunk/ptclassic/src/domains/cg/kernel/NamedList.h
@@ -5,15 +5,15 @@ $Id$
  Copyright (c) 1992 The Regents of the University of California.
                        All Rights Reserved.
 
-Programmer: J. Pino
+Programmer: J. Pino, T. M. Parks
 Date: 10/1/92
-NameList is container list class.  Each node on the list, has
-an associated unique name.  Four functions are supported:
-add, get, remove and initialize.
+
+NamedList is container list class.
+Each node on the list has an associated unique name.
 *******************************************************************/
 
-#ifndef _NameList_h
-#define _NameList_h 1
+#ifndef _NamedList_h
+#define _NamedList_h 1
 
 #ifdef __GNUG__
 #pragma interface
@@ -22,41 +22,62 @@ add, get, remove and initialize.
 #include "DataStruct.h"
 #include "type.h"
 
-class NameNode
-{
-	friend class NameList;
+class NamedNode;
 
-    const char* name;
-    Pointer object;
-    
-    NameNode(const char* nm, Pointer a);
-    ~NameNode();
+class NamedList : private SequentialList
+{
+    friend class NamedListIter;
+public:
+    NamedList() {}
+
+    ~NamedList()
+    {
+	deleteNodes();
+    }
+
+    void initialize()
+    {
+	deleteNodes();
+	SequentialList::initialize();
+    }
+
+    // Put an object called "name" at the end of the list.
+    // If a different object with that name is already
+    // in the list, return FALSE.
+    int append(Pointer object, const char* name);
+
+    // Put an object called "name" at the beginning of the list.
+    int prepend(Pointer object, const char* name);
+
+    // Get the object called "name" from the list.
+    // With no name, get the first object in the list.
+    // If the named object is not in the list, return NULL.
+    Pointer get(const char* name = NULL) const;
+
+    // Remove the object called "name" from the list.
+    // With no name, remove the first object in the list.
+    // If the named object is not in the list, return FALSE.
+    int remove(const char* name = NULL);
+
+private:
+    // Delete all the nodes in the list.
+    void deleteNodes();
+
+    // Get the NamedNode for the object called "name".
+    // If no name is specified, get the first node in the list.
+    // If the named object is not in the list, return NULL.
+    NamedNode* getNamedNode(const char* name = NULL) const;
 };
 
-class NameList : private SequentialList
+class NamedListIter : private ListIter
 {
 public:
-    // Empty the list.
-    void initialize();
+    NamedListIter(NamedList& list) : ListIter(list) {}
 
-    // Destructor
-    ~NameList();
+    Pointer next();
+    Pointer operator ++() { return next(); }
 
-    // add a new object called name.  If there exists another object
-    // on the list has the same name but points to a different object,
-    // return FALSE else return TRUE.
-    int add(const char* name, Pointer object);
-
-    // return the pointer for the object of the specified name.  if the
-    // named pointer does not exist, return null
-    Pointer get(const char* name);
-
-    // remove a NameNode from the list.  if the named
-    // pointer does not exist, return FALSE.
-    int remove(const char* name);
-private:
-    // return the pointer to the NameNode named 'name'
-    NameNode* getNameNode(const char* name);
+    ListIter::reset;
 };
 
 #endif
