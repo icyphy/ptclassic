d542c815a269a02a0a891309a6bb2c830a6f1523
kennard <kennard@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-10-28 10:54:17 +0000

    Initial revision

---
+++ trunk/ptclassic/src/octtools/octls/octls.c
@@ -0,0 +1,112 @@
+/*
+ * octls.c
+ * Original version is by Phil Lapsley for Gabriel.
+ * Tweaked by kennard
+ *
+ * $Id$
+ */
+
+#include "port.h"
+#include "utility.h"
+#include "options.h"
+#include "oct.h"
+#include "oh.h"
+#include <sys/file.h>
+
+optionStruct optionList[] = {
+    {"c",       0,              "check masters"},
+    {0, 0, 0}
+};
+
+int	checkmasters = 0;
+
+main(argc, argv)
+int argc;
+char **argv;
+{
+    char *input_name;
+    octObject facet, instance;
+    octGenerator gen;
+    int count = 0;
+    extern int optind;
+    extern char *optarg;
+
+    if (options(argc, argv) == -1) {
+	exit(1);
+    }
+
+    if (argc - optind != 1) {
+	optUsage();
+    }
+    input_name = argv[optind];
+
+    octBegin();
+    
+    /*
+     *  Get the input and output facet names
+     */
+    ohUnpackDefaults(&facet, "r", "::contents");
+    OH_ASSERT(ohUnpackFacetName(&facet, input_name));
+
+    /* 
+     *  Open the facet 
+     */
+    if (octOpenFacet(&facet) < OCT_OK) {
+	octError("can not open the facet");
+	exit(-1);
+    }
+
+    OH_ASSERT(octInitGenContents(&facet, OCT_INSTANCE_MASK, &gen));
+
+    /* Avoid infinite loops. */
+    count = ohCountContents( &facet, OCT_INSTANCE_MASK );
+    while (octGenerate(&gen, &instance) == OCT_OK) {
+	if ( count-- == 0 ) break;
+	if (checkmasters) {
+		if (bad_master(instance.contents.instance.master))
+			printf("%s %s\n", input_name,
+				instance.contents.instance.master);
+	} else
+		printf("%s %s\n",
+			input_name, instance.contents.instance.master);
+    }
+    octFreeGenerator(&gen);
+    OH_ASSERT(octCloseFacet(&facet));
+    
+    exit(0);
+}
+
+/*
+ * Return 1 if the master is bad (doesn't exist), 0 otherwise.
+ */
+int
+bad_master(master)
+	char	*master;
+{
+	int	ret;
+	char	*p;
+
+	p = util_tilde_expand(master);
+	ret = access(p, F_OK);
+
+	return ((ret == -1) ? 1 : 0);
+}
+
+
+int
+options(argc, argv)
+	int	argc;
+	char	*argv[];
+{
+	int	option;
+
+	while ((option = optGetOption(argc, argv)) != EOF) {
+		switch(option) {
+		case 'c':
+			checkmasters = 1;
+			break;
+		}
+	}
+
+	return (0);
+}
