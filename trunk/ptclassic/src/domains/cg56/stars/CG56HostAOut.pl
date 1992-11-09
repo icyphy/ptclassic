1ba4f17357732d823eecf087abe909a445600a36
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-09 13:33:56 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg56/stars/CG56HostAOut.pl
@@ -0,0 +1,43 @@
+defstar {
+    name { HostAOut }
+    domain { CG56 }
+    desc { Asyncronous Host Output. }
+    version { $Id$ }
+    author { Kennard White }
+    copyright { 1992 The Regents of the University of California }
+    location { CG56 library }
+    explanation {
+blah
+    }
+    input {
+	name { input }
+	type { FIX }
+    }
+    state {
+    	name { label }
+	type { STRING }
+    	desc { Label for graph on host. }
+	default { "Label" }
+    }
+    state {
+	name { blockSize }
+	type { INT }
+	desc { Number of samples per block. }
+	default { 1 }
+    }
+    start {
+        input.setSDFParams(int(blockSize),int(blockSize)-1);
+    }
+    codeblock(cbAio) {
+graph $ref(input,0) $fullname() "$val(label)"
+    }
+    initCode {
+	genMiscCmd(cbAio);
+    }
+    go {
+	; // nothing to do
+    }
+    execTime { 
+        return 1;
+    }
+}
