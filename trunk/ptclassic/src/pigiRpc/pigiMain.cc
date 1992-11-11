08e843481088fcd5d1d079fa135b0c532a4e2eca
kennard <kennard@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-11 21:45:36 +0000

    added pigiFilename handling

--- trunk/ptclassic/src/pigiRpc/pigiMain.cc
+++ trunk/ptclassic/src/pigiRpc/pigiMain.cc
@@ -17,10 +17,12 @@ extern "C" {
 	void CompileInit();
 	void KcLoadInit(const char*);
 	void RPCMain (int argc, char** argv);
+	const char* pigiFilename;
 };
 
 main (int argc, char ** argv)
 {
+	pigiFilename = argv[0];
 	KcLoadInit(argv[0]);
 	KcInitLog("pigiLog.pt");
 	CompileInit();
