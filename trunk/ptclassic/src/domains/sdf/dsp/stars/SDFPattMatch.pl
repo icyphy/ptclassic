f4f85f1f370d25d01b39816c29390a7b8e48cf4a
haskell <haskell@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-12 15:04:31 +0000

    Now output each cross-correlation value.

--- trunk/ptclassic/src/domains/sdf/dsp/stars/SDFPattMatch.pl
+++ trunk/ptclassic/src/domains/sdf/dsp/stars/SDFPattMatch.pl
@@ -9,22 +9,20 @@ defstar {
 This star accepts a template and a search window.
 The template is slid over the window one sample
 at a time and cross correlations are calculated
-at each step. The output is the value of the
-index which gives the largest cross correlation.
-This index refers to a position on the search
-window beginning with 0 corresponding to the
-earliest arrived sample of the search window
+at each step.  The cross-correlations are output
+on the 'values' output.  The 'index' output is the
+value of the time-shift which gives the largest
+cross correlation.  This index refers to a position
+on the search window beginning with 0 corresponding
+to the earliest arrived sample of the search window
 which is part of the "best match" with the template.
-Note that by "uncommenting" the three lines of
-code below which include the term "values", a user
-may recompile this star to include an output
 for viewing the values of the cross correlations.
 	}
 
 	input { name { templ } type { float } }
 	input { name { window } type { float } }
 	output { name { index } type { int } }
-	//output { name { values } type { float } }
+	output { name { values } type { float } }
 
 	defstate {
 		name { tempSize }
@@ -48,7 +46,7 @@ for viewing the values of the cross correlations.
 		N = winSize - tempSize + 1;
 		templ.setSDFParams( tempSize, tempSize - 1 );
 		window.setSDFParams( winSize, winSize - 1 );
-		//values.setSDFParams( N, N - 1 );
+		values.setSDFParams( N, N - 1 );
 	}
 
 	go {
@@ -64,7 +62,7 @@ for viewing the values of the cross correlations.
 						float ( window%( winSize - 1 - ( n + m ) ) );
 			}
 			Cofn = top / bottom;
-			//values%( N - 1 - n ) << Cofn;
+			values%( N - 1 - n ) << Cofn;
 			if ( Cofn > Cmax ) {
 				Cmax = Cofn;
 				nmax = n;
