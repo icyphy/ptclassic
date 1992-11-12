4708d1dfc6b6b874da41b0db307291b0536daee5
haskell <haskell@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-12 14:56:51 +0000

    Renamed these files from "BitArray.*".

--- trunk/ptclassic/src/domains/de/kernel/SeqATMCell.h
+++ trunk/ptclassic/src/domains/de/kernel/SeqATMCell.h
@@ -1,38 +1,38 @@
-// filename: BitArray.h
+// filename: SeqATMCell.h
 // SCCS info: $Id$
 //
-// Default values set for BitArray to contain 53 bytes with
+// Default values set for SeqATMCell to contain 53 bytes with
 // 8 bits/byte or 424 bits. The variable "cell" will be a pointer
 // to an array of (unsigned short)-types. The array will be large
-// enough to contain designated number of bits. The BitArray idea,
+// enough to contain designated number of bits. The SeqATMCell idea,
 // as well as much of the following code, was taken from
 // C++ PRIMER by Stanley B. Lippman, Addison Weslsey, 2nd ed., 1991.
 
-#ifndef BitArray_h
-#define BitArray_h
+#ifndef SeqATMCell_h
+#define SeqATMCell_h
 #include "Message.h"
 
 #ifdef __GNUG__
 #pragma interface
 #endif
 
-class BitArray : public Message {
+class SeqATMCell : public Message {
 public:
-	BitArray( const int sequenceNo = 0 );
-	BitArray( const BitArray& ); // copy constructor
-	~BitArray() { LOG_DEL; delete [] cell; }
+	SeqATMCell( const int sequenceNo = 0 );
+	SeqATMCell( const SeqATMCell& ); // copy constructor
+	~SeqATMCell() { LOG_DEL; delete [] cell; }
 
-	BitArray& set( const int );
-	BitArray& unset( const int );
+	SeqATMCell& set( const int );
+	SeqATMCell& unset( const int );
 	int isON( const int ) const;
 	int isOFF( const int ) const;
 	void setSeq( int num ) { seqNo = num; }
 	int readSeq() const { return( seqNo ); }
 
-	const char* dataType() const { return "BitArray"; }
-	ISA_INLINE( BitArray, Message ); // is_A() function;
+	const char* dataType() const { return "SeqATMCell"; }
+	ISA_INLINE( SeqATMCell, Message ); // is_A() function;
 	Message* clone() const {
-		INC_LOG_NEW; return new BitArray( *this );
+		INC_LOG_NEW; return new SeqATMCell( *this );
 	}
 	double asFloat() const { return( double( seqNo ) ); }
 	StringList print() const;
@@ -42,8 +42,7 @@ private:
 	int seqNo;
 	int getIndex( const int ) const; // returns word containing int
 	int getOffSet( const int ) const; // returns pos of bit in word
-	const int wordSize; // number of bits in unsigned short
-	const int size; // number of bits desired in BitArray
+	const int size; // number of bits desired in SeqATMCell
 };
 
 #endif
