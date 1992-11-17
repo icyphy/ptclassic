7ad441c868830284b313c053155f6994eb858d66
kennard <kennard@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-17 21:01:38 +0000

    support scaling and locking modes

--- trunk/ptclassic/src/domains/cg56/stars/CG56HostAOut.pl
+++ trunk/ptclassic/src/domains/cg56/stars/CG56HostAOut.pl
@@ -26,6 +26,25 @@ blah
 	default { 1 }
     }
     state {
+	name { mode }
+	type { STRING }
+	desc { Communication mode: one-of DIRECT or HOSTLOCK. }
+	default { "DIRECT" }
+    }
+    state {
+	name { bufLock }
+	type { INT }
+	desc { Semaphore for buffer locking. }
+	default { 0 }
+	attributes { A_NONCONSTANT|A_NONSETTABLE|A_XMEM }
+    }
+    state {
+	name { buffer }
+	type { FIXARRAY }
+	desc { Buffer which host reads sample from. }
+	attributes { A_NONCONSTANT|A_NONSETTABLE }
+    }
+    state {
 	name { yMin }
 	type { FLOAT }
 	desc { "Host mapping of -1 for sample(s)." }
@@ -49,17 +68,50 @@ blah
 	desc { "Host x-axis mapping of (last+1) sample in block." }
 	default { "1.0" }
     }
+    protected {
+	int	doCopyB;
+    }
     start {
         input.setSDFParams(int(blockSize),int(blockSize)-1);
+	doCopyB = FALSE;
+	const char *m = mode;
+	/*IF*/ if ( strcmp(m,"DIRECT")==0 ) {
+	    buffer.resize(0);
+	    buffer.clearAttributes(A_YMEM);
+	} else if ( strcmp(m,"HOSTLOCK")==0 ) {
+	    buffer.resize(int(blockSize));
+	    buffer.setAttributes(A_YMEM);
+	    doCopyB = TRUE;
+	} else {
+	    Error::abortRun(*this,"Invalid mode");
+	    return;
+	}
     }
-    codeblock(cbAio) {
-aio_graph $ref(input,0) $fullname() {$val(label)} $val(blockSize) $val(yMin) $val(yMax) $val(xMin) $val(xMax)
+    codeblock(cbAioDirect) {
+aio_graph $ref(input,0) $fullname() {$val(label)} $val(blockSize) $val(mode) - $val(yMin) $val(yMax) $val(xMin) $val(xMax)
+    }
+    codeblock(cbAioHostLock) {
+aio_graph $ref(buffer,0) $fullname() {$val(label)} $val(blockSize) $val(mode) $ref(bufLock) $val(yMin) $val(yMax) $val(xMin) $val(xMax)
+    }
+    codeblock(cbCopy) {
+	move	#$addr(bufLock),r2
+	move	#$addr(input),r0	; copy entire {input} into {buffer}
+	move	#$addr(buffer),r1
+	jset	#0,x:(r2),$label(skip)
+	.LOOP	#$val(blockSize)
+	  move	x:(r0)+,x0
+	  move	x0,y:(r1)+
+	.ENDL
+	bset	#0,x:(r2)
+$label(skip)		
     }
     initCode {
-	addCode(cbAio,"aioCmds");
+	addCode( doCopyB ? cbAioHostLock : cbAioDirect, "aioCmds");
     }
     go {
-	; // nothing to do
+	if ( doCopyB ) {
+	    addCode(cbCopy);
+	}
     }
     execTime { 
         return 1;
