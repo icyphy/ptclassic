dd706ea6e5bf0f9eea582e4bf75c1f5accd442bc
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-19 14:38:53 +0000

    Initial revision

---
+++ ptclassic/src/domains/cgc/stars/CGCXYgraph.pl
@@ -0,0 +1,35 @@
+defstar {
+	name { XYgraph }
+	derivedFrom { Xgraph }
+	domain { CGC }
+	desc {
+Generates an X-Y plot with the xgraph program.
+The X data is on "xInput" and the Y data is on "input".
+	}
+	version {$Id$}
+	author { S. Ha }
+	copyright { 1992 The Regents of the University of California }
+	location { CGC main library }
+	explanation {
+The input signal is plotted using the \fIxgraph\fR program, with one
+input interpreted as the x-axis data, and the other input as y-axis data.
+.Id "graph, X-Y"
+.Ir "xgraph program"
+	}
+	seealso {Xgraph XMgraph xgraph Xhistogram}
+	input {
+		name { xInput }
+		type { anytype }
+	}
+	constructor {
+		// make xInit and xUnits invisible
+		xUnits.setAttributes(A_NONSETTABLE);
+		xInit.setAttributes(A_NONSETTABLE);
+	}
+	go {
+		count++;
+		if (count <= int(ignore)) return;
+                addCode(
+"\tfprintf($starSymbol(fp),\"%g %g\\n\",$ref(xInput),$ref(input));\n");
+	}
+}
---
+++ ptclassic/src/domains/cgc/stars/CGCXscope.pl
@@ -0,0 +1,58 @@
+defstar {
+	name { Xscope }
+	domain { CGC }
+	desc { Generate a multi-trace plot with the xgraph program. }
+	derivedFrom { Xgraph }
+	version {$Id$}
+	author { S. Ha }
+	copyright { 1992 The Regents of the University of California }
+	location { CGC main library }
+	explanation {
+This star is an enhanced version of Xgraph.  It is identical
+except that it can plot multiple traces, like an oscilloscope.
+As for Xgraph,
+the \fItitle\fR parameter specifies a title for the plot.
+The \fIsaveFile\fR parameter optionally specifies a file for
+storing the data in a syntax acceptable to xgraph.
+A null string prevents any such storage.
+The \fIoptions\fR string is passed directly to the xgraph program
+as command-line options.  See the manual section describing xgraph
+for a complete explanation of the options.
+.pp
+Multiple traces may be plotted by setting the \fItraceLength\fR
+state to a nonzero value.  In this case, a new plot (starting at
+x value zero) is started every \fItraceLength\fR samples.  The
+first \fIignore\fR samples are not plotted; this is useful for letting
+transients die away.
+.Ir "xgraph program"
+.Id "oscilloscope, X window"
+.Id "graph, X window, multi-trace"
+	}
+	defstate {
+		name {traceLength}
+		type {int}
+		default {"0"}
+		desc { Number of samples per trace.  If 0, only one trace. }
+	}
+	protected {
+		int traceCount;
+		int nTracesSoFar;
+	}
+
+	setup {
+		CGCXgraph::setup();
+		traceCount = 0;
+		nTracesSoFar = 0;
+	}
+
+	go {
+		if (int(traceLength) > 0 && traceCount >= int(traceLength)) {
+			traceCount = 0;
+			addCode("\tfprintf($starSymbol(fp), \"move \");\n");
+			index = xInit;	// reinitialize x index
+			nTracesSoFar++;
+		}
+		traceCount++;
+		CGCXgraph::go();
+	}
+}
