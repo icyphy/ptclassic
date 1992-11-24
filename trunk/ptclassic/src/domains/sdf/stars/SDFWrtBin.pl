bc54771aba45845e0411610c18b0e42ed864ec5f
aok <aok@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-24 18:35:12 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/stars/SDFWrtBin.pl
@@ -0,0 +1,118 @@
+defstar
+{
+    name { WrtBin }
+    domain { SDF }
+    version { $Id$ }
+    author { Alan W. Peevers }
+    copyright { 1992 The Regents of the University of California }
+
+
+
+    location { SDF main library }
+    descriptor { Write floating point data to a binary file. The data type \
+written is 'float'.
+    		}
+
+    explanation{ The byte ordering of floating point data values may vary 
+from machine to machine, so an option is provided to allow byte
+swapping of the data. }
+
+    ccinclude { "Scheduler.h" }
+
+    input
+    {
+	name { input }
+	type { float }
+    }
+
+    state
+    {
+	name { fileName }
+	type { string }
+	default { "/dev/null" }
+	descriptor { Output File }
+    }
+
+    state
+    {
+	name { byteswap }
+	type { string }
+	default { "NO" }
+	descriptor { If YES, do a byte swap before outputting }
+    }
+
+    protected
+    {
+	int fd;	 
+    }
+
+    code
+    {
+	union floatChar
+	{
+		float f;
+		unsigned char c[sizeof(float)];
+	};
+
+	// return a byte-swapped version of arg.
+	float swap(float arg)
+	{
+		floatChar a;
+		a.f = arg;
+		unsigned char t = a.c[0];
+		a.c[0] = a.c[3];
+		a.c[3] = t;
+		t = a.c[1];
+		a.c[1] = a.c[2];
+		a.c[2] = t;
+		return a.f;
+	}
+    }
+
+    start
+    {
+
+	// open input file
+	fd = open(expandPathName(fileName), 0x202, 0x1A4); // Yeah, I know...
+	if (fd < 0) {
+		Error::abortRun(*this, "can't open file ", fileName);
+		return;
+	}
+    }
+
+    go
+    {
+	float x = 0;
+	int r;
+	
+	x = (input%0);
+
+ 	if (!strcmp(byteswap,"YES"))
+		x=swap(x);
+	
+   	r = write(fd, &x, sizeof(x)); // Inefficient, but it works...
+	if (r != sizeof(x))
+	{
+	    fprintf(stderr, "Error on write!!!\n");
+            Scheduler::requestHalt();
+
+	}        
+
+      }
+
+    wrapup
+    {
+        close(fd);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ trunk/ptclassic/src/domains/sdf/stars/SDFWrtBinInt.pl
@@ -0,0 +1,115 @@
+defstar
+{
+    name { WrtBinInt }
+    domain { SDF }
+    version { $Id$ }
+    author { Alan W. Peevers }
+    copyright { 1992 The Regents of the University of California }
+
+
+
+    location { SDF main library }
+    descriptor { Write integers to a 16-bit binary file. The data type written \
+is 'short'. 
+               }
+
+
+        ccinclude { "Scheduler.h" }
+
+    input
+    {
+	name { input }
+	type { int }
+    }
+
+    state
+    {
+	name { fileName }
+	type { string }
+	default { "/dev/null" }
+	descriptor { Output file }
+    }
+
+    state
+    {
+	name { endian }
+	type { string }
+	default { "n" }
+	descriptor { Endian-ness of data: b=big-endian, l=little-endian, n=native }
+    }
+
+    protected
+    {
+	int fd;	 
+	int bigEndian;
+    }
+
+    code
+    {
+	union shortChar
+	{
+		short s;
+		unsigned char c[sizeof(short)];
+	};
+
+	// This routine determines whether the machine is big-endian.
+	// Returns TRUE (1) if it is.
+	int isBigEndian()
+	{
+		shortChar a;
+		a.s = 1;
+		return a.c[1];
+	}
+
+	// return a byte-swapped version of arg.
+	short swap(short arg)
+	{
+		shortChar a;
+		a.s = arg;
+		unsigned char t = a.c[0];
+		a.c[0] = a.c[1];
+		a.c[1] = t;
+		return a.s;
+	}
+    }
+
+    start
+    {
+
+	// open input file
+	fd = open(expandPathName(fileName), 0x201, 0x1E4); 
+	if (fd < 0) {
+		Error::abortRun(*this, "can't open file ", fileName);
+		return;
+	}
+ 	bigEndian = isBigEndian();
+   }
+
+    go
+    {
+	float x = 0;
+	short y = 0;
+	int r;
+	
+	x = (input%0);
+        y = (short) x;
+	
+	if (((!bigEndian&&strcmp(endian,"b")) ||
+	      ( bigEndian&&!strcmp(endian,"l")) &&
+		!(strcmp(endian,"n")==0))) y=swap(y);
+	
+   	r = write(fd, &y, sizeof(y));
+	if (r != sizeof(y))
+	{
+	    fprintf(stderr, "Error on write!!!\n");
+            Scheduler::requestHalt();
+
+	}        
+
+    }
+
+    wrapup
+    {
+        close(fd);
+    }
+}
