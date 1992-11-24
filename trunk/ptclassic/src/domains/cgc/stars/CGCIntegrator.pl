bd0da76b89fc6cfa3b5b5d0fc1ff1514afa9c371
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-24 12:42:09 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cgc/stars/CGCIntegrator.pl
@@ -0,0 +1,124 @@
+defstar {
+	name { Integrator }
+	domain { CGC }
+	desc {
+
+An integrator with leakage, limits, and reset.
+With the default parameters, input samples are simply accumulated,
+and the running sum is the output.  To prevent any resetting in the
+middle of a run, connect a d.c. source with value 0.0 to the "reset"
+input.  Otherwise, whenever a non-zero is received on this input,
+the accumulated sum is reset to the current input (i.e. no feedback).
+
+Limits are controlled by the "top" and "bottom" parameters.
+If top <= bottom, no limiting is performed (default).  Otherwise,
+the output is kept between "bottom" and "top".  If "saturate" = YES,
+saturation is performed.  If "saturate" = NO, wrap-around is performed
+(default).  Limiting is performed before output.
+
+Leakage is controlled by the "feedbackGain" state (default 1.0).
+The output is the data input plus feedbackGain*state, where state
+is the previous output.
+	}
+	version { $Id$ }
+	author { E. A. Lee }
+	copyright { 1992 The Regents of the University of California }
+	location { CGC main library }
+	explanation {
+.Id "filter, integrator"
+	}
+	input {
+		name { data }
+		type { float }
+	}
+	input {
+		name { reset }
+		type { int }
+	}
+	output {
+		name { output }
+		type { float }
+	}
+	defstate {
+		name {feedbackGain}
+		type {float}
+		default {"1.0"}
+		desc { The gain on the feedback path.}
+	}
+	defstate {
+		name {top}
+		type {float}
+		default {"0.0"}
+		desc { The upper limit.}
+	}
+	defstate {
+		name {bottom}
+		type {float}
+		default {"0.0"}
+		desc { The lower limit.}
+	}
+	defstate {
+		name {saturate}
+		type {int}
+		default {"YES"}
+		desc { Saturate if YES, wrap around otherwise.}
+	}
+	defstate {
+		name {state}
+		type {float}
+		default {"0.0"}
+		desc { An internal state.}
+		attributes { A_NONCONSTANT|A_SETTABLE }
+	}
+	protected {
+		double spread;
+	}
+	start {
+		spread = double(top) - double(bottom);
+	}
+	codeblock (declarations) {
+	    double $starSymbol(t);
+	}
+	initCode {
+	    addDeclaration(declarations);
+	}
+	codeblock (integrate) {
+	    if ($ref(reset) != 0) {
+		$starSymbol(t) = $ref(data);
+	    } else {
+		$starSymbol(t) = $ref(data) +
+			$val(feedbackGain) * $ref(state);
+	    }
+	}
+	codeblock (limitWithSat) {
+	    /* Limiting is in effect */
+	    /* Take care of the top */
+	    if ($starSymbol(t) > $val(top)) $starSymbol(t) = $val(top);
+	    /* Take care of the bottom */
+	    if ($starSymbol(t) < $val(bottom)) $starSymbol(t) = $val(bottom);
+	}
+	codeblock (limitWithoutSat) {
+	    /* Limiting is in effect */
+	    /* Take care of the top */
+	    if ($starSymbol(t) > $val(top))
+		do $starSymbol(t) -= ($val(top) - $val(bottom));
+		while ($starSymbol(t) > $val(top));
+	    /* Take care of the bottom */
+	    if ($starSymbol(t) < $val(bottom))
+		do $starSymbol(t) += ($val(top) - $val(bottom));
+		while ($starSymbol(t) < $val(bottom));
+	}
+	codeblock (write) {
+	    $ref(output) = $starSymbol(t);
+	    $ref(state) = $starSymbol(t);
+	}
+	go {
+	    addCode(integrate);
+	    if (spread > 0.0) 
+		if (int(saturate))
+		    addCode(limitWithSat);
+		else
+		    addCode(limitWithoutSat);
+	    addCode(write);
+	}
+}
---
+++ trunk/ptclassic/src/domains/cgc/stars/CGCQuant.pl
@@ -0,0 +1,87 @@
+defstar {
+	name { Quant }
+	domain { CGC }
+	desc {
+Quantizes input to one of N+1 possible output levels using N thresholds.
+For an input less than or equal to the n-th threshold, but larger than all
+previous thresholds, the output will be the n-th level.  If the input is
+greater than all thresholds, the output is the N+1-th level.  If level is
+specified, there must be one more level than thresholds; the default
+value for level is 0, 1, 2, ... N.
+	}
+	version { $Id$ }
+	author { E. A. Lee and J. Buck }
+	copyright { 1992 The Regents of the University of California }
+	location { CGC main library }
+	input {
+		name {input}
+		type {float}
+	}
+	output {
+		name {output}
+		type {float}
+	}
+	defstate {
+		name { thresholds }
+		type { floatarray }
+		default {0.0}
+		desc { Quantization thresholds, in increasing order }
+	}
+	defstate {
+		name { levels }
+		type { floatarray }
+		default {""}
+		desc { Output levels.  If empty, use 0, 1, 2, ...}
+	}
+	start {
+		int n = thresholds.size();
+		if (levels.size() == 0) {
+			// default: 0, 1, 2...
+
+			levels.resize(n + 1);
+			for (int i = 0; i <= n; i++)
+				levels[i] = i;
+		}
+		else if (levels.size() != n+1) {
+			Error::abortRun (*this,
+			      "must have one more level than thresholds");
+		}
+	}
+	codeblock (declarations) {
+	    int $starSymbol(siz);
+	}
+	initCode {
+	    addDeclaration(declarations);
+	}
+	codeblock (maindecl) {
+		float in;
+		int lo, hi, mid;
+	}
+	codeblock (main) {
+		in = $ref(input);
+		lo = 0;
+		hi = $starSymbol(siz);
+		mid = (hi+lo)/2;
+		do {
+			if (in <= $ref2(thresholds,mid)) {
+				hi = mid;
+			} else {
+				lo = mid+1;
+			}
+			mid = (hi+lo)/2;
+		} while (mid < $starSymbol(siz) && hi > lo);
+
+		/* now in is <= thresholds[mid] but > all smaller ones. */
+		/* (where thresholds[$starSymbol(siz)] is infinity) */
+
+		$ref(output) = $ref2(levels,mid);
+	}
+	go {
+	    addCode (maindecl);
+	    StringList st = "\t\t$starSymbol(siz) = ";
+	    st << thresholds.size();
+	    st << ";\n";
+	    addCode((const char*)st);
+	    addCode (main);
+	}
+}
