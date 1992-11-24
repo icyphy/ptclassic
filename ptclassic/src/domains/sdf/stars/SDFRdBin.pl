0db7623b8646c901826c26f85e49193263ce9e1f
aok <aok@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-24 18:35:12 +0000

    Initial revision

---
+++ ptclassic/src/domains/sdf/stars/SDFRdBin.pl
@@ -0,0 +1,132 @@
+defstar
+{
+    name { RdBin }
+    domain { SDF }
+    version { $Id$ }
+    author { Alan W. Peevers }
+    copyright { 1992 The Regents of the University of California }
+
+
+
+    location { SDF main library }
+
+    descriptor { Read floating point data from a binary file. The data type \
+read is 'float' }
+
+    explanation{ The byte ordering of floating point data values may vary 
+from machine to machine, so an option is provided to allow byte
+swapping of the data. }
+
+    ccinclude { "Scheduler.h" }
+
+    output
+    {
+	name { output }
+	type { float }
+    }
+
+    state
+    {
+	name { fileName }
+	type { string }
+	default { "/dev/null" }
+	descriptor { Input file }
+    }
+
+    state
+    {
+	name { haltAtEnd }
+	type { int }
+	default { "NO" }
+	descriptor { Halt the run at the end of the input file }
+    }
+
+    state
+    {
+	name { periodic }
+	type { int }
+	default { "NO" }
+	descriptor { Make output periodic or zero-padded }
+    }
+
+    state
+    {
+	name { byteswap }
+	type { string }
+	default { "NO" }
+	descriptor { If YES, do a byte swap before outputting }
+    }
+
+    protected
+    {
+	int fd;	 
+    }
+
+    code
+    {
+	union floatChar
+	{
+		float f;
+		unsigned char c[sizeof(float)];
+	};
+
+	// return a byte-swapped version of arg.
+	float swap(float arg)
+	{
+		floatChar a;
+		a.f = arg;
+		unsigned char t = a.c[0];
+		a.c[0] = a.c[3];
+		a.c[3] = t;
+		t = a.c[1];
+		a.c[1] = a.c[2];
+		a.c[2] = t;
+		return a.f;
+	}
+    }
+
+   start
+    {
+
+	// open input file
+	fd = open(expandPathName(fileName), 0);
+	if (fd < 0) {
+		Error::abortRun(*this, "can't open file ", fileName);
+		return;
+	}
+    }
+
+    go
+    {
+	float x = 0.0;
+        int r;
+
+        r = read(fd, &x, sizeof(x)); // inefficient, but don't ask!
+        if (r != sizeof(x))
+	{
+	    if (haltAtEnd)		// halt the run
+		Scheduler::requestHalt();
+	    else if (periodic)		// close and re-open file
+	    {
+		fd = open(expandPathName(fileName), 0);
+		if (fd < 0) 
+                {
+			Error::abortRun(*this, "can't re-open file ", fileName);
+			return;
+		}
+
+	    }
+
+	}
+
+ 	if (!strcmp(byteswap,"YES"))
+		x=swap(x);
+	
+	output%0 << x;
+    }
+
+    wrapup
+    {
+        close(fd);
+    }
+}
---
+++ ptclassic/src/domains/sdf/stars/SDFRdBinInt.pl
@@ -0,0 +1,147 @@
+defstar
+{
+    name { RdBinInt }
+    domain { SDF }
+    version { $Id$ }
+    author { Alan W. Peevers }
+    copyright { 1992 The Regents of the University of California }
+
+
+
+    location { SDF main library }
+    descriptor { Read integers from a 16-bit binary file. The data type \
+assumed is 'short'.}
+
+    ccinclude { "Scheduler.h" }
+    ccinclude { <iostream.h> }
+    output
+    {
+	name { output }
+	type { int }
+    }
+
+    state
+    {
+	name { fileName }
+	type { string }
+	default { "/dev/null" }
+	descriptor { Input file }
+    }
+
+    state
+    {
+	name { haltAtEnd }
+	type { int }
+	default { "NO" }
+	descriptor { Halt the run at the end of the input file }
+    }
+
+    state
+    {
+	name { periodic }
+	type { int }
+	default { "NO" }
+	descriptor { Make output periodic or zero-padded }
+    }
+
+    state
+    {
+	name { endian }
+	type { string }
+	default { "n" }
+	descriptor { Endian-ness of data: b=big-endian, l=little-endian, n=native }
+    }
+
+    protected
+    {
+	int fd;	 
+	int bigEndian;
+    }
+
+    code
+    {
+	union shortChar
+	{
+		short s;
+		unsigned char c[sizeof(short)];
+	};
+
+	// This routine determines whether the machine is big-endian.
+	// Returns TRUE (1) if it is.
+	int isBigEndian()
+	{
+		shortChar a;
+		a.s = 1;
+		return a.c[1];
+	}
+
+	// return a byte-swapped version of arg.
+	short swap(short arg)
+	{
+		shortChar a;
+		a.s = arg;
+		unsigned char t = a.c[0];
+		a.c[0] = a.c[1];
+		a.c[1] = t;
+		return a.s;
+	}
+    }
+
+    setup
+    {
+	// open input file
+	fd = open(expandPathName(fileName), 0);
+	if (fd < 0)
+	 {
+		Error::abortRun(*this, "can't open file ", fileName);
+		return;
+	}
+	bigEndian = isBigEndian();
+     }
+
+    go
+    {
+	short x = 0;
+	int r;
+
+   	r = read(fd, &x, sizeof(x)); // inefficient, don't ask!
+	if (r != sizeof(x))
+	{
+	    if (haltAtEnd)		// halt the run
+		Scheduler::requestHalt();
+	    else if (periodic)		// close and re-open file
+	    {
+		fd = open(expandPathName(fileName), 0);
+		if (fd < 0) 
+                {
+			Error::abortRun(*this, "can't re-open file ", fileName);
+			return;
+		}
+
+	    }
+
+	}
+
+	if (((!bigEndian&&strcmp(endian,"b")) ||
+	      ( bigEndian&&!strcmp(endian,"l")) &&
+		!(strcmp(endian,"n")==0))) x=swap(x);
+	
+	output%0 <<  x;
+    }
+
+    wrapup
+    {
+        close(fd);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
