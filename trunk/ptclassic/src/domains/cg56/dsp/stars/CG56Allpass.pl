e2180ed16f630913ef133883189b937bc79c17d1
huangc <huangc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-30 21:30:17 +0000

    copyright

--- trunk/ptclassic/src/domains/cg56/dsp/stars/CG56Allpass.pl
+++ trunk/ptclassic/src/domains/cg56/dsp/stars/CG56Allpass.pl
@@ -11,7 +11,7 @@ All rights reserved.
 See the file ~ptolemy/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
 	}
-	location { CG56 demo library }
+	location { CG56 dsp library }
         explanation {
 An allpass filter with one pole and one zero.
 The location of these is given by the polezero input.
