11ede89663e5df34d1ae72c9b596f2b0713ad7f5
haskell <haskell@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-11-30 11:33:39 +0000

    copyright

--- trunk/ptclassic/src/domains/sdf/image/stars/SDFMotionCmpInv.pl
+++ trunk/ptclassic/src/domains/sdf/image/stars/SDFMotionCmpInv.pl
@@ -3,7 +3,12 @@ defstar {
 	domain		{ SDF }
 	version		{ $Id$ }
 	author		{ Paul Haskell }
-	copyright	{ 1992 Regents of the University of California }
+	copyright {
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
 	location	{ SDF image palette }
 	desc {
 For NULL inputs on 'mvIn' (motion-vector-in), copy the 'diffIn' input
