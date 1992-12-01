528f8367398ae2b040a2ca5702097fa75a0b43d8
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1992-12-01 18:01:59 +0000

    add strcasecmp prototype

--- trunk/ptclassic/src/compat/cfront/std.h
+++ trunk/ptclassic/src/compat/cfront/std.h
@@ -2,5 +2,8 @@
 #include <stdlib.h>
 #include <sysent.h>
 #include "../../kernel/miscFuncs.h"
+// we could include <strings.h> too, but Sun Cfront 2.1 has an
+// incorrect prototype for strcasecmp.
+int strcasecmp(const char*, const char*);
 
 
