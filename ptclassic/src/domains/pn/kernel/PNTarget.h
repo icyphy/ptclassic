20bb04e367c33d96ac8caa1a4cbc383b6c194439
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-01-09 17:01:47 +0000

    updated copyright notice; added parameter to select real-time scheduling

--- ptclassic/src/domains/pn/kernel/PNTarget.h
+++ ptclassic/src/domains/pn/kernel/PNTarget.h
@@ -1,8 +1,28 @@
+/* 
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
 /*  Version $Id$
-
-    Copyright 1992 The Regents of the University of California.
-			All Rights Reserved.
-
     Programmer:		T.M. Parks
     Date of creation:	2 April 1992
 */
@@ -15,9 +35,7 @@
 #endif
 
 #include "Target.h"
-#include "MTDFScheduler.h"
-
-class Galaxy;
+#include "IntState.h"
 
 class MTDFTarget : public Target
 {
@@ -36,6 +54,10 @@ public:
 
     // End simulation.
     /* virtual */ void wrapup();
+
+protected:
+    // Select Scheduler type.
+    IntState realTime;
 };
 
 #endif
