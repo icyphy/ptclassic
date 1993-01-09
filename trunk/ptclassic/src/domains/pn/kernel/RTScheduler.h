e0ffa1b04d6e34cd23375318853a1acbe11b7ed1
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-01-09 17:03:47 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/pn/kernel/RTScheduler.h
@@ -0,0 +1,68 @@
+/* 
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
+/*  Version $Id$
+    Programmer:		T.M. Parks
+    Date of creation:	7 January 1993
+*/
+
+#ifndef _RTDFScheduler_h
+#define _RTDFScheduler_h
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include "MTDFScheduler.h"
+#include "Clock.h"
+
+class RTDFScheduler : public MTDFScheduler
+{
+public:
+    // Constructor.
+    RTDFScheduler();
+
+    // Initialization.
+    void setup();
+
+    // Run (or continue) the simulation.
+    int run();
+
+    // Current time.
+    /* virtual */ void setCurrentTime(double);
+    /* should be virtual? */ double now() const;
+
+protected:
+    // Select Thread function for a Star.
+    /* virtual */ void (*thread(MTDFStar*))(MTDFStar*);
+
+    // Thread functions.
+    static void periodicThread(MTDFStar*);
+
+private:
+    Clock clock;
+    TimeVal startTime;
+};
+
+#endif
