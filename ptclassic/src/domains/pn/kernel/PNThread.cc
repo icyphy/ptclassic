62509643423cd518340d9cb1288932dbfa244288
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-01-09 16:49:11 +0000

    updated copyright notice; moved thread functions to MTDFScheduler; added sleep() method and methods to access system-wide min and max priority

--- ptclassic/src/domains/pn/kernel/PNThread.cc
+++ ptclassic/src/domains/pn/kernel/PNThread.cc
@@ -1,8 +1,28 @@
+/* 
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
 /*  Version $Id$
-
-    Copyright 1992 The Regents of the University of California.
-			All Rights Reserved.
-
     Programmer:		T.M. Parks
     Date of creation:	18 February 1992
 */
@@ -14,47 +34,49 @@ static const char file_id[] = "$RCSfile$";
 #endif
 
 #include "MTDFThread.h"
-#include "MTDFStar.h"
-#include "MTDFScheduler.h"
-#include "CriticalSection.h"
+#include "TimeVal.h"
 
 #ifdef __GNUG__
 extern "C"
 {
     int lwp_setpri(thread_t, int);
+    int lwp_sleep(timeval*);
+    int pod_getmaxpri();
+    int pod_setmaxpri(int);
 }
 #endif
 
-// Thread for normal Stars.
-void starThread(MTDFStar* star)
-{
-    // Fire the Star ad infinitum.
-    while(TRUE)	star->run();
-}
-
-// Thread for source Stars.
-void sourceThread(MTDFStar* star)
-{
-    MTDFScheduler* sched = (MTDFScheduler*) star->scheduler();
-
-    // Wait for notification from the Scheduler, then fire the Star.
-    while(TRUE)
-    {
-	{
-	    CriticalSection x(sched->monitor);
-	    sched->start.wait();
-	}
-	star->run();
-    }
-}
-
 // Constructor.
-MTDFThread::MTDFThread(unsigned int priority, MTDFStar* star)
-    : Thread(priority, (star->numInputs() == 0) ? (void(*)(void*))sourceThread : (void(*)(void*))starThread, star)
+MTDFThread::MTDFThread(int priority, void (*thread)(MTDFStar*), MTDFStar* star)
+    : Thread(priority, (void(*)(void*))thread, star)
 {}
 
 // Change the Thread's priority.
-int MTDFThread::setPriority(unsigned int priority)
+int MTDFThread::setPriority(int priority)
 {
     return lwp_setpri(*this, priority);
+}
+
+// Set system-wide maximum priority.
+int MTDFThread::setMaxPriority(int priority)
+{
+    return pod_setmaxpri(priority);
+}
+
+// System-wide maximum priority.
+int MTDFThread::maxPriority()
+{
+    return pod_getmaxpri();
+}
+
+// System-wide minimum priority.
+int MTDFThread::minPriority()
+{
+    return MINPRIO;
+}
+
+// Disable Thread for the specified time.
+int MTDFThread::sleep(TimeVal delay)
+{
+    return lwp_sleep((timeval*)&delay);
 }
