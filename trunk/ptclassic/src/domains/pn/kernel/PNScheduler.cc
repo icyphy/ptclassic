0ceebb3b8d1321590bc1a94d9c61abc16b19a7fd
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-01-09 16:37:54 +0000

    updated copyright notice; moved thread functions from MTDFThread

--- trunk/ptclassic/src/domains/pn/kernel/PNScheduler.cc
+++ trunk/ptclassic/src/domains/pn/kernel/PNScheduler.cc
@@ -1,8 +1,28 @@
+/* 
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
 /*  Version $Id$
-
-    Copyright 1992 The Regents of the University of California.
-			All Rights Reserved.
-
     Programmer:		T.M. Parks
     Date of creation:	7 February 1992
 */
@@ -15,6 +35,7 @@ static const char file_id[] = "$RCSfile$";
 
 #include "MTDFScheduler.h"
 #include "MTDFStar.h"
+#include "MTDFThread.h"
 #include "MTDFThreadIter.h"
 #include "GalIter.h"
 #include "CriticalSection.h"
@@ -22,11 +43,10 @@ static const char file_id[] = "$RCSfile$";
 extern const char MTDFdomainName[];
 
 const int stackSize = 0x4000;
-const int minPriority = MINPRIO;
-const int maxPriority = MINPRIO+1;
 
 // Create static Thread for main().
-MTDFThread& MTDFScheduler::main = (MTDFThread&)Thread::init(maxPriority, stackSize);
+MTDFThread& MTDFScheduler::main
+    = (MTDFThread&)Thread::init(MTDFThread::minPriority()+1, stackSize);
 
 // Constructor.
 MTDFScheduler::MTDFScheduler() : starThreads(), monitor(), start(monitor)
@@ -57,21 +77,22 @@ void MTDFScheduler::setup()
 	return;
     }
 
-    currentTime = 0.0;
     galaxy()->initialize();
 
-    // Delete any threads from the last run before creating new ones.
+    // Delete any left-over threads before creating new ones.
     deleteThreads();
     createThreads();
+
+    currentTime = 0.0;
 }
 
 // Run (or continue) the simulation.
 int MTDFScheduler::run()
 {
-    // Lower the priority so that the Threads can run.
-    main.setPriority(minPriority);
+    // Lower priority to allow Threads to run.
+    main.setPriority(MTDFThread::minPriority());
 
-    while((currentTime < stopTime) && !haltRequested())
+    while((currentTime < stopTime) && !SimControl::haltRequested())
     {
 	// Notify all source Threads to start.
 	{
@@ -81,10 +102,40 @@ int MTDFScheduler::run()
 	currentTime += schedulePeriod;
     }
 
-    // Raise priority again to prevent Threads from running.
-    main.setPriority(maxPriority);
+    // Raise priority to prevent Threads from running.
+    main.setPriority(MTDFThread::maxPriority());
 
-    return !haltRequested();
+    return !SimControl::haltRequested();
+}
+
+// Select thread function for a star.
+void (*MTDFScheduler::thread(MTDFStar* star))(MTDFStar*)
+{
+    if (star->isSource()) return sourceThread;
+    else return starThread;
+}
+
+// Thread for normal Stars.
+void MTDFScheduler::starThread(MTDFStar* star)
+{
+    // Fire the Star ad infinitum.
+    while(TRUE)	star->run();
+}
+
+// Thread for source Stars.
+void MTDFScheduler::sourceThread(MTDFStar* star)
+{
+    MTDFScheduler* sched = (MTDFScheduler*) star->scheduler();
+
+    // Wait for notification from the Scheduler, then fire the Star.
+    while(TRUE)
+    {
+	{
+	    CriticalSection x(sched->monitor);
+	    sched->start.wait();
+	}
+	star->run();
+    }
 }
 
 // Create threads and build ThreadList.
@@ -92,22 +143,24 @@ void MTDFScheduler::createThreads()
 {
     GalStarIter starIter(*galaxy());
     MTDFStar* star;
-    MTDFThread* thread;
+    MTDFThread* t;
+    int p = MTDFThread::maxPriority();
 
     // Create Threads for all the Stars.
     while((star = (MTDFStar*)starIter++) != NULL)
     {
-	LOG_NEW; thread = new MTDFThread(maxPriority, star);
-	starThreads.append(thread);
+	LOG_NEW; t = new MTDFThread(p, thread(star), star);
+	starThreads.append(t);
     }
 }
 
 // Delete Threads and clear the ThreadList.
 void MTDFScheduler::deleteThreads()
 {
-    // Delete all Threads in the list.
     MTDFThreadIter threadIter(starThreads);
     MTDFThread* t;
+
+    // Delete all Threads in the list.
     while((t = threadIter++) != NULL)
     {
 	LOG_DEL; delete t;
--- trunk/ptclassic/src/domains/pn/kernel/PNScheduler.h
+++ trunk/ptclassic/src/domains/pn/kernel/PNScheduler.h
@@ -1,8 +1,28 @@
+/* 
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
 /*  Version $Id$
-
-    Copyright 1992 The Regents of the University of California.
-			All Rights Reserved.
-
     Programmer:		T.M. Parks
     Date of creation:	7 February 1992
 */
@@ -15,18 +35,16 @@
 #endif
 
 #include "Scheduler.h"
-#include "MTDFThread.h"
 #include "MTDFThreadList.h"
 #include "Monitor.h"
 #include "Condition.h"
+#include "TimeVal.h"
 
-class Galaxy;
 class MTDFStar;
+class MTDFThread;
 
 class MTDFScheduler : public Scheduler
 {
-    friend void sourceThread(MTDFStar*);
-
 public:
     // Constructor.
     MTDFScheduler();
@@ -56,17 +74,24 @@ public:
     void deleteThreads();
 
     // Duration of one schedule period or iteration.
-    double schedulePeriod;
+    TimeVal schedulePeriod;
 
 protected:
     // Stopping time.
-    double stopTime;
+    TimeVal stopTime;
 
     // Thread for main().
     static MTDFThread& main;
 
     // List of Star Threads.
     MTDFThreadList starThreads;
+
+    // Select thread function for a star.
+    virtual void (*thread(MTDFStar*))(MTDFStar*);
+
+    // Thread functions.
+    static void starThread(MTDFStar*);
+    static void sourceThread(MTDFStar*);
 
     // Monitor for guarding the Conditions used by the Scheduler.
     Monitor monitor;
