5d382f1a093bde1158ecefcd1eed4feafb5a0d4f
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-01-09 16:49:11 +0000

    updated copyright notice; moved thread functions to MTDFScheduler; added sleep() method and methods to access system-wide min and max priority

--- trunk/ptclassic/src/domains/pn/kernel/PNThread.h
+++ trunk/ptclassic/src/domains/pn/kernel/PNThread.h
@@ -1,11 +1,30 @@
+/* 
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
 /*  Version $Id$
-
-    Copyright 1992 The Regents of the University of California.
-			All Rights Reserved.
-
     Programmer:		T.M. Parks
     Date of creation:	14 February 1992
-
 */
 
 #ifndef _MTDFThread_h
@@ -18,15 +37,24 @@
 #include "Thread.h"
 
 class MTDFStar;
+class TimeVal;
 
 class MTDFThread : public Thread
 {
 public:
     // Constructor.
-    MTDFThread(unsigned int priority, MTDFStar*);
+    MTDFThread(int priority, void (*thread)(MTDFStar*), MTDFStar*);
 
     // Change the Thread's priority.
-    int setPriority(unsigned int);
+    int setPriority(int);
+
+    // System-wide limits on priority.
+    static int setMaxPriority(int);
+    static int maxPriority();
+    static int minPriority();
+
+    // Disable Thread for the specified time.
+    int sleep(TimeVal);
 };
 
 #endif
