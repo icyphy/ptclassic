d6d04a94e6e9f236ffffd7f3da3b53ab4a6ff8d1
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-01-09 16:41:42 +0000

    updated copyright notice; added support to limit the capacity of the Geodesic and to wait for multiple Particles

--- trunk/ptclassic/src/domains/pn/kernel/PNGeodesic.h
+++ trunk/ptclassic/src/domains/pn/kernel/PNGeodesic.h
@@ -1,13 +1,34 @@
+/* 
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
 /*  Version $Id$
-
-    Copyright 1992 The Regents of the University of California.
-			All Rights Reserved.
-
     Programmer:		T.M. Parks
     Date of creation:	6 February 1992
 
     Geodesic with mutual exclusion for all methods which access data members
-    at run-time.
+    at run-time.  The capacity of the Geodesic may be limited to throttle the
+    source.
 */
 
 #ifndef _MTDFGeodesic_h
@@ -42,9 +63,15 @@ public:
     // size with mutual exclusion.
     int size();
 
+    void setCapacity(int);
+    void wait(int);
+
 protected:
     Monitor geoMon;
     Condition notEmpty;
+    Condition notFull;
+    int capacity;
+    int threshold;
 };
 
 #endif
