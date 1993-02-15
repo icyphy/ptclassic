603e1632d8c1b537480beed45b8c395d9e9ae944
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-02-15 12:58:52 +0000

    Initial revision

---
+++ ptclassic/src/domains/cgc/stars/CGCExpr.pl
@@ -0,0 +1,50 @@
+defstar
+{
+    name { Expr }
+    domain { CGC } 
+    desc { General expression evaluation. }
+    version { $Id$ }
+    author { T. M. Parks }
+    location { CGC main library }
+
+    inmulti
+    {
+	name { in }
+	type { float }
+    }
+
+    output
+    {
+	name { out }
+	type { float}
+    }
+
+    state
+    {
+	name { expr }
+	type { string }
+	default { "$ref(in#1)" }
+	desc { Expression to evaulate. }
+    }
+
+    state
+    {
+	name { include }
+	type { stringArray }
+	default { "<math.h>" }
+	desc { List of necessary include files. }
+    }
+
+    setup
+    {
+	for(int i = 0; i < include.size(); i++)
+	    addInclude(include[i]);
+    }
+
+    go
+    {
+	StringList code;
+	code << "$ref(out) = " << expr << ";\n";
+	addCode(code);
+    }
+}
