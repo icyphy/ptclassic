6072878377e154c1ad1c748ceeb9a135afc406c9
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-02-20 12:50:16 +0000

    derived from CGWormBase class.

--- ptclassic/src/domains/cgc/kernel/CGCWormhole.cc
+++ ptclassic/src/domains/cgc/kernel/CGCWormhole.cc
@@ -40,6 +40,45 @@ ENHANCEMENTS, OR MODIFICATIONS.
 #include "StringList.h"
 #include "Error.h"
 
+/*******************************************************************
+
+	class CGCWormhole methods
+
+********************************************************************/
+
+void CGCWormhole :: setup() {
+	profile = myProfile;
+
+	// get the number of processors.
+	setOuterTarget(targetPtr);
+
+	// main setup routine.
+	CGWormBase :: setup();
+}
+
+void CGCWormhole :: go() {
+	downLoadCode(0, 0);
+}
+
+CGWormBase* CGCWormhole :: myWormhole() { return selfWorm; }
+
+Profile* CGCWormhole :: getProfile(int pNum) {
+	return CGWormBase :: getProfile(pNum);
+}
+
+// Constructor
+CGCWormhole :: CGCWormhole(Galaxy& g, Target* t) : CGWormBase(*this,g,t)
+	{ buildEventHorizons(); }
+
+CGCWormhole :: ~CGCWormhole() {
+	freeContents();
+}
+
+// cloner -- clone the inside and make a new wormhole from that.
+Block* CGCWormhole :: clone() const {
+	LOG_NEW; return new CGCWormhole(gal.clone()->asGalaxy(), target->cloneTarget());
+}
+
 /**************************************************************************
 
 	methods for CGCtoUniversal
