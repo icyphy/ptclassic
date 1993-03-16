a044fe2cd36f53ef497dce95787f1c21648cca35
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-03-16 14:15:23 +0000

    finalize the support for recursion construct.

--- ptclassic/src/domains/cgc/targets/multiproc/CGCDDFTarget.h
+++ ptclassic/src/domains/cgc/targets/multiproc/CGCDDFTarget.h
@@ -40,6 +40,8 @@ ENHANCEMENTS, OR MODIFICATIONS.
 
 #include "CGDDFTarget.h"
 
+class CGCTarget;
+
 class CGCDDFTarget : public CGDDFTarget {
 public:
 	CGCDDFTarget(const char* name, const char* starclass, const char* desc);
@@ -48,27 +50,47 @@ public:
 	int isA(const char*) const;
 
 ///////// target dependent section ////////////////
+	// For "For" and "Recur" construct, we need to arrange the target
+	// dependent codes for UnixSend, UnixReceive stars.
+	/* virtual */ void prepCode(MultiTarget*, Profile*, int, int);
+	// Signal that replication may occur or finish depending on the flag.
+	/* virtual */ void signalCopy(int);
 
 	// for Case construct
-	void startCode_Case(CGStar*,Geodesic*,CGTarget*);
-	void middleCode_Case(int, CGTarget*);
-	void endCode_Case(CGTarget*);
+	/* virtual */ void startCode_Case(CGStar*,Geodesic*,CGTarget*);
+	/* virtual */ void middleCode_Case(int, CGTarget*);
+	/* virtual */ void endCode_Case(CGTarget*);
 
 	// for DoWhile construct
-	void startCode_DoWhile(Geodesic*, Geodesic*, CGTarget*);
-	void endCode_DoWhile(CGStar*, Geodesic*, CGTarget*);
+	/* virtual */ void startCode_DoWhile(Geodesic*, Geodesic*, CGTarget*);
+	/* virtual */ void endCode_DoWhile(CGStar*, Geodesic*, CGTarget*);
 
 	// for For construct
-	void startCode_For(CGStar*, Geodesic*, CGTarget*);
-	void middleCode_For(CGStar*, CGStar*, int, int, CGTarget*);
-	void endCode_For(CGTarget*);
+	/* virtual */ void startCode_For(CGStar*, Geodesic*, CGTarget*);
+	/* virtual */ void 
+		middleCode_For(CGStar*, CGStar*, int, int, CGTarget*);
+	/* virtual */ void endCode_For(CGTarget*);
 
-/*
 	// for Recur construct
-	void startCode_Recur(int,int,CGTarget*);
-	void middleCode_Recur(int, CGTarget*);
-	void endCode_Recur(CGTarget*);
-*/
+	/* virtual */ void 
+		startCode_Recur(Geodesic*, PortHole*, const char*, CGTarget*);
+	/* virtual */ void 
+		middleCode_Recur(Geodesic*, Geodesic*, const char*, CGTarget*);
+	/* virtual */ void endCode_Recur(Geodesic*, const char*, CGTarget*);
+
+private:
+	// No nested recursion is supported here. To allow nested recursion,
+	// We need stack of these.
+
+	// save code stream for recursion construct.
+	CodeStream* saveGalStruct;
+	CodeStream* saveMainInit;
+	CodeStream* saveMainDecls;
+	CodeStream* saveMainClose;
+	CodeStream* saveCode;
+
+	// recursive function definition.
+	StringList funcName;
 };
 
 #endif
