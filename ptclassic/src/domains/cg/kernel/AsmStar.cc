6ca8ed89a78a7b2c040fde7b53844ee532e766e4
jbuck <jbuck@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-06-15 17:12:28 +0000

    add targetBaseName method (returns base of filename for codegen)

--- ptclassic/src/domains/cg/kernel/AsmStar.cc
+++ ptclassic/src/domains/cg/kernel/AsmStar.cc
@@ -234,6 +234,13 @@ void AsmStar :: zapStateEntries() {
 	}
 }
 
+// return ptr to target's "base name": root for generated filenames, if
+// defined.
+const char* AsmStar::targetBaseName() {
+	AsmTarget* asmTargetPtr = (AsmTarget*)targetPtr;
+	return asmTargetPtr ? asmTargetPtr->baseName() : 0;
+}
+
 // destructor
 AsmStar::~AsmStar() {
 	zapStateEntries();
