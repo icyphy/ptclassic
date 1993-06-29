149436daac102961780583d333bb0aa2212a25db
aok <aok@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-06-29 13:13:19 +0000

    Initial revision

---
+++ ptclassic/src/pigilib/ptcl.h
@@ -0,0 +1,7 @@
+// ptcl is a global object pointer for all code that wants to use the ptcl
+// object.  To use ptcl, the following two lines need to be in all code
+
+#include "PTcl.h"
+
+extern PTcl *ptcl;  
+
