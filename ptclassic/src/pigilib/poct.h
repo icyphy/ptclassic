aeab3603aa52ce9166c19f58fc1254fa2df3b4cb
aok <aok@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-07-26 19:23:01 +0000

    Initial revision

---
+++ ptclassic/src/pigilib/poct.h
@@ -0,0 +1,10 @@
+// Version Information:
+//  $Date$  $Id$
+
+// pcot is a global object pointer for all code that wants to use the Poct
+// object.  To use POct, the following two lines need to be in all code
+
+#include "POct.h"
+
+extern POct *poct;  
+
