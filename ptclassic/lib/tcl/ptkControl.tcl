bfbbe9f2be8fe1e01f4130bdae80fdc6a144a678
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
1993-07-29 18:35:44 +0000

    Modified run to use Alan's POct facilities Improved the run control panel in many ways.

--- ptclassic/lib/tcl/ptkControl.tcl
+++ ptclassic/lib/tcl/ptkControl.tcl
@@ -24,94 +24,198 @@
 # CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 # ENHANCEMENTS, OR MODIFICATIONS.
 #                                                         COPYRIGHTENDKEY
+# The global array ptkRunFlag($name), indexed by the name of the universe,
+# has the following values:
+#	undefined or 0		no active run window, universe not created
+#	1			active run window, but no run yet
+#	2			active run
+#	3			paused run
+#	4			finished run
 
-set ptkRunFlag 0
+set ptkRunFlag(main) 0
+
+#######################################################################
+# Return the ptkRunFlag for a given name, or return zero if none exists.
+#
+proc ptkGetRunFlag { name } {
+    global ptkRunFlag
+    if {[info exists ptkRunFlag($name)] && [set temp $ptkRunFlag($name)] > 0} {
+	return $temp
+    } { return 0 }
+}
+
+#######################################################################
+# Set the ptkRunFlag to 4, indicating the run is over, and pop up the
+# GO button.
+#
+proc ptkClearRunFlag { name } {
+    global ptkRunFlag
+    set  cntrWindow .run_$name
+    catch {$cntrWindow.panel.go configure -relief raised}
+    set ptkRunFlag($name) 4
+}
 
 #######################################################################
 # Procedure to run a universe.
 # The argument is the default number of iterations.
 #
-proc ptkRunControl { name defNumIter } {
-# FIX ME: more than one run control window might be desired at once
+proc ptkRunControl { name octHandle } {
     global ptkRunFlag
-    if {$ptkRunFlag == 1} {
-        ptkImportantMessage .error {Sorry.  Only one run window at time. }
+    if {[ptkGetRunFlag $name] > 0} {
+        ptkImportantMessage .error \
+		"Sorry.  Only one run window for $name at time. "
 	return
     }
-    set ptkRunFlag 1
-    set w .runCntrWindow
+    # Mark an open window, but with no run yet.
+    set ptkRunFlag($name) 1
+
+    set w .run_$name
     catch {destroy $w}
     toplevel $w
     wm title $w "Run $name"
     wm iconname $w "Run $name"
 
+    set defNumIter [ptkGetRunLength $octHandle]
+
     message $w.msg -font -Adobe-times-medium-r-normal--*-180* -width 25c \
-            -text $name -justify center
+            -text "Control panel for $name" -justify center
+
+    # Define the entry that controls the number of iterations
     frame $w.iter -bd 10
 	label $w.iter.label -text "Number of iterations:"
-        entry $w.iter.entry -relief sunken -width 20
+        entry $w.iter.entry -relief sunken -width 10 -bg wheat3
 	$w.iter.entry insert @0 $defNumIter
-	bind $w.iter.entry <Return> "ptkGo $w \[$w.iter.entry get]"
-	bind $w.iter.entry <Escape> ptkStop
-	bind $w.iter.entry <space> ptkPause
 	pack append $w.iter $w.iter.entry right $w.iter.label left
+
+    # The following empty frames are created so that they are available
+    # to stars to insert custom controls into the control panel.
+    # By convention, we tend to use "high" for entries, "middle"
+    # for buttons, and "low" for sliders.  The full name for the
+    # frame is .run_${name}.$position, where $name is the name of the
+    # universe, and $position is "high", "middle", or "low".
+    frame $w.high -bd 10
+    frame $w.middle -bd 10
+    frame $w.low -bd 10
+
+    # Define the panel of control buttons
     frame $w.panel -bd 10
 	button $w.panel.go -text " GO <Return> " \
-	    -command "ptkGo $w \[$w.iter.entry get]"
-	button $w.panel.pause -text "PAUSE <Space>" -command ptkPause
-	button $w.panel.stop -text "STOP <Escape>" -command ptkStop
+	    -command "ptkGo $name $octHandle"
+		
+	button $w.panel.pause -text "PAUSE <Space>" -command "ptkPause $name"
+	button $w.panel.stop -text "STOP <Escape>" -command "ptkStop $name"
 	pack append $w.panel \
 	    $w.panel.go {left expand fill} \
 	    $w.panel.pause {left expand fill} \
 	    $w.panel.stop {right expand fill}
-    button $w.dismiss -text "DISMISS" -command "set ptkRunFlag 0; destroy $w"
+
+    button $w.dismiss -text "DISMISS" -command \
+	"ptkRunControlDel $name $w $octHandle $defNumIter"
+
     pack append $w \
 	$w.msg {top fill expand} \
 	$w.iter top \
 	$w.panel top \
+	$w.high top \
+	$w.middle top \
+	$w.low top \
 	$w.dismiss {top fill expand}
 
-    bind $w <Return> "ptkGo $w \[$w.iter.entry get]"
-    bind $w <space> ptkPause
-    bind $w <Escape> ptkStop
-
-    set ptkRunFlag 1
     wm geometry $w +400+400
     set prevFocus [focus]
     focus $w
-# The following two commands would ideally not be necessary, but are now.
-#   grab $w
-#   tkwait window $w
-#   focus $prevFocus
+    wm protocol $w WM_DELETE_WINDOW \
+	"ptkRunControlDel $name $w $octHandle $defNumIter"
+
+    set olduniverse [curuniverse]
+    newuniverse $name
+    ptkCompile $octHandle
+    curuniverse $olduniverse
+
+    bind $w.iter.entry <Return> \
+		"ptkGo $name $octHandle"
+    bind $w.iter.entry <Escape> "ptkStop $name"
+    bind $w.iter.entry <space> "ptkPause $name"
+    bind $w <Return> "ptkGo $name $octHandle"
+    bind $w <space> "ptkPause $name"
+    bind $w <Escape> "ptkStop $name"
+    bind $w <Control-d> "ptkRunControlDel $name $w $octHandle $defNumIter"
+}
+
+#######################################################################
+# Procedure to delete a control window
+#
+proc ptkRunControlDel { name window octHandle defNumIter} {
+    global ptkRunFlag
+    if {$ptkRunFlag($name) == 2} "ptkStop $name"
+	 set ptkRunFlag($name) 0
+    # update the oct facet only if the number of iterations has changed.
+    if {$defNumIter != [$window.iter.entry get]} {
+         ptkSetRunLength $octHandle [$window.iter.entry get]
+    }
+    deluniverse $name
+    destroy $window
 }
 
 #######################################################################
 # procedure to stop a run
-proc ptkStop {} {
-        ptkImportantMessage .error {Stop not implemented yet}
+proc ptkStop { name } {
+    global ptkRunFlag
+    # Ignore if the named system is not running
+    if {$ptkRunFlag($name) != 2} return
+    halt
+    # Finish processing the run command
+    update
+    set cntrWindow .run_$name
+    catch {$cntrWindow.panel.go configure -relief raised}
+    catch {$cntrWindow.panel.pause configure -relief raised}
+    wrapup
+    ptkClearRunFlag $name
 }
 
 #######################################################################
 # procedure to pause a run
-proc ptkPause {} {
-        ptkImportantMessage .error {Pause not implemented yet}
+proc ptkPause { name } {
+    ptkImportantMessage .error {Pause not implemented yet}
+#   global ptkRunFlag
+#   #Ignore the command if the system is not running
+#   if {$ptkRunFlag($name) != 2} return
+#   set olduniv [curuniverse]
+#   curuniverse $name
+#   halt
+#   curuniverse $olduniv
+#   # Finish processing the run command
+#   update
+#   set cntrWindow .run_$name
+#   catch {$cntrWindow.panel.go configure -relief raised}
+#   catch {$cntrWindow.panel.pause configure -relief sunken}
+#   set ptkRunFlag($name) 3
 }
 
 #######################################################################
 #procedure to go
-proc ptkGo {cntrWindow numIter} {
+proc ptkGo {name octHandle} {
+    set  cntrWindow .run_$name
+    set w .run_$name
+    set numIter [$w.iter.entry get]
     global ptkRunFlag
-    if {$ptkRunFlag == 2} {
-        ptkImportantMessage .error {Sorry.  Only one run window at time. }
+    # For now, we allow only one run at a time.
+    if {$ptkRunFlag([curuniverse]) == 2} {
+        ptkImportantMessage .error \
+		"Sorry.  Only one run at time. "
 	return
     }
-    set ptkRunFlag 2
-    $cntrWindow.panel.go configure -relief sunken
-#   $cntrWindow.panel.pause configure -relief raised
-    run $numIter
-    wrapup
-# Need to set the number of iterations in the oct database at this point,
-# preferably only if they have changed.  How to do this?
-    catch {$cntrWindow.panel.go configure -relief raised}
-    set ptkRunFlag 1
+    set prevRunFlag $ptkRunFlag($name)
+    set ptkRunFlag($name) 2
+    catch {$cntrWindow.panel.go configure -relief sunken}
+    catch {$cntrWindow.panel.pause configure -relief raised}
+    if {$prevRunFlag != 3} {
+        curuniverse $name
+        ptkCompile $octHandle
+        run $numIter
+    } { cont }
+    if {$ptkRunFlag($name) != 3} {
+	wrapup
+	ptkClearRunFlag $name
+    }
 }
