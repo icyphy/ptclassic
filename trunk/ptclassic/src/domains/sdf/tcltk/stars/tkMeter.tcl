0bf11b66ea4df8015058a67e0f9fe39124ab353d
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-07-29 19:25:59 +0000

    added support for state put_in_control_panel

--- trunk/ptclassic/src/domains/sdf/tcltk/stars/tkMeter.tcl
+++ trunk/ptclassic/src/domains/sdf/tcltk/stars/tkMeter.tcl
@@ -17,16 +17,26 @@
 #	high
 # where the last three are given values corresponding to parameter values.
 
-set s .run_[curuniverse].${uniqueSymbol}meter
+if {![info exists putInCntrPan]} {set putInCntrPan 1}
+
+if {$putInCntrPan} \
+   { set s .run_[curuniverse].low.${uniqueSymbol}meter } \
+   { set s .run_[curuniverse].${uniqueSymbol}meter }
 
 # If a window with the right name already exists, we assume it was
 # created by a previous run of the very same star, and hence can be
 # used for this new run.
 
 if {![winfo exists $s]} {
-    toplevel $s
-    wm title $s "Bar Meters"
-    wm iconname $s "Bar Meters"
+
+    if {$putInCntrPan} {
+	frame $s
+	pack append .run_[curuniverse].low $s top
+    } {
+        toplevel $s
+        wm title $s "Bar Meters"
+        wm iconname $s "Bar Meters"
+    }
 
     frame $s.f
     message $s.msg -font -Adobe-times-medium-r-normal--*-180* -width 12c \
@@ -34,21 +44,25 @@ if {![winfo exists $s]} {
     for {set i 0} {$i < $numInputs} {incr i} {
     	ptkMakeMeter $s.f m$i "" $low $high
     }
-    button $s.ok -text "DISMISS" -command "destroy $s"
-    pack append $s $s.msg {top expand} $s.f top $s.ok {top fillx}
+    pack append $s $s.msg {top expand} $s.f top
+
+    if {!$putInCntrPan} {
+        button $s.ok -text "DISMISS" -command "destroy $s"
+        pack append $s $s.ok {top fillx}
+    }
 
     # In the following procedure, all variables are passed, so
     # there is no need to evaluat any values when the file is sourced.
-    proc tkMeterSetValues {uniqueSymbol numInputs} {
-        set s .run_[curuniverse].${uniqueSymbol}meter
-        set c $s.f
-        set inputVals [${uniqueSymbol}grabInputs]
-	for {set i 0} {$i < $numInputs} {incr i} {
-            set in [lindex $inputVals $i]
-            ptkSetMeter $s.f m$i $in
+    proc tkMeterSetValues {uniqueSymbol numInputs} "
+        set win $s
+        set c \$win.f
+        set inputVals \[${uniqueSymbol}grabInputs]
+	for {set i 0} {\$i < \$numInputs} {incr i} {
+            set in \[lindex \$inputVals \$i]
+            ptkSetMeter \$win.f m\$i \$in
 	}
         update
-    }
+    "
 
     # In the following definition, the value of uniqueSymbol and
     # numInputs is evaluated when the file is sourced.
