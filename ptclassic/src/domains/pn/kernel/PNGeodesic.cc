c88299ea5ebc826fbdf39487aeb9b8e427fed6a0
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-08-17 12:04:50 +0000

    eliminated capacity limits; updated to reflect multi-threading support in Geodesic base class

--- ptclassic/src/domains/pn/kernel/PNGeodesic.cc
+++ ptclassic/src/domains/pn/kernel/PNGeodesic.cc
@@ -23,11 +23,10 @@ ENHANCEMENTS, OR MODIFICATIONS.
 							COPYRIGHTENDKEY
 */
 /*  Version $Id$
-    Programmer:		T.M. Parks
-    Date of creation:	6 February 1992
+    Author:	T.M. Parks
+    Created:	6 February 1992
 
-    Geodesic with mutual exclusion for all methods which access data members
-    at run-time.
+    Geodesic which blocks if accessed when full or empty.
 */
 
 static const char file_id[] = "$RCSfile$";
@@ -37,80 +36,50 @@ static const char file_id[] = "$RCSfile$";
 #endif
 
 #include "MTDFGeodesic.h"
-#include "CriticalSection.h"
 
 // Class identification.
 ISA_FUNC(MTDFGeodesic, Geodesic);
 
 // Constructor.
-MTDFGeodesic::MTDFGeodesic() : geoMon(), notEmpty(geoMon), notFull(geoMon)
-{
-    // Non-positive capacity indicates that there is no limit.
-    capacity = 0;
+MTDFGeodesic::MTDFGeodesic() : notEmpty(0)
+{ }
 
-    // By default, only one Particle is required for notEmpty.
-    threshold = 1;
+void MTDFGeodesic::makeLock(const PtGate& master)
+{
+    LOG_DEL; delete notEmpty;
+    notEmpty = 0;
+    Geodesic::makeLock(master);
+    LOG_NEW; notEmpty = new LwpCondition((LwpMonitor*)gate);
 }
 
-// put with mutual exclusion.
-void MTDFGeodesic::put(Particle* p)
+void MTDFGeodesic::delLock()
 {
-    CriticalSection x(geoMon);
-    if (capacity > 0 && Geodesic::size() >= capacity) notFull.wait();
-    Geodesic::put(p);
-    if (Geodesic::size() == threshold) notEmpty.notifyAll();
+    LOG_DEL; delete notEmpty;
+    notEmpty = 0;
+    Geodesic::delLock();
 }
 
-// get with mutual exclusion.
-Particle* MTDFGeodesic::get()
+// Notify when not empty.
+void MTDFGeodesic::slowPut(Particle* p)
 {
-    CriticalSection x(geoMon);
-    if (Geodesic::size() < threshold) notEmpty.wait();
-    threshold = 1;	// reset threshold
-    Particle* p = Geodesic::get();
-    if (Geodesic::size() == capacity-1) notFull.notifyAll();
+    CriticalSection region(gate);
+    Geodesic::slowPut(p);
+    notEmpty->notifyAll();
+}
+
+// Block util not empty.
+Particle* MTDFGeodesic::slowGet()
+{
+    CriticalSection region(gate);
+    if (size() < 1) notEmpty->wait();
+    Particle* p = Geodesic::slowGet();
     return p;
 }
 
-// pushBack with mutual exclusion.
+// Notify when not empty.
 void MTDFGeodesic::pushBack(Particle* p)
 {
-    CriticalSection x(geoMon);
+    CriticalSection region(gate);
     Geodesic::pushBack(p);
-    if (Geodesic::size() == threshold) notEmpty.notifyAll();
-}
-
-// size with mutual exclusion.
-int MTDFGeodesic::size()
-{
-    CriticalSection x(geoMon);
-    return Geodesic::size();
-}
-
-// Limit the capacity of the Geodesic.
-void MTDFGeodesic::setCapacity(int c)
-{
-    CriticalSection x(geoMon);
-    capacity = c;
-    if (capacity > 0 && threshold > capacity) threshold = capacity;
-}
-
-/* Set threshold and wait until the Geodesic is full.  Capacity is
-   temporarily limited to automatically block the source as soon as the
-   Geodesic is full.
-*/
-void MTDFGeodesic::wait(int t)
-{
-    CriticalSection x(geoMon);
-    threshold = t;
-    if (capacity > 0 && threshold > capacity) threshold = capacity;
-    if (Geodesic::size() < threshold)
-    {
-	int c = capacity;	// save current capacity
-	capacity = threshold;
-	notEmpty.wait();
-	capacity = c;		// restore original capacity
-	if (capacity <= 0 || Geodesic::size() < capacity)
-	    notFull.notifyAll();
-    }
+    if (gate) notEmpty->notifyAll();
 }
