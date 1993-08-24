af1c376f224efe8cebdd4fd2752423c7269a5f4e
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-08-24 12:39:09 +0000

    Allow dynamically varying numbers of outputs and Optionally avoid initializing the recursive galaxy on each firing.

--- trunk/ptclassic/src/domains/ddf/stars/DDFSelf.pl
+++ trunk/ptclassic/src/domains/ddf/stars/DDFSelf.pl
@@ -1,9 +1,23 @@
 defstar {
 	name { Self }
 	domain { DDF }
-	desc { Realizes recursion. }
+	desc {
+This star represents the galaxy given by the parameter "recurGal",
+which must be above it in the hierarchy.  The instance of the galaxy
+is not created until it is actually needed, so the number of instances
+(the depth of the recursion) does not need to be known a-priori.
+If the parameter "reinitialize" is FALSE, then the instance of the
+galaxy is created the first time it fires and reused on subsequent firings.
+If "reinitialize" is TRUE, then the galaxy is created on every firing
+and destroyed after the firing.  Inputs are sent to the instance
+of the galaxy and outputs are retrieved from it.  The inputs of the
+named galaxy must be named "input#?" and the outputs must be named
+"output#?", where "?" is replaced with an integer starting with zero.
+This allows the inputs and outputs of this star to be matched
+unambiguously with the inputs and outputs of the referenced galaxy.
+	}
 	version { $Id$ }
-	author { Soonhoi Ha }
+	author { Soonhoi Ha and E. A. Lee }
 	copyright {
 Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
 All rights reserved.
@@ -12,18 +26,19 @@ limitation of liability, and disclaimer of warranty provisions.
 	}
 	location { DDF library }
 	explanation {
-This is a star for recursion.  At runtime, it appears to contain
-a clone of the galaxy whose name is given by "recurGal".
-.Ir "recursion"
-That galaxy is supplied with the input particles and executed.
-That galaxy may be one within which this instance of Self resides,
-hence realizing recursion.
-At compile time, this star appears to just be an atomic star.
+Each input must have exactly one token for the star to fire.
+The number of outputs produced can depend on the internal galaxy.
+.pp
+Creating a new instance of the galaxy is quite an expensive operation,
+so setting "reinitialize" to TRUE will cause dramatically slower
+execution.  Thus this should not be used unless it is absolutely
+necessary.
 	}
 	seealso { fibonnacci }
 	hinclude { "InterpGalaxy.h",  "Domain.h", "DDFScheduler.h"}
 	protected {
-		InterpGalaxy* gal;
+		InterpGalaxy* masterGal;
+		InterpGalaxy *myGal;
 		DDFScheduler  sched;
 	}
 	inmulti {
@@ -33,6 +48,7 @@ At compile time, this star appears to just be an atomic star.
 	outmulti {
 		name { output }
 		type { ANYTYPE }
+		num { 0 }
 	}
 // made one change: the value "", not NULL, is given to indicate recurGal
 // not set.
@@ -41,6 +57,12 @@ At compile time, this star appears to just be an atomic star.
 		type { string }
 		default { "" }
 		desc { Name of galaxy to be invoked. }
+	}
+	defstate {
+		name { reinitialize }
+		type { INT }
+		default { "NO" }
+		descriptor { Initialize the galaxy on each firing }
 	}
 // isItSelf method.
 	method {
@@ -57,9 +79,17 @@ At compile time, this star appears to just be an atomic star.
 		code { return "Self"; }
 	}
 	constructor {
-		gal = 0;
+		masterGal = 0;
+		myGal = 0;
 	}	
+	destructor {
+		LOG_DEL; delete myGal;
+	}
 	setup {
+		// The following should not be required because of the
+		// num { 0 } above.  At one time, this did not work.
+		// output.setDDFParams(0);
+
 		StringList msg = "DDFSelf Star \"";
 		msg += name();
 		msg += "\" :\n";
@@ -80,12 +110,12 @@ At compile time, this star appears to just be an atomic star.
 			Error :: abortRun(msg);
 			return;
 		}
-		gal = (InterpGalaxy*) &(b->asGalaxy());
+		masterGal = (InterpGalaxy*) &(b->asGalaxy());
 
 		// match portholes.
 		int inNum = 0, outNum = 0;
-		BlockPortIter next(*gal);
-		for (int i = gal->numberPorts(); i > 0; i--) {
+		BlockPortIter next(*masterGal);
+		for (int i = masterGal->numberPorts(); i > 0; i--) {
 			PortHole& p = *next++;
 			if (p.isItInput()) inNum++;
 			else outNum++;
@@ -106,9 +136,10 @@ At compile time, this star appears to just be an atomic star.
 		if (flag) Error::abortRun(msg);
 	}
 	go {
+	    if (myGal == 0) {
 		// clone the recursion galaxy
-		InterpGalaxy* myGal = (InterpGalaxy*) gal->clone();
-		myGal->setNameParent(gal->name(), this);
+		myGal = (InterpGalaxy*) masterGal->clone();
+		myGal->setNameParent(masterGal->name(), this);
 		sched.resetFlag();
 
 		// make a image connection between DDFSelf and galaxy.
@@ -129,28 +160,31 @@ At compile time, this star appears to just be an atomic star.
 			Error :: abortRun("error in setup the scheduler in DDFSelf\n");
 			return;
 		}
+	    } else {
+		if (int(reinitialize)) myGal->initialize();
+	    }
 
-		// move data into galaxy
-		MPHIter nexti(input);
-		for (i = input.numberPorts(); i > 0; i--) {
-			InDDFPort& p = *(InDDFPort*) nexti++;
-			p.receiveData();
-			p.moveData();
-		}
+	    // move data into galaxy
+	    MPHIter nexti(input);
+	    for (int j = input.numberPorts(); j > 0; j--) {
+	    	InDDFPort& p = *(InDDFPort*) nexti++;
+	    	p.receiveData();
+		if (Scheduler :: haltRequested()) return;
+	    	p.moveData();
+	    }
 
-		// scheduler run
-		sched.run();	// default stop condition = 1
+	    // scheduler run.  Unless we reinitialize, we increment the
+	    // stop time by one each time through.
+	    sched.setStopTime(sched.getStopTime() + 1.0);
+	    sched.run();
 
-		// move data out of galaxy
-		MPHIter nexto(output);
-		for (i = output.numberPorts(); i > 0; i--) {
-			OutDDFPort& p = *(OutDDFPort*) nexto++;
-			p.moveData();
-			p.sendData();
-		}
-
-		// delete all
-		LOG_DEL; delete myGal;
+	    // move data out of galaxy
+	    int numXfred;
+	    MPHIter nexto(output);
+	    for (j = output.numberPorts(); j > 0; j--) {
+	    	OutDDFPort& p = *(OutDDFPort*) nexto++;
+	    	numXfred = p.moveData();
+		for (int c = numXfred; c>0; c--) p.sendData();
+	    }
 	}
 }
-
