2411efbac82472f2d4c21415f6835f151e995fd9
mchen <mchen@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-09-26 18:06:24 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/stars/SDFFixToInt.pl
@@ -0,0 +1,27 @@
+defstar {
+	name { FixToInt }
+	domain { SDF }
+	version { $Id$ }
+	author { Mike J. Chen }
+	copyright {
+Copyright (c) 1990-1993 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { SDF conversion palette }
+	desc { Converts a fix type input to an integer type output. }
+	input {
+		name { input }
+		type { fix }
+		desc { Input fix type }
+	}
+	output {
+		name { output }
+		type { int }
+		desc { Output int type }
+	}
+	go {
+	    output%0 << (int)(input%0);
+	}
+}
