2015f39c8b411d243473a2a59a6db904d6166065
aok <aok@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-10-05 14:43:11 +0000

    Initial revision

---
+++ ptclassic/src/pigilib/PVem.h
@@ -0,0 +1,85 @@
+/**************************************************************************
+Version identification:
+$Id$
+
+Copyright (c) 1993  The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+		       
+ Programmer:  Alan Kamas
+ Date of creation: October 1993
+
+This file implements a class that adds Ptolemy-specific Vem commands to
+a Tcl interpreter.  
+
+**************************************************************************/
+
+#ifndef _PVem_h
+#define _PVem_h 1
+#include "tcl.h"
+#include "TclObj.h"
+
+typedef int boolean;
+extern "C" {
+#define Pointer screwed_Pointer
+#include "oct.h"  // Oct Pointer Structure
+#include "rpc.h"  // Oct Pointer Structure
+#include "local.h" // Defines useful items like TRUE and FALSE
+#undef Pointer
+}
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+class PVem : TclObj {
+
+public:
+	// the active Tcl interpreter, for error reporting.
+	static Tcl_Interp* activeInterp;
+
+	PVem(Tcl_Interp* interp = 0);
+	~PVem();
+
+	// the dispatcher is called by Tcl to handle all extension
+	// commands.
+	static int dispatcher(ClientData,Tcl_Interp*,int,char*[]);
+
+        // the following are the Tcl-callable functions.  Each returns TCL_OK
+        // or TCL_ERROR, and may set the Tcl result to return a string
+        int pvOpenWindow (int argc,char** argv);
+
+protected:
+	// helper functions, not callable directly
+	void AdjustScalePan (Window window, octId facetId);
+
+private:
+        static PVem* findPVem(Tcl_Interp*);
+        void makeEntry();
+        void removeEntry();
+	// function to register extensions with the Tcl interpreter
+	void registerFuncs();
+
+};
+
+#endif		// _PVem_h
+
