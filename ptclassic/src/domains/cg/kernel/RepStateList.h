8789bc1cfe34afde307dc2a4e70b827d8e1af227
Jose Luis Pino <joseluispino@users.noreply.github.com>
1993-10-06 14:56:43 +0000

    Initial revision

---
+++ ptclassic/src/domains/cg/kernel/RepStateList.h
@@ -0,0 +1,52 @@
+#ifndef _RepStateList_h
+#define _RepStateList_h 1
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include "State.h"
+
+/**************************************************************************
+Version identification:
+$Id$
+
+Copyright (c) 1993 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+
+ Programmer: Jose Luis Pino
+ Date of creation: 10/3/93
+
+**************************************************************************/
+
+//  This class is used to store a list of states where the state names
+//  are repeated.  It can find equivalent states in the list where equivalent
+//  means that the state is of the same name, type, size.  One example,
+//  of where this is used is with global cg states.
+
+class RepStateList : StateList {
+public:
+	State* equivalentState(const State& s) const;
+};
+
+#endif
