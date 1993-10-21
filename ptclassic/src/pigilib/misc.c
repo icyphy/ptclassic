83b9c388d41a8db66b2e356b5a70d15667b36a2a
aok <aok@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-10-21 21:56:58 +0000

    Added open facet window

--- ptclassic/src/pigilib/misc.c
+++ ptclassic/src/pigilib/misc.c
@@ -357,129 +357,18 @@ long userOptionWord;
 static void AdjustScalePan();
 
 /* 6/27/89 = tries to open facet read-only first */
+/* 10/21/93  Now uses Wei-Jen's nifty Tk code */
 int 
 RpcOpenFacet(spot, cmdList, userOptionWord) /* ARGSUSED */
 RPCSpot *spot;
 lsList cmdList;
 long userOptionWord;
 {
-    static dmTextItem items[] = {
-	{"Name", 1, dmWidth, "untitled", NULL},
-	{"View", 1, dmWidth, "schematic", NULL},
-	{"Facet", 1, dmWidth, "contents", NULL}
-    };
-#define ITEMS_N sizeof(items) / sizeof(dmTextItem)
-    octObject facet, prop;
-    octStatus status;
-    RPCArg *theArg;
-    Window newWindow;  /* the vem window looking at facet */
-    octBox bbox;  /*  an argument to octBB, return value not used */
-
-
     ViInit("open-facet");
     ErrClear();
-
-    /* see if the facet name was given as a Vem argument */
-    if ((lsLastItem(cmdList, (lsGeneric *) &theArg, LS_NH) == VEM_OK) &&
-	(theArg->argType == VEM_TEXT_ARG)) {
-	/* set default values */
-	facet.contents.facet.view = "schematic";
-	facet.contents.facet.facet = "contents";
-	facet.objectId = 0;	/* silence Purify */
-	if (ohUnpackFacetName(&facet, theArg->argData.string) != OCT_OK) {
-	    PrintErr("Cannot parse window specification");
-	    ViDone();
-	}
-	else {
-	    char buf[512];
-	    items[0].value = facet.contents.facet.cell;
-	    items[1].value = facet.contents.facet.view;
-	    items[2].value = facet.contents.facet.facet;
-	    sprintf (buf, "opening %s:%s:%s", items[0].value, items[1].value,
-		     items[2].value);
-	    PrintDebug (buf);
-	    sleep(1);
-	}
-    }
-    /* not on command line, prompt for it */
-    else if (dmMultiText("open-facet", ITEMS_N, items) != VEM_OK) {
-	PrintCon("Aborted entry");
-	ViDone();
-    }
-    status = OpenFacet(&facet, items[0].value, items[1].value, items[2].value,
-	"r");
-    if (status == OCT_NO_EXIST) {
-	status = OpenFacet(&facet, items[0].value, items[1].value,
-	    "contents", "a");
-	if (status <= 0) {
-	    PrintErr(octErrorString());
-	    ViDone();
-	} else if (status == OCT_NEW_FACET) {
-	    GetOrCreatePropStr(&facet, &prop, "TECHNOLOGY", UTechProp);
-	    GetOrCreatePropStr(&facet, &prop, "VIEWTYPE", "SCHEMATIC");
-	    /* If facet is schematic:contents then use schematic editstyle,
-		else ask...
-	    */
-	    if (strcmp(items[2].value, "contents") == 0 &&
-		strcmp(items[1].value, "schematic") == 0) {
-		GetOrCreatePropStr(&facet, &prop, "EDITSTYLE", "SCHEMATIC");
-	    }
-	}
-    } else if (status <= 0) {
-	PrintErr(octErrorString());
-	ViDone();
-    }
-    newWindow = vemOpenWindow(&facet, NULL);
-    /* if the facet has no bounding box, that means it has no geometry
-       to define a bounding box, then we zoom-out to the appropriate scale
-       and pan to let the X-Y axes disappearing from the window. */
-    if (octBB(&facet, &bbox) == OCT_NO_BB) {
-        AdjustScalePan(newWindow, facet.objectId);
-    }
+    TCL_CATCH_ERR( 
+	Tcl_VarEval( ptkInterp, "dialog.openFacet", (char *) NULL) );
     ViDone();
-#undef ITEMS_N
-}
-
-/* this function is only used in RpcOpenFacet.  Since vem opens a window 
-on a facet having no geometry with a very inconvenient scale and annoying 
-cross-hair(X-Y axes), we do an automatic zoom-out  to an appropriate scale
-and pan to a point far from the origin so that the cross hair won't be seen
-in the window.  */
-static void
-AdjustScalePan(window, facetId)
-Window window;
-octId facetId;
-{
-    RPCSpot spot;
-    lsList argList;
-    RPCArg boxArg;
-    RPCPointArg bbPoints[2];
-    char *command;
-
-    spot.theWin = window;
-    spot.facet = facetId;
-    /* center point of window after panning, should be far from origin. */
-    spot.thePoint.x = 1000;
-    spot.thePoint.y = 1000;
-    argList= lsCreate();
-    boxArg.argType = VEM_BOX_ARG;
-    boxArg.length = 1;
-    bbPoints[0].theWin = window;  bbPoints[1].theWin = window;
-    bbPoints[0].facet = facetId;  bbPoints[1].facet = facetId;
-    /* zoom-out to box (-1,-1)(1,1).  Since vem opens a window on a empty facet
-       with corner points at (-22,22)(22,22),  the zoom-out factor is about
-       22 times and is appropriate for constructing ptolemy schematic 
-       diagrams.  */
-    bbPoints[0].thePoint.x = -1;  bbPoints[1].thePoint.x = 1;
-    bbPoints[0].thePoint.y = -1;  bbPoints[1].thePoint.y = 1;
-    boxArg.argData.points = bbPoints;
-    lsNewEnd(argList, (lsGeneric) &boxArg, LS_NH);
-    command = "zoom-out";
-    vemCommand(command, &spot, argList, (long) 0);
-    command = "pan";
-    vemCommand(command, &spot, argList, (long) 0);
-    lsDestroy(argList, NULL);
-    return;
 }
 
 /* Load the star pointed to */
