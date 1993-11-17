ea254a2f169067e0a56691569f3f1015d6960156
banerjea <banerjea@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-11-17 11:37:35 +0000

    Finished writing code but not tried to compile yet

--- trunk/ptclassic/src/domains/de/kernel/CalendarQueue.h
+++ trunk/ptclassic/src/domains/de/kernel/CalendarQueue.h
@@ -60,6 +60,9 @@ public:
 	double	level;		// currently, level is "double" type.
 	double fineLevel;	// If levels are equal, we may need
 				// finerLevel which is optional.
+	Star* dest;             // All the destinations are also stored
+				// contiguously to allow sequential
+				// popping off of same Star event;
 	LevelLink() {}
 
 private:
@@ -68,7 +71,7 @@ private:
 
 	// v sets the level, and fv sets the fineLevel of the entry.
 	// Numerically smaller number represents  the higher priority.
-	LevelLink* setLink(Pointer a, double v, double fv, LevelLink* n, LevelLink* b);
+	LevelLink* setLink(Pointer a, double v, double fv, Star* d, LevelLink* n, LevelLink* b);
 };
 
 	//////////////////////////////////////
@@ -82,30 +85,22 @@ public:
 	// first and its fineLevel (fv) second.
 	// Numerically smaller number represents the higher priority.
 	// (i.e., highest level is at the tail)
-	LevelLink* levelput(Pointer a, double v, double fv = 1.0);
+	LevelLink* levelput(Pointer a, double v, double fv = 1.0, Star* dest);
 
 	// Push back the link just gotten.
 	void pushBack(LevelLink*);
 
 	// Remove and return link from the head of the queue...
-	Pointer getFirstElem();
+	Pointer getFirstElem() {
+		LevelLink *f = get();
+		putFreeLink(f);
+		return f->e;
+	}
 
 	// Remove and return link from the head of the queue...
 	// WARNING -- Must call putFreeLink() after finishing with it.
 	// Use getFirstElem() to get and free in one step.
-	LevelLink* get();
-
-	// Return the next node on list, relative to the last reference
-	LevelLink* next();
-
-	// Reset the reference pointer
-	void reset() {lastReference = lastNode ;}
-
-	// Extract a link within the queue, which deviates from the
-   	// definition of "queue", but necessary-and-convenient method
-	// for getting all elements of same level and of same property...
-	// The extracted Link is out into the free List.
-	void extract(LevelLink*);
+	LevelLink* get() { return NextEvent(); }
 
 	// Return number of elements currently in queue...
 	int length() {return numberNodes;}
@@ -117,7 +112,6 @@ public:
 	// put the link into the pool of free links.
 	virtual void 	   putFreeLink(LevelLink*);
 
-// FIXME
 	// Constructor
 	CalendarQueue() : freeLinkHead(0), numberNodes(0), numFreeLinks(0), 
 			  cq_resizeEnabled(1) 
@@ -125,6 +119,9 @@ public:
 	virtual ~CalendarQueue();
 
 protected:
+	void enableResized()  { cq_resizeEnabled = 1;}
+	void disableResized() { cq_resizeEnabled = 0;}
+
 	LevelLink **cq_bucket;
 
 	int cq_resizeEnabled;  
@@ -134,14 +131,10 @@ protected:
 
 	int cq_bottomThreshold, cq_topThreshold;  
 	int cq_bucketNum;       /* number of buckets                     */
-	int cq_eventNum;               /* number of events in calendar queue    */
-	double cq_interval;        /* size of intervals of each bucket      */
+	int cq_eventNum;        /* number of events in calendar queue    */
+	double cq_interval;     /* size of intervals of each bucket      */
 	int cq_firstSub;
 	LevelLink* CalendarQ[QUEUE_SIZE];
-
-	// Store last access to the list for sequential access
-	LevelLink* lastReference;
-	int lastReferenceBucket; // Bucket number where the above element lies
 
 	// To avoid memory (de)allocation overhead at each push/pop, we
 	// store the freeLinks once created.
