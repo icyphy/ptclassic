84a55bee48e53b257b148a664c4442fa30eeb943
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-12-07 13:54:34 +0000

    Corrected grammar

--- ptclassic/src/domains/de/stars/DEPriorityQueue.pl
+++ ptclassic/src/domains/de/stars/DEPriorityQueue.pl
@@ -4,13 +4,15 @@ defstar {
 	desc {
 A priority queue.
 Inputs have priorities according to the number of the input, with
-"inData#1" having highest priority, "inData#2" being next, etc.
+"inData#1" having highest priority, "inData#2" having the second
+highest priority, etc.
 When a "demand" is received, outputs are produced by selecting first
-based on priority, and then based on time of arrival, using a FIFO policy.
-A finite total capacity can be specified by setting the "capacity"
-parameter to a positive integer.
-When capacity is reached, further inputs are sent to the "overflow"
-output, and not stored.
+based on priority, and then based on the time of arrival, using a
+first-in first-out (FIFO) policy.
+A finite total capacity of the queue can be specified by setting the
+"capacity" parameter to a positive integer.
+When the capacity has been reached, further inputs are sent to the "overflow"
+output and are not stored.
 The "numDemandsPending" and "consolidateDemands" states have the same
 meaning as in other Queue stars.
 The size of the queue is sent to the "size" output whenever an "inData"
@@ -38,20 +40,20 @@ earlier arrivals on \fIinData#2\fR.
 .pp
 The first particle to arrive at an input is always passed directly
 to the output, unless \fInumDemandsPending\fR is initialized to 0.
-If several particles arrive first with the same time stamp, the highest
+If several particles arrive first with the same time stamp, then the highest
 priority particle will go to the output.
 .pp
 If \fIconsolidateDemands\fR is set to TRUE (the default),
 then \fInumDemandsPending\fR is not permitted to rise above unity.
 This means that
 if multiple \fIdemand\fR particles with the same time stamp
-are waiting at the \fIdemand\fR input, they are
+are waiting at the \fIdemand\fR input, then they are
 consolidated into a single demand, and only one output is produced.
 As usual, if a \fIdemand\fR input arrives between the time that the queue
-goes empty and the next arrival of a data input, it enables
+becomes empty and the next arrival of data input, then the star enables
 the next \fIinData\fR particle to pass immediately to the output by
 setting the state \fInumDemandsPending\fR to unity.
-But if more than one \fIdemand\fR event arrives in this time period, the
+But if more than one \fIdemand\fR event arrives in this time period, then the
 effect is the same as if only one such event had arrived.
 .pp
 If \fIconsolidateDemands\fR is set to FALSE, then every \fIdemand\fR input
@@ -59,28 +61,29 @@ will eventually stimulate an output, even if successive demands arrive
 when the queue is empty, and even if successive demands arrive with the
 same time stamp.
 .pp
-A total capacity limit is specified.
+A total capacity limit can be specified.
 This is the limit on the total number of particles that the star
-may store from one firing to the next, regardless of which input they came from.
+may store from one firing to the next, regardless of which input they came.
 Note that storage is allocated dynamically, so a large capacity
 does not necessarily imply a large storage usage.
-When the capacity is reached, all further inputs will be routed
+When the capacity has been reached, all further inputs will be routed
 to the \fIoverflow\fR output
 until a demand input makes space in the star.
-To make the capacity infinite, simply enter a negative number.
+To make the capacity infinite, simply enter a negative number for the
+"capacity" parameter.
 .pp
 Like the
 .c FIFOQueue
-star, this star de-queues particles on demand.
+star, this star dequeues particles on demand.
 Data outputs are produced when a demand input arrives, if the any of the
 queues has particles in it.
-If all queues are empty, no data output is produced
+If all queues are empty, then no data output is produced
 until the next time a data input arrives.
-That next data input will be directed to the data output immediately.
+The next data input will be directed to the data output immediately.
 Any intervening demand inputs (between the time that the queues
 go empty and the next arrival of a data input) are ignored.
 .pp
-Each time a data or demand input arrives, the size of the queue
+Each time data or demand input arrives, the size of the queue
 after processing the input is sent to the \fIsize\fR output.
 	}
 	seealso {FIFOQueue}
@@ -108,7 +111,7 @@ after processing the input is sent to the \fIsize\fR output.
 		name {overflow}
 		type {=inData}
 		desc {
-Arrival data that can not be queued due to capacity limit.
+Arrival data that cannot be queued due to capacity limit.
 		}
 	}
 
