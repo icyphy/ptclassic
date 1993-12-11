3faf01d44c208afdcfd1b51757f776a93485d3d0
Christopher Brooks <cxh@eecs.berkeley.edu>
1993-12-11 22:31:50 +0000

    simplified sending of Matrix using << *matrix

--- ptclassic/src/domains/sdf/matrix/stars/SDFPackInt_M.pl
+++ ptclassic/src/domains/sdf/matrix/stars/SDFPackInt_M.pl
@@ -33,8 +33,7 @@ defstar {
   go {
     // collect inputs and put into the matrix
     IntMatrix *matrix = new IntMatrix(int(numRows),int(numCols),input);
-    Envelope pkt(*matrix);
-    output%0 << pkt;
+    output%0 << *matrix;
   }
 }
 
