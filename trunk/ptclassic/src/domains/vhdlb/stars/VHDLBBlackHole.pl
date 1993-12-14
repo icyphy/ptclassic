933b4515874c31afe3c465c3128c80acd3540cbc
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1993-12-14 22:36:51 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/vhdlb/stars/VHDLBBlackHole.pl
@@ -0,0 +1,20 @@
+defstar {
+	name { BlackHole }
+	domain {VHDLB}
+	desc {
+Discards all inputs
+	}
+        version { $Id$ }
+	author { E. A. Lee }
+	copyright {
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { VHDLB main library }
+	input {
+		name { input }
+		type { int }
+	}
+}
