bb3e0269fc624e477d2b5427b1a542665231936f
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
1993-12-16 13:00:54 +0000

    Initial revision

---
+++ ptclassic/src/domains/de/stars/DEEtherRec.pl
@@ -0,0 +1,113 @@
+defstar {
+	name {EtherRec}
+	domain {DE}
+	derivedfrom { DEEther }
+	version { $Id$ }
+	author { E. A. Lee  and T. Parks }
+	copyright {
+Copyright (c) 1994 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { DE main library }
+	desc {
+Receiver for a shared medium when received type is float.
+	}
+	explanation {
+This star is derived from
+.c Ether.
+Please see the manual entry for that star for a basic introduction.
+.pp
+This star implements a receiver for a shared medium when the received
+particle is a simple floating point number.
+A similar star could be made for other
+.c Particle
+types, so this should be viewed as a concept demonstration only.
+.pp
+The star will produce an output only if some transmitter
+(which must be also derived from
+.c EtherSend )
+uses the same medium and transmits to this receivers address.
+The time at which the output is produced is specified by the transmitter
+when it transfers the particle by calling the public
+method
+.c schedReception
+defined in this star.
+That method takes two arguments, a
+.c Particle
+reference, and a
+.c double
+giving the time at which the particle should be sent to the output.
+	}
+	output {
+	    name { recData }
+	    type { float }
+	    desc { Received data. }
+	}
+	state {
+	    name { recName }
+	    type { string }
+	    default { "0" }
+	    desc { Unique name of the receiver. }
+	}
+	private {
+	    // Store a received particle.
+	    Particle* recdData;
+
+	    // Use a local private variable to ensure that received
+	    // data is output exactly once.
+	    int dataValid;
+
+	    // Flag used to identify the first firing
+	    int firstFiring;
+	}
+	method {
+	    name { schedReception }
+	    access { public }
+	    arglist { "(Particle& data, double firingTime)" }
+	    type { void }
+	    code {
+		if (dataValid)
+		    Error::warn(*this,"Overwriting previously received data");
+		recdData = data.clone();
+		refireAtTime(firingTime);
+		feedbackOut.sendData();
+		dataValid = 1;
+	    }
+	}
+	setup {
+	    DEEther::setup();
+
+	    firstFiring = 1;
+
+	    if(!registerReceiver(recName, this)) {
+		Error::warn(*this,
+	"Additional receivers with the same name will be ignored. Name is ",
+                        recName);
+	    }
+
+	    // Initialize the local flag to indicate that we don't now have
+	    // valid data pending output.
+	    dataValid = 0;
+	}
+	go {
+	    // Ignore the time zero event that is automatically scheduled
+	    // by the base class, RepeatStar
+	    if (firstFiring) {
+		firstFiring = 0;
+		return;
+	    }
+
+	    if (dataValid) {
+		// Output the stored data
+		recData.put(arrivalTime) = *recdData;
+
+		// Release the particle
+		recdData->die();
+
+		// Mark that we no longer have valid data waiting for output
+		dataValid = 0;
+	    }
+	}
+}
---
+++ ptclassic/src/domains/de/stars/DEEtherSend.pl
@@ -0,0 +1,117 @@
+defstar {
+	name {EtherSend}
+	domain {DE}
+	derivedfrom { DEEther }
+	version { $Id$ }
+	author { E. A. Lee and T. Parks }
+	copyright {
+Copyright (c) 1994 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { DE main library }
+	desc {
+Transmitter for a shared medium.
+	}
+	explanation {
+This star is derived from
+.c Ether.
+Please see the manual entry for that star for a basic introduction.
+.pp
+This star implements a transmitter for a shared medium.
+Any type of
+.c Particle
+can be transmitted.
+If the "address" input is a non-negative integer, then the transmitter
+looks for a receiver that shares the medium that has a matching address.
+If it is found, a copy of the Particle is sent to the receiver.
+The receiver is told to output that particle after a certain
+elapsed time, given by the most recent value that arrived on
+the "duration" input.
+If no such particle has arrived, a duration of zero is used.
+If the address is a negative integer, then the data particle
+is broadcast to all receivers on the medium.
+.pp
+The transmitter "occupies" the medium for the specified duration.
+A collision occurs if the medium is occupied when a transmission is
+requested.  In this case,
+the data to be transmitted is sent to the "collision" output.
+	}
+	input {
+	    name { address }
+	    type { int }
+	    desc { Address of the receiver. Negative for broadcast. }
+	}
+	input {
+	    name { sendData }
+	    type { anytype }
+	    desc { Data to transmit. }
+	}
+	input {
+	    name { duration }
+	    type { float }
+	    desc { Time delay to receiver, and time occupying the medium. }
+	}
+	output {
+	    name { collision }
+	    type { = sendData }
+	    desc { Indicate the medium is occupied. }
+	} 
+	ccinclude { "DEEtherRec.h" }
+	private {
+	    double latestDuration;
+
+	    // The address is stored as a String.
+	    StringList latestAddress;
+
+	    // flag determining that the next particle(s) should be broadcast
+	    int broadcast;
+	}
+	setup {
+	    DEEther::setup();
+	    broadcast = 0;
+	    latestDuration = 0.0;
+	}
+	go {
+	    if (duration.dataNew) {
+		latestDuration = double(duration.get());
+	    }
+	    if (address.dataNew) {
+		Particle& adp = address.get();
+		if (int(adp) < 0) broadcast = 1;
+		else broadcast = 0;
+		latestAddress = (address.get()).print();
+	    }
+	    if (sendData.dataNew) {
+		Particle& pp = sendData.get();
+		if (med->occupiedAt(arrivalTime)) {
+		    collision.put(arrivalTime) = pp;
+		} else {
+		    if (!broadcast) {
+			DEEtherRec* receiveStar =
+				med->findReceiver(latestAddress);
+			if (receiveStar == 0) {
+			    Error::warn(*this,
+			    "Attempt to transmit to non-existant receiver ",
+			    latestAddress);
+			    return;
+			}
+			receiveStar->schedReception(pp,
+				arrivalTime + latestDuration);
+		    } else {
+		        // handle broadcast case
+			HashTableIter stars(med->receivers);
+			HashEntry* starEntry;
+			while ((starEntry = stars++) != 0) {
+			    DEEtherRec* receiveStar =
+				(DEEtherRec*)(starEntry->value());
+			    receiveStar->schedReception(pp,
+				arrivalTime + latestDuration);
+			}
+		    }
+		    med->occupyUntil(arrivalTime + latestDuration);
+		}
+	    }
+	}
+}
