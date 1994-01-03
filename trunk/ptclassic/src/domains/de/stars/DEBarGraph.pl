2b0c149c57d9d24ed06e99950229d06c1365e9ed
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-03 11:15:37 +0000

    Made the star accept multiple inputs

--- trunk/ptclassic/src/domains/de/stars/DEBarGraph.pl
+++ trunk/ptclassic/src/domains/de/stars/DEBarGraph.pl
@@ -1,22 +1,22 @@
 defstar {
 	name { BarGraph }
 	domain { DE }
-	desc { Generate a plot with the xgraph program. }
+	desc { Generate bar chart plots with the xgraph program. }
 	version { $Id$ }
 	author { Brian Evans and Edward Lee }
 	copyright {
-Copyright (c) 1993 The Regents of the University of California.
+Copyright (c) 1993 and 1994 The Regents of the University of California.
 All rights reserved.
 See the file ~ptolemy/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
 	}
 	location { DE main library }
 	explanation {
-Draws a bar graph with the xgraph function.
+Draws one or more bar graphs with the xgraph function.
 It is assumed that "xgraph" is on your path, or this will not work!!!
 	}
 	seealso {Xgraph XMgraph xgraph Xhistogram }
-	input {
+	inmulti {
 		name { input }
 		type { anytype }
 	}
@@ -45,32 +45,48 @@ It is assumed that "xgraph" is on your path, or this will not work!!!
 		desc { Command line options for a bar graph in xgraph.}
 	}
 	hinclude { "Display.h" }
-	public {
-		float lastDataValue;
-		int firstPoint; 
-	}
 	protected {
 		XGraph graph;
+		float *lastDataValues;
+		int *firstPointFlags;
+		int numports;
 	}
 	setup {
-	 	lastDataValue = base;
-		firstPoint = TRUE;
-		graph.initialize(this, 1, (const char*) options,
-				    (const char*) title,
-				    (const char*) saveFile);
+		numports = input.numberPorts();
+		LOG_NEW; lastDataValues = new float[ numports + 1 ];
+		LOG_NEW; firstPointFlags = new int[ numports + 1];
+		for ( int port = 1; port <= numports; port++ ) {
+	 	  lastDataValues[port] = base;
+		  firstPointFlags[port] = TRUE;
+		}
+		graph.initialize(this,
+				 input.numberPorts(),
+				 (const char*) options,
+				 (const char*) title,
+				 (const char*) saveFile);
 	}
 	go {
-		float data = float(input.get());
-		if ( firstPoint )
-		  firstPoint = FALSE;
-		else
-		  graph.addPoint(arrivalTime, lastDataValue);
-		graph.addPoint(arrivalTime, base);
-		graph.addPoint(arrivalTime, data);
-		lastDataValue = data;
+		InDEMPHIter nextp(input);
+		for ( int port = 1; port <= numports; port++ ) {
+		  InDEPort *iportp = nextp++;
+		  if ( iportp->dataNew ) {
+		    float data = float(iportp->get());
+		    if ( firstPointFlags[port] )
+		      firstPointFlags[port] = FALSE;
+		    else
+		      graph.addPoint(port, arrivalTime, lastDataValues[port]);
+		    graph.addPoint(port, arrivalTime, base);
+		    graph.addPoint(port, arrivalTime, data);
+		    lastDataValues[port] = data;
+		  }
+		  iportp->dataNew = FALSE;
+		}
 	}
 	wrapup {
-		graph.addPoint(arrivalTime, lastDataValue);
+		for ( int port = 1; port <= numports; port++ )
+		  graph.addPoint(port, arrivalTime, lastDataValues[port]);
 		graph.terminate();
+		LOG_DEL; delete [] lastDataValues;
+		LOG_DEL; delete [] firstPointFlags;
 	}
 }
