c780cee8f64e88fd3a940ffbde839e9104fd4b75
mchen <mchen@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-04 16:35:13 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/matrix/stars/SDFCxToFix_M.pl
@@ -0,0 +1,70 @@
+defstar {
+  name { CxToFix_M }
+  domain { SDF }
+  desc {
+    Takes an input ComplexMatrix and converts it to an FixMatrix.  This
+    is done by constructing a new FixMatrix with the given precision.
+    Each entry of the ComplexMatrix is cast using the given masking function
+    and then copied to the new matrix.
+  }
+  version { $Id$ }
+  author { Mike J. Chen }
+  copyright { 1993 The Regents of the University of California }
+  location  { SDF matrix library }
+  input {
+    name { input }
+    type { COMPLEX_MATRIX_ENV }
+  }
+  output {
+    name { output }
+    type { FIX_MATRIX_ENV }
+  }
+  defstate {
+    name { FixPrecision }
+    type { string }
+    default { "4.14" }
+    desc {
+Precision of the entries in the FixMatrix.  The floating-point values
+of the ComplexMatrix are converted to this precision. }
+  }
+  defstate {
+    name { masking }
+    type { string }
+    default { "truncate" }
+    desc {
+Masking method.  This parameter is used to specify the way the floating-point
+number is masked for casting to the fixed-point notation.  The keywords are:
+"truncate" (default), "round". }
+  }
+  ccinclude { "Matrix.h" }
+  protected {
+    const char* Precision;
+    const char* Masking;
+    int intBits;
+    int length;
+  }
+  setup {
+    Precision = FixPrecision;
+    Masking = masking;
+    intBits = Fix::get_intBits(FixPrecision);
+    length = Fix::get_length(FixPrecision);
+  }
+  go {
+    // get input
+    Envelope inpkt;
+    (input%0).getMessage(inpkt);
+    const ComplexMatrix& matrix = *(const ComplexMatrix *)inpkt.myData();
+
+    // do conversion using copy constructor
+    if(strcmp(Masking, "truncate") == 0) {
+      FixMatrix& result = *(new FixMatrix(matrix,intBits,length,Fix::mask_truncate));
+      output%0 << result;
+    }
+    else if(strcmp(Masking, "round") == 0) {
+      FixMatrix& result = *(new FixMatrix(matrix,intBits,length,Fix::mask_truncate_round));
+      output%0 << result;
+    }
+    Error::abortRun(*this, ": not a valid function for masking");
+  }
+}
+
---
+++ trunk/ptclassic/src/domains/sdf/matrix/stars/SDFCxToInt_M.pl
@@ -0,0 +1,36 @@
+defstar {
+  name { CxToInt_M }
+  domain { SDF }
+  desc {
+    Takes an input ComplexMatrix and converts it to an IntMatrix.  This
+    is done by using the cast conversion method of the ComplexMatrix class.
+    The conversion results in an IntMatrix that has entries which are the
+    int of the absolute value of each corresponding entry of the 
+    ComplexMatrix being converted.  
+    I.e. IntMatrix.entry(i) = (int)abs(ComplexMatrix.entry(i))
+  }
+  version { $Id$ }
+  author { Mike J. Chen }
+  copyright { 1993 The Regents of the University of California }
+  location  { SDF matrix library }
+  input {
+    name { input }
+    type { COMPLEX_MATRIX_ENV }
+  }
+  output {
+    name { output }
+    type { INT_MATRIX_ENV }
+  }
+  ccinclude { "Matrix.h" }
+  go {
+    // get input
+    Envelope inpkt;
+    (input%0).getMessage(inpkt);
+    const ComplexMatrix& matrix = *(const ComplexMatrix *)inpkt.myData();
+
+    // do conversion using copy constructor
+    IntMatrix& result = *(new IntMatrix(IntMatrix(matrix)));
+    output%0 << result;
+  }
+}
+
