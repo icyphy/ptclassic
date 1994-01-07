f599a3efad7f39740e7558fbd11090a68ed9007d
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-07 09:37:34 +0000

    tuning the documentation

--- trunk/ptclassic/src/domains/de/stars/DEStopTimer.pl
+++ trunk/ptclassic/src/domains/de/stars/DEStopTimer.pl
@@ -5,6 +5,9 @@ defstar {
 Generate an output at the stopTime of the
 DEScheduler under which this block is running.
 This can be used to force actions at the end of a simulation.
+Within a wormhole, it can used to force actions at the end
+of each invocation of the wormhole.
+An input event is required to enable the star.
 	}
 	version { $Id$}
 	author { Soonhoi Ha }
@@ -16,10 +19,26 @@ limitation of liability, and disclaimer of warranty provisions.
 	}
 	location { DE main library }
 	explanation {
-This block generates an output at the stopTime of the DEScheduler controlling 
-the block.  It is useful for clocking the input of the Sampler
-or statistical Blocks in order to observe their final outputs at the end
-of the simulation.
+Once enabled by an input event,
+this star generates an output at the stopTime of the DEScheduler controlling 
+the star.  It is useful, for example, to clock the
+.c Statistics
+star in order to observe its final outputs at the end
+of a simulation.
+It also has interesting uses within a wormhole.
+If it enabled during a particular invocation of the wormhole scheduler,
+then it will generate an event with time stamp equal to the time
+at which this scheduler will return control to the calling system.
+Suppose, for example, that a DE subsystem is used within an SDF
+system.  The DE subsystem is required to produce output particles,
+so that the SDF model of computation is obeyed.
+The
+.c StopTimer
+star can be used to produce the output events,
+or to trigger production of those output events, by
+clocking the
+.c Sampler
+star for example.
 	}
 	input {
 		name { input }
