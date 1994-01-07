cb08032a1c8df3173ecefcafa16b8e6b84d2ee14
alao <alao@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-07 10:10:16 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/de/atm/stars/DEToBusFl.pl
@@ -0,0 +1,48 @@
+
+defstar {
+        name { ToBusFl }
+	domain { DE }
+	desc { Converts from MPH to multiple single ports or vice-versa for floats }
+
+	version {$Id$}
+	author { Allen Lao }
+
+	copyright {
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+
+	location { ATM demo library }
+
+	inmulti {
+		name {input}
+		type {float}
+	}
+
+	outmulti {
+		name {output}
+		type {float}
+	}
+
+	go {
+
+	   completionTime = arrivalTime;
+
+	   InDEMPHIter nextp(input);
+	   OutDEMPHIter nextq(output);
+
+	   InDEPort* iport;
+	   OutDEPort* oport;
+
+	   while ((iport = nextp++) != 0) {
+		oport = nextq++;
+		if(iport->dataNew)
+		    oport -> put(completionTime) = iport -> get();
+	   }
+
+	}
+
+}   // end defstar
+
