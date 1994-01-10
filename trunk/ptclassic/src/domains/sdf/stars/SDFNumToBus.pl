f615b1e29192226c93216b76a62e9c6d2ed5cdb6
kalavade <kalavade@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-10 19:58:43 +0000

    removed state "bits", cleaned up code

--- trunk/ptclassic/src/domains/sdf/stars/SDFNumToBus.pl
+++ trunk/ptclassic/src/domains/sdf/stars/SDFNumToBus.pl
@@ -22,45 +22,46 @@ limitation of liability, and disclaimer of warranty provisions.
 		name {output}
 		type {int}
 	}
-	defstate {
-		name {bits}
-		type {int}
-		default {"7"}
-		desc {number of bits to output}
+	protected {
+		int bits;
+		int *binary;
 	}
+        constructor {
+                binary = 0;
+        }
+        destructor {
+                LOG_DEL; delete binary;
+        }
+        setup {
+                bits = output.numberPorts();
+                LOG_DEL; delete binary;
+                LOG_NEW; binary = new int[bits];
+        }
 	go {
-		int i =0 ;
-		LOG_NEW; int *binary = new int[bits+1];
-	
+		int i =0;
 	    	int in = int (input%0);
 
 /* 	convert the integer to binary and store it into an array
 	to be output on the bus, msb is the sign bit
 */
 
-	if(in<0){
-	  binary[bits-1]=1;
-	// in = in + 2^bits
-	  in= ( 2 << (bits-1) ) + in;
-		while (i< (bits-1)) {
+		if(in<0)	{binary[bits-1] = 1; in += 2 << (bits-1);}
+		else		binary[bits-1] = 0;
+
+		while (i< (bits-1)) 
+		{
  	     		binary[i++] = in & 0x1;
 			in = in >> 1;
-				    }
 		}
-	else {
-	  binary[bits-1]=0;
-		while (i< (bits-1) ) {
- 	     		binary[i++] = in & 0x1;
-			in = in >> 1;
-					}
-	     }
+
 		MPHIter nextp(output);
 		i=0;
 		PortHole* p;
-		while ((p=nextp++) != 0){
+		while ((p=nextp++) != 0)
+		{
 			(*p)%0  << binary[i];
 			i++;
-					}
-		LOG_DEL; delete [] binary;
-	    }
+		}
+
+	    } //go
 }
