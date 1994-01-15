3b89d244910343a9508bfc3413d904aa7d8e72d9
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-15 18:10:22 +0000

    Documentation

--- ptclassic/src/domains/sdf/dsp/stars/SDFFFTCx.pl
+++ ptclassic/src/domains/sdf/dsp/stars/SDFFFTCx.pl
@@ -2,11 +2,12 @@ defstar {
 	name {FFTCx}
 	domain {SDF}
 	desc {
-Compute the discrete-time Fourier transform of a complex input using the
-fast Fourier transform (FFT) algorithm. The parameter "order" (default 8)
-is the log base 2 of the transform size. The parameter "size" (default 256)
-is the number of samples read (<= 2^order). The parameter "direction"
-(default 1) is 1 for the forward, -1 for the inverse FFT.
+Compute the discrete Fourier transform of a complex input using the
+fast Fourier transform (FFT) algorithm.
+The parameter "order" (default 8) is the log base 2 of the transform size.
+The parameter "size" (default 256) is the number of samples read (<= 2^order).
+The parameter "direction" (default 1) is 1 for the forward and
+-1 for the inverse FFT.
 	}
 	version {$Id$}
 	author { J. T. Buck }
@@ -35,10 +36,17 @@ For example, to compute just one FFT, only one iteration should be run.
 .lp
 \fBBugs\fR: the routine currently used (from Gabriel) recomputes trig
 .Ir "Gabriel"
-functions for each term, instead of using a table.  Instead,
-FFTCx::setup() should compute a table of appropriate size to save
-time.  This has no effect, obviously, if only one transform
-is performed.
+functions for each term, instead of using a table.
+Instead, FFTCx::setup() should compute a table of appropriate size
+in order to save time.
+This approach does not offer any improvement, obviously, if only
+one transform is performed.
+.ID "Oppenheim, A. V."
+.ID "Schafer, R. W."
+.UH REFERENCES
+.ip [1]
+A. V. Oppenheim and R. W. Schafer, \fIDiscrete-Time Signal Processing\fR,
+Prentice-Hall: Englewood Cliffs, NJ, 1989.
 	}
 	input {
 		name {input}
@@ -64,7 +72,7 @@ is performed.
 		name {direction}
 		type {int}
 		default {1}
-		desc { = 1 for forward, = -1 for inverse. }
+		desc { Equals 1 for forward, or -1 for inverse, transform. }
 	}
 	protected {
 		double* data;
