39c773716a6efef348176e5bf4e8906fe3119625
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-17 17:55:56 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/de/tcltk/stars/DETkShowValues.pl
@@ -0,0 +1,59 @@
+defstar {
+    name {TkShowValues}
+    domain {DE}
+    derivedFrom { TclScript }
+    desc {
+Display the values of the inputs in textual form.
+The print method of the input particles is used,
+so any data type can be handled, although the space
+allocated on the screen may need to be adjusted.
+    }
+    version { $Id$ }
+    author { Brian Evans and Edward Lee }
+    copyright {
+Copyright (c) 1993 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
+    location { DE tcltk library }
+    explanation {
+This star uses the "print()" method of the input particles to create
+the display.
+The width of the display window is appropriate for integer, float, and
+complex particles.
+Particles that require more space to display may create problems.
+    }
+    hinclude { "ptk.h" }
+    defstate {
+        name {label}
+        type {string}
+        default {"Inputs to the TkShowValues star:"}
+        desc {A label to put on the display}
+    }
+    defstate {
+        name {put_in_control_panel}
+        type {int}
+        default { "YES" }
+        desc {Specify to put bars in the control panel (vs. their own window)}
+    }
+    defstate {
+        name {wait_between_outputs}
+        type {int}
+        default { "NO" }
+        desc {Specify whether to wait for user input between output values}
+    }
+    setup {
+        if (output.numberPorts() > 0) {
+            Error::abortRun(*this, "Outputs not supported");
+            return;
+        }
+        tcl_file = "$PTOLEMY/src/domains/sdf/tcltk/stars/tkShowValues.tcl";
+
+        DETclScript::setup();
+    }
+    constructor {
+        output.setAttributes(P_HIDDEN);
+        tcl_file.clearAttributes(A_SETTABLE);
+    }
+}
