ac1ff3f858ed66f6aa267c4a9de071151cae14a4
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-19 17:22:12 +0000

    Moved setup code from go() to setup()

--- trunk/ptclassic/src/domains/sdf/stars/SDFIntToFix.pl
+++ trunk/ptclassic/src/domains/sdf/stars/SDFIntToFix.pl
@@ -26,41 +26,41 @@ limitation of liability, and disclaimer of warranty provisions.
                 type { string }
                 default { "16.0" }
                 desc {
-Precision of the output, in bits.  The integer number is cast to a double
-and then converted to this precision.  If the value of the double cannot
-be represented by the number of bits specified in the precision parameter,
-then a error message is given. }
+Precision of the output, in bits.
+The integer number is cast to a double and then converted to this precision.
+If the value of the double cannot be represented by the number of bits
+specified in the precision parameter, then a error message is given.
+		}
         }
         defstate {
                 name { masking }
                 type { string }
                 default { "truncate" }
                 desc {
-Masking method.  This parameter is used to specify the way the complex number
-converted to a double is masked for casting to the fixed-point notation. The
-keywords are: "truncate" (default), "round". }
+Masking method.
+This parameter is used to specify the way the complex number converted
+to a double is masked for casting to the fixed-point notation.
+The keywords are: "truncate" (the default) and "round".
+		}
         }
         protected {
-                const char* OutputPrecision;
-                const char* Masking;
-                int outIntBits;
-                int outLen;
+		Fix out;
         }
         setup {
-                OutputPrecision = outputPrecision;
-                Masking = masking;
-                outIntBits = Fix::get_intBits(OutputPrecision);
-                outLen = Fix::get_length(OutputPrecision);
-        }
-	go {
-                Fix out(outLen, outIntBits, (double)((int)(input%0)));
+                const char* Masking = masking;
+                const char* OutputPrecision = outputPrecision;
+                int outIntBits = Fix::get_intBits(OutputPrecision);
+                int outLen = Fix::get_length(OutputPrecision);
+                out = Fix(outLen, outIntBits);
                 if(strcmp(Masking, "truncate") == 0)
                   out.Set_MASK(Fix::mask_truncate);
                 else if (strcmp(Masking, "round") == 0)
                   out.Set_MASK(Fix::mask_truncate_round);
-                else {
+                else
                   Error::abortRun(*this, ": not a valid function for masking");
-                }
+        }
+	go {
+		out = (double) ((int)(input%0));
                 output%0 << out;
 	}
 }
