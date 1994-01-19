b967dfa3b9708ee411556c3dd6fc9b8b527d192c
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-19 13:47:56 +0000

    Initial revision

---
+++ ptclassic/src/domains/vhdlb/stars/VHDLBXnor3.pl
@@ -0,0 +1,40 @@
+defstar {
+	name { Xnor3 }
+	domain {VHDLB}
+	desc {
+Output the logical xnor of the three inputs with the given delay.
+	}
+        version { $Id$ }
+	author { M. C. Williamson }
+	copyright {
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { VHDLB main library }
+	input {
+		name { input1 }
+		type { int }
+	}
+	input {
+		name { input2 }
+		type { int }
+	}
+	input {
+		name { input3 }
+		type { int }
+	}
+	output {
+		name { output }
+		type { int }
+	}
+        defstate {
+                name { delay }
+	        type { int }
+	        default { 1 }
+	        desc { The propagation delay, in nsec }
+	}
+	go {
+	}
+}
