d9b8f5c2d3d2a4845ae23e7135998702d48a80ca
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-22 10:16:26 +0000

    Fixed typo in last change, we were not looking	far enough down the directory tree for init.tcl and tk.tcl.

--- trunk/ptclassic/lib/tcl/pigilib.tcl
+++ trunk/ptclassic/lib/tcl/pigilib.tcl
@@ -38,13 +38,13 @@ proc pigilib_init_env {} {
     set ptolemy $env(PTOLEMY)
     #set env(TCL_LIBRARY) $env(PTOLEMY)/tcl/tcl[info tclversion]/lib
     #set env(TK_LIBRARY) $env(PTOLEMY)/tcl/tk$tk_version/lib
-    set env(TCL_LIBRARY) $env(PTOLEMY)/tcltcl/tcl/lib
-    set env(TK_LIBRARY) $env(PTOLEMY)/tcltk/tk/lib
+    set env(TCL_LIBRARY) $env(PTOLEMY)/tcltcl/tcl/lib/tcl
+    set env(TK_LIBRARY) $env(PTOLEMY)/tcltk/tk/lib/tk
     set tk_library $env(TK_LIBRARY)
     #uplevel #0 source $env(PTOLEMY)/tcl/tcl[info tclversion]/lib/init.tcl
     #uplevel #0 source $env(PTOLEMY)/tcl/tk$tk_version/lib/tk.tcl
-    uplevel #0 source $env(PTOLEMY)/tcltk/tcl/lib/init.tcl
-    uplevel #0 source $env(PTOLEMY)/tcltk/tk/lib/tk.tcl
+    uplevel #0 source $env(PTOLEMY)/tcltk/tcl/lib/tcl/init.tcl
+    uplevel #0 source $env(PTOLEMY)/tcltk/tk/lib/tk/tk.tcl
 
     set tcl_prompt1 "puts -nonewline stdout {pigi> }"
     set tcl_prompt2 "puts -nonewline stdout {pigi? }"
