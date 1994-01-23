94ec56cebb080cb5c2da46d2daceb562c4137089
ptdesign <ptdesign@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-23 14:00:55 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/de/stars/DEPulseGen.pl
@@ -0,0 +1,49 @@
+defstar
+{
+  name { PulseGen }
+  derivedFrom {RepeatStar}
+  domain {DE}
+  desc {
+This star generates events with specified values at specified moments.
+The events are specified in the "value" array, which consists of
+time-value pairs, given in the syntax of complex numbers.
+  }
+  explanation { none }
+  version { $Revision$ $Id$ }
+  author { Stefan De Troch (of IMEC) }
+  copyright {
+Copyright (c) 1994 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+  }
+  location { }
+  output {
+    name { output }
+    type { float }
+  }
+  defstate {
+    name { value }
+    type { complexarray }
+    default { "(0,1) (1,0)" }
+  }
+  protected {
+    int pos;
+  }
+  setup {
+    pos = 0;
+  }
+  go {
+    // first event comes at time 0
+    // check whether the first event is too early
+    if (arrivalTime >= real(value[pos]))
+    {
+      output.put(real(value[pos])) << double(imag(value[pos]));
+      pos++;
+    }
+    if (pos < value.size())
+    {
+      refireAtTime(real(value[pos]));
+    }
+  }
+}
