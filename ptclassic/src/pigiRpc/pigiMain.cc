6aa0d3cfd17a7e0b9a546c02f2ad2aba6d1992fe
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-01-25 23:26:10 +0000

    Added #include <string.h> for CC compile.

--- ptclassic/src/pigiRpc/pigiMain.cc
+++ ptclassic/src/pigiRpc/pigiMain.cc
@@ -2,7 +2,7 @@
 SCCS version identification
 $Id$
 
-Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+Copyright (c) 1990-1994 The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
@@ -30,6 +30,7 @@ ENHANCEMENTS, OR MODIFICATIONS.
  Main program for pigiRpc.
 
 ********************************************************************/
+#include <string.h>
 
 extern ptkConsoleWindow();
 
