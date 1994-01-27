dea1d49515adb1fd2680774642b3f125172885a9
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-27 10:12:31 +0000

    Initial revision

---
+++ ptclassic/src/domains/vhdlb/stars/VHDLBThrough.pl
@@ -0,0 +1,26 @@
+defstar {
+	name { Through }
+	domain {VHDLB}
+	desc {
+Output the input value.
+	}
+        version { $Id$ }
+	author { Edward A. Lee }
+	copyright {
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { VHDLB main library }
+	input {
+		name { input }
+		type { int }
+	}
+	output {
+		name { output }
+		type { int }
+	}
+	go {
+	}
+}
