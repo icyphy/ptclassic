7f53a2a6d4e78bca41908cca49b06b0efb7bcf94
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-31 11:40:16 +0000

    Cleaned up format; added new copyright for Ptolemy 0.5

--- ptclassic/src/domains/de/stars/DETimeoutStar.pl
+++ ptclassic/src/domains/de/stars/DETimeoutStar.pl
@@ -3,47 +3,42 @@ defstar
     name { TimeoutStar }
     derivedFrom { RepeatStar }
     domain { DE }
-    descriptor
-    {
+    descriptor {
 This is the base class for stars that check time-out conditions.
 The methods "set", "clear", and "expired" are provided for
 setting and testing the timer.
     }
     version { $Id$ }
     author { T. M. Parks }
-    copyright
-    {
-Copyright 1992 The Regents of the University of California.
+    copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
 All rights reserved.
-See the file ~ptolemy/copyright for copyright notice,
+See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
     }
     location { DE main library }
-    explanation
-    {
-The \fIset\fP method resets and starts a timer.  Invoking \fIset\fP
-again will reset the timer and restart it.  Invoking \fIclear\fP
-stops the timer.  The \fIexpired\fP method indicates whether or not the
-timer has reached \fItime-out\fR.
+    explanation {
+The \fIset\fP method resets and starts a timer.
+Invoking \fIset\fP again will reset the timer and restart it.
+Invoking \fIclear\fP stops the timer.
+The \fIexpired\fP method indicates whether or not the timer
+has reached \fItime-out\fR.
     }
 
-    state
-    {
+    state {
 	name { timeout }
 	type { float }
 	default { 1.0 }
 	descriptor { Duration of the timeout. }
     }
 
-    private
-    {
+    private {
 	int safe : 1;
 	int expireFlag : 1;
 	double deadline;
     }
 
-    method
-    {
+    method {
 	name { set }
 	type { void }
 	code
@@ -55,48 +50,39 @@ timer has reached \fItime-out\fR.
 	}
     }
 
-    method
-    {
+    method {
 	name { clear }
 	type { void }
-	code
-	{
+	code {
 	    safe = TRUE;
 	    expireFlag = FALSE;
 	}
     }
 
-    method
-    {
+    method {
 	name { expired }
 	type { int }
-	code
-	{
+	code {
 	    return expireFlag;
 	}
     }
 
-    method
-    {
+    method {
 	name { initialize }
 	type { void }
 	access { public }
-	code
-	{
+	code {
 	    clear();
 	    DERepeatStar::initialize();
 	}
     }
 
-    method
-    {
+    method {
 	name { run }
 	type { int }
 	access { public }
-	code
-	{
-	    if (canGetFired())	// time-out event
-	    {
+	code {
+	    if (canGetFired()) { 	// time-out event
 		if (!safe && arrivalTime == deadline)
 		    expireFlag = TRUE;
 	    }
