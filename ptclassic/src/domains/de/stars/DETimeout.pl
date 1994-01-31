201fac5e34315f4623332798c981b4686f99c4db
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-01-31 11:31:45 +0000

    Cleaned up format of file; added copyright for Ptolemy 0.5

--- ptclassic/src/domains/de/stars/DETimeout.pl
+++ ptclassic/src/domains/de/stars/DETimeout.pl
@@ -1,10 +1,8 @@
-defstar
-{
+defstar {
     name { Timeout }
     derivedFrom { TimeoutStar }
     domain { DE }
-    descriptor
-    {
+    descriptor {
 This star detects time-out conditions and generates an alarm if too
 much time elapses before resetting or stopping the timer.  Events
 arriving on the "Set" input reset and start the timer.  Events arriving
@@ -14,31 +12,27 @@ arrive within "timeout" time units of the most recent "Set", then that
     }
     version { $Id$ }
     author { T. M. Parks }
-    copyright
-    {
-Copyright (C) 1992-1994 The Regents of the University of California.
+    copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
 All rights reserved.
-See the file ~ptolemy/copyright for copyright notice,
+See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
     }
     location { DE main library }
 
-    input
-    {
+    input {
 	name { Set }
 	type { anyType }
 	desc { Reset and start the timer. }
     }
 
-    input
-    {
+    input {
 	name { Clear }
 	type { anyType }
 	desc { Stop the timer. }
     }
 
-    output
-    {
+    output {
 	name { alarm }
 	type { = Set }
 	desc
@@ -48,22 +42,18 @@ The event which last started the timer is output.
 	}
     }
 
-    go
-    {
-	if (Clear.dataNew)
-	{
+    go {
+	if (Clear.dataNew) {
 	    Clear.dataNew = FALSE;
 	    clear();
 	}
 
-	if (Set.dataNew)
-	{
+	if (Set.dataNew) {
 	    Set.dataNew = FALSE;
 	    set();
 	}
 
-	if (expired())
-	{
+	if (expired()) {
 	    // Output the event which started the timer.
 	    alarm.put(arrivalTime) = Set%0;
 	}
