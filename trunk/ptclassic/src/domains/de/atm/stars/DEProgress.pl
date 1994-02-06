e686ed328412a33234b61094bcfb0a249282384a
alao <alao@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-02-06 17:26:31 +0000

    revised comments, copyright

--- trunk/ptclassic/src/domains/de/atm/stars/DEProgress.pl
+++ trunk/ptclassic/src/domains/de/atm/stars/DEProgress.pl
@@ -3,25 +3,24 @@ defstar {
 	name { Progress }
 	domain { DE }
 	desc {
-Print to <stdout> current time of simulation, every "update" time units,
-pass particle through with no latency
+Periodically print to <stdout> current time of simulation
         }
 
 	version {$Id$}
 	author { Allen Y. Lao }
 
 	copyright { 
-Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+Copyright (c) 1990-1994 The Regents of the University of California.
 All rights reserved.
-See the file ~ptolemy/copyright for copyright notice,
+See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
 }
 	location { ATM demo library }
 
 	explanation {
 This star is used to print to <stdout> the progress of a simulation
-running in the DE domain, the state 'step' indicates that for the particle
-passing into the star's input at integral multiples of 'step', a message
+running in the DE domain, the state \fIstep\fR indicates that for the particle
+passing into the star's input at integral multiples of \fIstep\fR, a message
 will be printed to indicate that system time has reached that point.
         }
 
