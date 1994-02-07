fbc0f1345c227c7a59c85a54da9a18cccbbfc8d9
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-02-07 18:29:59 +0000

    simplified state, port, and block list environments

--- ptclassic/doc/elisp/ptinfo-fmt.el
+++ ptclassic/doc/elisp/ptinfo-fmt.el
@@ -8,46 +8,59 @@
 (put 'Contrib 'latexinfo-format 'latexinfo-format-author)
 
 ;
+; Command for beginning a description, such as statelist, blocklist.
+;
+
+(defvar ptinfo-index-suffix nil)
+
+(defun ptinfo-index-description ()
+  (setq ptinfo-index-suffix (latexinfo-parse-line-arg))
+  (latexinfo-discard-command)
+  (save-excursion (insert "\\begin{description}\n")))
+
+;
 ; Commands for formatting a statelist environment
 ;
-(put 'statelist 'latexinfo-format 'latexinfo-statelist)
-(put 'statelist 'latexinfo-end 'latexinfo-end-statelist)
-(put 'statelist 'latexinfo-item 'latexinfo-state-item)
-(put 'state 'latexinfo-format 'latexinfo-item)
+(put 'statelist 'latexinfo-format 'ptinfo-index-description)
+(put 'statelist 'latexinfo-end 'latexinfo-end-description)
+(put 'state 'latexinfo-format 'ptinfo-state)
 
-(defun latexinfo-statelist ()
-  (latexinfo-begin-list 'statelist ""))
-
-(defun latexinfo-end-statelist ()
-  (latexinfo-end-list 'statelist))
-
-(defun latexinfo-state-item ()
+(defun ptinfo-state ()
   (let ((type (latexinfo-parse-line-arg))
         (name (latexinfo-parse-line-arg))
         (default (latexinfo-parse-line-arg)))
     (latexinfo-discard-command)
     (save-excursion
-      (insert ?\b name " (\\code{" type "})")
-      (insert " Default = \\samp{" default "}\n"))))
+      (insert "\\item[" name "]")
+      (insert "(\\code{" type "}) Default = \\samp{" default "}")
+      (insert "\\vindex{" name " " ptinfo-index-suffix "}\n"))))
 
 ;
 ; Commands for formatting a portlist environment
 ;
-(put 'portlist 'latexinfo-format 'latexinfo-portlist)
-(put 'portlist 'latexinfo-end 'latexinfo-end-portlist)
-(put 'portlist 'latexinfo-item 'latexinfo-port-item)
-(put 'port 'latexinfo-format 'latexinfo-item)
+(put 'portlist 'latexinfo-format 'ptinfo-index-description)
+(put 'portlist 'latexinfo-end 'latexinfo-end-description)
+(put 'port 'latexinfo-format 'ptinfo-port)
 
-(defun latexinfo-portlist ()
-  (latexinfo-begin-list 'portlist ""))
-
-(defun latexinfo-end-portlist ()
-  (latexinfo-end-list 'portlist))
-
-(defun latexinfo-port-item ()
+(defun ptinfo-port ()
   (let ((type (latexinfo-parse-line-arg))
         (name (latexinfo-parse-line-arg)))
     (latexinfo-discard-command)
     (save-excursion
-      (insert ?\b  name " (\\code{" type "})\n"))))
+      (insert "\\item[" name "]")
+      (insert "(\\code{" type "})")
+      (insert "\\vindex{" name " " ptinfo-index-suffix "}\n"))))
 
+;
+; Commands for formatting a blocklist environment
+;
+(put 'blocklist 'latexinfo-format 'ptinfo-index-description)
+(put 'blocklist 'latexinfo-end 'latexinfo-end-description)
+(put 'block 'latexinfo-format 'ptinfo-block)
+
+(defun ptinfo-block ()
+  (let ((name (latexinfo-parse-line-arg)))
+    (latexinfo-discard-command)
+    (save-excursion
+      (insert "\\item[" name "]")
+      (insert "\\tindex{" name " " ptinfo-index-suffix "}"))))
