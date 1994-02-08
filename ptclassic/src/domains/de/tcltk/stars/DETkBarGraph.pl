e8bb68403d3a963e3a2cb5e2684f25df8c920ff4
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
1994-02-08 23:54:32 +0000

    C-front compatibility changes

--- ptclassic/src/domains/de/tcltk/stars/DETkBarGraph.pl
+++ ptclassic/src/domains/de/tcltk/stars/DETkBarGraph.pl
@@ -71,16 +71,16 @@ limitation of liability, and disclaimer of warranty provisions.
 	    if(float(top) <= float(bottom)) {
 		Error::abortRun(*this, "invalid range for the scale");
 	    }
-	    // the cast on the string state label and position must
-	    // be (const char *) because a (char *) cast has not been
-	    // defined for string states but bar.setup wants (char *)
+	    // Make non-const copies of label and position
+	    InfString labCopy((const char*)label);
+	    InfString posCopy((const char*)position);
 	    bar.setup(this,
-		(const char *) label,
+		(char*) labCopy,
 		input.numberPorts(),
 		(int) number_of_bars,
 		(float) top,
 		(float) bottom,
-		(const char *) position,
+		(char*) posCopy,
 		(float) bar_graph_width,
 		(float) bar_graph_height);
 	    count = 0;
