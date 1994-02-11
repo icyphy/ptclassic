5dae20e2ff11f38f1bbcefcf666981ac34b138b9
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-02-11 13:29:47 +0000

    Swapped order of #if defined() || defined for hppa port.  Looks like hpux9.01 does not understand ||?

--- trunk/ptclassic/src/octtools/Packages/port/port.h
+++ trunk/ptclassic/src/octtools/Packages/port/port.h
@@ -1,7 +1,7 @@
 /* Version Identification:
  * $Id$
  */
-/* Copyright (c) 1990-1993 The Regents of the University of California.
+/* Copyright (c) 1990-1994 The Regents of the University of California.
  * All rights reserved.
  * 
  * Permission is hereby granted, without written agreement and without
@@ -307,7 +307,7 @@ extern char *malloc(), *realloc(), *calloc();
 #endif
 #endif
 
-#if defined(aiws) || defined(hpux)
+#if defined(hppa) || defined(aiws)
 extern int sprintf();
 #else
 #ifndef _IBMR2
