d612d979634ae68e8550e76e21ac589ce8fe5620
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-02-15 20:57:06 +0000

    Modified for hppa cfront port

--- trunk/ptclassic/src/compat/cfront/std.h
+++ trunk/ptclassic/src/compat/cfront/std.h
@@ -1,9 +1,14 @@
 // $Id$
+#if defined(hppa)
+#include <stdlib.h>
+#include "../../kernel/miscFuncs.h"
+#else
+// Sun
 #include <stdlib.h>
 #include <sysent.h>
 #include "../../kernel/miscFuncs.h"
 // we could include <strings.h> too, but Sun Cfront 2.1 has an
 // incorrect prototype for strcasecmp.
 extern "C" int strcasecmp(const char*, const char*);
-
+#endif
 
