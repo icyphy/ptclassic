3b677356c84e3c4cff256a42d7d7d0889e14fde4
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-02-15 21:28:57 +0000

    Added #if defined(hppa)/#else/#endif.

--- trunk/ptclassic/src/compat/cfront/builtin.h
+++ trunk/ptclassic/src/compat/cfront/builtin.h
@@ -7,7 +7,9 @@
 // with templates we will need only
 // template<class T> inline T abs(T a) { return (a >= 0) ? a : -a;}
 
+#if defined(hppa)
+#else
 inline double abs(double a) { return (a >= 0) ? a : -a;}
 inline int abs(int a) { return (a >= 0) ? a : -a;}
-
+#endif
 #endif
