83ada07d59f9a98a4c226d023d7f733b4643cb2f
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-02-18 09:40:38 +0000

    hppa cfront: random is rand

--- ptclassic/src/compat/cfront/Random.h
+++ ptclassic/src/compat/cfront/Random.h
@@ -3,6 +3,23 @@
 #define _Random_h 1
 // This version substitutes for libg++ random-number classes.
 // This is the abstract base class for other random-number types.
+
+#ifdef hppa
+#ifndef __GNUG__
+
+extern "C" int rand();
+
+// returns a random value between 0 and 1.
+
+class Random {
+protected:
+	double unif01() { return double(rand())/2147483648.0;}
+public:
+	virtual double operator()() = 0;
+};
+#endif /* __GNUG__ */
+#else /* hppa */
+
 extern "C" long random();
 
 // returns a random value between 0 and 1.
@@ -13,5 +30,6 @@ protected:
 public:
 	virtual double operator()() = 0;
 };
+#endif /* hppa */
 #endif
 
