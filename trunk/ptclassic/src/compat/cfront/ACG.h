b65432fd9f6936bec992e80cdaed4a8d953bc5f8
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-02-18 09:36:54 +0000

    hppa cfront: srandom is srand

--- trunk/ptclassic/src/compat/cfront/ACG.h
+++ trunk/ptclassic/src/compat/cfront/ACG.h
@@ -6,10 +6,21 @@
 // stream, with seed set by srandom and values read by random.
 // This works fine within Ptolemy.
 
+#ifdef hppa
+#ifndef __GNUG__
+extern "C" void srand(unsigned);
+
+class ACG {
+public:
+	ACG(unsigned seed = 1) {srand(seed);}
+};
+#endif /* !__GNUG__ */
+#else /* hppa */
 extern "C" void srandom(unsigned);
 
 class ACG {
 public:
 	ACG(unsigned seed = 1) {srandom(seed);}
 };
+#endif /* hppa */
 #endif
