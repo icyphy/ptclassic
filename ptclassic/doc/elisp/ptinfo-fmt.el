e6d99d830e49ce43e3ccc8e898928002ba97cce0
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-02-28 16:52:19 +0000

    replaced statelist, portlist, and blocklist with indexlist; added list items for functions, variables, methods, and members

--- ptclassic/doc/elisp/ptinfo-fmt.el
+++ ptclassic/doc/elisp/ptinfo-fmt.el
@@ -8,23 +8,18 @@
 (put 'Contrib 'latexinfo-format 'latexinfo-format-author)
 
 ;
-; Command for beginning a description, such as statelist, blocklist.
+; A list environment with automatic index entries.
 ;
-
-(defvar ptinfo-index-suffix nil)
-
+(put 'indexlist 'latexinfo-format 'ptinfo-index-description)
+(put 'indexlist 'latexinfo-end 'latexinfo-end-description)
+(defvar ptinfo-index-key nil)
 (defun ptinfo-index-description ()
-  (setq ptinfo-index-suffix (latexinfo-parse-line-arg))
+  (setq ptinfo-index-key (latexinfo-parse-line-arg))
   (latexinfo-discard-command)
   (save-excursion (insert "\\begin{description}\n")))
 
-;
-; Commands for formatting a statelist environment
-;
-(put 'statelist 'latexinfo-format 'ptinfo-index-description)
-(put 'statelist 'latexinfo-end 'latexinfo-end-description)
+; A list item for states.
 (put 'state 'latexinfo-format 'ptinfo-state)
-
 (defun ptinfo-state ()
   (let ((type (latexinfo-parse-line-arg))
         (name (latexinfo-parse-line-arg))
@@ -33,15 +28,10 @@
     (save-excursion
       (insert "\\item[" name "]")
       (insert "(\\code{" type "}) Default = \\samp{" default "}")
-      (insert "\\vindex{" name " " ptinfo-index-suffix "}\n"))))
+      (insert "\\vindex{" name " " ptinfo-index-key "}\n"))))
 
-;
-; Commands for formatting a portlist environment
-;
-(put 'portlist 'latexinfo-format 'ptinfo-index-description)
-(put 'portlist 'latexinfo-end 'latexinfo-end-description)
+; A list item for ports.
 (put 'port 'latexinfo-format 'ptinfo-port)
-
 (defun ptinfo-port ()
   (let ((type (latexinfo-parse-line-arg))
         (name (latexinfo-parse-line-arg)))
@@ -49,18 +39,58 @@
     (save-excursion
       (insert "\\item[" name "]")
       (insert "(\\code{" type "})")
-      (insert "\\vindex{" name " " ptinfo-index-suffix "}\n"))))
+      (insert "\\vindex{" name " " ptinfo-index-key "}\n"))))
 
-;
-; Commands for formatting a blocklist environment
-;
-(put 'blocklist 'latexinfo-format 'ptinfo-index-description)
-(put 'blocklist 'latexinfo-end 'latexinfo-end-description)
+; A list item for blocks.
 (put 'block 'latexinfo-format 'ptinfo-block)
-
 (defun ptinfo-block ()
   (let ((name (latexinfo-parse-line-arg)))
     (latexinfo-discard-command)
     (save-excursion
       (insert "\\item[" name "]")
-      (insert "\\tindex{" name " " ptinfo-index-suffix "}"))))
+      (insert "\\tindex{" name " " ptinfo-index-key "}"))))
+
+; A list item for functions.
+(put 'function 'latexinfo-format 'ptinfo-function)
+(defun ptinfo-function ()
+  (let ((type (latexinfo-parse-line-arg))
+        (name (latexinfo-parse-line-arg))
+        (args (latexinfo-parse-line-arg)))
+    (latexinfo-discard-command)
+    (save-excursion
+      (insert "\\item[" type " " name " " args "]")
+      (insert "\\findex{" name ptinfo-index-key "}\n"))))
+
+; A list item for variables.
+(put 'variable 'latexinfo-format 'ptinfo-variable)
+(defun ptinfo-variable ()
+  (let ((type (latexinfo-parse-line-arg))
+        (name (latexinfo-parse-line-arg)))
+    (latexinfo-discard-command)
+    (save-excursion
+      (insert "\\item[" type " " name "]")
+      (insert "\\vindex{" name ptinfo-index-key "}\n"))))
+
+; A list item for C++ class member functions (methods).
+(put 'method 'latexinfo-format 'ptinfo-method)
+(defun ptinfo-method ()
+  (let ((type (latexinfo-parse-line-arg))
+        (name (latexinfo-parse-line-arg))
+        (args (latexinfo-parse-line-arg)))
+    (latexinfo-discard-command)
+    (save-excursion
+      (insert "\\item[" type " " name " " args "]")
+      (insert "\\findex{" name ", " ptinfo-index-key " method}")
+      (insert "\\findex{" ptinfo-index-key " class, " name " method}\n"))))
+
+; A list item for C++ class data members.
+(put 'member 'latexinfo-format 'ptinfo-member)
+(defun ptinfo-member ()
+  (let ((type (latexinfo-parse-line-arg))
+        (name (latexinfo-parse-line-arg)))
+    (latexinfo-discard-command)
+    (save-excursion
+      (insert "\\item[" type " " name "]")
+      (insert "\\vindex{" name ", " ptinfo-index-key " member}\n")
+      (insert "\\vindex{" ptinfo-index-key " class, " name " member}\n"))))
+
