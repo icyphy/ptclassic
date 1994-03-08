d71837063b88454c84f4e8e23f1607f5ba3a3c19
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-03-08 16:12:42 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg56/targets/CG56XCPeek.pl
@@ -0,0 +1,40 @@
+defstar {
+	name { XCAReceive }
+	domain { CG56 }
+	desc { CGC to S56X Asynchronous Receive star }
+	version { $Id$ }
+	author { Jose Luis Pino }
+ 	derivedFrom {XCABase}
+	copyright {
+Copyright (c) 1994,1993 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { CG56 Target Directory }
+	explanation {
+	}
+protected {
+	friend class S56XTargetWH;
+	}
+
+	output {
+		name {output}
+		type {ANYTYPE}
+	}
+
+codeblock(receiveData) {
+	move	$ref(value),x0
+	move	x0,$ref(output)
+}
+
+
+go {
+	addCode(receiveData);
+}
+
+execTime {
+	return 2;
+}
+
+}
---
+++ trunk/ptclassic/src/domains/cg56/targets/CGCXPoke.pl
@@ -0,0 +1,55 @@
+defstar {
+    name { XASend }
+    domain { CGC }
+    desc { Send data asynchronously from CGC to S56X }
+    derivedFrom { XABase }
+    version { $Id$ }
+    author { Jose Luis Pino }
+
+copyright { 
+Copyright (c) 1994,1993 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+}
+
+location { CG56 Target Directory }
+
+explanation {}
+
+input {
+	name {input}
+	type {ANYTYPE}
+}
+
+state {
+	name {buffer}
+	type {int}
+	default {0}
+	attributes {A_NONCONSTANT|A_NONSETTABLE}
+}
+
+codeblock(sendData) {
+	if (value != $ref(buffer)) {
+		$ref(buffer) = value;
+		if (qckPoke(dsp,"$val(VariableName)",value) == -1) { 
+			fprintf(stderr, "$val(VariableName):Send Data Failed\n", qckErrString);
+			exit(1);
+		}
+        }
+}
+
+go {
+	StringList code = "\tint value;\n";
+	const char* intSend = "\tvalue = $ref(input);\n";
+	const char* fixSend = "\tvalue = $ref(input)*(1<<23);\n";
+	if (strcmp(input.resolvedType(),INT)==0)
+	    code << intSend;
+	else
+	    code << fixSend;
+	code << sendData;
+	addCode(updateLink(code));
+}
+
+}
+
