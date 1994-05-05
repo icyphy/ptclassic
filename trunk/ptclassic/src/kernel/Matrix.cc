65f3f57cfa52b444064ecfa3509e555c8c0aaaf6
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-05-05 15:00:21 +0000

    Renamed Matrix class to PMatrix to avoid clash with Matlab's Matrix data type

--- trunk/ptclassic/src/kernel/Matrix.cc
+++ trunk/ptclassic/src/kernel/Matrix.cc
@@ -49,12 +49,12 @@ ENHANCEMENTS, OR MODIFICATIONS.
  It is possible to define some of these functions in terms of others,
  for example:
 
-  Matrix operator + (const Matrix& src1, const Matrix& src2) {
-    Matrix result(src1);
+  PMatrix operator + (const PMatrix& src1, const PMatrix& src2) {
+    PMatrix result(src1);
     result += src2;
     return result;
   }
-  Matrix& operator += (const Matrix& src2) {
+  PMatrix& operator += (const PMatrix& src2) {
     // do add stuff
   }
 
@@ -483,13 +483,13 @@ IntMatrix& multiply (const IntMatrix& left,const IntMatrix& right,
 // Matrix Message Class
 /////////////////////////////////////////////////////////////
 
-int Matrix::compareType(const Matrix & m) const {
+int PMatrix::compareType(const PMatrix & m) const {
   if(typesEqual(m)) return 1;
   StringList msg = "Attempt to copy ";
   msg += m.dataType();
-  msg += " Matrix to ";
+  msg += " Ptolemy Matrix to ";
   msg += dataType();
-  msg += " Matrix";
+  msg += " Ptolemy Matrix";
   Error::abortRun (msg);
   return 0;
 }
@@ -601,7 +601,7 @@ StringList ComplexMatrix::print() const {
   return StringList(strm.str());
 }
 
-int ComplexMatrix::operator == (const Matrix& src) const {
+int ComplexMatrix::operator == (const PMatrix& src) const {
   if(!typesEqual(src)) return 0;
   if((nRows != ((const ComplexMatrix*)&src)->nRows) || 
      (nCols != ((const ComplexMatrix*)&src)->nCols))
@@ -635,7 +635,7 @@ ComplexMatrix::operator IntMatrix () const {
 }
 
 // destructive replacement operators
-Matrix& ComplexMatrix::operator = (const Matrix& m) {
+PMatrix& ComplexMatrix::operator = (const PMatrix& m) {
 // WARNING: any SubMatricies refering to the data in this matrix 
 // will become invalid if this matrix's storage is reallocated.
   if(compareType(m)) {
@@ -1052,7 +1052,7 @@ StringList FixMatrix::print() const {
   return StringList(strm.str());
 }
 
-int FixMatrix::operator == (const Matrix& src) const {
+int FixMatrix::operator == (const PMatrix& src) const {
   if(!typesEqual(src)) return 0;
   if((nRows != ((const FixMatrix*)&src)->nRows) || 
      (nCols != ((const FixMatrix*)&src)->nCols))
@@ -1086,7 +1086,7 @@ FixMatrix::operator IntMatrix () const {
 }
 
 // destructive replacement operators
-Matrix& FixMatrix::operator = (const Matrix& m) {
+PMatrix& FixMatrix::operator = (const PMatrix& m) {
 // WARNING: any SubMatricies refering to the data in this matrix 
 // will become invalid if this matrix's storage is reallocated.
   if(compareType(m)) {
@@ -1391,7 +1391,7 @@ StringList FloatMatrix::print() const {
   return StringList(strm.str());
 }
 
-int FloatMatrix::operator == (const Matrix& src) const {
+int FloatMatrix::operator == (const PMatrix& src) const {
   if(!typesEqual(src)) return 0;
   if((nRows != ((const FloatMatrix*)&src)->nRows) || 
      (nCols != ((const FloatMatrix*)&src)->nCols))
@@ -1425,7 +1425,7 @@ FloatMatrix::operator IntMatrix () const {
 }
 
 // destructive replacement operators
-Matrix& FloatMatrix::operator = (const Matrix& m) {
+PMatrix& FloatMatrix::operator = (const PMatrix& m) {
 // WARNING: any SubMatricies refering to the data in this matrix 
 // will become invalid if this matrix's storage is reallocated.
   if(compareType(m)) {
@@ -1731,7 +1731,7 @@ StringList IntMatrix::print() const {
   return StringList(strm.str());
 }
 
-int IntMatrix::operator == (const Matrix& src) const {
+int IntMatrix::operator == (const PMatrix& src) const {
   if(!typesEqual(src)) return 0;
   if((nRows != ((const IntMatrix*)&src)->nRows) || 
      (nCols != ((const IntMatrix*)&src)->nCols))
@@ -1765,7 +1765,7 @@ IntMatrix::operator FloatMatrix () const {
 }
 
 // destructive replacement operators
-Matrix& IntMatrix::operator = (const Matrix& m) {
+PMatrix& IntMatrix::operator = (const PMatrix& m) {
 // WARNING: any SubMatricies refering to the data in this matrix 
 // will become invalid if this matrix's storage is reallocated.
   if(compareType(m)) {
