dc94a3d9e4008aa553e2fa33c1a6a941038749d1
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-05-20 17:00:20 +0000

    Initial revision

---
+++ ptclassic/src/domains/sdf/matlab/stars/SDFMatlabCx_M.pl
@@ -0,0 +1,27 @@
+defstar {
+	name { MatlabCx_M }
+	domain { SDF }
+	derivedFrom { Matlab_M }
+	desc {
+Evaluate a Matlab function if inputs are given or
+evaluate a Matlab command if no inputs are given.
+	}
+	version { $Id$ }
+	author { Brian L. Evans }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { SDF main library }
+	explanation {
+See the explanation for SDFMatlab_M star.
+The only difference is that this star forces the outputs to be complex matrices.
+.Ir "Matlab interface"
+	}
+	outmulti {
+		name { output }
+		type { COMPLEX_MATRIX_ENV }
+	}
+}
