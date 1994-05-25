4c7b7c0caac34b01e7042c2fe291d20c085ace94
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-05-25 14:26:48 +0000

    Initial revision

---
+++ ptclassic/src/domains/xxx/stars/XXXNop.pl
@@ -0,0 +1,25 @@
+defstar {
+	name {Nop}
+	domain {XXX}
+	desc { Does nothing }
+	version {$Id$}
+	author { E. A. Lee and D. Niehaus }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { XXX main library }
+	output {
+		name {output}
+		type {=input}
+	}
+	input {
+	        name {input}
+		type {anytype}
+	}
+	go {
+	}
+}
+
