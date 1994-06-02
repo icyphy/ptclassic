840bb856bbb508905d9b20612c24036241610624
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-06-02 14:37:30 +0000

    (sol2): if SYSV, then extern optind.  This might not be correct for platforms other than Solaris2.x

--- ptclassic/src/octtools/Packages/options/options.c
+++ ptclassic/src/octtools/Packages/options/options.c
@@ -1,7 +1,7 @@
 #ifndef lint
 static char SccsId[]="$Id$";
 #endif /*lint*/
-/* Copyright (c) 1990-1993 The Regents of the University of California.
+/* Copyright (c) 1990-1994 The Regents of the University of California.
  * All rights reserved.
  * 
  * Permission is hereby granted, without written agreement and without
@@ -47,7 +47,16 @@ char OPT_CONT[] = "c";		/* continues description */
 
 char *optProgName = "unknown program";
 char *optarg;
+
+#ifdef SYSV
+/*
+ * rjjt - it seems that SYSV has getopt which uses optind
+ * this might only be specific to Solaris 2.x
+ */
+extern int optind;
+#else
 int optind = 0;
+#endif
 static char *scanPtr;
 
 
