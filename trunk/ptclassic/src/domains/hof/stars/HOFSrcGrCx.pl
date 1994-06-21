0a073f2b94449fe18faa73a48e8ee82249bcf7f9
ptdesign <ptdesign@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-06-21 22:49:02 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/hof/stars/HOFSrcGrCx.pl
@@ -0,0 +1,34 @@
+defstar {
+	name { ParSourcesGrCx }
+	domain { HOF }
+	derivedFrom { MapGr }
+	desc {
+Create one or more instances of the named block to produce the
+required number of output stream(s).  This is implemented by replacing
+the ParSourcesGrCx star with the named block at setup time.  The replacement
+block(s) are connected as illustrated by the example block that is connected.
+Their parameters are determined by "parameter_map".
+	}
+	version { $Id$ }
+	author { Edward A. Lee }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { HOF main library }
+	explanation {
+See the explanation for MapGr star, from which this is derived.
+The only difference is that this star forces the number of inputs to zero
+and output datatype to complex.  It also hides the input ports and the
+\fIinput_map\fR parameter.
+.Sr "MapGr"
+	}
+	constructor {
+	  output.setPort("output", this, COMPLEX);
+	  exin.setPort("exin", this, COMPLEX);
+	  input.setAttributes(P_HIDDEN);
+	  exout.setAttributes(P_HIDDEN);
+	}
+}
---
+++ trunk/ptclassic/src/domains/hof/stars/HOFSrc_M.pl
@@ -0,0 +1,38 @@
+defstar {
+	name { ParSources_M }
+	domain { HOF }
+	derivedFrom { Map }
+	desc {
+Create one or more instances of the named block to produce the
+required number of output stream(s).  This is implemented by replacing
+the ParSources star with the named block at setup time.  The replacement
+block(s) are connected as specified by "input_map", using the existing
+connections to the ParSources star.  Their parameters are determined
+by "parameter_map".
+	}
+	version { $Id$ }
+	author { Edward A. Lee }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { HOF main library }
+	explanation {
+See the explanation for Map star, from which this is derived.
+The only difference is that this star forces the number of inputs to zero
+and output datatype to float matrix.  It also hides the input ports and the
+\fIinput_map\fR parameter.
+.Sr "Map"
+	}
+	ccinclude { "Matrix.h" } 
+	constructor {
+	  output.setPort("output", this, FLOAT_MATRIX_ENV);
+	  input.setAttributes(P_HIDDEN);
+	  input_map.clearAttributes(A_SETTABLE);
+	  input_map.setInitValue("");
+	  blockname.setInitValue("Identity_M");
+	  parameter_map.setInitValue("");
+	}
+}
