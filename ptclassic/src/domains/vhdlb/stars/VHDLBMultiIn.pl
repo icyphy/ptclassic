9792f57deeed56053809e4e2aded3d0bbd2a590a
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-06-23 15:53:38 +0000

    Initial revision

---
+++ ptclassic/src/domains/vhdlb/stars/VHDLBMultiIn.pl
@@ -0,0 +1,42 @@
+defstar {
+	name { MultiIn }
+	domain {VHDLB}
+	desc {
+Output the first of multiple inputs, discarding the rest.
+	}
+        version { $Id$ }
+	author { Michael C. Williamson }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { VHDLB main library }
+	inmulti {
+		name { input }
+		type { int }
+	}
+	output {
+		name { output }
+		type { int }
+	}
+	method {
+                name { portWidthName }
+                type { "const char*" }
+                arglist { "(const MultiVHDLBPort* pp)" }
+                access { protected }
+                code {
+                        if(!strcmp(pp.name(),"input")) {
+			  return((const char*) "N");
+			}
+                        else {
+			  Error::abortRun(*this, "No MultiVHDLBPort named",
+					  pp.name(), "in star");
+			  return((const char*) "ERROR");
+			}
+                }
+	}
+	go {
+	}
+}
