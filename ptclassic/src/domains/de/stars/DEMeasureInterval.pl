903fc92d2b798e77664f2039167c11ba2fd127ef
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
1994-06-24 18:11:35 +0000

    Initial revision

---
+++ ptclassic/src/domains/de/stars/DEMeasureInterval.pl
@@ -0,0 +1,34 @@
+defstar {
+	name { MeasureInterval }
+	domain { DE }
+	version { $Id$ }
+	author { Edward A. Lee }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { DE main library }
+	desc {
+The value of each output event is the simulated time since the last input
+event (or since zero, for the first input event). The time stamp of each 
+output event is the time stamp of the input event that triggers it.
+	}
+	input {
+		name { input }
+		type { ANYTYPE }
+	}
+	output {
+		name { output }
+		type { float }
+	}
+	constructor {
+		input.triggers(output);
+	}
+	go {
+		output.put(arrivalTime) << (arrivalTime - completionTime);
+		completionTime = arrivalTime;
+	}
+}
+
