97553b444c7dea0dcda7cc176aec0bc6f3f4cef0
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-06-24 18:11:35 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/de/stars/DETimeStamp.pl
@@ -0,0 +1,34 @@
+defstar {
+	name { TimeStamp }
+	domain { DE }
+	version { $Id$ }
+	author { Edward A. Lee }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { DE main library }
+	desc {
+The value of the output events is the time stamp of the input events.
+The time stamp of the output events is also the time stamp of the input
+events.
+	}
+	input {
+		name { input }
+		type { ANYTYPE }
+	}
+	output {
+		name { output }
+		type { float }
+	}
+	constructor {
+		input.triggers(output);
+	}
+	go {
+		completionTime = arrivalTime;
+		output.put(completionTime) << completionTime;
+	}
+}
+
