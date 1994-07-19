e2d1eade5d6173220843fba09562b99046beaaf5
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-07-19 22:47:36 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg/kernel/CGCluster.cc
@@ -0,0 +1,89 @@
+static const char file_id[] = "CGCluster.cc";
+/******************************************************************
+Version identification:
+ $Id$
+
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+
+ Programmer: Jose Luis Pino
+ Date of creation: 6/10/94
+
+*******************************************************************/
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include "CGCluster.h"
+#include "SDFScheduler.h"
+#include "LoopScheduler.h"
+#include "SDFCluster.h"
+
+int CGCluster::run() {
+    return Cluster::run();
+}
+
+void CGCluster::setMasterBlock(Block*m,PortHole**newPorts) {
+    DFClusterBase::setMasterBlock(m,newPorts);
+    StringState* scheduler =
+	(StringState*) Cluster::master->stateWithName("Scheduler");
+    if (scheduler) {
+	const char* schedType = scheduler->initValue();
+	IntState* processor = (IntState*) Cluster::master->stateWithName("procId");
+	if (processor) setProcId((int) (*processor));
+	Scheduler* newSched;
+	if (strcmp(schedType,"Cluster") == 0)
+	    newSched = new SDFClustSched("");
+	else if (strcmp(schedType,"Loop") == 0)
+	    newSched = new LoopScheduler("");
+	else
+	    newSched = new SDFScheduler;
+	setInnerSched(newSched);
+    }
+}	    
+	
+CGCluster::CGCluster(const char* domain):
+CGStar(),DFClusterBase(*this,domain)
+{};
+
+CGClusterPort::CGClusterPort(const PortHole* master, Star* parent)
+: CGPortHole(), ClusterPort(*this,*master,parent) {
+    const DFPortHole& dfmaster = *(const DFPortHole*)master;
+    DFPortHole::maxBackValue = dfmaster.maxDelay();
+}
+
+PortHole* CGCluster::clonePort(const PortHole* master, Star* parent) {
+    return new CGClusterPort(master,parent);
+}
+
+Cluster* CGCluster::newCluster(Block* master,const char* domain) const {
+    LOG_NEW; Cluster* cluster = new CGCluster(domain);
+    if (master) cluster->setMasterBlock(master);
+    if (domain)
+	cluster->myDomain = domain;
+    else
+	cluster->myDomain = myDomain;
+    return cluster;
+}
+
---
+++ trunk/ptclassic/src/domains/cg/kernel/CGCluster.h
@@ -0,0 +1,91 @@
+#ifndef _CGCluster_h
+#define _CGCluster_h 1
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+/*******************************************************************
+ SCCS Version identification :
+ $Id$
+
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+
+ Programmer : Jose Luis Pino
+ Date of Creation : 6/10/90
+	
+********************************************************************/
+
+#include "Cluster.h"
+#include "DFClusterBase.h"
+#include "CGStar.h"
+#include "CGPortHole.h"
+
+class CGClusterPort : public CGPortHole, public ClusterPort{
+public:
+    CGClusterPort(const PortHole* master, Star* parent);
+    /*virtual*/ int isItInput() const {
+	return ClusterPort::isItInput();
+    }
+    /*virtual*/ int isItOutput() const {
+	return ClusterPort::isItOutput();
+    }
+    /*virtual*/ ClusterPort* asClusterPort() { return this; }
+};
+
+class CGCluster : public CGStar, public DFClusterBase{
+public:
+    
+    CGCluster(const char* domain);
+
+    /*virtual*/ int run();
+
+    /*virtual*/ int flattenGalaxy(Galaxy* galaxy) {
+	return ! (int)galaxy.stateWithName("Scheduler");
+    }
+
+    /*virtual*/ void setMasterBlock(Block*,PortHole**);
+
+    /*virtual*/ PortHole* clonePort(const PortHole* master, Star* parent);
+    
+    /*virtual*/ Cluster* newCluster(Block* master,const char* domain) const;
+
+    /*virtual*/ Block* makeNew() const { return &newCluster()->star(); }
+
+    /*virtual*/ Block* clone() const { return cloneCluster(); }
+    
+    /*virtual*/ Cluster* asCluster() { return this; }
+    
+    /*virtual*/ int isSDFinContext() const {
+	return isClusterSDFinContext();
+    }
+
+    // return my domain.  all clusters should redefine this method of block.
+    /*virtual*/ const char* domain () const { return myDomain; }
+};
+#endif
+
+
+
+
+
