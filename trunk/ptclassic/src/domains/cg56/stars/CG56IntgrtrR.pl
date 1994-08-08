0e14fe9b171c7446d8f37ca914366486b0443028
ptolemy <ptolemy@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-08-08 17:14:18 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg56/stars/CG56IntgrtrR.pl
@@ -0,0 +1,36 @@
+defstar {
+	name { IntgrtrR }
+	domain { CG56 }
+	desc { 
+Integrator with reset input.  Still under construction.
+	}
+	version { $Id$ }
+	author { Kennard White }
+	copyright {
+Copyright (c) 1990, 1991, 1992 The Regents of the University of California.
+All rights reserved.
+See the file ~ptolemy/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { CG56 library }
+	explanation {
+Sill under construction.
+	}
+	input {
+		name { input }
+		type { FIX }
+ 	}
+	output {
+		name { output }
+		type { FIX }
+	}
+ 	setup {
+		;
+	}
+	go {
+		;
+	}
+	execTime {
+		return 1;
+	}
+}
