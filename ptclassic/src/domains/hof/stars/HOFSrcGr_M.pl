af9ae4e7e6c0179c97614b047feca1c185e85e89
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-08-09 18:32:47 +0000

    Changes names of ParSources stars to ParSrc

--- ptclassic/src/domains/hof/stars/HOFSrcGr_M.pl
+++ ptclassic/src/domains/hof/stars/HOFSrcGr_M.pl
@@ -1,11 +1,11 @@
 defstar {
-	name { ParSourcesGr_M }
+	name { ParSrcGr_M }
 	domain { HOF }
 	derivedFrom { MapGr }
 	desc {
 Create one or more instances of the named block to produce the
 required number of output stream(s).  This is implemented by replacing
-the ParSourcesGr_M star with the named block at setup time.  The replacement
+the ParSrcGr_M star with the named block at setup time.  The replacement
 block(s) are connected as illustrated by the example block that is connected.
 Their parameters are determined by "parameter_map".
 	}
