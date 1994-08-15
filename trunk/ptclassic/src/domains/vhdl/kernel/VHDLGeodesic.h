85669e3d5c898d05478ccd6f667d0172122756bb
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-08-15 10:05:07 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/vhdl/kernel/VHDLGeodesic.h
@@ -0,0 +1,61 @@
+#ifndef _VHDLGeodesic_h
+#define _VHDLGeodesic_h 1
+/******************************************************************
+Version identification:
+$Id$
+
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+
+ Programmer: Edward A. Lee, Michael C. Williamson
+
+ Geodesic for VHDL code generation.
+ In addition to fork management provided by CG Geodesics,
+ they manage definition and naming of buffers.
+
+*******************************************************************/
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include "CGGeodesic.h"
+
+class VHDLGeodesic : public CGGeodesic {
+public:
+	VHDLGeodesic() : bufName(0) {}
+	~VHDLGeodesic() {LOG_DEL; delete bufName; }
+
+	// class identification
+	int isA(const char*) const;
+
+	void setBufName(char* n) {bufName = n;}
+	char* getBufName() const;
+
+protected:
+
+private:
+	// Name of the VHDL object used as a buffer
+	char* bufName;
+};
+
+#endif
