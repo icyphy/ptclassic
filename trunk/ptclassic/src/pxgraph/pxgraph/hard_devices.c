85cbe258a0c3ed920a7482a7c7664eece6d0ad8c
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-09-06 08:45:22 +0000

    If we are on a HP, define USE_LP and use lp instead of lpr.

--- trunk/ptclassic/src/pxgraph/pxgraph/hard_devices.c
+++ trunk/ptclassic/src/pxgraph/pxgraph/hard_devices.c
@@ -15,6 +15,27 @@ extern int mifInit();
 extern int psInit();
 extern int idrawInit();
 
+#if defined(hpux) || defined(__hpux) 
+#define USE_LP
+#endif
+
+#ifdef USE_LP
+/* sigh.  Not all machines have lpr, so we use lp in a pinch */
+struct hard_dev hard_devices[] = {
+    { "HPGL", hpglInit, "lp -d%s", "xgraph.hpgl", "paper",
+	27.5, "1", 14.0, "1", 12.0, NONE },
+    { "MIF", mifInit,
+	"(cat > /tmp/ptxgraphpr$; export FMPRINTER; FMPRINTER=%s; fmprint /tmp/ptxgraphpr$; /bin/rm /tmp/ptxgraphpr$)", "~/xgraph.mif", "lw",
+	16.0, "Helvetica", 12.0,
+	"Helvetica", 10.0, NONE },
+    { "Postscript", psInit, "lp -d%s", "xgraph.ps", "lw",
+	19.0, "Times-Bold", 18.0, "Times-Roman", 12.0, NO },
+    { "Idraw", idrawInit,
+	"cat > /usr/tmp/idraw.tmp.ps; %s /usr/tmp/idraw.tmp.ps&",
+	"~/.clipboard", "/usr/local/idraw", 19.0, "Times-Bold", 18.0,
+	"Times-Roman", 12.0, NONE }
+};
+#else /* USE_LP */
 struct hard_dev hard_devices[] = {
     { "HPGL", hpglInit, "lpr -P%s", "xgraph.hpgl", "paper",
 	27.5, "1", 14.0, "1", 12.0, NONE },
@@ -29,7 +50,7 @@ struct hard_dev hard_devices[] = {
 	"~/.clipboard", "/usr/local/idraw", 19.0, "Times-Bold", 18.0,
 	"Times-Roman", 12.0, NONE }
 };
-
+#endif /* USE_LP */
 int hard_count = sizeof(hard_devices)/sizeof(struct hard_dev);
 
 #define CHANGE_D(name, field) \
