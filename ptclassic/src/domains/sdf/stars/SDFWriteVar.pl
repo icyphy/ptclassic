f0d258e8fd733068cef3250b934586d29fbb9c9f
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-09-30 08:45:28 +0000

    Added copyright.

--- ptclassic/src/domains/sdf/stars/SDFWriteVar.pl
+++ ptclassic/src/domains/sdf/stars/SDFWriteVar.pl
@@ -2,6 +2,14 @@ defstar {
   name { WriteVar }
   domain { SDF }
   derivedfrom {SDFSharedMem}
+  version { $Id$ }
+  author { Stefan De Troch (IMEC) }
+  copyright{ 
+Copyright (c) 1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+}
   desc {}
   input
   {
