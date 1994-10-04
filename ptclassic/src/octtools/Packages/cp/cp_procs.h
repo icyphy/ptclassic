43e2b45c9ef8b9a41d3c8e8acf8a93d86cc81b4b
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-10-04 10:53:28 +0000

    Initial revision

---
+++ ptclassic/src/octtools/Packages/cp/cp_procs.h
@@ -0,0 +1,47 @@
+/* Version Identification:
+ * $Id$
+ */
+/* Copyright (c) 1990-1994 The Regents of the University of California.
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the above
+ * copyright notice and the following two paragraphs appear in all copies
+ * of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+ * FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+ * THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ * PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ * CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ * 
+ */
+#ifndef CP_PROCS_H
+#define CP_PROCS_H
+
+#include "ansi.h"
+
+extern int cp_get_integer
+        ARGS((int* ret_val));
+extern int cp_get_path
+	ARGS((int *number,cp_path **path));
+extern int cp_get_point
+	ARGS((cp_point *point));
+extern int cp_get_sinteger
+	ARGS((int *ret_val));
+extern int cp_get_shortname
+	ARGS((char **ret_ptr));
+extern void cp_get_transform
+	ARGS((double array[2][2], int32 *x, int32 *y,int *type));
+extern void flush
+	ARGS((type_type type));
+
+#endif /*CP_PROCS_H*/
