d3ffc5943cc60bac182f80ea477fae810fe99156
parks <parks@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-10-05 12:47:41 +0000

    Initial revision

---
+++ ptclassic/src/domains/pn/stars/PNFilter.pl
@@ -0,0 +1,50 @@
+defstar
+{
+    name { Filter }
+    domain { PN }
+    version { $Id$ }
+    desc { Filter out multiples of a prime number. }
+    author { T. M. Parks }
+    copyright
+    {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
+    location { PN library }
+
+    input
+    {
+	name { input }
+	type { int }
+    }
+
+    output
+    {
+	name { output }
+	type { int }
+	attributes { P_DYNAMIC }
+    }
+
+    state
+    {
+	name { prime }
+	type { int }
+	default { 2 }
+	desc { Prime number to filter. }
+    }
+
+    go
+    {
+	int N = input%0;
+	int P = prime;
+	
+	if (N%P) 
+	{
+	    output.receiveData();
+	    output%0 = input%0;
+	    output.sendData();
+	}
+    }
+}
