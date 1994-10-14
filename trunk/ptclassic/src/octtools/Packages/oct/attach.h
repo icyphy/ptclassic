ae3ab02162c8955f603c62c8c648bcab7a70124a
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-10-14 17:17:12 +0000

    Initial revision

---
+++ trunk/ptclassic/src/octtools/Packages/oct/attach.h
@@ -0,0 +1,44 @@
+/* Version Identification:
+ * $Id$
+ */
+/* Copyright (c) 1990-1994 The Regents of the University of California.
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the above
+ * copyright notice and the following two paragraphs appear in all copies
+ * of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+ * FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+ * THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ * PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ * CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ * 
+ */
+#ifndef OCT_ATTACH_H
+#define OCT_ATTACH_H
+#include "ansi.h"
+
+int oct_do_attach
+	ARGS((generic *cptr, generic *optr, int only_once));
+int oct_do_undetach
+	ARGS((generic *cptr, generic *optr));
+int oct_do_unattach
+	ARGS((generic *cptr, generic *optr));
+int oct_do_detach
+	ARGS((generic *cptr, generic *optr));
+octStatus oct_detach_default
+	ARGS((generic *cptr, generic *optr));
+octStatus oct_undetach_default
+	ARGS((generic *cptr, generic *optr));
+
+#endif
---
+++ trunk/ptclassic/src/octtools/Packages/oct/chain.h
@@ -0,0 +1,42 @@
+/* Version Identification:
+ * $Id$
+ */
+/* Copyright (c) 1990-1994 The Regents of the University of California.
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the above
+ * copyright notice and the following two paragraphs appear in all copies
+ * of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+ * FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+ * THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ * PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ * CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ * 
+ */
+#ifndef OCT_CHAIN_H
+#define OCT_CHAIN_H
+#include "ansi.h"
+
+octStatus oct_detach_chain
+	ARGS((struct chain **chain, generic *container, generic *object));
+octStatus oct_attach_chain
+	ARGS((generic *container, struct chain **chain, generic *object, int only_once));
+octStatus oct_delete_contents
+	ARGS((generic *object));
+int oct_put_chain
+	ARGS((struct chain *chain));
+int oct_get_chain
+	ARGS((generic *container, struct chain **chain_ptr));
+
+#endif
