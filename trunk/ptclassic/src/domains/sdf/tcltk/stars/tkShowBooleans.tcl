9e3e3dfede292e36e5969dd33f00979e8dce60cd
ptdesign <ptdesign@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-10-24 05:42:24 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/tcltk/stars/tkShowBooleans.tcl
@@ -0,0 +1,150 @@
+# Tcl/Tk source for a display that shows Booleans
+#
+# Author: Edward A. Lee
+# Version: $Id$
+#
+# Copyright (c) 1994 The Regents of the University of California.
+# All rights reserved.
+# See the file $PTOLEMY/copyright for copyright notice,
+# limitation of liability, and disclaimer of warranty provisions.
+#
+
+set s $ptkControlPanel.booleans_$starID
+
+# If a window with the right name already exists, we assume it was
+# created by a previous run of the very same star, and hence can be
+# used for this new run.  Some trickiness occurs, however, because
+# parameter values may have changed, including the number of inputs.
+
+if {[winfo exists $s]} {
+    set window_previously_existed 1
+} {
+    set window_previously_existed 0
+}
+
+if {!$window_previously_existed} {
+
+    proc tkShowBooleanMakeWindowSDF {putInCntrPan win label numInputs univ starID ids} {
+	if {![winfo exists $win]} {
+	    global ptkControlPanel
+            if {$putInCntrPan} {
+	        frame $win
+	        pack $win -after $ptkControlPanel.low -fill x -expand 1
+            } {
+                toplevel $win
+                wm title $win "Show Booleans"
+                wm iconname $win "Show Booleans"
+            }
+
+            frame $win.f
+	    if {$label != {}} {
+		message $win.msg -width 12c
+		pack $win.msg -expand 1
+	    }
+            pack $win.f -fill both -expand 1 -padx 10 -pady 10
+	}
+	if {$label != {}} {
+	    $win.msg configure -text $label
+	}
+
+	# The following flag is used if the waitBetweenOutputs parameter is set
+	global $starID
+	set ${starID}(tkShowBooleanWaitTrig) 0
+        for {set i 0} {$i < $numInputs} {incr i} {
+	    catch {destroy $win.f.att$i}
+	    frame $win.f.att$i -class Attention
+    	    button $win.f.att$i.button -relief raised -width 40 \
+		-command "incr ${starID}(tkShowBooleanWaitTrig)" \
+		-state disabled
+
+	    $win.f.att$i.button configure \
+		    -text "[lindex $ids $i]: no Boolean yet"
+
+	    pack $win.f.att$i.button -fill x -expand 1
+	    pack $win.f.att$i -fill x -expand 1
+        }
+
+        if {!$putInCntrPan} {
+	    if {![winfo exists $win.ok]} {
+                button $win.ok -text "DISMISS" -command \
+		    "catch {incr ${starID}(tkShowBooleanWaitTrig)}
+		     ptkStop $univ
+		     destroy $win"
+                pack $win.ok -fill x -expand 1
+	    }
+	    # It would be nice to allow user resizing of the window,
+	    # but it is totally mysterious in Tk how to do this in
+	    # a reasonable way without hardwiring in some guess about
+	    # a reasonable minimum size.
+        }
+	if {[set ${starID}(wait_between_outputs)]} {
+	    # Arrange for the tkShowBooleanWaitTrig variable
+	    # to be updated if the run status changes (for example,
+	    # a halt is requested)
+	    global ptkRunFlag
+	    trace variable ptkRunFlag($univ) w tkShowBooleanReleaseWaitSDF
+	}
+    }
+}
+
+proc tkShowBooleanReleaseWaitSDF {runflag univ op} "
+	global ${starID}
+	incr ${starID}(tkShowBooleanWaitTrig)
+"
+
+tkShowBooleanMakeWindowSDF [set ${starID}(put_in_control_panel)] \
+	$s [set ${starID}(label)] \
+	[set ${starID}(numInputs)] [curuniverse] $starID \
+	[set ${starID}(identifiers)]
+
+if {!$window_previously_existed} {
+
+    proc tkShowBooleanSetValuesSDF {starID numInputs win ids} {
+        set c $win.f
+        set inputVals [grabInputs_$starID]
+	for {set i 0} {$i < $numInputs} {incr i} {
+            if {[lindex $inputVals $i]} {
+		set in {TRUE}
+		$win.f.att$i.button configure -background [ptkColor white]
+	    } {
+		set in {FALSE}
+		$win.f.att$i.button configure -background [ptkColor burlywood1]
+	    }
+	    $win.f.att$i.button configure -text "[lindex $ids $i]: $in"
+	}
+    }
+
+    proc tkShowBooleanWaitSDF {flag starID numInputs win univ} {
+	if {$flag} {
+	    global $starID
+	    set ${starID}(tkShowBooleanWaitTrig) 0
+	    for {set i 0} {$i < $numInputs} {incr i} {
+	        $win.f.att$i.button configure -state normal
+	    }
+	    tkwait variable ${starID}(tkShowBooleanWaitTrig)
+	    for {set i 0} {$i < $numInputs} {incr i} {
+		# In case the window was dismissed, trap errors
+	        catch {$win.f.att$i.button configure -state disabled}
+	    }
+	}
+    }
+}
+
+# In the following definition, the value of $s
+# is evaluated when the file is sourced.
+proc goTcl_$starID {starID} "
+        tkShowBooleanSetValuesSDF $starID [set ${starID}(numInputs)] $s \
+		[list [set ${starID}(identifiers)]]
+	tkShowBooleanWaitSDF [set ${starID}(wait_between_outputs)] $starID \
+		[set ${starID}(numInputs)] $s [curuniverse]
+"
+
+proc destructorTcl_$starID {starID} {
+        global $starID
+        if {[set ${starID}(put_in_control_panel)]} {
+            # Remove the meters from the control panel, if they still exist
+            global ptkControlPanel
+            destroy $ptkControlPanel.booleans_$starID
+        }
+}
+unset s
