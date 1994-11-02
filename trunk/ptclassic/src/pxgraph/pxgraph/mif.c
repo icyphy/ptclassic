d5a13daf8616a0be6451f5a5e0b2cda6f27b9cd0
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-11-02 22:15:56 +0000

    Added copyright, fixed up font printf, removed extra %

--- trunk/ptclassic/src/pxgraph/pxgraph/mif.c
+++ trunk/ptclassic/src/pxgraph/pxgraph/mif.c
@@ -1,4 +1,31 @@
-/* Ptolemy version: $Id$
+/*******************************************************************
+SCCS version identification
+$Id$
+
+Copyright (c) 1989-1994 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+*/
+
 /*
  * MIF Output
  *
@@ -18,7 +45,6 @@
 #define MAPX(x) (x)/VDPID
 #define MAPY(y) (y)/VDPID
 
-#include "copyright.h"
 #include <stdio.h>
 #include "xgout.h"
 /* FIXME -- remove plotter.h dependence */
@@ -148,7 +174,7 @@ mifText(userState,x,y,text,just,style)
             break;
         case T_TITLE:
             fprintf(userState->plotterFile,
-		" <Font <FFamily `%s%'> <FSize %d> <FBold Yes>>\n",
+		" <Font <FFamily `%s'> <FSize %d> <FBold Yes>>\n",
                 userState->title_family,
                 userState->title_size);
 	    height = rd( userState->title_size * VDPI * INCHES_PER_POINT );
@@ -209,7 +235,9 @@ mifText(userState,x,y,text,just,style)
 
 
 
+#ifdef NEVER
 static int penselect[8] = { PEN1, PEN2, PEN3, PEN4, PEN5, PEN6, PEN7, PEN8};
+#endif
 
 static char* dashselect[] = {
     "<DashedStyle Solid>",
