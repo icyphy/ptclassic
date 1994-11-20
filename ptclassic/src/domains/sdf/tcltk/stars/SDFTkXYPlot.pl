7031642fcadc6dbf625c6fded13d3c3c10a17e6a
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
1994-11-20 14:02:05 +0000

    Made inputs multiple, supported line style

--- ptclassic/src/domains/sdf/tcltk/stars/SDFTkXYPlot.pl
+++ ptclassic/src/domains/sdf/tcltk/stars/SDFTkXYPlot.pl
@@ -2,10 +2,16 @@ defstar {
 	name {TkXYPlot}
 	domain {SDF}
 	desc {
-Dynamically display points in an XY plot
+Plot Y input(s) vs. X input(s) with dynamic updating.
+Two styles are currently supported: style = 0 causes
+points to be plotted, whereas style = 1 causes connected
+lines to be plotted. Drawing a box in the plot will
+reset the plot area to that outlined by the box.
+There are also buttons for zooming in and out, and for
+resizing the box to just fit the data in view.
 	}
 	version { $Id$ }
-	author { E. A. Lee and D. Niehaus }
+	author { Wei-Jen Huang, E. A. Lee, and D. Niehaus }
 	copyright {
 Copyright (c) 1990-1994 The Regents of the University of California.
 All rights reserved.
@@ -20,12 +26,12 @@ limitation of liability, and disclaimer of warranty provisions.
                 name {label}
                 type{string}
                 default {"Tk XY Plot"}
-                desc {The label used for the icon and title names}
+                desc {The title of the plot }
         }
         defstate {
 		name {geometry}
 		type{string}
-                default { "600x600+100+100" }
+                default { "600x400+0+0" }
                 desc { Specified the location and/or size of the window }
 	}
 	defstate {
@@ -59,38 +65,44 @@ limitation of liability, and disclaimer of warranty provisions.
 		desc {The number of points displayed at any one time }
 	}
 	defstate {
+	        name {style}
+		type {int}
+		default {"0"}
+		desc {Plot style (0 for points, 1 for lines)}
+	}
+	defstate {
 	        name {updateSize}
 		type{int}
 		default {10}
 		desc { The number of points drawn simulataneously. Higher numbers make the response faster.  }
 	}
 	
-	input {
+	inmulti {
 		name {X}
 		type {float}
 		desc { horizontal coordinate }
 	}
-	input {
+	inmulti { 
 		name {Y}
 		type {float}
 		desc { vertical coordinate }
 	}
 	protected {
-		XYPlot xyplot;
+	  XYPlot xyplot;
+	  InfString labCopy, geoCopy, xtCopy, ytCopy;
+	  double xMin, xMax, yMin, yMax;
 	}
 
 	begin {
 	  // Need to make non-const copies of  strings to
 	  // avoid compilation warnings
-	  InfString labCopy((const char*)label);
-	  InfString geoCopy((const char*)geometry);
-	  InfString xtCopy((const char*)xTitle);
-	  InfString ytCopy((const char*)yTitle);
+	  labCopy = (const char*)label;
+	  geoCopy = (const char*)geometry;
+	  xtCopy = (const char*)xTitle;
+	  ytCopy = (const char*)yTitle;
 
 	  // parse the x and y ranges which are
 	  // specified as strings for user convenience
-	  double xMin, xMax, yMin, yMax;
-
 	  if ((sscanf((const char *)xRange,"%lf %lf", &xMin, &xMax) != 2) ||
 	      (xMax <= xMin )) {
 	    Error::abortRun(*this, "xRange parameter values are invalid");
@@ -99,7 +111,7 @@ limitation of liability, and disclaimer of warranty provisions.
 	      (yMax <= yMin )) {
 	    Error::abortRun(*this, "yRange parameter values are invalid");
 	  }
-	  
+
 	  // create the XYplot window labeling, scaling,
 	  // and ranging as specified by the parameters
 	  xyplot.setup(this,	   
@@ -112,10 +124,17 @@ limitation of liability, and disclaimer of warranty provisions.
 		                xMax,        // maximum X range value
 		       (char*)  ytCopy,      // Title for Y-axis
 		                yMin,	     // minimum Y range value
-		                yMax);       // maximum Y range value
+		                yMax,        // maximum Y range value
+		                Y.numberPorts(),   // The number of data sets
+		       (int)    style);      // the plot style to use
 
 	}
 	go {
-	  xyplot.addPoint(double(X%0),double(Y%0));
+	  MPHIter nextx(X), nexty(Y);
+	  PortHole *px, *py;
+	  int set = 1;
+	  while ((px = nextx++) != 0 && (py = nexty++) != 0) {
+	    xyplot.addPoint(double((*px)%0),double((*py)%0), set++);
+	  }
 	}
 }
