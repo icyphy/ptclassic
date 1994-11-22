fdbd72730c8379e36c2ea441cc151458c7486ae6
Christopher Brooks <cxh@eecs.berkeley.edu>
1994-11-22 16:23:01 +0000

    go(): gcc-2.6.1 warns that outA and outB might be used unitialized.

--- ptclassic/src/domains/sdf/stars/SDFBusFork.pl
+++ ptclassic/src/domains/sdf/stars/SDFBusFork.pl
@@ -34,7 +34,7 @@ limitation of liability, and disclaimer of warranty provisions.
 	}
 	go {
 	  MPHIter nextin(input), nextoutA(outputA), nextoutB(outputB);
-	  PortHole *in, *outA, *outB;
+	  PortHole *in, *outA = (PortHole *)NULL, *outB = (PortHole *)NULL;
 	  while (((in = nextin++) != 0) &&
 		 ((outA = nextoutA++) != 0) &&
 		 ((outB = nextoutB++) != 0)) {
