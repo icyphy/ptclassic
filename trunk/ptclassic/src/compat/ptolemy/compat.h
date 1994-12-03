a369fd97a462fbd977c48084d6bddd1548f20ce1
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1994-12-03 14:31:32 +0000

    Various extern decls to quiet down -Wall warnings

--- trunk/ptclassic/src/compat/ptolemy/compat.h
+++ trunk/ptclassic/src/compat/ptolemy/compat.h
@@ -37,6 +37,11 @@ extern "C" {
 #define SOL2
 #endif
 
+#if defined(__sparc) && !defined(__svr4__)
+/* Really, we mean sun4 running sunos4.1.x */
+#define SUN4
+#endif
+
 #ifndef ARGS
 #if defined(__STDC__) || defined(__cplusplus)
 #define ARGS(args)	args
@@ -53,7 +58,11 @@ extern int sprintf();
 #ifndef SOL2
 #ifndef ultrix
 #if !(defined(sun) && defined (__GNUC__)) && !defined(hppa) && !defined(__hppa__)
+#if defined(sun) && !defined(__GNUC__) && defined(__cplusplus) && !defined(SOL2)
+/* SunOS4.1.3 Sun Cfront */	
+#else
 extern char *sprintf();
+#endif
 #endif /*sun && __GNUC__*/
 #endif /*ultrix*/
 #endif /*SOL2*/
@@ -63,26 +72,83 @@ extern char *sprintf();
 
 #ifdef __GNUC__
 
-#include <stdio.h>		/* Get the decl for FILE.  sigh.
-				 * Also get sprintf() for linux. */
+#include <stdio.h>			/* Get the decl for FILE.  sigh.
+					 * Also get sprintf() for linux. */
+#include <sys/types.h>			/* Need for bind(). */
+#include <sys/socket.h>			/* Need for bind(). */
+
+#ifdef SUN4
+#include <sys/time.h>			/* For select() */
+/* Place all SunOS4.1.3 specific declarations here. (In alphabetical order). */
+
+extern int accept(int, struct sockaddr *, int *);
+extern void bzero(char *, int); 	/* vem/rpc/server.c */
+
+extern int fgetc(FILE *);
+extern int fputs(const char *, FILE *);
+
+/* pxgraph/xgraph.c, octtools/Xpackages/rpc/{appOct.c,rpc.c} all call fread()*/
+extern long unsigned int 
+fread(void *, long unsigned int, long unsigned int, FILE *);
+extern long unsigned int 
+fwrite(const void *, long unsigned int, long unsigned int, FILE *);
+
+extern int getdtablesize();		/* vem/rpc/server.c */
+
+extern int gethostname(char *, int);	/* vem/rcp/serverNet.c (among others)*/
+
+/* Don't define getsockname for SOL2.  thor/kernel/rpc.c calls getsockname().*/
+extern int getsockname(int, struct sockaddr *, int *);
+
+/* octtools/Xpackages/iv/ivGetLine.c calls ioctl */
+extern int ioctl(int, int request, caddr_t);
+
+/* octtools/Xpackages/rpc/{appNet.c,serverNet.c} thor/analyzer/X11/event.c
+   all call select */
+extern select(int, fd_set *, fd_set *, fd_set *, struct timeval *);
+
+/* Under SunOS4.1.3, strcasecmp is in strings.h, not string.h */
+/* octtools/Packages/tap/quantize.c uses strcasecmp */
+extern int strcasecmp(const char *, const char *);
+
+extern int system(const char *);
+
+/* Under SunOS4.1.3, tolower() and toupper are not in ctype.h */
+/* vem/strings/str.c (and other files ) use tolower() and toupper(). */
+extern int tolower(int c);
+extern int toupper(int c);
+
+extern int ungetc(int, FILE *);
+extern int vfork();
+extern int vfprintf(FILE *, const char *, char *);
+
+#endif /* SUN4 */
+
 /* Alphabetical, please */
+				/* thor/kernel/rpc.c use bind(), listen(). */
+extern int bind(int, struct sockaddr *, int);
+
+extern void endpwent();		/* octtools/Packages/fc/fc.c and
+				   octtools/Packages/utility/texpand.c */
+
 extern int fclose (FILE *);
 extern int fflush (FILE *);
 extern int fprintf (FILE *, const char *, ...);
 extern int fscanf (FILE *, const char *, ...);
-/* gcc-2.5.8 under SunOS4.1.3 can't deal with a full declaration of
- * pclose, or we get an undefined symbol:   _pclose__FP6_iobuf 
- * cg/kernel/CGUtilities.cc uses pclose().
- */
-/*extern int pclose(FILE *);*/
-/*extern int pclose(...);*/
+extern int listen(int, int);
+extern int pclose(FILE *);
 extern void perror (const char *);
 extern int printf (const char *, ...);
 #if !defined(hppa) && !defined(__hppa__) && !defined(linux)
 extern int putenv (char *);
 #endif
 extern int puts (const char *);
+
+extern void setpwent();		/* octtools/Packages/fc/fc.c and
+				   octtools/Packages/utility/texpand.c */
+
 extern int sscanf (const char *, const char *, ...);
+extern int socket(int, int, int); /* thor/kernel/rpc.c uses socket() */
 extern int unlink(const char *);
 #endif /* __GNUC__ */
 
