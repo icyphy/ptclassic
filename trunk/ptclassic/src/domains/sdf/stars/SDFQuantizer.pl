76878ccffc766275cc87205cb7a44cf15066f143
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-02-04 16:42:38 +0000

    Improved the English in the documentation

--- trunk/ptclassic/src/domains/sdf/stars/SDFQuantizer.pl
+++ trunk/ptclassic/src/domains/sdf/stars/SDFQuantizer.pl
@@ -2,16 +2,16 @@ defstar {
   name { Quantizer }
   domain { SDF }
   desc {
-Scalar quantizer. It will quantize the input value to one of possible 
-output levels in the given codebook, ie, it will find the neareast value
-in the given codebook. Output levels in the codebook is not necessary
-to be in specific order. This star will do the full search in the
-codebook, and will be slower than Quant star as expected. 
+This star quantizes the input value to the nearest output value
+in the given codebook.  The nearest value is found by a full search
+of the codebook, so the star will be significantly slower than
+either Quant or LinQuantIdx.  The absolute value of the difference
+is used as a distance measure.
   }
   version { $Id$ }
   author { Bilung Lee }
   copyright {
-Copyright (c) 1990-1994 The Regents of the University of California.
+Copyright (c) 1995 The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
@@ -24,18 +24,18 @@ limitation of liability, and disclaimer of warranty provisions.
   output {
     name { output }
     type { float }
-    desc { Output the corresponding level in the codebook }
+    desc { Closest value in the codebook }
   }
   output {
     name { outIndex }
     type { int }
-    desc { Output the corresponding index in the codebook }
+    desc { Index of the closest value in the codebook }
   }
   defstate {
     name { floatCodebook }
     type { floatarray }
     default { "0.0 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0" }
-    desc { Output levels. }
+    desc { Possible output values }
   }
   go {
     double in = double(input%0);
