d66683981d1b8bae786a5521e4c14ee5af19d361
eal <eal@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-02-13 17:46:45 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/stars/SDFModuloInt.pl
@@ -0,0 +1,34 @@
+defstar {
+	name {ModuloInt}
+	domain {SDF}
+	desc { 
+The output is equal to the remainder after dividing the
+integer input by the integer "modulo" parameter.
+	}
+	version {$Id$}
+	author { E. A. Lee }
+	copyright {
+Copyright (c) 1995 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { SDF main library }
+	input {
+		name{input}
+		type{int}
+	}
+	output {
+		name{output}
+		type{int}
+	}
+	defstate {
+		name{modulo}
+		type{int}
+		default{"10"}
+		desc {The modulo parameter}
+	}
+	go {
+	   output%0 << int(input%0) % int(modulo);
+	}
+}
