668ba21a5ebf70717363968ae05ad209859726f0
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-02 22:45:23 +0000

    Added copyright

--- trunk/ptclassic/src/domains/cgc/stars/CGCSGIAudioOut.pl
+++ trunk/ptclassic/src/domains/cgc/stars/CGCSGIAudioOut.pl
@@ -6,6 +6,13 @@ defstar
     desc { Put samples into audio output port. }
     version { $Id$ }
     author { T. M. Parks }
+    copyright {
+Copyright (c) 1990-%Q% The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
+    location { CGC main library }
 
     input
     {
