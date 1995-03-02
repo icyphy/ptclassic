4f6b7eed53558bc68c6c108b9fe571e1e4b02963
Christopher Brooks <cxh@eecs.berkeley.edu>
1995-03-02 23:09:23 +0000

    Added copyright.

--- ptclassic/src/domains/sdf/dsp/stars/SDFGGAL.pl
+++ ptclassic/src/domains/sdf/dsp/stars/SDFGGAL.pl
@@ -6,6 +6,12 @@ defstar
     desc { Ganged Gradient Adaptive Lattice filters. }
     version { $Id$ }
     author { T. M. Parks }
+    copyright {
+Copyright (c) 1990-%Q% The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
     location { SDF dsp library }
 
     input
