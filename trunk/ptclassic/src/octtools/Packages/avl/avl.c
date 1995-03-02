bb98c99375957c2baa1e49e8c7f07224268f60a7
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-02 14:25:32 +0000

    copyright

--- trunk/ptclassic/src/octtools/Packages/avl/avl.c
+++ trunk/ptclassic/src/octtools/Packages/avl/avl.c
@@ -7,9 +7,9 @@ All rights reserved.
 
 Permission is hereby granted, without written agreement and without
 license or royalty fees, to use, copy, modify, and distribute this
-software and its documentation for any purpose, provided that the above
-copyright notice and the following two paragraphs appear in all copies
-of this software.
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
 
 IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
 FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
@@ -24,7 +24,7 @@ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
 CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 ENHANCEMENTS, OR MODIFICATIONS.
 
-						PT_COPYRIGHT_VERSION_3
+						PT_COPYRIGHT_VERSION_2
 						COPYRIGHTENDKEY
 */
 /* LINTLIBRARY */
