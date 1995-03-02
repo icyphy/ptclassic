8d28059d510ee360785b52ff9e950cc709f6ad0d
Christopher Brooks <cxh@eecs.berkeley.edu>
1995-03-02 22:53:07 +0000

    Yet another fix for copyright

--- ptclassic/src/domains/pn/stars/PNEndCase.pl
+++ ptclassic/src/domains/pn/stars/PNEndCase.pl
@@ -12,7 +12,7 @@ inputs.
     version { $Id$ }
     author { T. M. Parks }
     copyright {
-Copyright (c) 1990-$Id$ The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
