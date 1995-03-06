c45fd89a81ab5db75584c4dfdd16576a62b14ec1
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-06 22:56:34 +0000

    Put description in standard form and cleaned up the way IntState parameters were being cast--- explicit casting was messing up the initializer method for the nb_fft class and was redundantly used in the code instead of being stored in a local variable

--- ptclassic/src/domains/sdf/contrib/stars/SDFCfft.pl
+++ ptclassic/src/domains/sdf/contrib/stars/SDFCfft.pl
@@ -1,22 +1,23 @@
 defstar {
-  name {Cfft}
-  domain {SDF}
-        copyright {
+	name {Cfft}
+	domain {SDF}
+	copyright {
 Copyright (c) 1990-1994 The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
-        }
-  desc {
-Compute the complex fft.
-InputSize is number of input samples.
-fftSize is the size of fft.  zero padding is used if 
-  fftSize > InputSize
-Inverse if Inverse == 1
-Use Hamming window if UseWindow
-	     }
-  author { N. Becker }
-  location { SDF user contribution library }
+	}
+	desc {
+Compute the complex FFT.
+\fIInputSize\fR is number of input samples.
+\fIfftSize\fR is the size of FFT.
+Zero padding is used if
+fftSize > InputSize
+Inverse FFT is used if \fIInverse\fR is 1.
+Use Hamming window if \fIUseWindow\fR is true.
+	}
+	author { N. Becker }
+	location { SDF user contribution library }
 	input {
 		name {input}
 		type {complex}
@@ -49,6 +50,7 @@ Use Hamming window if UseWindow
 		default {0}
 		desc { Equals 1 to use Hamming window }
 	}
+  hinclude {"nb_fft.h"}
   protected {
     Complex *Temp;
     Complex *In;
@@ -58,19 +60,17 @@ Use Hamming window if UseWindow
     In = 0;
   }
   destructor {
-    delete Temp;
-    delete In;
+    delete [] Temp;
+    delete [] In;
   }
   setup {
-    delete Temp;
-    Temp = new Complex[ (int)fftSize ];
-    delete In;
-    In = new Complex[ (int)fftSize ];
-    input.setSDFParams( (int)InputSize, (int)InputSize-1 );
-    output.setSDFParams( (int)fftSize, (int)fftSize-1 );
+    delete [] Temp;
+    Temp = new Complex[ int(fftSize) ];
+    delete [] In;
+    In = new Complex[ int(fftSize) ];
+    input.setSDFParams( int(InputSize), int(InputSize)-1 );
+    output.setSDFParams( int(fftSize), int(fftSize)-1 );
   }
-  hinclude {"fft.h"}
-
   inline method {
     name { WinVal }
     type { double }
@@ -82,33 +82,35 @@ Use Hamming window if UseWindow
   }
 
   go {
-    fft F( (int)Inverse, (int)fftSize );
+    int invflag = Inverse;
+    int fftlength = fftSize;
+    nb_fft F( invflag, fftlength );
 
-    for (int i = 0; i < (int)InputSize; i++ )
-      In[ (int)InputSize - i - 1 ] = input%i;
-    for (i = (int)InputSize; i < (int)fftSize; i++ )
+    for (int i = 0; i < int(InputSize); i++ )
+      In[ int(InputSize) - i - 1 ] = (input%i);
+    for (i = int(InputSize); i < fftlength; i++ )
       In[ i ] = 0;
 
-    double WinNorm;
-    if( (int)UseWindow ) {
+    double WinNorm = 1;
+    if( int(UseWindow) ) {
       double Sum = 0;
-      for( i = 0; i < (int)fftSize; i++ ) {
-	double TimeArg = ( (double)i / ( (double)(int)fftSize - 1.) ) - 0.5;
+      for( i = 0; i < fftlength; i++ ) {
+	double TimeArg = ( double(i) / ( double(fftlength) - 1.0 ) ) - 0.5;
 	double WV = WinVal( TimeArg );
 	In[ i ] *= WV;
 	Sum += WV;
       }
-      WinNorm = (int)fftSize / Sum;	// This is the 1/(DC gain) compared to rect. window
+      // This is the 1/(DC gain) compared to rect. window
+      WinNorm = fftlength / Sum;
     }
-    else WinNorm = 1;
-          
+
     Complex* Result = F.Compute( In, Temp );
 
-    double Norm = (int)Inverse ? WinNorm : WinNorm / (double)(int)fftSize;
-    for( i = 0; i < (int)fftSize; i++ )
+    double Norm = int(Inverse) ? WinNorm : WinNorm / double(fftlength);
+    for( i = 0; i < fftlength; i++ )
       Result[ i ] *= Norm;
 
-    for ( i = 0; i < (int)fftSize; i++ )
-      output%i << Result[ (int)fftSize - i - 1 ];
+    for ( i = 0; i < fftlength; i++ )
+      (output%i) << Result[ fftlength - i - 1 ];
   }
 }
