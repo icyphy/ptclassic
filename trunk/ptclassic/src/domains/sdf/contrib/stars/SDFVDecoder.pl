6f3df6200837090b88bb0ab03c587cc802618ddd
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-06 16:49:29 +0000

    Changed old style casts to new style casts and added casts for the two-argument call to D (the instance of the decoder)

--- trunk/ptclassic/src/domains/sdf/contrib/stars/SDFVDecoder.pl
+++ trunk/ptclassic/src/domains/sdf/contrib/stars/SDFVDecoder.pl
@@ -3,15 +3,15 @@ defstar {
 	domain {SDF}
 	desc { Viterbi Decoder }
 	location { SDF user contribution library }
+	version { $Id$ }
 	author { N. Becker }
-        copyright {
-Copyright (c) 1990-1994 The Regents of the University of California.
+	copyright {
+Copyright (c) 1990-1995 The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
-        }
+	}
 	location { SDF main library }
-	hinclude { "Decoder.h" }
 	input {
 	    name {I}
 	    type {int}
@@ -24,21 +24,22 @@ limitation of liability, and disclaimer of warranty provisions.
 	    name {out}
 	    type {int}
 	}
+	defstate {
+	    name{ Gain }
+	    type{ float }
+	    default{ 1 }
+	    desc{ The nominal level of the input }
+	}
+	hinclude { "Decoder.h" }
 	private {
 	    Decoder D;
 	}
-	defstate {
-	  name{ Gain }
-	  type{ float }
-	  default{ 1 }
-	  desc{ The nominal level of the input }
-	}
-	conscalls{ D ((int)Gain) }
+	conscalls{ D(int(Gain)) }
 	setup {
-	  D.Reset();
-	  D.SetGain( (int)Gain );
+	    D.Reset();
+	    D.SetGain( int(Gain) );
 	}
 	go {
-	    out%0 << D( I%0, Q%0 );
+	    out%0 << D( int(I%0), int(Q%0) );
 	}
 }
