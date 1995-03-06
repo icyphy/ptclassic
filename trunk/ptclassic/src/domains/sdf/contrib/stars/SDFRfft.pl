1da340c085b89b659c47d7172c2e8ab4a4cb4e2d
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-06 22:56:34 +0000

    Put description in standard form and cleaned up the way IntState parameters were being cast--- explicit casting was messing up the initializer method for the nb_fft class and was redundantly used in the code instead of being stored in a local variable

--- trunk/ptclassic/src/domains/sdf/contrib/stars/SDFRfft.pl
+++ trunk/ptclassic/src/domains/sdf/contrib/stars/SDFRfft.pl
@@ -1,23 +1,23 @@
 defstar {
-  name {Rfft}
-  domain {SDF}
-  desc {
-Compute the fft of a real input, using the '2-channel trick'.
-InputSize is number of input samples.
-fftSize is the size of fft.  zero padding is used if 
-  fftSize > InputSize
-Inverse if Inverse == 1
-Use Hamming window if UseWindow
+	name {Rfft}
+	domain {SDF}
+	desc {
+Compute the FFT of a real input, using the '2-channel trick'.
+\fIInputSize\fR is number of input samples and
+\fIfftSize\fR is the size of FFT.
+Zero padding is used if \fIfftSize\fR > \fIInputSize\fR.
+The inverse FFT is taken if \fIInverse\fR is non-zero.
+Apply a Hamming window to the data if \fIUseWindow\fR is non-zero.
 The output is half the size of the input.
-	     }
-  author { N. Becker }
+	}
+	author { N. Becker }
         copyright {
 Copyright (c) 1990-1994 The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
         }
-  location { SDF user contribution library }
+	location { SDF user contribution library }
 	input {
 		name {input}
 		type {float}
@@ -61,21 +61,22 @@ limitation of liability, and disclaimer of warranty provisions.
     In = 0;
   }
   destructor {
-    delete Temp1;
-    delete Temp2;
-    delete In;
+    delete [] Temp1;
+    delete [] Temp2;
+    delete [] In;
   }
   setup {
-    delete Temp1;
-    Temp1 = new Complex[ (int)fftSize/2 ];
-    delete Temp2;
-    Temp2 = new Complex[ (int)fftSize/2 ];
-    delete In;
-    In = new double[ (int)fftSize ];
-    input.setSDFParams( (int)InputSize, (int)InputSize-1 );
-    output.setSDFParams( (int)fftSize / 2, ((int)fftSize/2)-1 );
+    int fftlength = fftSize;
+    delete [] Temp1;
+    Temp1 = new Complex[ fftlength/2 ];
+    delete [] Temp2;
+    Temp2 = new Complex[ fftlength/2 ];
+    delete [] In;
+    In = new double[ fftlength ];
+    input.setSDFParams( int(InputSize), int(InputSize)-1 );
+    output.setSDFParams( fftlength / 2, (fftlength/2)-1 );
   }
-  hinclude {"fft.h"}
+  hinclude {"nb_fft.h"}
 
   inline method {
     name { WinVal }
@@ -88,39 +89,44 @@ limitation of liability, and disclaimer of warranty provisions.
   }
 
   go {
-    fft F( (int)Inverse, (int)fftSize / 2 );
+    int invflag = Inverse;
+    int fftlength = fftSize;
+    nb_fft F( invflag, fftlength / 2 );
 
-    for (int i = 0; i < (int)InputSize; i++ )
-      In[ (int)InputSize - i - 1 ] = input%i;
-    for (i = (int)InputSize; i < (int)fftSize; i++ )
+    for (int i = 0; i < int(InputSize); i++ )
+      In[ int(InputSize) - i - 1 ] = input%i;
+    for (i = int(InputSize); i < fftlength; i++ )
       In[ i ] = 0;
 
-    double WinNorm;
-    if( (int)UseWindow ) {
+    double WinNorm = 1;
+    if( int(UseWindow) ) {
       double Sum = 0;
-      for( i = 0; i < (int)fftSize; i++ ) {
-	double TimeArg = ( (double)i / ( (double)(int)fftSize - 1.) ) - 0.5;
+      for( i = 0; i < fftlength; i++ ) {
+	double TimeArg = ( double(i) / ( double(fftlength) - 1.) ) - 0.5;
 	double WV = WinVal( TimeArg );
 	In[ i ] *= WV;
 	Sum += WV;
       }
-      WinNorm = (int)fftSize / Sum;	// This is the 1/(DC gain) compared to rect. window
+      // This is the 1/(DC gain) compared to rect. window
+      WinNorm = fftlength / Sum;
     }
-    else WinNorm = 1;
-          
-    for( i = 0; i < (int)fftSize / 2; i++ )
+
+    for( i = 0; i < fftlength / 2; i++ )
       Temp1[ i ] = Complex( In[ 2 * i ], In[ ( 2 * i ) + 1 ] );
 
     Complex* Result = F.Compute( Temp1, Temp2 );
 
-    double Norm = (int)Inverse ? WinNorm : WinNorm / (double)(int)fftSize;
-    for( i = 0; i < (int)fftSize; i++ )
+    double Norm = int(Inverse) ? WinNorm : WinNorm / (double(fftlength));
+    for( i = 0; i < fftlength; i++ )
       Result[ i ] *= Norm;
 
-    for( i = 0; i < (int)fftSize/2; i++ ) {
-      Complex X1 = .5 * ( Result[ i ] + conj( Result[ i == 0 ? 0 : (int)fftSize/2 - i ] ) );
-      Complex X2 = Complex( 0, -.5 ) * ( Result[ i ] - conj( Result[ i == 0 ? 0 : (int)fftSize/2 - i ] ) );
-      output%((int)fftSize/2 - i - 1) << Norm * ( X1 + F.getW( -i, (int)fftSize ) * X2 );
+    for( i = 0; i < fftlength/2; i++ ) {
+      Complex X1 = .5 * ( Result[ i ] +
+		   conj( Result[ i == 0 ? 0 : fftlength/2 - i ] ) );
+      Complex X2 = Complex( 0, -.5 ) * ( Result[ i ] -
+		   conj( Result[ i == 0 ? 0 : fftlength/2 - i ] ) );
+      output%(fftlength/2 - i - 1) <<
+		Norm * ( X1 + F.getW( -i, fftlength ) * X2 );
     }
   }
 }
