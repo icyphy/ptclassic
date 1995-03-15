7bf800c9581fb99732e7aa83b257a59349f26348
aok <aok@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-15 12:01:57 +0000

    added Alpha signals

--- ptclassic/src/kernel/ptsignals.cc
+++ ptclassic/src/kernel/ptsignals.cc
@@ -91,7 +91,7 @@ as a blank function and define ptBlockSig and ptReleaseSig as below.
 #include <signal.h>
 #include "compat.h" 
 
-#if defined(PTSOL2) || defined(PTIRIX5) || defined(PTLINUX)
+#if defined(PTSOL2) || defined(PTIRIX5) || defined(PTLINUX) || defined(PTALPHA)
 void ptSafeSig( int SigNum ) {
 	struct sigaction pt_alarm_action;
 	sigaction( SigNum, NULL, &pt_alarm_action);
@@ -128,4 +128,4 @@ void ptSafeSig( int SigNum ) {};
 
 #endif /* PTSUN4 */
 #endif /* PTHPPA */
-#endif /* PTSOL2  PTIRIX5  PTLINUX */
+#endif /* PTSOL2  PTIRIX5  PTLINUX PTALPHA */
