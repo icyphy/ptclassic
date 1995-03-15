4f3d0c1edf0c85c719d68da355addfe35474afa8
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-15 16:41:39 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/cg/kernel/AnyCGStar.h
@@ -0,0 +1,55 @@
+#ifndef _AnyCGStar_h
+#define _AnyCGStar_h 1
+/******************************************************************
+Version identification:
+$Id$
+
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+
+ Programmer: J. Buck
+
+This file defines a class of star that can be used under any
+assembly language domain in addition to the type that generates
+code for that domain.
+
+*******************************************************************/
+
+#ifdef __GNUG__
+#pragma interface
+#endif
+#include "CGStar.h"
+
+#define AnyCGPortHole CGPortHole
+#define InAnyCGPort InCGPort
+#define OutAnyCGPort OutCGPort
+#define MultiInAnyCGPort MultiInCGPort
+#define MultiOutAnyCGPort MultiOutCGPort
+
+class AnyCGStar : public CGStar {
+public:
+	const char* domain() const;
+	int isA(const char*) const;
+};
+
+#endif
