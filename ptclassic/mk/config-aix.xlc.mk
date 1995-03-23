4e3597d87d73cfc4d8c8fe20049e6a3b876a8bd7
Christopher Brooks <cxh@eecs.berkeley.edu>
1995-03-23 10:17:25 +0000

    Initial revision

---
+++ ptclassic/mk/config-aix.xlc.mk
@@ -0,0 +1,92 @@
+# Copyright (c) 1990-1995 The Regents of the University of California.
+# All rights reserved.
+# 
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the
+# above copyright notice and the following two paragraphs appear in all
+# copies of this software.
+# 
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+# 
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+# 
+# 						PT_COPYRIGHT_VERSION_2
+# 						COPYRIGHTENDKEY
+#	Programmer: Xavier Warzee (Thomson CSF)
+#
+# Config file to build under IBM AIX
+#
+# @(#)config-aix.mk	1.1 3/14/95
+
+# --------------------------------------------------------------------
+# |  Please see the file ``config-default.mk'' in this directory!    |
+# --------------------------------------------------------------------
+include $(ROOT)/mk/config-default.mk
+
+# Get the g++ definitions; we override some below.
+include $(ROOT)/mk/config-g++.mk
+
+#
+# Programs to use
+#
+RANLIB =	ranlib
+# Use gcc everywhere including in octtools
+CC =		gcc
+
+OPTIMIZER =	-O2
+#-Wsynth is new in g++-2.6.x
+WARNINGS =	-Wall -Wcast-qual -Wsynth
+GPPFLAGS =	-g $(MEMLOG) $(WARNINGS) $(OPTIMIZER)
+# If you are not using gcc, then you might have problems with the WARNINGS flag
+CFLAGS =	-g $(MEMLOG) $(WARNINGS) $(OPTIMIZER)
+
+#
+# Variables for the linker
+#
+
+# Flag that gcc expects to create statically linked binaries.
+# Binaries that are shipped should be statically linked.
+# Note that cc uses -Bstatic
+CC_STATIC = 	-static
+
+#
+# Directories to use
+#
+X11_INCSPEC =	-I/usr/X11/include
+X11_LIBSPEC =	-L/usr/X11/lib -lX11
+
+# Variables for Pure Inc tools (purify, purelink, quantify)
+COLLECTOR =
+
+PURELINK =
+PURIFY =
+QUANTIFY =
+PURECOV =
+
+# Variable for the Ariel DSP56000 board
+# S56 directory is only used on sun4.
+S56DIR =
+
+# Variables for local Matlab installation
+# -- If Matlab is installed, then MATLABDIR points to where MATLAB is installed
+#    and MATLABLIBDIR points to the directory containing the Matlab libraries
+# -- If Matlab is not installed, then MATLABDIR equals $ROOT/src/compat/matlab
+#    and MATLABLIBIDR is undefined
+#MATLABDIR =	/usr/sww/matlab
+#MATLABLIBDIR =	-L$(MATLABDIR)/extern/lib/$(ARCH)
+MATLABDIR =	$(ROOT)/src/compat/matlab
+MATLABLIBDIR =
+
+# Used to compile xv.  Use -traditional to avoid varargs problems
+XV_CC =		gcc -traditional
+
