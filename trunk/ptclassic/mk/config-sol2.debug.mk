9e926a53566847ec0542caa98652813a20ce7d1c
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-03-31 15:21:43 +0000

    fixed for sol24

--- trunk/ptclassic/mk/config-sol2.debug.mk
+++ trunk/ptclassic/mk/config-sol2.debug.mk
@@ -67,9 +67,13 @@ OPTIMIZER =
 #-Wsynth is new in g++-2.6.x
 WARNINGS =	-Wall -Wcast-qual -Wsynth
 MULTITHREAD =	-D_REENTRANT
-GPPFLAGS =	-g $(MEMLOG) $(WARNINGS) $(OPTIMIZER) $(MULTITHREAD)
+# Define PTSOL2_4 if you are on Solaris2_4
+LOCALFLAGS =    -DPTSOL2_4
+GPPFLAGS =	-g $(MEMLOG) $(WARNINGS) $(OPTIMIZER) $(MULTITHREAD) $(LOCALFLAG
+S)
 # If you are not using gcc, then you might have problems with the WARNINGS flag
-CFLAGS =	-g $(MEMLOG) $(WARNINGS) $(OPTIMIZER) $(MULTITHREAD)
+CFLAGS =	-g $(MEMLOG) $(WARNINGS) $(OPTIMIZER) $(MULTITHREAD) $(LOCALFLAG
+S)
 
 #
 # Variables for the linker
