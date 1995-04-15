9dc53db86b0476b7c6d430204c7a0d39efb90ba4
Christopher Brooks <cxh@eecs.berkeley.edu>
1995-04-15 15:36:54 +0000

    Changes from Joe: Remove stuff about patching stdlib.h, no longer needed. Don't use -pipe, it makes life worse on small-memory systems. Don't use -m486, it's the default, except for those with the Pentium optimized compiler; for them -m486 makes things worse. Link non-debug binaries dynamically. Set defaults appropriately for Slackware 2.x (with X11R6)

--- ptclassic/mk/config-linux.mk
+++ ptclassic/mk/config-linux.mk
@@ -46,35 +46,16 @@ CC =		gcc
 # OCT_CC is used in src/octtools/vem-{lib,bin}.mk
 OCT_CC =	gcc
 
-
-# /usr/include/stdlib.h may need to be modified for optimization to work.
-# In particular, there could be a problem with __OPTIMIZE__ being defined
-# and random().  Below is diff for the changes:
-#    *** /usr/include/stdlib.h-dist	Mon Nov 29 08:36:11 1993
-#    --- /usr/include/stdlib.h	Sun Aug  7 21:10:43 1994
-#    ***************
-#    *** 135,144 ****
-#    --- 135,146 ----
-#      extern void * setstate __P((void * __statebuf));
-#      
-#      #ifdef	__OPTIMIZE__
-#    + #ifndef NO_RAND_OPTIMIZE
-#      #define	random()		__random()
-#      #define	srandom(seed)		__srandom(seed)
-#      #define	initstate(s, b, n)	__initstate((s), (b), (n))
-#      #define	setstate(state)		__setstate(state)
-#    + #endif /* NO_RAND_OPTIMIZE */
-#      #endif	/* Optimizing.  */
-#      #endif	/* Use BSD.  */
-
-
 # debug version
 LINUXDEF =	-Dlinux #-D_GNU_SOURCE -D_BSD_SOURCE
-OPTIMIZER =	-m486 -pipe -g
+#	Don't use -pipe, it makes life worse on small-memory systems.
+#	Don't use -m486, it's the default, except for those with the
+#	Pentium optimized compiler; for them -m486 makes things worse.
+#OPTIMIZER =	-g #-m486 -pipe
 
 #
 # 'production' version with optimization
-#OPTIMIZER =	-O2 -m486 -fomit-frame-pointer -pipe
+OPTIMIZER =	-O2 #-fomit-frame-pointer #-m486 -pipe
 #LINUXDEF =	-Dlinux -DNO_RAND_OPTIMIZE #-D_GNU_SOURCE -D_BSD_SOURCE
 
 # -Wsynth is new in g++-2.6.x, however 2.5.x does not support it
@@ -89,7 +70,7 @@ CFLAGS =	$(LINUXDEF) $(OPTIMIZER) -fwritable-strings
 # system libraries (libraries from the environment)
 SYSLIBS=-lg++ -lm
 
-LINKFLAGS=-L$(LIBDIR) -Xlinker -S -Xlinker -x -static
+LINKFLAGS=-L$(LIBDIR) -Xlinker -S -Xlinker #-x -static
 LINKFLAGS_D=-L$(LIBDIR) -g -static
 
 # octtools/attache uses this
@@ -109,7 +90,7 @@ TERMLIB_LIBSPEC = -ltermcap
 #X11_INCSPEC =	-I$(ROOT)/src/compat -I/usr/sww/X11R6/include
 #X11_LIBSPEC =	-L/usr/sww/X11R6/lib -lX11
 # Use -lSM -lICE for X11R6, don't use then for X11R5
-#X11EXT_LIBSPEC=-lXext -lSM -lICE
+X11EXT_LIBSPEC=-lXext -lSM -lICE
 #
 # Slackware >=1.2 locations
 #
