6fe204a576eb19a9ad030ca70e262c708a6bafed
wtc <wtc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-04-18 14:46:48 +0000

    Cleaned up the code.  Do not customize the binding to the individual event buttons, but rather use button's -command option to achieve the same effects.  In destructor, always destroy the buttons whether they are in the control panel or not, and delete Tcl procs created by this star.  Also removed use of the global variable ptkControlPanel in the destructor.  Renamed ptkButtonsClearButtons to tkButtonsClearButtons_$starID to avoid name conflict.

--- trunk/ptclassic/src/domains/de/tcltk/stars/tkButtons.tcl
+++ trunk/ptclassic/src/domains/de/tcltk/stars/tkButtons.tcl
@@ -1,6 +1,6 @@
 # Tcl/Tk source for a panel of buttons
 #
-# Author: Edward A. Lee
+# Authors: Edward A. Lee and Wan-Teh Chang
 # Version: $Id$
 #
 # Copyright (c) 1990-%Q% The Regents of the University of California.
@@ -36,7 +36,7 @@ set s $ptkControlPanel.buttons_$starID
 # If a window with the right name already exists, we assume it was
 # created by a previous run of the very same star, and hence can be
 # used for this new run.  Some trickiness occurs, however, because
-# parameter values may have changed, including the number of inputs.
+# parameter values may have changed, including the number of outputs.
 
 if {[winfo exists $s]} {
     set window_previously_existed 1
@@ -63,14 +63,28 @@ if {[set ${starID}(label)] != {}} {
     $s.msg configure -text [set ${starID}(label)]
 }
 
-# If allow_simultaneous_events is TRUE, then we do nothing at the time this is called.
-if {[set ${starID}(allow_simultaneous_events)]} {
-    proc setOut_$starID {num} {}
-} {
-    proc setOut_$starID {num} "setOutput_$starID \$num [set ${starID}(value)]"
+# Create the individual event buttons.
+for {set i 1} {$i <= [set ${starID}(numOutputs)]} {incr i} {
+    # Don't use ptkMakeButton here because I want more compact spacing.
+    set but $s.b.b$i
+    catch {destroy $but}
+    button $but -text [lindex [set ${starID}(identifiers)] [expr $i-1]]
+    pack $but -fill x -expand yes -anchor e
+
+    if {[set ${starID}(allow_simultaneous_events)]} {
+	# If allow_simultaneous_events is TRUE, then we do nothing
+	# at the time an event button is pressed.  In particular,
+	# leave the pressed button sunken.
+	$but configure -command "$but configure -relief sunken"
+    } {
+	$but configure -command "setOutput_$starID $i [set ${starID}(value)]"
+    }
 }
 
-proc ptkButtonsClearButtons {win numOut starID val} {
+
+# If allow_simultaneous_events is TRUE, then this is called when the
+# "PUSH TO PRODUCE EVENTS" button is pressed.
+proc tkButtonsClearButtons_$starID {win numOut starID val} {
     for {set i 1} {$i <= $numOut} {incr i} {
 	if {[lindex [$win.b.b$i configure -relief] 4] == {sunken}} {
 	    setOutput_$starID $i $val
@@ -78,31 +92,14 @@ proc ptkButtonsClearButtons {win numOut starID val} {
 	}
     }
 }
-
-for {set i 1} {$i <= [set ${starID}(numOutputs)]} {incr i} {
-    # Don't use ptkMakeButton here because I want more compact spacing
-    # Also, customize the binding to the button
-    set but $s.b.b$i
-    catch {destroy $but}
-    button $but -text [lindex [set ${starID}(identifiers)] [expr $i-1]] \
-	    -command "setOut_$starID $i"
-    pack $but -fill x -expand yes -anchor e
-    bind $but <ButtonPress-1> "$but configure -relief sunken; $but invoke"
-
-    if {[set ${starID}(allow_simultaneous_events)]} {
-	# Do nothing.  In particular, leave the button sunken
-	bind $but <ButtonRelease-1> ""
-    } {
-	bind $but <ButtonRelease-1> "$but configure -relief raised"
-    }
-}
 if {[set ${starID}(allow_simultaneous_events)]} {
-    # Create a button that causes output events to be produced
+    # Create a button that causes output events to be produced.
     # It is put in a frame with class Attention to get a different color.
     catch {destroy $s.b.sync}
     frame $s.b.sync -class Attention
     ptkMakeButton $s.b.sync b "PUSH TO PRODUCE EVENTS" \
-	    "ptkButtonsClearButtons $s [set ${starID}(numOutputs)] $starID [set ${starID}(value)]"
+	    "tkButtonsClearButtons_$starID $s [set ${starID}(numOutputs)] \
+	    $starID [set ${starID}(value)]"
     pack $s.b.sync -fill x
 }
 
@@ -110,17 +107,14 @@ if {!$window_previously_existed} {
 
     pack $s.b -fill x
 
-    proc destructorTcl_$starID {starID} {
-	global $starID
-	if {[set ${starID}(put_in_control_panel)]} {
-	    # Remove the buttons from the control panel, if they still exist
-	    global ptkControlPanel
-	    destroy $ptkControlPanel.buttons_$starID
-	}
-    }
+    proc destructorTcl_$starID {starID} "
+	# Remove the buttons
+	destroy $s
+
+	rename tkButtonsClearButtons_$starID \{\}
+    "
 
     tkwait visibility $s
 }
 
-unset s
-
+unset s window_previously_existed
