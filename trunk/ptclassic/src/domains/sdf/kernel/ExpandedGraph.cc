12e30301a7495121cc4d6457eb06ac0083b705a3
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-04-24 14:42:23 +0000

    Need explicit cast to int here for sol2.cfront, otherwise cgc:multirate:loop will consume all swap because num will be very large.  See bug cg56/254.

--- trunk/ptclassic/src/domains/sdf/kernel/ExpandedGraph.cc
+++ trunk/ptclassic/src/domains/sdf/kernel/ExpandedGraph.cc
@@ -124,7 +124,10 @@ void ExpandedGraph::initialize_invocations()
 // class use this fact for efficiency reason (e.g. ParNode constructor).
 void ExpandedGraph::createInvocations(DataFlowStar* s)
 {
-	int num = s->repetitions;
+        // Need explicit cast to int here for sol2.cfront, otherwise
+        // cgc:multirate:loop will consume all swap because num will
+        // be very large.  See bug cg56/254.
+	int num = (int) s->repetitions;
 	EGNode* prev = 0;
 	for (int i = 1; i <= num ; i++) {
 		EGNode * new_node = newNode(s,i);
