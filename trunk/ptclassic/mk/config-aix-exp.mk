dd291dec25f841ea3da51577f8069924b7a68d48
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-05-23 15:06:39 +0000

    Xavier Warzee: Added sdfcontrib, removed EXP_OPTS.

--- trunk/ptclassic/mk/config-aix-exp.mk
+++ trunk/ptclassic/mk/config-aix-exp.mk
@@ -79,6 +79,7 @@ libsdf.exp\
 libsdfatmstars.exp\
 libsdfdspstars.exp\
 libsdfimagestars.exp\
+libsdfcontrib.exp\
 libsdfmatrixstars.exp\
 libsdfstars.exp\
 libsdftclstars.exp\
@@ -95,65 +96,6 @@ libvhdlfstars.exp\
 libgantt.exp\
 libpigi.exp\
 libX11.exp
-
-EXP_OPTS=\
- -Xlinker -bE:$(LIBDIR)/libDC.exp\
- -Xlinker -bE:$(LIBDIR)/libDL.exp\
- -Xlinker -bE:$(LIBDIR)/libHu.exp\
- -Xlinker -bE:$(LIBDIR)/libImage.exp\
- -Xlinker -bE:$(LIBDIR)/libLS.exp\
- -Xlinker -bE:$(LIBDIR)/libPar.exp\
- -Xlinker -bE:$(LIBDIR)/libatm.exp\
- -Xlinker -bE:$(LIBDIR)/libbdf.exp\
- -Xlinker -bE:$(LIBDIR)/libbdfstars.exp\
- -Xlinker -bE:$(LIBDIR)/libcg.exp\
- -Xlinker -bE:$(LIBDIR)/libcg56.exp\
- -Xlinker -bE:$(LIBDIR)/libcg56dspstars.exp\
- -Xlinker -bE:$(LIBDIR)/libcg56stars.exp\
- -Xlinker -bE:$(LIBDIR)/libcg96.exp\
- -Xlinker -bE:$(LIBDIR)/libcg96dspstars.exp\
- -Xlinker -bE:$(LIBDIR)/libcg96stars.exp\
- -Xlinker -bE:$(LIBDIR)/libcgc.exp\
- -Xlinker -bE:$(LIBDIR)/libcgcm5.exp\
- -Xlinker -bE:$(LIBDIR)/libcgcstars.exp\
- -Xlinker -bE:$(LIBDIR)/libcgctcltk.exp\
- -Xlinker -bE:$(LIBDIR)/libcgstars.exp\
- -Xlinker -bE:$(LIBDIR)/libcompileSDF.exp\
- -Xlinker -bE:$(LIBDIR)/libddf.exp\
- -Xlinker -bE:$(LIBDIR)/libddfstars.exp\
- -Xlinker -bE:$(LIBDIR)/libde.exp\
- -Xlinker -bE:$(LIBDIR)/libdeatmstars.exp\
- -Xlinker -bE:$(LIBDIR)/libdestars.exp\
- -Xlinker -bE:$(LIBDIR)/libdetclstars.exp\
- -Xlinker -bE:$(LIBDIR)/libhof.exp\
- -Xlinker -bE:$(LIBDIR)/libhofstars.exp\
- -Xlinker -bE:$(LIBDIR)/libgslider.exp\
- -Xlinker -bE:$(LIBDIR)/libmdsdf.exp\
- -Xlinker -bE:$(LIBDIR)/libmdsdfstars.exp\
- -Xlinker -bE:$(LIBDIR)/libmdsdftclstars.exp\
- -Xlinker -bE:$(LIBDIR)/libmq.exp\
- -Xlinker -bE:$(LIBDIR)/libptk.exp\
- -Xlinker -bE:$(LIBDIR)/libptolemy.exp\
- -Xlinker -bE:$(LIBDIR)/libsdf.exp\
- -Xlinker -bE:$(LIBDIR)/libsdfatmstars.exp\
- -Xlinker -bE:$(LIBDIR)/libsdfdspstars.exp\
- -Xlinker -bE:$(LIBDIR)/libsdfimagestars.exp\
- -Xlinker -bE:$(LIBDIR)/libsdfmatrixstars.exp\
- -Xlinker -bE:$(LIBDIR)/libsdfstars.exp\
- -Xlinker -bE:$(LIBDIR)/libsdftclstars.exp\
- -Xlinker -bE:$(LIBDIR)/libsilage.exp\
- -Xlinker -bE:$(LIBDIR)/libsilagestars.exp\
- -Xlinker -bE:$(LIBDIR)/libsproc.exp\
- -Xlinker -bE:$(LIBDIR)/libsprocstars.exp\
- -Xlinker -bE:$(LIBDIR)/libthor.exp\
- -Xlinker -bE:$(LIBDIR)/libthorstars.exp\
- -Xlinker -bE:$(LIBDIR)/libvhdlb.exp\
- -Xlinker -bE:$(LIBDIR)/libvhdlbstars.exp\
- -Xlinker -bE:$(LIBDIR)/libvhdlf.exp\
- -Xlinker -bE:$(LIBDIR)/libvhdlfstars.exp\
- -Xlinker -bE:$(LIBDIR)/libgantt.exp\
- -Xlinker -bE:$(LIBDIR)/libpigi.exp\
- -Xlinker -bE:$(LIBDIR)/libX11.exp
 
 $(EXP): $(LIB)
 
