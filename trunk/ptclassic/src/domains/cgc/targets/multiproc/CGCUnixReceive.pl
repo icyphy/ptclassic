9a44c4e316963a3147ee827c9cb6fc4d6d508206
sha <sha@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-07-19 13:56:56 +0000

    use one socket per processor pairs. Previously, the number of connections are determined by the number of send/receive pairs.

--- trunk/ptclassic/src/domains/cgc/targets/multiproc/CGCUnixReceive.pl
+++ trunk/ptclassic/src/domains/cgc/targets/multiproc/CGCUnixReceive.pl
@@ -28,71 +28,15 @@ Produce code for inter-process communication (receive-side)
 		attributes { A_NONSETTABLE }
 	}
 	state {
-		name { IPCHandlerName }
+		name { socketId }
 		type { STRING }
-		default { "IPCHandler" }
-		desc { Name od sender's IPC handler function. }
-		attributes { A_NONSETTABLE }
-	}
-	state {
-		name { hostPort }
-		type { int }
-		default { 0 }
-		desc { Host port number }
+		default { "sid" }
+		desc { socket id }
 		attributes { A_NONSETTABLE }
 	}
 	setup {
 		numData = output.numXfer();
 	}
-
-	codeblock (ipcHandler) {
-
-    void $val(IPCHandlerName)() {
-	int len;
-	struct sockaddr_in addr, far_addr;
-
-	/* Open a TCP socket (an Internet stream socket */
-	if (($starSymbol(sId) = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("cannot open stream socket in $val(IPCHandlerName).\n");
-		exit(1);
-	}
-
-	/* Bind local address */
-	bzero((char*) &addr, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	addr.sin_port = htons($val(hostPort));
-
-	if (bind($starSymbol(sId),(struct sockaddr*) &addr,sizeof(addr)) < 0) {
-		printf("bind on port $val(hostPort) failed.\n");
-		exit(1);
-	}
-
-	listen($starSymbol(sId), 5);
-
-	/* wait for connection from a sender */
-	len = sizeof(far_addr);
-	$starSymbol(newSId) = accept($starSymbol(sId), 
-		(struct sockaddr*) &far_addr, &len);
-	if ($starSymbol(newSId) < 0) {
-		printf("accept error on port $val(hostPort).\n");
-		exit(1);
-	}
-	printf("accept connection on port $val(hostPort).\n");
-    }	
-	}
-	initCode {
-		StringList IPCproc = processCode(ipcHandler);	
-		addProcedure(IPCproc, IPCHandlerName);
-		addGlobal("int $starSymbol(sId);\n");
-		addGlobal("int $starSymbol(newSId);\n");
-		addInclude("<stdio.h>");
-		addInclude("<sys/types.h>");
-		addInclude("<sys/socket.h>");
-		addInclude("<netinet/in.h>");
-		addInclude("<arpa/inet.h>");
-	}
-		
 	codeblock (block) {
 	int i, pos;
 	union {
@@ -102,7 +46,7 @@ Produce code for inter-process communication (receive-side)
 
 	for (i = 0; i < $val(numData); i++) {
 		pos = $val(numData) - 1 + i;
-		if (read($starSymbol(newSId), &myData.l, sizeof(myData)) != sizeof(myData)) {
+		if (read($val(socketId), &myData.l, sizeof(myData)) != sizeof(myData)) {
 			printf("write fails.\n");
 			break;
 		}
@@ -112,10 +56,6 @@ Produce code for inter-process communication (receive-side)
 	}
 	go {
 		addCode(block);
-	}
-	wrapup {
-		addCode("\tclose($starSymbol(sId));\n");
-		addCode("\tclose($starSymbol(newSId));\n");
 	}
 }
 
