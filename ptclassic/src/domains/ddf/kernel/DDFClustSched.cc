e4ab771d3bb942b68e259dd3ee37cedf6899a9f3
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
1995-08-09 17:34:59 +0000

    Added support for runUntilDeadlock option. Changed "hint" to "pragma". Removed call to initStructures, which was irrelevant. I also made various changes to the scheduling algorithm to try to preserve bounded memory, but ths scheduler is still not complete.

--- ptclassic/src/domains/ddf/kernel/DDFClustSched.cc
+++ ptclassic/src/domains/ddf/kernel/DDFClustSched.cc
@@ -114,9 +114,6 @@ void DDFClustSched::initStructures() {
     restructured = TRUE;
   }
 
-  if (!amISDF)
-    DynDFScheduler::initStructures();
-
   Block* gpar = galaxy()->parent();
   if (gpar && gpar->isItWormhole()) 
     renewNumTokens(gpar, FALSE);
@@ -164,7 +161,7 @@ void DDFClustSched :: selectScheduler(SDFClusterGal& cgalaxy) {
 // To enable restrucuturing, you must set the appropriate target parameter.
 //
 DDFClustSched::DDFClustSched (const char* log):
-	logFile(log), logstrm(0), realSched(0), cgal(0) {
+	logFile(log), logstrm(0), realSched(0), cgal(0), runUntilDeadlock(0) {
   amISDF = FALSE;
   restructured = FALSE;
 }
@@ -180,30 +177,30 @@ DDFClustSched::~DDFClustSched() {
 
 ///////////////////////////////////////////////////////////////////////
 // Check to see whether the star associated with the specified atomic
-// cluster has a targetHint registered with it.  As a side effect,
-// the value of the hint will be parsed and stored in the star.
+// cluster has a pragma registered with it.  As a side effect,
+// the value of the pragma will be parsed and stored in the star.
 // In addition, a pointer to the star will be added to the
-// hintStars list.
+// pragmaStars list.
 //
-int DDFClustSched :: hintRegistered(SDFAtomCluster* as) {
+int DDFClustSched :: pragmaRegistered(SDFAtomCluster* as) {
   // For efficiency, check to see whether as->getIter() returns
-  // non-zero.  This means we have already parsed a hint.
+  // non-zero.  This means we have already parsed a pragma.
   if (as->real().getIter() > 0) return 1;
 
   // The following conditional is a hopefully unnecessary precaution,
   // since the star should certainly have a target.
   if (as->real().target()) {
-    // The registered name is a concatenation of the parent classname
+    // The pragma is registered under the parent classname
     // and the name of the star.
-    StringList nm;
-    nm << as->real().parent()->className() << "." << as->real().name();
-    StringList hint = as->real().target()->hint(nm,"firingsPerIteration");
-    if (hint.length() != 0) {
+    StringList pragma = as->real().target()->pragma(as->real().parent()->className(),
+						    as->real().name(),
+						    "firingsPerIteration");
+    if (pragma.length() != 0) {
       // Store the required number of iterations of the cluster.
       // Note that this will register a zero if no integer is recognized
       // in the specified string.
-      as->real().setIter(atoi((const char*)hint));
-      hintStars.append(&(as->real()));
+      as->real().setIter(atoi((const char*)pragma));
+      pragmaStars.append(&(as->real()));
       return 1;
     }
   }
@@ -214,7 +211,7 @@ int DDFClustSched :: hintRegistered(SDFAtomCluster* as) {
 // Group SDF stars as a cluster.
 // This grouping will hopefully improve the runtime speed by using the
 // compile-time SDF scheduler.  If a star has been annotated with the
-// firingsPerIteration target hint, then do not cluster it.
+// firingsPerIteration target pragma, then do not cluster it.
 // The considerable overhead of having a parallel tree, however, makes
 // it doubtful that this is really faster than straight DDF scheduling,
 // except for very long runs.
@@ -233,7 +230,7 @@ int DDFClustSched :: makeSDFClusters (SDFClusterGal& galaxy) {
   SDFCluster* c;
 
   // Reset visit flag and iteration count.
-  // The iteration count is used in this scheduler to store the users
+  // The iteration count is used in this scheduler to store the user's
   // requested number of iterations for each star.  It can be nonzero
   // only for atomic clusters.
   while ((c = nextClust++) != 0) {
@@ -245,8 +242,8 @@ int DDFClustSched :: makeSDFClusters (SDFClusterGal& galaxy) {
     }
   }
 
-  // Clear the list of stars for which hints have been registered.
-  hintStars.initialize();
+  // Clear the list of stars for which pragmas have been registered.
+  pragmaStars.initialize();
 
   nextClust.reset();
   while ((c = nextClust++) != 0) {
@@ -254,8 +251,10 @@ int DDFClustSched :: makeSDFClusters (SDFClusterGal& galaxy) {
 
     SDFAtomCluster* as = (SDFAtomCluster*) c;
     // Skip clustering if either the star is not SDF or a target
-    // hint of type "firingsPerIteration" has been registered with it.
-    if (!as->isSDF() || hintRegistered(as)) {
+    // pragma of type "firingsPerIteration" has been registered with it.
+    // We have to call "pragmaRegistered" first to ensure that
+    // pragmas will be registered for non-sdf stars.
+    if (pragmaRegistered(as) || !as->isSDF()) {
       flagDDF = TRUE;
       continue;
     }
@@ -292,8 +291,8 @@ int DDFClustSched :: expandCluster (SDFCluster* c,
     if (s->asBag()) continue;
     SDFAtomCluster* as = (SDFAtomCluster*) s;
     // Skip clustering if the farside star is not SDF or has a
-    // "firingsPerIteration" hint registered with it.
-    if (!as->real().isSDF() || hintRegistered(as)) continue;
+    // "firingsPerIteration" pragma registered with it.
+    if (!as->real().isSDF() || pragmaRegistered(as)) continue;
     s->setVisit(1);
     if (!expandCluster(s, galaxy, bag)) return FALSE;
     bag->absorb(s, galaxy);
@@ -307,16 +306,26 @@ int DDFClustSched :: expandCluster (SDFCluster* c,
 
 // The strategy of the runtime DDF scheduler is as follows:
 //
+// One iteration, by default, consists of firing as many enabled
+// and non-deferrable clustered actors as possible without firing any one
+// actor more than once.  A deferrable actor is one whose
+// output arcs all have enough data to satisfy the destination actors.
+//
 // E = enabled actors
 // D = deferrable enabled actors
 // S = source actors
+// F = actors that have fired once already in this one iteration
 //
 // Note that S is a subset of E.
 //
 // One default iteration consists of:
-//	if (E-D != 0) {
-//	  fire (E-D);
-//	} else {
+//	F = 0
+//	while (E-D-F != 0) {
+//	  fire (E-D-F)
+//	  F += E-D
+//	  update E,D
+//	}
+//	if (F == 0) {
 //	  // All enabled actors are deferrable.  Try the non-sources first.
 //	  if (E-S != 0) {
 //	    fire (E-S);
@@ -328,17 +337,16 @@ int DDFClustSched :: expandCluster (SDFCluster* c,
 //	}
 //	if (no stars fired above) deadlock;
 // 
-// One default iteration is defined to fire all clustered actors
+// This default iteration is defined to fire all clustered actors
 // at most once.  A larger notion of an iteration can be specified using
-// the targetHints mechanism to identify particular stars that must fire
+// the targetPragmas mechanism to identify particular stars that must fire
 // some specific number of times (greater than or equal to one) in
 // each iteration.  This will be accomplished by executing enough default
 // iterations until these specified numbers are met or exceeded.
 //
-// Note that a single firing of a clustered actor may in fact involve more
-// than one firing of an SDF actor within it.  This suggests that the
-// clustering may not be such a good idea.  It greatly complicates the
-// user notion of an iteration in certain circumstances.
+// Note that if a targetPragma is specified for an SDF actor, then clustering
+// is disabled for that actor.  This is because if we cluster the actor,
+// then it becomes very difficult to control how many times it fires.
 //
 int DDFClustSched :: run() {
   if (realSched) {
@@ -382,53 +390,60 @@ int DDFClustSched :: run() {
       //     are examined.
       int firedOne = FALSE;
       int firedOneInPass;
+      SequentialList enabledDefNonSources, defSources;
       do {
 	firedOneInPass = FALSE;
 	nextClust.reset();
 	while ((c = nextClust++) != 0) {
 	  if (c->visited()) continue;
-	  if (enabledState(c) == 1) { // enabled
+	  int state = enabledState(c);
+	  if (state == 1) { // enabled and non-deferrable
 	    if (haltRequested() || !c->run()) return FALSE;
 	    c->setVisit(1);
 	    firedOne = TRUE;
 	    firedOneInPass = TRUE;
+	  } else if (state == 2) { // enabled, deferrable, non-source
+	    // Construct a list of enabled, deferrable, non-source clusters
+	    // for phase two of scheduling.
+	    enabledDefNonSources.append(c);
+	  } else if (state == 3) { // source, deferrable.
+	    // Construct a list of deferrable sources for phase three.
+	    defSources.append(c);
 	  }
 	}
       } while (firedOneInPass == TRUE);
 
       // (2) If the above yielded no firings at all, then we proceed to the
-      //     step of firing all enabled actors that are not sources.
-      //	   This is done in a single pass to catch only the problematic
-      //     actors.
+      //     step of firing all enabled, deferrable actors that are not sources.
+      //     This is done in a single pass to catch only the problematic
+      //     actors. To maintain determinacy, the list of enabled actors
+      //     fired is the one constructed in phase 1 (i.e., the contents
+      //     of the list does not change as we fire actors).
       if (!firedOne) {
-	nextClust.reset();
-	while ((c = nextClust++) != 0) {
-	  // enabled or deferrable, but not a source
-	  if (enabledState(c) && !isSource(*c) && !c->visited()) {
-	    if (haltRequested() || !c->run()) return FALSE;
-	    c->setVisit(1);
-	    firedOne = TRUE;
-	  }
+	ListIter next(enabledDefNonSources);
+	while ((c = (SDFCluster*)next++) != 0) {
+	  if (haltRequested() || !c->run()) return FALSE;
+	  c->setVisit(1);
+	  firedOne = TRUE;
 	}
       }
 
       // (3) If the above still yielded no firings, then the graph
-      //     cannot be executed in bounded memory.  We now simply fire
-      //     all sources, which is simply all enabled actors.
+      //     cannot be executed in bounded memory.  All enabled actors
+      //     are deferrable sources.  We now simply fire
+      //     all these sources.
       if (!firedOne) {
-	nextClust.reset();
-	while ((c = nextClust++) != 0) {
-	  if (enabledState(c) && !c->visited()) {
-	    if (haltRequested() || !c->run()) return FALSE;
-	    c->setVisit(1);
-	    firedOne = TRUE;
-	  }
+	ListIter next(defSources);
+	while ((c = (SDFCluster*)next++) != 0) {
+	  if (haltRequested() || !c->run()) return FALSE;
+	  c->setVisit(1);
+	  firedOne = TRUE;
 	}
       }
 
       // (4) If we still have not fired a star, we are deadlocked.
       if (!firedOne) {
-	if (numFiring < stopTime) {
+	if (numFiring < stopTime && !runUntilDeadlock) {
 	  Error :: abortRun("deadlock detected: check for  ",
 			    "insufficient initial tokens on feedback arcs");
 	  return FALSE;
@@ -436,8 +451,8 @@ int DDFClustSched :: run() {
       }
 
       // Check to see whether enough firings have occurred to satisfy
-      // all registered "firingsPerIteration" hints.
-      ListIter ns(hintStars);
+      // all registered "firingsPerIteration" pragmas.
+      ListIter ns(pragmaStars);
       DataFlowStar* np;
       while ((np = (DataFlowStar*)ns++) != 0) {
 	if (!np->enoughFirings()) {
@@ -445,6 +460,8 @@ int DDFClustSched :: run() {
 	  break;
 	}
       }
+
+      if (runUntilDeadlock) doAgain = firedOne;
     } while (doAgain);
 
     // end of ONE iteration
@@ -488,51 +505,55 @@ int DDFClustSched :: isSource(Star& s) {
 // Check the input ports and return the state of the actor.
 // If it is enabled and non-deferrable, return 1.
 // If it is a non-source, enabled and deferrable, return 2.
-// Otherwise, return 0.
+// If it is a deferrable source, return 3.
+// Otherwise, return 0 (it is not enabled).
 // An actor is deferrable if all its output connections already
 // have enough data to enable the downstream actor.
 //
 int DDFClustSched :: enabledState(SDFCluster* s) {
 
-  // If s is a bag, then s is an SDF cluster.
-
-  if (s->asBag()) {
-    if (blockedOnInput(s) > 0) return 0;	   // not enabled
-    else if (!isOutputDeferrable(s)) return 1; // enabled
-    else return 2;
-  }
-
-  // If s is not a cluster, then check to see whether it
-  // has a waitPort (a port it is waiting for data on), and
-  // whether that port has enough data.
+  // If s is an atomic cluster (one star only), then the star might be
+  // a DDF star. Check to see whether it has a waitPort (a port it is
+  // waiting for data on), and whether that port has enough data.
   // FIXME: Note that this code depends on the fact that if
   // s is not an SDFClusterBag, then it must be an SDFAtomCluster.
   // This is true now, but if more classes are added, things could change.
+  // If s is a bag, then s is an SDF cluster.
 
-  SDFAtomCluster* as = (SDFAtomCluster*) s;
-  DataFlowStar& ds = as->real();
-  DFPortHole* wp = ds.waitPort();
-  if (wp) {
-    // If fewer tokens are available than are needed, return 0
-    if (wp->numTokens() < ds.waitTokens()) return 0;
+  if (!s->asBag()) {
+    SDFAtomCluster* as = (SDFAtomCluster*) s;
+    DataFlowStar& ds = as->real();
+    DFPortHole* wp = ds.waitPort();
+    if (wp) {
+      // If fewer tokens are available than are needed, not enabled.
+      if (wp->numTokens() < ds.waitTokens()) return 0;
 
-    // If there are enough inputs, but the output ports have
-    // enough data already, return 1.
-    if (!isOutputDeferrable(s)) return 1;
-  } 
+      // If there are enough inputs, and the output ports do not have
+      // enough data already, enabled and not deferrable.
+      if (!isOutputDeferrable(s)) return 1;
+
+      // The actor is enabled and deferrable.  It cannot possibly be
+      // a source, since it has a wait port.
+      return 2;
+    }
+  }
 
   // If s is not a cluster and has no waitPort, then check all inputs
   // for enough data, and check outputs for enough data.
 
-  if (blockedOnInput(s) > 0) return 0;
-  else if (!isOutputDeferrable(s)) return 1;
-  else return 2;
+  if (blockedOnInput(s) > 0) return 0;         // not enabled
+  // enabled
+  else if (!isOutputDeferrable(s)) return 1;   // enabled, not deferrable
+  // enabled and deferrable
+  else if (!isSource(*s)) return 2;            // enabled, deferrable, non-source
+  // enabled, deferrable source
+  else return 3;                               // enabled, deferrable, source
 }
 
 ///////////////////////////////////////////////////////////////////////
 // Check to see whether a cluster is blocked on input.
 // Return 1 if it is, 0 otherwise.  So a return value of zero
-// means that the cluster is enabled.
+// means that the cluster is enabled (which could means it's a source).
 //
 int DDFClustSched :: blockedOnInput(SDFCluster* s) {
   SDFClustPortIter nextPort(*s);
@@ -554,8 +575,9 @@ int DDFClustSched :: blockedOnInput(SDFCluster* s) {
 }
 
 ///////////////////////////////////////////////////////////////////////
-// Check to see whether the outputs have enough data already to
-// satisfy downstream arcs.  If there are no outputs, always return FALSE.
+// Check to see whether all the outputs have enough data already to
+// satisfy downstream arcs.  If there are no outputs, always return FALSE
+// (this prevents deferring of sink actors).
 //
 int DDFClustSched :: isOutputDeferrable(SDFCluster* s) {
   SDFClustPortIter nextPort(*s);
@@ -563,17 +585,20 @@ int DDFClustSched :: isOutputDeferrable(SDFCluster* s) {
   int outputFound = FALSE;
   while ((p = nextPort++) != 0) {
     if (p->isItInput()) continue;
-    SDFClustPort* inp = p;
+    SDFClustPort* outp = p;
     outputFound = TRUE;
-    while (inp->inPtr()) inp = inp->inPtr();
-    DFPortHole& dp = inp->real();
+
+    // Find the actual porthole.
+    while (outp->inPtr()) outp = outp->inPtr();
+    DFPortHole& dp = outp->real();
 
     // If output is not connected, continue
-    if (!dp.far()) continue;
+    PortHole* far = dp.far();
+    if (!far) continue;
 
-    DataFlowStar* ds = (DataFlowStar*) dp.far()->parent();
+    DataFlowStar* ds = (DataFlowStar*) far->parent();
     DFPortHole* wp = ds->waitPort();
-    if (wp && (dp.far() == (PortHole*) wp)) {
+    if (wp && (far == (PortHole*) wp)) {
       // The downstream actor is waiting for data on this port.
       // If the number of tokens available is too small...
       if (wp->numTokens() < ds->waitTokens()) return 0;
