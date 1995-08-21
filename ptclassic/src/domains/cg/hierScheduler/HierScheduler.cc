dca5ec90a12271c342a50766c2cfd332295a378f
Jose Luis Pino <joseluispino@users.noreply.github.com>
1995-08-21 15:39:08 +0000

    Now sets loopingLevel for all child targets.... some targets must now that they are looping to produce correct code

--- ptclassic/src/domains/cg/hierScheduler/HierScheduler.cc
+++ ptclassic/src/domains/cg/hierScheduler/HierScheduler.cc
@@ -82,6 +82,13 @@ void MultiScheduler :: setup () {
     // Now fix buffer sizes
     topCluster->fixBufferSizes(1);
 
+    // Set all looping levels for child targets > 0.  Targets might
+    // have to do different style buffering (ie. CGC)
+    int childNum = 0;
+    while (CGTarget* child = mtarget->cgChild(childNum++)) {
+      child->loopingLevel = 3;
+    }
+    
     return;
 }
 
