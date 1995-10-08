4f666ca74a98076d86130b8bea0d15ce8c8b731a
Christopher Brooks <cxh@eecs.berkeley.edu>
1995-10-08 01:40:33 +0000

    Added sccs vars bak.

--- ptclassic/src/octtools/tkoct/tkoct/appinit.c
+++ ptclassic/src/octtools/tkoct/tkoct/appinit.c
@@ -1,8 +1,8 @@
 /*******************************************************************
 SCCS version identification
-@(#)appinit.c	1.4 11/11/94
+$Id$
 
-Copyright (c) 1990-1994 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
