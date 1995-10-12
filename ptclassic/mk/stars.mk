444717d73ea59f9e78217976c30e598a1c3ede96
Jose Luis Pino <joseluispino@users.noreply.github.com>
1995-10-12 14:20:22 +0000

    moved ptcl library to stars.mk

--- ptclassic/mk/stars.mk
+++ ptclassic/mk/stars.mk
@@ -433,4 +433,4 @@ endif
 PT_DEPEND += $(LIBDIR)/libptcl.$(LIBSUFFIX) $(LIBDIR)/libptolemy.a \
 	$(LIBFILES) $(STARS)
 
-LIBS += -lptolemy $(ITCL_LIBSPEC) $(TCL_LIBSPEC) $(SYSLIBS) $(LIB_FLUSH_CACHE)
+LIBS += -lptcl -lptolemy $(ITCL_LIBSPEC) $(TCL_LIBSPEC) $(SYSLIBS) $(LIB_FLUSH_CACHE)
