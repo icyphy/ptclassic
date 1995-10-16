568d8fde3db3dbb2981c25cf5280affc9c9cc32f
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-10-16 17:59:07 +0000

    Corrected error in computing window values for Barlett windows and added several comments

--- trunk/ptclassic/src/domains/cg56/dsp/stars/CG56Window.pl
+++ trunk/ptclassic/src/domains/cg56/dsp/stars/CG56Window.pl
@@ -4,10 +4,10 @@ defstar {
 	derivedFrom { WaveForm }
 	desc {
 Generates standard window functions:
-Rectangle, Hanning, Hamming, Blackman, and SteepBlackman.
+Rectangle, Bartlett, Hanning, Hamming, Blackman, and SteepBlackman.
 }
-	version {$Id$}
-	author { Jose Luis Pino, based on SDFWaveForm by Kennard White}
+	version { $Id$ }
+	author { Jose Luis Pino, based on CG56WaveForm by Kennard White}
 	copyright {
 Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
@@ -35,11 +35,15 @@ window length (i.e, period=0).
 .lp
 One period of samples are produced on every firing.
 	}
+
 	state {
 		name { name }
 		type { string }
 		default { "Hanning" }
-		desc { Name of the window function to generate. }
+		desc {
+Name of the window function to generate:
+Rectangle, Bartlett, Hanning, Hamming, Blackman, or SteepBlackman.
+		}
 	}
 	state {
 		name { length }
@@ -48,20 +52,22 @@ One period of samples are produced on every firing.
 		desc { Length of the window function to produce. }
 		attributes { A_SETTABLE|A_CONSTANT }
 	} 
-    protected {
-	int winType;
-	int realLen;
-	int realPeriod;
-	double scale0;
-	double scale1;
-	double freq1;
-	double scale2;
-	double freq2;
-    }
-    code {
-	extern "C" {
-	    extern int strcasecmp(const char*,const char*);
-	}
+
+	protected {
+		int winType;
+		int realLen;
+		int realPeriod;
+		double scale0;
+		double scale1;
+		double freq1;
+		double scale2;
+		double freq2;
+    	}
+
+    	code {
+		extern "C" {
+		    extern int strcasecmp(const char*, const char*);
+		}
 #define SDFWinType_Null		(0)
 #define SDFWinType_Rectangle	(1)
 #define SDFWinType_Bartlett	(2)
@@ -69,10 +75,12 @@ One period of samples are produced on every firing.
 #define SDFWinType_Hamming	(4)
 #define SDFWinType_Blackman	(5)
 #define SDFWinType_SteepBlackman	(6)
-    }
-	constructor {
-	value.setAttributes(A_NONSETTABLE|A_CONSTANT);
 	}
+
+	constructor {
+		value.setAttributes(A_NONSETTABLE|A_CONSTANT);
+	}
+
 	setup {
 	const char *wn = name;
 
@@ -89,72 +97,78 @@ One period of samples are produced on every firing.
 	} else if ( strcasecmp( wn, "SteepBlackman")==0 ) {
 	    winType = SDFWinType_SteepBlackman;
 	} else {
-	    Error::abortRun(*this, ": Unknown window name");
+	    Error::abortRun(*this, ": Unknown window name ", wn);
 	    return;
 	}
+
+	// Don't want to risk divide by zero
 	realLen = int(length);
 	if ( realLen < 4 ) {
-	    /* Dont want to risk divide by zero */
 	    Error::abortRun(*this, ": Window length too small");
 	    return;
 	}
+
 	double base_w = M_PI/(realLen-1);
 	double sin_base_w = sin(base_w);
 	double sin_2base_w = sin(2*base_w);
 	double d = - (sin_base_w/sin_2base_w) * (sin_base_w/sin_2base_w);
 	scale0 = 0; scale1 = 0; freq1 = 0; scale2 = 0; freq2 = 0;
-	/* 
-	 *  Window defs taken from Jackson, Digital Filters and Signal
-	 *  Processing, Second Ed, chap 7.
-	 */
+
+	// Window defs taken from Jackson, Digital Filters and Signal
+	//  Processing, Second Ed, chap 7.
 	switch ( winType ) {
-	case SDFWinType_Rectangle:
+	  case SDFWinType_Rectangle:
 	    scale0 = 1;
 	    break;
-	case SDFWinType_Bartlett:
+	  case SDFWinType_Bartlett:
 	    scale0 = 0;		scale1 = 2.0/realLen;
 	    break;
-	case SDFWinType_Hanning:
+	  case SDFWinType_Hanning:
 	    scale0 = .5;	scale1 = -.5;	freq1 = 2*base_w;
 	    break;
-	case SDFWinType_Hamming:
+	  case SDFWinType_Hamming:
 	    scale0 = .54;	scale1 = -.46;	freq1 = 2*base_w;
 	    break;
-	case SDFWinType_Blackman:
-	    /* This is a special case of SteepBlackman */
-	    d = -.16;		/* scale0 = .42, scale1 = -.5, scale2=.08 */
-	    /*FALLTHROUGH*/
+	  case SDFWinType_Blackman:
+	    // This is a special case of SteepBlackman
+	    d = -.16;		// scale0 = .42, scale1 = -.5, scale2=.08
+	    // FALLTHROUGH
 	case SDFWinType_SteepBlackman:
-	    /* See Jackson 2ed, eqns 7.3.6 through 7.3.10 */
-	    scale0 = (d+1)/2;	scale1 = -.5;	freq1 = 2*base_w;
-	    			scale2 = -d/2;	freq2 = 4*base_w;
+	    // See Jackson 2ed, eqns 7.3.6 through 7.3.10
+	    scale0 = (d+1)/2;
+	    scale1 = -.5;
+	    freq1 = 2*base_w;
+	    scale2 = -d/2;
+	    freq2 = 4*base_w;
 	    break;
 	default:
 	    Error::abortRun(*this, ": Invalid window type");
 	    return;
 	}
-	    
+
 	value.resize(int(length));
 	StringList window;
 	int i;
 	double val;
 
 	switch ( winType ) {
-	case SDFWinType_Bartlett:
+	  case SDFWinType_Bartlett:
 	    for ( i=0; i < realLen/2; i++) {
 		val = scale1 * (i+1);
 		window << val << " ";
 	    }
 	    for ( ; i < realLen; i++) {
 		val = scale1 * (realLen-(i+1));
+		// Outputs created at the time the star is compiled
+		// Handled as a special case by Waveform::go method.
 		output%(realPeriod-(i+1)) << val;
 		window << val;
-		if (i!=realLen-1) window = " ";
+		if (i != realLen-1) window << " ";
 	    }
 	    break;
-	default:
+	  default:
 	    for ( i=0; i < realLen; i++) {
-	    if (i!=0) window << " ";
+		if (i!=0) window << " ";
 		val = scale0 + scale1 * cos(freq1*i) + scale2 * cos(freq2*i);
 		window << val;
 	    }
