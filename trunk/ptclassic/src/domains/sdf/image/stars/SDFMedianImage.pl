5dfb0015cced2c35935dcb04fb33cee65b834a5b
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-10-20 21:27:16 +0000

    Fixed bug in retMedian method which returned the wrong index of the sorted data values as the median value and defined the output image at the image boundaries to be a copy of boundary pixels from the input image

--- trunk/ptclassic/src/domains/sdf/image/stars/SDFMedianImage.pl
+++ trunk/ptclassic/src/domains/sdf/image/stars/SDFMedianImage.pl
@@ -11,24 +11,22 @@ limitation of liability, and disclaimer of warranty provisions.
 	}
 	location	{ SDF image library }
 	desc {
-Accept an input GrayImage, median-filter the image, and send the result
-to the output.
+Accept an input GrayImage, median filter the image, and send the result
+to the output.  Filter widths of 1, 3, 5 work well.  Any length longer
+than five will take a long time to run.
 
-Filter widths of 1, 3, 5 work well. Any length longer than five will
-take a long time to run.
-
-Median filtering is useful for removing impulse-type noise from images.
+Median filtering is useful for removing impulsive noise from images.
 It also smooths out textures, so it is a useful pre-processing step
-before edge detection. It removes inter-field flicker quite well
+before edge detection.  It removes inter-field flicker quite well
 when displaying single frames from a moving sequence.
 	}
 	explanation {
+Pixels at the image boundaries are copied and not median filtered.
 .Id "filter, median"
 .Id "image, median filter"
 .Id "median filtering"
 	}
 
-// INPUT AND STATES.
 	input { name { inData } type { message } }
 
 	output { name { outData } type { message } }
@@ -40,13 +38,12 @@ when displaying single frames from a moving sequence.
 		desc	{ Size of median filter window (should be odd number). }
 	}
 
-////// CODE
 	ccinclude { "GrayImage.h", <std.h> }
 
 	code {
-// Function to sort unsigned char's.
-// These types and casts are required to satisfy cfront's
-// ridiculously strict rules.
+		// Function to sort unsigned char's.
+		// These types and casts are required to satisfy cfront's
+		// ridiculously strict rules.
 		extern "C" {
 			static int sortUC(const void* aV, const void* bV) {
 				unsigned const char* a = (unsigned const char*) aV;
@@ -66,9 +63,9 @@ when displaying single frames from a moving sequence.
 	setup {
 		size = int(FilterWidth);
 		if (size <= 0) { size = 3; }
-		size = 1 + 2*(size/2); // an odd number for sure
+		size = 1 + 2*(size/2);		// an odd number for sure
 		delete [] buf;
-		LOG_NEW; buf = new unsigned char[size*size];
+		buf = new unsigned char[size*size];
 	}
 
 	destructor { delete [] buf; }
@@ -79,47 +76,60 @@ when displaying single frames from a moving sequence.
 		type	{ "unsigned char" }
 		arglist { "(unsigned const char* p, int pi, int pj)" }
 		code {
-		// Copy data to buf.
-			int i, j, cnt = 0;
-			for(i = pi-size/2; i <= pi+size/2; i++) {
-				for(j = pj-size/2; j <= pj+size/2; j++) {
+			// Copy data to buf.
+			int cnt = 0;
+			for(int i = pi-size/2; i <= pi+size/2; i++) {
+				for(int j = pj-size/2; j <= pj+size/2; j++) {
 					buf[cnt++] = p[i*width+j];
-			}	}
+				}
+			}
 
-		// Sort data and return result.
-			qsort(buf, size*size, sizeof(unsigned char), sortUC);
-			return(buf[(size/2) * (size+1)]); // median value.
+			// Sort data
+			int numitems = size * size;
+			qsort(buf, numitems, sizeof(unsigned char), sortUC);
+
+			// Data indexed from 0 ... numitems - 1
+			// Median value is at the middle index
+			return buf[ (numitems - 1)/2 ];
 		} // end code {}
 	} // end method { retMedian }
 
 
 	go {
-// Read data from input and initialize.
+		// Read data from input and initialize.
 		Envelope inEnvp;
 		(inData%0).getMessage(inEnvp);
 		TYPE_CHECK(inEnvp, "GrayImage");
 		const GrayImage* inImage = (const GrayImage*) inEnvp.myData();
 		if (inImage->fragmented() || inImage->processed()) {
-			Error::abortRun(*this,
-					"Need a full-sized input image.");
-			return;
+		    Error::abortRun(*this,
+				    "Need a full-sized input image.");
+		    return;
 		}
 		width = inImage->retWidth();
 		height = inImage->retHeight();
 
-// Create output image. clone(1) prevents memory copying.
+		// Median filtering -- don't do boundary pixels for now
 		GrayImage* outImage = (GrayImage*) inImage->clone(1);
-
-// Do the median filtering--don't do boundary pixels for now.
-		int i, j;
 		unsigned char* outP = outImage->retData();
 		unsigned const char* inP = inImage->constData();
-		for(i = size/2; i < height-size/2; i++) {
-			for(j = size/2; j < width-size/2; j++) {
-				outP[j + i*width] = retMedian(inP, i, j);
-		}	}
+		int halfsize = size / 2;
+		for (int i = halfsize; i < height; i++) {
+		    int rowloc = i * width;
+		    for (int j = 0; j < width; j++) {
+			if ( (halfsize <= i) && (i < height - halfsize) &&
+			     (halfsize <= i) && (j < width - halfsize) ) {
+			    outP[j + rowloc] = retMedian(inP, i, j);
+			}
+			// Do not apply median definition at boundaries
+			else {
+			    outP[j + rowloc] = inP[j + rowloc];
+			}
+		    }
+		}
 
-// Send the output on its way.
-		Envelope outEnvp(*outImage); outData%0 << outEnvp;
+		// Send the output on its way.
+		Envelope outEnvp(*outImage);
+		outData%0 << outEnvp;
 	} // end go{}
 } // end defstar { MedianImage }
