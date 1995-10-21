6331b7fa95beb5f8454638b4a2a18062bae15971
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-10-21 15:42:33 +0000

    Added comments and further sped up star's operation

--- ptclassic/src/domains/sdf/image/stars/SDFMedianImage.pl
+++ ptclassic/src/domains/sdf/image/stars/SDFMedianImage.pl
@@ -12,8 +12,8 @@ limitation of liability, and disclaimer of warranty provisions.
 	location	{ SDF image library }
 	desc {
 Accept an input GrayImage, median filter the image, and send the result
-to the output.  Filter widths of 1, 3, 5 work well.  Any length longer
-than five will take a long time to run.
+to the output.  Filter widths of 3, 5, and 7 work well.  Any length longer
+than seven will take a long time to run.
 
 Median filtering is useful for removing impulsive noise from images.
 It also smooths out textures, so it is a useful pre-processing step
@@ -21,6 +21,8 @@ before edge detection.  It removes inter-field flicker quite well
 when displaying single frames from a moving sequence.
 	}
 	explanation {
+The \fIFilterWidth\fR parameter will always be odd.
+If an even value is specified, the next higher odd number will be used.
 Pixels at the image boundaries are copied and not median filtered.
 .Id "filter, median"
 .Id "image, median filter"
@@ -43,20 +45,23 @@ Pixels at the image boundaries are copied and not median filtered.
 	code {
 #define INT_DIVIDE_BY_TWO(d) ((d) >> 1)
 
-		// Function to sort unsigned char's.
-		// These types and casts are required to satisfy cfront's
-		// ridiculously strict rules.
-		extern "C" {
-			static int sortUC(const void* aV, const void* bV) {
-				unsigned const char* a = (unsigned const char*) aV;
-				unsigned const char* b = (unsigned const char*) bV;
-				if (*a < *b) return -1;
-				else if (*a == *b) return 0;
-				else return 1;
-	}	}	}
+	    // Function to sort unsigned char's.
+	    // These types and casts are required to satisfy cfront's
+	    // ridiculously strict rules.
+	    extern "C" {
+		static int sortUC(const void* aV, const void* bV) {
+		    unsigned const char* a = (unsigned const char*) aV;
+		    unsigned const char* b = (unsigned const char*) bV;
+		    if (*a < *b) return -1;
+		    else if (*a == *b) return 0;
+		    else return 1;
+		}
+	    }
+	}
 
 	protected {
 		int width, height, size;
+		int halfNeighborhoodSize, neighborhoodSize;
 		unsigned char* buf;
 	}
 
@@ -66,8 +71,10 @@ Pixels at the image boundaries are copied and not median filtered.
 		size = int(FilterWidth);
 		if (size <= 0) { size = 3; }
 		size = 1 + 2*(size/2);		// an odd number for sure
+		neighborhoodSize = size*size;
+		halfNeighborhoodSize = neighborhoodSize/2;
 		delete [] buf;
-		buf = new unsigned char[size*size];
+		buf = new unsigned char[neighborhoodSize];
 	}
 
 	destructor { delete [] buf; }
@@ -78,28 +85,33 @@ Pixels at the image boundaries are copied and not median filtered.
 		type	{ "unsigned char" }
 		arglist { "(unsigned const char* p, int pi, int pj)" }
 		code {
-			// Copy data to buf.
-			int cnt = 0;
-			int halfsize = INT_DIVIDE_BY_TWO(size);
-			int maxi = pi + halfsize;
-			int maxj = pj + halfsize;
-			for (int i = pi - halfsize; i <= maxi; i++) {
-				int rowloc = i * width;
-				for (int j = pj - halfsize; j <= maxj; j++) {
-					buf[cnt++] = p[rowloc + j];
-				}
+		    // Select size x size neighbor around pixel (pi, pj)
+		    // and write data to one-dimensional buffer
+		    // We want to copy pixel values at p[i][j] where
+		    //   i = (pi - halfsize) ... pi ... (pi + halfsize)
+		    //   j = (pj - halfsize) ... pj ... (pj + halfsize)
+		    // where halfsize = halfNeighborhoodSize
+		    int maxi = pi + halfNeighborhoodSize;
+		    int pjstart = pj - halfNeighborhoodSize;
+		    unsigned char *bufp = buf;
+		    for (int i = pi - halfNeighborhoodSize; i <= maxi; i++) {
+			// pijptr begins at p[i][pj - halfsize]
+			unsigned const char *pijptr = &p[i * width + pjstart];
+			for (int k = 0; k < size; k++) {
+			    *bufp++ = *pijptr++;
 			}
+		    }
 
-			// Sort data
-			int numitems = size * size;
-			qsort(buf, numitems, sizeof(unsigned char), sortUC);
+		    // Sort data
+		    qsort(buf, neighborhoodSize, sizeof(unsigned char), sortUC);
 
-			// Data indexed from 0 ... numitems - 1
-			// Median value is at the middle index
-			return buf[ (numitems - 1)/2 ];
-		} // end code {}
-	} // end method { retMedian }
-
+		    // Data indexed from 0 ... (neighborhoodSize - 1)
+		    // Median value at middle index, (neighborhoodSize - 1)/2
+		    // Because neighborhoodSize is odd, we can just use
+		    // neighborhoodSize/2
+		    return buf[ halfNeighborhoodSize ];
+		}
+	}
 
 	go {
 		// Read data from input and initialize.
@@ -112,24 +124,35 @@ Pixels at the image boundaries are copied and not median filtered.
 				    "Need a full-sized input image.");
 		    return;
 		}
+
+		// Median filtering -- don't filter boundary pixels for now
 		width = inImage->retWidth();
 		height = inImage->retHeight();
-
-		// Median filtering -- don't do boundary pixels for now
 		GrayImage* outImage = (GrayImage*) inImage->clone(1);
-		unsigned char* outP = outImage->retData();
-		unsigned const char* inP = inImage->constData();
-		int halfsize = INT_DIVIDE_BY_TWO(size);
+		unsigned char* outptr = outImage->retData();
+		unsigned const char* inptr = inImage->constData();
+		int halfsize = halfNeighborhoodSize;
 		for (int i = 0; i < height; i++) {
-		    int rowloc = i * width;
-		    for (int j = 0; j < width; j++) {
-			if ( (halfsize <= i) && (i < height - halfsize) &&
-			     (halfsize <= j) && (j < width - halfsize) ) {
-			    outP[j + rowloc] = retMedian(inP, i, j);
+		    int j = 0;
+		    if ( (halfsize <= i) && (i < height - halfsize) ) {
+			// Just copy the boundary pixels
+			for ( ; j < halfsize; j++) {
+			    *outptr++ = *inptr++;
 			}
-			// Do not apply median definition at boundaries
-			else {
-			    outP[j + rowloc] = inP[j + rowloc];
+			// Median filter
+			for ( ; j < width - halfsize; j++) {
+			    *outptr++ = retMedian(inptr, i, j);
+			    inptr++;
+			}
+			// Just copy the boundary pixels
+			for ( ; j < width; j++) {
+			    *outptr++ = *inptr++;
+			}
+		    }
+		    // Just copy the boundary pixels
+		    else {
+			for ( ; j < width; j++) {
+			    *outptr++ = *inptr++;
 			}
 		    }
 		}
