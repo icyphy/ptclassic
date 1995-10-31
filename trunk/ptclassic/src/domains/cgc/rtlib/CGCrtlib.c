2c390286d88eecbe49066fb0b90080efee086078
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-10-31 10:51:33 +0000

    Typo fix from last change

--- trunk/ptclassic/src/domains/cgc/rtlib/CGCrtlib.c
+++ trunk/ptclassic/src/domains/cgc/rtlib/CGCrtlib.c
@@ -37,7 +37,7 @@ ENHANCEMENTS, OR MODIFICATIONS.
 
 #include <stdio.h>
 #include <math.h>
-#include <memset.h>		/* declare memset for sun4 */
+#include <memory.h>		/* declare memset for sun4 */
 #include <string.h>		/* declare memset for sol2 */
 
 #include "CGCrtlib.h"
