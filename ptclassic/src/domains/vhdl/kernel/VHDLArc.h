611636707cee03e243780d487883d8cc1d4f69ad
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-10-31 15:52:45 +0000

    Initial revision

---
+++ ptclassic/src/domains/vhdl/kernel/VHDLArc.h
@@ -0,0 +1,116 @@
+#ifndef _VHDLArc_h
+#define _VHDLArc_h 1
+/******************************************************************
+Version identification:
+$Id$
+
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+
+ Programmer: Michael C. Williamson
+
+ Methods defining VHDL arcs.
+
+*******************************************************************/
+#ifdef __GNUG__
+#pragma interface
+#endif
+
+#include "VHDLObj.h"
+#include "VHDLObjList.h"
+
+class VHDLArc : public VHDLObj
+{
+ public:
+  // Constructors.
+  VHDLArc();
+  VHDLArc(const char*, Block*, const char*);
+
+  // Destructor.
+  ~VHDLArc();
+
+  // Name.
+//  StringList name;
+  // Data Type.
+  StringList type;
+  // Low write marker.
+  int lowWrite;
+  // High write marker.
+  int highWrite;
+  // Low read marker.
+  int lowRead;
+  // High read marker.
+  int highRead;
+
+  // Class Idenitification.
+  /* virtual */ int isA(const char*) const;
+  /* virtual */ const char* className() const;
+
+  // Return a pointer to a new copy of the VHDLArc.
+  VHDLArc* newCopy();
+
+ protected:
+ private:
+};
+
+class VHDLArcList : public VHDLObjList
+{
+  friend class VHDLArcListIter;
+
+ public:
+  // Add VHDLArc to list.
+  void put(VHDLArc& v) { VHDLObjList::put(v); }
+
+  // Return first VHDLArc on list (const, non-const forms).
+  VHDLArc* head() { return (VHDLArc*) VHDLObjList::head(); }
+  const VHDLArc* head() const {
+    return (const VHDLArc*) VHDLObjList::head();
+  }
+
+  // Remove a VHDLArc from the list.
+  // Note:  the VHDLArc is not deleted.
+  int remove (VHDLArc* v) { return VHDLObjList::remove(v); }
+
+  // Find VHDLArc with given name (const, non-const forms).
+  VHDLArc* vhdlArcWithName(const char* name) {
+    return (VHDLArc*) vhdlObjWithName(name);
+  }
+
+  const VHDLArc* vhdlArcWithName(const char* name) const {
+    return (const VHDLArc*) vhdlObjWithName(name);
+  }
+
+  // Return a pointer to a new copy of the list.
+  VHDLArcList* newCopy();
+
+};
+
+class VHDLArcListIter : public VHDLObjListIter {
+ public:
+  VHDLArcListIter(VHDLArcList& l) : VHDLObjListIter(l) {}
+  VHDLArc* next() { return (VHDLArc*) VHDLObjListIter::next(); }
+  VHDLArc* operator++(POSTFIX_OP) { return next(); }
+  VHDLObjListIter::reset;
+};
+
+#endif
---
+++ ptclassic/src/domains/vhdl/kernel/VHDLCluster.cc
@@ -0,0 +1,64 @@
+static const char file_id[] = "VHDLCluster.cc";
+/******************************************************************
+Version identification:
+$Id$
+
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+
+Permission is hereby granted, without written agreement and without
+license or royalty fees, to use, copy, modify, and distribute this
+software and its documentation for any purpose, provided that the above
+copyright notice and the following two paragraphs appear in all copies
+of this software.
+
+IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY 
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES 
+ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF 
+THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+							COPYRIGHTENDKEY
+
+ Programmer: Edward A. Lee, Michael C. Williamson
+
+ Methods for VHDL CompDecls.
+
+*******************************************************************/
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include "VHDLCluster.h"
+
+// Constructors.
+VHDLCluster :: VHDLCluster() {
+  VHDLObj::initialize();
+}
+
+VHDLCluster :: VHDLCluster(const char* n, Block* p, const char* d) : VHDLObj(n,p,d) {
+  VHDLObj::initialize();
+}
+
+// Destructor.
+VHDLCluster :: ~VHDLCluster() {}
+
+// Return a pointer to a new copy of the VHDLCluster.
+VHDLCluster* VHDLCluster :: newCopy() {
+  VHDLCluster* newCluster = new VHDLCluster;
+  newCluster->name = this->name;
+  newCluster->firingList = this->firingList->newCopy();
+
+  return newCluster;
+}
+
+// Class identification.
+const char* VHDLCluster :: className() const { return "VHDLCluster"; }
+
+ISA_FUNC(VHDLCluster,VHDLObj);
