fc757ab02d1ff616ff8142bf06a8b64923b9de7f
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-11-09 16:27:58 +0000

    Moved PtolemyToMatlab and MatlabToPtolemy methods to the MatlabIfc class and call them through the matlabInterface instance

--- trunk/ptclassic/src/domains/sdf/matlab/stars/SDFMatlab_M.pl
+++ trunk/ptclassic/src/domains/sdf/matlab/stars/SDFMatlab_M.pl
@@ -217,126 +217,6 @@ The values of the input ports will be passed as arguments to this function.
 	  }
 	}
 
-	method {
-	  name { PtolemyToMatlab }
-	  access { protected }
-	  type { "Matrix*" }
-	  arglist { "(Particle& particle, DataType portType, int *errflag)" }
-	  code {
-		  // can't use a switch because enumerated data types
-		  // are assigned to strings and not to integers
-		  double *realp = 0;
-		  double *imagp = 0;
-		  Matrix *matlabMatrix = 0;
-		  if ( portType == INT ||
-		       portType == FLOAT ||
-		       portType == FIX ) {
-		    matlabMatrix = mxCreateFull(1, 1, MXREAL);
-		    realp = mxGetPr(matlabMatrix);
-		    *realp = double(particle);
-		  }
-		  else if ( portType == COMPLEX ) {
-		    matlabMatrix = mxCreateFull(1, 1, MXCOMPLEX);
-		    realp = mxGetPr(matlabMatrix);
-		    imagp = mxGetPi(matlabMatrix);
-		    Complex temp = particle;
-		    *realp = real(temp);
-		    *imagp = imag(temp);
-		  }
-		  else if ( portType == COMPLEX_MATRIX_ENV ) {
-		    Envelope Apkt;
-		    particle.getMessage(Apkt);
-		    const ComplexMatrix& Amatrix =
-			*(const ComplexMatrix *)Apkt.myData();
-
-		    // allocate a Matlab matrix and name it
-		    int rows = Amatrix.numRows();
-		    int cols = Amatrix.numCols();
-		    matlabMatrix = mxCreateFull(rows, cols, MXCOMPLEX);
-
-		    // copy values in the Ptolemy matrix to the Matlab matrix
-		    // Matlab stores values in column-major order like Fortran
-		    double *realp = mxGetPr(matlabMatrix);
-		    double *imagp = mxGetPi(matlabMatrix);
-		    for ( int icol = 0; icol < cols; icol++ ) {
-		      for ( int irow = 0; irow < rows; irow++ ) {
-			Complex temp = Amatrix[irow][icol];
-			*realp++ = real(temp);
-			*imagp++ = imag(temp);
-		      }
-		    }
-		  }
-		  else if ( portType == FIX_MATRIX_ENV ) {
-		    Envelope Apkt;
-		    particle.getMessage(Apkt);
-		    const FixMatrix& Amatrix =
-			*(const FixMatrix *)Apkt.myData();
-
-		    // allocate a Matlab matrix and name it
-		    int rows = Amatrix.numRows();
-		    int cols = Amatrix.numCols();
-		    matlabMatrix = mxCreateFull(rows, cols, MXREAL);
-
-		    // copy values in the Ptolemy matrix to the Matlab matrix
-		    // Matlab stores values in column-major order like Fortran
-		    double *realp = mxGetPr(matlabMatrix);
-		    for ( int icol = 0; icol < cols; icol++ ) {
-		      for ( int irow = 0; irow < rows; irow++ ) {
-			*realp++ = double(Amatrix[irow][icol]);
-		      }
-		    }
-		  }
-		  else if ( portType == FLOAT_MATRIX_ENV ) {
-		    Envelope Apkt;
-		    particle.getMessage(Apkt);
-		    const FloatMatrix& Amatrix =
-			*(const FloatMatrix *)Apkt.myData();
-
-		    // allocate a Matlab matrix and name it
-		    int rows = Amatrix.numRows();
-		    int cols = Amatrix.numCols();
-		    matlabMatrix = mxCreateFull(rows, cols, MXREAL);
-
-		    // copy values in the Ptolemy matrix to the Matlab matrix
-		    // Matlab stores values in column-major order like Fortran
-		    double *realp = mxGetPr(matlabMatrix);
-		    for ( int icol = 0; icol < cols; icol++ ) {
-		      for ( int irow = 0; irow < rows; irow++ ) {
-			*realp++ = Amatrix[irow][icol];
-		      }
-		    }
-		  }
-		  else if ( portType == INT_MATRIX_ENV ) {
-		    Envelope Apkt;
-		    particle.getMessage(Apkt);
-		    const IntMatrix& Amatrix =
-		        *(const IntMatrix *)Apkt.myData();
-
-		    // allocate a Matlab matrix and name it
-		    int rows = Amatrix.numRows();
-		    int cols = Amatrix.numCols();
-		    matlabMatrix = mxCreateFull(rows, cols, MXREAL);
-
-		    // copy values in the Ptolemy matrix to the Matlab matrix
-		    // Matlab stores values in column-major order like Fortran
-		    double *realp = mxGetPr(matlabMatrix);
-		    for ( int icol = 0; icol < cols; icol++ ) {
-		      for ( int irow = 0; irow < rows; irow++ ) {
-			*realp++ = double(Amatrix[irow][icol]);
-		      }
-		    }
-		  }
-		  else {
-		    *errflag = TRUE;
-		    matlabMatrix = mxCreateFull(1, 1, MXREAL);
-		    double *realp = mxGetPr(matlabMatrix);
-		    *realp = 0.0;
-		  }
-
-		  return matlabMatrix;
-	  }
-	}
-
 	// Returns 1 for Failure and 0 for Success
 	method {
 	  name { processInputMatrices }
@@ -354,7 +234,8 @@ The values of the input ports will be passed as arguments to this function.
 		  PortHole *iportp = nexti++;
 		  DataType portType = iportp->resolvedType();
 		  Matrix *matlabMatrix =
-		  	PtolemyToMatlab((*iportp)%0, portType, &errflag);
+		  	matlabInterface.PtolemyToMatlab(
+				(*iportp)%0, portType, &errflag);
 
 		  // check for error in the call to PtolemyToMatlab
 		  if ( errflag ) {
@@ -377,73 +258,6 @@ The values of the input ports will be passed as arguments to this function.
 		}
 
 		return( errorFlag );
-	  }
-	}
-
-	// Returns 1 for Failure and 0 for Success
-	// copy a Matlab output matrix to a Ptolemy matrix
-	method {
-	  name { MatlabToPtolemy }
-	  access { protected }
-	  type { int }
-	  arglist { "(Particle &particle, DataType portType, Matrix* matlabMatrix, const char* matrixId, int *errflag)" }
-	  code {
-		  // error flags
-		  *errflag = FALSE;
-		  int incompatibleOutportPort = FALSE;
-
-		  // allocate a Ptolemy matrix
-		  int rows = mxGetM(matlabMatrix);
-		  int cols = mxGetN(matlabMatrix);
-
-		  if ( mxIsFull(matlabMatrix) ) {
-
-		    // for real matrices, imagp will be null
-		    double *realp = mxGetPr(matlabMatrix);
-		    double *imagp = mxGetPi(matlabMatrix);
-
-		    // copy Matlab matrices (in column-major order) to Ptolemy
-		    if ( portType == COMPLEX_MATRIX_ENV ) {
-		      ComplexMatrix& Amatrix = *(new ComplexMatrix(rows, cols));
-		      for ( int jcol = 0; jcol < cols; jcol++ ) {
-			for ( int jrow = 0; jrow < rows; jrow++ ) {
-			  double realValue = *realp++;
-			  double imagValue = ( imagp ) ? *imagp++ : 0.0;
-			  Amatrix[jrow][jcol] = Complex(realValue, imagValue);
-			}
-		      }
-		      // write the matrix to output port j
-		      // do not delete Amatrix: particle class handles that
-		      particle << Amatrix;
-		    }
-		    else if ( portType == FLOAT_MATRIX_ENV ) {
-		      FloatMatrix& Amatrix = *(new FloatMatrix(rows, cols));
-		      if ( mxIsComplex(matlabMatrix) ) {
-			StringList myerrstr;
-			myerrstr = "\nImaginary components ignored for the ";
-			myerrstr << "Matlab matrix " << matrixId;
-			Error::warn(myerrstr);
-		      }
-		      for ( int jcol = 0; jcol < cols; jcol++ ) {
-			for ( int jrow = 0; jrow < rows; jrow++ ) {
-			  Amatrix[jrow][jcol] = *realp++;
-			}
-		      }
-		      // write the matrix to output port j
-		      // do not delete Amatrix: particle class handles that
-		      particle << Amatrix;
-		    }
-		    // this situation should never be encountered since the
-		    // setup method should have checked for output data type
-		    else {
-		      incompatibleOutportPort = TRUE;
-		    }
-		  }
-		  else {
-		    *errflag = TRUE;
-		  }
-
-		  return incompatibleOutportPort;
 	  }
 	}
 
@@ -494,9 +308,9 @@ The values of the input ports will be passed as arguments to this function.
 		  StringList matrixId = matlabOutputNames[j];
 		  matrixId << " on output port " << j;
 		  int errflag = FALSE;
-		  int badport = MatlabToPtolemy((*oportp)%0, portType,
-		  				matlabMatrix, matrixId,
-						&errflag);
+		  int badport = matlabInterface.MatlabToPtolemy(
+		  			(*oportp)%0, portType,
+		  			matlabMatrix, matrixId, &errflag);
 		  incompatibleOutportPort = incompatibleOutportPort || badport;
 
 		  if ( errflag ) {
