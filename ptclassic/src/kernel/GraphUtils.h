c3a9ee598a710fc7c875ae13e436e5a9ff8a4623
Jose Luis Pino <joseluispino@users.noreply.github.com>
1995-11-15 18:51:04 +0000

    GraphMatrix class based on IntMatrix

--- ptclassic/src/kernel/GraphUtils.h
+++ ptclassic/src/kernel/GraphUtils.h
@@ -39,30 +39,60 @@ Programmer: Jose Luis Pino
 #include "StringList.h"
 #include "Galaxy.h"
 #include "GalIter.h"
+#include "Matrix.h"
 
 
 // Delete-safe iterators
-inline int defaultBlockTest(Block&) { return TRUE; }
 
-class DSBlockIter {
+// DSNamedObjIter is not meant to be used except as a base class
+class DSNamedObjIter {
 public:
-    DSBlockIter(int (*test)(Block&) = defaultBlockTest):testBlock(test){};
-    virtual ~DSBlockIter() { delete nextBlockThatMeetsTest; }
-    inline Block* next() { return nextBlockThatMeetsTest->next(); }
-    inline Block* operator++(POSTFIX_OP) { return next();}
-    inline void reset() { nextBlockThatMeetsTest->reset(); }
-    inline void put(Block& b) {
-	if (testBlock(b)) blocksThatMeetTest.put(b);
-    }
+    virtual ~DSNamedObjIter() { delete nextObjectThatMeetsTest; }
+    inline NamedObj* next() { return nextObjectThatMeetsTest->next(); }
+    inline NamedObj* operator++(POSTFIX_OP) { return next();}
+    inline void reset() { nextObjectThatMeetsTest->reset(); }
+    inline void put(NamedObj& o) { objectsThatMeetTest.put(o); }
 protected:
     // This method must be called at the end of all derived class constructors
     inline void initInternalIterator() {
-	nextBlockThatMeetsTest = new BlockListIter(blocksThatMeetTest);
+	nextObjectThatMeetsTest = new NamedObjListIter(objectsThatMeetTest);
     }
 private:
-    int (*testBlock)(Block&);
-    BlockList blocksThatMeetTest;
-    BlockListIter *nextBlockThatMeetsTest;
+    NamedObjList objectsThatMeetTest;
+    NamedObjListIter *nextObjectThatMeetsTest;
+};   
+
+inline int defaultPortTest(PortHole&) { return TRUE; }
+class DSBlockPortIter:private DSNamedObjIter {
+public:
+    DSBlockPortIter(Block&,int (*)(PortHole&) = defaultPortTest);
+    virtual ~DSBlockPortIter() {};
+    inline PortHole* next() { return (PortHole*)DSNamedObjIter::next(); }
+    inline PortHole* operator++(POSTFIX_OP) { return next();}
+    inline void put(PortHole& p) { if (testPort(p)) DSNamedObjIter::put(p); }
+    DSNamedObjIter::reset;
+protected:
+    // This method must be called at the end of all derived class constructors
+    DSNamedObjIter::initInternalIterator;
+private:
+    int (*testPort)(PortHole&);    
+};   
+
+inline int defaultBlockTest(Block&) { return TRUE; }
+class DSBlockIter:private DSNamedObjIter {
+public:
+    DSBlockIter(int (*test)(Block&) = defaultBlockTest)
+    :DSNamedObjIter(), testBlock(test) {};
+    virtual ~DSBlockIter() {};
+    inline Block* next() { return (Block*)DSNamedObjIter::next(); }
+    inline Block* operator++(POSTFIX_OP) { return next();}
+    inline void put(Block& b) { if (testBlock(b)) DSNamedObjIter::put(b); }
+    DSNamedObjIter::reset;
+protected:
+    // This method must be called at the end of all derived class constructors
+    DSNamedObjIter::initInternalIterator;
+private:
+    int (*testBlock)(Block&);    
 };   
 
 class DSGalAllBlockIter:public DSBlockIter {
@@ -83,8 +113,6 @@ public:
     inline Galaxy* next() { return (Galaxy*)DSGalAllBlockIter::next(); }
     inline Galaxy* operator++(POSTFIX_OP) { return next();}
     DSGalAllBlockIter::reset;
-
-    // need a public destructor because of private derivation
     virtual ~GalGalaxyIter() {}
 };
 
@@ -95,7 +123,7 @@ int totalNumberOfStars(Galaxy& g);
 
 class BlockParentIter {
 public:
-    BlockParentIter(Block&);
+    BlockParentIter(Block& b):first(b) { reset(); }
     Block* next();
     inline Block* operator++(POSTFIX_OP) { return next();}
     inline void reset() { nextParent = first.parent();}
@@ -116,4 +144,59 @@ StringList printTopBlockDot(Galaxy&, const char*);
 void replaceString(char* /*master string*/,
 		   const char* /*string to match*/,
 		   const char* /*string to insert*/);
+
+// Number all of the blocks inside of the galaxy at the specified flag
+// location
+void numberAllBlocks(Galaxy&,int =0);
+
+// Number the top blocks inside of the galaxy at the specified flag
+// location
+void numberBlocks(Galaxy&,int =0);
+
+// reset the flag specified for all the blocks inside of the galaxy
+void resetAllFlags(Galaxy&,int =0);
+
+// reset the flag specified only the top blocks inside of the galaxy
+void resetFlags(Galaxy&,int =0);
+
+// This class respects galaxy boundaries
+class GraphMatrix: public IntMatrix {
+friend class GraphMatrixIter;
+public:
+    GraphMatrix(Galaxy& g, int flagLocation=0);
+    ~GraphMatrix();
+
+    virtual int cluster(Block& replacement,int,int);
+
+    Block** graphBlock;
+
+    /*virtual*/ StringList print();
+protected:
+    int flagLoc;
+    int numBlocks;
+};
+
+class GraphMatrixIter {
+public:
+    inline GraphMatrixIter(GraphMatrix& m):myMatrix(m),current(0) {};
+    inline int next() {
+	while(current < myMatrix.numBlocks &&
+	      myMatrix.graphBlock[current] == NULL)
+	    current++;
+    	if (current >= myMatrix.numBlocks) current = -1;
+	return current++;
+    }
+    inline int operator++(POSTFIX_OP) { return next();}
+    inline void reset() { current = 0;}
+private:
+    GraphMatrix& myMatrix;
+    int current;
+};
+
+class TopologyMatrix: public GraphMatrix {
+public:
+    TopologyMatrix(Galaxy& g, int flagLocation=0);
+    /*virtual*/ int cluster(Block& replacement,int,int);
+};
+
 #endif
