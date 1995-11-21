8f0de97d7a35dc2bafb21a6c55a13c968aeca326
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-11-21 09:07:46 +0000

    first working chain clustering version!!

--- trunk/ptclassic/src/domains/cg/hierScheduler/HierCluster.h
+++ trunk/ptclassic/src/domains/cg/hierScheduler/HierCluster.h
@@ -43,13 +43,20 @@ ENHANCEMENTS, OR MODIFICATIONS.
 
 class HierCluster : public Cluster {
 public:
-    HierCluster(Star&);
-    HierCluster(Cluster&);
-    HierCluster(Galaxy&);
 
-    /*virtual*/ int absorb(Block&);    
-    /*virtual*/ int merge(Cluster&);
-    /*virtual*/ int flatten();
+    HierCluster():Cluster(), repetitions(0) {};
+
+    /*virtual*/ Block* makeNew() const { return new HierCluster; }
+    
+    /*virtual*/ int absorb(Cluster&, int=TRUE);    
+    /*virtual*/ int merge(Cluster&, int=TRUE);
+    /*virtual*/ int flatten(Galaxy* = NULL, int = TRUE);
+
+    /*virtual*/ int flattenTest(Galaxy&);
+
+    /*virtual*/ Cluster* convertGalaxy(Galaxy&);
+
+    /*virtual*/ Cluster* convertStar(Star&);
 
     // The number of repetitions of the cluster in a periodic
     // schedule.  Initialized to 0 by the constructors.  Set to correct
@@ -57,7 +64,7 @@ public:
     int repetitions;
 
 private:
-    void adjustRepetitions(Block& /*blockToMerge*/);
+    void adjustRepetitions(HierCluster& /*clusterToMerge*/);
     int adjustRepetitions(int /*newRepetitionCount*/);
 };
 
