d32fc51ac3987efb58d3210f2bc61cce2b0406bb
kalavade <kalavade@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-11-22 03:54:17 +0000

    added a command "ptkSetDMMFlag $number" that is used by the DMM domain to check for parameter dependencies

--- trunk/ptclassic/lib/tcl/ptkParams.tcl
+++ trunk/ptclassic/lib/tcl/ptkParams.tcl
@@ -802,6 +802,8 @@ proc ed_Apply {facet number args} {
   if { $changeFlag } {
     set ed_Parameters($facet,$number) $newParamArray
     ed_WriteParam $ed_Parameters($facet,$number) $facet $number $args
+    # added for DMM support 11/22/95 (kalavade)
+    ptkSetDMMChangedFlag $number
   }
 }
 
