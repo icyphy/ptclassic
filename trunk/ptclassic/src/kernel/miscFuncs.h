b608a52d68ec727d2dce84e31a3a8656ca83d16e
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-11-30 19:00:06 +0000

    Updated comments

--- trunk/ptclassic/src/kernel/miscFuncs.h
+++ trunk/ptclassic/src/kernel/miscFuncs.h
@@ -68,17 +68,24 @@ extern "C" double pow(double,double);
 
 // return an expanded version of the filename argument, which may
 // start with "~", "~user", or "$var".  The returned value points
-// to static storage and is wiped out by the next call.
+// to dynamic storage which should be freed using delete [] when no
+// longer used.
 char* expandPathName(const char*);
 
-// return a new, unique temporary file name.  It lives on the heap
-// and may be deleted when no longer needed.
+// return a new, unique temporary file name.  The returned value points
+// to dynamic storage which should be freed using delete [] when no
+// longer used.
 char* tempFileName();
 
-// return a new string that is formed by replacing each character
-// "target" in string "str" with string "substr"
+// return a new string formed by replacing each character "target"
+// in string "str" with string "substr".  The returned value points
+// to dynamic storage which should be freed using delete [] when no
+// longer used.
 char* subCharByString(const char *str, char target, const char *substr);
 
+// return a new copy of the string 'txt'.  The returned value points
+// to dynamic storage which should be freed using delete [] when no
+// longer used.
 inline char* savestring (const char* txt) {
 	INC_LOG_NEW; return strcpy (new char[strlen(txt)+1], txt);
 }
@@ -86,13 +93,11 @@ inline char* savestring (const char* txt) {
 // Save the string in a systemwide hash table, if it is not there
 // already, and return a pointer to the saved copy.  Subsequent
 // hashstrings with the same string return the same address.
-// Do not delete strings returned by hashstring
-
+// Do not delete strings returned by hashstring.
 const char* hashstring(const char*);
 
 // I would prefer to make this int pow(int,int), but g++ gets confused
 // because the other function is extern "C".
-
 int power(int base,int exp);
 inline double power(double base,double exp) { return pow(base,exp);}
 
