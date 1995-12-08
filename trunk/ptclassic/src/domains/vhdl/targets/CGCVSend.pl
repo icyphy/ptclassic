e1dc625cff95598682c24f231acb5a71b10c470b
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-12-08 06:38:28 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/vhdl/targets/CGCVSend.pl
@@ -0,0 +1,118 @@
+defstar {
+    name { VSend }
+    domain { CGC }
+    desc { Send data synchronously from CGC to VHDL }
+//    derivedFrom { VSynchComm }
+    version { $Id$ }
+    author { Michael C. Williamson, Jose Luis Pino }
+copyright { 
+Copyright (c) 1994,1993 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+}
+
+location { VHDL Target Directory }
+explanation {}
+  public {
+    int numXfer;
+  }
+  protected {
+  }
+input {
+  name {input}
+  type {ANYTYPE}
+}
+  defstate {
+    name {pairNumber}
+    type {int}
+    default {11}
+  }
+setup {
+  numXfer = input.numXfer();
+//  VHDLCSynchComm::setup();
+}
+
+initCode {
+        CodeStream *compileOptions, *linkOptions;
+        if ((compileOptions = getStream("compileOptions")) == FALSE)
+            return;
+        if ((linkOptions = getStream("linkOptions")) == FALSE)
+            return;
+//        compileOptions->put("-I$S56DSP/include ","S56X Include");
+        linkOptions->put("-lsocket -lnsl","socket Link");
+  addInclude("<stdio.h>");
+  addInclude("<string.h>");
+  addInclude("<unistd.h>");
+  addInclude("<sys/types.h>");
+  addInclude("<sys/socket.h>");
+  addInclude("<sys/un.h>");
+  addGlobal("#define BUFFSIZE 32", "buffer");
+  addDeclaration("
+  /* Decls */
+  int $starSymbol(count) = 0;
+  int $starSymbol(intptr) = 0;
+  int $starSymbol(nbytes) = 12;
+  int $starSymbol(sendsock), $starSymbol(xmitsock), $starSymbol(writestat);
+  char $starSymbol(buffer)[BUFFSIZE];
+  char *$starSymbol(dummy) = \"  csource Error\";
+  char *$starSymbol(sendstring) = \"/tmp/snd$val(pairNumber)\\0\";
+  struct sockaddr $starSymbol(sendaddr), $starSymbol(xmitaddr);
+  int $starSymbol(sendnamelen) = strlen($starSymbol(sendstring));
+  int $starSymbol(xmitaddrlen) = sizeof($starSymbol(xmitaddr));
+  int $starSymbol(sendaddrlen) = sizeof($starSymbol(sendaddr));
+  int $starSymbol(i);
+");
+  addCode("
+  /* Init */
+  for ($starSymbol(i)=0 ; $starSymbol(i) < BUFFSIZE ; $starSymbol(i)++) {
+    $starSymbol(buffer)[$starSymbol(i)] = (char) 0;
+  }
+  $starSymbol(sendsock) = socket(AF_UNIX, SOCK_STREAM, 0);
+  if ($starSymbol(sendsock) == -1) {
+    perror($starSymbol(dummy));
+  }
+  $starSymbol(sendaddr).sa_family = AF_UNIX;
+  (void) strncpy($starSymbol(sendaddr).sa_data, $starSymbol(sendstring), $starSymbol(sendnamelen)+1);
+  (void) unlink($starSymbol(sendaddr).sa_data);
+  if(bind($starSymbol(sendsock), &$starSymbol(sendaddr), $starSymbol(sendaddrlen)) == -1) {
+    perror($starSymbol(dummy));
+  }
+  if(listen($starSymbol(sendsock), 5) == -1) {
+    perror($starSymbol(dummy));
+  }
+  $starSymbol(xmitsock) = accept($starSymbol(sendsock), &$starSymbol(xmitaddr), &$starSymbol(xmitaddrlen));
+  if($starSymbol(xmitsock) < 0) {
+    perror($starSymbol(dummy));
+  }
+");
+}
+
+go {
+  addCode("
+  /* Go */
+  $starSymbol(intptr) = 0;
+  $starSymbol(writestat) = 0;
+  if($starSymbol(writestat) >= 0) {
+    (void) sprintf($starSymbol(buffer), \"%d\", $ref(input));
+    $starSymbol(writestat) = write($starSymbol(xmitsock), $starSymbol(buffer), $starSymbol(nbytes));
+    if($starSymbol(writestat) < 0) {
+      perror($starSymbol(dummy));
+    }
+  }
+");
+}
+
+wrapup {
+  addCode("
+  /* Wrapup */
+  (void) shutdown($starSymbol(sendsock),2);
+  (void) close($starSymbol(sendsock));
+  (void) unlink($starSymbol(sendaddr).sa_data);
+  (void) shutdown($starSymbol(xmitsock),2);
+  (void) close($starSymbol(xmitsock));
+  (void) unlink($starSymbol(xmitaddr).sa_data);
+");
+}
+
+}
