2ceb811e7529972892ea2638c523e8bd08536b7b
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-12-11 04:08:16 +0000

    cleanup and making CGCV stars similar in preparation for deriving from sigle new CGCVSynchComm star

--- ptclassic/src/domains/vhdl/targets/CGCVReceive.pl
+++ ptclassic/src/domains/vhdl/targets/CGCVReceive.pl
@@ -1,34 +1,30 @@
 defstar {
-    name { VReceive }
-    domain { CGC }
-    desc { Receive data synchronously from VHDL to CGC }
-//    derivedFrom { VSynchComm }
-    version { $Id$ }
-    author { Michael C. Williamson, Jose Luis Pino }
-    copyright { 
-Copyright (c) 1994,1993 The Regents of the University of California.
-All rights reserved.
-See the file $PTOLEMY/copyright for copyright notice,
-limitation of liability, and disclaimer of warranty provisions.
+  name { VReceive }
+  domain { CGC }
+  desc { Receive data synchronously from VHDL to CGC }
+//  derivedFrom { VSynchComm }
+  version { $Id$ }
+  author { Michael C. Williamson, Jose Luis Pino }
+  copyright { 
+    Copyright (c) 1994,1993 The Regents of the University of California.
+      All rights reserved.
+      See the file $PTOLEMY/copyright for copyright notice,
+      limitation of liability, and disclaimer of warranty provisions.
     }
 
-    location { VHDL Target Directory }
-    explanation { }
+  location { VHDL Target Directory }
+  explanation { }
   public {
     int numXfer;
-//    StringList destDir;
-//    StringList filePre;
   }
-  protected {
+  output {
+    name {output}
+    type {ANYTYPE}
   }
-    output {
-	name {output}
-	type {ANYTYPE}
-      }
   defstate {
     name {pairNumber}
     type {int}
-    default {21}
+    default {0}
   }
   defstate {
     name {destDir}
@@ -40,94 +36,90 @@ limitation of liability, and disclaimer of warranty provisions.
     type {string}
     default {"CGCVHDL"}
   }
-setup {
-//  destDir = "/users/cameron/PTOLEMY_SYSTEMS";
-//  filePre = "CGCVHDLCG56Test2";
-  numXfer = output.numXfer();
-//  CGCVSynchComm::setup();
-}
+  setup {
+    numXfer = output.numXfer();
+//    CGCVSynchComm::setup();
+  }
 
-initCode {
-        CodeStream *compileOptions, *linkOptions;
-        if ((compileOptions = getStream("compileOptions")) == FALSE)
-            return;
-        if ((linkOptions = getStream("linkOptions")) == FALSE)
-            return;
-//        compileOptions->put("-I$S56DSP/include ","S56X Include");
-        linkOptions->put("-lsocket -lnsl","socket Link");
-  addInclude("<stdio.h>");
-  addInclude("<string.h>");
-  addInclude("<unistd.h>");
-  addInclude("<sys/types.h>");
-  addInclude("<sys/socket.h>");
-  addInclude("<sys/un.h>");
-  addGlobal("#define BUFFSIZE 32", "buffer");
-  addDeclaration("
+  initCode {
+    CodeStream *compileOptions, *linkOptions;
+    if ((compileOptions = getStream("compileOptions")) == FALSE)
+      return;
+    if ((linkOptions = getStream("linkOptions")) == FALSE)
+      return;
+    linkOptions->put("-lsocket -lnsl","socket Link");
+    addInclude("<stdio.h>");
+    addInclude("<string.h>");
+    addInclude("<unistd.h>");
+    addInclude("<sys/types.h>");
+    addInclude("<sys/socket.h>");
+    addInclude("<sys/un.h>");
+    addGlobal("#define BUFFSIZE 32", "buffer");
+    addDeclaration("
   /* Decls */
   int $starSymbol(count) = 0;
   int $starSymbol(intptr) = 0;
   int $starSymbol(nbytes) = 12;
-  int $starSymbol(recvsock), $starSymbol(xmitsock), $starSymbol(readstat);
+  int $starSymbol(nearsock), $starSymbol(xmitsock), $starSymbol(status);
   char $starSymbol(buffer)[BUFFSIZE];
-  char *$starSymbol(dummy) = \"  csink Error\";
-  char *$starSymbol(recvstring) = \"/tmp/rcv$val(pairNumber)\\0\";
-  struct sockaddr $starSymbol(recvaddr), $starSymbol(xmitaddr);
-  int $starSymbol(recvnamelen) = strlen($starSymbol(recvstring));
+  char *$starSymbol(dummy) = \"  CGCVReceive socket error\";
+  char *$starSymbol(nearstring) = \"/tmp/rcv$val(pairNumber)\\0\";
+  struct sockaddr $starSymbol(nearaddr), $starSymbol(xmitaddr);
+  int $starSymbol(nearnamelen) = strlen($starSymbol(nearstring));
   int $starSymbol(xmitaddrlen) = sizeof($starSymbol(xmitaddr));
-  int $starSymbol(recvaddrlen) = sizeof($starSymbol(recvaddr));
+  int $starSymbol(nearaddrlen) = sizeof($starSymbol(nearaddr));
   int $starSymbol(i);
 ");
 
-// This must be the first call to add code to mainInit
-  StringList command = "";
-  command << "cd " << (const char*) destDir;
-  command << " ; ";
-  command << "vhdlsim -i " << filePre << ".com " << "parts";
+// This must be the first call to add code to mainInit.
+    StringList command = "";
+    command << "cd " << (const char*) destDir;
+    command << " ; ";
+    command << "vhdlsim -i " << filePre << ".com " << "parts";
 
-//  system(command);
-  StringList startvss = "";
-  startvss << "
+    StringList startvss = "";
+    startvss << "
   /* Start VSS Simulator */
   system(\"";
-  startvss << command << "&";
-  startvss << "\");\n";
-  addCode(startvss, "mainInit", "startVSS");
-  addCode("
+    startvss << command << "&";
+    startvss << "\");\n";
+    addCode(startvss, "mainInit", "startVSS");
+    addCode("
   /* Init */
   for ($starSymbol(i)=0 ; $starSymbol(i) < BUFFSIZE ; $starSymbol(i)++) {
     $starSymbol(buffer)[$starSymbol(i)] = (char) 0;
   }
-  $starSymbol(recvsock) = socket(AF_UNIX, SOCK_STREAM, 0);
-  if ($starSymbol(recvsock) == -1) {
+  $starSymbol(nearsock) = socket(AF_UNIX, SOCK_STREAM, 0);
+  if ($starSymbol(nearsock) == -1) {
     perror($starSymbol(dummy));
   }
-  $starSymbol(recvaddr).sa_family = AF_UNIX;
-  (void) strncpy($starSymbol(recvaddr).sa_data, $starSymbol(recvstring), $starSymbol(recvnamelen)+1);
-  (void) unlink($starSymbol(recvaddr).sa_data);
-  if(bind($starSymbol(recvsock), &$starSymbol(recvaddr), $starSymbol(recvaddrlen)) == -1) {
+  $starSymbol(nearaddr).sa_family = AF_UNIX;
+  (void) strncpy($starSymbol(nearaddr).sa_data, $starSymbol(nearstring), $starSymbol(nearnamelen)+1);
+  (void) unlink($starSymbol(nearaddr).sa_data);
+  if(bind($starSymbol(nearsock), &$starSymbol(nearaddr), $starSymbol(nearaddrlen)) == -1) {
     perror($starSymbol(dummy));
   }
-  if(listen($starSymbol(recvsock), 5) == -1) {
+  if(listen($starSymbol(nearsock), 5) == -1) {
     perror($starSymbol(dummy));
   }
-  $starSymbol(xmitsock) = accept($starSymbol(recvsock), &$starSymbol(xmitaddr), &$starSymbol(xmitaddrlen));
+  $starSymbol(xmitsock) = accept($starSymbol(nearsock), &$starSymbol(xmitaddr), &$starSymbol(xmitaddrlen));
   if($starSymbol(xmitsock) < 0) {
     perror($starSymbol(dummy));
   }
 ");
-}
+  }
 
-go {
-  addCode("
+  go {
+    addCode("
   /* Go */
   $starSymbol(intptr) = 0;
-  $starSymbol(readstat) = 0;
-  if($starSymbol(readstat) >= 0) {
-    $starSymbol(readstat) = read($starSymbol(xmitsock), $starSymbol(buffer), $starSymbol(nbytes));
-    while($starSymbol(readstat) == 0) {
-      $starSymbol(readstat) = read($starSymbol(xmitsock), $starSymbol(buffer), $starSymbol(nbytes));
+  $starSymbol(status) = 0;
+  if($starSymbol(status) >= 0) {
+    $starSymbol(status) = read($starSymbol(xmitsock), $starSymbol(buffer), $starSymbol(nbytes));
+    while($starSymbol(status) == 0) {
+      $starSymbol(status) = read($starSymbol(xmitsock), $starSymbol(buffer), $starSymbol(nbytes));
     }
-    if($starSymbol(readstat) < 0) {
+    if($starSymbol(status) < 0) {
       perror($starSymbol(dummy));
     }
     else {
@@ -135,15 +127,18 @@ go {
     }
   }
 ");
-}
+  }
 
-wrapup {
-  addCode("
+  wrapup {
+    addCode("
   /* Wrapup */
-  (void) shutdown($starSymbol(recvsock),2);
-  (void) close($starSymbol(recvsock));
-  (void) unlink($starSymbol(recvaddr).sa_data);
+  (void) shutdown($starSymbol(xmitsock),2);
+  (void) close($starSymbol(xmitsock));
+  (void) unlink($starSymbol(xmitaddr).sa_data);
+  (void) shutdown($starSymbol(nearsock),2);
+  (void) close($starSymbol(nearsock));
+  (void) unlink($starSymbol(nearaddr).sa_data);
 ");
-}
+  }
 
 }
