c5810c63e1b299c167254bf296c5c82dbccc09d9
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1995-12-12 13:26:55 +0000

    Send and Receive now derived from SynchComm, common code

--- ptclassic/src/domains/vhdl/targets/CGCVSynchComm.pl
+++ ptclassic/src/domains/vhdl/targets/CGCVSynchComm.pl
@@ -31,6 +31,16 @@ defstar {
     type {string}
     default {"CGCVHDL"}
   }
+  defstate {
+    name {classname}
+    type {string}
+    default {"CGCVSynchComm"}
+  }
+  defstate {
+    name {sndrcv}
+    type {string}
+    default {"sndrcv"}
+  }
   setup {
   }
 
@@ -55,9 +65,21 @@ defstar {
   int $starSymbol(nbytes) = 12;
   int $starSymbol(nearsock), $starSymbol(xmitsock), $starSymbol(status);
   char $starSymbol(buffer)[BUFFSIZE];
-  char *$starSymbol(dummy) = \"  CGCVSend socket error\";
-  char *$starSymbol(nearstring) = \"/tmp/snd$val(pairNumber)\\0\";
-  struct sockaddr $starSymbol(nearaddr), $starSymbol(xmitaddr);
+");
+
+    StringList oneLine;
+
+    oneLine = "  char *$starSymbol(dummy) = \"  ";
+    oneLine << classname;
+    oneLine << " socket error\";";
+    addDeclaration(oneLine);
+
+    oneLine = "  char *$starSymbol(nearstring) = \"/tmp/";
+    oneLine << sndrcv;
+    oneLine << "$val(pairNumber)\\0\";";
+    addDeclaration(oneLine);
+    
+    addDeclaration("  struct sockaddr $starSymbol(nearaddr), $starSymbol(xmitaddr);
   int $starSymbol(nearnamelen) = strlen($starSymbol(nearstring));
   int $starSymbol(xmitaddrlen) = sizeof($starSymbol(xmitaddr));
   int $starSymbol(nearaddrlen) = sizeof($starSymbol(nearaddr));
@@ -98,21 +120,6 @@ defstar {
   $starSymbol(xmitsock) = accept($starSymbol(nearsock), &$starSymbol(xmitaddr), &$starSymbol(xmitaddrlen));
   if($starSymbol(xmitsock) < 0) {
     perror($starSymbol(dummy));
-  }
-");
-  }
-
-  go {
-    addCode("
-  /* Go */
-  $starSymbol(intptr) = 0;
-  $starSymbol(status) = 0;
-  if($starSymbol(status) >= 0) {
-    (void) sprintf($starSymbol(buffer), \"%d\", $ref(input));
-    $starSymbol(status) = write($starSymbol(xmitsock), $starSymbol(buffer), $starSymbol(nbytes));
-    if($starSymbol(status) < 0) {
-      perror($starSymbol(dummy));
-    }
   }
 ");
   }
