de0339f96a88180c3ea901bde45b26aa2464b38c
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-01-02 09:48:15 +0000

    Modified rule that installs libdialogs and librpcserver for shared libraries. shared libraries should not have ranlib run on them. Under hppa, you will get /usr/lib/dld.sl: Call to mmap() failed - TEXT /users/ptdesign/octtools/lib.hppa/libdialogs.sl If libdialogs.sl is not executable.

--- trunk/ptclassic/src/octtools/vem-bin.mk
+++ trunk/ptclassic/src/octtools/vem-bin.mk
@@ -31,11 +31,16 @@ CC =	$(OCT_CC)
 
 all::	makefile $(OBJS)
 
-# Rule to install libdialogs and librpcserver for sol2
+ifeq ($(USE_SHARED_LIBS),yes)
+# Rule to install libdialogs and librpcserver for hppa
 $(OCTLIBDIR)/$(LIB_INSTALL): $(LIB_INSTALL) 
-		cp $(LIB_INSTALL) $(OCTLIBDIR)
+		ln $(LIB_INSTALL) $(OCTLIBDIR)
+else
+# Rule to install libdialogs and librpcserver for sol2, non-shared
+$(OCTLIBDIR)/$(LIB_INSTALL): $(LIB_INSTALL) 
+		ln $(LIB_INSTALL) $(OCTLIBDIR)
 		$(RANLIB) $(OCTLIBDIR)/$(LIB_INSTALL)
-
+endif
 install: makefile $(OCTLIBDIR)/$(LIB_INSTALL)
 
 
