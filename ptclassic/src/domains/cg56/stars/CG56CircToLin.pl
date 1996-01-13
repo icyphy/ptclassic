6422382392ffbe053cd095e7a1e9ad95348c1a46
Jose Luis Pino <joseluispino@users.noreply.github.com>
1996-01-13 22:23:51 +0000

    fix for when buffers are of length 1

--- ptclassic/src/domains/cg56/stars/CG56CircToLin.pl
+++ ptclassic/src/domains/cg56/stars/CG56CircToLin.pl
@@ -1,22 +1,22 @@
 defstar {
-	name { CircToLin }
-	domain { CG56 }
-	desc {
+    name { CircToLin }
+    domain { CG56 }
+    desc {
 Copy data from a circular buffer to a linear buffer.
 If N is zero (the default), the number of samples transferred per execution
 equals the number of samples read per execution by the star that reads
 from the output. 
-	}
-	version { $Id$ }
-	author { J. Buck }
-	copyright {
+    }
+    version { $Id$ }
+    author { J. Buck & Jose Luis Pino }
+    copyright {
 Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
-	}
-	location { CG56 demo library }
-	explanation {
+    }
+    location { CG56 demo library }
+    explanation {
 .Id "circular to linear buffer copy"
 .Ir "buffer, circular"
 .Ir "buffer, linear"
@@ -24,84 +24,79 @@ limitation of liability, and disclaimer of warranty provisions.
 .Ir "circular buffer"
 This star is normally inserted automatically, as needed.
 Data movement is repeated inline so may not be efficient for large N.
+    }
+    input {
+        name { input }
+        type { anytype }
+        attributes { P_CIRC}
+    }
+    output {
+	name { output }
+	type { =input }
+    }
+    protected {
+	int n;
+    }
+    defstate {
+	name { N }
+	type { int }
+	desc { number to transfer per execution }
+	default { 0 }
+    }
+    setup {
+	n = int(N);
+	if (n == 0) {
+	    n = output.far()->numXfer();
 	}
-	input {
-		name { input }
-		type { anytype }
-		attributes { P_CIRC}
+	input.setSDFParams(n,n-1);
+	output.setSDFParams(n,n-1);
+    }
+    defstate {
+	name { ptr }
+	type { int }
+	desc { storage for pointer register (internal) }
+	default { 0 }
+	attributes { A_YMEM|A_NOINIT|A_NONSETTABLE }
+    }
+    initCode { addCode(init);}
+    go {
+	if (n>1) addCode(setupC);
+	for (int j = 0; j < n; j++) {
+	    if (input.resolvedType() == COMPLEX)
+		addCode(oneComplex(j,n==1?"$addr(output)":"(r0)+"));
+	    else
+		addCode(oneReal(j,n==1?"$addr(output)":"(r0)+"));
 	}
-	output {
-		name { output }
-		type { =input }
-	}
-	protected {
-		int n;
-	}
-	defstate {
-		name { N }
-		type { int }
-		desc { number to transfer per execution }
-		default { 0 }
-	}
-	setup {
-		n = int(N);
-		if (n == 0) {
-			n = output.far()->numXfer();
-		}
-		input.setSDFParams(n,n-1);
-		output.setSDFParams(n,n-1);
-	}
-	defstate {
-		name { i }
-		type { int }
-		desc { internal }
-		default { 0 }
-		attributes { A_NONCONSTANT|A_NONSETTABLE }
-	}
-	defstate {
-		name { ptr }
-		type { int }
-		desc { storage for pointer register (internal) }
-		default { 0 }
-		attributes { A_YMEM|A_NOINIT|A_NONSETTABLE }
-	}
-	initCode { addCode(init);}
-	go {
-		addCode(setupC);
-		for (int j = 0; j < n; j++) {
-			i = j;
-			if (input.resolvedType() == COMPLEX)
-				addCode(oneComplex);
-			else
-				addCode(oneReal);
-		}
-		addCode(restore);
-	}
-	
-	exectime {
-		return 4+2*n;
-	}
+	if (n>1) addCode(restore);
+    }
+    
+    exectime {
+	return 4+2*n;
+    }
 
-	codeblock(init) {
+    codeblock(init) {
 	move	#>$addr(input),x0
 	move	x0,$ref(ptr)
-	}
+    }
 
-	codeblock(setupC) {
+    codeblock(setupC) {
 	move	$ref(ptr),r0
 	move	#$size(input)-1,m0
 	nop			; can't read from circbuf right away
-	}
-	codeblock(oneReal) {
-	move	x:(r0)+,x0
-	move	x0,$ref(output,i)
-	}
-	codeblock(oneComplex) {
-	move	L:(r0)+,x
-	move	x,L:$addr(output,i)
-	}
-	codeblock(restore) {
+    }
+
+    codeblock(oneReal,"int i, char* xmemLoc") {
+	move	@xmemLoc,x0
+	move	x0,$ref(output,@i)
+    }
+	
+    codeblock(oneComplex,"int i, char* xmemLoc") {
+	move	L:@xmemLoc,x
+	move	x,L:$addr(output,@i)
+    }
+
+    codeblock(restore) {
 	move	r0,$ref(ptr)
 	move	m7,m0
-	}
+    }
 }
--- ptclassic/src/domains/cg56/stars/CG56LinToCirc.pl
+++ ptclassic/src/domains/cg56/stars/CG56LinToCirc.pl
@@ -1,20 +1,20 @@
 defstar {
-	name { LinToCirc }
-	domain { CG56 }
-	desc {
+    name { LinToCirc }
+    domain { CG56 }
+    desc {
 Copy data from a linear buffer to a circular buffer.  If N is zero (default),
 the number transferred per execution equals the number written per execution
 by the star that feeds the input. }
-	version { $Id$ }
-	author { J. Buck }
-	copyright {
+    version { $Id$ }
+    author { J. Buck, Jose Luis Pino }
+    copyright {
 Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
-	}
-	location { CG56 demo library }
-	explanation {
+    }
+    location { CG56 demo library }
+    explanation {
 .Id "linear to circular buffer copy"
 .Ir "buffer, circular"
 .Ir "buffer, linear"
@@ -22,85 +22,77 @@ limitation of liability, and disclaimer of warranty provisions.
 .Ir "circular buffer"
 This star is normally inserted automatically, as needed.  Data movement
 is repeated inline so may not be efficient for large N.
+    }
+    input {
+	name { input }
+	type { anytype }
+    }
+    output {
+	name { output }
+	type { =input }
+	attributes { P_CIRC }
+    }
+    defstate {
+	name { N }
+	type { int }
+	desc { number to transfer per execution }
+	default { 0 }
+    }
+    defstate {
+	name { ptr }
+	type { int }
+	desc { storage for pointer register }
+	default { 0 }
+	attributes { A_YMEM|A_NOINIT|A_NONSETTABLE }
+    }
+    protected {
+	int n;
+    }
+    setup {
+	n = int(N);
+	if (n == 0)
+	    n = input.far()->numXfer();
+	input.setSDFParams(n,n-1);
+	output.setSDFParams(n,n-1);
+    }
+    initCode { addCode(init);}
+    go {
+	if (n>1) addCode(setupC);
+	for (int j = 0; j < n; j++) {
+	    if (input.resolvedType() == COMPLEX)
+		addCode(oneComplex(j,n==1?"$addr(output)":"(r0)+"));
+	    else
+		addCode(oneReal(j,n==1?"$addr(output)":"(r0)+"));
 	}
-	input {
-		name { input }
-		type { anytype }
-	}
-	output {
-		name { output }
-		type { =input }
-		attributes { P_CIRC }
-	}
-	defstate {
-		name { N }
-		type { int }
-		desc { number to transfer per execution }
-		default { 0 }
-	}
-	defstate {
-		name { ptr }
-		type { int }
-		desc { storage for pointer register }
-		default { 0 }
-		attributes { A_YMEM|A_NOINIT|A_NONSETTABLE }
-	}
-	defstate {
-		name { i }
-		type { int }
-		desc { internal }
-		default { 0 }
-		attributes { A_NONCONSTANT|A_NONSETTABLE }
-	}
-	protected {
-		int n;
-	}
-	setup {
-		n = int(N);
-		if (n == 0)
-			n = input.far()->numXfer();
-		input.setSDFParams(n,n-1);
-		output.setSDFParams(n,n-1);
-	}
-	initCode { addCode(init);}
-	go {
-		addCode(setupC);
-		for (int j = 0; j < n; j++) {
-			i = j;
-			if (input.resolvedType() == COMPLEX)
-				addCode(oneComplex);
-			else
-				addCode(oneReal);
-		}
-		addCode(restore);
-	}
-	
-	exectime {
-		return 4+2*n;
-	}
+	if (n>1) addCode(restore);
+    }
+    
+    exectime {
+	return 4+2*n;
+    }
 
-	codeblock(init) {
+    codeblock(init) {
 	move	#>$addr(output),x0
 	move	x0,$ref(ptr)
-	}
+    }
 
-	codeblock(setupC) {
+    codeblock(setupC) {
 	move	#$size(output)-1,m0
 	move	$ref(ptr),r0
-	}
+    }
 
-	codeblock(oneReal) {
-	move	$ref(input,i),x0
-	move	x0,x:(r0)+
-	}
+    codeblock(oneReal,"int i, char* xmemLoc") {
+	move	$ref(input,@i),x0
+	move	x0,x:@xmemLoc
+    }
 
-	codeblock(oneComplex) {
-	move	L:$addr(input,i),x
-	move	x,L:(r0)+
-	}
+    codeblock(oneComplex,"int i, char* xmemLoc") {
+	move	L:$addr(input,@i),x
+	move	x,L:@xmemLoc
+    }
 
-	codeblock(restore) {
+    codeblock(restore) {
 	move	r0,$ref(ptr)
 	move	m7,m0
-	}
+    }
 }
