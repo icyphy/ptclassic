0ba79301733513234849d19b4868a7d50546d9b0
Christopher Brooks <cxh@eecs.berkeley.edu>
1996-01-14 19:14:23 +0000

    Compacted makefiles, all and depend rule into one rule, as they no longer need to be different since we don't need make.template.

--- ptclassic/mk/toptydomain.mk
+++ ptclassic/mk/toptydomain.mk
@@ -35,18 +35,7 @@
 
 MAKEVARS = "PTARCH=$(PTARCH)"
 
-makefiles:
-	@for x in $(DIRS); do \
-	    if [ -w $$x ] ; then \
-		( cd $$x ; \
-		  echo Updating makefile in domains/$(ME)/$$x ; \
-		  $(MAKE) $(MFLAGS) $(MAKEVARS) \
-			VPATH=../../../tycho/domains/$(ME)/$$x $@ ; \
-		) \
-	    fi ; \
-	done
-
-all install clean sources realclean checkjunk sccsinfo:
+makefiles depend all install clean sources realclean checkjunk sccsinfo:
 	@for x in $(DIRS); do \
 	    if [ -w $$x ] ; then \
 		( cd $$x ; \
@@ -69,14 +58,3 @@ TAGS:
 	    fi ; \
 	done
 	-cat -s $(DIRS:%=%/TAGS) > $@
-
-depend:
-	@for x in $(DIRS); do \
-	    if [ -w $$x ] ; then \
-		( cd $$x ; \
-		  echo making $@ in domains/$(ME)/$$x ; \
-		  $(MAKE) -f make.template $(MFLAGS) $(MAKEVARS) \
-			VPATH=../../../tycho/domains/$(ME)/$$x $@ ; \
-		) \
-	    fi ; \
-	done
