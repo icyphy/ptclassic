a8a572c135448104ec7b965c02247ae98de8f454
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-01-16 17:23:41 +0000

    Initial revision

---
+++ ptclassic/src/domains/vhdl/targets/VHDLCSynchComm.pl
@@ -0,0 +1,34 @@
+defstar	{
+  name { CSynchComm }
+  domain { VHDL }
+  desc { Base class for VHDL-CGC synchronous communication }
+  version { $Id$ }
+  author { Michael C. Williamson, Jose Luis Pino }
+  copyright {
+Copyright (c) 1994, 1993 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright	for copyright notice,
+limitation of liability, and disclaimer	of warranty provisions.
+}
+  location { VHDL Target Directory }
+  explanation { }
+  public {
+    int numXfer;
+  }
+  protected {
+    friend class VHDLTarget;
+  }
+  hinclude { "VHDLTarget.h" }
+  defstate {
+    name {pairNumber}
+    type {int}
+    default {0}
+  }
+  defstate {
+    name {rtype}
+    type {string}
+    default {"type"}
+  }
+  setup {
+  }
+}
