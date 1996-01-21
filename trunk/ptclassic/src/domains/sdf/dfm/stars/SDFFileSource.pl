db793bc12cd7d19ddb9053a0f3daa42ced04a3d4
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-01-21 22:09:05 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/dfm/stars/SDFFileSource.pl
@@ -0,0 +1,43 @@
+defstar
+{
+    name { FileSource }
+    domain { SDF }
+    version { $Id$ }
+    author { E. A. Lee }
+    copyright {
+Copyright (c) 1990-%Q% The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
+    location { SDF dmm library }
+    descriptor {
+Output the given file name on each firing.
+If no name is given, then output a unique name of a temporary file
+on each firing.
+    }
+    ccinclude { "FileMessage.h" }
+
+    output {
+	name { output }
+	type { message }
+    }
+
+    defstate {
+	name { fileName }
+	type { string }
+	default { "" }
+	descriptor { File name to produce (use temporary file if empty). }
+    }
+    go {
+	FileMessage* fm;
+	const char* fn = (const char*) fileName;
+	if (*fn == '\0') {
+	    fm = new FileMessage();
+	} else {
+	    fm = new FileMessage(fn);
+	}
+	Envelope pkt(*fm);
+	output%0 << pkt;
+    }
+}
