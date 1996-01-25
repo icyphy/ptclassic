ed133db565db2913b0c93cdf2afd3aba5b62d6ba
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-01-25 02:51:18 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/dsp/stars/SDFGoertzelBase.pl
@@ -0,0 +1,97 @@
+defstar {
+	name { GoertzelBase }
+	domain { SDF }
+	version { $Id$ }
+	desc {
+Base class for Goertzel algorithm stars.
+	}
+	author { Brian L. Evans }
+	copyright {
+Copyright (c) 1990-%Q% The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { SDF dsp library }
+	input {
+		name{input}
+		type{float}
+	}
+	defstate {
+		name { k }
+		type { int }
+		default { 0 }
+		desc { the DFT coefficient to compute }
+	}
+	defstate {
+		name { N }
+		type { int }
+		default { 32 }
+		desc { length of the DFT }
+	}
+	defstate {
+		name { d1 }
+		type { float }
+		default { "0.0" }
+		desc {
+first-order feedback coefficient which is a function of k and N }
+		attributes { A_NONCONSTANT|A_NONSETTABLE }
+	}
+	defstate {
+		name { state1 }
+		type { float }
+		default { "0.0" }
+		desc { internal state. }
+		attributes { A_NONCONSTANT|A_NONSETTABLE }
+	}
+	defstate {
+		name { state2 }
+		type { float }
+		default { "0.0" }
+		desc {internal state. }
+		attributes { A_NONCONSTANT|A_NONSETTABLE }
+	}
+	protected {
+		double theta;
+	}
+	constructor {
+		theta = 0.0;
+	}
+	ccinclude { <math.h> }
+	setup {
+		if ( int(k) < 0 ) {
+		  Error::abortRun(*this, "The value for state k ",
+				  "must be nonnegative.");
+		  return;
+		}
+		if ( int(N) <= 0 ) {
+		   Error::abortRun(*this, "The value for state N ",
+				   "must be positive.");
+		   return;
+		}
+		if ( int(k) >= int(N) ) {
+		   Error::abortRun(*this, "The DFT coefficient must ",
+				   "be less than the DFT length.");
+		   return;
+		}
+		double kd = int(k);
+		double Nd = int(N);
+		theta = -2.0 * M_PI * kd / Nd;
+		d1 = -2.0 * cos(theta);
+		input.setSDFParams(int(N), int(N)-1);
+	}
+	go {
+		// Run all-pole section of Goertzel's algorithm N iterations
+		// Only one multipler (d1) in iteration
+		// FIXME: Zero state1 & state2 each time go method is called?
+		double acc = 0.0;
+		double d1val = d1;
+		for (int i = int(N)-1; i >= 0; i--) {
+		  acc = double(input%i);
+		  acc += d1val * double(state1);
+		  acc -= double(state2);
+		  state2 = double(state1);
+		  state1 = acc;
+		}
+	}
+}
