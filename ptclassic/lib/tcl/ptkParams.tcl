75587ab035f1bc2b7ba9dc0fe46e5a591f5c69df
Christopher Brooks <cxh@eecs.berkeley.edu>
1996-01-26 08:18:01 +0000

    Removed dmm

--- ptclassic/lib/tcl/ptkParams.tcl
+++ ptclassic/lib/tcl/ptkParams.tcl
@@ -802,8 +802,6 @@ proc ed_Apply {facet number args} {
   if { $changeFlag } {
     set ed_Parameters($facet,$number) $newParamArray
     ed_WriteParam $ed_Parameters($facet,$number) $facet $number $args
-    # added for DMM support 11/22/95 (kalavade)
-    ptkSetDMMChangedFlag $facet
   }
 }
 
