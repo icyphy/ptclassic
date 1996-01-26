5de91780a58f3a8ad80e05de3e79971da8059ef3
Jose Luis Pino <joseluispino@users.noreply.github.com>
1996-01-26 19:31:22 +0000

    Initial revision

---
+++ ptclassic/src/domains/cg56/stars/CG56IntToFix.pl
@@ -0,0 +1,30 @@
+defstar {
+    name { IntToFix }
+    domain { CG56 }
+    desc { Convert a integer input to a fixed-point output. }
+    version { $Id$ }
+    author { Jose Luis Pino }
+    copyright {
+Copyright (c) 1990-%Q% The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
+    location { CG56 conversion library }
+    input {
+	name {input}
+	type {INT}
+    }
+    output {
+	name {output}
+	type {FIX}
+    }
+    codeblock(convert) {
+	move	$ref(input),x1
+	move	x1,$ref(output)
+    }
+    go {
+	addCode(convert);
+    }
+    exectime { return 2; }
+}
