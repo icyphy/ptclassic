0a00c3712e6a150426c79d76ae7ee7065548dadc
pino <pino@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-01-26 21:00:59 +0000

    added inheritTypeFrom in WormMultiPort::newPort

--- trunk/ptclassic/src/kernel/EventHorizon.cc
+++ trunk/ptclassic/src/kernel/EventHorizon.cc
@@ -294,8 +294,9 @@ PortHole& WormMultiPort::newPort() {
 
                 from.setEventHorizon(in, worm, numToken);
 		fromPort.setPort(ghostName(toPort), parent(), type);
-                fromPort.inheritTypeFrom (realP);
-
+                fromPort.inheritTypeFrom(realP);
+		toPort.inheritTypeFrom(fromPort);
+		
                 to.ghostConnect(from);
 
                 fromPort.connect(realP,0);
@@ -313,8 +314,9 @@ PortHole& WormMultiPort::newPort() {
 		
                 to.setEventHorizon(out, worm, numToken);
 		toPort.setPort(ghostName(fromPort), parent(), type);
-                toPort.inheritTypeFrom (realP);
-
+                toPort.inheritTypeFrom(realP);
+		fromPort.inheritTypeFrom(toPort);
+		
                 to.ghostConnect(from);
 		
                 realP.connect(toPort,0);
