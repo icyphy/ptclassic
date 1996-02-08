162f3fa085a3bd3e9ae2f41940b343b40c2c8064
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-08 00:52:32 +0000

    Rewrote the replicated codeblock to take a local variable instead of relying on the current value of the inputNum state, removed inputNum state, and added comments in the generated code

--- trunk/ptclassic/src/domains/cg56/stars/CG56Mpy.pl
+++ trunk/ptclassic/src/domains/cg56/stars/CG56Mpy.pl
@@ -23,47 +23,44 @@ The inputs are multiplied and the result is written on the output.
 		name {output}
 		type {FIX}
 	}
-        state  {
-                name { inputNum }
-                type { int }
-                default { 0 }
-                desc { input#() }
-                attributes { A_NONCONSTANT|A_NONSETTABLE }
-        }
 	constructor {
 		noInternalState();
 	}
-	codeblock (std) {
-	move	$ref(input#1),x0	 ; 1st input -> x0
-        move	$ref(input#2),y0         ; 2nd input -> y0
-	}
-	codeblock (sat) {
-	mpy	x0,y0,a
-	move	a,$ref(output)	
-	}
-	codeblock (one) {
+
+	codeblock (copyInput) {
 	move	$ref(input#1),x0	; just move data from in to out
 	move	x0,$ref(output)
 	}
-        codeblock(loop) {
-        mpy     x0,y0,a     $ref(input#inputNum),x0
-	move    a,y0
+
+	codeblock (multiplyStart) {
+	move	$ref(input#1),x0	 ; 1st input -> x0
+        move	$ref(input#2),y0         ; 2nd input -> y0
+	}
+
+        codeblock(multiply,"int i") {
+        mpy     x0,y0,a     $ref(input#@i),x0	; a = x0 * y0, get next input
+	move    a,y0		; y0 = a
         }
+
+	codeblock (multiplyEnd) {
+	mpy	x0,y0,a		; a = x0 * y0
+	move	a,$ref(output)	
+	}
 
 	go {
 		if (input.numberPorts() == 1) {
-			addCode(one);
+			addCode(copyInput);
 			return;
 		}
-		addCode(std);
+
+		addCode(multiplyStart);
 		for (int i = 3; i <= input.numberPorts(); i++) {
-	                inputNum=i;
-			addCode(loop);
+			addCode(multiply(i));
 		}
-	        addCode(sat);
+	        addCode(multiplyEnd);
 	}
 	exectime {
 		if (input.numberPorts() == 1) return 2;
-		else return input.numberPorts() + 2;
+		return 2 + input.numberPorts();
 	}
 }
