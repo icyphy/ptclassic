7884a34ee50ce4c259a23366854a236f388a3649
luisgm <luisgm@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-09 21:41:03 +0000

    *** empty log message ***

--- ptclassic/src/domains/cg56/dsp/stars/CG56DTMFPostTest.pl
+++ ptclassic/src/domains/cg56/dsp/stars/CG56DTMFPostTest.pl
@@ -1,11 +1,11 @@
-defstar{
+ defstar{
 	name { DTMFPostTest }
 	domain { CG56 }
 	desc { 
 Returns whether or not a valid dual-tone modulated-frequency has
 been correctly detected based on the last three detection results.
-	}
-	version { @(#)CG56DTMFPostTest.pl	1.1	1/30/96 }
+        }
+	version { @(#)CG56DTMFPostTest.pl 1.17 1/29/96 }
 	acknowledge { }
 	explanation{
 The assumption is that the 100 msec DTMF interval has been split into
@@ -24,7 +24,7 @@ Electronic Data News, March 21, 1985.  Reprinted in
 \fIDigital Signal Processing Applications with the TMS320 Family\fR,
 Texas Instruments, 1986.
 	}
-	author { Luis Javier Gutierrez }
+	author { Luis Gutierrez }
 	copyright { 
 Copyright (c) 1990- The Regents of the University of California.
 All rights reserved.
@@ -34,69 +34,66 @@ limitation of liability, and disclaimer of warranty provisions.
 	location { CG56 dsp library }
 	input {
 		name { input }
-		type { INT }
+		type { int }
 	}
 	output {
 		name { output }
-		type { INT }
+		type { int }
 	}
 	input {
 		name { valid }
-		type { INT }
+		type { int }
 	}
 	state {
-		name { last_in }
-		type { INT }
+		name { last }
+		type { int }
+		default { "-1" }
 		desc { internal }
-		attributes {A_RAM|A_XMEM|A_NONCONSTANT|A_NONSETTABLE}
+		attributes {A_YMEM|A_NONCONSTANT|A_NONSETTABLE}
 	}
 	state{
-		name { sec_to_last }
-		type { INT }
+		name { secondToLast }
+		type { int }
+		default { "-1" }
 		desc { internal }
-		attributes {A_RAM|A_XMEM|A_NONCONSTANT|A_NONSETTABLE}
+		attributes {A_YMEM|A_NONCONSTANT|A_NONSETTABLE}
 	}
 	state { 
-		name { initial_last_input }
-		type { INT }
-		default { " -1" }
+		name { initialLastInput }
+		type { int }
+		default { "-1" }
 		desc{
-The value to assign to last and secondToLast internal states, which hold the
-previous two valid inputs.  For the purposes of DTMF detection, it is
-set to an integer that does not represent a DTMF digit.
-
-		}
-		attributes{A_ROM|A_XMEM}
+The value to assign to last and secondToLast internal states, which hold
+the previous two valid inputs.  For the purposes of DTMF detection, it
+is set to an integer that does not represent a DTMF digit.
+        	}
 	}
 
-	codeblock ( only ){
-		clr a0 #$val(initial_last_input),x0 
-		move $ref(valid),y0 
-		move $ref(last_in),x1
-		move $ref(sec_to_last),y1
-		cmp y0,a
-		jeq $label(_not_valid)
-		move $ref(input), x0
-		cmp x0,x1
-		jne $label(_not_valid)
-		cmp x1,y1
-		jeq $label(_not_valid)
-		move #-1,a
-		;assumes a==TRUE iff a!=0
-	  $label(_not_valid)
-		move a0,$ref(output)
-		move x0,$ref(last_in)
-		move x1,$ref(sec_to_last)
+	codeblock(block){
+		clr b                  #$val(initialLastInput),a1
+		move $ref(valid),y0
+		cmp y0,b               $ref(last),x0
+		jeq $label(not_valid)
+		move $ref(secondToLast),b
+		cmp x0,b               $ref(input),a
+		jeq $label(not_valid)
+		cmp x0,a
+		jne $label(not_valid)
+		move y0,b0
+$label(not_valid)
+		move b0,$ref(output)
+		move a1,$ref(last)
+		move x0,$ref(secondToLast)
+	}
+	setup {
+		secondToLast = int(initialLastInput);
+		last = int(initialLastInput);
 	}
 
-	exectime{ return 15; }
-
-	setup{
-		last_in = int(initial_last_input);
-		sec_to_last = int(initial_last_input);
-	}
-	
  	go{
-		addCode(only);
+		addCode(block);
 	}
+
+	// Return an execution estimate in pairs of cycles
+	exectime{ return 16; }
 }
