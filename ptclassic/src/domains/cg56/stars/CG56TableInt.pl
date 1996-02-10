9cceec3bc352c26b53ec0bbd91c2bb97344e7e89
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-10 01:33:24 +0000

    Fixed the assemly code generation

--- ptclassic/src/domains/cg56/stars/CG56TableInt.pl
+++ ptclassic/src/domains/cg56/stars/CG56TableInt.pl
@@ -30,28 +30,31 @@ limitation of liability, and disclaimer of warranty provisions.
 		name { values }
 		type { intarray }
 		default { "-1 1" }
-		desc { "table of values to output" }
-		attributes { A_NONCONSTANT|A_YMEM|A_NOINIT }
+		desc { table of values to output }
+		attributes { A_YMEM|A_CONSEC }
 	}
 	defstate {
 		name { runTimeCheck }
 		type { int }
 		default { YES }
-		desc { "If YES, check range of index at runtime, else omit"}
+		desc {
+If YES, check range of index at runtime, else omit.
+This is currently not supported in the CG56 domain.
+		}
 	}
 	constructor {
 		noInternalState();
 	}
 	codeblock(readIdx) {
-	move	#$addr(values),r0			; table address in y memory
-	move    $ref(input),n0			; table index
+	move	#>$addr(values),r0			; table address in y memory
+	move	$ref(input),n0			; table index
 	nop				; account for pipelining delay
 	}
 	codeblock(check) {
 ; There is no check to make sure that the index is in bounds
 	}
 	codeblock(lookup) {
-	move	y:(r0)+n0,x0		; read table value
+	move	y:(r0+n0),x0		; read table value
 	move	x0,$ref(output)			; write table value to output
 	}
 	go {
