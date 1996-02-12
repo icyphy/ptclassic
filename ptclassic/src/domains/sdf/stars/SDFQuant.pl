01d713d577ccd4e1757ca53ef4c09a801ed26963
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-12 20:24:04 +0000

    Added new public member quantizationLevel so that the derived star SDFQuantIdx can access its value

--- ptclassic/src/domains/sdf/stars/SDFQuant.pl
+++ ptclassic/src/domains/sdf/stars/SDFQuant.pl
@@ -2,13 +2,15 @@ defstar {
 	name { Quant }
 	domain { SDF }
 	desc {
-Quantize the input value to one of N+1 possible output levels using N thresholds.
-For an input less than or equal to the n-th threshold, but larger than all
-previous thresholds, the output will be the n-th level.  If the input is
-greater than all thresholds, the output is the N+1-th level.  If level is
-specified, there must be one more level than thresholds; the default
-value for level is 0, 1, 2, ... N.  This star is much slower than LinQuantIdx,
-so if possible, that one should be used instead.
+Quantize the input value to one of N+1 possible output levels using
+N thresholds.  For an input less than or equal to the n-th threshold,
+but larger than all previous thresholds, the output will be the n-th
+level.  If the input is greater than all thresholds, the output is
+the N+1-th level.  If level is specified, there must be one more level
+than thresholds; the default value for level is 0, 1, 2, ... N.  This
+star takes on the order of log N steps to find the right level, whereas
+the LinQuantIdx star takes a constant amount of time.  Therefore, use
+the LinQuantIdx star when possible.
 	}
 	version {$Id$}
 	author { E. A. Lee and J. Buck }
@@ -39,11 +41,14 @@ limitation of liability, and disclaimer of warranty provisions.
 		default {""}
 		desc { Output levels.  If empty, use 0, 1, 2, ...}
 	}
+	public {
+		int quantizationLevel;
+	}
 	setup {
 		int n = thresholds.size();
-		if (levels.size() == 0) {
-			// default: 0, 1, 2...
+		quantizationLevel = 0;
 
+		if (levels.size() == 0) {	// set to default: 0, 1, 2...
 			levels.resize(n + 1);
 			for (int i = 0; i <= n; i++)
 				levels[i] = i;
@@ -54,7 +59,7 @@ limitation of liability, and disclaimer of warranty provisions.
 		}
 	}
 	go {
-		double in = input%0;
+		double in = double(input%0);
 		int lo = 0;
 		int siz = thresholds.size();
 		int hi = siz;
@@ -71,6 +76,7 @@ limitation of liability, and disclaimer of warranty provisions.
 		// now in is <= thresholds[mid] but > all smaller ones.
 		// (where thresholds[siz] is infinity)
 
+		quantizationLevel = mid;
 		output%0 << levels[mid];
 	}
 }
