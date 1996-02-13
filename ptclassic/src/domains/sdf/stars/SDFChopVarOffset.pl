79b1e1e664a288fa6be31ef4455afbcdd0a32ac6
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-13 04:59:05 +0000

    Removed the inherited state use_past_inputs from user view since it depends on a fixed value for the offset state which this star dynamically changes at runtime

--- ptclassic/src/domains/sdf/stars/SDFChopVarOffset.pl
+++ ptclassic/src/domains/sdf/stars/SDFChopVarOffset.pl
@@ -12,17 +12,21 @@ See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
         }
 	desc {
-This star has the same functionality as the
-Chop star except that the offset parameter is
-determined at run time by a control input.
+This star has the same functionality as the Chop star except that the
+offset parameter is determined at run time by a control input and that
+the use_past_inputs parameter is set to FALSE and not accessible.
 	}
 
 	input { name { offsetCntrl } type { int } }
 
-	constructor { offset.setAttributes(A_NONCONSTANT | A_NONSETTABLE); }
+	constructor {
+		offset.setAttributes(A_NONCONSTANT | A_NONSETTABLE);
+		use_past_inputs.setAttributes(A_NONCONSTANT | A_NONSETTABLE);
+		use_past_inputs = FALSE;
+	}
 
 	go {
-		offset = int( offsetCntrl%0 );
+		offset = int(offsetCntrl%0);
 		SDFChop :: go();
 	}
 }
