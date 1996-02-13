492ed9b83ddd96254ba75100e7ec634ff4376baa
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-13 22:15:14 +0000

    Added noInternalState declaration to the constructor, moved the explanation comments to the description, and fixed the location information

--- trunk/ptclassic/src/domains/cg56/stars/CG56Sgn.pl
+++ trunk/ptclassic/src/domains/cg56/stars/CG56Sgn.pl
@@ -2,7 +2,8 @@ defstar {
 	name {Sgn}
 	domain {CG56}
 	desc {
-   	Signum.
+This star outputs the signum of its input.  The output is either -1.0
+or +0.99999999, since 1.0 is not a valid fixed point number on the DSP56001.
 	}
 	version {$Id$}
 	acknowledge { Gabriel version by E. A. Lee }
@@ -13,15 +14,9 @@ All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
 	}
-       	location { CG56 nonlinear functions library }
+       	location { CG56 main library }
 	explanation { 
-This star outputs the signum of its input.
 .Id "signum"
-The output is either -1.0 or +0.99999999, since 1.0 is not
-a valid fixed point number on the DSP56001.
-	}
-	execTime {
-		return 6;
 	}
 	input {
 		name{input}
@@ -31,6 +26,9 @@ a valid fixed point number on the DSP56001.
 		name{output}
 		type{FIX}
 	}
+	constructor {
+		noInternalState();
+	}
 	codeblock (sgnblock) {
         move    $ref(input),a
         move    #.99999999,b
@@ -39,9 +37,11 @@ a valid fixed point number on the DSP56001.
         tmi     x0,b
         move    b,$ref(output)
 	}	
-
 	go {
 		addCode(sgnblock);
  
+	}
+	execTime {
+		return 6;
 	}
 }
