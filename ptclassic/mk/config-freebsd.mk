32dea41495216038e554b3f2bf7c2aff8e18c1ae
Christopher Brooks <cxh@eecs.berkeley.edu>
1996-02-14 09:13:38 +0000

    Further config-freebsd modifications from Peter Dufault

--- ptclassic/mk/config-freebsd.mk
+++ ptclassic/mk/config-freebsd.mk
@@ -29,8 +29,50 @@
 #
 # @(#)config-freebsd.mk
 #                      
-# Programmer: John Wehle 
-#             john@feith.com
+# Programmer: 	John Wehle, john@feith.com
+# Modified by: 	Peter Dufault, dufault@hda.com (HD Associates, Inc)  
+#
+
+# In February  1996, the following configuration was tested:
+# OS: FreeBSD 2.1-STABLE.  This should work OK with 2.1-Release and
+# 2.1-Stable.  Any of the post 2.1-Stable snapshots are good bets since
+# people will be real interested in problems.  2.2 users are on their
+# own, as they know.
+# 
+# Compiler: the Pentium FreeBSD port of gcc 2.7.2 in the FreeBSD ports
+# collection with the following minor fix to disable .weak symbols.
+# This patch will be installed in the port, so hopefully no one else
+# will need to apply it.  I built ptolemy with both "-m486" and
+# "-mpentium", and it appears to work OK in both cases.  I didn't do any
+# timing studies.  Here is the .weak patch for the compiler:
+# 
+# [The file to modify would be $PTOLEMY/src/gnu/src/gcc/config/i386/freebsd.h
+# This patch fixes a bug where ld gets errors because there are multiply
+# defined symbols in different .o files that make up a library. ]
+#
+# *** freebsd.h.b4	Mon Feb 12 18:40:53 1996
+# --- freebsd.h	Mon Feb 12 18:41:25 1996
+# ***************
+# *** 122,130 ****
+# --- 122,134 ----
+#   
+#   /* This is how we tell the assembler that a symbol is weak.  */
+#   
+# + #if 0
+# + /* This requires support in both the assembler and the loader:
+# +  */
+#   #define ASM_WEAKEN_LABEL(FILE,NAME) \
+#     do { fputs ("\t.weak\t", FILE); assemble_name (FILE, NAME); \
+#          fputc ('\n', FILE); } while (0)
+# + #endif
+#   
+#   /* The following macro defines the format used to output the second
+#      operand of the .type assembler directive.  Different svr4 assemblers
+# 
+# Library: libg++-2.7.1 directly from prep.  It configures and installs with
+# no problems.  I'll look at installing a FreeBSD port for this.
+# 
+
 # --------------------------------------------------------------------
 # |  Please see the file ``config-default.mk'' in this directory!    |
 # --------------------------------------------------------------------
@@ -64,12 +106,14 @@ OCT_CC =	gcc $(OCTTOOLS_MM_LIB)
 #	      of the OS for this architecture for c++
 # USERFLAGS - Ptolemy makefiles should never set this, but the user can set it.
 
-OPTIMIZER=	-O2 -m486 -pipe
-#OPTIMIZER=	-O2 -m486 -fomit-frame-pointer -pipe
+
+OPTIMIZER =	-O2 -mpentium -pip3
+#OPTIMIZER =	-O2 -m486 -pipe
+#OPTIMIZER =	-O2 -m486 -fomit-frame-pointer -pipe
 
 # Under gcc-2.7.0, you will need to add -fno-for-scope to $WARNINGS
 WARNINGS =	-Wall -Wcast-align #-Wcast-qual 
-ARCHFLAGS=	-Dfreebsd
+ARCHFLAGS =	-Dfreebsd
 LOCALCFLAGS =	-fwritable-strings
 
 GPPFLAGS =	$(OPTIMIZER) $(MEMLOG) $(WARNINGS) \
@@ -88,29 +132,29 @@ GNULIB =	/usr/lib
 CC_STATIC = 	-static
 
 # system libraries for linking .o files from C files only
-CSYSLIBS=-lm -lc -lcompat
+CSYSLIBS =	-lm -lc -lcompat
 # system libraries (libraries from the environment)
-SYSLIBS=-lg++ $(CSYSLIBS)
+SYSLIBS =	-lg++ -lstdc++  $(CSYSLIBS)
 
-LINKFLAGS=-L$(LIBDIR) -Xlinker -S -Xlinker -x -static
-LINKFLAGS_D=-L$(LIBDIR) -g -static
+LINKFLAGS =	-L$(LIBDIR) -Xlinker -S -Xlinker -x -static
+LINKFLAGS_D =	-L$(LIBDIR) -g -static
 
 # octtools/attache uses this
 TERMLIB_LIBSPEC = -ltermcap
 
 # ptcl and pigiRPc use this for cuserid.o when CGTarget is used
 # only defined in this config file
-COMPATLIB = -lcompat
+COMPATLIB = 	-lcompat
 
 #
 # Directories to use
 #
-X11_INCSPEC = -I/usr/X11R6/include
-X11INCL     = -I/usr/X11R6/include
-X11_LIBSPEC = -L/usr/X11R6/lib -lX11
+X11_INCSPEC = 	-I/usr/X11R6/include
+X11INCL     = 	-I/usr/X11R6/include
+X11_LIBSPEC = 	-L/usr/X11R6/lib -lX11
 
 # Use -lSM -lICE for X11R6, don't use then for X11R5
-X11EXT_LIBSPEC=-lXext -lSM -lICE
+X11EXT_LIBSPEC=	-lXext -lSM -lICE
 #X11EXT_LIBSPEC=-lXext
 
 # Variables for Pure Inc tools (purify, purelink, quantify)
@@ -131,5 +175,9 @@ QUANTIFY =
 S56DIR =
 
 # Used to compile xv.  Use -traditional to avoid varargs problems
-XV_CC =		gcc -traditional $(X11_INCSPEC) $(X11_LIBSPEC)
+# I'm not proud of this "const" work around for FreeBSD.  I'm not sure
+# why NetBSD doesn't need it - We need "-traditional" for varargs and
+# then we get into problems with "const" in the headers.
+XV_CC =		gcc -Dconst= -D__const= -traditional \
+			-DXLIB_ILLEGAL_ACCESS $(X11_INCSPEC) $(X11_LIBSPEC)
 XMKMF =		xmkmf
