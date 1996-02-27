f52dbc03b8c7a6fcc787874262b101de5f31d9ee
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-27 00:50:02 +0000

    Synchronized the interface and implementation to match the SDF version: the lower and upper limits on indexing from the input to output are computed by the C code since the value of offset can change; and update execution time estimates

--- trunk/ptclassic/src/domains/cgc/stars/CGCChopVarOffset.pl
+++ trunk/ptclassic/src/domains/cgc/stars/CGCChopVarOffset.pl
@@ -21,18 +21,30 @@ determined at run time through a control input.
 
 	constructor { 
 		noInternalState();
-		offset.setAttributes(A_NONCONSTANT | A_NONSETTABLE); 
+		offset.setAttributes(A_NONCONSTANT|A_NONSETTABLE); 
 	}
 
 	codeblock (init) {
-		$ref(offset) = $ref(offsetCntrl);
+	$ref(offset) = $ref(offsetCntrl);
 	}
+        codeblock(range) {
+        int hiLim = $val(nwrite) - $ref(offset) - 1;
+        if (hiLim >= $val(nwrite)) hiLim = $val(nwrite) - 1;
+        else if ($val(use_past_inputs)) hiLim = $val(nwrite) - 1;
+
+        int loLim = $val(nwrite) - $val(nread) - $ref(offset);
+        if (loLim < 0) loLim = 0;
+
+        int inidx = $val(nread) - $val(nwrite) + $ref(offset);
+        if (inidx < 0) inidx = 0;
+        }
 	go {
-		addCode(decl);	// look at CGCChop star
+		addCode(decl);		// look at CGCChop star
 		addCode(init);
-		addCode(out);	// look at CGCChop star
+		addCode(range);
+		addCode(out);		// look at CGCChop star
 	}
 	exectime {
-		return int(nwrite) + 1;
+		return CGCChop::myExecTime() + 8;
 	}
 }
