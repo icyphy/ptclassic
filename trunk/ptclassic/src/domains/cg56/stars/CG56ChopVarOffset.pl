6ed166413be774994a88ee6557bf05d3b61a5284
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-27 16:47:29 +0000

    Filled in missing desc, explanation, and location fields; added SCCS identifiers; derived star from CG56Chop; and reformatted generated assembly code

--- trunk/ptclassic/src/domains/cg56/stars/CG56ChopVarOffset.pl
+++ trunk/ptclassic/src/domains/cg56/stars/CG56ChopVarOffset.pl
@@ -1,107 +1,86 @@
 defstar {
 	name { ChopVarOffset }
+	derivedFrom { Chop }
 	domain { CG56 }
-	desc { }
-	version {@(#)CG56ChopVarOffset.pl 1.10 1/26/96 }
-	author { Luis Gutierrez }
+	desc {
+This star has the same functionality as the
+Chop star except now the offset parameter is
+determined at run time through a control input.
+	}
+	version { $Id$ }
+	author { Luis Javier Gutierrez }
 	copyright{
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, an ddisclaimer of warranty provisions.
 	}
-	location { }
-	explanation { }
-	
-	input{
-		name{ input }
-		type {ANYTYPE}
-	}
-	input{
-		name { offset }
+	location { CG56 main library }
+
+	input {
+		name { offsetCntrl }
 		type { int }
-	}
-	output{
-		name{ output }
-		type {=input}
-	}
-	state{
-		name { nread }
-		type { int }
-		default { 128 }
-		desc { Number of particles read.}
-		attributes { A_YMEM|A_NONCONSTANT }
-	}
-	state{
-		name { nwrite }
-		type { int }
-		default { 64 }
-		desc { Number of particles written.}
-		attributes { A_YMEM|A_NONCONSTANT}
-	}
-	state{
-		name{ one }
-		type { int }
-		default { 1 }
-		attributes { A_YMEM|A_CONSTANT|A_NONSETTABLE }
-		desc { 
-temporary solution to a problem--will get rid of this when i rewrite the star
-		     }
-	}
-	
-	setup{
-		if ((int)nread <= 0){
-			Error::abortRun(*this, "The number of samples to read ",
-							"must be positive");
-			return;
-		}
-		if ((int)nwrite <= 0){
-			Error::abortRun(*this, "The number of samples to write",
-							"must be positive");
-			return;
-		}
-		input.setSDFParams(int(nread),int(nread) - 1);
-		output.setSDFParams(int(nwrite),int(nwrite)-1);
 	}
 
-	codeblock(chop){
-		move $ref(offset),b
-		clr a             b,x0                ;B=X0=offset          
-		neg b             #$addr(output),r5   ;B=-offset
-		cmp x0,a          $ref(nread),y0      ;Y0=nread
-		tgt a,b                               ;if (offset > 0) B=0
-		jle $label(skip_padding)              ;do does at leat 1
-		do x0,$label(skip_padding)            ;iteration so if offset
-		move a,$mem(output):(r5)+             ;is 0 one most skip this
+	constructor {
+		offset.setAttributes(A_NONCONSTANT | A_NONSETTABLE);
+		use_past_inputs.setAttributes(A_NONCONSTANT | A_NONSETTABLE);
+		use_past_inputs = FALSE;
+	}
+
+	// Inherit the setup method from the Chop star
+
+	codeblock(init_range) {
+; compute hiLim, index, and padding
+	}
+
+	codeblock(chop) {
+; Register usage
+; x0 = offset
+; x1 = 1
+; y0 = nread
+; y1 = address of input buffer
+; r5 = address of output buffer
+; r0 = y1 + nread - 1
+	move	$ref(offset),b
+	clr	a	b,x0		; B=X0=offset	  
+	neg	b	#$addr(output),r5		; B=-offset
+	cmp	x0,a	$ref(nread),y0			; Y0=nread
+	tgt	a,b			; if (offset > 0) B=0
+	jle	$label(skip_padding)		; do does at least 1
+	do	x0,$label(skip_padding)		; iteration so if offset
+	move	a,$mem(output):(r5)+		; is 0 one most skip this
 $label(skip_padding) 
-		add x0,a          $ref(one),x1        ;A=offset X1=1
-		add y0,a          $ref(nwrite),x0     ;A=offset+readn=hiLim X0=nwrite
-		cmp x0,a          $addr(input),y1
-		tlt x0,a                              ;if (nwrite < hiLim) hiLim = nwrite
-		cmp y0,b	  		      
-		jgt $label(index_ok)                  ;if (index(=B)>= nread)
-		move y0,b                             ; index=nread-1
-		sub x1,b         
+	add	x0,a	$ref(one),x1		; A=offset X1=1
+; Register usage change: x0 = nwrite
+	add	y0,a	$ref(nwrite),x0		; A=offset+readn=hiLim X0=nwrite
+	cmp	x0,a	$addr(input),y1
+	tlt	x0,a			; if nwrite < hiLim, hiLim = nwrite
+	cmp	y0,b
+	jgt	$label(index_ok)		; if (index >= nread)
+	move	y0,b			; index = nread-1
+	sub	x1,b
 $label(index_ok)
-		add y1,b                              ;
-		move b,r0
-		do a,$label(input_to_output)
-		move $mem(input):(r0+n0)+,x1          ;copy input to output
-		move x1,$mem(output):(r5)+
+	add	y1,b
+	move	b,r0
+	do	a,$label(input_to_output)
+	move	$mem(input):(r0+n0)+,x1		; copy input to output
+	move	x1,$mem(output):(r5)+
 $label(input_to_output)
-		sub x0,a  #$0,b			      ;A=hiLim-nwrite B=0
-		cmp a,b                               ;if (hiLim-nwrite < 0)
-		jge $label(skip_end_padding)
-		neg a	                              ; A=nwrite-hiLim
-		do a,$label(skip_end_padding)
-		move b,$mem(output):(r5)+           
+	sub	x0,a			; A = hiLim-nwrite B=0
+	tst	a			; if (hiLim-nwrite < 0)
+	jge	$label(skip_end_padding)
+	neg	a			; A = nwrite-hiLim
+	do	a,$label(skip_end_padding)
+	move	b,$mem(output):(r5)+	   
 $label(skip_end_padding)
-	 }
-	go{
+	}
+
+        go {
 		addCode(chop);
 	}
+
+	exectime {
+		return CG56Chop::myExecTime() + 8;
+	}
 }
-	
-	
-							
-		
