6dd51e33ab92d3a7f918d96b48b8b1225efc7c91
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-02-27 16:53:14 +0000

    Added comments in generated assembly code; a data memory location is no longer allocated for the step parameter because its value is used instead; and cleaned up location, version, and explanation fields

--- ptclassic/src/domains/cg56/stars/CG56RampInt.pl
+++ ptclassic/src/domains/cg56/stars/CG56RampInt.pl
@@ -2,19 +2,19 @@ defstar {
 	name { RampInt }
 	domain { CG56 }
 	desc { Integer Ramp Generator }
-	version {@(#)CG56RampInt.pl 1.10 1/26/96 }
-	author { Luis Gutierrez }
+	version { $Id$ }
+	author { Luis Javier Gutierrez and David G. Messerschmitt }
 	copyright{
 Copyright (c) 1990-1996 The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, an ddisclaimer of warranty provisions.
 	}
-	location { CG56 signal sources library }
+	location { CG56 main library }
 	explanation {
 Generates an integer ramp signal, starting at \fIvalue\fR (default 0)
-with step size \fIstep\fR (default 1) saturating upon reaching largest integer
-if \fIsaturation\R="YES"
+with step size \fIstep\fR (default 1) saturating upon reaching largest
+integer if \fIsaturation\fR is "YES".
 	}
 	output {
 		name { output }
@@ -25,13 +25,12 @@ if \fIsaturation\R="YES"
 		type { int }
 		default { 1 }
 		desc { Increment from one sample to the next }
-		attributes{ A_YMEM|A_NONCONSTANT }
 	}
 	state {
 		name { value }
 		type { int }
 		default { 0 }
-		desc { initial value output by RampInt.}
+		desc { initial value output by RampInt }
 	}
 	state {
 		name { saturation }
@@ -46,29 +45,31 @@ if \fIsaturation\R="YES"
 		desc { internal accumulator }
 		attributes { A_NONSETTABLE|A_YMEM|A_NONCONSTANT }
 	}
-	setup{
-		sum=int(value);
+	setup {
+		sum = int(value);
 	}
-	go{
+	go {
 		addCode(init);
 		addCode(add);
-		if (int(saturation))
-				addCode(saturate);
-		else
-				addCode(no_saturate);
+		if (int(saturation)) {
+			addCode(saturate);
+		}
+		else {
+			addCode(no_saturate);
+		}
 	}
-        codeblock(init){
-                move $ref(sum),a
-                move $ref(step),x0
+        codeblock(init) {
+	move	$ref(sum),a		; a holds the last sum
+	move	#$val(step),x0		; x0 is the step
         }
-        codeblock(add){
-                add x0,a  a,$ref(output)
+        codeblock(add) {
+	add	x0,a	a,$ref(output)		; output last sum
         }
-        codeblock(no_saturate){
-                move a1,$ref(sum)
+        codeblock(no_saturate) {
+	move	a1,$ref(sum)
         }
-	codeblock(saturate){
-		move a,$ref(sum)
+	codeblock(saturate) {
+	move	a,$ref(sum)
 	}
 
 	exectime{
