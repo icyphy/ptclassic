7c0823026d7e45cb0c4fd8dca1f3aa15e7c3f669
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-03-07 22:42:20 +0000

    copyright

--- trunk/ptclassic/src/utils/libexttools/MathematicaIfc.cc
+++ trunk/ptclassic/src/utils/libexttools/MathematicaIfc.cc
@@ -4,9 +4,9 @@ All rights reserved.
 
 Permission is hereby granted, without written agreement and without
 license or royalty fees, to use, copy, modify, and distribute this
-software and its documentation for any purpose, provided that the above
-copyright notice and the following two paragraphs appear in all copies
-of this software.
+software and its documentation for any purpose, provided that the
+above copyright notice and the following two paragraphs appear in all
+copies of this software.
 
 IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
 FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
@@ -20,6 +20,9 @@ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
 PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
 CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 ENHANCEMENTS, OR MODIFICATIONS.
+
+						PT_COPYRIGHT_VERSION_2
+						COPYRIGHTENDKEY
 
 Programmer: Steve X. Gu and Brian L. Evans
 Date of creation: 01/13/96
