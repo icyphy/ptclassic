3c467bc7123a39f8a9caf361fd6f1218fb63a108
luisgm <luisgm@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-03-07 19:27:14 +0000

    fixed exectime; improved error checking

--- trunk/ptclassic/src/domains/cg56/stars/CG56Chop.pl
+++ trunk/ptclassic/src/domains/cg56/stars/CG56Chop.pl
@@ -13,10 +13,10 @@ consumed on previous firings (if "use_past_inputs" parameter is YES),
 or zero (otherwise).  If "offset" is negative, then the first "offset"
 input particles will be discarded.
 	}
-	version { $Id$ }
+	version { @(#)CG56Chop.pl	1.2	2/27/96 }
 	author { Luis Javier Gutierrez and Edward A. Lee }
 	copyright{
-Copyright (c) 1990-%Q% The Regents of the University of California.
+Copyright (c) 1990- The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, an disclaimer of warranty provisions.
@@ -70,7 +70,8 @@ if offset > 0, specify whether to use previously read inputs
 		attributes { A_NONSETTABLE|A_YMEM }
 	}
         protected {
-                int hiLim, index, padding;
+                int hiLim, index, padding, num_of_inputs;
+		int time=3;
         }
         method {
             name { computeRange }
@@ -84,13 +85,24 @@ if offset > 0, specify whether to use previously read inputs
 		// it resets the offset parameter
 		hiLim = int(nread) + int(offset);
 		if (hiLim > int(nwrite)) hiLim = int(nwrite);
-
+		num_of_inputs = int(nread);
 		padding = int(offset);
-		if (padding < 0 ) padding = 0;
-
+		if (padding < 0 ) {
+			padding = 0;
+			num_of_inputs = hiLim;
+		};
+		 if (padding > int(nwrite)){
+			padding = int(nwrite);
+		};
+		if ((int(nwrite) - padding) < num_of_inputs) num_of_inputs = int(nwrite) - padding;
 		index = -int(offset);
-		if (index < 0) index = 0;
-		else if (index >= int(nread)) index = int(nread)-1;
+		if (index < 0) { index = 0; }
+		else if (index > int(nread)){
+			Error::abortRun(*this, "absolute value of negative offsets",
+					       " must not be larger than nread");
+			return;
+		}
+		
 	    }
 	}
 
@@ -108,32 +120,40 @@ if offset > 0, specify whether to use previously read inputs
 			return;
 		}
 
-		// FIXME: We had to use a past_inputs state because
-		// the CG56 domain does not support a construct like
-		// input.setSDFParams(int(nread),int(nread)+int(offset)-1);
-		if (int(use_past_inputs) && int(offset) > 0) { 
-			past_inputs.resize(int(offset));
-		} 
 		input.setSDFParams(int(nread),int(nread)-1);
 		output.setSDFParams(int(nwrite),int(nwrite)-1);
-
 		computeRange();
-	}
+		// FIXME: We had to use a past_inputs state because
+		// the CG56 domain does not support a construct like
+		// input.setSDFParams(int(nread), int(nread)+int(offset)-1);
+		if (int(use_past_inputs) > 0) {
+			past_inputs.resize(padding);
+		}
+	}	
 
 	go {
 		addCode(init_registers(index));
 		if (padding > 0) {
 			if (int(use_past_inputs)) { 
 				int save_index = (int(nread) - padding);
+				if (save_index < 0) save_index = 0;
+				else if (save_index > int(nread)) save_index = int(nread);
 				addCode(pad_and_save_past_inputs(padding, save_index));
+				time += ( 5 + 3*padding );
 			}
 			else {
-				addCode(pad_with_zeroes(padding)); 
+				addCode(pad_with_zeroes(padding));
+				time += ( padding + 3); 
 			}
 		}
-		addCode(read_to_write(hiLim));
+		if (num_of_inputs > 0) {
+			addCode(read_to_write(num_of_inputs));
+			time += ( 3 + 2*num_of_inputs);
+		}
 		if (hiLim < int(nwrite)) {
-			addCode(pad_with_zeroes(int(nwrite)-hiLim));
+			int pad_end = int(nwrite) - hiLim;
+			time += (pad_end + 3);
+			addCode(pad_with_zeroes(pad_end));
 		}
 	}
 
@@ -142,30 +162,30 @@ if offset > 0, specify whether to use previously read inputs
 ; r0 = address of the input sample (initialized to start of input buffer)
 ; r1 = address of the output sample (initialized to start of output buffer)
 		move #($addr(input)+(@x)),r0
-		clr	a	move #$addr(output),r1		; set a = 0
+		move #$addr(output),r1	
+		move #0,x0
 	}
 	codeblock(pad_with_zeroes,"int N"){
-		do	#@N,$label(loop)		; pad with @N zeroes
-		move	a,$mem(output):(r1)+
+		do	#@N,$label(loop)	
+		move	x0,$mem(output):(r1)+
 $label(loop)
 	}
 	codeblock(pad_and_save_past_inputs,"int N, int loc"){
 		move	#$addr(past_inputs),r4
 		move	#($addr(input)+@loc),r3
 		do	#@N,$label(loop2)
-		tst	b	$mem(input):(r3)+,b	y:(r4),a
-		tst	b	a,$mem(output):(r1)+	b,y:(r4)+
+		tst	b	$mem(input):(r3)+,a	y:(r4),b
+		tst	b	b,$mem(output):(r1)+	a,y:(r4)+
 $label(loop2)
 	}
 	codeblock(read_to_write,"int N"){
-		do	#@N,$label(loop3)		; copy input to output
+		do	#@N,$label(loop3)	
 		move	$mem(input):(r0)+,a
 		move	a,$mem(output):(r1)+
 $label(loop3)
 	}
 
 	exectime {
-		computeRange();
-		return 8;		// FIXME
+		return time;
 	}
 }
