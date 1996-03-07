292bc530ef6c17beec41a9b61f316d8cfb68c771
Christopher Brooks <cxh@eecs.berkeley.edu>
1996-03-07 10:56:47 +0000

    Updated to %Q%

--- ptclassic/src/domains/sdf/tcltk/stars/SDFTkBreakPt.pl
+++ ptclassic/src/domains/sdf/tcltk/stars/SDFTkBreakPt.pl
@@ -10,7 +10,7 @@ If the expression evaluates to true, it causes the run to pause.
 	version { $Id$ }
 	author { Alan Kamas }
 	copyright {
-Copyright (c) 1990-1995 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
