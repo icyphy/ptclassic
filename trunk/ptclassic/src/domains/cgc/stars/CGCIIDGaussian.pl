4379a6743873158778a48d07940418c89a5f3bb1
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-03-07 15:57:21 +0000

    Fixed start time of copyright

--- trunk/ptclassic/src/domains/cgc/stars/CGCIIDGaussian.pl
+++ trunk/ptclassic/src/domains/cgc/stars/CGCIIDGaussian.pl
@@ -7,7 +7,7 @@ Generate pseudo-IID-Gaussian random variables.
 	version { $Id$ }
 	author { Soonhoi Ha }
         copyright {
-Copyright (c) 1992-%Q% The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
