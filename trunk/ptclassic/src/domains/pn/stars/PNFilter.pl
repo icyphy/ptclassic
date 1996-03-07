2c491e42b88f070fb3679e8be1c56be801c234a5
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-03-07 13:05:45 +0000

    Added %Q

--- trunk/ptclassic/src/domains/pn/stars/PNFilter.pl
+++ trunk/ptclassic/src/domains/pn/stars/PNFilter.pl
@@ -6,7 +6,7 @@ defstar
     desc { Filter out multiples of a prime number. }
     author { T. M. Parks }
     copyright {
-Copyright (c) 1990-1994 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
