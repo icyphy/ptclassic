9529ee1d9b1cd937010395f71a1a0be005d9ae21
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-03-07 20:21:02 +0000

    Conversion to %Q

--- trunk/ptclassic/src/domains/sdf/contrib/stars/SDFTWTA.pl
+++ trunk/ptclassic/src/domains/sdf/contrib/stars/SDFTWTA.pl
@@ -14,7 +14,7 @@ and linear interpolation/extrapolation is used to find the phase and
 output amplitude, which is converted back to a complex output sample.
 }
   copyright {
-Copyright (c) 1990-1995 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
