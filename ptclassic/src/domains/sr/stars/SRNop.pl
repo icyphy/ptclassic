49b0811407ef8fb1a76f35eb08c37e9e02820ce2
sedwards <sedwards@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-03-19 10:59:43 +0000

    Initial revision

---
+++ ptclassic/src/domains/sr/stars/SRNop.pl
@@ -0,0 +1,25 @@
+defstar {
+	name {Nop}
+	domain {SR}
+	desc { Does nothing }
+	version {$Id$}
+	author { E. A. Lee and D. Niehaus }
+	copyright {
+Copyright (c) 1990-1994 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	location { SR main library }
+	output {
+		name {output}
+		type {=input}
+	}
+	input {
+	        name {input}
+		type {anytype}
+	}
+	go {
+	}
+}
+
