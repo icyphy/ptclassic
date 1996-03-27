b11ee4f4898d5e284d48ecb61daebcdca23b0832
luisgm <luisgm@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-03-27 14:52:56 +0000

    reimplemented goertzel using first order recursive algorithm

--- trunk/ptclassic/src/domains/cg56/dsp/stars/CG56Goertzel.pl
+++ trunk/ptclassic/src/domains/cg56/dsp/stars/CG56Goertzel.pl
@@ -1,10 +1,10 @@
 defstar {
 	name { Goertzel }
 	domain { CG56 }
-	derived { GoertzelBase }
+	derived	{ GoertzelBase }
 	version { $Id$ }
 	desc {
-Second-order recursive computation of the kth coefficient of an
+First-order recursive computation of the kth coefficient of an
 N-point DFT using Goertzel's algorithm.
 	}
 	author { Brian L. Evans }
@@ -17,25 +17,12 @@ limitation of liability, and disclaimer of warranty provisions.
 	location { CG56 dsp library }
 	explanation {
 .lp
-This two-pole, one-zero IIR filter has a transfer function of
+This one-pole IIR filter has a transfer function of
 .EQ
-{1 ~-~ {W sub N} sup k z sup -1} over
-{1 ~-~ 2 cos ( {2 pi} over {N} k ) z sup -1 ~+~ sup -2} ~.
+{1} over
+{1 ~-~ {W sub N} sup {-k} z sup -1 } ~.
 .EN
 .ID "Goertzel's algorithm"
-This filter is a biquad filter with
-.sp 0.5
-.(l
-n0 = 1
-n1 = -Wn = exp(j 2 pi k / N)
-n2 = 0
-d1 = -2 cos(2 pi k / N)
-d2 = 1
-.)l
-.sp 0.5
-The Goertzel's implementation takes the simpler forms of n0, n2, and d2
-into account, and computes n1 and d1 from the parameters k and N.
-It is implemented in direct form II.
 .ID "Oppenheim, A. V."
 .ID "Schafer, R. W."
 .UH REFERENCES
@@ -47,50 +34,20 @@ Prentice-Hall: Englewood Cliffs, NJ, 1989.
 		name{output}
 		type{complex}
 	}
-	defstate {
-		name { negWnReal }
-		type { fix }
-		default { "0.0" }
-		desc {
-internal state for the storage of the negative of the real part of the
-twiddle factor, which is a function of k and N
-		}
-		attributes { A_NONCONSTANT|A_NONSETTABLE }
-	}
-	defstate {
-		name { negWnImag }
-		type { fix }
-		default { "0.0" }
-		desc {
-internal state for the storage of the imaginary part of the twiddle factor,
-which is a function of k and N
-		}
-		attributes { A_NONCONSTANT|A_NONSETTABLE }
-	}
-	setup {
-		// check parameter values and compute theta
-		CG56GoertzelBase::setup();
 
-		negWnReal = -cos(theta);
-		negWnImag = -sin(theta);
-	}
+	//Inherit setup from GoertzelBase
 
 	codeblock(result) {
-; compute complex-valued Goertzel filter output a + j b.  Register usage:
-; b  = state1 - state2*cos(theta)   where  x0 = state2=y[n-1]  and  y0 = -cos(theta)
-; a  = -state2*sin(theta)           where  x1 = state2=y[n-1]  and  y1 = -sin(theta)
-; note that accumulator b initially contains the value of y[n] = state1
-	move	#$val(negWnImag),y1
-	mpyr	y1,x0,a		#$val(negWnReal),y0
-	macr	x1,y0,b		a,y:$addr(output)
-	move	b,x:$addr(output)
+;when the loop ends the real coeff. is stored in x0
+;the imaginary coeff is stored in b
+	move 	x0,a
+	move	ab,$ref(output)
 	}
 
 	go {
-		// Discard all but the last sample
-		CG56GoertzelBase::go();
-
-		// Compute the complex result 
+		//Discard all but the last sample
+		CG56NewGoertzelBase::go();
+		//Output the complex result
 		addCode(result);
 	}
 
@@ -99,6 +56,7 @@ which is a function of k and N
 		// oscillator cycles because that's the way it was done in
 		// Gabriel: they simply counted the number of instructions.
 
-		return 4 + CG56GoertzelBase::myExecTime();
+		return ( 2 + CG56GoertzelBase::myExecTime() );
 	}
 }
+
