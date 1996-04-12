4d5c1446c2d69bc7b06c4efed5fd40fc56f05922
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-04-12 11:40:50 +0000

    added firing as parameter to registerPortHole

--- trunk/ptclassic/src/domains/vhdl/kernel/VHDLTarget.h
+++ trunk/ptclassic/src/domains/vhdl/kernel/VHDLTarget.h
@@ -109,8 +109,8 @@ public:
 	virtual void registerState(State*, const char*, int=-1, int=-1);
 
 	// Register PortHole reference.
-	virtual void registerPortHole(VHDLPortHole*, const char*, int=-1,
-				      const char* ="");
+	virtual void registerPortHole(VHDLPortHole*, const char*, int,
+				      int=-1, const char* ="");
 
 	// Return the assignment operators for States and PortHoles.
 	virtual const char* stateAssign();
