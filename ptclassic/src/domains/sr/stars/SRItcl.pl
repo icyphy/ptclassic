9c7b3bbab0009fa413564642bf4e575414820a5b
sedwards <sedwards@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-04-25 18:18:00 +0000

    Initial revision

---
+++ ptclassic/src/domains/sr/stars/SRItcl.pl
@@ -0,0 +1,14 @@
+defstar {
+	name { Itcl }
+	domain { SR }
+        derivedFrom { SRItclStar }
+	desc { Does nothing }
+	version { $Id$ }
+	author { S. A. Edwards }
+	copyright {
+Copyright (c) 1990-1996 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+}
