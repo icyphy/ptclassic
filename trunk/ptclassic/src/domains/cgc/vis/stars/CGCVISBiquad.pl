6f9083cf52fa554b17093364a05ccee16722d881
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-05-23 03:58:19 +0000

    works

--- trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISBiquad.pl
+++ trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISBiquad.pl
@@ -1,7 +1,7 @@
 defstar {
 	name { QuadBiquad }
-	domain { SDF }
-	version { @(#)SDFQuadBiquad.pl	1.2 05/06/96 }
+	domain { CGC }
+	version { $Id$ }
 	author { William Chen }
 	copyright {
 Copyright (c) 1990-1996 The Regents of the University of California.
@@ -9,7 +9,7 @@ All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
 	}
-	location { SDF vis library }
+	location { CGC vis library }
 	desc { 
 	  An IIR Biquad filter.
 	}
@@ -23,7 +23,6 @@ limitation of liability, and disclaimer of warranty provisions.
 	  type { float }
 	  desc { Output float type }
 	}
-        ccinclude {<vis_proto.h>}
 	defstate {
 	  name {numtaps}
 	  type {floatarray}
@@ -61,94 +60,86 @@ limitation of liability, and disclaimer of warranty provisions.
 #define UPPERBOUND (32767) 
 #define LOWERBOUND (-32768)
 	}
-	protected {
-	  double *inarray,*split_result;
-	  float *result;
-	  short *dennum,*state,*outarray;
-	  short n0,scaledown;
+	codeblock(mainDecl) {
+	  const int $starSymbol(NUMPACK)=4;
+	  double* $starSymbol(inarray) = (double*)
+	    memalign(sizeof(double),sizeof(double));
+	  double* $starSymbol(split_result) = (double*)
+	    memalign(sizeof(double),sizeof(short)*$starSymbol(NUMPACK));
+	  float* $starSymbol(result) = (float *)
+	    memalign(sizeof(float),sizeof(float));
+	  short* $starSymbol(dennum) = (short *)
+	    memalign(sizeof(double),sizeof(short)*$starSymbol(NUMPACK));
+	  short* $starSymbol(state) = (short *)
+	    memalign(sizeof(float),sizeof(short)*$starSymbol(NUMPACK));
+	  short* $starSymbol(outarray) = (short *)
+	    memalign(sizeof(double),sizeof(short)*$starSymbol(NUMPACK));
 	}
-	constructor {
-	  inarray=split_result=0;
-	  result=0;
-	  dennum=state=outarray=0;
-	  scaledown=n0=0;
+	initCode {
+	  addInclude("<vis_proto.h>");
+	  addDeclaration(mainDecl);
 	}
-	destructor {
-	  free(inarray);
-	  free(split_result);
-	  free(result);
-	  free(dennum);
-	  free(state);
-	  free(outarray);
-	}
-	begin {
-	  int i;
-	  short *indexcount;
-
-  	  // allocate tap and state arrays
-	  free(inarray);
-	  free(split_result);
-	  free(result);
-	  free(dennum);
-	  free(state);
-	  free(outarray);
-	  inarray = (double *) memalign(sizeof(double),sizeof(double));
-	  split_result = (double *)
-	    memalign(sizeof(double),sizeof(short)*NUMPACK);
-	  result = (float *) memalign(sizeof(float),sizeof(float));
-	  dennum = (short *) memalign(sizeof(double),sizeof(short)*NUMPACK);
-	  state = (short *) memalign(sizeof(float),sizeof(short)*NUMPACK);
-	  outarray = (short *) memalign(sizeof(double),sizeof(short)*NUMPACK);
-
-	  scaledown = (short) 1 << scalefactor;
-
-	  // initialize n0
-	       n0 = ((short) scale/scaledown*numtaps[0]);
-
-	  // initialize denominator/numerator array
-	       indexcount = dennum;
-	  for(i=0;i<2;i++){
-	    *indexcount++ = ((short) scale/scaledown*dentaps[i]);
-	    *indexcount++ = ((short) scale/scaledown*numtaps[i+1]);
-	  }
-
-	  // initialize states
-	       for(i=0;i<4;i++){
-		 state[i]=0;
-	       }
-	}
-	go {	
+	codeblock(localDecl) {
 	  double *outvalue,*packedfilt;
 	  double upper, lower;
 	  double *allstates,*alltaps;
 	  float *statemem,*result_ptr;
-	  int outerloop,innerloop;
+	  int outerloop,innerloop,i;
 	  short *statetap_prod,*invalue,*result_den;
-
-	  vis_write_gsr(8);
-	  *inarray = double(signalIn%0);
-       	  invalue = (short *) inarray;
-	  statemem = (float *) state;
-	  allstates = (double *) state;
-	  alltaps = (double *) dennum;
+	  short *indexcount,n0,scaledown;
+	}
+	codeblock(filter){
+	  scaledown = (short) 1 << $val(scalefactor);
+	  /* initialize n0 */
+	  n0 = ((short) $val(scale)/scaledown*(double)$ref2(numtaps,0));
+	  /*initialize denominator/numerator array*/
+	  indexcount = $starSymbol(dennum);
+	  for(i=0;i<2;i++){
+	    *indexcount++ = (short)
+	      $val(scale)/scaledown*(double)$ref2(dentaps,i);
+	    *indexcount++ = (short)
+	      $val(scale)/scaledown*(double)$ref2(numtaps,i+1);
+	  }
+	  *$starSymbol(inarray) = (double)$ref(signalIn);
+       	  invalue = (short *) $starSymbol(inarray);
+	  statemem = (float *) $starSymbol(state);
+	  allstates = (double *) $starSymbol(state);
+	  alltaps = (double *) $starSymbol(dennum);
 
 	  for(outerloop=3;outerloop>=0;outerloop--){
-	    // find product of states/taps
-		 upper = vis_fmul8sux16(*allstates,*alltaps);
+	    /* find product of states/taps */
+	    upper = vis_fmul8sux16(*allstates,*alltaps);
 	    lower = vis_fmul8ulx16(*allstates,*alltaps);
-	    *split_result = vis_fpadd16(upper,lower);
-	    result_ptr = (float *) split_result;
-	    *result = vis_fpadd16s(result_ptr[0],result_ptr[1]);
-	    statetap_prod = (short *) result;
-	    // find next_state
-		 statemem[1] = statemem[0];
-	    state[0]=(invalue[outerloop]-(statetap_prod[0]<<1))<<scalefactor;
-	    state[1] = state[0];
-	    // find output
-	    outarray[outerloop]=(n0*state[0]>>15)+(statetap_prod[1]<<1);
+	    *$starSymbol(split_result) = vis_fpadd16(upper,lower);
+	    result_ptr = (float *) $starSymbol(split_result);
+	    *$starSymbol(result) = vis_fpadd16s(result_ptr[0],result_ptr[1]);
+	    statetap_prod = (short *) $starSymbol(result);
+	    /* find next_state */
+	    statemem[1] = statemem[0];
+	    $starSymbol(state)[0]=
+	      (invalue[outerloop]-(statetap_prod[0]<<1))<<$val(scalefactor);
+	    $starSymbol(state)[1] = $starSymbol(state)[0];
+	    /* find output */
+	    $starSymbol(outarray)[outerloop] =
+	      (n0*$starSymbol(state)[0]>>15)+(statetap_prod[1]<<1);
 	  }
-	  outvalue = (double *) outarray;
-	  signalOut%0 <<  *outvalue;
+	  outvalue = (double *) $starSymbol(outarray);
+	  $ref(signalOut)= *outvalue;
+	}	
+	go {
+	  addCode(localDecl);
+	  addCode(filter);
 	}
+	codeblock(freeit){
+          free($starSymbol(inarray));
+          free($starSymbol(split_result));
+          free($starSymbol(result));
+          free($starSymbol(dennum));
+          free($starSymbol(state));
+          free($starSymbol(outarray));
+	}
+	wrapup {
+          addCode(freeit);
+        }
 }
 
