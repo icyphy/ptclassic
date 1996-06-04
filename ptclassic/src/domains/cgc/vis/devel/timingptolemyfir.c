eb7201f2f17b42c59ad7da27853f559fa0a70792
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-04 14:48:50 +0000

    works

--- ptclassic/src/domains/cgc/vis/devel/timingptolemyfir.c
+++ ptclassic/src/domains/cgc/vis/devel/timingptolemyfir.c
@@ -13,8 +13,9 @@
 
 #define NTIME 1000
 #define LENGTH 8192
-#define NTAPS 16
-
+#define NTAPS 32
+#define NPACK 4
+#define SCALE 32767
 /***************************************************************/
 
 main (int argc, char *argv[])
@@ -23,71 +24,101 @@ main (int argc, char *argv[])
   extern  int   optind;
   int           c;
   int      ntime, length;
-  int      verbose;
+  int      verbose,print;
   hrtime_t start, end;
   float    time1, time2;
   double sinarg,twopiover50;
-  vis_s16  *dst,*src,*filtertaps;
-  int      i,j;
+  vis_s16  *dst0,*src,*srcptolemy,*filtertaps,*shift_taparray,*indexcount;
+  int      i,j,maxpast,tappadlength,taprowindex,tapcolindex;
+  int	   scalefactor,scaledown,numloop,taplength;
+  vis_d64  *dst1;
+  short    *tmp;
 
   twopiover50 = 0.0;
   ntime = NTIME;
   length = LENGTH;
+  taplength = NTAPS;
   verbose = 0;
-  while ((c = getopt(argc, argv, "hvn:l:")) != -1)
+  print = 0;
+  while ((c = getopt(argc, argv, "hvpn:l:t:")) != -1)
     switch (c) {
-      case 'h':
-        printf("usage: %s [-h] [-v] [-n ntimes] [-l length]\n", argv[0]);
-        printf("  Default Setting  \n");
-        printf("===================\n");
-        printf(" NTIMES = 1000 \n");
-        printf(" LENGTH = 8192 \n");
-        exit(1);
-      case 'v': verbose = 1;
-                break;
-      case 'n': ntime = atoi(optarg);
-                break;
-      case 'l': length = atoi(optarg);
-                break;
+    case 'h':
+      printf("usage: %s [-h] [-v] [-p] [-n ntimes] [-l length] [-t ntaps]\n", argv[0]);
+      printf("  Default Setting  \n");
+      printf("===================\n");
+      printf(" NTIMES = 1000 \n");
+      printf(" LENGTH = 8192 \n");
+      printf(" NTAPS = 16 \n");
+      exit(1);
+    case 'v': verbose = 1;
+      break;
+    case 'p': print = 1;
+      break;
+    case 'n': ntime = atoi(optarg);
+      break;
+    case 'l': length = atoi(optarg);
+      break;
+    case 't': length = atoi(optarg);
+      break;
     };
 /***************************************************************/
-dst = (vis_s16 *) memalign(sizeof(double),sizeof(short)*LENGTH);
-src = (vis_s16 *) memalign(sizeof(double),sizeof(short)*(LENGTH+NTAPS-1));
+dst0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*LENGTH);
+dst1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*LENGTH/4);
+src = (vis_s16 *) memalign(sizeof(double),sizeof(short)*LENGTH);
 filtertaps = (vis_s16 *) memalign(sizeof(double),sizeof(short)*NTAPS);
+srcptolemy = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
 /***************************************************************/
   for(i=0;i<LENGTH;i++){
     sinarg = twopiover50;
     twopiover50 += 0.628318530717958;
-    dst[i] = (short)32767 * (double) sin(sinarg);
+    src[i] = (short)SCALE * (double) sin(sinarg);
   }
-    filtertaps[0]=(short)32767* -4.10485902015429e-05;
-    filtertaps[1]=(short)32767*-6.27991234694974e-06;
-    filtertaps[2]=(short)32767*-6.48044369959903e-06;
-    filtertaps[3]=(short)32767*-6.43306093680702e-06;
-    filtertaps[4]=(short)32767*-6.18549614648338e-06;
-    filtertaps[5]=(short)32767*-5.65484231686277e-06;
-    filtertaps[6]=(short)32767*-4.90690921899934e-06;
-    filtertaps[7]=(short)32767*-3.87530545584888e-06;
-    filtertaps[8]=(short)32767*-2.64796014539528e-06;
-    filtertaps[9]=(short)32767*-1.1672977954277e-06;
-    filtertaps[10]=(short)32767*4.65807896753724e-07;
-    filtertaps[11]=(short)32767*2.31694868332586e-06;
-    filtertaps[12]=(short)32767*4.26108402754801e-06;
-    filtertaps[13]=(short)32767*6.34709045098423e-06;
-    filtertaps[14]=(short)32767*8.32039368507842e-06;
-    filtertaps[15]=(short)32767*1.03524342421933e-05;
+    filtertaps[0]=(short)SCALE*5.749331917706068e-02;
+    filtertaps[1]=(short)SCALE*2.356117725608943e-01;
+    filtertaps[2]=(short)SCALE*4.326064391182815e-01;
+    filtertaps[3]=(short)SCALE*3.9298714665431733e-01;
+    filtertaps[4]=(short)SCALE*6.9707352819524643e-02;
+    filtertaps[5]=(short)SCALE*-1.9527235873539017e-01;
+    filtertaps[6]=(short)SCALE*-1.1501073848243082e-01;
+    filtertaps[7]=(short)SCALE*1.0384807075682692e-01;
+    filtertaps[8]=(short)SCALE*1.0127325405350047e-01;
+    filtertaps[9]=(short)SCALE*-6.2250082138045150e-02;
+    filtertaps[10]=(short)SCALE*-8.0637756012925166e-02;
+    filtertaps[11]=(short)SCALE*4.1872874030868444e-02;
+    filtertaps[12]=(short)SCALE*6.1733323254462361e-02;
+    filtertaps[13]=(short)SCALE*-3.0889724643843371e-02;
+    filtertaps[14]=(short)SCALE*-4.5776126368330651e-02;
+    filtertaps[15]=(short)SCALE*2.4137901399342250e-02;
+    filtertaps[16]=(short)SCALE*5.749331917706068e-02;
+    filtertaps[17]=(short)SCALE*2.356117725608943e-01;
+    filtertaps[18]=(short)SCALE*4.326064391182815e-01;
+    filtertaps[19]=(short)SCALE*3.9298714665431733e-01;
+    filtertaps[20]=(short)SCALE*6.9707352819524643e-02;
+    filtertaps[21]=(short)SCALE*-1.9527235873539017e-01;
+    filtertaps[22]=(short)SCALE*-1.1501073848243082e-01;
+    filtertaps[23]=(short)SCALE*1.0384807075682692e-01;
+    filtertaps[24]=(short)SCALE*1.0127325405350047e-01;
+    filtertaps[25]=(short)SCALE*-6.2250082138045150e-02;
+    filtertaps[26]=(short)SCALE*-8.0637756012925166e-02;
+    filtertaps[27]=(short)SCALE*4.1872874030868444e-02;
+    filtertaps[28]=(short)SCALE*6.1733323254462361e-02;
+    filtertaps[29]=(short)SCALE*-3.0889724643843371e-02;
+    filtertaps[30]=(short)SCALE*-4.5776126368330651e-02;
+    filtertaps[31]=(short)SCALE*2.4137901399342250e-02;
+    scalefactor = 0;
 /***************************************************************/
-
   if (verbose) {
     printf("NTIME=%d\n", ntime);
     printf("\n");
     printf("LENGTH=%d\n", length);
     printf("\n");
+    printf("NTAPS=%d\n", taplength);
+    printf("\n");
   }
-
+/***************************************************************/
   start = gethrtime();
   for (j = 0; j < ntime; j ++) {
-    vdk_c_fir_16(src, dst, length, filtertaps, NTAPS);
+    vdk_c_ptolemyfir(src, dst0, length, filtertaps, taplength);
   }
   end = gethrtime();
   time1 = (float) (end - start)/1000000/ntime;
@@ -95,14 +126,58 @@ filtertaps = (vis_s16 *) memalign(sizeof(double),sizeof(short)*NTAPS);
   if (verbose) {
     printf("    C-GL Time = %f msec\n", time1);
   }
-
+/***************************************************************/
+  /*adjust the src*/
+  for(i=0;i<length/4;i++){
+   srcptolemy[NPACK*i+3]=src[i*NPACK];
+   srcptolemy[NPACK*i+2]=src[i*NPACK+1];
+   srcptolemy[NPACK*i+1]=src[i*NPACK+2];
+   srcptolemy[NPACK*i]=src[i*NPACK+3];
+  }
+  /* determine padded tap buffer size */
+  if (taplength == 0){
+    maxpast = 0;
+  }
+  else if ((taplength-1)%NPACK==0){
+    maxpast = (int)(taplength-1)/NPACK+1;
+  }
+  else{
+    maxpast = (int)(taplength-1)/NPACK+2;
+  }
+  numloop = maxpast;
+  tappadlength = NPACK*maxpast;  
+  shift_taparray = (vis_s16 *)
+    memalign(sizeof(double),sizeof(short)*NPACK*tappadlength);
+  /* fill the tap buffer */
+  indexcount = shift_taparray;
+  for(taprowindex=0;taprowindex<NPACK;taprowindex++){
+    for(tapcolindex=0;tapcolindex<tappadlength;tapcolindex++){
+      *indexcount++ = 0;
+    }
+  }
+  for(taprowindex=0;taprowindex<NPACK;taprowindex++){
+    indexcount = shift_taparray + (tappadlength+1)*taprowindex;
+    for(tapcolindex=0;tapcolindex<taplength;tapcolindex++){
+      *indexcount++ = (short)(filtertaps[tapcolindex]>>scalefactor);
+    }
+  }
+  vis_write_gsr((scalefactor+1)<<3);	  
+	  
   start = gethrtime();
   for (j = 0; j < ntime; j ++) {
-    vdk_vis_fir_16(src, dst, length, filtertaps, NTAPS);
+    vdk_vis_ptolemyfir((double*)srcptolemy, dst1, length/4,
+		       (double*) shift_taparray, numloop);
   }
   end = gethrtime();
   time2 = (float) (end - start)/1000000/ntime;
-
+/***************************************************************/
+  if (print) {
+    printf("compare outputs\n");
+    tmp=(short*)dst1;
+    for(i=0;i<length;i++){
+      printf("%i %i %i\n",i,dst0[i],tmp[i]);
+    }
+  }
   if (verbose) {
     printf("  VIS-GL Time = %f msec\n", time2);
     printf("  C-GL/VIS-GL = %f\n", time1/time2);
@@ -113,5 +188,4 @@ filtertaps = (vis_s16 *) memalign(sizeof(double),sizeof(short)*NTAPS);
 
   exit(0); 
 }
-
 /***************************************************************/
