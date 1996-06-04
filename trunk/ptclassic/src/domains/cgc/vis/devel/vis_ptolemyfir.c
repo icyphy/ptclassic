4a34667135be69118f9fd93416fca12e65f7adb0
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-04 22:18:41 +0000

    unrolled loop to speed up

--- trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyfir.c
+++ trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyfir.c
@@ -6,27 +6,31 @@
 void vdk_vis_ptolemyfir(double* src, double* dst1, int dlen, double*
 		   shift_taparray, int numloop)
 {
-  double	*tapptr[4], tapvalue;
+  double	*tapptr0,*tapptr1,*tapptr2,*tapptr3, tapvalue;
   double	pairlohi, pairlolo, pairhilo,pairhihi;
   double 	pairlo, pairhi, pair, packedOut,*packedaccum;
-  float  	packedhi, packedlo, datahi[1],datalo[1];
-  float 	tappairhi, tappairlo, splithi,splitlo;
-  int  		outerloop, innerloop,nminusk;
-  int	     	n,i,j,k;
   double 	result[2];
-  double	accumpair[4];
+  double	accumpair0, accumpair1, accumpair2, accumpair3;
+  float  	packedhi, packedlo, datahi,datalo;
+  float 	tappairhi, tappairlo, splithi,splitlo;
+  float		*r;
+  int  		outerloop, nminusk;
+  int	     	n,i,j,k;
   short 	*t0,*t1,*t2;
-  double	resultcheck[2];
-  float		*f1,*r1,*r;
-  float		resultprint[2];
-  short		*s1;
-  double	d[1];
 
   for(i=0;i<dlen;i++){
-    for (j = 0; j < 4; j++) {
-      accumpair[j] = vis_fzero();
-      tapptr[j] = (double *) (shift_taparray + j * numloop);
-    }
+    accumpair0 = vis_fzero();
+    tapptr0 = (double *) (shift_taparray);
+
+    accumpair1 = vis_fzero();
+    tapptr1 = (double *) (shift_taparray + numloop);
+
+    accumpair2 = vis_fzero();
+    tapptr2 = (double *) (shift_taparray + 2 * numloop);
+
+    accumpair3 = vis_fzero();
+    tapptr3 = (double *) (shift_taparray + 3 * numloop);
+
     nminusk = i;
     n = numloop;
     if(nminusk<numloop)
@@ -34,46 +38,106 @@ void vdk_vis_ptolemyfir(double* src, double* dst1, int dlen, double*
     /* filter data */
     for (outerloop = 0; outerloop < n; outerloop++) {
       /* set up data */
-      *datahi = vis_read_hi(src[nminusk]);
-      *datalo = vis_read_lo(src[nminusk]);
+      datahi = vis_read_hi(src[nminusk]);
+      datalo = vis_read_lo(src[nminusk]);
       /* calculate four outputs */
-      for (innerloop = 0; innerloop < 4; innerloop++) {
-	/*
-	 * set up tap pairs for each
-	 * shifted taparray
-	 */
-	tapvalue = *tapptr[innerloop]++;
-	tappairhi = vis_read_hi(tapvalue);
-	tappairlo = vis_read_lo(tapvalue);
-	/* take inner products */
-	pairlolo = vis_fmuld8sux16(*datalo, tappairlo);
-	pairlohi = vis_fmuld8ulx16(*datalo, tappairlo);
-	pairhilo = vis_fmuld8sux16(*datahi, tappairhi);
-	pairhihi = vis_fmuld8ulx16(*datahi, tappairhi);
-	/* accumulate results */
-	pairlo = vis_fpadd32(pairlolo, pairlohi);
-	pairhi = vis_fpadd32(pairhilo, pairhihi);
-	pair = vis_fpadd32(pairhi, pairlo);
-	accumpair[innerloop] = vis_fpadd32(accumpair[innerloop],pair);
-
-      }
+      /*
+       * 0: set up tap pairs for each
+       * shifted taparray
+       */
+      tapvalue = *tapptr0++;
+      tappairhi = vis_read_hi(tapvalue);
+      tappairlo = vis_read_lo(tapvalue);
+      /* take inner products */
+      pairlolo = vis_fmuld8sux16(datalo, tappairlo);
+      pairlohi = vis_fmuld8ulx16(datalo, tappairlo);
+      pairhilo = vis_fmuld8sux16(datahi, tappairhi);
+      pairhihi = vis_fmuld8ulx16(datahi, tappairhi);
+      /* accumulate results */
+      pairlo = vis_fpadd32(pairlolo, pairlohi);
+      pairhi = vis_fpadd32(pairhilo, pairhihi);
+      pair = vis_fpadd32(pairhi, pairlo);
+      accumpair0 = vis_fpadd32(accumpair0,pair);
+      /*
+       * 1: set up tap pairs for each
+       * shifted taparray
+       */
+      tapvalue = *tapptr1++;
+      tappairhi = vis_read_hi(tapvalue);
+      tappairlo = vis_read_lo(tapvalue);
+      /* take inner products */
+      pairlolo = vis_fmuld8sux16(datalo, tappairlo);
+      pairlohi = vis_fmuld8ulx16(datalo, tappairlo);
+      pairhilo = vis_fmuld8sux16(datahi, tappairhi);
+      pairhihi = vis_fmuld8ulx16(datahi, tappairhi);
+      /* accumulate results */
+      pairlo = vis_fpadd32(pairlolo, pairlohi);
+      pairhi = vis_fpadd32(pairhilo, pairhihi);
+      pair = vis_fpadd32(pairhi, pairlo);
+      accumpair1 = vis_fpadd32(accumpair1,pair);
+      /*
+       * 2: set up tap pairs for each
+       * shifted taparray
+       */
+      tapvalue = *tapptr2++;
+      tappairhi = vis_read_hi(tapvalue);
+      tappairlo = vis_read_lo(tapvalue);
+      /* take inner products */
+      pairlolo = vis_fmuld8sux16(datalo, tappairlo);
+      pairlohi = vis_fmuld8ulx16(datalo, tappairlo);
+      pairhilo = vis_fmuld8sux16(datahi, tappairhi);
+      pairhihi = vis_fmuld8ulx16(datahi, tappairhi);
+      /* accumulate results */
+      pairlo = vis_fpadd32(pairlolo, pairlohi);
+      pairhi = vis_fpadd32(pairhilo, pairhihi);
+      pair = vis_fpadd32(pairhi, pairlo);
+      accumpair2 = vis_fpadd32(accumpair2,pair);
+      /*
+       * 3: set up tap pairs for each
+       * shifted taparray
+       */
+      tapvalue = *tapptr3++;
+      tappairhi = vis_read_hi(tapvalue);
+      tappairlo = vis_read_lo(tapvalue);
+      /* take inner products */
+      pairlolo = vis_fmuld8sux16(datalo, tappairlo);
+      pairlohi = vis_fmuld8ulx16(datalo, tappairlo);
+      pairhilo = vis_fmuld8sux16(datahi, tappairhi);
+      pairhihi = vis_fmuld8ulx16(datahi, tappairhi);
+      /* accumulate results */
+      pairlo = vis_fpadd32(pairlolo, pairlohi);
+      pairhi = vis_fpadd32(pairhilo, pairhihi);
+      pair = vis_fpadd32(pairhi, pairlo);
+      accumpair3 = vis_fpadd32(accumpair3,pair);
+      
       nminusk--;
     }
     /*
      * sum accumulators and pack outputs into a
      * double
      */
-    for (k = 3; k >=0; k--) {
-      splithi = vis_read_hi(accumpair[3-k]);
-      splitlo = vis_read_lo(accumpair[3-k]);
-      r=(float *) result;
-      r[k] = vis_fpadd32s(splithi, splitlo);
-    }
-    packedaccum = (double *) result;
-    packedhi = vis_fpackfix(*packedaccum);
-    packedaccum++;
-    packedlo = vis_fpackfix(*packedaccum);
+    r=(float *) result;
+
+    splithi = vis_read_hi(accumpair0);
+    splitlo = vis_read_lo(accumpair0);
+    r[3] = vis_fpadd32s(splithi, splitlo);
+    
+    splithi = vis_read_hi(accumpair1);
+    splitlo = vis_read_lo(accumpair1);
+    r[2] = vis_fpadd32s(splithi, splitlo);
+    
+    splithi = vis_read_hi(accumpair2);
+    splitlo = vis_read_lo(accumpair2);
+    r[1] = vis_fpadd32s(splithi, splitlo);
+    
+    splithi = vis_read_hi(accumpair3);
+    splitlo = vis_read_lo(accumpair3);
+    r[0] = vis_fpadd32s(splithi, splitlo);
+
+    packedhi = vis_fpackfix(result[0]);
+    packedlo = vis_fpackfix(result[1]);
     dst1[i] = vis_freg_pair(packedhi, packedlo);
-  }	
+  }
 }
+
 
