6b2b146c9b1d12e97f2fff4cbaecadf6f73c652f
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-04 18:18:04 +0000

    Deleted unorthodox header delimited by 'ident

--- trunk/ptclassic/src/domains/de/stars/DEVarServer.pl
+++ trunk/ptclassic/src/domains/de/stars/DEVarServer.pl
@@ -1,32 +1,9 @@
-ident
-{
-/**************************************************************************
-Version identification:
-$Id$
-
- Copyright (c) 1990-%Q% The Regents of the University of California.
-                       All Rights Reserved.
-
- Programmer:  T. M. Parks
- Date of creation: 1/14/91
-
- This star emulates a server with a variable service time.
- If input events arrive when it is idle, they will be serviced
- immediately and will be delayed only by the service time.
- If input events arrive while another event is being serviced,
- they will be queued.  When the server becomes free, it will
- service any events waiting in its queue.
-
-**************************************************************************/
-}
-
 defstar
 {
     name { VarServer }
     domain { DE }
     derivedFrom { Server }
-    desc
-    {
+    desc {
 This star emulates a server with a variable service time.
 If input events arrive when it is idle, they will be serviced
 immediately and will be delayed only by the service time.
@@ -36,27 +13,24 @@ service any events waiting in its queue.
     }
     version { $Id$ }
     author { T. M. Parks }
-	copyright {
+    copyright {
 Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
-	}
+    }
 
-    input
-    {
+    input {
 	name { newServiceTime }
 	type { float }
     }
 
-    constructor
-    {
+    constructor {
 	// state is no longer constant
 	serviceTime.clearAttributes(A_CONSTANT|A_SETTABLE);
     }
 
-    go
-    {
+    go {
 	if (newServiceTime.dataNew)
 	    serviceTime = newServiceTime.get();
 
