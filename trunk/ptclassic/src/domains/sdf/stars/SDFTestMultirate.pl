ab23517d9357ddba97e9e5c93a5130772d8227af
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-04 19:11:24 +0000

    Added FIXME comment concerning printf statement in go method, and made minor formatting changes

--- trunk/ptclassic/src/domains/sdf/stars/SDFTestMultirate.pl
+++ trunk/ptclassic/src/domains/sdf/stars/SDFTestMultirate.pl
@@ -2,9 +2,9 @@ defstar {
 	name { TestMultirate }
 	domain { SDF }
 	desc {
-This star consumes and produces any fixed number of tokens
-on any number of inputs and outputs.  The output value is always
-zero.  IT IS INTENDED FOR TESTING SCHEDULERS ONLY.
+This star consumes and produces any fixed number of tokens on any number
+of inputs and outputs.  The output value is always zero.  IT IS INTENDED
+FOR TESTING SCHEDULERS ONLY.
 	}
 	version { $Id$ }
 	author { Edward A. Lee, Richard Stevens, and Christopher Scannell }
@@ -16,7 +16,7 @@ limitation of liability, and disclaimer of warranty provisions.
 
 Richard Stevens and Christopher Scannell are employees of the
 U.S. Government whose contributions to this computer program fall
-within the scope of 17 U.S.C. A7 105 
+within the scope of 17 U.S.C. A7 105.
 	}
 	location { SDF main library }
 	inmulti {
@@ -28,54 +28,57 @@ within the scope of 17 U.S.C. A7 105
 		type { float }
 	}
 	state {
-	  name { consume }
-	  type { intarray }
-	  default { 1 }
-	  descriptor { number of inputs consumed for each port }
+		name { consume }
+		type { intarray }
+		default { 1 }
+		descriptor { number of inputs consumed for each port }
 	}
 	state {
-	  name {produce }
-	  type {intarray }
-	  default { 1 }
-	  descriptor { number of tokens produced on each output }
+		name { produce }
+		type { intarray }
+		default { 1 }
+		descriptor { number of tokens produced on each output }
 	}
 	setup {
-	  if ((input.numberPorts() != consume.size()) ||
-	      (output.numberPorts() != produce.size())) {
-	    Error::abortRun(*this,
-              "list of number of tokens consumed or produced does not match"
-	      " the actual number of inputs or outputs.");
-	    return;
-	  }
-	  MPHIter nexti(input);
-	  DFPortHole *p;
-	  int i = 0;
-	  while ((p = (DFPortHole*)nexti++) != 0) {
-	    p->setSDFParams(consume[i],1);
-	    i++;
-	  }
-	  MPHIter nexto(output);
-	  i = 0;
-	  while ((p = (DFPortHole*)nexto++) != 0) {
-	    p->setSDFParams(produce[i],1);
-	    i++;
-	  }
+		if ((input.numberPorts() != consume.size()) ||
+		    (output.numberPorts() != produce.size())) {
+		    Error::abortRun(*this,
+			"the list of number of tokens consumed or produced ",
+			"does not match the actual number of inputs or ",
+			"outputs.");
+		    return;
+		}
+		MPHIter nexti(input);
+		DFPortHole* iportp;
+		int i = 0;
+		while ((iportp = (DFPortHole*)nexti++) != 0) {
+		    iportp->setSDFParams(consume[i], 1);
+		    i++;
+		}
+		MPHIter nexto(output);
+		DFPortHole* oportp;
+		i = 0;
+		while ((oportp = (DFPortHole*)nexto++) != 0) {
+		    oportp->setSDFParams(produce[i], 1);
+		    i++;
+		}
 	}
 	go {
-	  printf("Running %s\n", name());
+		// FIXME: Why is there a printf statement in a go method?
+		printf("Running %s\n", name());
 
-	  // Don't need to read each input port: SDF scheduler handles it
-	  // because we set setSDFParams for input ports in the setup method
+		// Don't need to read each input port: SDF scheduler handles it
+		// because we set setSDFParams for input ports in setup method
 
-	  // Output the right number of zeroes on each output port
-	  MPHIter nexto(output);
-	  DFPortHole *p;
-	  int j = 0;
-	  while ((p = (DFPortHole*)nexto++) != 0) {
-	    for (int i = 0; i < produce[j]; i++) {
-	      (*p)%i << 0.0;
-	    }
-	    j++;
-	  }
+		// Output the right number of zeroes on each output port
+		MPHIter nexto(output);
+		DFPortHole* oportp;
+		int j = 0;
+		while ((oportp = (DFPortHole*)nexto++) != 0) {
+			for (int i = 0; i < produce[j]; i++) {
+				(*oportp)%i << 0.0;
+			}
+			j++;
+		}
 	}
 }
