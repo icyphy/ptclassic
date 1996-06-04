4ddcc48045b22fdd65ee0656b95c694fe6318e4b
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-04 18:31:31 +0000

    added in fpu

--- ptclassic/src/domains/cgc/vis/devel/c_ptolemyfir.c
+++ ptclassic/src/domains/cgc/vis/devel/c_ptolemyfir.c
@@ -3,7 +3,7 @@
 #include "vis_proto.h"
 
 /***************************************************************/
-void vdk_c_ptolemyfir(vis_s16 *src, vis_s16 *dst, int dlen, 
+void vdk_cint_ptolemyfir(vis_s16 *src, vis_s16 *dst, int dlen, 
                    vis_s16 *fir, int flen)
 {
     int i,k,nminusk,n;
@@ -22,6 +22,28 @@ void vdk_c_ptolemyfir(vis_s16 *src, vis_s16 *dst, int dlen,
       }
       dst[i] = (accum>>15);
     }
-}
+ }
+
+/***************************************************************/
+void vdk_cfloat_ptolemyfir(vis_d64 *src, vis_d64 *dst, int dlen, 
+                   vis_d64 *fir, int flen)
+{
+    int i,k,nminusk,n;
+    vis_d64 accum;
+
+    for(i=0;i<dlen;i++){
+      accum = 0;
+      nminusk=i;
+      n = flen;
+      if (nminusk < flen) {
+	n = nminusk+1;
+      }
+      for (k = 0; k < n; k++ ) {
+	accum += fir[k] * src[nminusk];
+	nminusk--;
+      }
+      dst[i] = accum;
+    }
+ }
 
 /***************************************************************/
