8062266f4afcbe75e05291f84a9a6003bce6d00c
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-05 15:32:47 +0000

    woks

--- trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyiir.c
+++ trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyiir.c
@@ -12,12 +12,10 @@ void vdk_vis_ptolemyiir(vis_s16* src, vis_s16* dst, int dlen, vis_s16
   float         resulthi,resultlo,result[1];
   vis_f32	ff1,ff2;
   vis_u32	fu;
-  short		state1,*statetapprod,*outarray,*invalue;
+  short		state1,*statetapprod;
 
   allstates = vis_fzero();
   ff1=ff2 = vis_fzeros();
-  invalue = (short *) src;
-  outarray = (short *) dst;
   statetapprod = (short *) result;
   for (i = 0; i < dlen; i++) {
     /* 0 */
@@ -36,54 +34,6 @@ void vdk_vis_ptolemyiir(vis_s16* src, vis_s16* dst, int dlen, vis_s16
     allstates=vis_freg_pair(ff1,ff2);
     /* find output */
     dst[i] = (n0 * state1 >> 15) + (statetapprod[1] << 1);
-    /* 1 */
-    /* find product of states/taps */
-    /*   upper = vis_fmul8sux16(allstates,alltaps);
-    lower = vis_fmul8ulx16(allstates,alltaps);
-    splitresult = vis_fpadd16(upper,lower);
-    resulthi = vis_read_hi(splitresult);
-    resultlo = vis_read_lo(splitresult);
-    result[0] = vis_fpadd16s(resulthi,resultlo);*/
-    /* find next_state */
-    /*    ff2=ff1;
-    state1 = (invalue[4*i+1] - (statetapprod[0] << 1)) << scalefactor;
-    fu = (state1<<16 | state1 & 0xffff);
-    ff1 = vis_to_float(fu);
-    allstates=vis_freg_pair(ff1,ff2);*/
-    /* find output */
-    /* outarray[4*i+1] = (n0 * state1 >> 15) + (statetapprod[1] << 1);*/
-    /* 2 */
-    /* find product of states/taps */
-    /* upper = vis_fmul8sux16(allstates,alltaps);
-    lower = vis_fmul8ulx16(allstates,alltaps);
-    splitresult = vis_fpadd16(upper,lower);
-    resulthi = vis_read_hi(splitresult);
-    resultlo = vis_read_lo(splitresult);
-    result[0] = vis_fpadd16s(resulthi,resultlo);*/
-    /* find next_state */
-    /*ff2=ff1;
-    state1 = (invalue[4*i+2] - (statetapprod[0] << 1)) << scalefactor;
-    fu = (state1<<16 | state1 & 0xffff);
-    ff1 = vis_to_float(fu);
-    allstates=vis_freg_pair(ff1,ff2);*/
-    /* find output */
-    /*outarray[4*i+2] = (n0 * state1 >> 15) + (statetapprod[1] << 1);*/
-    /* 3 */
-    /* find product of states/taps */
-    /*upper = vis_fmul8sux16(allstates,alltaps);
-    lower = vis_fmul8ulx16(allstates,alltaps);
-    splitresult = vis_fpadd16(upper,lower);
-    resulthi = vis_read_hi(splitresult);
-    resultlo = vis_read_lo(splitresult);
-    result[0] = vis_fpadd16s(resulthi,resultlo);*/
-    /* find next_state */
-    /*ff2=ff1;
-    state1 = (invalue[4*i+3] - (statetapprod[0] << 1)) << scalefactor;
-    fu = (state1<<16 | state1 & 0xffff);
-    ff1 = vis_to_float(fu);
-    allstates=vis_freg_pair(ff1,ff2);*/
-    /* find output */
-    /*outarray[4*i+3] = (n0 * state1 >> 15) + (statetapprod[1] << 1);*/
   }  
 }	
 
