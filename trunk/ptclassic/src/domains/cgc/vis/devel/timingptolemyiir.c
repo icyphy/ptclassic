268e62d26cbee27f8cafe3c7768e52a2c7ca190c
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-05 15:02:30 +0000

    int and fp works

--- trunk/ptclassic/src/domains/cgc/vis/devel/timingptolemyiir.c
+++ trunk/ptclassic/src/domains/cgc/vis/devel/timingptolemyiir.c
@@ -13,11 +13,10 @@
 
 #define NTIME 1000
 #define LENGTH 8192
-#define NTAPS 32
 #define NPACK 4
-#define SCALE 32767
+#define SCALETAPS 32767
+#define SCALEDATA 32767/8
 /***************setup program arguments**************************/
-
 main (int argc, char *argv[])
 {
   extern  char *optarg;
@@ -28,27 +27,27 @@ main (int argc, char *argv[])
   hrtime_t start, end;
   float    time1, time2,time3;
   double sinarg,twopiover50;
-  vis_s16  *dst0,*src0,*srcptolemy,*filtertaps,*shift_taparray,*indexcount;
-  int      i,j,maxpast,tappadlength,taprowindex,tapcolindex;
-  int	   scalefactor,scaledown,numloop,taplength;
-  vis_d64  *dst1,*dst2,*src1,*filtertaps_d64;
+  vis_d64  *dst1,*src1,numtaps[2],dentaps[3],*taps1,taps2;
+  vis_d64  t0,t1,t2,t3;
+  int      i,j;
+  int	   scalefactor;
+  short    scaledown;
+  vis_s16  *dst0,*dst2,*src0,n0,*taps0;
   short    *tmp;
 
   twopiover50 = 0.0;
   ntime = NTIME;
   length = LENGTH;
-  taplength = NTAPS;
   verbose = 0;
   print = 0;
-  while ((c = getopt(argc, argv, "hvpn:l:t:")) != -1)
+  while ((c = getopt(argc, argv, "hvpn:l:")) != -1)
     switch (c) {
     case 'h':
-      printf("usage: %s [-h] [-v] [-p] [-n ntimes] [-l length] [-t ntaps]\n", argv[0]);
+      printf("usage: %s [-h] [-v] [-p] [-n ntimes] [-l length]\n", argv[0]);
       printf("  Default Setting  \n");
       printf("===================\n");
       printf(" NTIMES = 1000 \n");
       printf(" LENGTH = 8192 \n");
-      printf(" NTAPS = 16 \n");
       exit(1);
     case 'v': verbose = 1;
       break;
@@ -58,104 +57,64 @@ main (int argc, char *argv[])
       break;
     case 'l': length = atoi(optarg);
       break;
-    case 't': length = atoi(optarg);
-      break;
     };
-    scalefactor = 0;
+    scalefactor = 1;
 /**************allocate memory************************************/
-dst0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*LENGTH);
-dst1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*LENGTH);
-dst2 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*LENGTH/4);
-src0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*LENGTH);
-src1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*LENGTH);
-filtertaps = (vis_s16 *) memalign(sizeof(double),sizeof(short)*NTAPS);
-filtertaps_d64 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*NTAPS);
-srcptolemy = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
-/**************setup filtertaps***********************************/
-  for(i=0;i<LENGTH;i++){
+dst0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
+dst1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*length);
+dst2 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
+src0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
+src1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*length);
+taps0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*5);
+taps1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*5);
+/**************setup input***********************************/
+  for(i=0;i<length;i++){
     sinarg = twopiover50;
     twopiover50 += 0.628318530717958;
-    src0[i] = (short)SCALE * (double) sin(sinarg);
+    src0[i] = (short)SCALEDATA * (double) sin(sinarg);
     src1[i] = sin(sinarg);
   }
-    filtertaps[0]=(short)SCALE*5.749331917706068e-02;
-    filtertaps[1]=(short)SCALE*2.356117725608943e-01;
-    filtertaps[2]=(short)SCALE*4.326064391182815e-01;
-    filtertaps[3]=(short)SCALE*3.9298714665431733e-01;
-    filtertaps[4]=(short)SCALE*6.9707352819524643e-02;
-    filtertaps[5]=(short)SCALE*-1.9527235873539017e-01;
-    filtertaps[6]=(short)SCALE*-1.1501073848243082e-01;
-    filtertaps[7]=(short)SCALE*1.0384807075682692e-01;
-    filtertaps[8]=(short)SCALE*1.0127325405350047e-01;
-    filtertaps[9]=(short)SCALE*-6.2250082138045150e-02;
-    filtertaps[10]=(short)SCALE*-8.0637756012925166e-02;
-    filtertaps[11]=(short)SCALE*4.1872874030868444e-02;
-    filtertaps[12]=(short)SCALE*6.1733323254462361e-02;
-    filtertaps[13]=(short)SCALE*-3.0889724643843371e-02;
-    filtertaps[14]=(short)SCALE*-4.5776126368330651e-02;
-    filtertaps[15]=(short)SCALE*2.4137901399342250e-02;
-    filtertaps[16]=(short)SCALE*5.749331917706068e-02;
-    filtertaps[17]=(short)SCALE*2.356117725608943e-01;
-    filtertaps[18]=(short)SCALE*4.326064391182815e-01;
-    filtertaps[19]=(short)SCALE*3.9298714665431733e-01;
-    filtertaps[20]=(short)SCALE*6.9707352819524643e-02;
-    filtertaps[21]=(short)SCALE*-1.9527235873539017e-01;
-    filtertaps[22]=(short)SCALE*-1.1501073848243082e-01;
-    filtertaps[23]=(short)SCALE*1.0384807075682692e-01;
-    filtertaps[24]=(short)SCALE*1.0127325405350047e-01;
-    filtertaps[25]=(short)SCALE*-6.2250082138045150e-02;
-    filtertaps[26]=(short)SCALE*-8.0637756012925166e-02;
-    filtertaps[27]=(short)SCALE*4.1872874030868444e-02;
-    filtertaps[28]=(short)SCALE*6.1733323254462361e-02;
-    filtertaps[29]=(short)SCALE*-3.0889724643843371e-02;
-    filtertaps[30]=(short)SCALE*-4.5776126368330651e-02;
-    filtertaps[31]=(short)SCALE*2.4137901399342250e-02;
+  numtaps[0] = 0.067455;
+  numtaps[1] = 0.135;
+  numtaps[2] = 0.067455;
+  dentaps[0] = -1.143;
+  dentaps[1] = 0.4128;
 
-    filtertaps_d64[0]= 5.749331917706068e-02;
-    filtertaps_d64[1]= 2.356117725608943e-01;
-    filtertaps_d64[2]= 4.326064391182815e-01;
-    filtertaps_d64[3]= 3.9298714665431733e-01;
-    filtertaps_d64[4]= 6.9707352819524643e-02;
-    filtertaps_d64[5]= -1.9527235873539017e-01;
-    filtertaps_d64[6]= -1.1501073848243082e-01;
-    filtertaps_d64[7]= 1.0384807075682692e-01;
-    filtertaps_d64[8]= 1.0127325405350047e-01;
-    filtertaps_d64[9]= -6.2250082138045150e-02;
-    filtertaps_d64[10]= -8.0637756012925166e-02;
-    filtertaps_d64[11]= 4.1872874030868444e-02;
-    filtertaps_d64[12]= 6.1733323254462361e-02;
-    filtertaps_d64[13]= -3.0889724643843371e-02;
-    filtertaps_d64[14]= -4.5776126368330651e-02;
-    filtertaps_d64[15]= 2.4137901399342250e-02;
-    filtertaps_d64[16]= 5.749331917706068e-02;
-    filtertaps_d64[17]= 2.356117725608943e-01;
-    filtertaps_d64[18]= 4.326064391182815e-01;
-    filtertaps_d64[19]= 3.9298714665431733e-01;
-    filtertaps_d64[20]= 6.9707352819524643e-02;
-    filtertaps_d64[21]= -1.9527235873539017e-01;
-    filtertaps_d64[22]= -1.1501073848243082e-01;
-    filtertaps_d64[23]= 1.0384807075682692e-01;
-    filtertaps_d64[24]= 1.0127325405350047e-01;
-    filtertaps_d64[25]= -6.2250082138045150e-02;
-    filtertaps_d64[26]= -8.0637756012925166e-02;
-    filtertaps_d64[27]= 4.1872874030868444e-02;
-    filtertaps_d64[28]= 6.1733323254462361e-02;
-    filtertaps_d64[29]= -3.0889724643843371e-02;
-    filtertaps_d64[30]= -4.5776126368330651e-02;
-    filtertaps_d64[31]= 2.4137901399342250e-02;
+  scaledown = (short) 1 << scalefactor;
+  taps0[0] = (short) SCALETAPS/scaledown*dentaps[0];
+  taps0[1] = (short) SCALETAPS/scaledown*dentaps[1];
+  taps0[2] = (short) SCALETAPS/scaledown*numtaps[0];
+  taps0[3] = (short) SCALETAPS/scaledown*numtaps[1];
+  taps0[4] = (short) SCALETAPS/scaledown*numtaps[2];
+
+  taps1[0] = dentaps[0];
+  taps1[1] = dentaps[1];
+  taps1[2] = numtaps[0];
+  taps1[3] = numtaps[1];
+  taps1[4] = numtaps[2];
+ 
+  vis_write_gsr(6);
+  taps2 = vis_fzero();
+  n0 = taps0[2];
+  t0 = taps0[0];
+  t1 = taps0[3];
+  t2 = taps0[1];
+  t3 = taps0[4];
+  taps2 = vis_faligndata(t3,taps2);
+  taps2 = vis_faligndata(t2,taps2);
+  taps2 = vis_faligndata(t1,taps2);
+  taps2 = vis_faligndata(t0,taps2);
 /***************************************************************/
   if (verbose) {
     printf("NTIME=%d\n", ntime);
     printf("\n");
     printf("LENGTH=%d\n", length);
     printf("\n");
-    printf("NTAPS=%d\n", taplength);
-    printf("\n");
   }
-/****************measure integer unit fir************************/
+/****************measure integer unit iir************************/
   start = gethrtime();
   for (j = 0; j < ntime; j ++) {
-    vdk_cint_ptolemyfir(src0, dst0, length, filtertaps, taplength);
+    vdk_cint_ptolemyiir(src0, dst0, length, taps0, scalefactor);
   }
   end = gethrtime();
   time1 = (float) (end - start)/1000000/ntime;
@@ -163,10 +122,10 @@ srcptolemy = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
   if (verbose) {
     printf("    CINT-GL Time = %f msec\n", time1);
   }
-/****************measure fp unit fir************************/
+/****************measure fp unit iir************************/
   start = gethrtime();
   for (j = 0; j < ntime; j ++) {
-    vdk_cfloat_ptolemyfir(src1, dst1, length, filtertaps_d64, taplength);
+    vdk_cfloat_ptolemyiir(src1, dst1, length, taps1);
   }
   end = gethrtime();
   time2 = (float) (end - start)/1000000/ntime;
@@ -174,56 +133,18 @@ srcptolemy = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
   if (verbose) {
     printf("    CFLOAT-GL Time = %f msec\n", time2);
   }
-/***************measure vis unit fir*****************************/
-  /*adjust the src*/
-  for(i=0;i<length/4;i++){
-   srcptolemy[NPACK*i+3]=src0[i*NPACK];
-   srcptolemy[NPACK*i+2]=src0[i*NPACK+1];
-   srcptolemy[NPACK*i+1]=src0[i*NPACK+2];
-   srcptolemy[NPACK*i]=src0[i*NPACK+3];
-  }
-  /* determine padded tap buffer size */
-  if (taplength == 0){
-    maxpast = 0;
-  }
-  else if ((taplength-1)%NPACK==0){
-    maxpast = (int)(taplength-1)/NPACK+1;
-  }
-  else{
-    maxpast = (int)(taplength-1)/NPACK+2;
-  }
-  numloop = maxpast;
-  tappadlength = NPACK*maxpast;  
-  shift_taparray = (vis_s16 *)
-    memalign(sizeof(double),sizeof(short)*NPACK*tappadlength);
-  /* fill the tap buffer */
-  indexcount = shift_taparray;
-  for(taprowindex=0;taprowindex<NPACK;taprowindex++){
-    for(tapcolindex=0;tapcolindex<tappadlength;tapcolindex++){
-      *indexcount++ = 0;
-    }
-  }
-  for(taprowindex=0;taprowindex<NPACK;taprowindex++){
-    indexcount = shift_taparray + (tappadlength+1)*taprowindex;
-    for(tapcolindex=0;tapcolindex<taplength;tapcolindex++){
-      *indexcount++ = (short)(filtertaps[tapcolindex]>>scalefactor);
-    }
-  }
-  vis_write_gsr((scalefactor+1)<<3);	  
-	  
+/***************measure vis unit iir*****************************/
   start = gethrtime();
   for (j = 0; j < ntime; j ++) {
-    vdk_vis_ptolemyfir((double*)srcptolemy, dst2, length/4,
-		       (double*) shift_taparray, numloop);
+    vdk_vis_ptolemyiir(src0, dst2, length, n0, taps2, scalefactor);
   }
   end = gethrtime();
   time3 = (float) (end - start)/1000000/ntime;
 /***************************************************************/
   if (print) {
     printf("compare outputs\n");
-    tmp=(short*)dst2;
     for(i=0;i<length;i++){
-      printf("%i %i %f %i\n",i,dst0[i],dst1[i]*SCALE,tmp[i]);
+      printf("%i %i %f %i\n",i,dst0[i],dst1[i]*SCALEDATA,dst2[i]);
     }
   }
   if (verbose) {
--- trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyiir.c
+++ trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyiir.c
@@ -3,77 +3,87 @@
 #include "vis_types.h"
 #include "vis_proto.h"
 
-void vdk_vis_ptolemyfir(double* src, double* dst1, int dlen, double*
-		   shift_taparray, int numloop)
+void vdk_vis_ptolemyiir(vis_s16* src, vis_s16* dst, int dlen, vis_s16
+			n0, double alltaps, int scalefactor)
 {
-  double	*tapptr[4], tapvalue;
-  double	pairlohi, pairlolo, pairhilo,pairhihi;
-  double 	pairlo, pairhi, pair, packedOut,*packedaccum;
-  float  	packedhi, packedlo, datahi[1],datalo[1];
-  float 	tappairhi, tappairlo, splithi,splitlo;
-  int  		outerloop, innerloop,nminusk;
-  int	     	n,i,j,k;
-  double 	result[2];
-  double	accumpair[4];
-  short 	*t0,*t1,*t2;
-  double	resultcheck[2];
-  float		*f1,*r1,*r;
-  float		resultprint[2];
-  short		*s1;
-  double	d[1];
+  int 		i;
+  double 	upper,lower,splitresult;
+  double        allstates;
+  float         resulthi,resultlo,result[1];
+  vis_f32	ff1,ff2;
+  vis_u32	fu;
+  short		state1,*statetapprod,*outarray,*invalue;
 
-  for(i=0;i<dlen;i++){
-    for (j = 0; j < 4; j++) {
-      accumpair[j] = vis_fzero();
-      tapptr[j] = (double *) (shift_taparray + j * numloop);
-    }
-    nminusk = i;
-    n = numloop;
-    if(nminusk<numloop)
-      n=nminusk+1;
-    /* filter data */
-    for (outerloop = 0; outerloop < n; outerloop++) {
-      /* set up data */
-      *datahi = vis_read_hi(src[nminusk]);
-      *datalo = vis_read_lo(src[nminusk]);
-      /* calculate four outputs */
-      for (innerloop = 0; innerloop < 4; innerloop++) {
-	/*
-	 * set up tap pairs for each
-	 * shifted taparray
-	 */
-	tapvalue = *tapptr[innerloop]++;
-	tappairhi = vis_read_hi(tapvalue);
-	tappairlo = vis_read_lo(tapvalue);
-	/* take inner products */
-	pairlolo = vis_fmuld8sux16(*datalo, tappairlo);
-	pairlohi = vis_fmuld8ulx16(*datalo, tappairlo);
-	pairhilo = vis_fmuld8sux16(*datahi, tappairhi);
-	pairhihi = vis_fmuld8ulx16(*datahi, tappairhi);
-	/* accumulate results */
-	pairlo = vis_fpadd32(pairlolo, pairlohi);
-	pairhi = vis_fpadd32(pairhilo, pairhihi);
-	pair = vis_fpadd32(pairhi, pairlo);
-	accumpair[innerloop] = vis_fpadd32(accumpair[innerloop],pair);
-
-      }
-      nminusk--;
-    }
-    /*
-     * sum accumulators and pack outputs into a
-     * double
-     */
-    for (k = 3; k >=0; k--) {
-      splithi = vis_read_hi(accumpair[3-k]);
-      splitlo = vis_read_lo(accumpair[3-k]);
-      r=(float *) result;
-      r[k] = vis_fpadd32s(splithi, splitlo);
-    }
-    packedaccum = (double *) result;
-    packedhi = vis_fpackfix(*packedaccum);
-    packedaccum++;
-    packedlo = vis_fpackfix(*packedaccum);
-    dst1[i] = vis_freg_pair(packedhi, packedlo);
-  }	
-}
+  allstates = vis_fzero();
+  ff1=ff2 = vis_fzeros();
+  invalue = (short *) src;
+  outarray = (short *) dst;
+  statetapprod = (short *) result;
+  for (i = 0; i < dlen; i++) {
+    /* 0 */
+    /* find product of states-taps */
+    upper = vis_fmul8sux16(allstates,alltaps);
+    lower = vis_fmul8ulx16(allstates,alltaps);
+    splitresult = vis_fpadd16(upper,lower);
+    resulthi = vis_read_hi(splitresult);
+    resultlo = vis_read_lo(splitresult);
+    result[0] = vis_fpadd16s(resulthi,resultlo);
+    /* find next_state */
+    ff2=ff1;
+    state1 = (src[i] - (statetapprod[0] << 1)) << scalefactor;
+    fu = (state1<<16 | state1 & 0xffff);
+    ff1 = vis_to_float(fu);
+    allstates=vis_freg_pair(ff1,ff2);
+    /* find output */
+    dst[i] = (n0 * state1 >> 15) + (statetapprod[1] << 1);
+    /* 1 */
+    /* find product of states/taps */
+    /*   upper = vis_fmul8sux16(allstates,alltaps);
+    lower = vis_fmul8ulx16(allstates,alltaps);
+    splitresult = vis_fpadd16(upper,lower);
+    resulthi = vis_read_hi(splitresult);
+    resultlo = vis_read_lo(splitresult);
+    result[0] = vis_fpadd16s(resulthi,resultlo);*/
+    /* find next_state */
+    /*    ff2=ff1;
+    state1 = (invalue[4*i+1] - (statetapprod[0] << 1)) << scalefactor;
+    fu = (state1<<16 | state1 & 0xffff);
+    ff1 = vis_to_float(fu);
+    allstates=vis_freg_pair(ff1,ff2);*/
+    /* find output */
+    /* outarray[4*i+1] = (n0 * state1 >> 15) + (statetapprod[1] << 1);*/
+    /* 2 */
+    /* find product of states/taps */
+    /* upper = vis_fmul8sux16(allstates,alltaps);
+    lower = vis_fmul8ulx16(allstates,alltaps);
+    splitresult = vis_fpadd16(upper,lower);
+    resulthi = vis_read_hi(splitresult);
+    resultlo = vis_read_lo(splitresult);
+    result[0] = vis_fpadd16s(resulthi,resultlo);*/
+    /* find next_state */
+    /*ff2=ff1;
+    state1 = (invalue[4*i+2] - (statetapprod[0] << 1)) << scalefactor;
+    fu = (state1<<16 | state1 & 0xffff);
+    ff1 = vis_to_float(fu);
+    allstates=vis_freg_pair(ff1,ff2);*/
+    /* find output */
+    /*outarray[4*i+2] = (n0 * state1 >> 15) + (statetapprod[1] << 1);*/
+    /* 3 */
+    /* find product of states/taps */
+    /*upper = vis_fmul8sux16(allstates,alltaps);
+    lower = vis_fmul8ulx16(allstates,alltaps);
+    splitresult = vis_fpadd16(upper,lower);
+    resulthi = vis_read_hi(splitresult);
+    resultlo = vis_read_lo(splitresult);
+    result[0] = vis_fpadd16s(resulthi,resultlo);*/
+    /* find next_state */
+    /*ff2=ff1;
+    state1 = (invalue[4*i+3] - (statetapprod[0] << 1)) << scalefactor;
+    fu = (state1<<16 | state1 & 0xffff);
+    ff1 = vis_to_float(fu);
+    allstates=vis_freg_pair(ff1,ff2);*/
+    /* find output */
+    /*outarray[4*i+3] = (n0 * state1 >> 15) + (statetapprod[1] << 1);*/
+  }  
+}	
 
