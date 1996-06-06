6ccd0612bc3770ace05480844b77eb02c9fed6af
luisgm <luisgm@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-06 12:04:33 +0000

    added capability to delay impulse or impulse train

--- trunk/ptclassic/src/domains/cg56/stars/CG56Impulse.pl
+++ trunk/ptclassic/src/domains/cg56/stars/CG56Impulse.pl
@@ -14,7 +14,8 @@ limitation of liability, and disclaimer of warranty provisions.
         explanation {
 The star produces at its output an impulse train with height given by the 
 parameter \fIimpulseSize\fR and period given by \fIperiod\fR which is 0 for
-a single impulse.
+a single impulse.  The impulse train or pulse can be delayed by setting
+the parameter \fIdelay\fR to a positive value.
 .PP
 A state variable is maintained to keep around the next output value.
 At initialization, the state is set to \fIimpulseSize\fR.
@@ -46,34 +47,65 @@ At initialization, the state is set to \fIimpulseSize\fR.
 		attributes { A_NONCONSTANT|A_NONSETTABLE|A_YMEM}
 	}
 	state {
+		name { delay }
+		type { int }
+		desc { output will be delayed by this amount }
+		default { 0 }
+	}
+	state {
 		name { pulse }
 		type { FIX }
 		desc { internal }
 		default { 0 }
 		attributes { A_NONCONSTANT|A_NONSETTABLE|A_YMEM }
 	}
+	protected {
+		int time;
+	}
  	setup {
 	           pulse=impulseSize;
 		   counter = 0;
+		   time = 0;
 		   if ( int(period) < 0 ){
 			Error::abortRun( *this, 
 				"Period must be non-negative.");
 		   };
-			
+		   if ( int(delay) < 0 ){
+		   	Error::abortRun( *this,
+		   		"Delay must be non-negative.");
+		   };
+		   if ( int(period) == 0 ){
+		   	counter = - int(delay);
+		   } else {
+		   	counter = -(int(delay)%int(period));
+		   };	
 	      }
 
 	codeblock(multiple) {
 	move 	$ref(counter),a
-	move	#1,r0
 	clr	b	$ref(period),x0
-	cmp 	x0,a	r0,x0
+	cmp 	x0,a	#>(0.0000001192093),x1
 	tge	b,a
 	tst	a	$ref(pulse),y0
 	teq	y0,b	
-	add	x0,a	b,$ref(output)
+	add	x1,a	b,$ref(output)
 	move	a,$ref(counter)
 	} 
 	
+	codeblock(delayedSingle){        
+	clr	b	$ref(period),x1
+	move	b,x0	$ref(counter),a
+	cmp 	x1,a	#>(0.0000001192093),y1
+	tge	b,a
+	tst	a	$ref(pulse),y0
+	teq	y0,b
+	move	b,$ref(output)
+	move	x0,b
+	tne	y0,b	
+	add	y1,a	b,$ref(pulse)
+	move	a,$ref(counter)
+	}	
+		
 	codeblock(single){
 	clr	a	$ref(pulse),b
 	move 	b,$ref(output)
@@ -82,14 +114,23 @@ At initialization, the state is set to \fIimpulseSize\fR.
 
         go { 
 		if (int(period) > 0) {
-			addCode(multiple); 
+			addCode(multiple);
+			time = 8; 
+		} else if ( int(delay) ){ 
+			addCode(delayedSingle);
+			time = 11;
 		} else {
 			addCode(single);
+			time = 3;
 		}
 	}
 
 	execTime { 
-		return 9;
+		return (time);
 	}
  }
+
+
+
+
 
