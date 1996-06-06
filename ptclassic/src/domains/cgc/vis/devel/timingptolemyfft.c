af2aabb8ab58343f8cd69da2e5d3facf48b51166
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-06 22:40:12 +0000

    works

--- ptclassic/src/domains/cgc/vis/devel/timingptolemyfft.c
+++ ptclassic/src/domains/cgc/vis/devel/timingptolemyfft.c
@@ -13,10 +13,9 @@
 #include "vis_proto.h"
 
 #define NTIME 1000
-#define LENGTH 8192
+#define LENGTH 256
 #define NPACK 4
-#define SCALETAPS 32767
-#define SCALEDATA 32767/8
+#define SCALE 32767/64
 /***************setup program arguments**************************/
 main (int argc, char *argv[])
 {
@@ -26,18 +25,12 @@ main (int argc, char *argv[])
   int      ntime, length;
   int      verbose,print;
   hrtime_t start, end;
-  float    time1, time2,time3;
+  float    time1, time2;
   double sinarg,twopiover50;
-  vis_d64  *dst1,*dst2,*src1,numtaps[2],dentaps[3],*taps1,taps2;
-  vis_d64  t0,t1,t2,t3,quadn0;
-  vis_u32  dbln0;
+  vis_d64  *src0re,*src0im;
   int      i,j;
-  int	   scalefactor;
-  short    scaledown;
-  vis_s16  *dst0,*src0,n0,*taps0;
-  short    *tmp;
+  vis_s16  *src1re,*src1im;
 
-  twopiover50 = 0.0;
   ntime = NTIME;
   length = LENGTH;
   verbose = 0;
@@ -45,11 +38,11 @@ main (int argc, char *argv[])
   while ((c = getopt(argc, argv, "hvpn:l:")) != -1)
     switch (c) {
     case 'h':
-      printf("usage: %s [-h] [-v] [-p] [-n ntimes] [-l length]\n", argv[0]);
+      printf("usage: %s [-h] [-v] [-p] [-n ntimes]\n", argv[0]);
       printf("  Default Setting  \n");
       printf("===================\n");
       printf(" NTIMES = 1000 \n");
-      printf(" LENGTH = 8192 \n");
+      printf(" LENGTH = 256 \n");
       exit(1);
     case 'v': verbose = 1;
       break;
@@ -57,59 +50,28 @@ main (int argc, char *argv[])
       break;
     case 'n': ntime = atoi(optarg);
       break;
-    case 'l': length = atoi(optarg);
-      break;
     };
-    scalefactor = 1;
 /**************allocate memory************************************/
-dst0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
-dst1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*length);
-dst2 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*length/4);
-src0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
-src1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*length);
-taps0 = (vis_s16 *) memalign(sizeof(double),sizeof(short)*5);
-taps1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*5);
+src0re = (vis_d64 *) memalign(sizeof(double),sizeof(double)*length);
+src0im = (vis_d64 *) memalign(sizeof(double),sizeof(double)*length);
+src1re = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
+src1im = (vis_s16 *) memalign(sizeof(double),sizeof(short)*length);
 /**************setup input***********************************/
+  twopiover50 = 0.0;
   for(i=0;i<length;i++){
     sinarg = twopiover50;
     twopiover50 += 0.628318530717958;
-    src0[i] = (short)SCALEDATA * (double) sin(sinarg);
-    src1[i] = sin(sinarg);
+    src0re[i] = sin(sinarg);
+    src1re[i] = (short)SCALE * (double) sin(sinarg);
   }
-  numtaps[0] = 0.067455;
-  numtaps[1] = 0.135;
-  numtaps[2] = 0.067455;
-  dentaps[0] = -1.143;
-  dentaps[1] = 0.4128;
-
-  scaledown = (short) 1 << scalefactor;
-  taps0[0] = (short) SCALETAPS/scaledown*dentaps[0];
-  taps0[1] = (short) SCALETAPS/scaledown*dentaps[1];
-  taps0[2] = (short) SCALETAPS/scaledown*numtaps[0];
-  taps0[3] = (short) SCALETAPS/scaledown*numtaps[1];
-  taps0[4] = (short) SCALETAPS/scaledown*numtaps[2];
-
-  taps1[0] = dentaps[0];
-  taps1[1] = dentaps[1];
-  taps1[2] = numtaps[0];
-  taps1[3] = numtaps[1];
-  taps1[4] = numtaps[2];
- 
-  vis_alignaddr(0,6);
-  tmp = (short *) taps0;
-  taps2 = vis_fzero();
-  n0 = taps0[2];
-  t0 = vis_ld_u16(tmp);
-  t1 = vis_ld_u16(tmp+3);
-  t2 = vis_ld_u16(tmp+1);
-  t3 = vis_ld_u16(tmp+4);
-  taps2 = vis_faligndata(t3,taps2);
-  taps2 = vis_faligndata(t2,taps2);
-  taps2 = vis_faligndata(t1,taps2);
-  taps2 = vis_faligndata(t0,taps2);
-
-  dbln0 = (n0<<16)|(n0&0xffff);
-  quadn0 = vis_to_double_dup(dbln0);
+  twopiover50 = 0.0;
+  for(i=0;i<length;i++){
+    sinarg = twopiover50;
+    twopiover50 += 0.628318530717958;
+    src0im[i] = cos(sinarg);
+    src1im[i] = (short)SCALE * (double) cos(sinarg);
+  }
+  vis_write_gsr(8);
 /***************************************************************/
   if (verbose) {
     printf("NTIME=%d\n", ntime);
@@ -117,53 +79,49 @@ taps1 = (vis_d64 *) memalign(sizeof(double),sizeof(double)*5);
     printf("LENGTH=%d\n", length);
     printf("\n");
   }
-/****************measure integer unit iir************************/
+/****************measure fp unit fft************************/
   start = gethrtime();
   for (j = 0; j < ntime; j ++) {
-    vdk_cint_ptolemyiir(src0, dst0, length, taps0, scalefactor);
+    vdk_cfloat_ptolemyfft(src0re, src0im, length);
   }
   end = gethrtime();
   time1 = (float) (end - start)/1000000/ntime;
 
+  reorderfloatfft(src0re, src0im, length);
+
   if (verbose) {
-    printf("    CINT-GL Time = %f msec\n", time1);
+    printf("    CFLOAT-GL Time = %f msec\n", time1);
   }
-/****************measure fp unit iir************************/
+/***************measure vis unit fft*****************************/
   start = gethrtime();
   for (j = 0; j < ntime; j ++) {
-    vdk_cfloat_ptolemyiir(src1, dst1, length, taps1);
+    vdk_vis_ptolemyfft((double*)src1re, (double*)src1im, length);
   }
   end = gethrtime();
   time2 = (float) (end - start)/1000000/ntime;
 
+  reordershortfft(src1re,src1im,length);
+
   if (verbose) {
-    printf("    CFLOAT-GL Time = %f msec\n", time2);
+    printf("  VIS-GL Time = %f msec\n", time2);
   }
-/***************measure vis unit iir*****************************/
-  vis_alignaddr(0,6);
-  start = gethrtime();
-  for (j = 0; j < ntime; j ++) {
-    vdk_vis_ptolemyiir((double*)src0, dst2, length/4, quadn0, taps2,
-		       scalefactor);
-  }
-  end = gethrtime();
-  time3 = (float) (end - start)/1000000/ntime;
-  printf("  VIS-GL Time = %f msec\n", time3);
 /***************************************************************/
   if (print) {
     printf("compare outputs\n");
-    tmp=(short*)dst2;
+    printf("real\n");
     for(i=0;i<length;i++){
-      printf("%i %i %f %i\n",i,dst0[i],dst1[i]*SCALEDATA,tmp[i]<<1);
+      printf("%i %f %i\n",i,src0re[i]*SCALE,src1re[i]);
+    }
+    printf("imag\n");
+    for(i=0;i<length;i++){
+      printf("%i %f %i\n",i,src0im[i]*SCALE,src1im[i]);
     }
   }
   if (verbose) {
-    printf("  CINT-GL/VIS-GL = %f\n", time1/time3);
-    printf("  CFLOAT-GL/VIS-GL = %f\n", time2/time3);
+    printf("  CFLOAT-GL/VIS-GL = %f\n", time1/time2);
     printf("\n");
   } else {
-    printf("%d\t%f\t%f\t%f\t%f\t%f\n", length, time1, time2, time3,
-	   time1/time3, time2/time3);
+    printf("%d\t%f\t%f\t%f\n", length, time1, time2, time1/time2);
   }
 
   exit(0); 
