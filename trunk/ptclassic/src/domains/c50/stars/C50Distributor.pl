546670a05127f75c8777f12d33595d54372a735e
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-08 00:28:13 +0000

    Updated description to match the SDF version and removed an unnecessary internal state outputNum

--- trunk/ptclassic/src/domains/c50/stars/C50Distributor.pl
+++ trunk/ptclassic/src/domains/c50/stars/C50Distributor.pl
@@ -1,9 +1,12 @@
 defstar {
 	name { Distributor }
 	domain { C50 }
-	desc { 
-One input, two output distributor. 
- }
+	desc {
+Takes one input stream and synchronously splits it into N output streams,
+where N is the number of outputs.  It consumes N*B input particles,
+where B = blockSize, and sends the first B particles to the first output,
+the next B particles to the next output, etc.
+	}
 	version { $Id$ }
 	author { A. Baensch, ported from Gabriel }
 	copyright {
@@ -25,53 +28,43 @@ Distributes an input signal among two outputs, alternating samples.
 		name {output}
 		type {=input}
 	}
-        state {
-                name {blockSize}
-                type {int}
-                default {1}
-                desc {Number of particles in a block.}
+	state {
+		name {blockSize}
+		type {int}
+		default {1}
+		desc {Number of particles in a block.}
 	}
-        state  {
-                name { outputNum }
-                type { int }
-                default { 0 }
-                desc { input#() }
-                attributes { A_NONCONSTANT|A_NONSETTABLE }
-        }
-
 	setup {
-                int n = output.numberPorts();
+		int n = output.numberPorts();
 		int bs = int(blockSize);
-                input.setSDFParams(n*bs,n*bs-1);
+		input.setSDFParams(n*bs,n*bs-1);
 		output.setSDFParams(bs,bs-1);
-        }
-
-        codeblock (one) {
-        splk    #$addr(input),BMAR        	;just move data from in to out
-        bldd    BMAR,#$addr(output#1)		;
-        }
+	}
+	codeblock (one) {
+	splk    #$addr(input),BMAR		;just move data from in to out
+	bldd    BMAR,#$addr(output#1)		;
+	}
 
  	codeblock(main) {
-        lar    	AR0,#$addr(input)		;Address input		=> AR0
-        mar	*,AR0				;
-        }
-        codeblock(loop) {
-        bldd    *,#$addr(output#outputNum)	;output port(n) = input(i)
+	lar    	AR0,#$addr(input)		;Address input		=> AR0
+	mar	*,AR0				;
+	}
+	codeblock(loop, "int i") {
+	bldd    *,#$addr(output#@i)	;output port(n) = input(i)
 	}
 
-        go {
-                if (output.numberPorts() == 1) {
-                        addCode(one);
-                        return;
-                }
-                addCode(main);
-                for (int i = 1; i <= output.numberPorts(); i++) {
-                        outputNum=i;
-                        addCode(loop);
-                }
-        }
-       exectime {
-                return (2*(int(output.numberPorts()))+2);
-        }
+	go {
+		if (output.numberPorts() == 1) {
+			addCode(one);
+		}
+		else {
+			addCode(main);
+			for (int i = 1; i <= output.numberPorts(); i++) {
+				addCode(loop(i));
+			}
+		}
+	}
+	exectime {
+		return (2*(int(output.numberPorts()))+2);
+	}
 }
-
