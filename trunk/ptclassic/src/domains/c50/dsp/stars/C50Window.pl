7661b52049d5f6a27c8d4bbb66245f02d10c113e
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-08 21:10:37 +0000

    Greatly simplified the implementation by reusing code in the PTDSPWindow module

--- trunk/ptclassic/src/domains/c50/dsp/stars/C50Window.pl
+++ trunk/ptclassic/src/domains/c50/dsp/stars/C50Window.pl
@@ -60,139 +60,51 @@ the window.
 		}
 	}
 	protected {
-		int realLen;
-		int realPeriod;
-		int winType;
-		double alpha;
-		double beta;
-		double freq1;
-		double freq2;
-		double norm;
-		double scale0;
-		double scale1;
-		double scale2;
-
 		// must be persistent data or there will be an error
 		// in value.setInitValue(window) because window will
 		// be destroyed if it were a local variable in setup
 		StringList window;
 	}
-	ccinclude { <string.h>, <math.h>, "cephes.h" }
-	code {
-#define SDFWinType_Null		(0)
-#define SDFWinType_Rectangle	(1)
-#define SDFWinType_Bartlett	(2)
-#define SDFWinType_Hanning	(3)
-#define SDFWinType_Hamming	(4)
-#define SDFWinType_Blackman	(5)
-#define SDFWinType_SteepBlackman	(6)
-#define SDFWinType_Kaiser       (7)
-	}
+
 	constructor {
 		value.setAttributes(A_NONSETTABLE|A_CONSTANT);
 	}
+
+	ccinclude { <string.h>, <math.h>, "cephes.h", "PTDSPWindow.h" }
+
 	setup {
-		const char *wn = name;
-
-		if ( strcasecmp( wn, "Rectangle")==0 ) {
-		    winType = SDFWinType_Rectangle;
-		} else if ( strcasecmp( wn, "Bartlett")==0 ) {
-		    winType = SDFWinType_Bartlett;
-		} else if ( strcasecmp( wn, "Hanning")==0 ) {
-		    winType = SDFWinType_Hanning;
-		} else if ( strcasecmp( wn, "Hamming")==0 ) {
-		    winType = SDFWinType_Hamming;
-		} else if ( strcasecmp( wn, "Blackman")==0 ) {
-		    winType = SDFWinType_Blackman;
-		} else if ( strcasecmp( wn, "SteepBlackman")==0 ) {
-		    winType = SDFWinType_SteepBlackman;
-		} else {
-		    Error::abortRun(*this, ": Unknown window name");
+		const char* wn = name;
+		int winType = PTDSPWindowNumber(wn);
+		if (winType == DSP_WINDOW_TYPE_NULL) {
+		    Error::abortRun(*this, "Unknown window name ", wn);
 		    return;
 		}
-		realLen = int(length);
+
+		// Don't want to risk divide by zero
+		int realLen = int(length);
 		if ( realLen < 4 ) {
-		    /* Dont want to risk divide by zero */
-		    Error::abortRun(*this, ": Window length too small");
+		    Error::abortRun(*this, "Window length is too small ",
+				    "(should be greater than 3)");
 		    return;
 		}
 
-		beta = double(WindowParameters[0]);
-
-		double base_w = M_PI/(realLen-1);
-		double sin_base_w = sin(base_w);
-		double sin_2base_w = sin(2*base_w);
-		double d = - (sin_base_w/sin_2base_w) * (sin_base_w/sin_2base_w);
-		scale0 = 0; scale1 = 0; freq1 = 0; scale2 = 0; freq2 = 0;
-		/* 
-		 *  Window defs taken from Jackson, Digital Filters and Signal
-		 *  Processing, Second Ed, chap 7.
-		 */
-		switch ( winType ) {
-		  case SDFWinType_Rectangle:
-		    scale0 = 1;
-		    break;
-		  case SDFWinType_Bartlett:
-		    scale0 = 0;		scale1 = 2.0/realLen;
-		    break;
-		  case SDFWinType_Hanning:
-		    scale0 = .5;	scale1 = -.5;	freq1 = 2*base_w;
-		    break;
-		  case SDFWinType_Hamming:
-		    scale0 = .54;	scale1 = -.46;	freq1 = 2*base_w;
-		    break;
-		  case SDFWinType_Blackman:
-		    /* This is a special case of SteepBlackman */
-		    d = -.16;		/* scale0 = .42, scale1 = -.5, scale2=.08 */
-		    /*FALLTHROUGH*/
-		  case SDFWinType_SteepBlackman:
-		    /* See Jackson 2ed, eqns 7.3.6 through 7.3.10 */
-		    scale0 = (d+1)/2;	scale1 = -.5;	freq1 = 2*base_w;
-		    			scale2 = -d/2;	freq2 = 4*base_w;
-		    break;
-		  case SDFWinType_Kaiser:
-		    alpha = double((realLen-1.0)/2.0);
-		    norm = fabs(i0(beta));
-		    break;
-		  default:
-		    Error::abortRun(*this, ": Invalid window type");
+		double* windowTaps = new double[realLen];
+		int validWindow = PTDSPWindow(windowTaps, realLen, winType,
+					      (double *) WindowParameters);
+		if (! validWindow) {
+		    delete [] windowTaps;
+		    Error::abortRun(*this, "Could not compute the taps for a ",
+				    wn, " window: PTDSPWindow failed.");
 		    return;
 		}
-		    
+
 		value.resize(int(length));
-		window.initialize();	// initialize StringList to null string
-		int i;
-
-		switch ( winType ) {
-		  case SDFWinType_Bartlett:
-		    for (i = 0; i < realLen/2; i++) {
-			double val = scale1 * (i+1);
-			window << val << " ";
-		    }
-		    for ( ; i < realLen; i++) {
-			double val = scale1 * (realLen-(i+1));
-			output%(realPeriod-(i+1)) << val;
-			window << val;
-			if (i != realLen-1) window = " ";
-		    }
-		    break;
-		  case SDFWinType_Kaiser:
-		    for (i = 0; i < realLen; i++){
-			double squared = double(pow(((i - alpha)/alpha), 2));
-			double entry = beta*sqrt(1-squared);
-			double val = i0(entry)/norm;
-			window << val;
-			if (i != realLen-1) window << " ";
-		    }
-		    break;
-		  default:
-		    for (i = 0; i < realLen; i++) {
-			if (i != 0) window << " ";
-			double val = scale0 + scale1 * cos(freq1*i) +
-				     scale2 * cos(freq2*i);
-			window << val;
-		    }
+		window.initialize();		// initialize to a null string
+		for (int i = 0; i < realLen; i++) {
+			window << windowTaps[i] << " ";
 		}
+		delete [] windowTaps;
+
 		value.setInitValue(window);
 		value.initialize();
 		C50WaveForm::setup();
