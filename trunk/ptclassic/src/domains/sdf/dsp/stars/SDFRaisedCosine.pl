cd1b75fa084613d83a13d82776a7c7f8623e276a
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-09 00:06:14 +0000

    Stripped out rcos and sqrtrcos and put them in a reusable library

--- trunk/ptclassic/src/domains/sdf/dsp/stars/SDFRaisedCosine.pl
+++ trunk/ptclassic/src/domains/sdf/dsp/stars/SDFRaisedCosine.pl
@@ -89,44 +89,7 @@ E. A. Lee and D. G. Messerchmitt,
 I. Korn, \fIDigital Communications\fR, Van Nostrand Reinhold, New York, 1985.
 	}
 	seealso {FIR, Window, pulses}
-	code {
-		// The code is written with integer arguments so that it
-		// can reliably detect the 0/0 condition, avoiding
-		// roundoff problems.
-		static double rcos (int t, int T, double excess) {
-		    const double DELTA = 1.0e-7;
-		    if (t == 0) return 1.0;
-		    double x = (double)t/(double)T;
-		    double s = sin (M_PI * x) / (M_PI * x);
-		    x *= excess;
-		    double den = 1.0 - 4 * x * x;
-		    if (den > -DELTA && den < DELTA) return s * M_PI/4.0;
-		    return s * cos (M_PI * x) / den;
-		}
-		static double sqrtrcos (int t, int T, double excess) {
-		    double sqrtT = sqrt(double(T));
-		    if (t == 0) {
-			return ((4*excess/M_PI) + 1 - excess)/sqrtT;
-		    }
-		    double x = double(t)/double(T);
-		    if (excess == 0.0) {
-			return sqrtT*sin( M_PI * x)/(M_PI * t);
-		    }
-		    double oneplus = (1.0+excess)*M_PI/T;
-		    double oneminus = (1.0-excess)*M_PI/T;
-		    // Check to see whether we will get divide by zero
-		    double den = t*t*16*excess*excess - T*T;
-		    if (den == 0) {
-		        return (T*sqrtT/(8 * excess * M_PI * t))
-			    * ( oneplus * sin( oneplus * t)
-			    - (oneminus*T/(4*excess*t)) * cos( oneminus * t )
-			    + (T/(4*excess*t*t)) * sin ( oneminus * t) );
-		    }
-		    return (4*excess/(M_PI*sqrtT))
-			* (cos(oneplus*t) + sin(oneminus*t)/(x*4*excess))
-			/ (1.0 - 16*excess*excess*x*x);
-		}
-	}
+	ccinclude { "PTDSPRaisedCosine.h" }
 	defstate {
 		name { length }
 		type { int }
@@ -158,18 +121,20 @@ I. Korn, \fIDigital Communications\fR, Van Nostrand Reinhold, New York, 1985.
 		interpolation.setInitValue("16");
 	}
 	setup {
-		if(double(excessBW) < 0.0)
+		if (double(excessBW) < 0.0)
 		    Error::abortRun(*this, "Invalid excess bandwidth");
-		if(int(symbol_interval) <= 0)
+		if (int(symbol_interval) <= 0)
 		    Error::abortRun(*this, "Invalid symbol interval");
 		taps.resize (length);
 		int center = int(length)/2;
-		for (int i = 0; i < int(length); i++)
-		    if(int(square_root))
-			taps[i] = sqrtrcos (i - center, symbol_interval, excessBW);
+		for (int i = 0; i < int(length); i++) {
+		    if (int(square_root))
+			taps[i] = PTDSPSqrtRaisedCosine(i - center,
+					int(symbol_interval), int(excessBW));
 		    else
-			taps[i] = rcos (i - center, symbol_interval, excessBW);
+			taps[i] = PTDSPRaisedCosine(i - center,
+					int(symbol_interval), int(excessBW));
+		}
 		SDFFIR :: setup();
 	}
 }
-
