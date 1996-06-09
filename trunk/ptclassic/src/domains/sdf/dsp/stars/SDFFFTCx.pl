4134362d5d48746a262df2aeb67b6b3415788b01
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-09 00:05:34 +0000

    Stripped out C code for the FFT and put it in a reusable library

--- trunk/ptclassic/src/domains/sdf/dsp/stars/SDFFFTCx.pl
+++ trunk/ptclassic/src/domains/sdf/dsp/stars/SDFFFTCx.pl
@@ -78,85 +78,13 @@ Prentice-Hall: Englewood Cliffs, NJ, 1989.
 		double* data;
 		int fftSize;
 	}
-// Code for the FFT function
-	code {
-
-/*
- * This fft routine is from ~gabriel/src/filters/fft/fft.c;
- * I am unsure of the original source.  The file contains no
- * copyright notice or description.
- * The declaration is changed to the prototype form but the
- * function body is unchanged.  (J. T. Buck)
- */
-
-#define SWAP(a, b) tempr=(a); (a)=(b); (b)=tempr
-
-/*
- * Replace data by its discrete Fourier transform, if isign is
- * input as 1, or by its inverse discrete Fourier transform, if 
- * "isign" is input as -1.  "data'"is a complex array of length "nn",
- * input as a real array data[0..2*nn-1]. "nn" MUST be an integer
- * power of 2 (this is not checked for!?)
- */
-
-static void fft_rif(double *data, int nn, int isign)
-{
-	int	n;
-	int	mmax;
-	int	m, j, istep, i;
-	double	wtemp, wr, wpr, wpi, wi, theta;
-	double	tempr, tempi;
-
-	data--;
-	n = nn << 1;
-	j = 1;
-
-	for (i = 1; i < n; i += 2) {
-		if(j > i) {
-			SWAP(data[j], data[i]);
-			SWAP(data[j+1], data[i+1]);
-		}
-		m= n >> 1;
-		while (m >= 2 && j >m) {
-			j -= m;
-			m >>= 1;
-		}
-		j += m;
-	}
-	mmax = 2;
-	while (n > mmax) {
-		istep = 2*mmax;
-		theta = -6.28318530717959/(isign*mmax);
-		wtemp = sin(0.5*theta);
-		wpr = -2.0*wtemp*wtemp;
-		wpi = sin(theta);
-		wr = 1.0;
-		wi = 0.0;
-		for (m = 1; m < mmax; m += 2) {
-			for (i = m; i < n; i += istep) {
-				j = i + mmax;
-				tempr = wr*data[j] - wi*data[j+1];
-				tempi = wr*data[j+1] + wi*data[j];
-				data[j] = data[i] - tempr;
-				data[j+1] = data[i+1] - tempi;
-				data[i] += tempr;
-				data[i+1] += tempi;
-			}
-			wr = (wtemp=wr)*wpr - wi*wpi+wr;
-			wi = wi*wpr + wtemp*wpi + wi;
-		}
-		mmax = istep;
-	}
-}
-
-	}	// end of code block for inclusion in .cc file
-
 	constructor {
 		data = 0;
 	}
 	destructor {
 		LOG_DEL; delete [] data;
 	}
+	ccinclude { "PTDSPfft_rif.h" }
 	setup {
 		fftSize = 1 << int(order);
 		if (fftSize < int(size)) {
@@ -165,8 +93,8 @@ static void fft_rif(double *data, int nn, int isign)
 		}
 		LOG_DEL; delete [] data;
 		LOG_NEW; data = new double[2*fftSize];
-		input.setSDFParams (int(size), int(size)-1);
-		output.setSDFParams (fftSize, fftSize-1);
+		input.setSDFParams(int(size), int(size)-1);
+		output.setSDFParams(fftSize, fftSize-1);
 	}
 	go {
 		// load up the array
@@ -182,7 +110,7 @@ static void fft_rif(double *data, int nn, int isign)
 			*p++ = 0.0;
 			*p++ = 0.0;
 		}
-		fft_rif (data, fftSize, int(direction));
+		PTDSPfft_rif(data, fftSize, int(direction));
 		// generate output data.  If inverse, we scale the result.
 		if (int(direction) != 1)
 			for (i = 0; i < 2*fftSize; i++)
