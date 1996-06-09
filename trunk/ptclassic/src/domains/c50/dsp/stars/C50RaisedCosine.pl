4a48df457390bad2190296243ed48e97617da116
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-09 00:32:24 +0000

    Synchronized wit the SDF version: removed the locally defined rcos and sqrtrcos functions and instead call functions in the PTDSP module to compute the same values

--- trunk/ptclassic/src/domains/c50/dsp/stars/C50RaisedCosine.pl
+++ trunk/ptclassic/src/domains/c50/dsp/stars/C50RaisedCosine.pl
@@ -20,45 +20,7 @@ limitation of liability, and disclaimer of warranty provisions.
 See the SDFRaisedCos star.
 	}
 	seealso { FIR, Window }
-	// <math.h> is already included to define M_PI and the sin function
-	code {
-		// The code is written with integer arguments so that it
-		// can reliably detect the 0/0 condition, avoiding
-		// roundoff problems.
-		static double rcos (int t, int T, double excess) {
-		    const double DELTA = 1.0e-7;
-		    if (t == 0) return 1.0;
-		    double x = (double)t/(double)T;
-		    double s = sin (M_PI * x) / (M_PI * x);
-		    x *= excess;
-		    double den = 1.0 - 4 * x * x;
-		    if (den > -DELTA && den < DELTA) return s * M_PI/4.0;
-		    return s * cos (M_PI * x) / den;
-		}
-		static double sqrtrcos (int t, int T, double excess) {
-		    double sqrtT = sqrt(double(T));
-		    if (t == 0) {
-			return ((4*excess/M_PI) + 1 - excess)/sqrtT;
-		    }
-		    double x = double(t)/double(T);
-		    if (excess == 0.0) {
-			return sqrtT*sin( M_PI * x)/(M_PI * t);
-		    }
-		    double oneplus = (1.0+excess)*M_PI/T;
-		    double oneminus = (1.0-excess)*M_PI/T;
-		    // Check to see whether we will get divide by zero
-		    double den = t*t*16*excess*excess - T*T;
-		    if (den == 0) {
-		        return (T*sqrtT/(8 * excess * M_PI * t))
-			    * ( oneplus * sin( oneplus * t)
-			    - (oneminus*T/(4*excess*t)) * cos( oneminus * t )
-			    + (T/(4*excess*t*t)) * sin ( oneminus * t) );
-		    }
-		    return (4*excess/(M_PI*sqrtT))
-			* (cos(oneplus*t) + sin(oneminus*t)/(x*4*excess))
-			/ (1.0 - 16*excess*excess*x*x);
-		}
-	}
+	ccinclude { "PTDSPRaisedCosine.h" }
 	defstate {
 		name { length }
 		type { int }
@@ -94,13 +56,16 @@ See the SDFRaisedCos star.
 		    Error::abortRun(*this, "Invalid excess bandwidth");
 		if (int(symbol_interval) <= 0)
 		    Error::abortRun(*this, "Invalid symbol interval");
-		taps.resize(length);
+		taps.resize (length);
 		int center = int(length)/2;
-		for (int i = 0; i < int(length); i++)
+		for (int i = 0; i < int(length); i++) {
 		    if (int(square_root))
-			taps[i] = sqrtrcos(i - center, symbol_interval, excessBW);
+			taps[i] = PTDSPSqrtRaisedCosine(i - center,
+					int(symbol_interval), int(excessBW));
 		    else
-			taps[i] = rcos (i - center, symbol_interval, excessBW);
+			taps[i] = PTDSPRaisedCosine(i - center,
+					int(symbol_interval), int(excessBW));
+		}
 		C50FIR :: setup();
 	}
 }
