323a634c3c763136a8a27217cd82673549de5205
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-11 17:14:47 +0000

    retimeby2 works

--- trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyiir.c
+++ trunk/ptclassic/src/domains/cgc/vis/devel/vis_ptolemyiir.c
@@ -92,4 +92,56 @@ void vdk_vis_ptolemyiir(vis_d64* src, vis_d64* dst, int dlen, vis_d64
     *dst++ = vis_fpadd16(accum,quadresult);
   }
 }
+/***************************************************************/
+double mult4x4(double mult1, double mult2)
+{
+  double          upper,lower,prod;
+
+  upper = vis_fmul8sux16(mult1,mult2);
+  lower = vis_fmul8ulx16(mult1,mult2);
+  return prod = vis_fpadd16(upper,lower);
+}
+/***************************************************************/
+void vdk_visretimeby2_ptolemyiir(float *src, float *dst3, int length,
+			      double alphatop, double alphabottom,
+			      double beta, int scalefactor)
+{
+  double quadsrc,repeatstate,accumpair0,accumpair1,accumpair2;
+  float t0,t1,t2,accum0,accum1;
+  vis_s32 result0,result1;
+  vis_f32 currentstate;
+  vis_u32 fu;
+  int i;
+  short out0,out1;
+
+  currentstate = vis_fzeros();
+  for(i=0;i<length;i++){
+    quadsrc = vis_freg_pair(*src++,*src);
+    repeatstate = vis_freg_pair(currentstate,currentstate);
+    accumpair0 = mult4x4(beta,repeatstate);
+    accumpair1 = mult4x4(alphatop,quadsrc);
+    accumpair2 = mult4x4(alphabottom,quadsrc);
+    
+    t0 = vis_read_hi(accumpair0);
+    t1 = vis_read_hi(accumpair1);
+    accum0 = vis_fpadd16s(t0,t1);
+    t2 = vis_read_lo(accumpair1);
+    accum0 = vis_fpadd16s(accum0,t2);
+    result0 = *((vis_s32*) &accum0);
+    out0 = (result0+(result0>>16))<<(scalefactor+1);
+
+    t0 = vis_read_lo(accumpair0);
+    t1 = vis_read_hi(accumpair2);
+    accum1 = vis_fpadd16s(t0,t1);
+    t2 = vis_read_lo(accumpair2);
+    accum1 = vis_fpadd16s(accum1,t2);
+    result1 = *((vis_s32*) &accum1);
+    out1 = (result1+(result1>>16))<<(scalefactor+1);
+
+    fu = out0 << 16 | out1 & 0xffff;
+    dst3[i] = currentstate = vis_to_float(fu);
+  }
+}
+/***************************************************************/
+
 
