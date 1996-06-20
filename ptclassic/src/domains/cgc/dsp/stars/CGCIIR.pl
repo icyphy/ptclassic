1bb3a2c901906efcb5e3ebb8815c6216d2eaa892
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-20 21:17:37 +0000

    Split iir codeblock into feedThrough and iir codeblocks to handle the special case of having only one numerator or one denominator coefficient

--- ptclassic/src/domains/cgc/dsp/stars/CGCIIR.pl
+++ ptclassic/src/domains/cgc/dsp/stars/CGCIIR.pl
@@ -3,20 +3,18 @@ defstar {
     domain {CGC}
     desc {
 An infinite impulse response (IIR) filter implemented in a direct form II
-realization.
-The transfer function is of the form H(z) = G*N(1/z)/D(1/z),
-where N() and D() are polynomials.
-The parameter "gain" specifies G, and the floating-point arrays
-"numerator" and "denominator" specify N() and D(), respectively.
-Both arrays start with the constant terms of the polynomial
-and decrease in powers of z (increase in powers of 1/z).
-Note that the constant term of D is not omitted, as is common in
-other programs that assume that it has been normalized to unity.
+realization.  The transfer function is of the form H(z) = G*N(1/z)/D(1/z),
+where N() and D() are polynomials.  The parameter "gain" specifies G, and
+the floating-point arrays "numerator" and "denominator" specify N() and D(),
+respectively.  Both arrays start with the constant terms of the polynomial
+and decrease in powers of z (increase in powers of 1/z).  Note that the
+constant term of D is not omitted, as is common in other programs that
+assume that it has been normalized to unity.
     }
     version { $Id$ }
     author { Yu Kee Lim }
     copyright {
-Copyright (c) 1990-1995 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
@@ -53,43 +51,39 @@ Prentice-Hall: Englewood Cliffs, NJ, 1989.
 	name {gain}
 	type {float}
 	default { "1" }
-	desc { Gain. }
+	desc { Filter gain }
     }
     defstate {
 	name {numerator}
 	type {floatarray}
-	default {
-		".5 .25 .1"
-	}
-	desc { Numerator coefficients. }
+	default { ".5 .25 .1" }
+	desc { Numerator coefficients }
     }
     defstate {
 	name {denominator}
 	type {floatarray}
-	default {
-		"1 .5 .3"
-	}
-	desc { Denominator coefficients. }
+	default { "1 .5 .3" }
+	desc { Denominator coefficients }
     }
     defstate {
 	name {state}
 	type {floatarray}
 	default { "0" }
-	desc {State.}
+	desc {State}
 	attributes { A_NONCONSTANT|A_NONSETTABLE }
     }
-
     protected {
 	int numState;
     }
-    ccinclude {
-	<minmax.h>	// for max()
+    constructor {
+	numState = 0;
     }
+    // for max()
+    ccinclude { <minmax.h> }
     setup {
 	int numNumer = numerator.size();
 	int numDenom = denominator.size();
 	double b0, scaleDenom, scaleNumer;
-
 	numState = max(numNumer,numDenom); 
 
 	// Set up scaling to distribute the gain through the numerator,
@@ -120,37 +114,36 @@ Prentice-Hall: Englewood Cliffs, NJ, 1989.
 	    state[i*3+2] = i < numDenom ? scaleDenom * -denominator[i] : 0;
 	}
     }
+    codeblock(feedThrough) {
+	/* No state for the IIR filter: just scale the input */
+	$ref(signalOut) = $ref(state,1) * $ref(signalIn);
+    }
     codeblock(iir) {
-	double s0 = $ref(signalIn);
+	/*
+	   v[0] is the current state variable, v[1] is the current numerator
+	   coefficient, and v[2] is the currrent denominator coefficient
+	 */
+	double* v = & $ref(state,3);
+	double* stateEnd = (double *) $ref(state) + $size(state);
+	double  s, y;
 
-	if ( $size(state) == 3 ) {
-	    // actually, this means no state; just feed through
-	    $ref(signalOut) = $ref(state,1) * s0;
-	} else {
-	    register double *v = & $ref(state,3);
-	    register double y;
-	    double s;
-	    
-	    double * stateEnd = (double *) $ref(state) + $size(state);
+	s = *v++;
+	y = s * *v++;
+	s0 += s * *v++;
 
-	    s = *v++;
-	    y = s * *v++;
+	for ( ; v < stateEnd; ) {
+	    double sTmp = *v;
+	    *v++ = s;
+	    s = sTmp;
+	    y += s * *v++;
 	    s0 += s * *v++;
-
-	    for ( ; v < stateEnd; ) {
-	      double sTmp = *v;
-	      *v++ = s;
-	      s = sTmp;
-	      y += s * *v++;
-	      s0 += s * *v++;
-	    }
-	    $ref(state,3) = s0;
-	    y += s0 * $ref(state,1);
-	    $ref(signalOut) = y;
-
 	}
+	$ref(state,3) = s0;
+	y += s0 * $ref(state,1);
+	$ref(signalOut) = y;
     }
     go {
-        addCode(iir);
+	if ( numState == 1 ) addCode(feedThrough);
+	else addCode(iir);
     }
 }
