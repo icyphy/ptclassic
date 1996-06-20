b88cb4a8d0fec972476cfaf6b376be263c70834f
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-20 22:16:11 +0000

    Minor code formatting change

--- ptclassic/src/domains/c50/kernel/C50Target.cc
+++ ptclassic/src/domains/c50/kernel/C50Target.cc
@@ -1,9 +1,9 @@
- static const char file_id[] = "C50Target.cc";
+static const char file_id[] = "C50Target.cc";
 /******************************************************************
 Version identification:
 $Id$
 
-Copyright (c) 1990-1994 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
