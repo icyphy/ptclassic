1d78c224476f31f5c7bf807a9aa20a62e894abc3
luisgm <luisgm@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-26 21:19:39 +0000

    Initial revision

---
+++ ptclassic/src/domains/c50/stars/C50ConstCx.pl
@@ -0,0 +1,51 @@
+defstar {
+    name { ConstCx }
+    domain { C50 }
+    desc { Constant source }
+    version { @(#)C50ConstCx.pl	1.3 3/7/96 }
+    author { Jose Luis Pino }
+    copyright {
+Copyright (c) 1990-1996 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
+    location { C50 main library }
+    explanation {
+There are no runtime instructions associated with this star.
+The output buffer is initialized with the specified DC value.
+    }
+    output {
+	name {output}
+	type {COMPLEX}
+	attributes{P_NOINIT}
+    }
+    state {
+	name {real}
+	type {float}
+	default {0.0}
+	descriptor { Real part of DC value. }
+    }
+    state {
+	name {imag}
+	type {float}
+	default {0.0}
+	descriptor { Imaginary part of DC value. }
+   }
+
+	codeblock(data){
+	.ds	$addr(output)
+	.q15	$val(real) 
+	.q15	$val(imag)
+	.text
+	}
+    constructor {
+	noInternalState();
+    }
+    go{
+	addCode(data);
+	}
+    execTime { return 0; }
+}
+
+
