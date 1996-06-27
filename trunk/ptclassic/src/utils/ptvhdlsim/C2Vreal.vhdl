76fc0bc12b842effb26ee241df8e82be38a40219
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-27 17:41:36 +0000

    removed needless CLI_PIN attributes on output pins

--- trunk/ptclassic/src/utils/ptvhdlsim/C2Vreal.vhdl
+++ trunk/ptclassic/src/utils/ptvhdlsim/C2Vreal.vhdl
@@ -24,8 +24,6 @@ architecture CLI of C2VREAL is
 	attribute CLI_CLOSE	of CLI	: architecture is "c2vreal_close";
 
 	attribute CLI_PIN	of go	: signal is CLI_ACTIVE;
-	attribute CLI_PIN	of data	: signal is CLI_PASSIVE;
-	attribute CLI_PIN	of done	: signal is CLI_PASSIVE;
 
 begin
 end;
