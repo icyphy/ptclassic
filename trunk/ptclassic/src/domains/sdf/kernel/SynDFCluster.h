f7e02b51fc8ceff6b0b94827ae2f5a635d0c164d
murthy <murthy@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-06-28 16:32:58 +0000

    remove virtual declaration for makeNewPort since its already declared virtual in Cluster

--- trunk/ptclassic/src/domains/sdf/kernel/SynDFCluster.h
+++ trunk/ptclassic/src/domains/sdf/kernel/SynDFCluster.h
@@ -88,7 +88,7 @@ public:
 	SynDFCluster();
 
 	/* virtual */ Block* makeNew() const { return new SynDFCluster; }
-	inline virtual ClusterPort* makeNewPort(GenericPort& p) {
+	inline /* virtual */ ClusterPort* makeNewPort(GenericPort& p) {
 	    return new SynDFClusterPort(p);
 	}
 	/* virtual */ Cluster* convertStar(Star&);
