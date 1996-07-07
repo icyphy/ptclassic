5c2e63aea60f2ef65ec6369bb6505a20956f7e77
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-07 18:45:06 +0000

    Functions names begin with Ptdsp_ instead of PTDSP

--- ptclassic/src/utils/libptdsp/ptdspMuLaw.c
+++ ptclassic/src/utils/libptdsp/ptdspMuLaw.c
@@ -28,14 +28,14 @@ Programmer: J. T. Buck
 Version: $Id$
 */
 
-#include "PTDSPMuLaw.h"
+#include "ptdspMuLaw.h"
 
 const int BIAS = 0x84;
 const int CLIP = 32635;
 
 /* MuLaw compression routine */
 
-unsigned char PTDSPMuLaw( int sample ) {
+unsigned char Ptdsp_MuLaw( int sample ) {
   static int exp_lut[256] = {0,0,1,1,2,2,2,2,3,3,3,3,3,3,3,3,
 			     4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,
 			     5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
