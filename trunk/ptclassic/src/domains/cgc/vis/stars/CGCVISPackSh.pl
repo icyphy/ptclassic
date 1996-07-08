61563c841bbc05bcbce72bb73464623b74edc595
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-08 15:51:37 +0000

    added forward/backward option and union structure

--- trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISPackSh.pl
+++ trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISPackSh.pl
@@ -14,7 +14,9 @@ limitation of liability, and disclaimer of warranty provisions.
 	  Pack four floating point numbers into a single floating
 	  point number.  Each input floating point number is first
 	  down cast into a 16 bit short and then packed into a series of
-	  four shorts.}
+	  four shorts.  The four shorts can be packed with the either
+	  the most current value leading or trailing but make sure 
+	  that you pack and unpack with the same endian setting. }
 	input {
 		name { in }
 		type { float }
@@ -32,6 +34,15 @@ limitation of liability, and disclaimer of warranty provisions.
 		desc { Input scale }
 		attributes { A_CONSTANT|A_SETTABLE }
 	}
+	defstate {
+	        name { forward }
+		type { int }
+		default { FALSE }
+		desc { forward = TRUE unpacks with most current sample at
+		       position 0; forward = FALSE unpacks with most
+		       current sample at position 3 }
+		attributes { A_CONSTANT|A_SETTABLE }
+	}
 	code{
 #define PACKIN (4)
 	}
@@ -42,35 +53,40 @@ limitation of liability, and disclaimer of warranty provisions.
 	  in.setSDFParams(PACKIN,PACKIN-1);
 	}
 	codeblock(mainDecl){
-	  short* $starSymbol(packedout) =
-	    (short*)memalign(sizeof(double),sizeof(short)*4);
-	}	  
+	  union $starSymbol(outoverlay) {
+	    vis_d64 outvaluedbl;
+	    vis_s16 outvaluesh[4];
+	  } $starSymbol(packedout);
+	}
 	initCode{
 	  addDeclaration(mainDecl);
+	  addInclude("<vis_types.h>");
 	}
-	codeblock(localDecl){
-	  double* outvalue;
-	}
-	codeblock(packit){
-          /*scale,cast,and pack input*/
-          $starSymbol(packedout)[0] = (short)
-	    ($val(scale)*(double)$ref2(in,0));
-          $starSymbol(packedout)[1] = (short)
-	    ($val(scale)*(double)$ref2(in,1));
-          $starSymbol(packedout)[2] = (short)
-	    ($val(scale)*(double)$ref2(in,2));
-          $starSymbol(packedout)[3] = (short)
-	    ($val(scale)*(double)$ref2(in,3));
+	codeblock(packbackwards){
+          /*scale and cast input*/
+          $starSymbol(packedout).outvaluesh[0] = (short)($val(scale)*(double)$ref2(in,0));
+          $starSymbol(packedout).outvaluesh[1] = (short)($val(scale)*(double)$ref2(in,1));
+          $starSymbol(packedout).outvaluesh[2] = (short)($val(scale)*(double)$ref2(in,2));
+          $starSymbol(packedout).outvaluesh[3] = (short)($val(scale)*(double)$ref2(in,3));
+
 	  /*output packed double*/	  
-	  outvalue = (double *) $starSymbol(packedout);
-	  $ref(out) = *outvalue;
+	  $ref(out) = $starSymbol(packedout).outvaluedbl;
+	}
+	codeblock(packforwards){
+          /*scale and cast input*/
+          $starSymbol(packedout).outvaluesh[0] = (short)($val(scale)*(double)$ref2(in,3));
+          $starSymbol(packedout).outvaluesh[1] = (short)($val(scale)*(double)$ref2(in,2));
+          $starSymbol(packedout).outvaluesh[2] = (short)($val(scale)*(double)$ref2(in,1));
+          $starSymbol(packedout).outvaluesh[3] = (short)($val(scale)*(double)$ref2(in,0));
+
+	  /*output packed double*/	  
+	  $ref(out) = $starSymbol(packedout).outvaluedbl;
 	}
 	go {
-	  addCode(localDecl);
-	  addCode(packit);
-	}
-	wrapup{
-	  addCode("free($starSymbol(packedout));\n");
+	  if (!forward)
+	    addCode(packbackwards);
+	  else
+	    addCode(packforwards);
 	}
 }
 
