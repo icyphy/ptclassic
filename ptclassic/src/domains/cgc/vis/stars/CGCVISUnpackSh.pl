64377b41e14dafe2cea6c9fcb39e94243f3790a8
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-08 15:51:37 +0000

    added forward/backward option and union structure

--- ptclassic/src/domains/cgc/vis/stars/CGCVISUnpackSh.pl
+++ ptclassic/src/domains/cgc/vis/stars/CGCVISUnpackSh.pl
@@ -32,6 +32,15 @@ limitation of liability, and disclaimer of warranty provisions.
 		desc { Output scale }
 		attributes { A_CONSTANT|A_SETTABLE }
 	}
+	defstate {
+	        name { forward }
+		type { int }
+		default { FALSE }
+		desc { forward = TRUE unpacks with most current sample at
+		       position 0; forward = FALSE unpacks with most
+		       current sample at position 3 }
+		attributes { A_CONSTANT|A_SETTABLE }
+	}
 	code{
 #define PACKOUT (4)
 	}
@@ -39,31 +48,46 @@ limitation of liability, and disclaimer of warranty provisions.
 	  out.setSDFParams(PACKOUT,PACKOUT-1);
 	}
 	codeblock(mainDecl){
-          double *$starSymbol(packedin) =
-	    (double*)memalign(sizeof(double),sizeof(double));
-	}
+	  union $starSymbol(inoverlay) {
+	    vis_d64 invaluedbl;
+	    vis_s16 invaluesh[4];
+	  } $starSymbol(packedin);
+	}	  
 	initCode{
 	  addDeclaration(mainDecl);
-	}
-	codeblock(localDecl){
-	  double outvalue;
-	  short *invalue;
+          addInclude("<vis_types.h>");
 	}
 	codeblock(unpackit){
-
-	  *$starSymbol(packedin) = (double) $ref(in);
-	  invalue = (short *) $starSymbol(packedin);
+	  $starSymbol(packedin).invaluedbl = (double) $ref(in);
+	}
+	codeblock(unpackbackwards){
 	  /*scale down and unpack input*/
-          $ref2(out,0)=(double) ($val(scaledown) * (double) invalue[0]);
-          $ref2(out,1)=(double) ($val(scaledown) * (double) invalue[1]);
-          $ref2(out,2)=(double) ($val(scaledown) * (double) invalue[2]);
-          $ref2(out,3)=(double) ($val(scaledown) * (double) invalue[3]);
+          $ref2(out,0)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[0]);
+          $ref2(out,1)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[1]);
+          $ref2(out,2)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[2]);
+          $ref2(out,3)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[3]);
+	}
+	codeblock(unpackforwards){
+	  /*scale down and unpack input*/
+          $ref2(out,0)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[3]);
+          $ref2(out,1)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[2]);
+          $ref2(out,2)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[1]);
+          $ref2(out,3)=(double) ($val(scaledown) * (double)
+				 $starSymbol(packedin).invaluesh[0]);
+
 	}
 	go {
-	  addCode(localDecl);
 	  addCode(unpackit);
+	  if (!forward)
+	    addCode(unpackbackwards);
+	  else
+	    addCode(unpackforwards);
 	}
-	wrapup{
-	  addCode("free($starSymbol(packedin));\n");
-       	}
 }
