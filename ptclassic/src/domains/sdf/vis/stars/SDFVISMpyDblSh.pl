43f1a383e2dfc15afda20c76915cae7ac68c7fde
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-09 16:52:50 +0000

    cleaned up code

--- ptclassic/src/domains/sdf/vis/stars/SDFVISMpyDblSh.pl
+++ ptclassic/src/domains/sdf/vis/stars/SDFVISMpyDblSh.pl
@@ -32,29 +32,30 @@ results in a 32 bit result, which is then rounded to 16bits.
 	  type { float }
 	  desc { Output float type }
 	}
+	hinclude { <vis_types.h> }
         ccinclude { <vis_proto.h> }
 	go {
 	  vis_write_gsr(8);
 
 	  // setup the data
-	  float dataAhi = vis_read_hi(double(inA%0));
-	  float dataAlo = vis_read_lo(double(inA%0));
-	  float dataBhi = vis_read_hi(double(inB%0));
-	  float dataBlo = vis_read_lo(double(inB%0));
+	  vis_f32 dataAhi = vis_read_hi(double(inA%0));
+	  vis_f32 dataAlo = vis_read_lo(double(inA%0));
+	  vis_f32 dataBhi = vis_read_hi(double(inB%0));
+	  vis_f32 dataBlo = vis_read_lo(double(inB%0));
 	  
 	  // calculate the partial products
-	  double resulthihi = vis_fmuld8sux16(dataAhi,dataBhi);
-	  double resulthilo = vis_fmuld8ulx16(dataAhi,dataBhi);
-	  double resulthi   = vis_fpadd32(resulthihi,resulthilo);
+	  vis_d64 resulthihi = vis_fmuld8sux16(dataAhi,dataBhi);
+	  vis_d64 resulthilo = vis_fmuld8ulx16(dataAhi,dataBhi);
+	  vis_d64 resulthi   = vis_fpadd32(resulthihi,resulthilo);
 	  
-	  double resultlohi = vis_fmuld8sux16(dataAlo,dataBlo);
-	  double resultlolo = vis_fmuld8ulx16(dataAlo,dataBlo);
-	  double resultlo   = vis_fpadd32(resultlohi,resultlolo);
+	  vis_d64 resultlohi = vis_fmuld8sux16(dataAlo,dataBlo);
+	  vis_d64 resultlolo = vis_fmuld8ulx16(dataAlo,dataBlo);
+	  vis_d64 resultlo   = vis_fpadd32(resultlohi,resultlolo);
 
 	  // pack and concat the final product*/
-	  float resultu = vis_fpackfix(resulthi);
-	  float resultl = vis_fpackfix(resultlo);
-	  double result = vis_freg_pair(resultu,resultl);
+	  vis_f32 resultu = vis_fpackfix(resulthi);
+	  vis_f32 resultl = vis_fpackfix(resultlo);
+	  vis_d64 result = vis_freg_pair(resultu,resultl);
 	  
           out%0 << result;
       	}
