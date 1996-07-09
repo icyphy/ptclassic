106997287e0104e5c210dfaff9c1cd5a0b82a967
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-09 15:21:49 +0000

    added in retiming and set tap functions

--- trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISBiquad.pl
+++ trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISBiquad.pl
@@ -2,7 +2,7 @@ defstar {
 	name { VISBiquad }
 	domain { CGC }
 	version { $Id$ }
-	author { William Chen }
+	author { William Chen and John Reekie}
 	copyright {
 Copyright (c) 1990-1996 The Regents of the University of California.
 All rights reserved.
@@ -58,12 +58,12 @@ limitation of liability, and disclaimer of warranty provisions.
       	defstate {
 	  name { scaletaps }
 	  type { float }
-	  default { "32767.0/16" }
+	  default { "32767.0" }
 	  desc { Filter tap scale }
 	  attributes { A_CONSTANT|A_SETTABLE }
 	}
 	setup {
-          signalIn.setSDFParams(1,2);
+          signalIn.setSDFParams(1,1);
 	}
 	code {
 #define NUMPACK (4)
@@ -79,102 +79,111 @@ limitation of liability, and disclaimer of warranty provisions.
 	    }
 	}
 	codeblock(settaps){
-	  static void settaps(vis_d64* taps1, vis_s16* taps,vis_d64 betatop,vis_d64
+	  static void settaps(vis_d64* filtertaps, vis_s16* tapmatrix,vis_d64* betatop,vis_d64*
 			      betabott,vis_s16 scaledown){
 
-	    vis_d64 t0,t1,t2,t3;
-	    vis_s16 stmp[8],*splitsh;
+	    vis_d64 t0,t1,t2,t3,top,bottom;
+	    vis_s16 statemem[8];
 
 	    vis_alignaddr(0,6);
 
-	    taps[0] = 0;
-	    taps[1] = 0;
-	    taps[2] = (vis_s16)$val(scaletaps)/scaledown*taps1[4];
-	    taps[3] = (vis_s16)$val(scaletaps)/scaledown*taps1[3];
-	    taps[4] = (vis_s16)$val(scaletaps)/scaledown*taps1[2];
-	    taps[5] = 0;
-	    taps[6] = 0;
-	    taps[7] = 0;
-	    taps[8] = 0;
-	    taps[9] = 0;
-	    taps[10] = (vis_s16)$val(scaletaps)/scaledown*(-(taps1[4]*taps1[0]));
-	    taps[11] = (vis_s16)$val(scaletaps)/scaledown*(taps1[4]-(taps1[3]*taps1[0]));
-	    taps[12] = (vis_s16)$val(scaletaps)/scaledown*(taps1[3]-(taps1[2]*taps1[0]));
-	    taps[13] = (vis_s16)$val(scaletaps)/scaledown*taps1[2];
-	    taps[14] = 0;
-	    taps[15] = 0;
-	    taps[16] = 0;
-	    taps[17] = 0;
-	    taps[18] =(vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[4]*taps1[0]*taps1[0]-taps1[4]*taps1[1]);
-	    taps[19] =(vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[3]*taps1[0]*taps1[0]-taps1[4]*taps1[0]-taps1[3]*taps1[1]);
-	    taps[20] =(vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[2]*taps1[0]*taps1[0]-taps1[3]*taps1[0]-taps1[2]*taps1[1]+taps1[4]);
-	    taps[21] = (vis_s16)$val(scaletaps)/scaledown * (taps1[3]-(taps1[2]*taps1[0]));
-	    taps[22] = (vis_s16)$val(scaletaps)/scaledown * taps1[2];
-	    taps[23] = 0;
-	    taps[24] = 0;
-	    taps[25] = 0;
-	    taps[26] = (vis_s16)$val(scaletaps)/scaledown *
-	      (2*taps1[4]*taps1[0]*taps1[1]-taps1[4]*taps1[0]*taps1[0]*taps1[0]);
-	    taps[27] = (vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[4]*taps1[0]*taps1[0]-taps1[4]*taps1[1] -
-	       taps1[3]*taps1[0]*taps1[0]*taps1[0]+2*taps1[3]*taps1[0]*taps1[1]);
-	    taps[28] = (vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[3]*taps1[0]*taps1[0] -
-	       taps1[2]*taps1[0]*taps1[0]*taps1[0] -
-	       taps1[3]*taps1[1]-taps1[4]*taps1[0]+2*taps1[2]*taps1[0]*taps1[1]);
-	    taps[29] = (vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[4]-taps1[3]*taps1[0]-taps1[2]*taps1[1]+taps1[2]*taps1[0]*taps1[0]);
-	    taps[30] = (vis_s16)$val(scaletaps)/scaledown*(taps1[3]-(taps1[2]*taps1[0]));
-	    taps[31] = (vis_s16)$val(scaletaps)/scaledown*taps1[2];
+	    tapmatrix[0] = 0;
+	    tapmatrix[1] = 0;
+	    tapmatrix[2] = (vis_s16)$val(scaletaps)/scaledown*filtertaps[4];
+	    tapmatrix[3] = (vis_s16)$val(scaletaps)/scaledown*filtertaps[3];
+	    tapmatrix[4] = (vis_s16)$val(scaletaps)/scaledown*filtertaps[2];
+	    tapmatrix[5] = 0;
+	    tapmatrix[6] = 0;
+	    tapmatrix[7] = 0;
+	    tapmatrix[8] = 0;
+	    tapmatrix[9] = 0;
+	    tapmatrix[10] = (vis_s16)$val(scaletaps)/scaledown*(-(filtertaps[4]*filtertaps[0]));
+	    tapmatrix[11] = (vis_s16)$val(scaletaps)/scaledown*(filtertaps[4]-(filtertaps[3]*filtertaps[0]));
+	    tapmatrix[12] = (vis_s16)$val(scaletaps)/scaledown*(filtertaps[3]-(filtertaps[2]*filtertaps[0]));
+	    tapmatrix[13] = (vis_s16)$val(scaletaps)/scaledown*filtertaps[2];
+	    tapmatrix[14] = 0;
+	    tapmatrix[15] = 0;
+	    tapmatrix[16] = 0;
+	    tapmatrix[17] = 0;
+	    tapmatrix[18] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[4]*filtertaps[0]*filtertaps[0]-filtertaps[4]*filtertaps[1]);
+	    tapmatrix[19] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[3]*filtertaps[0]*filtertaps[0]-filtertaps[4]*filtertaps[0]-filtertaps[3]*filtertaps[1]);
+	    tapmatrix[20] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[2]*filtertaps[0]*filtertaps[0] -
+	       filtertaps[3]*filtertaps[0]-filtertaps[2]*filtertaps[1]+filtertaps[4]);
+	    tapmatrix[21] = (vis_s16)$val(scaletaps)/scaledown * (filtertaps[3]-(filtertaps[2]*filtertaps[0]));
+	    tapmatrix[22] = (vis_s16)$val(scaletaps)/scaledown * filtertaps[2];
+	    tapmatrix[23] = 0;
+	    tapmatrix[24] = 0;
+	    tapmatrix[25] = 0;
+	    tapmatrix[26] = (vis_s16)$val(scaletaps)/scaledown *
+	      (2*filtertaps[4]*filtertaps[0]*filtertaps[1] -
+	       filtertaps[4]*filtertaps[0]*filtertaps[0]*filtertaps[0]);
+	    tapmatrix[27] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[4]*filtertaps[0]*filtertaps[0]-filtertaps[4]*filtertaps[1] -
+	       filtertaps[3]*filtertaps[0]*filtertaps[0]*filtertaps[0]+2*filtertaps[3]*filtertaps[0]*filtertaps[1]);
+	    tapmatrix[28] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[3]*filtertaps[0]*filtertaps[0] -
+	       filtertaps[2]*filtertaps[0]*filtertaps[0]*filtertaps[0] -
+	       filtertaps[3]*filtertaps[1]-filtertaps[4]*filtertaps[0]+2*filtertaps[2]*filtertaps[0]*filtertaps[1]);
+	    tapmatrix[29] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[4]-filtertaps[3]*filtertaps[0] -
+	       filtertaps[2]*filtertaps[1]+filtertaps[2]*filtertaps[0]*filtertaps[0]);
+	    tapmatrix[30] = (vis_s16)$val(scaletaps)/scaledown*(filtertaps[3]-(filtertaps[2]*filtertaps[0]));
+	    tapmatrix[31] = (vis_s16)$val(scaletaps)/scaledown*filtertaps[2];
 	    
-	    stmp[0] = (vis_s16)$val(scaletaps)/scaledown*(-taps1[1]);
-	    stmp[1] = (vis_s16)$val(scaletaps)/scaledown*(-taps1[0]);
-	    stmp[2] = (vis_s16)$val(scaletaps)/scaledown*(taps1[0]*taps1[1]);
-	    stmp[3] = (vis_s16)$val(scaletaps)/scaledown*(taps1[0]*taps1[0]-taps1[1]);
-	    stmp[4] = (vis_s16)$val(scaletaps)/scaledown*(taps1[1]*taps1[1]-taps1[0]*taps1[0]*taps1[1]);
-	    stmp[5] = (vis_s16)$val(scaletaps)/scaledown*(2*taps1[0]*taps1[1]-taps1[0]*taps1[0]*taps1[0]);
-	    stmp[6] = (vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[0]*taps1[0]*taps1[0]*taps1[1]-2*taps1[0]*taps1[1]*taps1[1]);
-	    stmp[7] = (vis_s16)$val(scaletaps)/scaledown *
-	      (taps1[0]*taps1[0]*taps1[0]*taps1[0]-3*taps1[0]*taps1[0]*taps1[1]+taps1[1]*taps1[1]);
+	    statemem[0] = (vis_s16)$val(scaletaps)/scaledown*(-filtertaps[1]);
+	    statemem[1] = (vis_s16)$val(scaletaps)/scaledown*(-filtertaps[0]);
+	    statemem[2] = (vis_s16)$val(scaletaps)/scaledown*(filtertaps[0]*filtertaps[1]);
+	    statemem[3] = (vis_s16)$val(scaletaps)/scaledown*(filtertaps[0]*filtertaps[0]-filtertaps[1]);
+	    statemem[4] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[1]*filtertaps[1]-filtertaps[0]*filtertaps[0]*filtertaps[1]);
+	    statemem[5] = (vis_s16)$val(scaletaps)/scaledown *
+	      (2*filtertaps[0]*filtertaps[1]-filtertaps[0]*filtertaps[0]*filtertaps[0]);
+	    statemem[6] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[0]*filtertaps[0]*filtertaps[0]*filtertaps[1]-2*filtertaps[0]*filtertaps[1]*filtertaps[1]);
+	    statemem[7] = (vis_s16)$val(scaletaps)/scaledown *
+	      (filtertaps[0]*filtertaps[0]*filtertaps[0]*filtertaps[0]
+	       -3*filtertaps[0]*filtertaps[0]*filtertaps[1]+filtertaps[1]*filtertaps[1]);
 
-	    betatop = vis_fzero();
-	    betabott = vis_fzero();
-	    splitsh = (vis_s16 *) stmp;
-	    t0 = vis_ld_u16(splitsh+0);
-	    t1 = vis_ld_u16(splitsh+1);
-	    t2 = vis_ld_u16(splitsh+2);
-	    t3 = vis_ld_u16(splitsh+3);
-	    betatop = vis_faligndata(t3,betatop);
-	    betatop = vis_faligndata(t2,betatop);
-	    betatop = vis_faligndata(t1,betatop);
-	    betatop = vis_faligndata(t0,betatop);
-	    t0 = vis_ld_u16(splitsh+4);
-	    t1 = vis_ld_u16(splitsh+5);
-	    t2 = vis_ld_u16(splitsh+6);
-	    t3 = vis_ld_u16(splitsh+7);
-	    betabott = vis_faligndata(t3,betabott);
-	    betabott = vis_faligndata(t2,betabott);
-	    betabott = vis_faligndata(t1,betabott);
-	    betabott = vis_faligndata(t0,betabott);
+	    top = vis_fzero();
+	    bottom = vis_fzero();
+	    t0 = vis_ld_u16(statemem+0);
+	    t1 = vis_ld_u16(statemem+1);
+	    t2 = vis_ld_u16(statemem+2);
+	    t3 = vis_ld_u16(statemem+3);
+	    top = vis_faligndata(t3,top);
+	    top = vis_faligndata(t2,top);
+	    top = vis_faligndata(t1,top);
+	    top = vis_faligndata(t0,top);
+	    t0 = vis_ld_u16(statemem+4);
+	    t1 = vis_ld_u16(statemem+5);
+	    t2 = vis_ld_u16(statemem+6);
+	    t3 = vis_ld_u16(statemem+7);
+	    bottom = vis_faligndata(t3,bottom);
+	    bottom = vis_faligndata(t2,bottom);
+	    bottom = vis_faligndata(t1,bottom);
+	    bottom = vis_faligndata(t0,bottom);
+	    *betatop=top;
+	    *betabott=bottom;
 	  }
 	}
 	codeblock(mainDecl) {
-	  vis_s16* $starSymbol(taps) = (vis_s16*)memalign(sizeof(vis_d64),sizeof(vis_s16)*32);
-	  vis_d64  $starSymbol(betabott),$starSymbol(betatop),$starSymbol(taps1)[5];
+	  vis_s16* $starSymbol(tapmatrix) = (vis_s16*)memalign(sizeof(vis_d64),sizeof(vis_s16)*32);
+	  vis_d64  $starSymbol(betabott),$starSymbol(betatop),$starSymbol(filtertaps)[5];
+	  vis_f32  $starSymbol(currentstate);
 	  vis_s16  $starSymbol(scaledown);
 	}
 	codeblock(initialize) {
-	  $starSymbol(taps1)[0] = $ref2(dentaps,0);
-	  $starSymbol(taps1)[1] = $ref2(dentaps,1);
-	  $starSymbol(taps1)[2] = $ref2(numtaps,0);
-	  $starSymbol(taps1)[3] = $ref2(numtaps,1);
-	  $starSymbol(taps1)[4] = $ref2(numtaps,2); 
+	  $starSymbol(filtertaps)[0] = $ref2(dentaps,0);
+	  $starSymbol(filtertaps)[1] = $ref2(dentaps,1);
+	  $starSymbol(filtertaps)[2] = $ref2(numtaps,0);
+	  $starSymbol(filtertaps)[3] = $ref2(numtaps,1);
+	  $starSymbol(filtertaps)[4] = $ref2(numtaps,2); 
 	  $starSymbol(scaledown) = (short) 1 << $val(scalefactor);
-	  settaps($starSymbol(taps1),$starSymbol(taps),$starSymbol(betatop),$starSymbol(betabott),$starSymbol(scaledown));
+	  settaps($starSymbol(filtertaps),$starSymbol(tapmatrix),&$starSymbol(betatop),&$starSymbol(betabott),$starSymbol(scaledown));
+	  $starSymbol(currentstate) = vis_fzeros();
       	}
 	initCode {
 	  addInclude("<vis_proto.h>");
@@ -190,26 +199,25 @@ limitation of liability, and disclaimer of warranty provisions.
 	  vis_d64 in0,in1,*indextaps;
 	  vis_f32 accumpair0hi,accumpair0lo,accumpair1hi,accumpair1lo;
 	  vis_f32 accumpair2hi,accumpair2lo,accumpair3hi,accumpair3lo;
-	  vis_f32 accumpair0,accumpair1,accumpair2,accumpair3,currentstate;
+	  vis_f32 accumpair0,accumpair1,accumpair2,accumpair3;
 	  vis_f32 statetophi,statetoplo,statebotthi,statebottlo;
 	  vis_s32 result0,result1,result2,result3;
 	  vis_u32 fu,ffu;
-	  int i;
 	  vis_s16 out0,out1,out2,out3;
 	}
 	codeblock(filter){
-	  currentstate = vis_fzeros();
 	  in1 = (vis_d64)$ref2(signalIn,0);
 	  in0 = (vis_d64)$ref2(signalIn,1);
-	  repeatstate = vis_freg_pair(currentstate,currentstate);
+	  
+	  repeatstate = vis_freg_pair($starSymbol(currentstate),$starSymbol(currentstate));
 	  statetop = mult4x4(repeatstate,$starSymbol(betatop));
 	  statetophi = vis_read_hi(statetop);
 	  statetoplo = vis_read_lo(statetop);
 	  statebott = mult4x4(repeatstate,$starSymbol(betabott));
 	  statebotthi = vis_read_hi(statebott);
 	  statebottlo = vis_read_lo(statebott);
-
-	  indextaps = (vis_d64*) $starSymbol(taps);
+	  
+	  indextaps = (vis_d64*) $starSymbol(tapmatrix);
 	  accumquad0 = mult4x4(in0,*indextaps++);
 	  quad0 = mult4x4(in1,*indextaps++);
 	  accumquad1 = mult4x4(in0,*indextaps++);
@@ -217,7 +225,7 @@ limitation of liability, and disclaimer of warranty provisions.
 	  accumquad2 = mult4x4(in0,*indextaps++);
 	  quad2 = mult4x4(in1,*indextaps++);
 	  accumquad3 = mult4x4(in0,*indextaps++);
-	  quad3 = mult4x4(in1,*indextaps);
+	  quad3 = mult4x4(in1,*indextaps++);
 
 	  accumquad0 = vis_fpadd16(accumquad0,quad0);
 	  accumpair0hi = vis_read_hi(accumquad0);
@@ -253,14 +261,14 @@ limitation of liability, and disclaimer of warranty provisions.
     
 	  fu = out2 << 16 | out3 & 0xffff;
 	  $ref(signalOut) = vis_to_double(ffu,fu);
-	  currentstate = vis_to_float(fu);
+	  $starSymbol(currentstate) = vis_to_float(fu);
 	}	
 	go {
 	  addCode(localDecl);
 	  addCode(filter);
 	}
 	wrapup {
-          addCode("free($starSymbol(taps));");
+          addCode("free($starSymbol(tapmatrix));");
         }
 }
 
