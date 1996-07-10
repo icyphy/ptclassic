2931313ed4aadb2df5d3b807b36a5f0999ebb758
yukee <yukee@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-10 17:54:21 +0000

    modifed comments to conform to ext format

--- ptclassic/src/utils/libptdsp/ptdspfft_rif.c
+++ ptclassic/src/utils/libptdsp/ptdspfft_rif.c
@@ -1,4 +1,15 @@
-/*
+/**CFile***********************************************************************
+
+  FileName    [ ptdspfft_rif.c ]
+
+  PackageName [ ptdsp ]
+
+  Synopsis    [ Function definition for Ptdsp_fft_rif ]
+
+  Author      [ Joseph Buck ]
+
+  Copyright   [ 
+
 Copyright (c) 1990-1996 The Regents of the University of California.
 All rights reserved.
 
@@ -23,14 +34,32 @@ ENHANCEMENTS, OR MODIFICATIONS.
 
 					PT_COPYRIGHT_VERSION_2
 					COPYRIGHTENDKEY
+ ]
 
-Programmer: Joseph Buck
-Version: $Id$
-*/
+  Version     [ $Id$ ]
+
+******************************************************************************/
 
 #include <math.h>
 #include "ptdspfft_rif.h"
 
+#define SWAP(a, b) tempr=(a); (a)=(b); (b)=tempr
+
+/*---------------------------------------------------------------------------*/
+/* Definition of exported functions                                          */
+/*---------------------------------------------------------------------------*/
+
+/**Function*******************************************************************
+  Synopsis    [ Replace data by its discrete Fourier transform or
+                inverse discrete Fourier transform ]
+  Description [ Replace data by its discrete Fourier transform, if
+                isign is input as 1, or by its inverse discrete
+		Fourier transform, if "isign" is input as -1. "data'"
+		is a complex array of length "nn", input as a real
+		array data[0..2*nn-1]. "nn" MUST be an integer power
+		of 2 (this is not checked for!?)
+  SideEffects []
+******************************************************************************/
 /*
  * This fft routine is from ~gabriel/src/filters/fft/fft.c;
  * I am unsure of the original source.  The file contains no
@@ -39,62 +68,52 @@ Version: $Id$
  * function body is unchanged.  (J. T. Buck)
  */
 
-#define SWAP(a, b) tempr=(a); (a)=(b); (b)=tempr
+void 
+Ptdsp_fft_rif(double *data, int nn, int isign) {
+  int	n;
+  int	mmax;
+  int	m, j, istep, i;
+  double wtemp, wr, wpr, wpi, wi, theta;
+  double tempr, tempi;
+  
+  data--;
+  n = nn << 1;
+  j = 1;
 
-/*
- * Replace data by its discrete Fourier transform, if isign is
- * input as 1, or by its inverse discrete Fourier transform, if 
- * "isign" is input as -1.  "data'"is a complex array of length "nn",
- * input as a real array data[0..2*nn-1]. "nn" MUST be an integer
- * power of 2 (this is not checked for!?)
- */
-
-void Ptdsp_fft_rif(double *data, int nn, int isign)
-{
-	int	n;
-	int	mmax;
-	int	m, j, istep, i;
-	double	wtemp, wr, wpr, wpi, wi, theta;
-	double	tempr, tempi;
-
-	data--;
-	n = nn << 1;
-	j = 1;
-
-	for (i = 1; i < n; i += 2) {
-		if (j > i) {
-			SWAP(data[j], data[i]);
-			SWAP(data[j+1], data[i+1]);
-		}
-		m = n >> 1;
-		while (m >= 2 && j >m) {
-			j -= m;
-			m >>= 1;
-		}
-		j += m;
-	}
-	mmax = 2;
-	while (n > mmax) {
-		istep = 2*mmax;
-		theta = -6.28318530717959/(isign*mmax);
-		wtemp = sin(0.5*theta);
-		wpr = -2.0*wtemp*wtemp;
-		wpi = sin(theta);
-		wr = 1.0;
-		wi = 0.0;
-		for (m = 1; m < mmax; m += 2) {
-			for (i = m; i < n; i += istep) {
-				j = i + mmax;
-				tempr = wr*data[j] - wi*data[j+1];
-				tempi = wr*data[j+1] + wi*data[j];
-				data[j] = data[i] - tempr;
-				data[j+1] = data[i+1] - tempi;
-				data[i] += tempr;
-				data[i+1] += tempi;
-			}
-			wr = (wtemp=wr)*wpr - wi*wpi+wr;
-			wi = wi*wpr + wtemp*wpi + wi;
-		}
-		mmax = istep;
-	}
-}
+  for (i = 1; i < n; i += 2) {
+    if (j > i) {
+      SWAP(data[j], data[i]);
+      SWAP(data[j+1], data[i+1]);
+    }
+    m = n >> 1;
+    while (m >= 2 && j >m) {
+      j -= m;
+      m >>= 1;
+    }
+    j += m;
+  }
+  mmax = 2;
+  while (n > mmax) {
+    istep = 2*mmax;
+    theta = -6.28318530717959/(isign*mmax);
+    wtemp = sin(0.5*theta);
+    wpr = -2.0*wtemp*wtemp;
+    wpi = sin(theta);
+    wr = 1.0;
+    wi = 0.0;
+    for (m = 1; m < mmax; m += 2) {
+      for (i = m; i < n; i += istep) {
+	j = i + mmax;
+	tempr = wr*data[j] - wi*data[j+1];
+	tempi = wr*data[j+1] + wi*data[j];
+	data[j] = data[i] - tempr;
+	data[j+1] = data[i+1] - tempi;
+	data[i] += tempr;
+	data[i+1] += tempi;
+      }
+      wr = (wtemp=wr)*wpr - wi*wpi+wr;
+      wi = wi*wpr + wtemp*wpi + wi;
+    }
+    mmax = istep;
+  }
+}	
