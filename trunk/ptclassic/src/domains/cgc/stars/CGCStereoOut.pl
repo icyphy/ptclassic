2eada5a9c07cac3e10b6a7b8c69f47379333de66
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-12 02:54:58 +0000

    Moved supporting codeblocks and methods into CGCStereoBase star

--- trunk/ptclassic/src/domains/cgc/stars/CGCStereoOut.pl
+++ trunk/ptclassic/src/domains/cgc/stars/CGCStereoOut.pl
@@ -1,7 +1,7 @@
 defstar {
     name { StereoOut }
     domain { CGC }
-    derivedFrom { AudioBase }
+    derivedFrom { StereoBase }
     descriptor {
 Writes Compact Disc audio format to a file given by "fileName".  The file
 can be the audio port /dev/audio, if supported by the workstation.  The
@@ -23,6 +23,14 @@ provisions.
     }
     location { CGC main library }
 
+    defstate {
+      name { outputPort }
+      type { string }
+      default { line_out }
+      desc { Audio input port: line_out or speaker. }
+      attributes { A_GLOBAL }
+    }
+
     input {
       name { left }
       type { float }
@@ -35,54 +43,6 @@ provisions.
       desc { Right channel input }
     }
 
-    defstate {
-      name { encodingType }
-      type { string }
-      default { "linear" }
-      desc { Type of encoding : Linear or U_Law PCM. }
-      attributes { A_GLOBAL }    
-    }
-
-    defstate {
-      name { sampleRate }
-      type { int }
-      default { 44100 }
-      desc { Number of samples per second. }
-      attributes { A_GLOBAL }
-    }
-
-    defstate {
-      name { precision }
-      type { int }
-      default { 16 }
-      desc { Bit-width of each sample. }
-      attributes { A_GLOBAL }    
-    }
-    
-    defstate {
-      name { volume }
-      type { int }
-      default { 100 }
-      desc { The volume of the audio output. range 0-100 }
-      attributes { A_GLOBAL }
-    }
-
-    defstate {
-      name { balance }
-      type { int }
-      default { 50 }
-      desc { Balance of audio output. range 0-100 }
-      attributes { A_GLOBAL }
-    }
-
-    defstate {
-      name { outputPort }
-      type { string }
-      default { line_out }
-      desc { Audio input port: line_out or speaker. }
-      attributes { A_GLOBAL }    
-    }
-
     constructor {
       blockSize.setAttributes(A_NONCONSTANT|A_NONSETTABLE);
       blockSize.setInitValue(8180);
@@ -92,109 +52,25 @@ provisions.
       int standardOutput:1;
     }
 
-    codeblock (outDeclarations) {
-	int $starSymbol(counter);
-    }
-
-    codeblock (globals) {
-	int $starSymbol(ctlfile);    
-    }
-
-    codeblock (set_parametersDef) {
-      /* The following function is used to set the */
-      /* audioctl device to the proper recording   */
-      /* format					   */
-
-      static void $starSymbol(set_parameters)()
-      {
-	 audio_info_t info;
-	 int encoding_val;
-	 int output_val;
-	 AUDIO_INITINFO(&info);
-
-	 if ($ref(encodingType) == "linear")
-	   encoding_val = 3;	// Linear
-	 else
-	   encoding_val = 1;	// U_law
-				
-	 if ($ref(outputPort) == "line_out")
-	   output_val = 0x04; 	// Line_Out
-	 else
-	   output_val = 0x01;	// Speaker
-
-	 ioctl($starSymbol(ctlfile), AUDIO_SETINFO, (caddr_t)(&info));	 
-
-	 info.play.encoding = encoding_val;
-
-	 ioctl($starSymbol(ctlfile), AUDIO_SETINFO, (caddr_t)(&info));
-
-	 info.play.precision =8;
-
-	 ioctl($starSymbol(ctlfile), AUDIO_SETINFO, (caddr_t)(&info));
-
-	 info.play.balance = AUDIO_MID_BALANCE*$ref(balance)/50;
-	 info.play.channels = 2;
-	 info.play.port = output_val;
-	 info.play.sample_rate = $ref(sampleRate);
-	 info.play.gain = AUDIO_MAX_GAIN*$ref(volume)/100;
-
-	 ioctl($starSymbol(ctlfile), AUDIO_SETINFO, (caddr_t)(&info));
-
-	 info.play.precision =$ref(precision);
-	 info.play.sample_rate = $ref(sampleRate);
-
-	 ioctl($starSymbol(ctlfile), AUDIO_SETINFO, (caddr_t)(&info));
-      }
-    }
-
-    codeblock (openCrtlfile) {
-      /* Open control device */
-      $starSymbol(ctlfile) = open("/dev/audioctl",O_RDWR, 0666);
-      if ($starSymbol(ctlfile) == -1) {
-	perror("Error in opening audio control device: /dev/audioctl");
-	exit(1);
-      }
-    }
-
     codeblock (convert) {
-      /* Take data from Input and put it in buffer */
-      /* Data in buffer is alternate left and right channels */
-      for ($starSymbol(counter)=0; $starSymbol(counter) <($val(blockSize)/2); 
-	   $starSymbol(counter) = $starSymbol(counter)+2) {
-
-	$starSymbol(buf)[$starSymbol(counter)] = 
-	  ceil($ref(left,$starSymbol(counter)/2)*32768.0);
-	$starSymbol(buf)[$starSymbol(counter)+1] = 
-	  ceil($ref(right,$starSymbol(counter)/2)*32768.0);
-
-      }
-    }
-
-    codeblock (closeCrtlfile) {
-      /* Close control device */
-      if (close($starSymbol(ctlfile)) != 0) {
-	perror("Error closing audio control device: /dev/audioctl");
-	exit(1);
-      }
+	/* Take data from Input and put it in buffer */
+	/* Data in buffer is alternate left and right channels */
+	for ($starSymbol(counter) = 0; $starSymbol(counter) < ($val(blockSize)/2); $starSymbol(counter) += 2) {
+		$starSymbol(buf)[$starSymbol(counter)] = 
+			ceil($ref(left,$starSymbol(counter)/2)*32768.0);
+		$starSymbol(buf)[$starSymbol(counter)+1] = 
+			ceil($ref(right,$starSymbol(counter)/2)*32768.0);
+	}
     }
 
     setup {
-      CGCAudioBase::setup();
+      CGCStereoBase::setup();
       left.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
       right.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
     }
       
     initCode {
-      addInclude("<stdio.h>");
-      addInclude("<unistd.h>");
-      addInclude("<sys/ioctl.h>");
-      addInclude("<math.h>");
-      addInclude("<sys/audioio.h>");
-      CGCAudioBase::initCode();
-      addDeclaration(declarations("short", int(blockSize)/2));
-      addDeclaration(outDeclarations);
-      addGlobal(globals);
-
+      CGCStereoBase::initCode();
       if (standardIO) {
 	addCode(noOpen);
       }
@@ -202,21 +78,24 @@ provisions.
 	addInclude("<fcntl.h>");            // Define O_WRONLY
 	addCode(openFileForWriting);
       }
-
       addCode(openCrtlfile);
-      addProcedure(set_parametersDef);
-      
+
       /* Update parameters */
-      addCode("$starSymbol(set_parameters) ();\n");
+      StringList updateParameters = "$starSymbol(set_parameters)";
+      updateParameters << "($starSymbol(ctlfile), "
+		       << "\"" << encodingType << "\", "
+		       << "\"" << outputPort << "\", "
+		       << "0);\n";
+      addCode(updateParameters);
     }
-    
+
     go {
       addCode(convert);
       addCode(write);
     }
     
     wrapup {
-      CGCAudioBase::wrapup();
+      CGCStereoBase::wrapup();
       addCode(closeCrtlfile);
     }
     
