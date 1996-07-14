94dc02cd9a786d80e2880d7b2ae0e3b1c5ad062c
sunil <sunil@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-14 16:55:09 +0000

    scale changes for volume and balance from 0-100 to 0-10 & -10to100resp

--- trunk/ptclassic/src/domains/cgc/tcltk/stars/CGCTkStereoOut.pl
+++ trunk/ptclassic/src/domains/cgc/tcltk/stars/CGCTkStereoOut.pl
@@ -5,7 +5,7 @@ defstar
     derivedFrom { StereoOut }
     desc {
 Just like StereoOut, except that a Tk slider is put in the master
-control panel to control the volume.
+control panel to control the volume and balance.
     }
     version { $Id$ }
     author { Sunil Bhave }
@@ -18,21 +18,32 @@ limitation of liability, and disclaimer of warranty provisions.
     }
 
     codeblock (tkSetup) {
-        /* Establish the Tk window for setting the value */
+      /* Establish the Tk window for setting the value    */
+      /* "position" is  a local variable which scales the */
+      /* volume from (0 - 10) to (0 - 100) for makeScale  */ 
+      {
+	int position;
+	position = $val(volume)*10;
         makeScale(".high",
 		  "$starSymbol(scale1)",
 		  "Volume control",
-		  $val(volume),
+		  position,
                   $starSymbol(setVolume));
 	displaySliderValue(".high", "$starSymbol(scale1)",
-			   "$val(volume)%");
+			   "$val(volume)");
+      }
+      /* "tkbalance" is  a local variable which scales the    */
+      /* balance from (-10 to 10) to (0 to 100) for makeScale */ 
+      {
+	int tkbalance;
+	tkbalance = 5*$val(balance)+50;
 	makeScale(".low",
 		  "$starSymbol(scale2)",
 		  "Balance control",
-		  $val(balance),
+		  tkbalance,
                   $starSymbol(setBalance));
-	displaySliderValue(".low", "$starSymbol(scale2)", "  $val(balance)%");
-
+	displaySliderValue(".low", "$starSymbol(scale2)", "  $val(balance)");
+      }
     }
     codeblock (setVolumeDef) {
         static int $starSymbol(setVolume)(dummy, interp, argc, argv)
@@ -41,20 +52,21 @@ limitation of liability, and disclaimer of warranty provisions.
             int argc;                           /* Number of arguments. */
             char **argv;                        /* Argument strings. */
         {
+	    audio_info_t info;
 	    static char buf[20];
-            if(sscanf(argv[1], "%d", &$ref(volume)) != 1) {
+	    int position;
+            if(sscanf(argv[1], "%d", &position) != 1) {
                 errorReport("Invalid volume");
                 return TCL_ERROR;
             }
-	    /* set_parameters function defined in base class */
-	    $sharedSymbol(CGCStereoBase,set_parameters)
-	      ($starSymbol(ctlfile),
-	       "$val(encodingType)",
-	       "$val(outputPort)",
-	       $ref (volume),
-	       $ref (balance),
-	       0);    
-	    sprintf(buf, "%5d%%", $ref(volume));
+	    /* conveying the value changes of volume to audioctl */
+	    $ref(volume) = (int) position/10;
+	    AUDIO_INITINFO(&info);
+	    ioctl($starSymbol(ctlfile), AUDIO_GETINFO,(caddr_t)(&info));
+	    info.play.gain = AUDIO_MAX_GAIN*$ref(volume)/10;
+	    ioctl($starSymbol(ctlfile), AUDIO_SETINFO, (caddr_t)(&info));
+
+	    sprintf(buf, "%d", $ref(volume));
 	    displaySliderValue(".high", "$starSymbol(scale1)", buf);
             return TCL_OK;
         }
@@ -67,27 +79,25 @@ limitation of liability, and disclaimer of warranty provisions.
             int argc;                           /* Number of arguments. */
             char **argv;                        /* Argument strings. */
         {
+	    audio_info_t info;
 	    static char buf[20];
-            if(sscanf(argv[1], "%d", &$ref(balance)) != 1) {
+	    int tkbalance;
+            if(sscanf(argv[1], "%d", &tkbalance) != 1) {
                 errorReport("Invalid balance");
                 return TCL_ERROR;
             }
-	    /* set_parameters function defined in base class */
-	    $sharedSymbol(CGCStereoBase,set_parameters)
-	      ($starSymbol(ctlfile),
-	       "$val(encodingType)",
-	       "$val(outputPort)",
-	       $ref (volume),
-	       $ref (balance),
-	       0);   
-	    sprintf(buf, "%5d%%", $ref(balance));
+	    $ref(balance) = ((int)tkbalance/5) - 10;
+	    /* conveying the value changes of balance to audioctl */
+	    AUDIO_INITINFO(&info);
+	    ioctl($starSymbol(ctlfile), AUDIO_GETINFO, (caddr_t)(&info));
+	    info.play.balance = AUDIO_MID_BALANCE*($ref(balance)+10)/10;
+	    ioctl($starSymbol(ctlfile), AUDIO_SETINFO, (caddr_t)(&info));
+	    
+	    sprintf(buf, "%d", $ref(balance));
 	    displaySliderValue(".low", "$starSymbol(scale2)", buf);
             return TCL_OK;
         }
     }
-
-
-
 
     initCode {
 	CGCStereoOut :: initCode();
