e90bcf716bf14f26b8a649ee0eacfafa3776bf94
williamc <williamc@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-16 00:44:50 +0000

    added comments

--- trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISFFTCx.pl
+++ trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISFFTCx.pl
@@ -12,8 +12,22 @@ limitation of liability, and disclaimer of warranty provisions.
 	}
 	location { CGC Visual Instruction Set library }
 	desc { 
-A single complex sequence FFT using radix 2.
-Input length must be power of two.
+A radix-2 FFT of a complex input.  The radix-2 decimation-in-time 
+decomposes the overall FFT operation into a series of smaller FFT 
+operations.  The smallest operation is the "FFT butterfly" which consists 
+of a single addition and subtraction.  Graphically, the full
+decomposition can be viewed as N stages of FFT butterflies with twiddle
+factors between each of the stages.  One standard implementation is to use 
+three nested for loops to calculate the FFT.  The innermost loop calculates 
+all the butterflies and performs twiddle factor multiplcations within a particular stage; 
+the next outer loop calculates the twiddle factors; and the outermost loop steps 
+through all the stages.  In order to take advantage of the 16-bit partitioned 
+multiplications and additions, the basic operation of the VIS FFT is actually doing 
+four "FFT butterflies" at once.  The implementation is similar to the standard 
+three nested for loops, but the last two stages are separated out. In order 
+to avoid packing and unpacking, the basic operation of the last two stages 
+switches from four to two to eventually just one "FFT butterfly".  After the FFT 
+is taken, the order of the sequence is bit-reversed.
 	}
 	input {
 	  name { realIn }
@@ -65,7 +79,8 @@ Input length must be power of two.
 	}
 
 	codeblock(calculateSinandCos) {
-void $sharedSymbol(CGCVISFFTCx,calcTwSinCos)(vis_s16 *sinsarray, vis_s16 *cossarray, int order, int size) {
+void $sharedSymbol(CGCVISFFTCx,calcTwSinCos)
+                  (vis_s16 *sinsarray,vis_s16 *cossarray,int order,int size){
 	vis_d64 twopioverN2;
 	vis_s16* indexcounts0 = cossarray;
 	vis_s16* indexcounts1 = sinsarray;
@@ -85,7 +100,7 @@ void $sharedSymbol(CGCVISFFTCx,calcTwSinCos)(vis_s16 *sinsarray, vis_s16 *cossar
 }
 	}
 	codeblock(quadmult) {
-vis_d64 $sharedSymbol(CGCVISFFTCx,mult4x4)(vis_d64 mult1,vis_d64 mult2) { 
+vis_d64 $sharedSymbol(CGCVISFFTCx,mult4x4d)(vis_d64 mult1,vis_d64 mult2) { 
 	vis_d64 prodhihi, prodhilo, prodlohi, prodlolo;
 	vis_d64 prodhi, prodlo, product;
 	vis_f32 packhi, packlo;
@@ -129,8 +144,8 @@ void $sharedSymbol(CGCVISFFTFx,reorderfft)(vis_s16* s1, vis_s16* s2, int nn) {
 	j = 1;
 	for (i = 1; i < n; i += 2){
 	  if (j > i) {
-	    SWAP(s1[(j-1)/2], s1[(i-1)/2], temppr);
-	    SWAP(s2[(j-1)/2], s2[(i-1)/2], temppr);
+	    SWAP(s1[(j-1)/2], s1[(i-1)/2], tempr);
+	    SWAP(s2[(j-1)/2], s2[(i-1)/2], tempr);
 	  }
 	  m = n >> 1;
 	  while(m >= 2 && j > m){
@@ -147,15 +162,19 @@ void $sharedSymbol(CGCVISFFTFx,reorderfft)(vis_s16* s1, vis_s16* s2, int nn) {
 	}
 
 	codeblock(mainDecl) {
-	  vis_d64 *$starSymbol(rein) = (vis_d64 *) memalign(sizeof(vis_d64),sizeof(vis_d64)*$val(sizeoffft)/4);
-	  vis_d64 *$starSymbol(imin) = (vis_d64 *) memalign(sizeof(vis_d64),sizeof(vis_d64)*$val(sizeoffft)/4);
-	  vis_s16 *$starSymbol(cossarray) = (vis_s16 *) memalign(sizeof(vis_d64),sizeof(vis_s16)*($val(sizeoffft)-4));
-	  vis_s16 *$starSymbol(sinsarray) = (vis_s16 *) memalign(sizeof(vis_d64),sizeof(vis_s16)*($val(sizeoffft)-4));
-	  int     $starSymbol(genindex);
+	  vis_d64 *$starSymbol(rein) = (vis_d64 *)
+	    memalign(sizeof(vis_d64),sizeof(vis_d64)*$val(sizeoffft)/4);
+	  vis_d64 *$starSymbol(imin) = (vis_d64 *)
+	    memalign(sizeof(vis_d64),sizeof(vis_d64)*$val(sizeoffft)/4);
+	  vis_s16 *$starSymbol(cossarray) = (vis_s16 *)
+	    memalign(sizeof(vis_d64),sizeof(vis_s16)*($val(sizeoffft)-4));
+	  vis_s16 *$starSymbol(sinsarray) = (vis_s16 *)
+	    memalign(sizeof(vis_d64),sizeof(vis_s16)*($val(sizeoffft)-4));
 	}
 	codeblock(initialize) {
-	  vis_write_gsr(8);
-	  $sharedSymbol(CGCVISFFTCx,calcTwSinCos)($starSymbol(sinsarray),$starSymbol(cossarray),$val(orderfft),$val(sizeoffft));
+	  /* pre-calculate all the twiddle factors */
+	  $sharedSymbol(CGCVISFFTCx,calcTwSinCos)
+	    ($starSymbol(sinsarray),$starSymbol(cossarray),$val(orderfft),$val(sizeoffft));
 	}
 	initCode {
 	  CGCVISBase::initCode();
@@ -174,10 +193,12 @@ void $sharedSymbol(CGCVISFFTFx,reorderfft)(vis_s16* s1, vis_s16* s2, int nn) {
 	  vis_d64 reindtmp,imagindtmp,xtcd,xtsd,ytsd,ytcd,t0,t1,*twC,*twS;
 	  vis_f32 reinftmp,imaginftmp,xtcf,ytsf,xtsf,ytcf;
 	  vis_f32 *Cf,*Sf,*splitf_rein,*splitf_imin,C0,S0;
-	  int     N,N1,N2,i,j,k,l;
+	  int     N,N1,N2,i,j,k,l,genindex;
 	  vis_s16 reinstmp,imaginstmp,*splits_rein,*splits_imin;
 	}
 	codeblock(fft) {  
+	  vis_write_gsr(8);
+
 	  splitf_rein = (vis_f32*)$starSymbol(rein);
 	  splitf_imin = (vis_f32*)$starSymbol(imin);
 	  splits_rein = (vis_s16*)$starSymbol(rein);
@@ -185,9 +206,9 @@ void $sharedSymbol(CGCVISFFTFx,reorderfft)(vis_s16* s1, vis_s16* s2, int nn) {
 	  twC = (vis_d64*) $starSymbol(cossarray);
 	  twS = (vis_d64*) $starSymbol(sinsarray);
 	  /*read in the input*/
-	  for($starSymbol(genindex)=0;$starSymbol(genindex)<$val(sizeoffft)/4;$starSymbol(genindex)++){
-	    $starSymbol(rein)[$starSymbol(genindex)] = (double) $ref2(realIn,$starSymbol(genindex));
-	    $starSymbol(imin)[$starSymbol(genindex)] = (double) $ref2(imagIn,$starSymbol(genindex));
+	  for(genindex=0;genindex<$val(sizeoffft)/4;genindex++){
+	    $starSymbol(rein)[genindex] = (double)$ref2(realIn,genindex);
+	    $starSymbol(imin)[genindex] = (double)$ref2(imagIn,genindex);
 	  }
 	  /*
 	   * first stages of fft (order of fft minus
@@ -204,15 +225,15 @@ void $sharedSymbol(CGCVISFFTFx,reorderfft)(vis_s16* s1, vis_s16* s2, int nn) {
 	      for(i = j;i < N/4;i += N1/4){
 		l = i + N2/4;
 		reindtmp = vis_fpsub16($starSymbol(rein)[i], $starSymbol(rein)[l]);
-		$starSymbol(rein)[i] = vis_fpadd16($starSymbol(rein)[i], $starSymbol(rein)[l]);
+		$starSymbol(rein)[i] = vis_fpadd16($starSymbol(rein)[i],$starSymbol(rein)[l]);
 		imagindtmp = vis_fpsub16($starSymbol(imin)[i], $starSymbol(imin)[l]);
-		$starSymbol(imin)[i] = vis_fpadd16($starSymbol(imin)[i], $starSymbol(imin)[l]);
-		xtcd = $sharedSymbol(CGCVISFFTCx,mult4x4)(reindtmp,t0);
-		xtsd = $sharedSymbol(CGCVISFFTCx,mult4x4)(reindtmp,t1);
-		ytsd = $sharedSymbol(CGCVISFFTCx,mult4x4)(imagindtmp,t1);
-		ytcd = $sharedSymbol(CGCVISFFTCx,mult4x4)(imagindtmp,t0);
-		$starSymbol(rein)[l] = vis_fpsub16(xtcd, ytsd);
-		$starSymbol(imin)[l] = vis_fpadd16(xtsd, ytcd);
+		$starSymbol(imin)[i] = vis_fpadd16($starSymbol(imin)[i],$starSymbol(imin)[l]);
+		xtcd = $sharedSymbol(CGCVISFFTCx,mult4x4d)(reindtmp,t0);
+		xtsd = $sharedSymbol(CGCVISFFTCx,mult4x4d)(reindtmp,t1);
+		ytsd = $sharedSymbol(CGCVISFFTCx,mult4x4d)(imagindtmp,t1);
+		ytcd = $sharedSymbol(CGCVISFFTCx,mult4x4d)(imagindtmp,t0);
+		$starSymbol(rein)[l] = vis_fpsub16(xtcd,ytsd);
+		$starSymbol(imin)[l] = vis_fpadd16(xtsd,ytcd);
 	      }
 	    }
 	  }
@@ -244,6 +265,7 @@ void $sharedSymbol(CGCVISFFTFx,reorderfft)(vis_s16* s1, vis_s16* s2, int nn) {
 	    splits_imin[i+1] = imaginstmp;
 	    i += 2;
 	  }	
+	  /*reorder the outputs*/
 	  $sharedSymbol(CGCVISFFTFx,reorderfft)(splits_rein,splits_imin,$val(sizeoffft));
 	  /*output the results*/
 	  for(i=0;i<$val(sizeoffft)/4;i++){
