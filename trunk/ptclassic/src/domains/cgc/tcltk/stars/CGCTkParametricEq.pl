f8168b8e0367f9cbdccff9a2dc3c051f777d01d5
sunil <sunil@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-17 14:42:19 +0000

    made sliders optional for different types of filters, also added nameofStar state, to print alongside the sliders.

--- trunk/ptclassic/src/domains/cgc/tcltk/stars/CGCTkParametricEq.pl
+++ trunk/ptclassic/src/domains/cgc/tcltk/stars/CGCTkParametricEq.pl
@@ -18,75 +18,90 @@ limitation of liability, and disclaimer of warranty provisions.
     }
 
     defstate {
-      name { highcenterFreq }
+      name { highFreq }
       type { float }
-      default { "20000" }
+      default { 20000.0 }
       desc { The highest possible center frequency. }
     }
     
     defstate {
-      name {lowcenterFreq}
+      name { lowFreq }
       type { float }
-      default { "1" }
-      desc { the lowest possible center frequency. }
+      default { 1.0 }
+      desc { The lowest possible center frequency. }
+    }
+    
+    defstate {
+      name { nameofStar }
+      type { string }
+      default { "left1" }
+      desc { A token to distinguish between multiple instances of same
+	       star in a universe.}
     }
 
     codeblock (tkSetup) {
-      /* Establish the Tk window for setting the value    */
+      /* Establish the Tk window for setting the values    */
       /* "tkgain" is  a local variable which scales the    */
       /* gain from (-10 to 10) to (0 to 100) for makeScale */ 
       {
 	int tkgain;
 	tkgain = (int) 5*$val(gain)+50;
 	makeScale(".low",
-		  "$starSymbol(scale2)",
-		  "Gain control",
+		  "$starSymbol(scale1)",
+		  "Gain control: $val(nameofStar)",
 		  tkgain,
                   $starSymbol(setGain));
-	displaySliderValue(".low", "$starSymbol(scale2)", "  $val(gain)");
+	displaySliderValue(".low", "$starSymbol(scale1)", " $val(gain)");
       }
-      /* "position" is  a local variable which scales the    */
-      /* bandwidth from (0.1 to 4) to (0 to 100) for makeScale */ 
+
+      /* "tkcenter" is a local variable which scale the center  */
+      /* freq from (lowFreq to highFreq) to 0-100 */
       {
-	int position;
-	position = (int) ($val(bandwidth)-0.01)*100/3.99;
-        makeScale(".high",
-		  "$starSymbol(scale1)",
-		  "Bandwidth control",
-		  position,
-                  $starSymbol(setBandwidth));
-	displaySliderValue(".high", "$starSymbol(scale1)",
-			   "$val(bandwidth)");
-      }      
-      /* "tkfreq" is a local variable which scale the center  */
-      /* freq from (lowcenterFreq to highcenterFreq) to 0-100 */
-      {
-	int tkfreq;
-	tkfreq = (int)
-	  100*($val(centerFreq)-$val(lowcenterFreq))/
-	  ($val(highcenterFreq) - $val(lowcenterFreq));
+	int tkcenter;
+	tkcenter = (int)
+	  100*($val(centerFreq)-$val(lowFreq))/
+	  ($val(highFreq) - $val(lowFreq));
 	makeScale(".middle",
-		  "$starSymbol(scale3)",
-		  "Center Frequency control",
-		  tkfreq,
+		  "$starSymbol(scale2)",
+		  "Center/Cutoff Freq crtl:$val(nameofStar) ",
+		  tkcenter,
                   $starSymbol(setcenterFreq));
-	displaySliderValue(".middle", "$starSymbol(scale3)",
+	displaySliderValue(".middle", "$starSymbol(scale2)",
 			   "$val(centerFreq)");
       }
-      {	
-	int tkpass;
-	tkpass = (int)
-	  100*($val(passFreq)-$val(lowcenterFreq))/
-	  ($val(highcenterFreq) - $val(lowcenterFreq));
-	makeScale(".middle",
-		  "$starSymbol(scale4)",
-		  "Pass Frequency control",
-		  tkpass,
-		  $starSymbol(setpassFreq));
-	displaySliderValue(".middle", "$starSymbol(scale4)",
-			   "$val(passFreq)");
+
+      if (strcasecmp("$val(filtertype)", "BAND") == 0) {
+	/* "tkband" is  a local variable which scales the    */
+	/* bandwidth from (0.1 to 4) to (0 to 100) for makeScale */ 
+	{
+	  int tkband;
+	  tkband = (int) ($val(bandwidth)-0.01)*100/3.99;
+	  makeScale(".high",
+		    "$starSymbol(scale3)",
+		    "Bandwidth control:$val(nameofStar)",
+		    tkband,
+		    $starSymbol(setBandwidth));
+	  displaySliderValue(".high", "$starSymbol(scale3)",
+			     "$val(bandwidth)");
+	}      
       }
-      
+      else {
+	/* "tkpass" is a local variable which scale the pass  */
+	/* freq from (lowFreq to highFreq) to 0-100 */
+	{	
+	  int tkpass;
+	  tkpass = (int)
+	    100*($val(passFreq)-$val(lowFreq))/
+	    ($val(highFreq) - $val(lowFreq));
+	  makeScale(".middle",
+		    "$starSymbol(scale4)",
+		    "Pass Frequency control:$val(nameofStar)",
+		    tkpass,
+		    $starSymbol(setpassFreq));
+	  displaySliderValue(".middle", "$starSymbol(scale4)",
+			     "$val(passFreq)");
+	}
+      }
     }   
 
     codeblock (setGainDef) {
@@ -123,42 +138,11 @@ limitation of liability, and disclaimer of warranty provisions.
 	       $starSymbol(filtertaps));  
 
 	    sprintf(buf, "%.2f", $ref(gain));
-	    displaySliderValue(".low", "$starSymbol(scale2)", buf);
+	    displaySliderValue(".low", "$starSymbol(scale1)", buf);
             return TCL_OK;
         }
     }
-    
-    codeblock (setBandwidthDef) {
-        static int $starSymbol(setBandwidth)(dummy, interp, argc, argv)
-            ClientData dummy;                   /* Not used. */
-            Tcl_Interp *interp;                 /* Current interpreter. */
-            int argc;                           /* Number of arguments. */
-            char **argv;                        /* Argument strings. */
-        {
-	    static char buf[20];
-	    int position;
-            if(sscanf(argv[1], "%d", &position) != 1) {
-                errorReport("Invalid bandwidth");
-                return TCL_ERROR;
-            }
-	    /* conveying the value changes of bandwidth to audio */
-	    /* only for bandpass filters, else inactive          */
-	    $ref(bandwidth) = position*0.0399 + 0.01;
-	    	    
-	    if (strcasecmp("$val(filtertype)", "BAND") == 0){
-	    $starSymbol(setparams)(&$starSymbol(parametric));
-	    $sharedSymbol(CGCParamBiquad,bandpass)(&$starSymbol(parametric),
-				    $starSymbol(filtercoeff));
-	    $sharedSymbol(CGCParamBiquad,setfiltertaps)
-	      (&$starSymbol(parametric), $starSymbol(filtercoeff),
-	       $starSymbol(filtertaps));  	    
-	    }
 
-	    sprintf(buf, "%.4f", $ref(bandwidth));
-	    displaySliderValue(".high", "$starSymbol(scale1)", buf);
-            return TCL_OK;
-        }
-    }
     codeblock (setcenterFreqDef) {
         static int $starSymbol(setcenterFreq)(dummy, interp, argc, argv)
             ClientData dummy;                   /* Not used. */
@@ -167,15 +151,15 @@ limitation of liability, and disclaimer of warranty provisions.
             char **argv;                        /* Argument strings. */
         {
 	    static char buf[20];
-	    int tkfreq;
-            if(sscanf(argv[1], "%d", &tkfreq) != 1) {
+	    int tkcenter;
+            if(sscanf(argv[1], "%d", &tkcenter) != 1) {
                 errorReport("Invalid center freq");
                 return TCL_ERROR;
             }
 	    /* set the new center frequency  */
 	    $ref(centerFreq) = 
-	      tkfreq*($val(highcenterFreq) - $val(lowcenterFreq))/100 
-	      + $val(lowcenterFreq);
+	      tkcenter*($val(highFreq) - $val(lowFreq))/100 
+	      + $val(lowFreq);
 	    
 	    $starSymbol(setparams)(&$starSymbol(parametric));
 	    if (strcasecmp("$val(filtertype)", "LOW") == 0){
@@ -195,10 +179,43 @@ limitation of liability, and disclaimer of warranty provisions.
 	       $starSymbol(filtertaps));  
 
 	    sprintf(buf, "%.2f", $ref(centerFreq));
-	    displaySliderValue(".middle", "$starSymbol(scale3)", buf);
+	    displaySliderValue(".middle", "$starSymbol(scale2)", buf);
             return TCL_OK;
         }
     }
+    
+    codeblock (setBandwidthDef) {
+        static int $starSymbol(setBandwidth)(dummy, interp, argc, argv)
+            ClientData dummy;                   /* Not used. */
+            Tcl_Interp *interp;                 /* Current interpreter. */
+            int argc;                           /* Number of arguments. */
+            char **argv;                        /* Argument strings. */
+        {
+	    static char buf[20];
+	    int tkband;
+            if(sscanf(argv[1], "%d", &tkband) != 1) {
+                errorReport("Invalid bandwidth");
+                return TCL_ERROR;
+            }
+	    /* conveying the value changes of bandwidth to audio */
+	    /* only for bandpass filters, else inactive          */
+	    $ref(bandwidth) = tkband*0.0399 + 0.01;
+	    	    
+	    if (strcasecmp("$val(filtertype)", "BAND") == 0){
+	    $starSymbol(setparams)(&$starSymbol(parametric));
+	    $sharedSymbol(CGCParamBiquad,bandpass)(&$starSymbol(parametric),
+				    $starSymbol(filtercoeff));
+	    $sharedSymbol(CGCParamBiquad,setfiltertaps)
+	      (&$starSymbol(parametric), $starSymbol(filtercoeff),
+	       $starSymbol(filtertaps));  	    
+	    }
+
+	    sprintf(buf, "%.4f", $ref(bandwidth));
+	    displaySliderValue(".high", "$starSymbol(scale3)", buf);
+            return TCL_OK;
+        }
+    }
+
     codeblock (setpassFreqDef) {
         static int $starSymbol(setpassFreq)(dummy, interp, argc, argv)
             ClientData dummy;                   /* Not used. */
@@ -214,8 +231,8 @@ limitation of liability, and disclaimer of warranty provisions.
             }
 	    /* set the new pass frequency  */
 	    $ref(passFreq) = 
-	      tkpass*($val(highcenterFreq) - $val(lowcenterFreq))/100 
-	      + $val(lowcenterFreq);
+	      tkpass*($val(highFreq) - $val(lowFreq))/100 
+	      + $val(lowFreq);
 	    
 	    $starSymbol(setparams)(&$starSymbol(parametric));
 	    if (strcasecmp("$val(filtertype)", "LOW") == 0){
@@ -240,9 +257,8 @@ limitation of liability, and disclaimer of warranty provisions.
 	CGCParamBiquad :: initCode();
 	addCode(tkSetup, "tkSetup");
         addCode(setGainDef, "procedure");
-        addCode(setBandwidthDef, "procedure");
 	addCode(setcenterFreqDef, "procedure");
+	addCode(setBandwidthDef, "procedure");
 	addCode(setpassFreqDef, "procedure");
-
     }
 }
