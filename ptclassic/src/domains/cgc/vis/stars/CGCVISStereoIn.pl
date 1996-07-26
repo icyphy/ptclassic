740810f1458c94a5629bb45bef729c9fd7fb76cf
sunil <sunil@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-26 18:02:59 +0000

    derived from CGCAudioBase

--- ptclassic/src/domains/cgc/vis/stars/CGCVISStereoIn.pl
+++ ptclassic/src/domains/cgc/vis/stars/CGCVISStereoIn.pl
@@ -1,7 +1,7 @@
 defstar {
     name { VISStereoIn }
     domain { CGC }
-    derivedFrom { StereoBase }
+    derivedFrom { AudioBase }
     descriptor {
 Reads Compact Disc audio format from a file given by "fileName". The file
 can be the audio port /dev/audio, if supported by the workstation.  The
@@ -13,7 +13,7 @@ This code is based on the description of the audio driver which can
 be obtained by looking at the man page for audio.
     }
     version { $Id$ }
-    author { Sunil Bhave }
+    author { Sunil Bhave and Bill Chen}
     copyright {
 Copyright (c) 1990-1996 The Regents of the University 
 of California. All rights reserved.
@@ -24,59 +24,149 @@ provisions.
     location { CGC vis library }
 
     output {
-      name { leftright }
+      name { left }
       type { float }
-      desc { Left and Right channel output }
+      desc { Left channel output }
+    }
+
+    output {
+      name { right }
+      type { float }
+      desc { Right channel output }
     }
 	
     defstate {
-       name { inputPort }
-       type { string }
-       default { line_in }
-       desc { Audio input port: line_in or internal_cd. }
-       attributes { A_GLOBAL }
+      name { interleave }
+      type { int }
+      default { FALSE }
+      desc {
+TRUE outputs interleaved audio samples through the left output;
+FALSE outputs separate audio samples through both the left and right outputs.}
+      attributes { A_CONSTANT|A_SETTABLE }
+	}
+
+    constructor {
+      encodingType.setAttributes(A_NONCONSTANT|A_NONSETTABLE);
+      encodingType.setInitValue("linear16");
+      channels.setAttributes(A_NONCONSTANT|A_NONSETTABLE);
+      channels.setInitValue(2);
     }
 
-    codeblock (convert) {
-      /* Convert data in buffer to Output format */
-      for (i=0;i<($val(blockSize)/2);i++) {
-	$ref(leftright,$val(blockSize)/2-1-i) = $starSymbol(buf)[i] /32768.0;
+    setup {
+
+      if (interleave) {
+	left.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
+      }
+      else {
+	left.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
+	right.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
       }
     }
-    
-    setup {
-      CGCStereoBase::setup();
-      leftright.setSDFParams(int(blockSize/2), int(blockSize/2)-1);
+
+    codeblock(globalDecl){
+      union $sharedSymbol(CGCVISStereoIn,regoverlay) {
+	vis_d64 regvaluedbl;
+	vis_s16 regvaluesh[4];
+      };
+    }
+
+    codeblock(mainDecl){
+      union $sharedSymbol(CGCVISStereoIn,regoverlay) $starSymbol(packright);
+      union $sharedSymbol(CGCVISStereoIn,regoverlay) $starSymbol(packleft);
     }
 
     initCode {
-      CGCStereoBase::initCode();
-      if (standardIO) {
-	addCode(noOpen);
+      CGCAudioBase::initCode();
+      addInclude("<vis_types.h>"); 
+      addGlobal(globalDecl,"CGCVISStereoIn_regoverlay");
+      addDeclaration(mainDecl);
+      if (interleave) {
+	addDeclaration(declarations("short", int(blockSize)));
       }
-      else {
-        addInclude("<fcntl.h>");            // Define O_RDONLY
-	addCode(openFileForReading);
+      else{
+	/* Declare "buffer" to be of type short and blockSize/2 bytes */
+	addDeclaration(declarations("short", int(blockSize*2)));
       }
+      /* Open file for reading data */
+      addCode(openFileForReading);	
+      /* Update audio_control */
+      StringList setupParameters = "$sharedSymbol(CGCAudioBase,audio_setup)";
+      setupParameters  << "($starSymbol(file),"
+		       << "\"" << encodingType << "\","
+		       <<  sampleRate << "," 
+		       <<  channels << ");\n";
+      addCode(setupParameters);
+      StringList controlParameters =  "$sharedSymbol(CGCAudioBase,audio_control)";
+      controlParameters     << "($starSymbol(file),"
+		       << "\"" << encodingType << "\","
+		       <<  volume << "," 
+		       <<  balance << ",1);\n";
+      addCode(controlParameters);
+    }
 
-      /* Update parameters */
-      StringList updateParameters = "$sharedSymbol(CGCStereoBase,set_parameters)";
-      updateParameters << "($starSymbol(file), "
-		       << "\"" << inputPort << "\", "
-		       <<  volume << ", " 
-		       <<  balance << ", " 
-		       << "1);\n";
-      addCode(updateParameters);
+    codeblock (setbufptr) {
+      $starSymbol(bufferptr) = $starSymbol(buffer);
+    }
+
+    codeblock(updatebufptr){
+      $starSymbol(bufferptr) = $starSymbol(bufferptr)+$val(blockSize)/2;
+    }
+
+    codeblock (convert_separate) {
+      /* Convert data in buffer to Output format */
+      {
+	int i, j;
+	for (i=0; i <($val(blockSize)/4); i++) {
+	  j = 8*i;
+	  $starSymbol(packleft).regvaluesh[0] = $starSymbol(buffer)[j];
+	  $starSymbol(packright).regvaluesh[0] = $starSymbol(buffer)[j+1];
+	  $starSymbol(packleft).regvaluesh[1] = $starSymbol(buffer)[j+2];
+	  $starSymbol(packright).regvaluesh[1] = $starSymbol(buffer)[j+3];
+	  $starSymbol(packleft).regvaluesh[2] = $starSymbol(buffer)[j+4];
+	  $starSymbol(packright).regvaluesh[2] = $starSymbol(buffer)[j+5];
+	  $starSymbol(packleft).regvaluesh[3] = $starSymbol(buffer)[j+6];
+	  $starSymbol(packright).regvaluesh[3] = $starSymbol(buffer)[j+7];
+	  $ref(left,$val(blockSize)/4-1-i) = $starSymbol(packleft).regvaluedbl;
+	  $ref(right,$val(blockSize)/4-1-i) = $starSymbol(packright).regvaluedbl;
+	}
+      }
+    }
+
+    codeblock (convert_interleave) {
+      /* Convert data in buffer to Output format */
+      {
+	int i, j;
+	for (i=0; i <($val(blockSize)/4); i++) {
+	  j = 4*i;
+	  $starSymbol(packleft).regvaluesh[0] = $starSymbol(buffer)[j];
+	  $starSymbol(packleft).regvaluesh[1] = $starSymbol(buffer)[j+1];
+	  $starSymbol(packleft).regvaluesh[2] = $starSymbol(buffer)[j+2];
+	  $starSymbol(packleft).regvaluesh[3] = $starSymbol(buffer)[j+3];
+	  $ref(left,$val(blockSize)/4-1-i) = $starSymbol(packleft).regvaluedbl;
+	}
+      }
     }
 
     go {
-      addDeclaration("int i;");
+      addCode(setbufptr);
       addCode(read);
-      addCode(convert);
+      addCode(updatebufptr);
+      addCode(read);
+
+      if (interleave) {
+	addCode(convert_interleave);
+      }
+      else {
+	addCode(updatebufptr);
+	addCode(read);
+	addCode(updatebufptr);
+	addCode(read);
+	addCode(convert_separate);
+      }
     }
 
     wrapup {
-      CGCStereoBase::wrapup();
+      CGCAudioBase::wrapup();
     }
     
     exectime {
--- ptclassic/src/domains/cgc/vis/stars/CGCVISStereoOut.pl
+++ ptclassic/src/domains/cgc/vis/stars/CGCVISStereoOut.pl
@@ -1,7 +1,7 @@
 defstar {
     name { VISStereoOut }
     domain { CGC }
-    derivedFrom { StereoBase }
+    derivedFrom { AudioBase }
     descriptor {
 Writes Compact Disc audio format to a file given by "fileName".  The file
 can be the audio port /dev/audio, if supported by the workstation.  The
@@ -12,8 +12,8 @@ size should be a multiple of 4.
 This code is based on the description of the audio driver which can be
 obtained by looking at the man page of audio.
     }
-    version { @(#)CGCStereoOut.pl	1.10 07/15/96 }
-    author { Sunil Bhave }
+    version { $Id$ }
+    author { Sunil Bhave and Bill Chen}
     copyright {
 Copyright (c) 1990- The Regents of the University 
 of California. All rights reserved.
@@ -22,66 +22,154 @@ limitation of liability, and disclaimer of warranty
 provisions.
     }
     location { CGC main library }
-    
+   
     input {
-      name { leftright }
+      name { left }
       type { float }
-      desc { Left and Right channel input }
+      desc { Left channel input }
+    }
+
+    input {
+      name { right }
+      type { float }
+      desc { Right channel input }
     }
 
     defstate {
-      name { outputPort }
-      type { string }
-      default { line_out }
-      desc { Audio input port: line_out or speaker. }
-      attributes { A_GLOBAL }
-    }
-
-    protected {
-      int standardOutput:1;
-    }
-    codeblock (convert) {
-	/* Take data from Input and put it in buffer */
-	/* Data in buffer is alternate left and right channels */
-	for (i = 0; i < ($val(blockSize)/2); i ++) {
-	  $starSymbol(buf)[i] = ceil($ref(leftright,$val(blockSize)/2-1-i)*32768.0);	
+      name { interleave }
+      type { int }
+      default { FALSE }
+      desc {
+TRUE inputs interleaved audio samples through the left input;
+FALSE inputs separate audio samples through both the left and right inputs.}
+      attributes { A_CONSTANT|A_SETTABLE }
 	}
+
+    constructor {
+      encodingType.setAttributes(A_NONCONSTANT|A_NONSETTABLE);
+      encodingType.setInitValue("linear16");
+      channels.setAttributes(A_NONCONSTANT|A_NONSETTABLE);
+      channels.setInitValue(2);
     }
 
     setup {
-      CGCStereoBase::setup();
-      leftright.setSDFParams(int(blockSize/2), int(blockSize/2)-1);
-    }
-      
-    initCode {
-      CGCStereoBase::initCode();
-      if (standardIO) {
-	addCode(noOpen);
+      if (interleave) {
+	left.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
       }
       else {
-	addInclude("<fcntl.h>");            // Define O_WRONLY
-	addCode(openFileForWriting);
+	left.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
+	right.setSDFParams(int(blockSize/4), int(blockSize/4)-1);
       }
+    }
 
-      /* Update parameters */
-      StringList updateParameters = "$sharedSymbol(CGCStereoBase,set_parameters)";
-      updateParameters << "($starSymbol(file), "
-		       << "\"" << outputPort << "\", "
-		       <<  volume << ", " 
-		       <<  balance << ", " 
-		       << "0);\n";
-      addCode(updateParameters);
+    codeblock(globalDecl){
+      union $sharedSymbol(CGCVISStereoOut,regoverlay) {
+	vis_d64 regvaluedbl;
+	vis_s16 regvaluesh[4];
+      };
+    }
 
+    codeblock(mainDecl){
+      union $sharedSymbol(CGCVISStereoOut,regoverlay) $starSymbol(unpackright);
+      union $sharedSymbol(CGCVISStereoOut,regoverlay) $starSymbol(unpackleft);
+    } 
+    
+    initCode {
+      CGCAudioBase::initCode();
+      addInclude("<vis_types.h>"); 
+      addGlobal(globalDecl,"CGCVISStereoOut_regoverlay");
+      addDeclaration(mainDecl);
+      if (interleave) {
+	addDeclaration(declarations("short", int(blockSize)));
+      }
+      else{
+	/* Declare "buffer" to be of type short and blockSize/2 bytes */
+	addDeclaration(declarations("short", int(blockSize*2)));
+      }
+      /* Open file for writing data */
+      addCode(openFileForWriting);	
+      /* audio_setup : to set encodingType, sampleRate and channels */
+      StringList setupParameters = "$sharedSymbol(CGCAudioBase,audio_setup)";
+      setupParameters  << "($starSymbol(file), "
+		       << "\"" << encodingType << "\", "
+		       <<  sampleRate << ", " 
+		       <<  channels << ");\n";
+      addCode(setupParameters);
+      /* audio_control : to set portType, volume and balance */
+      StringList controlParameters = "$sharedSymbol(CGCAudioBase,audio_control)";
+      controlParameters << "($starSymbol(file), "
+			<< "\"" << portType << ",\" "
+			<<  volume << ",\" " 
+			<<  balance << ",\" "
+			<< "0);\n";
+      addCode(controlParameters);
+    }
+
+    codeblock (setbufptr) {
+      $starSymbol(bufferptr) = $starSymbol(buffer);
+    }
+
+    codeblock(updatebufptr){
+      $starSymbol(bufferptr) = $starSymbol(bufferptr)+$val(blockSize)/2;
+    }
+
+    codeblock (convert_separate) {
+      /* Convert data in buffer to Output format */
+      {
+	int i, j;
+
+	for (i=0; i <($val(blockSize)/4); i++) {
+	  j = 8*i;
+	  $starSymbol(unpackleft).regvaluedbl = $ref(left,$val(blockSize)/4-1-i);
+	  $starSymbol(unpackright).regvaluedbl = $ref(right,$val(blockSize)/4-1-i);
+
+	  $starSymbol(buffer)[j]   = $starSymbol(unpackleft).regvaluesh[0];
+	  $starSymbol(buffer)[j+1] = $starSymbol(unpackright).regvaluesh[0];
+	  $starSymbol(buffer)[j+2] = $starSymbol(unpackleft).regvaluesh[1];
+	  $starSymbol(buffer)[j+3] = $starSymbol(unpackright).regvaluesh[1];
+	  $starSymbol(buffer)[j+4] = $starSymbol(unpackleft).regvaluesh[2];
+	  $starSymbol(buffer)[j+5] = $starSymbol(unpackright).regvaluesh[2];
+	  $starSymbol(buffer)[j+6] = $starSymbol(unpackleft).regvaluesh[3];
+	  $starSymbol(buffer)[j+7] = $starSymbol(unpackright).regvaluesh[3];
+	}
+      }
+    }
+
+    codeblock (convert_interleave) {
+      /* Convert data in buffer to Output format */
+      {
+	int i, j;
+	for (i=0; i <($val(blockSize)/4); i++) {
+	  j = 4*i;
+	  $starSymbol(unpackleft).regvaluedbl = $ref(left,$val(blockSize)/4-1-i);
+
+	  $starSymbol(buffer)[j]   = $starSymbol(unpackleft).regvaluesh[0];
+	  $starSymbol(buffer)[j+1] = $starSymbol(unpackleft).regvaluesh[1];
+	  $starSymbol(buffer)[j+2] = $starSymbol(unpackleft).regvaluesh[2];
+	  $starSymbol(buffer)[j+3] = $starSymbol(unpackleft).regvaluesh[3];
+	}
+      }
     }
 
     go {
-      addDeclaration("int i;");
-      addCode(convert);
+      addCode(setbufptr);
+      if (interleave) {
+	addCode(convert_interleave);
+      }
+      else {
+	addCode(convert_separate);
+	addCode(write);
+	addCode(updatebufptr);
+	addCode(write);
+	addCode(updatebufptr);
+      }
+      addCode(write);
+      addCode(updatebufptr);
       addCode(write);
     }
     
     wrapup {
-      CGCStereoBase::wrapup();
+      CGCAudioBase::wrapup();
     }
     
     exectime {
