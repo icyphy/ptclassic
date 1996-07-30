2f2327376088a97edd7b6977bf7b2cb9f3723b06
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-30 21:40:38 +0000

    Added C50 domain to TITarget constructor

--- trunk/ptclassic/src/domains/c50/targets/SubC50Target.cc
+++ trunk/ptclassic/src/domains/c50/targets/SubC50Target.cc
@@ -45,7 +45,7 @@ ENHANCEMENTS, OR MODIFICATIONS.
 #include "TITarget.h"
 
 SubC50Target :: SubC50Target(const char* nam, const char* desc) :
-	C50Target(nam,desc),TITarget(nam,desc,"C50Star")
+	C50Target(nam,desc),TITarget(nam,desc,"C50Star","C50")
 {
 	initStates();
 }
