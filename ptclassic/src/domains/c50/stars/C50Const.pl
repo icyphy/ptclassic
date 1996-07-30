4347f9ae7dd7bccf7fb6fe6a79634bab16e783d8
luisgm <luisgm@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-07-30 18:22:56 +0000

    added code so that fix numbers are formatted properly

--- ptclassic/src/domains/c50/stars/C50Const.pl
+++ ptclassic/src/domains/c50/stars/C50Const.pl
@@ -26,11 +26,13 @@ output buffer is initialized with the specified DC value.
 		default {"0.0"}
 		descriptor {Constant value to be output}
 	}
+
+	protected {
+	    StringList constValue;
+	}
+
 	codeblock (org) {
 	.ds	$addr(output)
-	}
-	codeblock (dc) {
-	.q15	$val(level)
 	}
 	codeblock (orgp) {
 	.text
@@ -39,8 +41,13 @@ output buffer is initialized with the specified DC value.
 		noInternalState();
 	}
 	initCode {
+		constValue.initialize();
+		double temp = level.asDouble();
+		char	buf[32];
+		sprintf(buf,"%.15f",temp);
+		constValue <<"\t.q15\t"<< buf <<"\n";
 		addCode(org);
-		for (int i = 0; i < output.bufSize(); i++) addCode(dc);
+		for (int i = 0; i < output.bufSize(); i++) addCode(constValue);
 		addCode(orgp);
 	}
 	execTime {
