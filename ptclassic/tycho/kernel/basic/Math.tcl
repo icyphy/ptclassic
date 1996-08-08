e469c32948138613cc7006ab6ba37af06efab0e2
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-08-08 21:08:03 +0000

    Improved commenting.

--- ptclassic/tycho/kernel/basic/Math.tcl
+++ ptclassic/tycho/kernel/basic/Math.tcl
@@ -1,11 +1,5 @@
-#
-# mapping.tcl
-#
 # Utility functions for mapping between number ranges, scaling
 # intervals, and so on.
-#
-
-##########################################################################
 #
 # Author:  H. John Reekie
 #
@@ -38,7 +32,48 @@
 ##########################################################################
 
 
-## interval x y
+#######################################################################
+#### axisIncrement
+#
+# Given a range, space, field width, and padding, figure out how
+# the field increment so they will fit.
+#
+proc axisIncrement {low high space width padding} {
+    set maxnum   [expr double($space) / ($width+$padding)]
+    set estimate [expr (double($high) - $low) / ($maxnum)]
+    set estimate [axisRoundUp $estimate]
+
+    return $estimate
+}
+
+
+#######################################################################
+#### axisRoundUp
+#
+# Given a number, round up to the nearest power of ten
+# times 1, 2, or 5.
+#
+# Note: The argument must be positive.
+#
+proc axisRoundUp {x} {
+    set exp [expr floor (log10($x))]
+    set x [expr $x * pow(10, -$exp)]
+
+    if {$x > 5.0} {
+	set x 10.0
+    } elseif {$x > 2.0} {
+	set x 5.0
+    } elseif {$x > 1.0 } {
+	set x 2.0
+    }
+
+    set x [expr $x * pow(10,$exp)]
+    return $x
+}
+
+
+#######################################################################
+#### interval
 #
 # Return list of integers in the range _x_ to _y_. For example,
 #
@@ -58,7 +93,116 @@ proc interval {x y} {
 }
 
 
-## spread n x y [-indented]
+#######################################################################
+#### mapRange
+#
+# Given two ranges and a list of numbers in the first range,
+# produce the mapping of that list to the second range.
+#
+proc mapRange {low high values lowdash highdash} {
+    set result {}
+
+    set scale [expr (double($highdash) - $lowdash) / ($high - $low)]
+    foreach n $values {
+	lappend result [expr $lowdash + ($n-$low) * $scale]
+    }
+
+    return $result
+}
+
+
+#######################################################################
+#### mapValue
+#
+# Given two ranges and a number in the first range,
+# produce the mapping of that number to the second range.
+#
+proc mapValue {low high value lowdash highdash} {
+    set scale [expr (double($highdash) - $lowdash) / ($high - $low)]
+    return [expr $lowdash + ($value-$low) * $scale]
+}
+
+
+#######################################################################
+#### rangeValues
+#
+# Given a range and an increment, return the list of numbers
+# within that range and on that increment.
+#
+proc rangeValues {low high inc} {
+    set result {}
+    set n      1
+
+    set val [roundUpTo $low $inc]
+    while {$val <= $high} {
+	lappend result $val
+	set val [expr $val + $n * $inc]
+    }
+
+    return $result
+}
+
+
+#######################################################################
+#### roundDownTwo
+#
+# Given a number, round down to the nearest power of two.
+#
+proc roundDownTwo {x} {
+    set exp [expr floor (log($x)/log(2))]
+    set x   [expr pow(2,$exp)]
+    return $x
+}
+
+
+#######################################################################
+#### roundDownTo
+#
+# Given two numbers, round down the first to the nearest multiple of
+# the second.
+#
+proc roundDownTo {x i} {
+    return [expr $i * int(floor(double($x)/$i))]
+}
+
+
+#######################################################################
+#### roundTo
+#
+# Given two numbers, round the first to the nearest multiple of
+# the second.
+#
+proc roundTo {x i} {
+    return [expr $i * round(double($x)/$i)]
+}
+
+
+#######################################################################
+#### roundUpTo
+#
+# Given two numbers, round up the first to the nearest multiple of
+# the second.
+#
+proc roundUpTo {x i} {
+    return [expr $i * int(ceil(double($x)/$i))]
+}
+
+
+#######################################################################
+#### roundUpTwo
+#
+# Given a number, round up to the nearest power of two.
+# The argument must be positive.
+#
+proc roundUpTwo {x} {
+    set exp [expr ceil (log($x)/log(2))]
+    set x   [expr pow(2,$exp)]
+    return $x
+}
+
+
+#######################################################################
+#### spread
 #
 # Return list of n numbers in the range x to y. The -indented
 # option causes the first and list numbers to be more or less
@@ -94,116 +238,3 @@ proc spread {n x y args} {
 
     return $result
 }
-
-
-#
-# Given a number, round up to the nearest power of ten
-# times 1, 2, or 5.
-#
-proc axisRoundUp {x} {
-    set exp [expr floor (log10($x))]
-    set x [expr $x * pow(10, -$exp)]
-
-    if {$x > 5.0} {
-	set x 10.0
-    } elseif {$x > 2.0} {
-	set x 5.0
-    } elseif {$x > 1.0 } {
-	set x 2.0
-    }
-
-    set x [expr $x * pow(10,$exp)]
-    return $x
-}
-
-
-#
-# Given a number, round up or down to the nearest power of two.
-#
-proc roundUpTwo {x} {
-    set exp [expr ceil (log($x)/log(2))]
-    set x   [expr pow(2,$exp)]
-}
-
-proc roundDownTwo {x} {
-    set exp [expr floor (log($x)/log(2))]
-    set x   [expr pow(2,$exp)]
-}
-
-
-#
-# Given a range, space, field width, and padding, figure out how
-# the field increment so they will fit.
-#
-proc axisIncrement {low high space width padding} {
-    set maxnum   [expr double($space) / ($width+$padding)]
-    set estimate [expr (double($high) - $low) / ($maxnum)]
-    set estimate [axisRoundUp $estimate]
-
-    return $estimate
-}
-
-
-#
-# Given a range and an increment, return the list of numbers
-# within that range and on that increment.
-#
-proc rangeValues {low high inc} {
-    set result {}
-    set n      1
-
-    set val [roundUpTo $low $inc]
-    while {$val <= $high} {
-	lappend result $val
-	set val [expr $val + $n * $inc]
-    }
-
-    return $result
-}
-
-
-#
-# Given two ranges and a list of numbers in the first range,
-# produce the mapping of that list to the second range.
-#
-proc mapRange {low high values lowdash highdash} {
-    set result {}
-
-    set scale [expr (double($highdash) - $lowdash) / ($high - $low)]
-    foreach n $values {
-	lappend result [expr $lowdash + ($n-$low) * $scale]
-    }
-
-    return $result
-}
-
-
-#
-# Given two ranges and a number in the first range,
-# produce the mapping of that number to the second range.
-#
-proc mapValue {low high value lowdash highdash} {
-    set scale [expr (double($highdash) - $lowdash) / ($high - $low)]
-    return [expr $lowdash + ($value-$low) * $scale]
-}
-
-
-## roundTo     x i
-## roundUpTo   x i
-## roundDownTo x i
-#
-# Given two numbers, round the first to the nearest, or up or down
-# to the nearest, multiple of the second.
-#
-proc roundTo {x i} {
-    return [expr $i * round(double($x)/$i)]
-}
-
-proc roundUpTo {x i} {
-    return [expr $i * int(ceil(double($x)/$i))]
-}
-
-proc roundDownTo {x i} {
-    return [expr $i * int(floor(double($x)/$i))]
-}
-
