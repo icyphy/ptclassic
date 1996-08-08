a813421768e196c7d58d92d41c0a0b0d2f3f4627
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-08-08 17:38:11 +0000

    newCopy no longer virtual; removed some commented-out lines

--- ptclassic/src/domains/vhdl/kernel/VHDLObj.h
+++ ptclassic/src/domains/vhdl/kernel/VHDLObj.h
@@ -64,12 +64,8 @@ class VHDLObj : public NamedObj
   /* virtual */ int isA(const char*) const;
   /* virtual */ const char* className() const;
 
-// cfront disapproves of this - conflict with derived classes
   // Return a pointer to a new copy of the VHDLObj.
-//  virtual VHDLObj* newCopy() {return new VHDLObj;};
-
-  // Return a pointer to a new copy of the VHDLObj.
-  virtual VHDLObj* newCopy();
+  VHDLObj* newCopy();
 
  protected:
  private:
