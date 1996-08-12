5e639277e68e01f5654909a1011b3b078cef4213
sunil <sunil@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-08-12 14:44:53 +0000

    added feature to write to a file.

--- trunk/ptclassic/src/domains/cgc/stars/CGCStereoOut.pl
+++ trunk/ptclassic/src/domains/cgc/stars/CGCStereoOut.pl
@@ -73,24 +73,27 @@ provisions.
     addDeclaration(declarations("short", int(blockSize)/2));
     /* Open file for writing data */
     addCode(openFileForWriting);	
-    /* audio_setup : to set encodingType, sampleRate and channels */
-    StringList setupParameters = "$sharedSymbol(CGCAudioBase,audio_setup)";
-    setupParameters  << "($starSymbol(file), "
-		     << "\"" << encodingType << "\", "
-		     <<  sampleRate << ", " 
-		     <<  channels   << ");\n";
+    /* Set the audio driver if file is "/dev/audio" */
+    if(strcasecmp(fileName, "/dev/audio") == 0)
+      {
+	/* audio_setup : to set encodingType, sampleRate and channels */
+	StringList setupParameters = "$sharedSymbol(CGCAudioBase,audio_setup)";
+	setupParameters  << "($starSymbol(file), "
+			 << "\"" << encodingType << "\", "
+			 <<  sampleRate << ", " 
+			 <<  channels   << ");\n";
 
-    addCode(setupParameters);
-    /* audio_control : to set portType, volume and balance */
-    StringList controlParameters = "$sharedSymbol(CGCAudioBase,audio_control)";
-    controlParameters << "($starSymbol(file), "
-		      << "\"" << portType << "\", "
-		      <<  volume << ", " 
-		      <<  balance << ", "
-		      << "0);\n";
-    addCode(controlParameters);
+	addCode(setupParameters);
+	/* audio_control : to set portType, volume and balance */
+	StringList controlParameters = "$sharedSymbol(CGCAudioBase,audio_control)";
+	controlParameters << "($starSymbol(file), "
+			  << "\"" << portType << "\", "
+			  <<  volume << ", " 
+			  <<  balance << ", "
+			  << "0);\n";
+	addCode(controlParameters);
+      }
   }
-  
   go {
     addCode(convert);
     addCode(setbufptr);
