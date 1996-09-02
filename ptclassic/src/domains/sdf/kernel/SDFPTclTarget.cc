6bc4247e9eb8a1bcb7275da51a48d0e7acc077f8
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-09-02 17:18:57 +0000

    Moved the ptclDescription function and its supporting functions to the ptclDescription.cc file in the ptcl library

--- ptclassic/src/domains/sdf/kernel/SDFPTclTarget.cc
+++ ptclassic/src/domains/sdf/kernel/SDFPTclTarget.cc
@@ -38,110 +38,22 @@ ENHANCEMENTS, OR MODIFICATIONS.
 #pragma implementation
 #endif
 
-#include "Scheduler.h"
+// Ptolemy kernel files
 #include "miscFuncs.h"
-#include "SDFPTclTarget.h"
-#include "GalIter.h"
+#include "Scheduler.h"
 #include "Star.h"
+#include "Galaxy.h"
+#include "GalIter.h"
 #include "DFPortHole.h"
 #include "KnownTarget.h"
-#include "pt_fstream.h"
 #include "ProfileTimer.h"
 
-// Supporting routines
+// Ptolemy ptcl files
+#include "ptclDescription.h"
 
-// Write out the header which resets the state of ptcl and declares the
-// new galaxy name
-static StringList ptclHeaderCode(Galaxy* localGalaxy) {
-    StringList ptclCode = "reset\n";
-    ptclCode << "newuniverse " << localGalaxy->name() << " "
-			       << localGalaxy->domain() << " \n";
-    return ptclCode;
-}
-
-// Generate PTcl code for a galaxy independent of the domain of the galaxy
-static StringList ptclGalaxyCode(Galaxy* localGalaxy) {
-    StringList ptclCode;
-
-    // Iterator over the stars in the galaxy
-    GalStarIter nextStar(*localGalaxy);
-    Star* star;
-
-    // Create table of connections between ports of different stars
-    int nports = setPortIndices(*localGalaxy);
-    int* portNumber = new int[nports];
-    while ((star = nextStar++) != NULL) {
-	int input = 1;
-	int output = 1;
-	BlockPortIter nextPort(*star);
-	PortHole* port;
-	while ((port = (PortHole*)nextPort++) != NULL) {
-	    portNumber[port->index()] = (port->isItInput()?input++:output++);
-	}
-    }
-    nextStar.reset();
-
-    // Generate the Ptcl code for the galaxy
-    while ((star = nextStar++) != NULL) {
-	StringList starName;
-	starName << " \"" << star->fullName() << "\" ";
-	StringList setstate;
-	setstate << "\tsetstate  " << starName << " ";
-	BlockPortIter nextPort(*star);
-	PortHole* port;
-	while ((port = nextPort++) != NULL) {
-	    if (port->isItOutput())
-		ptclCode << "\tconnect" << starName
-			 << " \"output#" << portNumber[port->index()]
-			 << "\" \"" << port->far()->parent()->fullName()
-			 << "\" \"input#" << portNumber[port->far()->index()]
-			 << "\"\n";
-	}
-    }
-
-    delete [] portNumber;
-
-    return ptclCode;
-}
-
-static void writePtclCode(StringList& ptclCode, const char* path) {
-    // The pt_ofstream constructor automatically expands environment variables
-    pt_ofstream ptclFile(path);
-    ptclFile << ptclCode;
-}
-
-// Generate a PTcl description for galaxy without having to execute it
-// If the galaxy is null, then a null StringList will be returned.
-StringList ptclDescription(Galaxy* localGalaxy, const char* path,
-			   int addHeader, const char* preamble,
-			   const char* epilog) {
-    StringList ptclCode;
-
-    if (localGalaxy) {
-	if (addHeader) {
-	    // FIXME: Necessary to store returned StringList to ensure that
-	    // temporary variables are handled correctly.  GNU 2.7 bug.
-    	    StringList headerCode = ptclHeaderCode(localGalaxy);
-    	    ptclCode << headerCode << "\n";
-	}
-
-	// Conditionally add the preamble
-	if (preamble) ptclCode << preamble << "\n";
-
-	// FIXME: Necessary to store returned StringList to ensure that
-	// temporary variables are handled correctly.  GNU 2.7 bug.
-	StringList galCode = ptclGalaxyCode(localGalaxy);
-	ptclCode << galCode << "\n";
-
-	// Conditionally add the epilog
-	if (epilog) ptclCode << epilog << "\n";
-
-	// Conditionally write the ptcl code to the file called path
-	if (path && *path) writePtclCode(ptclCode, path);
-    }
-
-    return ptclCode;
-}
+// Ptolemy domain files
+#include "SDFTarget.h"
+#include "SDFPTclTarget.h"
 
 // Constructor
 SDFPTclTarget::SDFPTclTarget(const char* nam,const char* desc) :
