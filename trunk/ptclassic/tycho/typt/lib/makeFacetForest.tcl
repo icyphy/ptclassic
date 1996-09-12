3ade15eec7bdd74cfa77990c0b3257ae6cfa0719
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-09-12 10:19:50 +0000

    Changed names from tycho to ptolemy

--- trunk/ptclassic/tycho/typt/lib/makeFacetForest.tcl
+++ trunk/ptclassic/tycho/typt/lib/makeFacetForest.tcl
@@ -1,10 +1,10 @@
-# Procedure to make a graph describing a Octtools facet hierarchy.
+# Procedure to make a forest describing a Octtools facet hierarchy.
 #
-# @Authors: Edward A. Lee, Christopher Hylands
+# @Authors: Christopher Hylands, Edward A. Lee 
 #
-# @Version: @(#)tychoMakeClassDAG.tcl	1.2 9/2/96
+# @Version: $Id$
 #
-# @Copyright (c) 1995-1996 The Regents of the University of California.
+# @Copyright (c) 1996 The Regents of the University of California.
 # All rights reserved.
 # 
 # Permission is hereby granted, without written agreement and without
@@ -29,19 +29,20 @@
 # 						PT_COPYRIGHT_VERSION_2
 # 						COPYRIGHTENDKEY
 
-# FIXME: octls only works on facets that it can write
+# This file is meant to be run standalone, in the background with
+# and itclsh so that we can convert Ptolemy facets to dags
 
-#### tychoMkFacetGraph
-# Make a class graph of the type displayed by the Tycho EditDAG class.
+#### ptolemyMkFacetGraph
+# Make a facet graph of the type displayed by the Tycho EditForest class.
 # The first argument is the name of the graph. The second argument is
 # the name of the graph file to create. The rest of the arguments are
 # any number of file names from which the graph should be created.
 # These file names should be absolute (or relative to an environment
-# variable like TYCHO) that the resulting hyperlinks work from any
-# directory. To create a graph for the entire Tycho tree, use the
-# procedure <code>tychoStandardClasses</code>.
+# variable like PTOLEMY) that the resulting hyperlinks work from any
+# directory. To create a graph for the entire Ptolemy tree, use the
+# procedure <code>ptolemyStandardFacets</code>.
 #
-proc tychoMkFacetGraph {name filename args} {
+proc ptolemyMkFacetGraph {name filename args} {
     set entries {}
     set outfd [open $filename w]
     # Put in titles and a reasonable default size.
@@ -51,13 +52,13 @@ proc tychoMkFacetGraph {name filename args} {
     puts $outfd "\{centeredText \{created: [exec date]\} subtitle title firebrick \{\{helvetica 16 bold i\} \{times 16 bold i\}\}\}"
 
     puts $outfd {{add src/domains/cgc/demo/init.pal {}}}
-    tychoMkFacet [lindex $args 0] $outfd $args
+    ptolemyMkFacet [lindex $args 0] $outfd $args
     close $outfd
 }
 
 set autoName 0
 set paletteList {}
-proc tychoMkFacet { nm outfd parentPalette} {
+proc ptolemyMkFacet { nm outfd parentPalette} {
     global autoName paletteList
     set infd [open "|octls $parentPalette" r]
     while {[gets $infd line ] >= 0} {
@@ -75,7 +76,7 @@ proc tychoMkFacet { nm outfd parentPalette} {
                 puts $palette
                 flush stdout
                 lappend paletteList $palette
-                tychoMkFacet $palette $outfd $palette
+                ptolemyMkFacet $palette $outfd $palette
             }
 	} {
             # A facet, not a palette containing facets
@@ -85,14 +86,13 @@ proc tychoMkFacet { nm outfd parentPalette} {
     close $infd
 }
 
-#### tychoStandardClasses
-# Update the Tycho classes graph.
+#### ptolemyStandardFacets
+# Update the Ptolemy facets graph.
 #
-proc tychoStandardDAG {} {
-    global PTOLEMY
-    set olddir [pwd]
-
-    eval tychoMkFacetGraph {{Ptdesign Class Hierarchy}} \
-	    /tmp/PtdesignFacets.dag /users/ptdesign/init.pal
+proc ptolemyStandardFacets {} {
+    global env
+    eval ptolemyMkFacetGraph {{Ptolemy Facet Hierarchy}} \
+	    PtolemyFacets.fst $env(PTOLEMY)/init.pal
 }
-tychoStandardDAG
+
+
