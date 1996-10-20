2bd40adab59e1c35ff4a51176f631b035f7dc2a5
bilung <bilung@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-10-20 02:41:22 +0000

    Don't support Moore and Mealy State...

--- ptclassic/src/domains/fsm/kernel/FSMStateStar.cc
+++ ptclassic/src/domains/fsm/kernel/FSMStateStar.cc
@@ -58,17 +58,48 @@ FSMStateStar::FSMStateStar ()
 {
     addPort(stateIn.setPort("stateIn",this,FLOAT));
     addPort(stateOut.setPort("stateOut",this,FLOAT));
-    addState(isInitState.setState("isInitState",this,"FALSE","Is this an initial state?"));
-    addState(conditions.setState("conditions",this,"","The conditions to determine the next transition state. Each condition must be surrounded by a pair of double-quotes."));
 
-    parsedCond = 0;
+    addState(isInitState.setState("isInitState",this,"FALSE","Is this an initial state?"));
+
+    addState(conditions.setState("conditions",this,"","The conditions to determine the next transition state. Each condition must be surrounded by a pair of double-quotes."));
+    addState(actions.setState("actions",this,"","The values/expressions to specify the outputs associated with all possible next transitions (outgoing arcs). Those values/expressions corresponding to one transition must be surrounded by a pair of curly-braces, and each value/expression must be surrounded by a pair of double-quotes."));
+
+    addState(entryType.setState("entryType",this,"","Specify the entry type for each possible transition out of this state. Available choices are 0 (History) or 1 (Initial). Note that the # of specified types should be equal to the # of outgoing arcs."));
+    addState(preemptive.setState("preemptive",this,"","Specify that each possible transition is preemptive or not. Note that the # of YES/NO should be equal to the # of outgoing arcs."));
+
+    parsedCond = NULL;
+    parsedAct = NULL;
+
 }
 
 FSMStateStar::~FSMStateStar() {
     delete [] parsedCond;
+    delete [] parsedAct;
 }
 
 void FSMStateStar::setup() {
+    if (entryType.size() != stateOut.numberPorts()) {
+      Error::abortRun(*this,"The number of specified entry types doesn't ",
+		      "match the number of possible next transitions");
+      return;
+    }     
+
+    if (preemptive.size() != stateOut.numberPorts()) {
+      Error::abortRun(*this,"The number of specified preemptive doesn't ",
+		      "match the number of possible next transitions");
+      return;
+    }     
+
+    FSMScheduler *sched = (FSMScheduler *)scheduler();
+
+    // Setup the list of internal event names as same as the scheduler.
+    internalEventNm = sched->getInternalEventNm();
+
+    if ( (myInterp = sched->interp()) == NULL ) {
+      Error::abortRun("FSMScheduler does not have a Tcl interpreter!");
+      return; 
+    }
+
     // Parse the conditions.
     delete [] parsedCond;
     if (!(parsedCond = strParser(conditions, numConds, "double-quote")))
@@ -80,74 +111,106 @@ void FSMStateStar::setup() {
       return;
     }
 
-    if (!(myInterp = ((FSMScheduler *)scheduler())->interp())) {
-      Error::abortRun("FSMScheduler does not have a Tcl interpreter!");
-      return; 
+    // Parse the actions.
+    int numStrs = 0;
+    char** tmpParsedAct = strParser(actions,numStrs,"curly-brace");
+    if (!tmpParsedAct) return;
+    if (numStrs != stateOut.numberPorts()) {
+      Error::abortRun(*this,"The number of action values/expressions in ",
+		      "curly-braces doesn't match the number of possible ",
+		      "next transitions.");
+      return;
     }
+
+    delete [] parsedAct;
+    parsedAct = new (char**)[numStrs];
+    for (int indx=0; indx<stateOut.numberPorts(); indx++) {
+      parsedAct[indx] = strParser(tmpParsedAct[indx],numStrs,"double-quote");
+      if (!parsedAct[indx]) return;
+      if ( numStrs != 
+	   (sched->outPorts()->numberPorts()+internalEventNm->numPieces()) ) {
+	InfString buf = "The number of action values/expressions ";
+	buf << "for transition #";
+	buf << indx;
+	buf << " doesn't match the number of outputs plus internal events ";
+	buf << "in this FSM.";
+	Error::abortRun(*this,buf);
+	return;
+      }
+    }
+
+    delete [] tmpParsedAct;
 }
 
-// Find the next state which meets the condition.
+// Find the next state that meets the condition depending on preemptive or not.
 // Return (1) "condNum", condition #, which is TRUE at this transition,
 // and (2) the corresponding connected state.
 // If no condition is TRUE, return (1) condNum = -1 and (2) "this" state.
-FSMStateStar * FSMStateStar::nextState (int& condNum, int) {
-    InfString buf;
+FSMStateStar * FSMStateStar::nextState (int& condNum, int preemption) {
+      InfString buf;
 
-    int* result = new int[numConds];
-    int checkResult = 0;
-    for (int i = 0; i < numConds; i++) {
-      buf = parsedCond[i];
-      if(Tcl_ExprBoolean(myInterp, (char*)buf, &(result[i])) != TCL_OK) {
-	buf = "Cannot evaluate the condition #";
-	buf << i;
-	buf << " in ";
-	buf << this->name();
-	buf << ". The error message in Tcl : ";
-	buf << myInterp->result;
-	Error::abortRun(*this,(char*)buf);
+      int* result = new int[numConds];
+      int checkResult = 0;
+      for (int i = 0; i < numConds; i++) {
+	if (preemptive[i] == preemption) {
+	  // If the preemptiveness of the i'th arc is equal to "preemption",
+	  // then evaluate the corresponding condition. 
+	  buf = parsedCond[i];
+	  if(Tcl_ExprBoolean(myInterp, (char*)buf, &(result[i])) != TCL_OK) {
+	    buf = "Cannot evaluate the condition #";
+	    buf << i;
+	    buf << " in ";
+	    buf << this->name();
+	    buf << ". The error message in Tcl : ";
+	    buf << myInterp->result;
+	    Error::abortRun(*this,(char*)buf);
+	    delete [] result;
+	    return NULL;
+	  }
+	  if (result[i] == 1) checkResult++;
+
+	} else {
+	  result[i] = 0;
+	}
+      }
+
+      if (checkResult == 1) {
+	// The next state is found.
+	
+	// Find the index # corresponding to the next state.
+	condNum = 0;
+	while (result[condNum] != 1) condNum++;
+
+	// Return the next state.
+	MPHIter nextp(stateOut);
+	PortHole* oport = 0;
+	for (int i=0; i<=condNum; i++)  {
+	  oport=nextp++;
+	}
+	
 	delete [] result;
-	return 0;
+	return (FSMStateStar *)(oport->far()->parent());
+
+      } else if (checkResult == 0) {
+
+	// No condition satisfy. Remain current state.
+	// ??Or issue error?
+	condNum = -1;
+	delete [] result;
+	return this;
+      }  
+
+      // checkResult > 1, this means "Transition Conflict".
+      buf = "Transition conflict. Conditions ";
+      for (i = 0; i < numConds; i++) {
+	if (result[i] == 1) {
+	  buf << i << ", ";
+	}
       }
-      if (result[i] == 1) checkResult++;
-    }
-
-    if (checkResult == 1) {
-      // The next state is found.
-
-      // Find the index # corresponding to the next state.
-      condNum = 0;
-      while (result[condNum] != 1) condNum++;
-
-      // Return the next state.
-      MPHIter nextp(stateOut);
-      PortHole* oport = 0;
-      for (int i=0; i<=condNum; i++)  {
-	oport=nextp++;
-      }
-
+      buf << "are all satisfied at the same time.";
+      Error::abortRun(*this, (char*)buf);
       delete [] result;
-      return (FSMStateStar *)(oport->far()->parent());
-
-    } else if (checkResult == 0) {
-
-      // No condition satisfy. Remain current state.
-      // ??Or issue error?
-      condNum = -1;
-      delete [] result;
-      return this;
-    }  
-
-    // checkResult > 1, this means "Transition Conflict".
-    buf = "Transition conflict. Conditions ";
-    for (i = 0; i < numConds; i++) {
-      if (result[i] == 1) {
-	buf << i << ", ";
-      }
-    }
-    buf << "are all satisfied at the same time.";
-    Error::abortRun(*this, (char*)buf);
-    delete [] result;
-    return 0;
+      return NULL;
 }
 
 Star* FSMStateStar::createWormhole(const char *galname,
@@ -300,42 +363,60 @@ int FSMStateStar::ioNmLists(StringList& inNmList, StringList& outNmList, Star* w
       else  outNmList << mp->name();
     }
 
+printf("\n----- Wormhole name: %s -----\n",this->name());
+const char* pNm;
+StringListIter nexti(inNmList);
+printf("input names: ");
+while ((pNm = nexti++) != 0) {
+  printf("%s ",pNm);
+}
+StringListIter nexto(outNmList);
+printf("\noutput names: ");
+while ((pNm = nexto++) != 0) {
+  printf("%s ",pNm);
+}
+printf("\n-----------------------\n");
+
     return TRUE;
 }
 
 Geodesic** FSMStateStar::setupGeodesic (PortList& pList, MultiPortHole* mph, Star* worm, StringList& pNmList) {
-    // Set up the Geodesics for the portholes of the worm with the names 
-    // in the pNmList. In addition, it collects all the corresponding 
-    // portholes in mph and puts them into pList.
+      // Set up the Geodesics for the portholes of the worm with the names 
+      // in the pNmList. In addition, it collects all the corresponding 
+      // portholes in mph and puts them into pList.
+      
+      Geodesic** myGeoArray = new (Geodesic*) [pNmList.numPieces()];
 
-    if (pNmList.numPieces() != mph->numberPorts()) {
-      InfString buf = "The number of ";
-      buf << mph->isItInput() ? "input" : "output";
-      buf << " PortHoles in the Wormhole \"";
-      buf << worm->name();
-      buf << "\" does not match the number in this FSM.";
-      Error::abortRun(*this,buf);
-      return NULL;
-    }
-
-    Geodesic** myGeoArray = new (Geodesic*) [pNmList.numPieces()];
-
-    int index = 0;
-    StringListIter nextNm(pNmList);
-    const char* pNm;
-    while ((pNm = nextNm++) != 0) {
-	MPHIter nextp(*mph);
-	PortHole* p;
+      int index = 0;
+      StringListIter nextpNm(pNmList);
+      const char* pNm;
+      MPHIter nextp(*mph);
+      PortHole* p;
+      StringListIter nextiNm(*internalEventNm);
+      const char* iNm;
+      while ((pNm = nextpNm++) != 0) {
+	nextp.reset();
 	while ((p = nextp++) != 0) {
 	  if (!strcmp(p->name(),pNm))  break;
 	}
-	if (p == 0) {
-	  Error::abortRun(*this, "There does not exist a matched Porthole in ",
-			  "this FSM corresponding to the name: ", pNm);
-	  delete [] myGeoArray;
-	  return NULL;
+	if (p != 0) {
+	  // The name is found as an PortHole in outer MultiPortHole.
+	  pList.put(*p);
+
+	} else {
+	  // Try to find the name in the list of internal events.
+	  nextiNm.reset();
+	  while ((iNm = nextiNm++) != 0) {
+	    if (!strcmp(iNm,pNm))  break;
+	  }
+	  if (iNm == 0) {
+	    Error::abortRun(*this, "There does not exist a matched I/O port ",
+		  "or internal event in this FSM corresponding to the name: ",
+		  pNm);
+	    delete [] myGeoArray;
+	    return NULL;
+	  }
 	}
-	pList.put(*p);
 
         PortHole* wp = worm->portWithName(pNm);
 	if (!wp) {
@@ -372,15 +453,68 @@ Geodesic** FSMStateStar::setupGeodesic (PortList& pList, MultiPortHole* mph, Sta
 	
 	myGeoArray[index] = geo;
 	index++;
-    }   // end of while ((pNm = nextNm++) != 0) 
+      }   // end of while ((pNm = nextpNm++) != 0) 
 
-    return myGeoArray;
+      return myGeoArray;
 }
 
-int FSMStateStar::doAction (int) {
-    Error::abortRun(*this, "doAction() is virtual and needs to be ",
-		    "redefined in derived class.");
-    return FALSE;
+int FSMStateStar::doAction (int actNum) {
+      ///////////////////////////////////////////////////////////////////
+      // Do the action for the next transition corresponding to "actNum".
+
+      FSMScheduler *sched = (FSMScheduler *)scheduler();
+
+      // Evaluate the actions for outer output portholes and send the outputs.
+      OutFSMPort* p;
+      MPHIter next(*(sched->outPorts()));
+      int index = 0;
+      double result = 0;
+      while ((p = (OutFSMPort*)next++) != 0) {
+	if (strcmp(parsedAct[actNum][index],"-")) {
+	  // If the action is NOT "-", then it's a value/expression.
+	  if(Tcl_ExprDouble(myInterp, parsedAct[actNum][index], &result) 
+	     != TCL_OK) {
+	    Error::abortRun(*this,"Cannot evaluate the value. ",
+			    "The error message in Tcl : ",
+			    myInterp->result);
+	    return FALSE;
+	  }
+	  // Send the result to outer output porthole.
+	  p->put() << result;
+
+	  // In addition, set the result into Tcl.
+	  InfString buf = result;
+	  Tcl_SetVar(myInterp,(char*)p->name(),buf,TCL_GLOBAL_ONLY);
+//printf("output : %s with value %f\n",(const char *)p->name(),double((*p)%0));
+	} // end of if (strcmp(parsedAct[actNum][index],"-")) 
+
+	index++;
+      }   // end of while ((p = (OutFSMPort*)next++) != 0)
+
+      // Evaluate the actions for internal events and set the values.
+      // NOTE: index # for the actions of internal events continues
+      // after the actions of outer output portholes.
+      StringListIter nextiNm(*internalEventNm);
+      const char* iNm;
+      while ((iNm = nextiNm++) != 0) {
+	if (strcmp(parsedAct[actNum][index],"-")) {
+	  // If the action is NOT "-", then it's a value/expression.
+	  if(Tcl_ExprDouble(myInterp, parsedAct[actNum][index], &result) 
+	     != TCL_OK) {
+	    Error::abortRun(*this,"Cannot evaluate the value. ",
+			    "The error message in Tcl : ",
+			    myInterp->result);
+	    return FALSE;
+	  }
+	  // Set the result into Tcl.
+	  InfString buf = result;
+	  Tcl_SetVar(myInterp,(char *)iNm,buf,TCL_GLOBAL_ONLY);
+	}
+
+	index++;
+      }
+
+      return TRUE;
 }
 
 int FSMStateStar::doInMach (int) {
@@ -389,17 +523,16 @@ int FSMStateStar::doInMach (int) {
     return FALSE;
 }
 
-int FSMStateStar::getEntryType (int) {
-    Error::abortRun(*this, "There is no entry type for each transition ",
-		    "in this type of machine.");
-    return FALSE;
-}
+int FSMStateStar::getEntryType (int transNum) {
+    ///////////////////////////////////////////////////////////////////
+    // Get the entry type of a possible transition out of this state
+    // corresponding to the transNum.
 
-void FSMStateStar::printOut () {
-    int indx;
-    printf("state with name : %s\n",this->name());
-    printf("numConds = %d\n",numConds);
-    for (indx = 0; indx < numConds; indx++) { 
-      printf("condition %d : %s\n", indx, parsedCond[indx]);
+    if (transNum>=stateOut.numberPorts() || transNum<0) {
+        Error::abortRun(*this,"The specified transition number is out ",
+			"of bound.");
+	return -1;
     }
+
+    return entryType[transNum];
 }
