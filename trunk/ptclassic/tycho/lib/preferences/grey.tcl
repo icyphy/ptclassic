1862d68c2ca0fb54a5d2df2f2469e6046813b806
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-10-30 02:07:25 +0000

    Initial revision

---
+++ trunk/ptclassic/tycho/lib/preferences/grey.tcl
@@ -0,0 +1,10 @@
+# Tycho preferences file to set color to default greys
+#
+# $Id$
+
+preference assign textedit textBackground grey93
+preference assign textedit selectColor    snow
+preference assign dialogs entryBackground grey90
+
+preference configure tkpalette -inherits tkdefaultpalettePreferences
+updateTkPalette
