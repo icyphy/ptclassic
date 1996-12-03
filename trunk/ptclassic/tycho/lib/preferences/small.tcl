b7326edef9b1154686f757904353685e08dc7a9c
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-12-03 16:25:02 +0000

    *** empty log message ***

--- trunk/ptclassic/tycho/lib/preferences/small.tcl
+++ trunk/ptclassic/tycho/lib/preferences/small.tcl
@@ -13,5 +13,3 @@ preference assign viewhtml fontSize small
 preference assign viewhtml textWidth 75
 preference assign viewhtml textHeight 50
 
-# Unfortunately, we can't do this yet with preferences!
-# option add *font [.tychoFonts getFont Helvetica 12]
