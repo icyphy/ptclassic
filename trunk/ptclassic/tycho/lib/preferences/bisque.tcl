f7309dfd8ca170c12dc1aa6688e767e48575a415
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-12-05 13:58:57 +0000

    Replaced preference with ::tycho::preference

--- trunk/ptclassic/tycho/lib/preferences/bisque.tcl
+++ trunk/ptclassic/tycho/lib/preferences/bisque.tcl
@@ -2,12 +2,12 @@
 #
 # $Id$
 
-preference assign edittext textBackground [ptkColor antiqueWhite white]
-preference assign edittext selectColor [ptkColor yellow white]
-preference assign dialogs entryBackground [ptkColor antiqueWhite white]
+::tycho::preference assign edittext textBackground [ptkColor antiqueWhite white]
+::tycho::preference assign edittext selectColor [ptkColor yellow white]
+::tycho::preference assign dialogs entryBackground [ptkColor antiqueWhite white]
 
-preference assign viewhtml textBackground [ptkColor linen white]
-preference assign graphics canvasColor [ptkColor antiqueWhite3 white]
+::tycho::preference assign viewhtml textBackground [ptkColor linen white]
+::tycho::preference assign graphics canvasColor [ptkColor antiqueWhite3 white]
 
-preference configure tkpalette -inherits tkbisquepalettePreferences
+::tycho::preference configure tkpalette -inherits tkbisquepalettePreferences
 updateTkPalette
--- trunk/ptclassic/tycho/lib/preferences/small.tcl
+++ trunk/ptclassic/tycho/lib/preferences/small.tcl
@@ -2,14 +2,14 @@
 #
 # $Id$
 
-preference assign fonts textFont  {Screen 12}
-preference assign fonts entryFont {Helvetica 12}
-preference assign fonts menuFont  {Helvetica 12}
+::tycho::preference assign fonts textFont  {Screen 12}
+::tycho::preference assign fonts entryFont {Helvetica 12}
+::tycho::preference assign fonts menuFont  {Helvetica 12}
 
-preference assign layout textWidth 75
-preference assign layout textHeight 50
+::tycho::preference assign layout textWidth 75
+::tycho::preference assign layout textHeight 50
 
-preference assign viewhtml fontSize small
-preference assign viewhtml textWidth 75
-preference assign viewhtml textHeight 50
+::tycho::preference assign viewhtml fontSize small
+::tycho::preference assign viewhtml textWidth 75
+::tycho::preference assign viewhtml textHeight 50
 
