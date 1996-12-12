221a9f9adfdcab4db537aa15747d680cc9bafd8a
galicia <galicia@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1996-12-12 16:20:32 +0000

    added "iswormhole" commands

--- trunk/ptclassic/src/pitcl/PITcl.h
+++ trunk/ptclassic/src/pitcl/PITcl.h
@@ -251,6 +251,7 @@ public:
 	int halt(int argc,char** argv);
 	int initialize(int argc,char** argv);
 	int isgalaxy(int argc,char** argv);
+	int iswormhole(int argc,char** argv);
 	int knownlist(int argc,char** argv);
 	int link(int argc,char** argv);
 	int listobjs(int argc,char** argv);
