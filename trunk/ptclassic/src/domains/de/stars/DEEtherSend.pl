f4b0631a4c2198e283783f4707271c321455c711
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-01-27 17:52:11 +0000

    (tgl): Addresses are now strings, and addresses that begin with - are handled specially

--- trunk/ptclassic/src/domains/de/stars/DEEtherSend.pl
+++ trunk/ptclassic/src/domains/de/stars/DEEtherSend.pl
@@ -14,9 +14,10 @@ limitation of liability, and disclaimer of warranty provisions.
 	desc {
 This star can transmit particles of any type to any or all receivers
 that have the same value for the "medium" parameter.  The receiver
-address is given by the "address" input, and it must be an integer.
-If the integer is negative, then copies of the particle are sent
-to all receivers that use the same medium.
+address string is given by the "address" input.  If something other than
+a string is passed, Particle::print() is used to convert it to a string.
+If the address string begins with '-', then copies of the data particle
+are sent to all receivers that use the same medium.
 
 The transmitter "occupies" the medium for the specified duration.
 A collision occurs if the medium is occupied when a transmission is
@@ -32,20 +33,20 @@ This star implements a transmitter for a shared medium.
 Any type of
 <tt>Particle</tt>
 can be transmitted.
-If the "address" input is a non-negative integer, then the transmitter
+If the "address" string does not start with '-', then the transmitter
 looks for a receiver that shares the medium that has a matching address.
 If it is found, a copy of the Particle is sent to the receiver.
 The receiver is told to output that particle after a certain
 elapsed time, given by the most recent value that arrived on
 the "duration" input.
 If no such particle has arrived, a duration of zero is used.
-If the address is a negative integer, then the data particle
+If the address does start with '-', then the data particle
 is broadcast to all receivers on the medium.
 	}
 	input {
 	    name { address }
-	    type { int }
-	    desc { Address of the receiver. Negative for broadcast. }
+	    type { string }
+	    desc { Address of the receiver. }
 	}
 	input {
 	    name { sendData }
@@ -83,10 +84,9 @@ is broadcast to all receivers on the medium.
 		latestDuration = double(duration.get());
 	    }
 	    if (address.dataNew) {
-		Particle& adp = address.get();
-		if (int(adp) < 0) broadcast = 1;
-		else broadcast = 0;
 		latestAddress = (address.get()).print();
+		if (*((const char *) latestAddress) == '-') broadcast = 1;
+		else broadcast = 0;
 	    }
 	    if (sendData.dataNew) {
 		Particle& pp = sendData.get();
