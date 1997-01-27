d68f26a5982759b14aaca05a007fc444af24d95d
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-01-27 17:46:39 +0000

    (tgl): exout is now of type anytype. Added preinitialize method.

--- trunk/ptclassic/src/domains/hof/stars/HOFMapGr.pl
+++ trunk/ptclassic/src/domains/hof/stars/HOFMapGr.pl
@@ -28,105 +28,104 @@ See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
 	}
 	location { HOF main library }
-	ccinclude { "ptk.h" }
-	ccinclude { "SimControl.h" }
 	ccinclude { "InfString.h" }
 	outmulti {
 	  name {exout}
-	  type {=input}
+	  type {anytype}
 	  desc {
 Example of a connection to the replacement block
           }
 	}
 	inmulti {
 	  name {exin}
-	  type {ANYTYPE}
+	  type {anytype}
 	  desc {
 Example of a connection to the replacement block
           }
 	}
 	constructor {
-	  output.inheritTypeFrom(exin);
 	  input_map.clearAttributes(A_SETTABLE);
 	  output_map.clearAttributes(A_SETTABLE);
 	  blockname.clearAttributes(A_SETTABLE);
 	  where_defined.clearAttributes(A_SETTABLE);
 	  parameter_map.setInitValue("");
 	}
-	setup {
-	  HOFBaseHiOrdFn::setup();
+	method {
+	  name { preinitialize }
+	  access { public }
+	  code {
+	    HOFBaseHiOrdFn::preinitialize();
 
-	  Galaxy* mom = idParent();
-	  if(!mom) return;
+	    Galaxy* mom = idParent();
+	    if(!mom) return;
 
-	  // Any HOF star has to call this method for all multiPortHoles
-	  // to be sure that HOFNop stars are dealt with properly.
-	  initConnections(exout);
-	  initConnections(exin);
-	  initConnections(input);
-	  initConnections(output);
+	    // Make sure we know the number of connections on the
+	    // input and output multiPortHoles.
+	    initConnections(input);
+	    initConnections(output);
+	    initConnections(exout);
+	    initConnections(exin);
 
-	  // At this point, any HOFNop stars will have been disconnected,
-	  // so there should be only one example star.
+	    // At this point, any HOFNop stars will have been disconnected,
+	    // so there should be only one example star.
 
-	  MPHIter nexti(input);
-	  MPHIter nexto(output);
-	  MPHIter nextexo(exout);
-	  MPHIter nextexi(exin);
+	    MPHIter nexti(input);
+	    MPHIter nexto(output);
+	    MPHIter nextexo(exout);
+	    MPHIter nextexi(exin);
 
-	  // Make the first block connection, and in the process, and set
-	  // the input_map and output_map and blockname states.
-	  PortHole *pi, *po, *pei, *peo;
-	  InfString inputMap, outputMap;
-	  GenericPort *sourcegp, *destgp;
-	  myblock = 0;
+	    // Make the first block connection, and in the process, build
+	    // the input_map and output_map and blockname states.
+	    PortHole *pi, *po, *pei, *peo;
+	    InfString inputMap, outputMap;
+	    GenericPort *sourcegp, *destgp;
+	    myblock = 0;
 
-	  // Iterate over the inputs first
-	  while ((peo = nextexo++) != 0) {
-	    if ((pi = nexti++) == 0) {
+	    // Iterate over the inputs first
+	    while ((peo = nextexo++) != 0) {
+	      if ((pi = nexti++) == 0) {
+		Error::abortRun(*this,
+				"Not enough inputs for the specified replacement block");
+		return;
+	      }
+	      if (!(destgp = breakConnection(peo))) return;
+	      if (inputMap.numPieces() > 0) inputMap += " ";
+	      inputMap += destgp->name();
+	      if (!connectInput(pi,destgp)) return;
+	    }
+	    input_map.setInitValue((const char*)inputMap);
+	    input_map.initialize();
+	    // Iterate over the outputs next
+	    while ((pei = nextexi++) != 0) {
+	      if ((po = nexto++) == 0) {
+		Error::abortRun(*this,
+				"Not enough outputs for the specified replacement block");
+		return;
+	      }
+	      if (!(sourcegp = breakConnection(pei))) return;
+	      if (outputMap.numPieces() > 0) outputMap += " ";
+	      outputMap += sourcegp->name();
+	      if (!connectOutput(po, sourcegp)) return;
+	    }
+	    if (!myblock) {
 	      Error::abortRun(*this,
-			      "Not enough inputs for the specified replacement block");
+			      "No connections to a replacement block!");
 	      return;
 	    }
-	    if (!(destgp = breakConnection(peo))) return;
-	    if (inputMap.numPieces() > 0) inputMap += " ";
-	    inputMap += destgp->name();
-	    if (!connectInput(pi,destgp)) return;
-	  }
-	  input_map.setInitValue((const char*)inputMap);
-	  input_map.initialize();
-	  // Iterate over the outputs next
-	  while ((pei = nextexi++) != 0) {
-	    if ((po = nexto++) == 0) {
-	      Error::abortRun(*this,
-			      "Not enough outputs for the specified replacement block");
-	      return;
-	    }
-	    if (!(sourcegp = breakConnection(pei))) return;
-	    if (outputMap.numPieces() > 0) outputMap += " ";
-	    outputMap += sourcegp->name();
-	    if (!connectOutput(po, sourcegp)) return;
-	  }
-	  if (!myblock) {
-	    Error::abortRun(*this,
-			    "No connections to a replacement block!");
-	    return;
-	  }
-	  output_map.setInitValue((const char*)outputMap);
-	  output_map.initialize();
-	  blockname.setInitValue(myblock->name());
-	  blockname.initialize();
-	  
-	  // Iterate over the remaining blocks.
-	  if (!iterateOverPorts(nexti,nexto,mom,2)) return;
+	    output_map.setInitValue((const char*)outputMap);
+	    output_map.initialize();
+	    blockname.setInitValue(myblock->name());
+	    blockname.initialize();
 
-	  // Set the parameter values after all the clones have been created
-	  // so that cloning takes the right default values.
-	  if(!setParams(myblock, 1)) return;
-	  myblock->setTarget(target());
-	  myblock->initialize();
+	    // Iterate over the remaining blocks.
+	    if (!iterateOverPorts(nexti,nexto,mom,2)) return;
 
-	  mom->deleteBlockAfterInit(*this);
+	    // Set the parameter values after all the clones have been created
+	    // so that cloning takes the right default values.
+	    if(!setParams(myblock, 1)) return;
+
+	    mom->deleteBlockAfterInit(*this);
+	  }
 	}
 	// Override the default method to create new block.
 	method {
@@ -140,19 +139,21 @@ Example of a connection to the replacement block
 		Error::abortRun(*this,
 				"Unable to create instance of block: ",
 				myblock->name());
+		return NULL;
 	      }
 	      // Set the target
 	      if (target()) block->setTarget(target());
 
-	      // Choose a name for the block
+	      // Choose a unique name for the block.
 	      StringList instancename = "Map_";
 	      instancename += (const char*)myblock->name();
-	      instancename += "__";
 	      instancename += count++;
 	      // The following amounts to a small memory leak
+	      // FIXME: see notes in HOFBase.pl
 	      const char* instance = hashstring(instancename);
 
 	      mom.addBlock(*block,instance);
+
 	      return block;
             }
 	}
