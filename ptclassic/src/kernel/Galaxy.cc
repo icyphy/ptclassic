7186d337f148a78d1dd97109b7b8a19f2ddee3dc
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-01-27 17:27:21 +0000

    (tgl): include KnownBlock.h initialize() now calls preinitialize() Added preinitialize() deleteBlockAfterInit() and initSubblocks() formerly called blocksToDelete.blockWithName(b.name())), now we call blocksToDelete.isInList(b)) Added BlockList::isInList(), Galaxy::maxContainedSerialNumber

--- ptclassic/src/kernel/Galaxy.cc
+++ ptclassic/src/kernel/Galaxy.cc
@@ -41,6 +41,7 @@ Methods for class Galaxy
 #include "StringList.h"
 #include "Galaxy.h"
 #include "Block.h"
+#include "KnownBlock.h"
 #include "Scheduler.h"
 #include "ConstIters.h"
 #include "GraphUtils.h"
@@ -131,12 +132,69 @@ int Galaxy :: setTarget(Target* t) {
         ////////////////////////////////////
 
 // Initialize myself and all sub-blocks.  Note that
-// Block::initialize calls start().
+// Block::initialize calls setup().
 // Stop the initialization if an error occurs.
 
 void Galaxy :: initialize() {
 	Block::initialize();
+	// Preinitialization of subblocks...
+	preinitialize();
+	// then regular initialization.
 	initSubblocks();
+}
+
+// Pre-initialization pass.
+// Blocks can change the galaxy topology during this pass.
+//
+// We actually do the preinitialization in two passes,
+// one that preinits only atomic blocks and a second to preinit all blocks.
+// This allows clean support of graphical recursion: for example,
+// a HOFIfElseGr star can control a recursive reference to the current
+// galaxy.  The IfElse star is guaranteed to get control before the
+// subgalaxy does, so it can delete the subgalaxy to stop the recursion.
+// The second pass must preinit all blocks in case a non-atomic block
+// adds a block to the current galaxy.
+//
+// Also note that because this is called from Galaxy::initialize,
+// blocks in subgalaxies will see a preinitialize call during this pass
+// and then another one when the subgalaxy is actually initialized.
+//
+// For both this reason and the one above, blocks must act safely if
+// preinitialized multiple times.  (HOF stars generally destroy themselves
+// when preinitialized, so they can't see extra calls.)
+//
+// If Wormholes passed through preinitialize calls, it'd be necessary
+// to discriminate against wormholes in the first pass to prevent infinite
+// recursion through a wormhole.  But currently, they don't and it isn't.
+
+void Galaxy :: preinitialize() {
+	// Initialize galaxy's states in case stars refer to them
+	initState();
+	// Preinit stars only
+	GalTopBlockIter next(*this);
+	Block *b;
+	while ((b = next++) && !Scheduler::haltRequested()) {
+	    if (b->isItAtomic() && !blocksToDelete.isInList(*b))
+		b->preinitialize();
+	}
+	// Preinit everyone
+	GalTopBlockIter next2(*this);
+	while ((b = next2++) && !Scheduler::haltRequested()) {
+	    if (!blocksToDelete.isInList(*b))
+		b->preinitialize();
+	}
+	if (Scheduler::haltRequested()) return;
+	// Delete anything we've been told to delete.
+	// We do this now, rather than allowing them to hang around until
+	// initSubblocks is called, to ensure that InterpGalaxy::preinitialize
+	// won't do something wrong if called again.
+	BlockListIter bdel(blocksToDelete);
+	Block *bd;
+	while ( (bd = bdel++) ) {
+	  removeBlock(*bd);
+	  LOG_DEL; delete bd;
+	}
+	blocksToDelete.initialize();
 }
 
 // this is separate so derived galaxies can have more control.
@@ -144,10 +202,11 @@ void Galaxy :: initSubblocks() {
 	GalTopBlockIter next(*this);
 	Block *b;
 	while ((b = next++) && !Scheduler::haltRequested()) {
-	    if (!blocksToDelete.blockWithName(b->name()))
+	    if (!blocksToDelete.isInList(*b))
 		b->initialize();
 	}
 	if (Scheduler::haltRequested()) return;
+	// Delete anything we've been told to delete
 	BlockListIter bdel(blocksToDelete);
 	Block *bd;
 	while ( (bd = bdel++) ) {
@@ -160,7 +219,7 @@ void Galaxy :: initSubblocks() {
 // Add a block to the list of blocks to be deleted after initialization
 void Galaxy::deleteBlockAfterInit(Block& b) {
   // Check to make sure the block is not already there
-  if(!blocksToDelete.blockWithName(b.name()))
+  if (!blocksToDelete.isInList(b))
      blocksToDelete.put(b);
 }
 
@@ -245,5 +304,42 @@ Galaxy::domain() const {
 	else return blocks.head()->domain();
 }
 
+// Return the largest KnownBlock serial number found within
+// the galaxy.  Optionally scan subgalaxies.
+// NOTE: the contents of wormholes are NOT recursively examined.
+// Should they be?
+long
+Galaxy::maxContainedSerialNumber (int recurse)
+{
+  long maxSN = 0;
+  GalTopBlockIter next(*this);
+  Block* b;
+  while ((b = next++) != 0) {
+    long thisSN = KnownBlock::serialNumber(*b);
+    if (thisSN > maxSN)
+      maxSN = thisSN;
+    if (recurse && ! b->isItAtomic()) {
+      long innerSN = b->asGalaxy().maxContainedSerialNumber(recurse);
+      if (innerSN > maxSN)
+	maxSN = innerSN;
+    }
+  }
+  return maxSN;
+}
+
 // isa
 ISA_FUNC(Galaxy,Block);
+
+// BlockList stuff
+
+// See if Block is present in list
+int BlockList :: isInList (const Block& b) const
+{
+	const Block *obj;
+	CBlockListIter next(*this);
+	while ((obj = next++) != 0) {
+		if (obj == &b)
+			return 1;
+	}
+	return 0;
+}
