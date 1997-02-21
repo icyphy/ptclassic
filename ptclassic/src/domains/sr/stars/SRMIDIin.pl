4c14c74da92073913655a057eaaf3f430bada027
sedwards <sedwards@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-02-21 16:47:37 +0000

    Changed to single channel output.  Added controllers.

--- ptclassic/src/domains/sr/stars/SRMIDIin.pl
+++ ptclassic/src/domains/sr/stars/SRMIDIin.pl
@@ -21,6 +21,12 @@ limitation of liability, and disclaimer of warranty provisions.
     }
 
     output {
+      name { channel }
+      type { int }
+      desc { Channel (0-15) of the current command }
+    }
+
+    output {
 	name { onPitch }
 	type { int }
 	desc { Pitch of a Note On command (0-127, 60 is middle C) }
@@ -29,11 +35,6 @@ limitation of liability, and disclaimer of warranty provisions.
 	name { onVelocity }
 	type { int }
 	desc { Velocity of a Note On command (0-127) }
-    }
-    output {
-	name { onChannel }
-	type { int }
-	desc { Channel of a Note On command (0-15) }
     }
 
     output {
@@ -46,21 +47,23 @@ limitation of liability, and disclaimer of warranty provisions.
 	type { int }
 	desc { Velocity of a Note Off command (0-127) }
     }
-    output {
-	name { offChannel }
-	type { int }
-	desc { Channel of a Note Off command (0-15) }
-    }
 
     output {
 	name { pitchBend }
 	type { int }
 	desc { Pitch bend value, 8192 is neutral }
     }
+
     output {
-	name { pitchChannel }
-	type { int }
-	desc { Channel of the Pitch Bend command (0-15) }
+      name { controller }
+      type { int }
+      desc { Controller number (0-127) }
+    }
+
+    output {
+      name { controlValue }
+      type { int }
+      desc { Value of the controller (0-127) }
     }
 
     output {
@@ -84,10 +87,10 @@ limitation of liability, and disclaimer of warranty provisions.
     }
 
     state {
-      name { lastPitch }
+      name { lastByte }
       type { int }
-      default { "60" }
-      desc { The most recently observed pitch value }
+      default { "0" }
+      desc { The last MIDI data value }
     }
 
     private {
@@ -113,14 +116,21 @@ limitation of liability, and disclaimer of warranty provisions.
       enum statusBytes {
 
 	// Voices
+	// (Here, the least significant bit distinguishes even and odd-numbered
+	// data bytes)
 
-	NoteOff = 0x80,
-	NoteOn = 0x90,
-	NoteAftertouch = 0xa0,
-	SetParameter = 0xb0,
+	NoteOffPitch = 0x80,
+	NoteOffVelocity = 0x81,
+	NoteOnPitch = 0x90,
+	NoteOnVelocity = 0x91,
+	NoteAftertouchPitch = 0xa0,
+	NoteAftertouchVelocity = 0xa1,
+	ControlChangeController = 0xb0,
+	ControlChangeValue = 0xb1,
 	ProgramChange = 0xc0,
 	ChannelAftertouch = 0xd0,
-	ChannelPitchwheel = 0xe0,
+	ChannelPitchwheelLSB = 0xe0,
+	ChannelPitchwheelMSB = 0xe1,
 
 	// System Common
 
@@ -192,60 +202,52 @@ limitation of liability, and disclaimer of warranty provisions.
 
 	    int byte = int(byteNum);
 
-	    switch ( status & 0xf0 ) {
-	    case NoteOff:
+	    switch ( status & 0xf0 | byte ) {
 
-	      if ( byte == 0 ) {
-		// Remember the pitch value
-		lastPitch = inVal;
-		nextByteNum = 1;
-	      } else {
-		// This byte is the velocity -- emit everything
-		offPitch.emit() << int(lastPitch);
-		offVelocity.emit() << inVal;
-		offChannel.emit() << int(status & 0xf);
-		nextByteNum = 0;
-	      }
-
+	    case NoteOffPitch:
+	    case NoteOnPitch:
+	    case ControlChangeController:
+	    case ChannelPitchwheelLSB:
+	      // Save the even-numbered byte
+	      lastByte = inVal;
+	      nextByteNum = 1;
 	      break;
 
-	    case NoteOn:
-
-	      if ( byte == 0 ) {
-		// Remember the pitch value
-		lastPitch = inVal;
-		nextByteNum = 1;
-	      } else {
-		// This byte is the velocity -- emit everything
-		if ( inVal == 0 ) {
-		  // Velocity is zero -- emit a NoteOff command
-		  offPitch.emit() << int(lastPitch);
-		  offVelocity.emit() << 64;
-		  offChannel.emit() << int(status & 0xf);
-		} else {
-		  // Velocity is non-zero -- emit a NoteOn command
-		  onPitch.emit() << int(lastPitch);
-		  onVelocity.emit() << inVal;
-		  onChannel.emit() << int(status & 0xf);
-		}
-		nextByteNum = 0;
-	      }
-
+	    case NoteOffVelocity:
+	      // Emit Note Off
+	      offPitch.emit() << int(lastByte);
+	      offVelocity.emit() << inVal;
+	      channel.emit() << int(status & 0xf);
+	      nextByteNum = 0;
 	      break;
 
-	    case ChannelPitchwheel:
-
-	      if ( byte == 0 ) {
-		// Remember the least significant seven bits
-		lastPitch = inVal;
-		nextByteNum = 1;
+	    case NoteOnVelocity:
+	      channel.emit() << int(status & 0xf);	      
+	      if ( inVal == 0 ) {
+		// Velocity is zero -- emit a NoteOff command
+		offPitch.emit() << int(lastByte);
+		offVelocity.emit() << 64;
 	      } else {
-		// This byte is the most significant -- emit everything
-		pitchBend.emit() << ((int(inVal) << 7) | lastPitch);
-		pitchChannel.emit() << int(status & 0xf);
-		nextByteNum = 0;
+		// Velocity is non-zero -- emit a NoteOn command
+		onPitch.emit() << int(lastByte);
+		onVelocity.emit() << inVal;
 	      }
+	      nextByteNum = 0;
+	      break;
 
+	    case ControlChangeValue:
+	      // Emit Control Change
+	      controller.emit() << int(lastByte);
+	      controlValue.emit() << inVal;
+	      channel.emit() << int(status & 0xf);
+	      nextByteNum = 0;	   
+	      break;
+	      
+	    case ChannelPitchwheelMSB:
+	      // Emit pitch bend
+	      pitchBend.emit() << ((int(inVal) << 7) | lastByte);
+	      channel.emit() << int(status & 0xf);
+	      nextByteNum = 0;
 	      break;
 
 	    default:
@@ -255,14 +257,19 @@ limitation of liability, and disclaimer of warranty provisions.
 	}
       }
 
+      if ( !channel.known() )		{ channel.makeAbsent(); }
+
       if ( !onPitch.known() )		{ onPitch.makeAbsent(); }
       if ( !onVelocity.known() )	{ onVelocity.makeAbsent(); }
-      if ( !onChannel.known() )		{ onChannel.makeAbsent(); }
+
       if ( !offPitch.known() )		{ offPitch.makeAbsent(); }
       if ( !offVelocity.known() )	{ offVelocity.makeAbsent(); }
-      if ( !offChannel.known() )	{ offChannel.makeAbsent(); }
+
       if ( !pitchBend.known() )		{ pitchBend.makeAbsent(); }
-      if ( !pitchChannel.known() )	{ pitchChannel.makeAbsent(); }
+
+      if ( !controller.known() )	{ controller.makeAbsent(); }
+      if ( !controlValue.known() )	{ controlValue.makeAbsent(); }
+
       if ( !reset.known() )		{ reset.makeAbsent(); }
 
     }
