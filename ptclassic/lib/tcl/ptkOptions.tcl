8be111d2f43eb72a0ed28d07947ec795620651f2
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
1997-02-23 22:55:11 +0000

    Grey color scheme.

--- ptclassic/lib/tcl/ptkOptions.tcl
+++ ptclassic/lib/tcl/ptkOptions.tcl
@@ -30,6 +30,10 @@
 # 						COPYRIGHTENDKEY
 
 ############################################################################
+# FIXME: This file is largely obsolete.  The Tycho preferences manager has
+# taken over most of the functionality.  It is kept for backward compatibility
+# with older widgets.
+#
 # The following section defines options (X resources) that get used indirectly
 # to set options associated with window classes or specific window
 # names.  The idea is to have all particular uses of a color consolidated
@@ -59,20 +63,20 @@
 #option add Pigi.mediumfont *-times-medium-r-normal--*-180-*-*-*-*-iso8859 startupfile
 #option add CGC.mediumfont  *-times-medium-r-normal--*-180-*-*-*-*-iso8859 startupfile
 
-option add Pigi.mediumfont *-times-medium-r-normal--*-180-* startupFile
+# option add Pigi.mediumfont *-times-medium-r-normal--*-180-* startupFile
 option add CGC.mediumfont *-times-medium-r-normal--*-180* startupFile
 
 # Font for major titles.  It is very large.
 # HPUX wants the following two lines, but we cannot specify them everywhere
 #option add Pigi.bigfont *-Helvetica-Bold-R-Normal-*-240-*-*-*-*-iso8859-*
 #option add CGC.bigfont  *-Helvetica-Bold-R-Normal-*-240-*-*-*-*-iso8859-*
-option add Pigi.bigfont  *-Helvetica-Bold-R-Normal-*-240-* startupFile
+# option add Pigi.bigfont  *-Helvetica-Bold-R-Normal-*-240-* startupFile
 option add CGC.bigfont  *-Helvetica-Bold-R-Normal-*-240-* startupFile
 
 #Define Special Colors
 option add Pigi.positiveColor [ptkColor blue] startupFile
 option add Pigi.negativeColor [ptkColor red] startupFile
-option add Pigi.highlightColor [ptkColor firebrick] startupFile
+# option add Pigi.highlightColor [ptkColor firebrick] startupFile
 
 #Colors used for plots with multiple traces
 option add *plotColor1 [ptkColor red3] startupFile
@@ -100,41 +104,43 @@ option add Pigi.scaleWidthInC 7.0 startupFile
 # automatically get the the specified colors and fonts.
 ############################################################################
 
-# Standard default colors for standard Tk widgets
-option add Pigi*Text.Background [ptkColor antiqueWhite] startupFile
-option add Pigi*Entry.Background [ptkColor wheat3] startupFile
-option add Pigi*selectForeground [ptkColor midnightblue] startupFile
-option add Pigi*Canvas.background [ptkColor antiqueWhite3] startupFile
-option add Pigi*Button.foreground [ptkColor firebrick] startupFile
-option add Pigi*Scale.Foreground [ptkColor bisque1] startupFile
-option add Pigi*troughColor [ptkColor tan4] startupFile
-option add Pigi*Scale.background [ptkColor bisque] startupFile
-option add Pigi*Scale.activeBackground [ptkColor bisque2] startupFile
-option add Pigi*Scale.background [ptkColor tan4] startupFile
-option add Pigi*Scale.sliderForeground [ptkColor bisque] startupFile
-option add Pigi*Scale.activeForeground [ptkColor bisque2] startupFile
+# Standard default colors for standard Tk widgets.
+# We have kept a minimal set in version 0.7, with the idea that even these
+# will eventually disappear.
+# These now match the default preferences in Tycho.
+option add Pigi*Text.Background [ptkColor grey93] startupFile
+option add Pigi*Entry.Background [ptkColor grey90] startupFile
+# option add Pigi*selectForeground [ptkColor midnightblue] startupFile
+option add Pigi*Canvas.background [ptkColor azure3] startupFile
+# option add Pigi*Button.foreground [ptkColor firebrick] startupFile
+# option add Pigi*Scale.Foreground [ptkColor bisque1] startupFile
+option add Pigi*troughColor [ptkColor grey90] startupFile
+# option add Pigi*Scale.background [ptkColor bisque] startupFile
+# option add Pigi*Scale.activeBackground [ptkColor bisque2] startupFile
+# option add Pigi*Scale.background [ptkColor tan4] startupFile
+# option add Pigi*Scale.sliderForeground [ptkColor bisque] startupFile
+# option add Pigi*Scale.activeForeground [ptkColor bisque2] startupFile
 
 # Make the Tk 4.0 widget traversal ring match the default background
-option add Pigi*highlightBackground [option get . background Pigi] startupFile
+# option add Pigi*highlightBackground [option get . background Pigi] startupFile
 
 # Default font for all message windows used in Pigi
-option add Pigi*Message*Font [option get . mediumfont Pigi] startupFile
+# option add Pigi*Message*Font [option get . mediumfont Pigi] startupFile
 
 # The following class is used to particularly highlight a widget,
 # for example to call attention to a button that must be pushed
 # in order to be able to continue with the program.
 option add Pigi*Attention*Button.foreground [ptkColor firebrick] startupFile
 option add Pigi*Attention*Button.activeForeground [ptkColor black] startupFile
-option add Pigi*Attention*Button.background [ptkColor burlywood1] startupFile
-option add Pigi*Attention*Button.activeBackground [ptkColor burlywood3] startupFile
+option add Pigi*Attention*Button.background [ptkColor azure1] startupFile
+option add Pigi*Attention*Button.activeBackground [ptkColor azure3] startupFile
 option add Pigi*Attention*Button.disabledForeground [ptkColor black] startupFile
-
 
 
 # Set up defaults for the Windows generated by CGC programs
 # #########################################################
 
-option add *CGC*Message*Font [option get . mediumfont Pigi] startupFile
+# option add *CGC*Message*Font [option get . mediumfont Pigi] startupFile
 
 option add *CGC*Background [ptkColor antiqueWhite2] startupFile
 option add *CGC*Text.Background [ptkColor antiqueWhite] startupFile
