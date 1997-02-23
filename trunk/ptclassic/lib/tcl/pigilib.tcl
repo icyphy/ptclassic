bc49fc0d4c278297369f4ecc194280cc2736d752
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-02-23 19:54:40 +0000

    Added comment about tk_bisque

--- trunk/ptclassic/lib/tcl/pigilib.tcl
+++ trunk/ptclassic/lib/tcl/pigilib.tcl
@@ -87,6 +87,9 @@ pigilib_init_env
 
 # Change color scheme to bisque
 tk_bisque
+# FIXME: If we comment out the tk_bisque line above, then pigi 
+# fails to start.
+#option add Pigi*highlightBackground #ffe4c4
 
 # Load the help system for ptcl
 source $ptolemy/lib/tcl/ptcl_help.tcl
