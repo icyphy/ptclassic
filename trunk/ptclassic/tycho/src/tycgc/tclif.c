28c619467a80c7c792ea10d565bd83de7b518590
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-03-03 14:49:58 +0000

    Made tclinterface() static.

--- trunk/ptclassic/tycho/src/tycgc/tclif.c
+++ trunk/ptclassic/tycho/src/tycgc/tclif.c
@@ -43,73 +43,79 @@ ENHANCEMENTS, OR MODIFICATIONS.
  * name of a Tcl variable which will be updated with the value of the
  * counter each time the task executes.
  */
- int
- tclinterface(ClientData dummy, Tcl_Interp *interp, int argc, char **argv) {
-     /* Check number of arguments */
-     if (argc < 2) {
-         sprintf(interp->result, "CGC Module %s: mode not specified", moduleName );
-         return TCL_ERROR;
-     }
-     if (argc > 3) {
-         sprintf(interp->result, "Too many arguments");
-         return TCL_ERROR;
-     }
+static int
+tclinterface(ClientData dummy, Tcl_Interp *interp, int argc, char **argv) {
+  /* Check number of arguments */
+  if (argc < 2) {
+    sprintf(interp->result, "CGC Module %s: mode not specified", moduleName );
+    return TCL_ERROR;
+  }
+  if (argc > 3) {
+    sprintf(interp->result, "Too many arguments");
+    return TCL_ERROR;
+  }
      
-     /* Switch on the first argument */
-     if ( ! strcmp(argv[1], "scriptfile") ) {
-         interp->result = scriptFile;
-         return TCL_OK;
+  /* Switch on the first argument */
+  if ( ! strcmp(argv[1], "scriptfile") ) {
+    /* Return the name of the script to create the user interface */
+    interp->result = scriptFile;
+    return TCL_OK;
 
-     } else if ( ! strcmp(argv[1], "setup") ) {
-         /* If a counter name is supplied, remember it */
-         if ( argc > 2 ) {
-             strcpy(tclcountername,argv[2]);
-             updatetclcounter = 1;
-         } else {
-             updatetclcounter = 0;
-         }
-         /* Call the setup function */
-         setup();
-         return TCL_OK;
+  } else if ( ! strcmp(argv[1], "init") ) {
+    /* Call the function to register callbacks */
+    tychoSetup();
+    return TCL_OK;
+
+  } else if ( ! strcmp(argv[1], "setup") ) {
+    /* If a counter name is supplied, remember it */
+    if ( argc > 2 ) {
+      strcpy(tclcountername,argv[2]);
+      updatetclcounter = 1;
+    } else {
+      updatetclcounter = 0;
+    }
+    /* Call the setup function */
+    setup();
+    return TCL_OK;
          
-     } else if ( ! strcmp(argv[1], "execute") ) {
-         int timedout;
+  } else if ( ! strcmp(argv[1], "execute") ) {
+    int timedout;
          
          /* Start the timer */
-         Ty_TimerStart();
+    Ty_TimerStart();
          
-         /* Call the execute function, which must check Ty_TimerElapsed()
+    /* Call the execute function, which must check Ty_TimerElapsed()
          * return when it returns 1
          */
-         timedout = execute();
+    timedout = execute();
          
-         /* If there is a tcl counter, update it with the iteration count */
-         if ( updatetclcounter ) {
-             char buffer[80];
-             sprintf(buffer,"%d", iterationCounter);
-             Tcl_SetVar(interp, tclcountername, buffer, 0);
-         }
+    /* If there is a tcl counter, update it with the iteration count */
+    if ( updatetclcounter ) {
+      char buffer[80];
+      sprintf(buffer,"%d", iterationCounter);
+      Tcl_SetVar(interp, tclcountername, buffer, 0);
+    }
          
-         /* Return "1" if the timer timed out so we know that the task
+    /* Return "1" if the timer timed out so we know that the task
          * didn't terminate; return "0" if the iterayion count was reached
          * so the scheduler can delete the task
          */
-         if ( timedout ) {
-             interp->result = "1";
-         } else {
-             interp->result = "0";
-         }
-         return TCL_OK;
+    if ( timedout ) {
+      interp->result = "1";
+    } else {
+      interp->result = "0";
+    }
+    return TCL_OK;
          
-     } else if ( ! strcmp(argv[1], "wrapup") ) {
-         /* Call the wrapup function */
-         wrapup();
-         return TCL_OK;
+  } else if ( ! strcmp(argv[1], "wrapup") ) {
+    /* Call the wrapup function */
+    wrapup();
+    return TCL_OK;
          
-     }
+  }
 
-     /* If we get to here, something went wrong */
-     sprintf(interp->result, "Invalid mode in module %s: %s", argv[1], moduleName);
-     return TCL_ERROR;
- }
+  /* If we get to here, something went wrong */
+  sprintf(interp->result, "Invalid mode in module %s: %s", argv[1], moduleName);
+  return TCL_ERROR;
+}
 
