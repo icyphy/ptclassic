5a3bfe891a174313d6efd1fc2a29bea1bc2b2404
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-03-03 10:44:58 +0000

    Initial revision

---
+++ ptclassic/tycho/typt/demo/graphic.tcl
@@ -0,0 +1,42 @@
+# The control panel for a ten-band graphic equalizer demo
+#
+# @Author: John Reekie
+#
+# @Version: $Id$
+#
+# @Copyright (c) 1996-1997 The Regents of the University of California.
+# All rights reserved.
+#
+# Permission is hereby granted, without written agreement and without
+# license or royalty fees, to use, copy, modify, and distribute this
+# software and its documentation for any purpose, provided that the above
+# copyright notice and the following two paragraphs appear in all copies
+# of this software.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+# FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+# THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+# PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+# CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+# ENHANCEMENTS, OR MODIFICATIONS.
+# 
+#                                        PT_COPYRIGHT_VERSION_2
+#                                        COPYRIGHTENDKEY
+#######################################################################
+
+# Turn on debugging
+# controlPanel debug 1
+
+# Create the AudioIO control sub-panel and add to the main panel
+::tycho::AudioIO controlPanel.io
+controlPanel add controlPanel.io -side left
+
+# Create the ten-band graphic eq sub-panel and add to the main panel
+::tycho::GraphicEQ controlPanel.eq
+controlPanel add controlPanel.eq -side left
