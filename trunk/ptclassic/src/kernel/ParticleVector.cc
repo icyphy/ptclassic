d7cd5e5cbbe867f551aca647345dcf1a0eb71f64
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-03-21 17:13:18 +0000

    Fixed sccs keywords

--- trunk/ptclassic/src/kernel/ParticleVector.cc
+++ trunk/ptclassic/src/kernel/ParticleVector.cc
@@ -1,9 +1,9 @@
 static const char file_id[] = "ParticleVector.cc";
 /**************************************************************************
 Version identification:
-@(#)ParticleVector.cc	1.3	03/19/97
+$Id$
 
-Copyright (c) 1990- The Regents of the University of California.
+Copyright (c) 1996-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
