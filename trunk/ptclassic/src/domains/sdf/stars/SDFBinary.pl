44499effbbbd722a2c0d78b2b20cffd9431c93e3
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-03-27 01:35:59 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/sdf/stars/SDFBinary.pl
@@ -0,0 +1,53 @@
+defstar {
+	name {Binary}
+	domain {SDF}
+	desc {
+Binary threshold of the input.  If the input is greater than zero,
+then a 1 is output; otherwise, the output is 0 if outputBipolarValue
+is False or -1 otherwise.
+	}
+	author { Biao Lu }
+	acknowledge { Brian L. Evans }
+	version { $Revision$	$Date$ }
+	location { SDF main library }
+	copyright {
+Copyright (c) 1997 The University of Texas.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+        }
+	explanation {
+Binary and bipolar are two kinds of the activation functions.
+The output of the binary is either 1 or 0, and the output of the bipolar
+is either 1 or -1.
+	}
+	input {
+		name{ input }
+		type{ float }
+	}
+	output {
+		name { output }
+		type { float }
+	}
+	private {
+		double falseValue;
+	}
+	state {
+	    name { outputBipolarValue }
+	    type { int }
+	    default { NO }
+	    desc {
+If true, output -1 if the input is negative and 1 otherwise.
+If false, output 0 if the input is negative and 1 otherwise.
+	    }
+	}
+	setup {
+	    falseValue = ( int(outputBipolarValue) ? -1.0 : 0.0 );
+	}
+	go {
+	   if (double(input%0) > 0.0)
+		output%0 << 1.0;
+	   else 
+		output%0 << falseValue;
+       }
+}
---
+++ trunk/ptclassic/src/domains/sdf/stars/SDFMPNeuron.pl
@@ -0,0 +1,94 @@
+defstar {
+	name {MPNeuron}
+	domain {SDF}
+	desc {
+This is a McCulloch-Pitts neuron. The activation of this neuron is binary.
+That is, at any time step, the neuron either fires, or does not fires.
+	}
+	author {Biao Lu}
+	acknowledge { Brian L. Evans }
+       	location { SDF main library }
+	version { $Revision$	$Date$ }
+	copyright {
+Copyright (c) 1997 The University of Texas.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
+	explanation {
+This MpNeuron is based on the McCulloch-Pitts model of artificial neurons [1].
+
+.UH REFERENCES
+.IP [1]
+W. S. McCulloch and W. Pitts, "A logical calculus of the ideas immanent 
+in nervous activity", Bulletin of Mathematical Biophysics, Vol. 5, 
+pp. 115-133, 1943
+	}
+	inmulti {
+		name { input }
+		type { float }
+		desc {
+The neuron can have the multiple inputs with the different weights.
+		}
+	}
+	output {
+		name { output }
+		type { float }
+		desc {
+The output will be only one which is the sum of the weighted inputs.
+		}
+	}
+	state {
+	    name { weights }
+	    type { floatarray }
+	    default {"1.0 1.0 1.0 1.0"}
+	    desc {
+Weights are associated with the connections between the two layers.
+They can be input by the users. All the weights must be precalculated.
+The number of the weights must be equal to the number of the inputs.
+	    }
+	}
+	state {
+	    name { number }
+	    type { int }
+	    default { "10" }
+	    desc { This number control the size of array. }
+	}
+	state {
+	    name { threshold }
+	    type { float }
+	    default { 1.0 }
+	    desc {
+This is the threshold for each neuron. If the sum of the weighted signal
+is greater than or equal to this threshold, the neuron will be fired.
+	    }
+	}
+	setup {
+		if ( input.numberPorts() != weights.size() ) {
+		    StringList msg = "The number of inputs ";
+		    msg << input.numberPorts()
+		        << " do not equal the number of weights "
+			<< weights.size();
+		    Error::abortRun(*this, msg);
+		    return;
+		}
+	}
+	go {
+		MPHIter nexti(input);
+		PortHole *p;
+		double out = 0.0;
+		int k = 0;
+		while ((p = nexti++) != 0 ) {
+		    double kthInput = double((*p)%0);
+		    out += weights[k] * kthInput;
+		    k++;
+		}
+
+		if (out >= double(threshold))
+		    out = 1.0;
+		else
+		    out = 0.0;
+
+		output%0 << out;
+	}
+}
