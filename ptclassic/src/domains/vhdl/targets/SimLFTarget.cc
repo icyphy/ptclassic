4ca8484b59c0e9d4b27e9e8d6163cb49e1d26c30
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-03-27 21:58:12 +0000

    Fixed version control info

--- ptclassic/src/domains/vhdl/targets/SimLFTarget.cc
+++ ptclassic/src/domains/vhdl/targets/SimLFTarget.cc
@@ -1,9 +1,9 @@
 static const char file_id[] = "SimLFTarget.cc";
 /******************************************************************
 Version identification:
-@(#)SimLFTarget.cc	1.51 01/30/97
+$Id$
 
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
