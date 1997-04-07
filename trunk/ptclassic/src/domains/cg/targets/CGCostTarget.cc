62651b5b711ac91559b9da3a3f5c4fef184fd97a
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-04-07 14:04:45 +0000

    Added 1996 to copyright

--- trunk/ptclassic/src/domains/cg/targets/CGCostTarget.cc
+++ trunk/ptclassic/src/domains/cg/targets/CGCostTarget.cc
@@ -1,6 +1,6 @@
 static const char file_id[] = "CGCostTarget.cc";
 /**********************************************************************
-Copyright (c) %Q% The Regents of the University of California.
+Copyright (c) 1996-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
