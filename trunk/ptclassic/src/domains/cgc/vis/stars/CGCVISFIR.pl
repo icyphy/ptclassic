3051853d5770f7bada2c120cefb8a16e6c2dd911
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-04-07 14:22:22 +0000

    Copyright 1996

--- trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISFIR.pl
+++ trunk/ptclassic/src/domains/cgc/vis/stars/CGCVISFIR.pl
@@ -4,7 +4,7 @@ defstar {
 	version { $Id$ }
 	author { William Chen, John Reekie, and Edward Lee}
 	copyright {
-Copyright (c) 1996-1996 The Regents of the University of California.
+Copyright (c) 1996-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
