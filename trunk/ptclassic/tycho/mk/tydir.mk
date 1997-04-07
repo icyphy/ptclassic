24a442d7b60cd5338bfb347006899784af6c94db
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-04-07 15:19:52 +0000

    Copyright 1996

--- trunk/ptclassic/tycho/mk/tydir.mk
+++ trunk/ptclassic/tycho/mk/tydir.mk
@@ -3,7 +3,7 @@
 # Version identification:
 # $Id$
 #
-# Copyright (c) %Q% The Regents of the University of California.
+# Copyright (c) 1996-%Q% The Regents of the University of California.
 # All rights reserved.
 # 
 # Permission is hereby granted, without written agreement and without
