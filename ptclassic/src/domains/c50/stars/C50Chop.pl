da014d38fca58aec00f2900c126485fd01ca3805
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-04-08 14:57:06 +0000

    copyright dates

--- ptclassic/src/domains/c50/stars/C50Chop.pl
+++ ptclassic/src/domains/c50/stars/C50Chop.pl
@@ -16,7 +16,7 @@ input particles will be discarded.
 	version { $Id$}
 	author { Luis Gutierrez, based on CG56 version }
 	copyright {
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
