3a2987182156419da627e4a9475d65902bce95c3
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-04-08 13:30:22 +0000

    copyright dates

--- ptclassic/src/domains/cgc/tcltk/lib/ptolemy-cgc.tcl
+++ ptclassic/src/domains/cgc/tcltk/lib/ptolemy-cgc.tcl
@@ -4,7 +4,7 @@
 # Author: Edward A. Lee
 # Version: $Id$
 #
-# Copyright (c) 1990-1996 The Regents of the University of California.
+# Copyright (c) 1990-%Q% The Regents of the University of California.
 # All rights reserved.
 # 
 # Permission is hereby granted, without written agreement and without
