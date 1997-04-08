e83f61032604ed5e8895d7277bcd427fef4185de
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-04-08 14:59:30 +0000

    copyright date

--- ptclassic/src/domains/c50/stars/C50Pulse.pl
+++ ptclassic/src/domains/c50/stars/C50Pulse.pl
@@ -5,7 +5,7 @@ defstar {
 	version { $Id$ }
 	author { Luis Gutierrez, based on CG56 version }
 	copyright {
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
