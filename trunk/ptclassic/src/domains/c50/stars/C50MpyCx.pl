bc45825e6d531215219d7924d953203b3b4ad2d2
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-04-08 15:03:55 +0000

    Copyright 1996

--- trunk/ptclassic/src/domains/c50/stars/C50MpyCx.pl
+++ trunk/ptclassic/src/domains/c50/stars/C50MpyCx.pl
@@ -7,7 +7,7 @@ Multiply any number of rectangular complex inputs, producing an output.
     version { $Id$ }
     author { Luis Gutierrez }
     copyright {
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
--- trunk/ptclassic/src/domains/vhdl/kernel/VHDLToken.cc
+++ trunk/ptclassic/src/domains/vhdl/kernel/VHDLToken.cc
@@ -3,7 +3,7 @@ static const char file_id[] = "VHDLToken.cc";
 Version identification:
 $Id$
 
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
