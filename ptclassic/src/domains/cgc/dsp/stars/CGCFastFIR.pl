684205faddd3748a936bd024aeed089e59ae6d75
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-04-08 15:03:55 +0000

    Copyright 1996

--- ptclassic/src/domains/cgc/dsp/stars/CGCFastFIR.pl
+++ ptclassic/src/domains/cgc/dsp/stars/CGCFastFIR.pl
@@ -11,7 +11,7 @@ coefficients with "fileName".
 	version {$Id$}
 	author { Soonhoi Ha, Bill Chen, and John Reekie }
 	copyright {
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
