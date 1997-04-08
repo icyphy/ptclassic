dbcdcf9e8be1ac15f946693fb220c525cea627ff
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-04-08 12:08:49 +0000

    copyright dates

--- trunk/ptclassic/src/domains/sdf/dsp/stars/SDFKalman_M.pl
+++ trunk/ptclassic/src/domains/sdf/dsp/stars/SDFKalman_M.pl
@@ -5,7 +5,7 @@ defstar {
   version   { $Id$ }
   author    { Mike J. Chen }
   copyright {
-Copyright (c) 1990-1995 The Regents of the University of California.
+Copyright (c) 1993-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
