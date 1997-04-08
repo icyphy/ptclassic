5194058568a146ebd0a479ef066b548ab9759c8e
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-04-08 13:14:57 +0000

    Copyright 1996

--- trunk/ptclassic/src/domains/sr/stars/SRAdd.pl
+++ trunk/ptclassic/src/domains/sr/stars/SRAdd.pl
@@ -7,7 +7,7 @@ Add the two inputs.
     version { @(#)SRAdd.pl	1.3 4/19/96 }
     author { S. A. Edwards }
     copyright {
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1996-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
