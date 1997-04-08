79408df9309b71a6f6ea3459b139fea4cc3021ab
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-04-08 14:02:14 +0000

    Copyright date

--- ptclassic/tycho/kernel/basic/Math.tcl
+++ ptclassic/tycho/kernel/basic/Math.tcl
@@ -5,7 +5,7 @@
 #
 # Version: $Id$
 #
-# Copyright (c) 1990-1996 The Regents of the University of California.
+# Copyright (c) 1996-%Q% The Regents of the University of California.
 # All rights reserved.
 # 
 # Permission is hereby granted, without written agreement and without
