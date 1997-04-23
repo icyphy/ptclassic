ce903f51adc074741ed9b1fec126c9c2a735f5c6
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-04-23 15:48:15 +0000

    Wholesale changes to make it more like the SDF and CGC LMS stars.

--- ptclassic/src/domains/vhdl/stars/VHDLLMS.pl
+++ ptclassic/src/domains/vhdl/stars/VHDLLMS.pl
@@ -5,7 +5,7 @@ defstar {
 Adaptive filter using LMS adaptation algorithm. 
 }
 	version { $Id$ }
-	author { E. A. Lee, A. P. Kalavade, M. C. Williamson }
+	author { M.C. Williamson, E.A. Lee, A.P. Kalavade }
 	copyright {
 Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
@@ -15,57 +15,23 @@ limitation of liability, and disclaimer of warranty provisions.
 	location { VHDL main library }
 	input {
 		name { signalIn }
-		type { int }
+		type { float }
 	}
 	input {
                 name { error }
-                type { int }
+                type { float }
         }
 	output {
 		name { signalOut }
-		type { int }
+		type { float }
 	}
 	defstate {
-		name { delay1 }
-		type { int }
-		default { 0 }
-		desc { Input stream delayed by 1. }
-	}
-	defstate {
-		name { delay2 }
-		type { int }
-		default { 0 }
-		desc { Input stream delayed by 2. }
-	}
-	defstate {
-		name { delay3 }
-		type { int }
-		default { 0 }
-		desc { Input stream delayed by 3. }
-	}
-	defstate {
-		name { tap0 }
-		type { int }
-		default { 1 }
-		desc { filter tap 0 }
-	}
-	defstate {
-		name { tap1 }
-		type { int }
-		default { 1 }
-		desc { filter tap 1 }
-	}
-	defstate {
-		name { tap2 }
-		type { int }
-		default { 1 }
-		desc { filter tap 2 }
-	}
-	defstate {
-		name { tap3 }
-		type { int }
-		default { 1 }
-		desc { filter tap 3 }
+		name {taps}
+		type {floatarray}
+		default {
+	"-.040609 -.001628 .17853 .37665 .37665 .17853 -.001628 -.040609"
+		}
+		desc { Filter tap values. }
 	}
         defstate {
                 name { stepSize }
@@ -73,19 +39,38 @@ limitation of liability, and disclaimer of warranty provisions.
                 default { "0.01" }
                 desc { Adaptation step size. }
         }
-	codeblock(std)
-	{
-$ref(signalOut) $assign(signalOut) $ref(signalIn) * $ref(tap0) + $ref(delay1) *
-  $ref(tap1) + $ref(delay2) * $ref(tap2) + $ref(delay3) * $ref(tap3);
-$ref(delay3) $assign(delay3) $ref(delay2);
-$ref(delay2) $assign(delay2) $ref(delay1);
-$ref(delay1) $assign(delay1) $ref(signalIn);
-$ref(tap0) $assign(tap0) $ref(tap0) + $val(stepSize) * $ref(error) * $ref(signalIn);
-$ref(tap1) $assign(tap1) $ref(tap1) + $val(stepSize) * $ref(error) * $ref(delay1);
-$ref(tap2) $assign(tap2) $ref(tap2) + $val(stepSize) * $ref(error) * $ref(delay2);
-$ref(tap3) $assign(tap3) $ref(tap3) + $val(stepSize) * $ref(error) * $ref(delay3);
-	}
 	go {
-	  addCode(std);
+	  StringList out;
+	  int tapSize = taps.size();
+
+	  // Perform calculation.
+	  int j = 0;
+	  for (int i = 0; i < int(tapSize); i++) {
+	      if (i%10 == 0) {
+		j = i;
+		if (i!=0) out << ";\n";
+		StringList condText = "";
+		condText << "$temp(signalOut" << i-10 << ",float) + ";
+		out <<  "$temp(signalOut" << i << ",float) := "
+		    << (i != 0 ? (const char*) condText : "") 
+		    << "$ref(signalIn," << i << ")*$ref(taps," << i << ")";
+	      }
+	      else {
+		out << "+ $ref(signalIn," << i << ")*$ref(taps," 
+		    << i << ")";
+	      }
+	  }
+	  out << ";\n";
+	  for (int k = 0; k < int(tapSize); k++) {
+	    // Update the tap values.
+	    out << "$ref(taps," << k << ") $assign(taps) $ref(taps," << k
+		<< ") + $val(stepSize) * $ref(error) * $ref(signalIn,"
+		<< k << ");\n";
+	  }
+	  out << "$ref(signalOut) $assign(signalOut) $temp(signalOut"
+	      << j << ",float);\n";
+
+	  addCode(out);
+	  out.initialize();
 	}
 }
