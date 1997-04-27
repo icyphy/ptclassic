284997ebcfdb561cf8c394a91d324a2f6d384e3d
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-04-27 16:48:50 +0000

    Added function to create a Sun compatible header.

--- ptclassic/src/utils/libptdsp/ptdspMuLaw.c
+++ ptclassic/src/utils/libptdsp/ptdspMuLaw.c
@@ -27,9 +27,10 @@ ENHANCEMENTS, OR MODIFICATIONS.
 					PT_COPYRIGHT_VERSION_2
 					COPYRIGHTENDKEY
 
- Programmer: Joseph T. Buck and Craig Reese
+ Programmers: Joseph T. Buck, Craig Reese, Christopher Hylands
 
-       Functions for conversions from PCMMuLaw to Linear and back.
+       Functions for conversions from PCMMuLaw to Linear and back and to
+       write Sun compatible mu-law files.
 
        Provides functions for compression of 16-bit linear data to PCM
        8-bit mu-law  data and back. The 8-bit mu-law format here is
@@ -112,3 +113,128 @@ Ptdsp_PCMMuLawToLinear(unsigned char ulawbyte) {
   if ( sign != 0 ) sample = -sample;
   return sample;
 }
+
+
+/* Size of a Sun mulaw header */
+#define SUNMULAW_HEADER_SIZE 24
+
+/* Write a Sun .au file header.
+   This function should be called twice, once before the data has been
+   written so that the header can be inserted into the file,.
+   Once after the file has all its data so the size can be updated
+
+   fp should be opened with "r+", as this function both reads and writes
+   to fp.  See the fopen() man page for details.
+
+   The Sox tool (http://www.spies.com/Sox) says that the format of the
+   header is 6 unsigned big-endian 4 byte longs.
+
+   sox-11/au.c says:
+
+   > Any extra bytes (totalling hdr_size - 24) are an
+   > "info" field of unspecified nature, usually a string.
+   > By convention the header size is a multiple of 4.
+
+   Under Solaris, you can use 'od -t u4 foo.au' to look at the fields.
+ */
+int
+Ptdsp_WriteSunMuLawHeader(FILE *fp) {
+
+    /* The header is 6 unsigned longs: */
+    unsigned long magic = 0x2e736e64;	/* Really '.snd' */
+    unsigned long hdrsize = SUNMULAW_HEADER_SIZE;
+    unsigned long datasize = 0;
+    unsigned long encoding = 1;
+    unsigned long samplerate = 8000;
+    unsigned long channels = 1;
+
+    unsigned long *hdrsizeptr = &hdrsize;
+    unsigned long tmpmagic;		/* We read the magic cookie into this*/
+    unsigned long *tmpmagicptr = &tmpmagic;
+
+    long currentposition = ftell(fp);
+    long filelength;  
+
+
+    /*  Get the file length */
+    if ( fseek(fp, 0L, SEEK_END ) == -1) {
+      perror("Ptdsp_WriteSunMuLawHeader: failed to seek to end");
+      return 0;
+    }
+    filelength = ftell(fp);
+
+    if ( fseek(fp, 0L, SEEK_SET ) == -1) {
+      perror("Ptdsp_WriteSunMuLawHeader: failed to seek to start");
+      return 0;
+    }
+
+
+    if (currentposition == 0 && filelength == 0) {
+        /* Nothing has been written yet, write the header.
+	   In theory, we could have created a struct here and try
+	   writing that, but I don't think the order of struct elements
+	   would be guarranteed.
+	 */  
+        fwrite((const void *)&magic, sizeof(unsigned long), 1, fp);
+        fwrite((const void *)&hdrsize, sizeof(unsigned long), 1, fp);
+        fwrite((const void *)&datasize, sizeof(unsigned long), 1, fp);
+        fwrite((const void *)&encoding, sizeof(unsigned long), 1, fp);
+        fwrite((const void *)&samplerate, sizeof(unsigned long), 1, fp);
+        fwrite((const void *)&channels, sizeof(unsigned long), 1, fp );
+
+	return 1;
+    } else {
+        /* Check that the magic cookie is present. */
+
+	if ( fseek(fp, 0L, SEEK_SET ) == -1) {
+	    perror("Ptdsp_WriteSunMuLawHeader: failed to seek to start");
+	    return 0;
+	}
+
+	if ( fread((void *)tmpmagicptr, sizeof(unsigned long), 1, fp) != 1) { 
+	    perror("Ptdsp_WriteSunMuLawHeader: failed to read magic cookie");
+	    return 0;
+	}
+
+	if (*tmpmagicptr == magic) {
+	    /* The magic cookie is present, so we can just
+	       write the size of the data section.
+	     */
+
+	    /* Get the header size, it might not be SUNMULAW_HEADER_SIZE
+	       if the file was created with an info field.
+	     */  
+	    if ( fread((void *)hdrsizeptr, sizeof(unsigned long), 1, fp) != 1){
+	      perror("Ptdsp_WriteSunMuLawHeader: failed to read hdrsize");
+	      return 0;
+	    }
+
+	    /* The Solaris fopen man pagesays that an input
+	       may not be folloed by output without a fseek, fsetpos
+	       or rewind (or an input operation that encounters an EOF).
+	     */  
+	    if ( fseek(fp, sizeof(unsigned long) * 2, SEEK_SET ) == -1) {
+	        perror("Ptdsp_WriteSunMuLawHeader: ");
+		return 0;
+	    }
+
+	    /* We are now at the proper location to right the datasize */
+	    datasize = filelength - hdrsize;
+	    fwrite((const void *)&datasize, sizeof(unsigned long), 1, fp);
+
+	    if ( fseek(fp, currentposition, SEEK_SET ) == -1) {
+	        perror("Ptdsp_WriteSunMuLawHeader: ");
+		return 0;
+	    }
+	    return 1;
+	} else {
+	    /* The file is non-zero in length, but the Sun mu law .au
+	       file magic cookie is not present, we should add
+	       a header and copy the file, but that should probably
+	       go in a function that takes a filename rather than FILE *.
+	     */
+	    return 0;
+	}
+    }
+}
+
