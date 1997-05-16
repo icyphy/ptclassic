c63e3970f2bf7f1154e7cab797fa47a32529fb4b
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-05-16 15:29:42 +0000

    Redid the indention of the writeCode() method.

--- ptclassic/src/domains/vhdl/targets/SynthArchTarget.cc
+++ ptclassic/src/domains/vhdl/targets/SynthArchTarget.cc
@@ -91,85 +91,85 @@ int SynthArchTarget :: compileCode() {
   // Generate the Synopsys design_analyer command script file here.
 
   if (int(analyze)) {
-  StringList comCode = "";
-  comCode << headerComment("/* \n * ","\n */\n"," * ");
+    StringList comCode = "";
+    comCode << headerComment("/* \n * ","\n */\n"," * ");
 
-  comCode << "/* Synopsys design_analyzer Command Script */\n";
+    comCode << "/* Synopsys design_analyzer Command Script */\n";
 
-  comCode << "\n";
-  comCode << "/* Set default variables. */\n";
-  comCode << "\n";
-  comCode << "designer = \"" << cuserid(NULL) << "\"" << "\n";
-  comCode << "temp = get_unix_variable(\"SYNOPSYS\")" << "\n";
-  comCode << "search_path = \". \" + temp + \"/libraries/syn\"" << "\n";
-  comCode << "link_library = \"* class.db\"" << "\n";
-  comCode << "target_library = \"class.db\"" << "\n";
-  comCode << "symbol_library = \"class.sdb\"" << "\n";
-  comCode << "default_schematic_options = \"-size infinite\"" << "\n";
-  // This is obsolete with v3.3a.
-  //  comCode << "hdlin_source_to_gates_mode = \"high\"" << "\n";
+    comCode << "\n";
+    comCode << "/* Set default variables. */\n";
+    comCode << "\n";
+    comCode << "designer = \"" << cuserid(NULL) << "\"" << "\n";
+    comCode << "temp = get_unix_variable(\"SYNOPSYS\")" << "\n";
+    comCode << "search_path = \". \" + temp + \"/libraries/syn\"" << "\n";
+    comCode << "link_library = \"* class.db\"" << "\n";
+    comCode << "target_library = \"class.db\"" << "\n";
+    comCode << "symbol_library = \"class.sdb\"" << "\n";
+    comCode << "default_schematic_options = \"-size infinite\"" << "\n";
+    // This is obsolete with v3.3a.
+    //  comCode << "hdlin_source_to_gates_mode = \"high\"" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Move to the working directory. */\n";
-  comCode << "\n";
-  comCode << "sh cd " << (const char*) destDirectory << "\n";
+    comCode << "\n";
+    comCode << "/* Move to the working directory. */\n";
+    comCode << "\n";
+    comCode << "sh cd " << (const char*) destDirectory << "\n";
 
-  comCode << "\n";
-  comCode << "/* Analyze the VHDL design file. */\n";
-  comCode << "\n";
-  comCode << "analyze -format vhdl -lib WORK {\""
-	  << filePrefix << ".vhdl" << "\"}" << "\n"; 
+    comCode << "\n";
+    comCode << "/* Analyze the VHDL design file. */\n";
+    comCode << "\n";
+    comCode << "analyze -format vhdl -lib WORK {\""
+	    << filePrefix << ".vhdl" << "\"}" << "\n"; 
 
-  if (int(elaborate)) {
-  comCode << "\n";
-  comCode << "/* Elaborate the design. */\n";
-  comCode << "\n";
-  comCode << "elaborate " << filePrefix
-	  << " -arch \"structure\" -lib WORK -update" << "\n";
+    if (int(elaborate)) {
+      comCode << "\n";
+      comCode << "/* Elaborate the design. */\n";
+      comCode << "\n";
+      comCode << "elaborate " << filePrefix
+	      << " -arch \"structure\" -lib WORK -update" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Create schematics for viewing the design. */\n";
-  comCode << "\n";
-  comCode << "create_schematic -size infinite -gen_database" << "\n";
-  comCode << "create_schematic -size infinite -symbol_view" << "\n";
-  comCode << "create_schematic -size infinite -hier_view" << "\n";
-  comCode << "create_schematic -size infinite -schematic_view" << "\n";
+      comCode << "\n";
+      comCode << "/* Create schematics for viewing the design. */\n";
+      comCode << "\n";
+      comCode << "create_schematic -size infinite -gen_database" << "\n";
+      comCode << "create_schematic -size infinite -symbol_view" << "\n";
+      comCode << "create_schematic -size infinite -hier_view" << "\n";
+      comCode << "create_schematic -size infinite -schematic_view" << "\n";
 
-  if (int(compile)) {
-  comCode << "\n";
-  comCode << "/* Uniquify the design hierarchy to prepare for mapping. */\n";
-  comCode << "\n";
-  comCode << "uniquify" << "\n";
+      if (int(compile)) {
+	comCode << "\n";
+	comCode << "/* Uniquify the design hierarchy to prepare for mapping. */\n";
+	comCode << "\n";
+	comCode << "uniquify" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Compile the design to a gate-mapped version. */\n";
-  comCode << "\n";
-  comCode << "compile -map_effort medium" << "\n";
+	comCode << "\n";
+	comCode << "/* Compile the design to a gate-mapped version. */\n";
+	comCode << "\n";
+	comCode << "compile -map_effort medium" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Set the current design unit. */\n";
-  comCode << "\n";
-  comCode << "current_design = \"" << filePrefix << ".db:" << filePrefix
-	  << "\"" << "\n";
+	comCode << "\n";
+	comCode << "/* Set the current design unit. */\n";
+	comCode << "\n";
+	comCode << "current_design = \"" << filePrefix << ".db:" << filePrefix
+		<< "\"" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Create schematics for viewing the design. */\n";
-  comCode << "\n";
-  comCode << "create_schematic -size infinite -gen_database" << "\n";
-  comCode << "create_schematic -size infinite -schematic_view"
-	  << " -symbol_view -hier_view" << "\n";
+	comCode << "\n";
+	comCode << "/* Create schematics for viewing the design. */\n";
+	comCode << "\n";
+	comCode << "create_schematic -size infinite -gen_database" << "\n";
+	comCode << "create_schematic -size infinite -schematic_view"
+		<< " -symbol_view -hier_view" << "\n";
 
-  if (int(report)) {
-  comCode << "\n";
-  comCode << "/* Generate reports on area and timing. */\n";
-  comCode << "\n";
-  comCode << "report_area" << "\n";
-  comCode << "report_timing -path full -delay max -max_paths 1 -nworst 1" << "\n";
-}
-}
-}  
-  writeFile(comCode, ".com", displayFlag);
-}
+	if (int(report)) {
+	  comCode << "\n";
+	  comCode << "/* Generate reports on area and timing. */\n";
+	  comCode << "\n";
+	  comCode << "report_area" << "\n";
+	  comCode << "report_timing -path full -delay max -max_paths 1 -nworst 1" << "\n";
+	}
+      }
+    }  
+    writeFile(comCode, ".com", displayFlag);
+  }
   // Return TRUE indicating success.
   return TRUE;
 }
