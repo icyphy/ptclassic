3ac34b3388341fddbc89453a2b806772f4b77e80
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-05-31 21:16:20 +0000

    Made the code more modular by adding the methods ReadErrorString to convert an error code into a string and ReadFileHeader which reads the AIFF file header; this rearrangement minimizes the dependence of the AIFF routines on Ptolemy so that we can split into a separate class

--- trunk/ptclassic/src/domains/sdf/contrib/stars/SDFReadAIFF.pl
+++ trunk/ptclassic/src/domains/sdf/contrib/stars/SDFReadAIFF.pl
@@ -21,7 +21,7 @@ limitation of liability, and disclaimer of warranty provisions.
 
 	header {
 	    // AIFF Type Definitions
-		typedef char  ID[4];
+	    typedef char  ID[4];
 
 	    struct  Chunk {
 		ID      ckID;
@@ -33,6 +33,12 @@ limitation of liability, and disclaimer of warranty provisions.
 		ID      ckID;
 		long    ckSize;
 	    };
+
+#define SDFREADAIFF_SUCCESS       0
+#define SDFREADAIFF_NOFILE	 -1
+#define SDFREADAIFF_CANT_READ	 -2
+#define SDFREADAIFF_COMPRESSED	 -3
+#define SDFREADAIFF_UNRECOGNIZED -4
 	}
 
 	output {
@@ -61,6 +67,9 @@ limitation of liability, and disclaimer of warranty provisions.
 	}
 
 	private {
+	    // Methods
+	    const char* ReadErrorString(int ret);
+	    int ReadFileHeader();
 	    int ReadChunk(Chunk &chunk);
 	    int ReadID(ID id);
 	    int ReadLONG(long &item);
@@ -70,6 +79,7 @@ limitation of liability, and disclaimer of warranty provisions.
 	    int ReadChunkHeader(ChunkHeader &chunk);
 	    void extended_to_double(const unsigned char *ps, double &pd);
 
+	    // Data members
 	    pt_ifstream input;
 	    int numChannels;
 	    unsigned long numSampleFrames;
@@ -79,181 +89,70 @@ limitation of liability, and disclaimer of warranty provisions.
 	    int sound_read;
 	}
 
-	destructor  { input.close(); }
+	destructor {
+	    input.close();
+	}
 
 	setup {
-   input.close();
-   input.open(fileName);
-   if(!input)
-   {
-      Error::abortRun(*this, "Unable to open file ", fileName);
-      return;
-   }
-
-   sound_position = -1;
-
-   // Read the form chunk.
-   Chunk FormChunk;
-   if(!ReadChunk(FormChunk))
-   {
-      Error::abortRun(*this, "File is not a valid AIFF file");
-      return;
-   }
-
-   // Ensure this is actually a FORM chunk.
-   if (strncmp(FormChunk.ckID, "FORM", 4) != 0)
-   {
-      Error::abortRun(*this, "File is not a valid AIFF file");
-      return;
-   }
-
-   int isAIFC;
-   if(strncmp(FormChunk.formType, "AIFF", 4) == 0)
-      isAIFC = 0;
-   else if(strncmp(FormChunk.formType, "AIFC", 4) == 0)
-      isAIFC = 1;
-   else
-   {
-      Error::abortRun(*this, "File is not a valid AIFF file");
-      return;
-   }
-
-   // Read the chunks.  Chunks need not be in any particular order.
-
-   ChunkHeader Header;
-   while (ReadChunkHeader(Header))
-   {
-      // Offset to point after the current chunk
-      int seek_offset = input.tellg() + Header.ckSize;
-
-      if (strncmp(Header.ckID, "COMM", 4) == 0) 
-      {
-	 /*
-	  * read comm chunk
-	  */
-
-	 unsigned char xsampleRate[10];
-
-	 ReadSHORT(numChannels);
-	 ReadULONG(numSampleFrames);
-	 ReadSHORT(sampleSize);
-	 input.read(xsampleRate, sizeof(char[10]));
-	 if(!input)
-	 {
-	    Error::abortRun(*this, "Error reading AIFF file");
-	    return;
-	 }
-
-	 extended_to_double(xsampleRate, sampleRate);
-	    
-	 if(isAIFC) 
-	 {
-	    ID compType;
-
-	    /* 
-	     * This is an AIFC audio file.  The format of the common
-	     * data chunk is different from that of the AIFF file.  All
-	     * is the same at the beginning, but there is an additional
-	     * compression type at the end.
-	     */
-
-	    ReadID(compType);
-	    if(strncmp(compType, "NONE", 4))
+	    input.close();
+	    input.open(fileName);
+	    if (!input)
 	    {
-	       Error::abortRun(*this, "Compressed AIFF file not supported.");
-	       return;
+		Error::abortRun(*this, "Unable to open file ", fileName);
+		return;
 	    }
 
-	 }
+	    sound_position = -1;
 
-      } 
-      else if (strncmp(Header.ckID, "SSND", 4) == 0) 
-      {
-	 /*
-	  * read ssnd chunk
-	  */
-	 unsigned long offset;
-	 unsigned long blockSize;
+	    int ret;
+	    if ( (ret = ReadFileHeader()) < 0)
+	    {
+		Error::abortRun(*this, ReadErrorString(ret));
+		return;
+	    }
 
-	 ReadULONG(offset);
-	 ReadULONG(blockSize);
+	    if (sound_position < 0)
+	    {
+		Error::abortRun(*this,
+				"Unable to find sound data in AIFF file.");
+		return;
+	    }
 
-	 /*
-	  * record position of sound data
-	  */
-
-	 sound_position = input.tellg();
-      } 
-      else if (strncmp(Header.ckID, "FVER", 4) == 0)
-      {
-	 /*
-	  * This indicates an AIFF-C file version.  Ensure it
-	  * matches the version for which is program was updated.
-	  */
-
-	 unsigned long timestamp;
-	 ReadULONG(timestamp);
-
-	 /* Ensure this is the correct version of AIFF-C */
-
-         const unsigned long AIFCVersion1 = 0xA2805140l; // Version 1 of AIFF-C
-
-	 if(timestamp != AIFCVersion1)
-	 {
-	    Error::abortRun(*this, "AIFF file format not recognized");
-	    return;
-	 }
-      }
-
-      // Seek to after the current chunk
-      input.seekg(seek_offset);
-      if (!input)
-      {
-	 Error::abortRun(*this, "Error reading AIFF file.");
-	 return;
-      }
-   }
-
-   if(sound_position < 0)
-   {
-      Error::abortRun(*this, "Unable to find sound data in AIFF file.");
-      return;
-   }
-
-   input.clear();
-   input.seekg(sound_position);
-   sound_read = 0;
+	    input.clear();
+	    input.seekg(sound_position);
+	    sound_read = 0;
 	}
 
 	go {
-   if((unsigned long)sound_read >= numSampleFrames)
-   {
-      // End of file.  Check what we are to do.
-      if(haltAtEnd)
-      {
-	 SimControl::requestHalt();
-	 return;
-      }
-      else if(periodic)
-      {
-	 input.seekg(sound_position);
-	 sound_read = 0;
-      }
-   }
+	    if ((unsigned long)sound_read >= numSampleFrames)
+	    {
+		// End of file.  Check what we are to do.
+		if(haltAtEnd)
+		{
+		    SimControl::requestHalt();
+		    return;
+		}
+	        else if (periodic)
+		{
+		    input.seekg(sound_position);
+		    sound_read = 0;
+		}
+	    }
 
-   // Read the sample
-   short sample;
-   ReadSHORT(sample);
-   output %0 << sample;
+	    // Read the sample
+	    short sample;
+	    ReadSHORT(sample);
+	    output%0 << sample;
 
-   // Handle possible multiple channels
-   if(numChannels > 1)
-      ReadSHORT(sample);
+	    // Handle possible multiple channels
+	    if (numChannels > 1) ReadSHORT(sample);
 
-   sound_read++;
+	    sound_read++;
 	}
 
-	wrapup { input.close(); }
+	wrapup {
+	    input.close();
+	}
 
         code {
 /*
@@ -261,7 +160,6 @@ limitation of liability, and disclaimer of warranty provisions.
  *  Description :  This reads an object of type Chunk from disk,
  *                 for any type of machine.
  */
-
 int
 SDFReadAIFF::ReadChunk(Chunk &chunk)
 {
@@ -278,7 +176,6 @@ SDFReadAIFF::ReadChunk(Chunk &chunk)
  *  Description :  This reads an object of type ChunkHeader from disk,
  *                 for any type of machine.
  */
-
 int
 SDFReadAIFF::ReadChunkHeader(ChunkHeader &chunk)
 {
@@ -292,7 +189,6 @@ SDFReadAIFF::ReadChunkHeader(ChunkHeader &chunk)
  *  Name :         SDFReadAIFF::ReadID()
  *  Description :  Reads an AIFF/AIFC file object of type ID.
  */
-
 int
 SDFReadAIFF::ReadID(ID id)
 {
@@ -305,7 +201,6 @@ SDFReadAIFF::ReadID(ID id)
  *  Name :         SDFReadAIFF::ReadLONG()
  *  Description :  Reads an AIFF/AIFC file object of type LONG.
  */
-
 int
 SDFReadAIFF::ReadLONG(long &item)
 {
@@ -330,7 +225,6 @@ SDFReadAIFF::ReadLONG(long &item)
  *  Name :         SDFReadAIFF::ReadULONG()
  *  Description :  Reads an AIFF/AIFC file object of type LONG.
  */
-
 int
 SDFReadAIFF::ReadULONG(unsigned long &item)
 {
@@ -354,7 +248,6 @@ SDFReadAIFF::ReadULONG(unsigned long &item)
  *  Name :         SDFReadAIFF::ReadSHORT()
  *  Description :  Reads an AIFF/AIFC file object of type SHORT
  */
-
 int
 SDFReadAIFF::ReadSHORT(short &item)
 {
@@ -365,13 +258,11 @@ SDFReadAIFF::ReadSHORT(short &item)
    return 1;
 }
 
-
 /*
  *  Name :         SDFReadAIFF::ReadSHORT()
  *  Description :  Reads an AIFF/AIFC file object of type SHORT
  *                 into an integer.  Note:  Unsigned.
  */
-
 int
 SDFReadAIFF::ReadSHORT(int &item)
 {
@@ -382,13 +273,11 @@ SDFReadAIFF::ReadSHORT(int &item)
    return 1;
 }
 
-
 /*
  *  Name :         SDFReadAIFF::extended_to_double()
  *  Description :  Convert stupid Apple extended floating point format
  *                 to a double.
  */
-
 void 
 SDFReadAIFF::extended_to_double(const unsigned char *ps, double &pd)
 {
@@ -434,7 +323,139 @@ SDFReadAIFF::extended_to_double(const unsigned char *ps, double &pd)
 
 }
 
+/*
+ *  Name :         SDFReadAIFF::ReadErrorString()
+ *  Description :  Convert error code return by ReadFileHeader into a string
+ */
+const char*
+SDFReadAIFF::ReadErrorString(int ret)
+{
+    char* str = "";
+
+    switch (ret) {
+      case SDFREADAIFF_NOFILE:
+	str = "File does not exist";
+	break;
+
+      case SDFREADAIFF_CANT_READ:
+	str = "Error while reading the AIFF file";
+	break;
+
+      case SDFREADAIFF_COMPRESSED:
+	str = "Compressed AIFF file not supported.";
+	break;
+
+      case SDFREADAIFF_UNRECOGNIZED:
+	str = "AIFF file format not recognized";
+	break;
+    }
+
+    return str;
 }
+
+/*
+ *  Name :         ReadFileHeader::ReadFileHeader()
+ *  Description :  Read the AIFF file
+ */
+int
+SDFReadAIFF::ReadFileHeader()
+{
+   // Read the form chunk.
+   Chunk FormChunk;
+   if(!ReadChunk(FormChunk)) return SDFREADAIFF_NOFILE;
+
+   // Ensure this is actually a FORM chunk.
+   if (strncmp(FormChunk.ckID, "FORM", 4) != 0) return SDFREADAIFF_NOFILE;
+
+   int isAIFC;
+   if (strncmp(FormChunk.formType, "AIFF", 4) == 0)
+      isAIFC = 0;
+   else if (strncmp(FormChunk.formType, "AIFC", 4) == 0)
+      isAIFC = 1;
+   else
+      return SDFREADAIFF_NOFILE;
+
+   // Read the chunks.  Chunks need not be in any particular order.
+
+   ChunkHeader Header;
+   while (ReadChunkHeader(Header))
+   {
+      // Offset to point after the current chunk
+      int seek_offset = input.tellg() + Header.ckSize;
+
+      if (strncmp(Header.ckID, "COMM", 4) == 0) 
+      {
+	 // Read comm chunk
+	 unsigned char xsampleRate[10];
+
+	 ReadSHORT(numChannels);
+	 ReadULONG(numSampleFrames);
+	 ReadSHORT(sampleSize);
+	 input.read(xsampleRate, sizeof(char[10]));
+
+	 // Check for error while reading
+	 if (!input) return SDFREADAIFF_CANT_READ;
+
+	 extended_to_double(xsampleRate, sampleRate);
+	    
+	 if(isAIFC) 
+	 {
+	    ID compType;
+
+	    /* 
+	     * This is an AIFC audio file.  The format of the common
+	     * data chunk is different from that of the AIFF file.  All
+	     * is the same at the beginning, but there is an additional
+	     * compression type at the end.
+	     */
+
+	    ReadID(compType);
+	    if(strncmp(compType, "NONE", 4)) return SDFREADAIFF_COMPRESSED;
+	 }
+      } 
+      else if (strncmp(Header.ckID, "SSND", 4) == 0) 
+      {
+	 /*
+	  * read ssnd chunk
+	  */
+	 unsigned long offset;
+	 unsigned long blockSize;
+
+	 ReadULONG(offset);
+	 ReadULONG(blockSize);
+
+	 /*
+	  * record position of sound data
+	  */
+
+	 sound_position = input.tellg();
+      } 
+      else if (strncmp(Header.ckID, "FVER", 4) == 0)
+      {
+	 /*
+	  * This indicates an AIFF-C file version.  Ensure it
+	  * matches the version for which is program was updated.
+	  */
+
+	 unsigned long timestamp;
+	 ReadULONG(timestamp);
+
+	 /* Ensure this is the correct version of AIFF-C */
+
+         const unsigned long AIFCVersion1 = 0xA2805140l; // Version 1 of AIFF-C
+
+	 if(timestamp != AIFCVersion1) return SDFREADAIFF_UNRECOGNIZED;
+      }
+
+      // Seek to after the current chunk
+      input.seekg(seek_offset);
+      if (!input) return SDFREADAIFF_CANT_READ;
+   }
+
+   return SDFREADAIFF_SUCCESS;
+}
+
+	}
 
 }
 
