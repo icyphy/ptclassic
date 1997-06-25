76e41b897111dba5402c5ad203bd7568293f91c1
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-06-25 14:54:48 +0000

    Added some comments. Use ::tycho::loadIfNotPresent

--- ptclassic/src/domains/de/java/stars/jnitest.tcl
+++ ptclassic/src/domains/de/java/stars/jnitest.tcl
@@ -1,24 +1,46 @@
 #
 # Tcl script used by the DEJNITest star
 # $Id$
-# Author: Jens Voigt
+# Authors: Jens Voigt, Christopher Hylands
+
+#######################################################################
+#### createjava_$starID
+# Create a Java virtual machine
+#
 proc createjava_$starID {starID} {
     cjvm
     puts " I have a JavaVM now"
 }
 
+#######################################################################
+#### loadlib_$starID
+# Load the libtyjni shared library
+#
 proc loadlib_$starID {starID} {
     puts "loading libtyjni[info sharedlibextension]"
-    load /users/voigtje/java/test/libtyjni.so
+    ::tycho::loadIfNotPresent jnifac tyjni
     puts "library has been loaded"
 }
     
+#######################################################################
+#### new_$starID
+#
+#
 proc new_$starID {starID} {
     
     set inputvals [grabInputsNumber_$starID]
     setOutputFactorial_$starID [jnifac [lindex $inputvals 0]] 
 }
     
+#######################################################################
+#### createjava_$starID
+# Destroy the Java virtual machine
+# Note that calling these seems to crash Ptolemy sometimes!
+# The Java JNI docs say:
+#
+#    You cannot unload the Java Virtual Machine without terminating
+#    the process. The DestroyJavaVM call simply returns an error code. 
+#
 proc destroyjava_$starID {starID} {
     djvm
     puts " I have the JavaVM destroyed"
