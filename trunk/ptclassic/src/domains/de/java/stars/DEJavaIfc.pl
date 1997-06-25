5fbf0476a6c8fa27d97263f169764913f3ccc9d1
ptuser <ptuser@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-06-25 14:41:46 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/de/java/stars/DEJavaIfc.pl
@@ -0,0 +1,26 @@
+defstar {
+	name { JavaIfc }
+	domain { DE }
+        derivedfrom { TclScript }
+	version { 0.1   6/25/97 }
+	author { J. Voigt }
+	copyright {
+            Copyright (c) 1997 Dresden University of Technology,
+            Mobile Communications Systems
+        }
+	desc { }
+        private {
+            int dummy;
+        }
+        begin {
+            // load the library only once
+            tcl.callTclProc("loadlib"); 
+            // now create us a JVM
+            dummy = tcl.callTclProc("createjava");
+        }
+        wrapup {
+            // and clean up
+            tcl.wrapup();
+        }
+    }
+   
