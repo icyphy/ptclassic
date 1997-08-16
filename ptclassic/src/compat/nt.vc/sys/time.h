f09c29ef492a0c3ce71b9cbc7bef3d4462f0672e
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-08-16 19:28:20 +0000

    Removed definition of timercmp macro since both nt4 and nt4.vc need it

--- ptclassic/src/compat/nt.vc/sys/time.h
+++ ptclassic/src/compat/nt.vc/sys/time.h
@@ -3,11 +3,3 @@
 /* FIXME!! NT does not have SIGALRM! */
 #define SIGALRM 4
 #endif
-
-/* src/kernel/TimeVal.cc uses timercmp */
-#define	timercmp(tvp, uvp, cmp) \
-	/* CSTYLED */ \
-	((tvp)->tv_sec cmp (uvp)->tv_sec || \
-	((tvp)->tv_sec == (uvp)->tv_sec && \
-	/* CSTYLED */ \
-	(tvp)->tv_usec cmp (uvp)->tv_usec))
