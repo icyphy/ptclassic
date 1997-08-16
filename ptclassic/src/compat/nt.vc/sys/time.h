b125d3c64926fdda6bee2b860f9c1e5a179c0ad4
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-08-16 17:34:59 +0000

    Added definition of timercmp macro

--- ptclassic/src/compat/nt.vc/sys/time.h
+++ ptclassic/src/compat/nt.vc/sys/time.h
@@ -1,6 +1,13 @@
 #include <winsock2.h>
 #ifdef PT_NT4VC
-// FIXME!! NT does not have SIGALRM!
+/* FIXME!! NT does not have SIGALRM! */
 #define SIGALRM 4
 #endif
 
+/* src/kernel/TimeVal.cc uses timercmp */
+#define	timercmp(tvp, uvp, cmp) \
+	/* CSTYLED */ \
+	((tvp)->tv_sec cmp (uvp)->tv_sec || \
+	((tvp)->tv_sec == (uvp)->tv_sec && \
+	/* CSTYLED */ \
+	(tvp)->tv_usec cmp (uvp)->tv_usec))
