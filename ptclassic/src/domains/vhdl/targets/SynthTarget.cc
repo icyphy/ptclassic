5718bbeb6b9a5e5519ca3cd0aecb2584ddc0a18e
cameron <cameron@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-09-22 18:59:02 +0000

    Cosmetic changes.

--- ptclassic/src/domains/vhdl/targets/SynthTarget.cc
+++ ptclassic/src/domains/vhdl/targets/SynthTarget.cc
@@ -44,14 +44,17 @@ ENHANCEMENTS, OR MODIFICATIONS.
 
 // Constructor.
 SynthTarget :: SynthTarget(const char* name,const char* starclass,
-			 const char* desc) :
-StructTarget(name,starclass,desc) {
-  addState(elaborate.setState("elaborate",this,"YES",
-			    "switch for elaborating design into structure."));
-  addState(compile.setState("compile",this,"YES",
-			    "switch for compiling structure into gates."));
-  addState(precision.setState("precision",this,"INTEGER RANGE 0 to 15",
-			      "Specifies precision for synthesis."));
+			   const char* desc) :
+    StructTarget(name,starclass,desc) {
+	addState(elaborate.setState
+		 ("elaborate",this,"YES",
+		  "switch for elaborating design into structure."));
+	addState(compile.setState
+		 ("compile",this,"YES",
+		  "switch for compiling structure into gates."));
+	addState(precision.setState
+		 ("precision",this,"INTEGER RANGE 0 to 15",
+		  "Specifies precision for synthesis."));
 }
 
 // Clone the Target.
@@ -61,6 +64,7 @@ Block* SynthTarget :: makeNew() const {
 
 static SynthTarget proto("Synth-VHDL", "VHDLStar",
 			 "VHDL code generation target for Synopsys");
+
 static KnownTarget entry(proto,"Synth-VHDL");
 
 void SynthTarget :: setup() {
@@ -74,130 +78,133 @@ void SynthTarget :: setup() {
 
 // Write the code to a file.
 void SynthTarget :: writeCode() {
-  writeFile(myCode,".vhdl",displayFlag);
+    writeFile(myCode,".vhdl",displayFlag);
 
-  // FIXME: Need to do this inside before code is generated instead.
-  // Change all integers to 4-bit types to simplify synthesis.
-  StringList command = "";
-  command << "cd " << (const char*) destDirectory;
-  command << " ; ";
-  command << "rm -f " << "temp" << filePrefix << ".vhdl";
-  command << " ; ";
-  command << "sed s/\" INTEGER\"/\" INTEGER range 0 to 15\"/g ";
-  command << filePrefix << ".vhdl" << " > " << "temp" << filePrefix << ".vhdl";
-  command << " ; ";
-  command << "mv -f " << "temp" << filePrefix << ".vhdl" << " "
-	  << filePrefix << ".vhdl";
-  command << " ; ";
-  command << "sed s/\" integer\"/\" INTEGER range 0 to 15\"/g ";
-  command << filePrefix << ".vhdl" << " > " << "temp" << filePrefix << ".vhdl";
-  command << " ; ";
-  command << "mv -f " << "temp" << filePrefix << ".vhdl" << " "
-	  << filePrefix << ".vhdl";
-  system(command);
+    // FIXME: Need to do this inside before code is generated instead.
+    // Change all integers to 4-bit types to simplify synthesis.
+    StringList command = "";
+    command << "cd " << (const char*) destDirectory;
+    command << " ; ";
+    command << "rm -f " << "temp" << filePrefix << ".vhdl";
+    command << " ; ";
+    command << "sed s/\" INTEGER\"/\" INTEGER range 0 to 15\"/g ";
+    command << filePrefix << ".vhdl" << " > " << "temp" << filePrefix
+	    << ".vhdl";
+    command << " ; ";
+    command << "mv -f " << "temp" << filePrefix << ".vhdl" << " "
+	    << filePrefix << ".vhdl";
+    command << " ; ";
+    command << "sed s/\" integer\"/\" INTEGER range 0 to 15\"/g ";
+    command << filePrefix << ".vhdl" << " > " << "temp" << filePrefix
+	    << ".vhdl";
+    command << " ; ";
+    command << "mv -f " << "temp" << filePrefix << ".vhdl" << " "
+	    << filePrefix << ".vhdl";
+    system(command);
 }
 
 // Write the command script to a file.
 int SynthTarget :: compileCode() {
-  // Generate the Synopsys design_analyer command script file here.
+    // Generate the Synopsys design_analyer command script file here.
 
-  if (int(analyze)) {
-  StringList comCode = "";
-  comCode << headerComment("/* \n * ","\n */\n"," * ");
+    if (int(analyze)) {
+	StringList comCode = "";
+	comCode << headerComment("/* \n * ","\n */\n"," * ");
 
-  comCode << "/* Synopsys design_analyzer Command Script */\n";
+	comCode << "/* Synopsys design_analyzer Command Script */\n";
 
-  comCode << "\n";
-  comCode << "/* Set default variables. */\n";
-  comCode << "\n";
-  comCode << "designer = \"" << cuserid(NULL) << "\"" << "\n";
-  comCode << "temp = get_unix_variable(\"SYNOPSYS\")" << "\n";
-  comCode << "search_path = \". \" + temp + \"/libraries/syn\"" << "\n";
-  comCode << "link_library = \"* class.db\"" << "\n";
-  comCode << "target_library = \"class.db\"" << "\n";
-  comCode << "symbol_library = \"class.sdb\"" << "\n";
-  comCode << "default_schematic_options = \"-size infinite\"" << "\n";
-  // This is obsolete with v3.3a.
-  //  comCode << "hdlin_source_to_gates_mode = \"high\"" << "\n";
+	comCode << "\n";
+	comCode << "/* Set default variables. */\n";
+	comCode << "\n";
+	comCode << "designer = \"" << cuserid(NULL) << "\"" << "\n";
+	comCode << "temp = get_unix_variable(\"SYNOPSYS\")" << "\n";
+	comCode << "search_path = \". \" + temp + \"/libraries/syn\"" << "\n";
+	comCode << "link_library = \"* class.db\"" << "\n";
+	comCode << "target_library = \"class.db\"" << "\n";
+	comCode << "symbol_library = \"class.sdb\"" << "\n";
+	comCode << "default_schematic_options = \"-size infinite\"" << "\n";
+	// This is obsolete with v3.3a.
+	//  comCode << "hdlin_source_to_gates_mode = \"high\"" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Move to the working directory. */\n";
-  comCode << "\n";
-  comCode << "sh cd " << (const char*) destDirectory << "\n";
+	comCode << "\n";
+	comCode << "/* Move to the working directory. */\n";
+	comCode << "\n";
+	comCode << "sh cd " << (const char*) destDirectory << "\n";
 
-  comCode << "\n";
-  comCode << "/* Analyze the VHDL design file. */\n";
-  comCode << "\n";
-  comCode << "analyze -format vhdl -lib WORK {\""
-	  << filePrefix << ".vhdl" << "\"}" << "\n"; 
+	comCode << "\n";
+	comCode << "/* Analyze the VHDL design file. */\n";
+	comCode << "\n";
+	comCode << "analyze -format vhdl -lib WORK {\""
+		<< filePrefix << ".vhdl" << "\"}" << "\n"; 
 
-  if (int(elaborate)) {
-  comCode << "\n";
-  comCode << "/* Elaborate the design. */\n";
-  comCode << "\n";
-  comCode << "elaborate " << filePrefix
-	  << " -arch \"structure\" -lib WORK -update" << "\n";
+	if (int(elaborate)) {
+	    comCode << "\n";
+	    comCode << "/* Elaborate the design. */\n";
+	    comCode << "\n";
+	    comCode << "elaborate " << filePrefix
+		    << " -arch \"structure\" -lib WORK -update" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Create schematics for viewing the design. */\n";
-  comCode << "\n";
-  comCode << "create_schematic -size infinite -gen_database" << "\n";
-  comCode << "create_schematic -size infinite -symbol_view" << "\n";
-  comCode << "create_schematic -size infinite -hier_view" << "\n";
-  comCode << "create_schematic -size infinite -schematic_view" << "\n";
+	    comCode << "\n";
+	    comCode << "/* Create schematics for viewing the design. */\n";
+	    comCode << "\n";
+	    comCode << "create_schematic -size infinite -gen_database" << "\n";
+	    comCode << "create_schematic -size infinite -symbol_view" << "\n";
+	    comCode << "create_schematic -size infinite -hier_view" << "\n";
+	    comCode << "create_schematic -size infinite -schematic_view" << "\n";
 
-  if (int(compile)) {
-  comCode << "\n";
-  comCode << "/* Uniquify the design hierarchy to prepare for mapping. */\n";
-  comCode << "\n";
-  comCode << "uniquify" << "\n";
+	    if (int(compile)) {
+		comCode << "\n";
+		comCode << "/* Uniquify the design hierarchy to prepare for mapping. */\n";
+		comCode << "\n";
+		comCode << "uniquify" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Compile the design to a gate-mapped version. */\n";
-  comCode << "\n";
-  comCode << "compile -map_effort medium" << "\n";
+		comCode << "\n";
+		comCode << "/* Compile the design to a gate-mapped version. */\n";
+		comCode << "\n";
+		comCode << "compile -map_effort medium" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Set the current design unit. */\n";
-  comCode << "\n";
-  comCode << "current_design = \"" << filePrefix << ".db:" << filePrefix
-	  << "\"" << "\n";
+		comCode << "\n";
+		comCode << "/* Set the current design unit. */\n";
+		comCode << "\n";
+		comCode << "current_design = \"" << filePrefix << ".db:" << filePrefix
+			<< "\"" << "\n";
 
-  comCode << "\n";
-  comCode << "/* Create schematics for viewing the design. */\n";
-  comCode << "\n";
-  comCode << "create_schematic -size infinite -gen_database" << "\n";
-  comCode << "create_schematic -size infinite -schematic_view"
-	  << " -symbol_view -hier_view" << "\n";
+		comCode << "\n";
+		comCode << "/* Create schematics for viewing the design. */\n";
+		comCode << "\n";
+		comCode << "create_schematic -size infinite -gen_database" << "\n";
+		comCode << "create_schematic -size infinite -schematic_view"
+			<< " -symbol_view -hier_view" << "\n";
 
-  if (int(report)) {
-  comCode << "\n";
-  comCode << "/* Generate reports on area and timing. */\n";
-  comCode << "\n";
-  comCode << "report_area" << "\n";
-  comCode << "report_timing -path full -delay max -max_paths 1 -nworst 1" << "\n";
-}
-}
-}  
-  writeFile(comCode, ".com", displayFlag);
-}
-  // Return TRUE indicating success.
-  return TRUE;
+		if (int(report)) {
+		    comCode << "\n";
+		    comCode << "/* Generate reports on area and timing. */\n";
+		    comCode << "\n";
+		    comCode << "report_area" << "\n";
+		    comCode << "report_timing -path full -delay max -max_paths 1"
+			    << " -nworst 1" << "\n";
+		}
+	    }
+	}  
+	writeFile(comCode, ".com", displayFlag);
+    }
+    // Return TRUE indicating success.
+    return TRUE;
 }
 
 // Run the code.
 int SynthTarget :: runCode() {
-  if (int(analyze)) {
-    // Startup Synopsys design_analyzer with the command script file here.
-    StringList command = "";
-    if (progNotFound("design_analyzer")) return FALSE;
-    command << "design_analyzer -f " << filePrefix << ".com" << " &";
-    StringList error = "";
-    error << "Could not analyze " << filePrefix << ".vhdl";
-    if (systemCall(command, error, targetHost)) return FALSE;
-  }
-  // Return TRUE indicating success.
-  return TRUE;
+    if (int(analyze)) {
+	// Startup Synopsys design_analyzer with the command script file here.
+	StringList command = "";
+	if (progNotFound("design_analyzer")) return FALSE;
+	command << "design_analyzer -f " << filePrefix << ".com" << " &";
+	StringList error = "";
+	error << "Could not analyze " << filePrefix << ".vhdl";
+	if (systemCall(command, error, targetHost)) return FALSE;
+    }
+    // Return TRUE indicating success.
+    return TRUE;
 }
 
 ISA_FUNC(SynthTarget,StructTarget);
