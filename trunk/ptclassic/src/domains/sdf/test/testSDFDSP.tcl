4b9703cc11edfcb66af110fab30daa6598ab34e7
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-10-01 17:47:00 +0000

    Added SDFFFTCx inverse test

--- trunk/ptclassic/src/domains/sdf/test/testSDFDSP.tcl
+++ trunk/ptclassic/src/domains/sdf/test/testSDFDSP.tcl
@@ -228,9 +228,9 @@ test SDFFIRFix { SDFFIRFix} {
 }}
 
 ######################################################################
-#### test SDFFFTCx
+#### test SDFFFTCx-1
 # 
-test SDFFFTCx { SDFFFTCx} {
+test SDFFFTCx-1 { SDFFFTCx} {
     set star FFTCx
 
     reset __empty__
@@ -315,6 +315,160 @@ test SDFFFTCx { SDFFFTCx} {
 (-1.0,-3.0)	
 }}
 
+
+######################################################################
+#### test SDFFFTCx-2
+# 
+test SDFFFTCx-2 { SDFFFTCx inverse} {
+    set star FFTCx
+
+    reset __empty__
+    domain SDF
+    newuniverse ${star}Test SDF
+    target loop-SDF
+
+    sdfTestRampCx RectToCxA
+    set tmpfile [sdfSetupPrinter]
+    star "$star.a" $star
+
+    connect "$star.a" output Printa input
+    connect RectToCxA output "$star.a" input
+    setstate "$star.a" order 2
+    setstate "$star.a" size 4
+    setstate "$star.a" direction -1
+    run 16
+    wrapup
+    readTmpFile $tmpfile
+} {{(1.5,0.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(5.5,2.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(9.5,4.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(13.5,6.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(17.5,8.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(21.5,10.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(25.5,12.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(29.5,14.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(33.5,16.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(37.5,18.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(41.5,20.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(45.5,22.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(49.5,24.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(53.5,26.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(57.5,28.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+(61.5,30.75)	
+(-0.25,-0.75)	
+(-0.5,-0.25)	
+(-0.75,0.25)	
+}}
+
+######################################################################
+#### test SDFFFTCx-3
+# 
+test SDFFFTCx-3 { Connect 2 SDFFFTCx together, one forward, on inverse } {
+    set star FFTCx
+
+    reset __empty__
+    domain SDF
+    newuniverse ${star}Test SDF
+    target loop-SDF
+
+    sdfTestRampCx RectToCxA
+    set tmpfile [sdfSetupPrinter]
+
+    star "$star.a" $star
+    star "$star.b" $star
+
+    connect "$star.b" output Printa input
+    connect "$star.a" output "$star.b" input
+    connect RectToCxA output "$star.a" input
+
+    setstate "$star.a" order 2
+    setstate "$star.a" size 4
+    setstate "$star.a" direction 1
+    setstate "$star.b" order 2
+    setstate "$star.b" size 4
+    setstate "$star.b" direction -1
+
+    run 8
+    wrapup
+    readTmpFile $tmpfile
+} {{(0.0,0.0)	
+(1.0,0.5)	
+(2.0,1.0)	
+(3.0,1.5)	
+(4.0,2.0)	
+(5.0,2.5)	
+(6.0,3.0)	
+(7.0,3.5)	
+(8.0,4.0)	
+(9.0,4.5)	
+(10.0,5.0)	
+(11.0,5.5)	
+(12.0,6.0)	
+(13.0,6.5)	
+(14.0,7.0)	
+(15.0,7.5)	
+(16.0,8.0)	
+(17.0,8.5)	
+(18.0,9.0)	
+(19.0,9.5)	
+(20.0,10.0)	
+(21.0,10.5)	
+(22.0,11.0)	
+(23.0,11.5)	
+(24.0,12.0)	
+(25.0,12.5)	
+(26.0,13.0)	
+(27.0,13.5)	
+(28.0,14.0)	
+(29.0,14.5)	
+(30.0,15.0)	
+(31.0,15.5)	
+}}
 
 ######################################################################
 #### test SDFIIRFix
