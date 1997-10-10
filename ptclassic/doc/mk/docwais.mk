774c6fcad7079d214e7e901bb0823cbe1ea2f56b
Christopher Brooks <cxh@eecs.berkeley.edu>
1997-10-10 14:38:29 +0000

    Removed webmaker stuff

--- ptclassic/doc/mk/docwais.mk
+++ ptclassic/doc/mk/docwais.mk
@@ -3,45 +3,21 @@
 # Created:	16 Jun 95
 # Create html from framemaker
 
-# 1. Log on to watson, cd /users/ptdesign/doc/user_man or prog_man
-# 2. make mifs
-# 3. Set your display variable, the next step needs it
-# 4. make man_html
-#	This rule will fix a small problem in obtainPtolemy.MIF, and then 
-#	generate the html text files for the users_man
-
-
-# 6. The table of contents and the title page are messed up.  You will need
-#	to copy these from wwwtocs
-#		make update_local_www
-# 7. Adjust the search scripts in each file
-#		make fix_wais_script
-# 8. Copy the files over to the ftp site:
-#		make update_ftp_www
-# 9. Update the wais server.
-#	Log into messier first, or the wais entry will point to the machine you
-#	are running on, which is probably not what you want.
-#	rsh ptolemy "cd /users/ptdesign/doc/users_man; make wais_man"
-# Jose writes:
-# > I wrote a short script to fix the figure bug in webmaker.  After running
-# > webmaker, before running wmgraph run this script on the .MIF files
-# > in the wmwork/graphics directory:
-# !/usr/sww/bin/perl -pi.bak
-# s/PgfNumString/PgfNumFormat/g;
-
-# Notes about tables of contents:
-# Make sure that paragraph headings like 1HeadingTOC map to Ignore.
-# almagest.wml should contain lines like:
-#   PARAGRAPH "1HeadingTOC" TYPE Normal { USES "Ignore" } 
-# If this is not done, you will get lots of html pages that have headers
-# and footers, but no text.
+# Wpublish overview:
+# users_man.wdt is the Quadralay wpublish template file for the user's manual
+# Things to fix:
+# title.doc.html, references.doc: get rid of gifs at the top
+# users_manTOC.doc.html, users_manIX.doc.html: change <a href="#"> to
+#  </a> everywhere. These seem to happen because quadralay can't handle
+#  the extra index markers we use.  There could also be a problem with
+#  nested html, where if the marker is supposed to be in bold, we get hosed.
+#  It would also be a good idea to add a line to the top of these
+#  two files that mention that they could have problems
+# users_manTOC.doc: at the bottom, make sure that the references and index
+#  are visible
+# pigi.doc.html: get rid of table in the front
+# Some of the links in the index and toc don't work
 #
-# All books should contain one and only one paragraph called Title or ContTitle
-# Title and ContTitle are used to trigger production of the table of contents.
-# For Ptolemy docs, try looking in the generated TOC file(i.e. prog_manTOC.MIF)
-# Also, you can use grep \`Title\' *.MIF to search for files that have
-# More than one instance of the Title tag.
-
 
 # Before including this makefile, be sure to set the following variables
 # in your makefile.
@@ -49,16 +25,6 @@
 # VERSION=	0.5.2
 # DOC_NAME=	prog_name
 # DOC_DESCRIPTION= "Ptolemy $(VERSION) Programmers Manual"
-
-# Webmaker binaries
-# To use the wmgraph binary, place /usr/tools/webmaker1.4a/bin.sunos in your path
-#WMCONFIG =	/usr/tools/webmaker1.4a/webmaker/bin/wmconfig
-WEBMAKER =	/usr/tools/webmaker1.4a/webmaker/bin/webmaker
-#WEBMAKER =	/usr/tools/webmaker/webmaker
-#WMGRAPH =	/usr/tools/webmaker1.4a/webmaker/bin/wmgraph
-
-# Webmaker configuration file for Almagest
-ALMAGEST_WML =	$(ROOT)/doc/lib/almagest.wml
 
 # Script to generate Wide Area Information Service (WAIS) indexes
 WAISINDEX =	/usr/tools/wais/wais/waisindex
@@ -70,10 +36,10 @@ WWWDIR =	/vol/ptolemy/pt0/ftp/pub/ptolemy/www
 WAIS_SOURCES =	$(WWWDIR)/wais-sources
 
 # Destination for  html pages
-MAN_ARCHIVEDIR =$(WWWDIR)/$(DOC_NAME).$(VERSION)
+MAN_ARCHIVEDIR =$(WWWDIR)/$(DOC_NAME)$(VERSION).html
 
 # All the .MIF files
-MIFS =		$(DOCS:.doc=.MIF)
+MIFS =		$(DOCS:.doc=.MIF) $(DOCS:.fm=.MIF)
 mifs: $(MIFS)
 
 # All the html files
@@ -87,55 +53,95 @@ htmls: $(HTMLS)
 	echo "SaveAs m $< $@" >> /tmp/fm2mif.fmb		
 	fmbatch -v /tmp/fm2mif.fmb
 	rm -f /tmp/fm2mif.fmb
+
+#%.MIF: %.fm
+#	echo "Open $<" > /tmp/fm2mif.fmb
+#	echo "SaveAs m $< $@" >> /tmp/fm2mif.fmb		
+#	fmbatch -v /tmp/fm2mif.fmb
+#	rm -f /tmp/fm2mif.fmb
+
+%.fm: %.MIF
+	echo "Open $<" > /tmp/mif2fm.fmb
+	echo "SaveAs d $< $@" >> /tmp/mif2fm.fmb		
+	fmbatch -v /tmp/mif2fm.fmb
+	rm -f /tmp/mif2fm.fmb
+
+%.MIF: %.book
 %.MIF: %.book
 	echo "Open $<" > /tmp/fm2mif.fmb
 	echo "SaveAs m $< $@" >> /tmp/fm2mif.fmb		
 	fmbatch -v /tmp/fm2mif.fmb
 	rm -f /tmp/fm2mif.fmb
 
-# Rule to convert mif files to html files
-%.html: %.MIF
-	$(WEBMAKER) -c $(ALMAGEST_WML) -t $(basename $<) $<
+%.txt: %.doc
+	$(PTOLEMY)/doc/bin/fm2txt $^
 
-# Don't run this, you'll clobber the configuration file we created by hand!
-#$(ALMAGEST.WML): users_man.MIF
-#	$(WMCONFIG) -c $@ $<
 
-.PHONY:	man_html man_graph wais_man
-man_html: $(MIFS) $(DOC_NAME).MIF $(OTHER_MIFS)
-	$(WEBMAKER) -c $(ALMAGEST_WML) -t $(DOC_DESCRIPTOR) -n $(SHORT_DOC_NAME) \
-		-w wmwork/www  $(DOC_NAME).MIF
-wmwork/www/next.gif:
-	cp $(dir $(WEBMAKER))/lib/*.gif $(dir $@)
+# Copy over chars for the title
+CHARS= captau.gif eta.gif epsilon.gif capalpha.gif lambda.gif alpha.gif \
+	gamma.gif sigma.gif tau.gif \
+	capmu.gif iota.gif chi.gif rho.gif omicron.gif \
+	pi.gif
 
-#man_graph:
-#	@echo "For best results run on babbage (image path name problems)"
-#	$(WMGRAPH) -G -n $(DOC_NAME)
-
-#  Tar file containing html version (used by mirror sites)
-$(DOC_NAME).$(VERSION)_html.tar.gz:
-	(cd wmwork/www; /usr/sww/bin/gtar -zcf ../../$@ .)
-
-# Fix the wais script references.  Each volume of the manual has a separate
-# wais database
-fix_wais_script:
-	for file in `ls wmwork/www/*`; \
+fix_wmwork: wmwork/search.html
+	-cp $(ROOT)/doc/lib/buttons/*.gif wmwork
+	-cp $(ROOT)/doc/lib/search.html wmwork
+	/users/ptdesign/doc/bin/addtoc $(DOCS)
+	(cd wmwork; rm -f idx.html; ln -s $(DOC_NAME)IX.doc.html idx.html)
+	rm -f wmwork/content.html
+	/users/ptdesign/doc/bin/shorttoc $(DOC_NAME)TOC.doc.html > wmwork/content.html
+	#(cd wmwork; rm -f content.html; ln -s $(DOC_NAME)TOC.doc.html content.html)
+	-mkdir wmwork/chars
+	for file in $(CHARS); \
 	do \
-		sed s=cgi-bin/almagest-wais.pl=cgi-bin/$(DOC_NAME)-wais.pl= \
-		$$file > /tmp/fix_wais; \
-		cp  /tmp/fix_wais $$file; \
+		cp /usr/tools/tools2/www/quadralay/htdocs/chars/$$file wmwork/chars; \
 	done
-# Update the ftp site with the html docs and a tar file.
-update_ftp_www: $(MAN_ARCHIVEDIR) \
-			$(DOC_NAME).$(VERSION)_html.tar.gz \
-			wmwork/www/$(DOC_NAME)_1.html
-	(cd wmwork/www; tar -cf - . ) | rsh ptolemy "cd $<; tar -xf -"
-	cp $(DOC_NAME).$(VERSION)_html.tar.gz $(WWWDIR)
 
-$(MAN_ARCHIVEDIR): wmwork/www/$(DOC_NAME)_2.html
-	if [ ! -d $@ ]; then mkdir $@; fi
-wmwork/www/$(DOC_NAME)_1.html:
-	(cd wmwork/www; ln -s $(DOC_NAME)_2.html $(DOC_NAME)_1.html)
+	# Fix the table of contents and index
+	sed 's|<a href="#">|</a>|' wmwork/$(DOC_NAME)TOC.doc.html > $(DOC_NAME)TOC.doc.html.tmp
+	mv $(DOC_NAME)TOC.doc.html.tmp wmwork/$(DOC_NAME)TOC.doc.html
+	sed 's|<a href="#">|</a>|' wmwork/$(DOC_NAME)IX.doc.html > $(DOC_NAME)IX.doc.html.tmp
+	mv $(DOC_NAME)IX.doc.html.tmp wmwork/$(DOC_NAME)IX.doc.html
+	rm -f wmwork/index.html
+	(cd wmwork; ln -s content.html index.html)
+	rm -f wmwork/mif*
+
+addtoc:
+	/users/ptdesign/doc/bin/addtoc $(DOCS)
+
+wmwork/search.html:
+	@echo "<html>" >> $@
+	@echo "<head>" >> $@
+	@echo "<title>Search $(DOC_DESCRIPTOR)<title>" >> $@
+	@echo "<body bgcolor=\"#faf0e6\">" >> $@
+	@echo "<h1>Search $(DOC_DESCRIPTOR)<h1>" >> $@
+	@echo "Click on the link below to go to the search page." >> $@
+	@echo "<p>" >> $@
+	@echo "<A HREF=\"http://ptolemy.eecs.berkeley.edu/cgi-bin/$(DOC_NAME)-wais.pl"><IMG ALIGN = BOTTOM SRC="search.gif" ALT=\"Search\"></a>" >> $@
+	@echo "<body>" >> $@
+	@echo "<html>" >> $@
+
+####################################################
+# Build tar files for the PostScript, PDF and HTML
+
+tars:	ps.tar pdf.tar html.tar
+ps.tar:
+	$(MAKE) fm2ps
+	(cd ..; gtar -zcf $(DOC_NAME)/$(DOC_NAME).tar.gz $(DOC_NAME)/*.ps \
+		$(DOC_NAME)/print_$(DOC_NAME)ual)
+
+pdf.tar:
+	$(MAKE) fm2ps
+	distill *.ps
+	(cd ..; gtar -zcf $(DOC_NAME)/$(DOC_NAME).pdf.tar.gz $(DOC_NAME)/*.pdf)
+
+html.tar:
+	@echo "We are assuming that you ran wpublish and converted the frame files"
+	rm -rf $(DOC_NAME)$(VERSION).html
+	$(MAKE) fix_wmwork
+	cp -r wmwork $(DOC_NAME)$(VERSION).html
+	gtar -zcf $(DOC_NAME)$(VERSION).html.tar.gz $(DOC_NAME)$(VERSION).html
+
 
 wais_man:
 	@echo "Run this from messier so the host name for the database is right"		
@@ -144,7 +150,14 @@ wais_man:
 	 -t URL $(WWWDIR) http://ptolemy.eecs.berkeley.edu \
 	 -r $(MAN_ARCHIVEDIR)
 
-#	 -t URL /users/cxh/public_html/wmwork/www \
-#		http://ptolemy.eecs.berkeley.edu/~cxh/wmwork/www \
-#	 -r /users/cxh/public_html/wmwork/www 
 
+####################################################
+# Generate a text only version of the document.  This is useful
+# for spell checking everything at once, in case there are bogus
+# words in the dictionary.
+all.txt:
+	$(FM2TXT) $(DOCS)
+	cat $(DOCS:.doc=.txt) > all.txt
+
+fmdoall:
+	../bin/fmdoall $(DOCS)
