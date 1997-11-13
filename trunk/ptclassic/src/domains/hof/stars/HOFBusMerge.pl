a21f52350204ad69542d3285ff4d68367b4260c5
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1997-11-13 15:51:07 +0000

    (tgl)6/24/97: changes for HOF stars adjacent to galaxy boundaries.

--- trunk/ptclassic/src/domains/hof/stars/HOFBusMerge.pl
+++ trunk/ptclassic/src/domains/hof/stars/HOFBusMerge.pl
@@ -2,6 +2,15 @@ defstar {
 	name {BusMerge}
 	domain {HOF}
 	derivedfrom {Base}
+	version { $Id$ }
+	author { Edward A. Lee, Tom Lane  }
+	location { HOF main library }
+	copyright {
+Copyright (c) 1994-%Q% The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
 	desc {
 Bridge inputs to outputs and then self-destruct.
 This star merges two input busses into a single bus.
@@ -10,15 +19,7 @@ bus width is N, then we require that N = M1 + M2.
 The first M1 outputs come from the first input bus,
 while the next M2 outputs come from the second input bus.
 	}
-	version {$Id$}
-	author { E. A. Lee }
-	copyright {
-Copyright (c) 1990-%Q% The Regents of the University of California.
-All rights reserved.
-See the file $PTOLEMY/copyright for copyright notice,
-limitation of liability, and disclaimer of warranty provisions.
-	}
-	location { HOF main library }
+
 	outmulti {
 		name {output}
 		type {anytype}
@@ -31,29 +32,22 @@ limitation of liability, and disclaimer of warranty provisions.
 	        name {bottom}
 		type {anytype}
 	}
-	ccinclude {"Galaxy.h"}
-
 
 	method {
-	  name { preinitialize }
-	  access { public }
+	  name { doExpansion }
+	  type { int }
 	  code {
-	    HOFBase::preinitialize();
-
 	    // Make sure we know the number of connections on the
 	    // input and output multiPortHoles.
-	    initConnections(output);
-	    initConnections(top);
-	    initConnections(bottom);
+	    if (! initConnections(output)) return 0;
+	    if (! initConnections(top)) return 0;
+	    if (! initConnections(bottom)) return 0;
 
 	    MPHIter nexttop(top);
 	    MPHIter nextbottom(bottom);
 	    // start with the top inputs
 	    MPHIter *nexti = &nexttop;
 	    MPHIter nexto(output);
-	    PortHole *source, *sink;
-	    const char *sourceDelayVals, *sinkDelayVals;
-	    int numInDelays, numOutDelays;
 
 	    PortHole *pi, *po;
 	    while ((po = nexto++) != 0) {
@@ -62,55 +56,24 @@ limitation of liability, and disclaimer of warranty provisions.
 		nexti = &nextbottom;
 		if ((pi = (*nexti)++) == 0) {
 		  // Still out of inputs
-		  Error::abortRun(*this,"Not enough inputs for the given number of outputs");
-		  return;
+		  Error::abortRun(*this,
+				  "Not enough inputs for the given number of outputs");
+		  return 0;
 		}
 	      }
-	      if((source = pi->far()) == 0 ||
-		 (sink = po->far()) == 0) {
-		Error::abortRun(*this,"Star is not fully connected");
-		return;
-	      }
-	      sourceDelayVals = pi->initDelayValues();
-	      sinkDelayVals = po->initDelayValues();
-	      numInDelays = pi->numInitDelays();
-	      numOutDelays = po->numInitDelays();
-
-	      // Get alias pointers before disconnecting
-	      GenericPort *gpo = aliasPointingAt(po);
-	      GenericPort *gpi = aliasPointingAt(pi);
-
-	      source->disconnect();
-	      sink->disconnect();
-
-	      int numDelays = 0;
-	      const char* delayVals = 0;
-	      if ((numInDelays > 0) || (sourceDelayVals && *sourceDelayVals)) {
-		numDelays = numInDelays;
-		delayVals = sourceDelayVals;
-		if ((numOutDelays > 0) || (sinkDelayVals && *sinkDelayVals)) {
-		  Error::warn(*this,
-			      "Cannot have delays on inputs and outputs."
-			      " Using input value");
-		}
-	      } else {
-		numDelays = numOutDelays;
-		delayVals = sinkDelayVals;
-	      }
-	      connectPorts(*source,*sink,numDelays,delayVals);
-	      fixAliases(gpi,pi,sink);
-	      fixAliases(gpo,po,source);
+	      if (!crossConnect(pi, po))
+		return 0;
 	    }                        // while loop
+
 	    // Out of outputs at this point.
 	    // Check to be sure we are also out of inputs.
 	    if ((nextbottom++ != 0) || (nexttop++ != 0)) {
-	      Error::abortRun(*this,"Not enough outputs for the given number of inputs");
-	      return;
+	      Error::abortRun(*this,
+			      "Not enough outputs for the given number of inputs");
+	      return 0;
 	    }
-	    // Now remove ourselves from the parent galaxy and self-destruct
-	    Galaxy* mom = idParent();
-	    if(!mom) return;
-	    mom->deleteBlockAfterInit(*this);
+
+	    return 1;
 	  }
 	}
 }
--- trunk/ptclassic/src/domains/hof/stars/HOFSrcGr.pl
+++ trunk/ptclassic/src/domains/hof/stars/HOFSrcGr.pl
@@ -2,6 +2,15 @@ defstar {
 	name { SrcGr }
 	domain { HOF }
 	derivedFrom { MapGr }
+	version { $Id$ }
+	author { Edward A. Lee, Tom Lane }
+	location { HOF main library }
+	copyright {
+Copyright (c) 1994-%Q% The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+	}
 	desc {
 Create one or more instances of the named block to produce the
 required number of output stream(s).
@@ -11,15 +20,6 @@ The replacement block(s) are connected as illustrated by the example
 block that is connected.
 Their parameters are determined by "parameter_map".
 	}
-	version { $Id$ }
-	author { Edward A. Lee }
-	copyright {
-Copyright (c) 1990-%Q% The Regents of the University of California.
-All rights reserved.
-See the file $PTOLEMY/copyright for copyright notice,
-limitation of liability, and disclaimer of warranty provisions.
-	}
-	location { HOF main library }
 	htmldoc {
 See the explanation for the
 <tt>MapGr</tt>
