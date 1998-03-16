8c918fcdb494efffcb7876514140553ef5c4f8f0
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1998-03-16 18:30:26 +0000

    Added ptolemy-specific Tycho initialization

--- trunk/ptclassic/tycho/typt/kernel/kernel.tcl
+++ trunk/ptclassic/tycho/typt/kernel/kernel.tcl
@@ -35,9 +35,69 @@
 # Programming" book
 
 package provide tycho.typt.kernel 0.2.1
+
 global env auto_path
-set env(KERNEL_LIBRARY) [file dirname [info script]]
-if { [lsearch -exact $auto_path $env(KERNEL_LIBRARY)] == -1 } {
-    lappend auto_path $env(KERNEL_LIBRARY)
+set env(PTKERNEL_LIBRARY) [file dirname [info script]]
+if { [lsearch -exact $auto_path $env(PTKERNEL_LIBRARY)] == -1 } {
+    lappend auto_path $env(PTKERNEL_LIBRARY)
 }
 
+### PTOLEMY-SPECIFIC INITIALIZATION
+
+global ::ptolemyfeature
+
+# ptolemyfeature(ptolemy) is set to 1 if ptolemy is present
+set ptolemyfeature(ptolemy) 0
+
+# ptolemyfeature(ptolemyinstalled) is set if the file
+# $ptolemy/copyright exists.
+# This is not the same as having ptolemy present,
+# which means the executing binary can execute ptolemy commands.
+set ptolemyfeature(ptolemyinstalled) \
+        [file exists [file join $ptolemy copyright]]
+
+# ptolemyfeature(pitcl) is set to 1 if the ::pitcl commands are present,
+# which indicates that we have access to the ptolemy kernel
+# via ::pitcl
+if {[uplevel #0 info namespace all pitcl] == "pitcl"} {
+    set ptolemyfeature(pitcl) 1
+    set ptolemyfeature(ptolemy) 1
+} {
+    set ptolemyfeature(pitcl) 0
+}
+
+# ptolemyfeature(ptcl) is set to 1 if the ptcl commands are present,
+# which indicates that we have access to the ptolemy kernel
+# via the older ptcl interface
+if {[uplevel #0 info commands domains] !=  {}} {
+    set ptolemyfeature(ptcl) 1
+    set ptolemyfeature(ptolemy) 1
+} {
+    set ptolemyfeature(ptcl) 0
+}
+
+# ptolemyfeature(octtools) is set to 1 if the ptk commands are present,
+# which indicates that we have access to the tcl/octtools commands.
+if {[uplevel #0 info commands ptkOpenFacet] != {} && \
+	[uplevel #0 info commands pvOpenWindow] != {}} {
+    set ptolemyfeature(octtools) 1
+    set ptolemyfeature(ptolemy) 1
+} {
+    set ptolemyfeature(octtools) 0
+}
+
+# Check to see if there is a configuration problem
+if {$ptolemyfeature(ptolemy) == 1 && $ptolemyfeature(ptolemyinstalled) == 0} {
+    puts stderr "Warning: This binary is ptolemy enabled, but we failed\nto\
+	    detect a complete ptolemy installation because\n\
+	    `[file join $ptolemy copyright]' is missing."
+    set ptolemyfeature(ptolemyinstalled) 1
+}
+
+
+### CATEGORIES
+global ::ptolemyfeature
+if $ptolemyfeature(ptolemyinstalled) {
+    ::tycho::register category new "ptolemy" -label "Ptolemy Tools" -underline 6
+    ::tycho::register category open "ptolemy" -label "Open Ptolemy Tools"
+}
