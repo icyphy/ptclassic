17fe248bba363a91d16e17b4b6e0e2f81aff9297
Christopher Brooks <cxh@eecs.berkeley.edu>
1998-04-23 10:08:43 +0000

    Fixed sccs keywords

--- ptclassic/src/octtools/octredomain/octredomain.c
+++ ptclassic/src/octtools/octredomain/octredomain.c
@@ -2,7 +2,7 @@
  * $Id$
  */
 /*
-Copyright (c) 1990-$Date$ The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
