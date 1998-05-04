7c4e34f0ca4e723a9c7207b9bbf71b996f55098a
ble <ble@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1998-05-04 16:05:47 +0000

    Added a new makeSymmetric state: when set to YES, the output is an odd number of samples which is a standard symmetric autocorrelation sequence; when makeSymmetric is NO, the output is an even number of samples (one endpoint is not output) which is more suitable for spectral analysis; the default value of makeSymmetric is NO for backward compatibility

--- trunk/ptclassic/src/domains/sdf/dsp/stars/SDFAutocor.pl
+++ trunk/ptclassic/src/domains/sdf/dsp/stars/SDFAutocor.pl
@@ -4,7 +4,9 @@ defstar {
 	version {$Id$}
 	desc { 
 Estimate an autocorrelation function by averaging input samples.
-Both biased and unbiased estimates are supported.
+Both biased and unbiased estimates are supported.  By default,
+the last sample in the autocorrelation sequence is not output
+so as to make the sequence have an even number of samples.
 	}
 	author { E. A. Lee }
 	copyright {
@@ -18,8 +20,12 @@ limitation of liability, and disclaimer of warranty provisions.
 <p>
 Estimates a certain number of samples of the autocorrelation of the input
 <a name="autocorrelation"></a>
-by averaging a certain number of input samples.  The number of outputs
-is twice the number of lags requested.  This makes the output almost
+by averaging a certain number of input samples.
+<p>
+If the parameter <i>makeSymmetric</i> is NO, then the output
+will be symmetric and contain a number of samples equal to twice
+the number of lags requested plus one.  Otherwise, the output
+will be twice the number of lags requested which will be almost
 symmetric (discard the last sample to get a perfectly symmetric output).
 <p>
 If the parameter <i>unbiased</i> is NO, then
@@ -81,14 +87,35 @@ autocorrelation estimate may have negative components.
 		default {"YES"}
 		desc { If YES, the estimate will be unbiased.}
 	}
+	defstate {
+		name {makeSymmetric}
+		type {int}
+		default {"NO"}
+		desc {
+If YES, then the star will output 2*noLags+1 samples (an odd number)
+whose values are symmetric about the midpoint.
+If NO, then the star will output 2*noLags samples (an even number)
+by omitting one of the endpoints. }
+	}
 	setup {
 	    if ( int(noInputsToAvg) <= int(noLags)) {
 		Error::abortRun(*this,
 			": noLags is larger than noInputsToAvg.");
 		return;
 	    }
+
 	    input.setSDFParams(int(noInputsToAvg), int(noInputsToAvg)-1);
-	    output.setSDFParams(2*int(noLags), 2*int(noLags));
+
+	    // 2*int(noLags)+1 samples of the autocorrelation sequence
+	    // are always computed so the buffer size on the output port
+	    // is always 2*int(noLags)+1 samples.  This buffer size is
+	    // specified by the second argument to setSDFParams.
+	    // The number of samples produced is either 2*int(noLags) or
+	    // 2*int(noLags)+1 depending on the value of makeSymmetric. BLE
+	    if ( int(makeSymmetric) )
+	        output.setSDFParams(2*int(noLags)+1, 2*int(noLags));
+	    else
+	        output.setSDFParams(2*int(noLags), 2*int(noLags));
 	}
 	go {
 	    int i;
@@ -96,8 +123,8 @@ autocorrelation estimate may have negative components.
 		double sum = 0.0;
 		for(int j = 0; j < (int(noInputsToAvg)-i); j++)
 		   sum += double(input%j) * double(input%(j+i));
-		if (unbiased)
-		    output%(i+int(noLags)) << sum/(int(noInputsToAvg) -i);
+		if ( int(unbiased) )
+		    output%(i+int(noLags)) << sum/(int(noInputsToAvg) - i);
 		else
 		    output%(i+int(noLags)) << sum/int(noInputsToAvg);
 	    }
