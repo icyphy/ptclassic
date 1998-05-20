b53da173815931917265b0f3d8e989e276a1e2cb
jamesl <jamesl@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1998-05-20 14:55:04 +0000

    Initial revision

---
+++ trunk/ptclassic/src/domains/acs/stars/ACSConstFPCGC.pl
@@ -0,0 +1,24 @@
+defcore {
+    name { Const }
+    domain { ACS }
+    coreCategory { FPCGC }
+    corona { Const } 
+    desc { Output the constant level as a floating-point value. }
+    version { @(#)ACSConstFPCGC.pl	1.1	05/07/98 }
+    author { Eric Pauer }
+    copyright {
+Copyright (c) 1998 The Regents of the University of California.
+All rights reserved.
+See the file $PTOLEMY/copyright for copyright notice,
+limitation of liability, and disclaimer of warranty provisions.
+    }
+    location { ACS main library }
+
+	go { addCode(std); }
+	codeblock (std) {
+	$ref(output) = $ref(level);
+	}
+	exectime {
+		return 1;
+	}
+}
