92068d1934815f112f179a8bde9b99458422756f
Christopher Brooks <cxh@eecs.berkeley.edu>
1998-05-28 18:31:58 +0000

    sccs keywords

--- ptclassic/src/domains/de/kernel/DERCScheduler.h
+++ ptclassic/src/domains/de/kernel/DERCScheduler.h
@@ -7,7 +7,7 @@
  
 Author: Mudit Goel, Neil Smyth
 
-Copyright (c) 1997- The Regents of the University of California.
+Copyright (c) 1997-%Q% The Regents of the University of California.
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
