ed3f66729c35c19b2c5ece8acac0117d80c58ae9
Christopher Brooks <cxh@eecs.berkeley.edu>
1998-06-22 09:48:32 +0000

    Moved TYDOC to tycho.mk

--- ptclassic/tycho/mk/tycommon.mk
+++ ptclassic/tycho/mk/tycommon.mk
@@ -221,9 +221,6 @@ makefiles: makefile
 # Rules for Itcl/Tcl
 
 # Generate html files from itcl files, requires itclsh and tycho
-# Note that $(ROOT) here is relative to the tycho directory, not
-# the Ptolemy directory.
-TYDOC=$(ROOT)/util/tydoc/bin/tydoc
 itcldocs: $(ITCL_SRCS) $(TCL_SRCS)
 	@if [ "$(TYDOC_DESC)" = "" ] ; then \
 		chmod a+x $(TYDOC); \
