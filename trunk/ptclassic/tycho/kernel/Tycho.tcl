c623a95b0198a4b3c6606fe22e3dc41f4ee04057
johnr <johnr@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1998-07-16 13:37:34 +0000

    typt back in

--- trunk/ptclassic/tycho/kernel/Tycho.tcl
+++ trunk/ptclassic/tycho/kernel/Tycho.tcl
@@ -277,12 +277,12 @@ package require tycho.util.tydoc
 package require tycho.util.idoc
 
 # Load ptolemy packages if they are present
-#if [file exists [file join $TYCHO typt kernel]] {
-#    package require tycho.typt.kernel
-#}
-#if [file exists [file join $TYCHO typt edit]] {
-#    package require tycho.typt.edit
-#}
+if [file exists [file join $TYCHO typt kernel]] {
+    package require tycho.typt.kernel
+}
+if [file exists [file join $TYCHO typt edit]] {
+    package require tycho.typt.edit
+}
 
 ::tycho::_announce "Loaded packages"
 
