5fa0b16bde8fed6e53ddf45f7f2fd93691d900f4
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1999-01-05 08:51:53 +0000

    Removed comment about ptcl

--- trunk/ptclassic/mk/config-nt4.mk
+++ trunk/ptclassic/mk/config-nt4.mk
@@ -43,8 +43,6 @@ include $(ROOT)/mk/config-default.mk
 # Get the g++ definitions; we override some below.
 include $(ROOT)/mk/config-g++.mk
 
-# Under NT, build a static ptcl first
-
 # Get the g++ definitions for shared libraries; we override some below.
 # Comment the next line out if you don't want shared libraries.
 #include $(ROOT)/mk/config-g++.shared.mk
