f8e9a6c04f3c2d5ace0a9f1778f14e9cd82c0b4e
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
1999-09-08 12:18:33 +0000

    Added SCCS Version keywords

--- trunk/ptclassic/src/domains/acs/kernel/ACSCGCore.cc
+++ trunk/ptclassic/src/domains/acs/kernel/ACSCGCore.cc
@@ -25,7 +25,7 @@ ENHANCEMENTS, OR MODIFICATIONS.
 
  Programmers:  James A. Lundblad
  Date of creation: 3/23/98
- Version: @(#)ACSCGCore.cc	1.4	05/26/98
+ Version: $Id$
 
 The ACS CG Core class provides a base class for all derived CG Core category classes. Many instance variables and methods are stolen from CGCStar.
 
