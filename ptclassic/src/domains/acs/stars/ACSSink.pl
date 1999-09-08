754be17e399b81c284e02cdb2759cd93a1dcb293
Christopher Brooks <cxh@eecs.berkeley.edu>
1999-09-08 12:38:00 +0000

    Fixed SCCS version

--- ptclassic/src/domains/acs/stars/ACSSink.pl
+++ ptclassic/src/domains/acs/stars/ACSSink.pl
@@ -4,7 +4,7 @@ defcorona {
 	desc {
 	    Swallows an input sample
 	}
-	version {1.9	12 August 1998}
+	version {$Id$}
 	author { E. A. Lee }
 	copyright {
 	    Copyright (c) 1990-1996 The Regents of the University of California.
