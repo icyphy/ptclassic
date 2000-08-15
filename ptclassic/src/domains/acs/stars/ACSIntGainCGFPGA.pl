35dfce9939cd69b888459b8fa4612c245d5a3f05
Christopher Brooks <cxh@eecs.berkeley.edu>
2000-08-15 17:07:51 +0000

    Changed copyright to 2000

--- ptclassic/src/domains/acs/stars/ACSIntGainCGFPGA.pl
+++ ptclassic/src/domains/acs/stars/ACSIntGainCGFPGA.pl
@@ -9,7 +9,7 @@ Produces constant coefficient multiply
 	version {$Id$
 	author { P. Fiore }
 	copyright {
-Copyright (c) 1998-1999 Sanders, a Lockheed Martin Company
+Copyright (c) 1998-%Q% Sanders, a Lockheed Martin Company
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
