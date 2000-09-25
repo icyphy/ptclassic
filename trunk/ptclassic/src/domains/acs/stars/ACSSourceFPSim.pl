192f86706e086171bbb161ef6075fdd2bccbd54d
cxh <cxh@392b1ab8-e989-4fed-86a6-4ff00515ffac>
2000-09-25 22:03:45 +0000

    From Eric Pauer.

--- trunk/ptclassic/src/domains/acs/stars/ACSSourceFPSim.pl
+++ trunk/ptclassic/src/domains/acs/stars/ACSSourceFPSim.pl
@@ -1,21 +1,18 @@
 defcore {
-    name { Source }
-    domain { ACS }
-    coreCategory { FPSim }
-    corona { Source } 
-    desc { Generic code generator source star. }
-    version {$Id$}
-    author { Eric Pauer }
-    copyright {
+	name { Source }
+	domain { ACS }
+	coreCategory { FPSim }
+	corona { Source }
+	desc {
+	    Discards input samples.
+	}
+	version {$Id$}
+	author { Eric Pauer }
+	copyright {
 Copyright (c) 1999 Sanders, a Lockheed Martin Company
-All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
-    }
-    location { ACS main library }
-
-	go {
-	    corona.output%0 << (double)(0.0);
 	}
-
+	location { ACS main library }
 }
+
