43330dfbce6a07a2eccedfe63799048426525fc2
Christopher Brooks <cxh@eecs.berkeley.edu>
2001-08-02 11:25:16 +0000

    Fixed copyright

--- ptclassic/src/domains/acs/stars/ACSRamDelayFixSim.pl
+++ ptclassic/src/domains/acs/stars/ACSRamDelayFixSim.pl
@@ -7,7 +7,7 @@ defcore {
     version { $Id$}
     author { Eric Pauer }
     copyright {
-Copyright (c) 1999, Sanders, a Lockheed Martin Company
+Copyright (c) 1999-%Q% Sanders, a Lockheed Martin Company
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
