a6bceafc3764a6436431eed30f695f1af8f2c5d2
Christopher Brooks <cxh@eecs.berkeley.edu>
2001-08-02 11:14:23 +0000

    sccs keywords

--- ptclassic/src/domains/acs/stars/ACSDelay.pl
+++ ptclassic/src/domains/acs/stars/ACSDelay.pl
@@ -4,10 +4,10 @@ defcorona {
 	desc {
 	    Generates a pipelined delay element for multiple lines
 	}
-	version {@(#)ACSDelay.pl	1.4 05/15/00}
+	version { $Id$}
 	author { K. Smith }
 	copyright {
-Copyright (c) 1990-1996 The Regents of the University of California.
+Copyright (c) 1990-%Q% The Regents of the University of California.
 All rights reserved.
 See the file $PTOLEMY/copyright for copyright notice,
 limitation of liability, and disclaimer of warranty provisions.
