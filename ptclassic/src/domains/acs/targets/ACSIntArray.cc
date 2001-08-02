a5fb0c363002c07809f0ea8991ad8867b00772e1
Christopher Brooks <cxh@eecs.berkeley.edu>
2001-08-02 10:54:18 +0000

    %Q

--- ptclassic/src/domains/acs/targets/ACSIntArray.cc
+++ ptclassic/src/domains/acs/targets/ACSIntArray.cc
@@ -1,5 +1,5 @@
 /**********************************************************************
-Copyright (c) 1999- Sanders, a Lockheed Martin Company
+Copyright (c) 1999-%Q% Sanders, a Lockheed Martin Company
 All rights reserved.
 
 Permission is hereby granted, without written agreement and without
